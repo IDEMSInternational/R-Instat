dc1a5c273c660d1d5029d378c7fc6370ec9090f5
deaspo <polycarpokock@live.com>
2015-10-18 09:32:26 +0300

    Modifications of the added dialogues.

--- instat/My Project/Application.Designer.vb
+++ instat/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            MainForm = Global.instat.frm_main
+            Me.MainForm = Global.instat.dlgDescriptiveStatistic
         End Sub
     End Class
 End Namespace
--- instat/dlgBoxPlot.Designer.vb
+++ instat/dlgBoxPlot.Designer.vb
@@ -1,17 +1,14 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
 Partial Class dlgBoxPlot
-    Inherits System.Windows.Forms.Form
+    Inherits instat.GraphicsDialogTemplate
 
     'Form overrides dispose to clean up the component list.
     <System.Diagnostics.DebuggerNonUserCode()> _
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-        Try
-            If disposing AndAlso components IsNot Nothing Then
-                components.Dispose()
-            End If
-        Finally
-            MyBase.Dispose(disposing)
-        End Try
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
     End Sub
 
     'Required by the Windows Form Designer
@@ -22,16 +19,7 @@ Partial Class dlgBoxPlot
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.SuspendLayout()
-        '
-        'dlgBoxPlot
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(356, 403)
-        Me.Name = "dlgBoxPlot"
-        Me.Text = "Graphics Dialog Template"
-        Me.ResumeLayout(False)
-
+        components = New System.ComponentModel.Container
     End Sub
+
 End Class
