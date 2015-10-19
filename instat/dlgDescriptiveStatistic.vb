a9e6ca28fa74392dc2b42442feb2c682e0ec84af
deaspo <polycarpokock@live.com>
2015-10-19 05:29:54 +0300

    Working copy before adding the function to rea excel,  spss, stata

--- instat/dlgDescriptiveStatistic.vb
+++ instat/dlgDescriptiveStatistic.vb
@@ -31,4 +31,18 @@
 
         End If
     End Sub
+
+    Private Sub CheckBox13_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox13.CheckedChanged
+
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+
+    End Sub
+
+    Private Sub CheckBox3_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox3.CheckedChanged
+
+    End Sub
+
+
 End Class
