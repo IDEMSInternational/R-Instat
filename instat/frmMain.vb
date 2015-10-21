046d73b4ab02f97338ab88bc20464826a1077ce9
deaspo <polycarpokock@live.com>
2015-10-21 17:42:33 +0300

    Updating the Polycarp's repo and the main branch with the upto date changes for the Stast package being developed.

--- instat/frmMain.vb
+++ instat/frmMain.vb
@@ -1,3 +1,14 @@
 ﻿Public Class frmMain
-
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        frmEditor.MdiParent = Me
+        frmCommand.MdiParent = Me
+        frmGraphics.MdiParent = Me
+        frmLog.MdiParent = Me
+        frmScript.MdiParent = Me
+        Me.LayoutMdi(MdiLayout.ArrangeIcons)
+        frmCommand.Dock = DockStyle.Right
+        frmEditor.Dock = DockStyle.Left
+        frmCommand.Show()
+        frmEditor.Show()
+    End Sub
 End Class
---
+++ instat/frmScript.vb
@@ -0,0 +1,3 @@
+﻿Public Class frmScript
+
+End Class
