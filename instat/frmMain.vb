6bbbff0a5ebb615559b1c0b10b18343898cd9fa2
deaspo <polycarpokock@live.com>
2015-11-03 10:43:33 +0300

    Final nail to fixing conflicts

--- instat/frmMain.vb
+++ instat/frmMain.vb
@@ -1,20 +1,4 @@
-﻿' Instat-R
-' Copyright (C) 2015
-'
-' This program is free software: you can redistribute it and/or modify
-' it under the terms of the GNU General Public License as published by
-' the Free Software Foundation, either version 3 of the License, or
-' (at your option) any later version.
-'
-' This program is distributed in the hope that it will be useful,
-' but WITHOUT ANY WARRANTY; without even the implied warranty of
-' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-' GNU General Public License for more details.
-'
-' You should have received a copy of the GNU General Public License k
-' along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-Imports RDotNet
+﻿Imports RDotNet
 Imports System.IO
 Public Class frmMain
 
@@ -75,6 +59,9 @@ Public Class frmMain
 
     Private Sub DescribeToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles DescribeToolStripMenuItem.Click
         'dlgDescriptiveStatistics.Show()
+    End Sub
+    Private Sub BoxplotToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles BoxplotToolStripMenuItem.Click
+        frmBoxPlot1.Show()
     End Sub
 
     Private Sub mnuFileOpenWorksheet_Click(sender As Object, e As EventArgs) Handles mnuFileOpenWorksheet.Click
