599b8e32b76ca2603bcbf64217d6f7beea691096
deaspo <polycarpokock@live.com>
2015-11-05 08:10:36 +0300

    Changes from Stephen

---
+++ instat/ucrReceiverMultiple.vb
@@ -0,0 +1,9 @@
+﻿Public Class ucrReceiverMultiple
+    Public Overloads Sub AddSelected(lstAvailableVariableSelectedItems)
+
+    End Sub
+
+    Private Sub lstSelectedVariables_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstSelectedVariables.SelectedIndexChanged
+
+    End Sub
+End Class
