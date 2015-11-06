d6660580a433f7af2e2e3de71229c5f74881c250
deaspo <polycarpokock@live.com>
2015-11-06 07:07:43 +0300

    Restore and updating of the  Rsyntax

--- instat/ucrReceiverMultiple.vb
+++ instat/ucrReceiverMultiple.vb
@@ -1,20 +1,5 @@
 ﻿Public Class ucrReceiverMultiple
-    Friend Shared Event removeSelectedItem(ByVal myString As String)
-
-    Public Sub setValues(ByVal myString As String)
-        lstSelectedVariables.Items.Add(myString)
-    End Sub
-    Public Sub removeValues(ByVal myString As String)
-        lstSelectedVariables.Items.Remove(myString)
-    End Sub
-    Public Sub New()
-
-        ' This call is required by the designer.
-        InitializeComponent()
-
-        ' Add any initialization after the InitializeComponent() call.
-
-        AddHandler ucrSelector.getSelectedItem, AddressOf setValues
+    Public Overloads Sub AddSelected(lstAvailableVariableSelectedItems)
 
     End Sub
 End Class
