e5f13025e77cb4697f4d3a96d4543d2d5e1de9c4
deaspo <polycarpokock@live.com>
2015-11-06 12:23:17 +0300

    Added dialogs, modified Rlink and Ryntax and Regular Sequence.

--- instat/rSyntax.vb
+++ instat/rSyntax.vb
@@ -17,24 +17,31 @@
 
 Public Class RSyntax
     Dim strFunction As String
-    Dim strParameter As String()
+    Dim strParameter(1, 0) As String
 
     Public Sub SetFunction(strFunctionName As String)
         strFunction = strFunctionName
     End Sub
 
     Public Sub AddParameter(strParameterName As String, strParameterValue As String)
-        strParameter = {strParameterName, strParameterValue}
-        Dim boundA As Integer = strParameter.GetUpperBound(0)
-        'Dim boundB As Integer = strParameter.GetUpperBound(1)
-        If strParameter.Length < 1 Then
-            strParameter = {strParameterName, strParameterValue}
-            'Else
-            '    For i As Integer = 0 To boundA
-            '        For j As Integer = 0 To boundB
-            '            strParameter = {strParameterName, strParameterValue}
-            '        Next
-            '    Next
+        Dim iBound, i, j As Integer
+        Dim bNew As Boolean = True
+        If strParameter(0, 0) Is Nothing Then
+            strParameter(0, 0) = strParameterName
+            strParameter(1, 0) = strParameterValue
+        Else
+            iBound = strParameter.GetUpperBound(1)
+            For i = 0 To iBound
+                If strParameter(0, i) = strParameterName Then
+                    strParameter(1, i) = strParameterValue
+                    bNew = False
+                End If
+            Next
+            If bNew Then
+                ReDim Preserve strParameter(1, iBound + 1)
+                strParameter(0, iBound + 1) = strParameterName
+                strParameter(1, iBound + 1) = strParameterValue
+            End If
         End If
     End Sub
 
@@ -44,7 +51,15 @@ Public Class RSyntax
 
     Public Function GetScript() As String
         Dim strScript As String
-        strScript = strFunction & "(" & strParameter(0) & "=" & strParameter(1) & ")"
+        Dim i As Integer
+        strScript = strFunction & "("
+        For i = 0 To strParameter.GetUpperBound(1)
+            If i > 0 Then
+                strScript = strScript & ","
+            End If
+            strScript = strScript & strParameter(0, i) & "=" & strParameter(1, i)
+        Next
+        strScript = strScript & ")"
         Return strScript
     End Function
 
