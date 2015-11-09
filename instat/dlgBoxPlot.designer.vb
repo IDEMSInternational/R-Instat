d3a5d8b8a30729cddcc71dbe0f9d09f1879ebddc
Lunalo <johnlunalo95@gmail.com>
2015-11-09 19:16:01 +0300

    Changes on Boxplot

--- instat/dlgBoxPlot.designer.vb
+++ instat/dlgBoxPlot.designer.vb
@@ -22,56 +22,61 @@ Partial Class dlgBoxPlot
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.UcrDataSelection1 = New instat.ucrDataSelection()
-        Me.UcrButtons1 = New instat.ucrButtons()
         Me.lblTittle = New System.Windows.Forms.Label()
         Me.txtTittle = New System.Windows.Forms.TextBox()
+        Me.UcrAddRemove = New instat.ucrSelectorAddRemove()
+        Me.ucrBase = New instat.ucrButtons()
+        Me.UcrReceiverSingle1 = New instat.ucrReceiverSingle()
         Me.SuspendLayout()
-        '
-        'UcrDataSelection1
-        '
-        Me.UcrDataSelection1.Location = New System.Drawing.Point(40, 3)
-        Me.UcrDataSelection1.Name = "UcrDataSelection1"
-        Me.UcrDataSelection1.Size = New System.Drawing.Size(297, 122)
-        Me.UcrDataSelection1.TabIndex = 1
-        Me.UcrDataSelection1.UseWaitCursor = True
-        '
-        'UcrButtons1
-        '
-        Me.UcrButtons1.Location = New System.Drawing.Point(12, 166)
-        Me.UcrButtons1.Name = "UcrButtons1"
-        Me.UcrButtons1.Size = New System.Drawing.Size(404, 32)
-        Me.UcrButtons1.TabIndex = 0
-        Me.UcrButtons1.UseWaitCursor = True
         '
         'lblTittle
         '
         Me.lblTittle.AutoSize = True
-        Me.lblTittle.Location = New System.Drawing.Point(23, 147)
+        Me.lblTittle.Location = New System.Drawing.Point(23, 204)
         Me.lblTittle.Name = "lblTittle"
         Me.lblTittle.Size = New System.Drawing.Size(30, 13)
         Me.lblTittle.TabIndex = 2
         Me.lblTittle.Tag = "Tittle"
         Me.lblTittle.Text = "Tittle"
-        Me.lblTittle.UseWaitCursor = True
         '
         'txtTittle
         '
-        Me.txtTittle.Location = New System.Drawing.Point(85, 140)
+        Me.txtTittle.Location = New System.Drawing.Point(126, 197)
         Me.txtTittle.Name = "txtTittle"
         Me.txtTittle.Size = New System.Drawing.Size(208, 20)
         Me.txtTittle.TabIndex = 3
-        Me.txtTittle.UseWaitCursor = True
+        '
+        'UcrAddRemove
+        '
+        Me.UcrAddRemove.Location = New System.Drawing.Point(26, 5)
+        Me.UcrAddRemove.Name = "UcrAddRemove"
+        Me.UcrAddRemove.Size = New System.Drawing.Size(186, 129)
+        Me.UcrAddRemove.TabIndex = 6
+        '
+        'ucrBase
+        '
+        Me.ucrBase.Location = New System.Drawing.Point(12, 223)
+        Me.ucrBase.Name = "ucrBase"
+        Me.ucrBase.Size = New System.Drawing.Size(410, 32)
+        Me.ucrBase.TabIndex = 4
+        '
+        'UcrReceiverSingle1
+        '
+        Me.UcrReceiverSingle1.Location = New System.Drawing.Point(267, 31)
+        Me.UcrReceiverSingle1.Name = "UcrReceiverSingle1"
+        Me.UcrReceiverSingle1.Size = New System.Drawing.Size(106, 26)
+        Me.UcrReceiverSingle1.TabIndex = 7
         '
         'dlgBoxPlot
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(419, 205)
+        Me.ClientSize = New System.Drawing.Size(458, 267)
+        Me.Controls.Add(Me.UcrReceiverSingle1)
+        Me.Controls.Add(Me.UcrAddRemove)
+        Me.Controls.Add(Me.ucrBase)
         Me.Controls.Add(Me.txtTittle)
         Me.Controls.Add(Me.lblTittle)
-        Me.Controls.Add(Me.UcrDataSelection1)
-        Me.Controls.Add(Me.UcrButtons1)
         Me.Cursor = System.Windows.Forms.Cursors.WaitCursor
         Me.MaximizeBox = False
         Me.MinimizeBox = False
@@ -79,14 +84,13 @@ Partial Class dlgBoxPlot
         Me.ShowIcon = False
         Me.Text = "Boxplot"
         Me.TopMost = True
-        Me.UseWaitCursor = True
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents UcrButtons1 As ucrButtons
-    Friend WithEvents UcrDataSelection1 As ucrDataSelection
     Friend WithEvents lblTittle As Label
     Friend WithEvents txtTittle As TextBox
+    Friend WithEvents ucrBase As ucrButtons
+    Friend WithEvents UcrAddRemove As ucrSelectorAddRemove
+    Friend WithEvents UcrReceiverSingle1 As ucrReceiverSingle
 End Class
