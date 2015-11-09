7123669fcb67077d958c00791f21f53b5166d288
deaspo <polycarpokock@live.com>
2015-11-09 17:02:45 +0300

    Addition of the Histogram

---
+++ instat/dlgHistogram.vb
@@ -0,0 +1,22 @@
+﻿Public Class dlgHistogram
+    Private Sub dlgHistogram_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        UcrBase.clsRsyntax.SetFunction("hist")
+        UcrSingleReceiver.Selector = UcrAddRemove
+    End Sub
+
+    Private Sub UcrReceiverSingle1_Enter(sender As Object, e As EventArgs) Handles UcrSingleReceiver.Enter
+        UcrSingleReceiver.SetMeAsReceiver()
+    End Sub
+
+    Private Sub UcrBase_ClickOk(sender As Object, e As EventArgs) Handles UcrBase.ClickOk
+        frmMain.clsRInterface.RunScript(UcrBase.clsRsyntax.GetScript(), 0)
+    End Sub
+
+    Private Sub UcrReceiverSingle1_Leave(sender As Object, e As EventArgs) Handles UcrSingleReceiver.Leave
+        UcrBase.clsRsyntax.AddParameter("x", "data$" & UcrSingleReceiver.txtReceiverSingle.Text & "")
+    End Sub
+
+    Private Sub txtHistogramTitle_TextChanged(sender As Object, e As EventArgs) Handles txtHistogramTitle.TextChanged
+        UcrBase.clsRsyntax.AddParameter("main", Chr(34) & txtHistogramTitle.Text & Chr(34))
+    End Sub
+End Class
