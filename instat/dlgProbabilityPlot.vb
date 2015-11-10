a560345c7829aa033663a5d131baa0ddf7ed579c
max <maxwellfundi@live.com>
2015-11-10 15:46:45 +0300

    probability plot dialog with normal distribution

--- instat/dlgProbabilityPlot.vb
+++ instat/dlgProbabilityPlot.vb
@@ -8,15 +8,16 @@
 
     End Sub
     Private Sub ucrBase_ClickOk(sender As Object, e As EventArgs) Handles ucrBase.ClickOk
-        ucrBase.clsRsyntax.AddParameter("x", "data$" & UcrReceiverSingle1.txtReceiverSingle.Text & "")
+        ucrBase.clsRsyntax.AddParameter("y", "data$" & UcrReceiverSingle1.txtReceiverSingle.Text & "")
         ucrBase.clsRsyntax.AddParameter("main", Chr(34) & txtTitle.Text & Chr(34))
         frmMain.clsRInterface.RunScript(ucrBase.clsRsyntax.GetScript(), 0)
     End Sub
 
     Private Sub rdoNormal_CheckedChanged(sender As Object, e As EventArgs) Handles rdoNormal.CheckedChanged
         If rdoNormal.Checked = True Then
-            txtTitle.Text = "Normal Probability Plof of"
+            txtTitle.Text = "Normal Probability Plot"
         End If
+
     End Sub
 End Class
 
