77a8a7080a1f1e1a6b7986e8981146d162865ca6
dannyparsons <danny@aims.ac.za>
2015-11-11 17:45:44 +0300

    updated user controls and dot plot to use R syntax correctly

--- instat/dlgDotPlot.designer.vb
+++ instat/dlgDotPlot.designer.vb
@@ -22,29 +22,29 @@ Partial Class dlgDotPlot
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.txtTittle = New System.Windows.Forms.TextBox()
+        Me.lblPlotTitle = New System.Windows.Forms.Label()
+        Me.txtTitle = New System.Windows.Forms.TextBox()
         Me.ucrBase = New instat.ucrButtons()
         Me.ucrAddRemove = New instat.ucrSelectorAddRemove()
-        Me.UcrReceiverSingle1 = New instat.ucrReceiverSingle()
-        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ucrReceiverSingle = New instat.ucrReceiverSingle()
+        Me.lblVariableToPlot = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-        'Label1
+        'lblPlotTitle
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(25, 141)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(51, 13)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "Plot Title "
+        Me.lblPlotTitle.AutoSize = True
+        Me.lblPlotTitle.Location = New System.Drawing.Point(25, 141)
+        Me.lblPlotTitle.Name = "lblPlotTitle"
+        Me.lblPlotTitle.Size = New System.Drawing.Size(51, 13)
+        Me.lblPlotTitle.TabIndex = 1
+        Me.lblPlotTitle.Text = "Plot Title "
         '
-        'txtTittle
+        'txtTitle
         '
-        Me.txtTittle.Location = New System.Drawing.Point(82, 141)
-        Me.txtTittle.Name = "txtTittle"
-        Me.txtTittle.Size = New System.Drawing.Size(273, 20)
-        Me.txtTittle.TabIndex = 2
+        Me.txtTitle.Location = New System.Drawing.Point(82, 141)
+        Me.txtTitle.Name = "txtTitle"
+        Me.txtTitle.Size = New System.Drawing.Size(273, 20)
+        Me.txtTitle.TabIndex = 2
         '
         'ucrBase
         '
@@ -60,47 +60,49 @@ Partial Class dlgDotPlot
         Me.ucrAddRemove.Size = New System.Drawing.Size(186, 129)
         Me.ucrAddRemove.TabIndex = 3
         '
-        'UcrReceiverSingle1
+        'ucrReceiverSingle
         '
-        Me.UcrReceiverSingle1.Location = New System.Drawing.Point(211, 38)
-        Me.UcrReceiverSingle1.Name = "UcrReceiverSingle1"
-        Me.UcrReceiverSingle1.Size = New System.Drawing.Size(106, 26)
-        Me.UcrReceiverSingle1.TabIndex = 4
+        Me.ucrReceiverSingle.Location = New System.Drawing.Point(211, 38)
+        Me.ucrReceiverSingle.Name = "ucrReceiverSingle"
+        Me.ucrReceiverSingle.Size = New System.Drawing.Size(106, 26)
+        Me.ucrReceiverSingle.TabIndex = 4
         '
-        'Label2
+        'lblVariableToPlot
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(211, 13)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(78, 13)
-        Me.Label2.TabIndex = 5
-        Me.Label2.Text = "Variable to Plot"
+        Me.lblVariableToPlot.AutoSize = True
+        Me.lblVariableToPlot.Location = New System.Drawing.Point(211, 13)
+        Me.lblVariableToPlot.Name = "lblVariableToPlot"
+        Me.lblVariableToPlot.Size = New System.Drawing.Size(39, 13)
+        Me.lblVariableToPlot.TabIndex = 5
+        Me.lblVariableToPlot.Tag = "Variable_to_Plot"
+        Me.lblVariableToPlot.Text = "Label1"
         '
         'dlgDotPlot
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(405, 226)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.UcrReceiverSingle1)
+        Me.Controls.Add(Me.lblVariableToPlot)
+        Me.Controls.Add(Me.ucrReceiverSingle)
         Me.Controls.Add(Me.ucrAddRemove)
-        Me.Controls.Add(Me.txtTittle)
-        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtTitle)
+        Me.Controls.Add(Me.lblPlotTitle)
         Me.Controls.Add(Me.ucrBase)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "dlgDotPlot"
-        Me.Text = "dlgDotPlot"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Tag = "Dot_Plot"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents ucrBase As ucrButtons
-    Friend WithEvents Label1 As Label
-    Friend WithEvents txtTittle As TextBox
+    Friend WithEvents lblPlotTitle As Label
+    Friend WithEvents txtTitle As TextBox
     Friend WithEvents ucrAddRemove As ucrSelectorAddRemove
-    Friend WithEvents UcrReceiverSingle1 As ucrReceiverSingle
-    Friend WithEvents Label2 As Label
+    Friend WithEvents ucrReceiverSingle As ucrReceiverSingle
+    Friend WithEvents lblVariableToPlot As Label
 End Class
