77c8d3ab136c0be8d492f9e5f286c0d1a5d104af
Marthafuraha Massawe <marthanick51@outlook.com>
2015-11-12 13:13:23 +0300

    Changes in the three summaries dialog

--- instat/dlgThreeSummaries.vb
+++ instat/dlgThreeSummaries.vb
@@ -1,3 +1,9 @@
-﻿Public Class dlgThreeSummaries
+﻿Imports instat.Translations
+Public Class dlgThreeSummaries
+    Private Sub ucrBase_Load(sender As Object, e As EventArgs) Handles ucrBase.Load
+        autoTranslate(Me)
+        ucrBase.clsRsyntax.SetFunction("climate_obj$plot_three_summaries")
+        ucrBase.clsRsyntax.iCallType = 0
+    End Sub
 
 End Class
