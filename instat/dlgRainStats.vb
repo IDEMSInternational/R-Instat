3438857c15a6ea9fb8ac2158f70bf27e35206de8
deaspo <polycarpokock@live.com>
2015-11-12 11:12:26 +0300

    Added rain stats, Seasonal Summary and Seasonal Summary Rain - Abib

---
+++ instat/dlgRainStats.vb
@@ -0,0 +1,12 @@
+﻿Imports instat.Translations
+Public Class dlgRainStats
+    Private Sub UcrButtons1_Load(sender As Object, e As EventArgs) Handles ucrBase.Load
+        autoTranslate(Me)
+        ucrBase.clsRsyntax.SetFunction("climate_obj$rain_stats()")
+        ucrBase.clsRsyntax.iCallType = 0
+    End Sub
+
+    Private Sub dlgRainStats_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
