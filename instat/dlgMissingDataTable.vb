56cdc821a587e95780d7bcf0dcf8c6b2b98be5c9
deaspo <polycarpokock@live.com>
2015-11-12 11:03:58 +0300

    Added missing data table

---
+++ instat/dlgMissingDataTable.vb
@@ -0,0 +1,12 @@
+﻿Imports instat.Translations
+Public Class dlgMissingDataTable
+    Private Sub UcrButtons1_Load(sender As Object, e As EventArgs) Handles ucrBase.Load
+        autoTranslate(Me)
+        ucrBase.clsRsyntax.SetFunction("climate_obj$missing_data_table()")
+        ucrBase.clsRsyntax.iCallType = 0
+    End Sub
+
+    Private Sub dlgMissingDataTable_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        End Sub
+    End Class
