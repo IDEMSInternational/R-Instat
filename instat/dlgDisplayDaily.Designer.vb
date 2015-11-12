bafa9c7829a442f23cde459d9f9b90a5fcf2d94a
deaspo <polycarpokock@live.com>
2015-11-12 10:41:50 +0300

    Added the modified display daily - Abib

--- instat/dlgDisplayDaily.Designer.vb
+++ instat/dlgDisplayDaily.Designer.vb
@@ -22,8 +22,31 @@ Partial Class dlgDisplayDaily
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.ucrBase = New instat.ucrButtons()
+        Me.SuspendLayout()
+        '
+        'ucrBase
+        '
+        Me.ucrBase.Location = New System.Drawing.Point(9, 230)
+        Me.ucrBase.Name = "ucrBase"
+        Me.ucrBase.Size = New System.Drawing.Size(410, 32)
+        Me.ucrBase.TabIndex = 0
+        '
+        'dlgDisplayDaily
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(423, 262)
+        Me.Controls.Add(Me.ucrBase)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "dlgDisplayDaily"
+        Me.Tag = "Display_Daily"
         Me.Text = "dlgDisplayDaily"
+        Me.ResumeLayout(False)
+
     End Sub
+
+    Friend WithEvents ucrBase As ucrButtons
 End Class
