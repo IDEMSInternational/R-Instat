4303e703df073bb6e6f90f507e24e755c804e7be
max <maxwellfundi@live.com>
2015-11-17 12:48:23 +0300

    Initial recode dialog

---
+++ instat/dlgRecode.vb
@@ -0,0 +1,5 @@
+﻿Public Class dlgRecode
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+End Class
