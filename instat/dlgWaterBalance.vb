b76b529fd3c31002b9a07f84152610e0c57d609b
aduut <k.aduut@gmail.com>
2015-11-17 23:24:39 +0000

    Water Balance dialog with all parameters.

--- instat/dlgWaterBalance.vb
+++ instat/dlgWaterBalance.vb
@@ -1,31 +1,32 @@
 ﻿Imports instat.Translations
 Public Class dlgWaterBalance
-    Private Sub ucrButtons_clickOk(sender As Object, e As EventArgs) Handles UcrButtons1.ClickOk
-        Dim strScript As String
-        strScript = "climate_obj$display_water_balance(data_list=list()"
-        strScript = strScript & ", capacity_max=" & nudMaximumCapacity.ToString()
-        strScript = strScript & ", evaporation=" & nudEvaporation.ToString()
-        strScript = strScript & ", decimal_places=" & nudDecimalPlace.ToString()
-        strScript = strScript & ", day_display=" & Chr(34) & txtDisplayDayName.Text.ToString() & Chr(34)
 
-        If chkPrintTable.Checked Then
-            strScript = strScript & ", print_tables=" & chkPrintTable.Checked.ToString().ToUpper
-        End If
-
-        strScript = strScript & ", col_name=" & Chr(34) & txtColumnName.Text & Chr(34)
-        strScript = strScript & ")"
-        frmMain.clsRInterface.climateObject()
-        frmMain.clsRInterface.RunScript(strScript, False)
+    Private Sub UcrButtons1_Load(sender As Object, e As EventArgs) Handles ucrBase.Load
+        autoTranslate(Me)
+        ucrBase.clsRsyntax.SetFunction("climate_obj$display_water_balance()")
+        ucrBase.clsRsyntax.iCallType = 0
         frmMain.FillData("climate_obj$climate_data_objects[[1]]$data")
         frmMain.FillData("climate_obj$used_data_objects[[1]]$data")
         Me.Hide()
+
     End Sub
 
+    Private Sub txtColunmName_Leave(sender As Object, e As EventArgs) Handles txtColumnName.Leave
+        ucrBase.clsRsyntax.AddParameter("col_name", Chr(34) & txtColumnName.Text & Chr(34))
+        ucrBase.clsRsyntax.AddParameter("evaporation", Chr(34) & txtColumnName.Text & Chr(34))
+        ucrBase.clsRsyntax.AddParameter("decimal_places", Chr(34) & txtColumnName.Text & Chr(34))
+        ucrBase.clsRsyntax.AddParameter("day_display", Chr(34) & txtColumnName.Text & Chr(34))
+        ucrBase.clsRsyntax.AddParameter("capacity_max", Chr(34) & txtColumnName.Text & Chr(34))
+        If chkPrintTable.Checked Then
+            ucrBase.clsRsyntax.AddParameter("print_tables", Chr(34) & txtColumnName.Text & Chr(34))
+
+        End If
+    End Sub
 
     Private Sub dlgStartofRains_Load(sender As Object, e As EventArgs) Handles Me.Load
-            'UpdateVisible()
-            autoTranslate(Me)
-        End Sub
+        'UpdateVisible()
+        autoTranslate(Me)
+    End Sub
 
     Private Sub dlgInventoryPlot_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
