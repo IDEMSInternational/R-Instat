8af2564756f786224b931a47040e8f5d87872335
Mary Mutahi <mutahi.nyambura@outlook.com>
2015-11-18 16:19:34 +0300

    Stagging

---
+++ instat/Bivariate Analysis of Variance.Designer.vb
@@ -0,0 +1,147 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class dlgBivariateAnalysisofVariance
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.UcrSelectorAddRemove1 = New instat.ucrSelectorAddRemove()
+        Me.UcrReceiverSingle1 = New instat.ucrReceiverSingle()
+        Me.UcrReceiverSingleB1 = New instat.ucrReceiverSingleB()
+        Me.UcrReceiverSingle2 = New instat.ucrReceiverSingle()
+        Me.UcrReceiverSingle3 = New instat.ucrReceiverSingle()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.UcrButtons1 = New instat.ucrButtons()
+        Me.SuspendLayout()
+        '
+        'UcrSelectorAddRemove1
+        '
+        Me.UcrSelectorAddRemove1.Location = New System.Drawing.Point(-1, 31)
+        Me.UcrSelectorAddRemove1.Name = "UcrSelectorAddRemove1"
+        Me.UcrSelectorAddRemove1.Size = New System.Drawing.Size(182, 118)
+        Me.UcrSelectorAddRemove1.TabIndex = 0
+        '
+        'UcrReceiverSingle1
+        '
+        Me.UcrReceiverSingle1.Location = New System.Drawing.Point(256, 46)
+        Me.UcrReceiverSingle1.Name = "UcrReceiverSingle1"
+        Me.UcrReceiverSingle1.Size = New System.Drawing.Size(106, 26)
+        Me.UcrReceiverSingle1.TabIndex = 1
+        '
+        'UcrReceiverSingleB1
+        '
+        Me.UcrReceiverSingleB1.Location = New System.Drawing.Point(256, 91)
+        Me.UcrReceiverSingleB1.Name = "UcrReceiverSingleB1"
+        Me.UcrReceiverSingleB1.Size = New System.Drawing.Size(106, 26)
+        Me.UcrReceiverSingleB1.TabIndex = 2
+        '
+        'UcrReceiverSingle2
+        '
+        Me.UcrReceiverSingle2.Location = New System.Drawing.Point(256, 139)
+        Me.UcrReceiverSingle2.Name = "UcrReceiverSingle2"
+        Me.UcrReceiverSingle2.Size = New System.Drawing.Size(106, 26)
+        Me.UcrReceiverSingle2.TabIndex = 3
+        '
+        'UcrReceiverSingle3
+        '
+        Me.UcrReceiverSingle3.Location = New System.Drawing.Point(256, 187)
+        Me.UcrReceiverSingle3.Name = "UcrReceiverSingle3"
+        Me.UcrReceiverSingle3.Size = New System.Drawing.Size(106, 26)
+        Me.UcrReceiverSingle3.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(258, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(258, 75)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(258, 123)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Label3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(258, 171)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(39, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Label4"
+        '
+        'UcrButtons1
+        '
+        Me.UcrButtons1.Location = New System.Drawing.Point(-1, 308)
+        Me.UcrButtons1.Name = "UcrButtons1"
+        Me.UcrButtons1.Size = New System.Drawing.Size(410, 32)
+        Me.UcrButtons1.TabIndex = 9
+        '
+        'dlgBivariateAnalysisofVariance
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(409, 338)
+        Me.Controls.Add(Me.UcrButtons1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.UcrReceiverSingle3)
+        Me.Controls.Add(Me.UcrReceiverSingle2)
+        Me.Controls.Add(Me.UcrReceiverSingleB1)
+        Me.Controls.Add(Me.UcrReceiverSingle1)
+        Me.Controls.Add(Me.UcrSelectorAddRemove1)
+        Me.Name = "dlgBivariateAnalysisofVariance"
+        Me.Text = "Bivariate Analysis of Variance"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents UcrSelectorAddRemove1 As ucrSelectorAddRemove
+    Friend WithEvents UcrReceiverSingle1 As ucrReceiverSingle
+    Friend WithEvents UcrReceiverSingleB1 As ucrReceiverSingleB
+    Friend WithEvents UcrReceiverSingle2 As ucrReceiverSingle
+    Friend WithEvents UcrReceiverSingle3 As ucrReceiverSingle
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents UcrButtons1 As ucrButtons
+End Class
