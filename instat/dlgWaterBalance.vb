a782567aabf473b2429d283976387da573b2f390
aduut <k.aduut@gmail.com>
2015-11-18 00:24:08 +0000

    Updates to the water Balance commits.

--- instat/dlgWaterBalance.vb
+++ instat/dlgWaterBalance.vb
@@ -4,7 +4,7 @@ Public Class dlgWaterBalance
     Private Sub UcrButtons1_Load(sender As Object, e As EventArgs) Handles ucrBase.Load
         autoTranslate(Me)
         ucrBase.clsRsyntax.SetFunction("climate_obj$display_water_balance()")
-        ucrBase.clsRsyntax.iCallType = 0
+        ucrBase.clsRsyntax.iCallType = 1
         frmMain.FillData("climate_obj$climate_data_objects[[1]]$data")
         frmMain.FillData("climate_obj$used_data_objects[[1]]$data")
         Me.Hide()
@@ -13,26 +13,32 @@ Public Class dlgWaterBalance
 
     Private Sub txtColunmName_Leave(sender As Object, e As EventArgs) Handles txtColumnName.Leave
         ucrBase.clsRsyntax.AddParameter("col_name", Chr(34) & txtColumnName.Text & Chr(34))
+    End Sub
+
+    Private Sub nudEvapration_Leave(sender As Object, e As EventArgs) Handles nudEvaporation.Leave
         ucrBase.clsRsyntax.AddParameter("evaporation", nudEvaporation.Value)
+    End Sub
+
+    Private Sub nudDecimalPlace_Leave(sender As Object, e As EventArgs) Handles nudDecimalPlace.Leave
+
         ucrBase.clsRsyntax.AddParameter("decimal_places", nudDecimalPlace.Value)
-        ucrBase.clsRsyntax.AddParameter("day_display", txtColumnName.Text)
+    End Sub
+    Private Sub txtDisplayDayName_Leave(sender As Object, e As EventArgs) Handles txtDisplayDayName.Leave
+
+        ucrBase.clsRsyntax.AddParameter("day_display", txtDisplayDayName.Text)
+    End Sub
+
+    Private Sub nudMaximumCapacity_Leave(sender As Object, e As EventArgs) Handles nudMaximumCapacity.Leave
+
         ucrBase.clsRsyntax.AddParameter("capacity_max", nudMaximumCapacity.Value)
+    End Sub
+    Private Sub chkPrintTable_Leave(sender As Object, e As EventArgs) Handles chkPrintTable.Leave
         If chkPrintTable.Checked Then
             ucrBase.clsRsyntax.AddParameter("print_tables", chkPrintTable.Checked.ToString.ToUpper())
 
         End If
     End Sub
 
-    Private Sub dlgStartofRains_Load(sender As Object, e As EventArgs) Handles Me.Load
-        'UpdateVisible()
-        autoTranslate(Me)
-    End Sub
 
-    Private Sub dlgInventoryPlot_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-    End Sub
-
-    Private Sub dlgWaterBalance_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
 End Class
