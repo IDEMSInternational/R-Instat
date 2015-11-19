29c55c4cdc0d966587ef5548aec2bb20b6cb5a0d
aduut <k.aduut@gmail.com>
2015-11-19 10:50:32 +0000

    Modification to Output for CPT, with all parameters.

--- instat/dlgOutputforCPT.vb
+++ instat/dlgOutputforCPT.vb
@@ -1,3 +1,36 @@
-﻿Public Class dlgOutputforCPT
+﻿
+' Instat-R
+' Copyright (C) 2015
+'
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+'
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License k
+' along with this program.  If not, see <http://www.gnu.org/licenses/>.
+Imports instat.Translations
+
+Public Class dlgOutputforCPT
+    Private Sub txtFilename_Leave(sender As Object, e As EventArgs) Handles txtFilename.Leave
+        ucrBase.clsRsyntax.AddParameter("Filename", Chr(34) & txtFilename.Text & Chr(34))
+
+    End Sub
+
+    Private Sub txtInterestedVariable_Leave(sender As Object, e As EventArgs) Handles txtInterestedVariable.Leave
+        ucrBase.clsRsyntax.AddParameter("Interested_vaiables", Chr(34) & txtInterestedVariable.Text & Chr(34))
+    End Sub
+
+    Private Sub dlgOutputforCPT_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        autoTranslate(Me)
+        ucrBase.clsRsyntax.SetFunction("climate_obj$output_for_CPT()")
+        ucrBase.clsRsyntax.iCallType = 1
+    End Sub
+
 
 End Class
