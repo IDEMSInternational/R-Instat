6eb846732dabc574d500382008269f981b6a926f
max <maxwellfundi@live.com>
2015-11-19 17:55:40 +0300

    Newly fixed dialogs

--- instat/dlgBoxPlot.vb
+++ instat/dlgBoxPlot.vb
@@ -1,19 +1,22 @@
 ﻿Public Class dlgBoxPlot
     Private Sub dlgBoxPlot_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ucrBase.clsRsyntax.SetFunction("boxplot")
-        UcrReceiverSingle1.Selector = UcrAddRemove
+        ucrBase.clsRsyntax.iCallType = 0
+        UcrReceiverSingle.Selector = UcrAddRemove
+        UcrReceiverSingle.SetMeAsReceiver()
     End Sub
 
-    Private Sub UcrReceiverSingle1_Enter(sender As Object, e As EventArgs) Handles UcrReceiverSingle1.Enter
-        UcrReceiverSingle1.SetMeAsReceiver()
+    Private Sub UcrReceiverSingle1_Enter(sender As Object, e As EventArgs) Handles UcrReceiverSingle.Enter
+        UcrReceiverSingle.SetMeAsReceiver()
     End Sub
 
     Private Sub UcrBase_ClickOk(sender As Object, e As EventArgs) Handles ucrBase.ClickOk
         frmMain.clsRInterface.RunScript(ucrBase.clsRsyntax.GetScript(), 0)
     End Sub
 
-    Private Sub ucrreceiversingle1_leave(sender As Object, e As EventArgs) Handles UcrReceiverSingle1.Leave
-        ucrBase.clsRsyntax.AddParameter("x", "data$" & UcrReceiverSingle1.txtReceiverSingle.Text & "")
+    Private Sub ucrReceiverSingle_leave(sender As Object, e As EventArgs) Handles UcrReceiverSingle.Leave
+        ucrBase.clsRsyntax.AddParameter("x", UcrReceiverSingle.GetVariables())
+
     End Sub
 
     Private Sub txtTitle_TextChanged(sender As Object, e As EventArgs) Handles txtTittle.TextChanged
@@ -26,7 +29,5 @@
         ucrBase.clsRsyntax.AddParameter("ylab", Chr(34) & txtYLabel.Text & Chr(34))
     End Sub
 
-    Private Sub UcrAddRemove_Load(sender As Object, e As EventArgs) Handles UcrAddRemove.Load
 
-    End Sub
 End Class
