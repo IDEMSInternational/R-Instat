5c366543d313c3ded7bd9fbf4c8fa05059415e7f
Lunalo <johnlunalo95@gmail.com>
2015-11-23 15:03:13 +0300

    changes on boxplot

--- instat/dlgBoxPlot.designer.vb
+++ instat/dlgBoxPlot.designer.vb
@@ -22,54 +22,35 @@ Partial Class dlgBoxPlot
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.lblTittle = New System.Windows.Forms.Label()
-        Me.txtTittle = New System.Windows.Forms.TextBox()
-        Me.UcrAddRemove = New instat.ucrSelectorAddRemove()
-        Me.ucrBase = New instat.ucrButtons()
-        Me.UcrReceiverSingle1 = New instat.ucrReceiverSingle()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.txtTitle = New System.Windows.Forms.TextBox()
         Me.lblYlab = New System.Windows.Forms.Label()
         Me.lblXLabel = New System.Windows.Forms.Label()
         Me.txtYLabel = New System.Windows.Forms.TextBox()
         Me.txtXLabel = New System.Windows.Forms.TextBox()
+        Me.ucrAddRemove = New instat.ucrSelectorAddRemove()
+        Me.ucrBase = New instat.ucrButtons()
+        Me.ucrReceiverMultiple = New instat.ucrReceiverMultiple()
+        Me.lblVariablesToBePlotted = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-        'lblTittle
+        'lblTitle
         '
-        Me.lblTittle.AutoSize = True
-        Me.lblTittle.Location = New System.Drawing.Point(35, 200)
-        Me.lblTittle.Name = "lblTittle"
-        Me.lblTittle.Size = New System.Drawing.Size(30, 13)
-        Me.lblTittle.TabIndex = 2
-        Me.lblTittle.Tag = "Tittle"
-        Me.lblTittle.Text = "Tittle"
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Location = New System.Drawing.Point(35, 200)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(27, 13)
+        Me.lblTitle.TabIndex = 2
+        Me.lblTitle.Tag = "Title"
+        Me.lblTitle.Text = "Title"
         '
-        'txtTittle
+        'txtTitle
         '
-        Me.txtTittle.Location = New System.Drawing.Point(165, 193)
-        Me.txtTittle.Name = "txtTittle"
-        Me.txtTittle.Size = New System.Drawing.Size(208, 20)
-        Me.txtTittle.TabIndex = 3
-        '
-        'UcrAddRemove
-        '
-        Me.UcrAddRemove.Location = New System.Drawing.Point(26, 5)
-        Me.UcrAddRemove.Name = "UcrAddRemove"
-        Me.UcrAddRemove.Size = New System.Drawing.Size(186, 129)
-        Me.UcrAddRemove.TabIndex = 6
-        '
-        'ucrBase
-        '
-        Me.ucrBase.Location = New System.Drawing.Point(12, 219)
-        Me.ucrBase.Name = "ucrBase"
-        Me.ucrBase.Size = New System.Drawing.Size(410, 32)
-        Me.ucrBase.TabIndex = 4
-        '
-        'UcrReceiverSingle1
-        '
-        Me.UcrReceiverSingle1.Location = New System.Drawing.Point(267, 31)
-        Me.UcrReceiverSingle1.Name = "UcrReceiverSingle1"
-        Me.UcrReceiverSingle1.Size = New System.Drawing.Size(106, 26)
-        Me.UcrReceiverSingle1.TabIndex = 7
+        Me.txtTitle.Location = New System.Drawing.Point(165, 193)
+        Me.txtTitle.Name = "txtTitle"
+        Me.txtTitle.Size = New System.Drawing.Size(208, 20)
+        Me.txtTitle.TabIndex = 3
+        Me.txtTitle.Tag = ""
         '
         'lblYlab
         '
@@ -105,20 +86,52 @@ Partial Class dlgBoxPlot
         Me.txtXLabel.Size = New System.Drawing.Size(100, 20)
         Me.txtXLabel.TabIndex = 10
         '
+        'ucrAddRemove
+        '
+        Me.ucrAddRemove.Location = New System.Drawing.Point(26, 5)
+        Me.ucrAddRemove.Name = "ucrAddRemove"
+        Me.ucrAddRemove.Size = New System.Drawing.Size(186, 129)
+        Me.ucrAddRemove.TabIndex = 6
+        '
+        'ucrBase
+        '
+        Me.ucrBase.Location = New System.Drawing.Point(12, 219)
+        Me.ucrBase.Name = "ucrBase"
+        Me.ucrBase.Size = New System.Drawing.Size(410, 32)
+        Me.ucrBase.TabIndex = 4
+        '
+        'ucrReceiverMultiple
+        '
+        Me.ucrReceiverMultiple.Location = New System.Drawing.Point(273, 30)
+        Me.ucrReceiverMultiple.Name = "ucrReceiverMultiple"
+        Me.ucrReceiverMultiple.Size = New System.Drawing.Size(121, 69)
+        Me.ucrReceiverMultiple.TabIndex = 11
+        '
+        'lblVariablesToBePlotted
+        '
+        Me.lblVariablesToBePlotted.AutoSize = True
+        Me.lblVariablesToBePlotted.Location = New System.Drawing.Point(270, 5)
+        Me.lblVariablesToBePlotted.Name = "lblVariablesToBePlotted"
+        Me.lblVariablesToBePlotted.Size = New System.Drawing.Size(112, 13)
+        Me.lblVariablesToBePlotted.TabIndex = 12
+        Me.lblVariablesToBePlotted.Tag = "Variables_to_be_plotted"
+        Me.lblVariablesToBePlotted.Text = "Variables to be plotted"
+        '
         'dlgBoxPlot
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(465, 269)
+        Me.Controls.Add(Me.lblVariablesToBePlotted)
+        Me.Controls.Add(Me.ucrReceiverMultiple)
         Me.Controls.Add(Me.txtXLabel)
         Me.Controls.Add(Me.txtYLabel)
         Me.Controls.Add(Me.lblXLabel)
         Me.Controls.Add(Me.lblYlab)
-        Me.Controls.Add(Me.UcrReceiverSingle1)
-        Me.Controls.Add(Me.UcrAddRemove)
+        Me.Controls.Add(Me.ucrAddRemove)
         Me.Controls.Add(Me.ucrBase)
-        Me.Controls.Add(Me.txtTittle)
-        Me.Controls.Add(Me.lblTittle)
+        Me.Controls.Add(Me.txtTitle)
+        Me.Controls.Add(Me.lblTitle)
         Me.Cursor = System.Windows.Forms.Cursors.WaitCursor
         Me.MaximizeBox = False
         Me.MinimizeBox = False
@@ -130,13 +143,14 @@ Partial Class dlgBoxPlot
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents lblTittle As Label
-    Friend WithEvents txtTittle As TextBox
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents txtTitle As TextBox
     Friend WithEvents ucrBase As ucrButtons
-    Friend WithEvents UcrAddRemove As ucrSelectorAddRemove
-    Friend WithEvents UcrReceiverSingle1 As ucrReceiverSingle
+    Friend WithEvents ucrAddRemove As ucrSelectorAddRemove
     Friend WithEvents lblYlab As Label
     Friend WithEvents lblXLabel As Label
     Friend WithEvents txtYLabel As TextBox
     Friend WithEvents txtXLabel As TextBox
+    Friend WithEvents ucrReceiverMultiple As ucrReceiverMultiple
+    Friend WithEvents lblVariablesToBePlotted As Label
 End Class
