8413ecb2b73fdebd683d0487b40ea888e739cdfe
John Lunalo <johnlunalo95@gmail.com>
2015-11-25 17:52:23 +0300

    dlgStack and dlgUnstack

--- instat/frmMain.vb
+++ instat/frmMain.vb
@@ -399,10 +399,18 @@ Public Class frmMain
     End Sub
 
     Private Sub OneSampleToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OneSampleToolStripMenuItem.Click
-        dlgOneSample.ShowDialog()
+        'dlgOneSample.ShowDialog()
     End Sub
 
     Private Sub FrequencyToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FrequencyToolStripMenuItem.Click
         dlgFreqTables.ShowDialog()
+    End Sub
+
+    Private Sub StacksToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles StacksToolStripMenuItem.Click
+        dlgStack.ShowDialog()
+    End Sub
+
+    Private Sub UnstackToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles UnstackToolStripMenuItem.Click
+        dlgUnstack.ShowDialog()
     End Sub
 End Class
