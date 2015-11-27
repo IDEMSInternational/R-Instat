8f9018db909b9d49350e7f387dfc21ac2368d77e
max <maxwellfundi@live.com>
2015-11-27 14:32:22 +0300

    Normal probability plot with default Values

--- instat/dlgProbabilityPlot.vb
+++ instat/dlgProbabilityPlot.vb
@@ -23,44 +23,8 @@ Public Class dlgProbabilityPlot
         ucrBase.clsRsyntax.SetFunction("qqnorm")
         ucrBase.clsRsyntax.iCallType = 0
         rdoNormal.Checked = True
-        txtTitle.Visible = False
         autoTranslate(Me)
 
-    End Sub
-
-    Private Sub rdoNormal_CheckedChanged(sender As Object, e As EventArgs) Handles rdoNormal.CheckedChanged
-        If rdoNormal.Checked = True Then
-            txtTitle.Text = "Normal Probability Plot of " & UcrReceiverSingle.txtReceiverSingle.Text & ""
-        End If
-
-    End Sub
-
-    Private Sub rdoGamma_CheckedChanged(sender As Object, e As EventArgs) Handles rdoGamma.CheckedChanged
-        If rdoGamma.Checked = True Then
-            txtTitle.Text = "Gamma Probability Plot"
-        End If
-    End Sub
-
-    Private Sub rdoExtrmeValue_CheckedChanged(sender As Object, e As EventArgs) Handles rdoExtrmeValue.CheckedChanged
-        If rdoExtrmeValue.Checked = True Then
-            txtTitle.Text = "Extreme Value Probability Plot"
-        End If
-    End Sub
-
-    Private Sub rdoEmperical_CheckedChanged(sender As Object, e As EventArgs) Handles rdoEmperical.CheckedChanged
-        If rdoEmperical.Checked = True Then
-            txtTitle.Text = "Emperical Probability Plot"
-        End If
-    End Sub
-
-    Private Sub rdoExponential_CheckedChanged(sender As Object, e As EventArgs) Handles rdoExponential.CheckedChanged
-        If rdoExponential.Checked = True Then
-            txtTitle.Text = "Exponential Probability Plot"
-        End If
-    End Sub
-
-    Private Sub txtTitle_Leave(sender As Object, e As EventArgs) Handles txtTitle.Leave
-        ucrBase.clsRsyntax.AddParameter("main", Chr(34) & txtTitle.Text & Chr(34))
     End Sub
 
     Private Sub UcrReceiverSingle_Leave(sender As Object, e As EventArgs) Handles UcrReceiverSingle.Leave
