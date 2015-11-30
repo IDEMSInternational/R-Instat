6df1ea9b4248b12e6abf85e43c187342c1c4029f
John Lunalo <johnlunalo95@gmail.com>
2015-11-30 11:40:16 +0300

    changes on the chi square

---
+++ instat/dlgChisquare.vb
@@ -0,0 +1,31 @@
+﻿' Instat-R
+' Copyright (C) 2015
+'
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+'
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License k
+' along with this program.  If not, see <http://www.gnu.org/licenses/>.
+Imports instat.Translations
+Public Class dlgChisquare
+    Private Sub dlgChisquare_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ucrBase.clsRsyntax.SetFunction("chisq")
+        ucrBase.clsRsyntax.iCallType = 0
+        ucrReceiverMultiple.Selector = ucrAddRemove
+
+
+    End Sub
+    Private Sub ucrReceiverSingle_Leave(sender As Object, e As EventArgs) Handles ucrReceiverMultiple.Leave
+        ucrBase.clsRsyntax.AddParameter("x", ucrReceiverMultiple.GetVariables())
+    End Sub
+
+
+
+End Class
