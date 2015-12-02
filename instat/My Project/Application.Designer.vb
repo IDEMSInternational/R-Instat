6c2b26cb60614dc19f80276c8533dff715460f01
David Stern <d.a.stern@reading.ac.uk>
2015-12-02 09:53:53 +0000

    Revert "Added all the components for Duplicate dialog though it is still not fully functioning."

--- instat/My Project/Application.Designer.vb
+++ instat/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.instat.dlgDuplicateColumns
+            Me.MainForm = Global.instat.frmMain
         End Sub
     End Class
 End Namespace
