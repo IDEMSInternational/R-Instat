83278a2392518b3a0e78d2a30d384dcfb3aac877
deaspo <polycarpokock@live.com>
2015-12-03 09:39:33 +0300

    Indicator (dummy) variables, complete interface though still not fully functional.

--- instat/dlgIndicatorVariable.Designer.vb
+++ instat/dlgIndicatorVariable.Designer.vb
@@ -22,18 +22,207 @@ Partial Class dlgIndicatorVariable
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.lblAvailableFactors = New System.Windows.Forms.Label()
+        Me.cboAvailableFactors = New System.Windows.Forms.ComboBox()
+        Me.cboNonFactors = New System.Windows.Forms.ComboBox()
+        Me.chkWithX = New System.Windows.Forms.CheckBox()
+        Me.lstPasteInto = New System.Windows.Forms.ListBox()
+        Me.lblInto = New System.Windows.Forms.Label()
+        Me.grpLeveltobeOmiited = New System.Windows.Forms.GroupBox()
+        Me.txtLevel = New System.Windows.Forms.TextBox()
+        Me.rdoLevelNumber = New System.Windows.Forms.RadioButton()
+        Me.rdoLast = New System.Windows.Forms.RadioButton()
+        Me.rdoFirst = New System.Windows.Forms.RadioButton()
+        Me.rdoNone = New System.Windows.Forms.RadioButton()
+        Me.ucrBase = New instat.ucrButtons()
+        Me.txtInto = New System.Windows.Forms.TextBox()
+        Me.grpLeveltobeOmiited.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'lblAvailableFactors
+        '
+        Me.lblAvailableFactors.AutoSize = True
+        Me.lblAvailableFactors.Location = New System.Drawing.Point(8, 9)
+        Me.lblAvailableFactors.Name = "lblAvailableFactors"
+        Me.lblAvailableFactors.Size = New System.Drawing.Size(85, 13)
+        Me.lblAvailableFactors.TabIndex = 0
+        Me.lblAvailableFactors.Tag = "Available_factors"
+        Me.lblAvailableFactors.Text = "Available factors"
+        '
+        'cboAvailableFactors
+        '
+        Me.cboAvailableFactors.FormattingEnabled = True
+        Me.cboAvailableFactors.Location = New System.Drawing.Point(11, 25)
+        Me.cboAvailableFactors.Name = "cboAvailableFactors"
+        Me.cboAvailableFactors.Size = New System.Drawing.Size(163, 21)
+        Me.cboAvailableFactors.TabIndex = 1
+        '
+        'cboNonFactors
+        '
+        Me.cboNonFactors.Enabled = False
+        Me.cboNonFactors.FormattingEnabled = True
+        Me.cboNonFactors.Location = New System.Drawing.Point(11, 75)
+        Me.cboNonFactors.Name = "cboNonFactors"
+        Me.cboNonFactors.Size = New System.Drawing.Size(163, 21)
+        Me.cboNonFactors.Sorted = True
+        Me.cboNonFactors.TabIndex = 2
+        '
+        'chkWithX
+        '
+        Me.chkWithX.AutoSize = True
+        Me.chkWithX.Location = New System.Drawing.Point(11, 52)
+        Me.chkWithX.Name = "chkWithX"
+        Me.chkWithX.Size = New System.Drawing.Size(111, 17)
+        Me.chkWithX.TabIndex = 3
+        Me.chkWithX.Tag = "With_an_x_variable"
+        Me.chkWithX.Text = "With an x variable"
+        Me.chkWithX.UseVisualStyleBackColor = True
+        '
+        'lstPasteInto
+        '
+        Me.lstPasteInto.FormattingEnabled = True
+        Me.lstPasteInto.Location = New System.Drawing.Point(200, 75)
+        Me.lstPasteInto.Name = "lstPasteInto"
+        Me.lstPasteInto.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple
+        Me.lstPasteInto.Size = New System.Drawing.Size(149, 134)
+        Me.lstPasteInto.Sorted = True
+        Me.lstPasteInto.TabIndex = 4
+        '
+        'lblInto
+        '
+        Me.lblInto.AutoSize = True
+        Me.lblInto.Location = New System.Drawing.Point(197, 9)
+        Me.lblInto.Name = "lblInto"
+        Me.lblInto.Size = New System.Drawing.Size(25, 13)
+        Me.lblInto.TabIndex = 5
+        Me.lblInto.Tag = "Into"
+        Me.lblInto.Text = "Into"
+        '
+        'grpLeveltobeOmiited
+        '
+        Me.grpLeveltobeOmiited.Controls.Add(Me.txtLevel)
+        Me.grpLeveltobeOmiited.Controls.Add(Me.rdoLevelNumber)
+        Me.grpLeveltobeOmiited.Controls.Add(Me.rdoLast)
+        Me.grpLeveltobeOmiited.Controls.Add(Me.rdoFirst)
+        Me.grpLeveltobeOmiited.Controls.Add(Me.rdoNone)
+        Me.grpLeveltobeOmiited.Location = New System.Drawing.Point(12, 102)
+        Me.grpLeveltobeOmiited.Name = "grpLeveltobeOmiited"
+        Me.grpLeveltobeOmiited.Size = New System.Drawing.Size(162, 117)
+        Me.grpLeveltobeOmiited.TabIndex = 6
+        Me.grpLeveltobeOmiited.TabStop = False
+        Me.grpLeveltobeOmiited.Tag = "Level_to_be_omitted"
+        Me.grpLeveltobeOmiited.Text = "Level to be omitted"
+        '
+        'txtLevel
+        '
+        Me.txtLevel.Enabled = False
+        Me.txtLevel.Location = New System.Drawing.Point(103, 89)
+        Me.txtLevel.Name = "txtLevel"
+        Me.txtLevel.Size = New System.Drawing.Size(42, 20)
+        Me.txtLevel.TabIndex = 4
+        '
+        'rdoLevelNumber
+        '
+        Me.rdoLevelNumber.AutoSize = True
+        Me.rdoLevelNumber.Location = New System.Drawing.Point(6, 90)
+        Me.rdoLevelNumber.Name = "rdoLevelNumber"
+        Me.rdoLevelNumber.Size = New System.Drawing.Size(91, 17)
+        Me.rdoLevelNumber.TabIndex = 3
+        Me.rdoLevelNumber.TabStop = True
+        Me.rdoLevelNumber.Tag = "Level_Number"
+        Me.rdoLevelNumber.Text = "Level Number"
+        Me.rdoLevelNumber.UseVisualStyleBackColor = True
+        '
+        'rdoLast
+        '
+        Me.rdoLast.AutoSize = True
+        Me.rdoLast.Location = New System.Drawing.Point(6, 67)
+        Me.rdoLast.Name = "rdoLast"
+        Me.rdoLast.Size = New System.Drawing.Size(45, 17)
+        Me.rdoLast.TabIndex = 2
+        Me.rdoLast.TabStop = True
+        Me.rdoLast.Tag = "Last"
+        Me.rdoLast.Text = "Last"
+        Me.rdoLast.UseVisualStyleBackColor = True
+        '
+        'rdoFirst
+        '
+        Me.rdoFirst.AutoSize = True
+        Me.rdoFirst.Location = New System.Drawing.Point(6, 44)
+        Me.rdoFirst.Name = "rdoFirst"
+        Me.rdoFirst.Size = New System.Drawing.Size(44, 17)
+        Me.rdoFirst.TabIndex = 1
+        Me.rdoFirst.TabStop = True
+        Me.rdoFirst.Tag = "First"
+        Me.rdoFirst.Text = "First"
+        Me.rdoFirst.UseVisualStyleBackColor = True
+        '
+        'rdoNone
+        '
+        Me.rdoNone.AutoSize = True
+        Me.rdoNone.Location = New System.Drawing.Point(6, 21)
+        Me.rdoNone.Name = "rdoNone"
+        Me.rdoNone.Size = New System.Drawing.Size(51, 17)
+        Me.rdoNone.TabIndex = 0
+        Me.rdoNone.TabStop = True
+        Me.rdoNone.Tag = "None"
+        Me.rdoNone.Text = "None"
+        Me.rdoNone.UseVisualStyleBackColor = True
+        '
+        'ucrBase
+        '
+        Me.ucrBase.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.ucrBase.Location = New System.Drawing.Point(0, 222)
+        Me.ucrBase.Name = "ucrBase"
+        Me.ucrBase.Size = New System.Drawing.Size(402, 32)
+        Me.ucrBase.TabIndex = 7
+        '
+        'txtInto
+        '
+        Me.txtInto.Location = New System.Drawing.Point(200, 26)
+        Me.txtInto.Multiline = True
+        Me.txtInto.Name = "txtInto"
+        Me.txtInto.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.txtInto.Size = New System.Drawing.Size(190, 43)
+        Me.txtInto.TabIndex = 8
         '
         'dlgIndicatorVariable
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(402, 254)
+        Me.Controls.Add(Me.txtInto)
+        Me.Controls.Add(Me.ucrBase)
+        Me.Controls.Add(Me.grpLeveltobeOmiited)
+        Me.Controls.Add(Me.lblInto)
+        Me.Controls.Add(Me.lstPasteInto)
+        Me.Controls.Add(Me.chkWithX)
+        Me.Controls.Add(Me.cboNonFactors)
+        Me.Controls.Add(Me.cboAvailableFactors)
+        Me.Controls.Add(Me.lblAvailableFactors)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.Name = "dlgIndicatorVariable"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "dlgIndicatorVariable"
+        Me.Text = "Indicator (dummy) Variable"
+        Me.grpLeveltobeOmiited.ResumeLayout(False)
+        Me.grpLeveltobeOmiited.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents lblAvailableFactors As Label
+    Friend WithEvents cboAvailableFactors As ComboBox
+    Friend WithEvents cboNonFactors As ComboBox
+    Friend WithEvents chkWithX As CheckBox
+    Friend WithEvents lstPasteInto As ListBox
+    Friend WithEvents lblInto As Label
+    Friend WithEvents grpLeveltobeOmiited As GroupBox
+    Friend WithEvents txtLevel As TextBox
+    Friend WithEvents rdoLevelNumber As RadioButton
+    Friend WithEvents rdoLast As RadioButton
+    Friend WithEvents rdoFirst As RadioButton
+    Friend WithEvents rdoNone As RadioButton
+    Friend WithEvents ucrBase As ucrButtons
+    Friend WithEvents txtInto As TextBox
 End Class
--- instat/dlgIndicatorVariable.vb
+++ instat/dlgIndicatorVariable.vb
@@ -1,3 +1,83 @@
-﻿Public Class dlgIndicatorVariable
+﻿'Instat-R
+'Copyright (C) 2015
+'This program is free software: you can redistribute it and/or modify
+'it under the terms of the GNU General Public License as published by
+'the Free Software Foundation, either version 3 of the License, or
+'(at your option) any later version.
+'This program is distributed in the hope that it will be useful,
+'but WITHOUT ANY WARRANTY; without even the implied warranty of
+'MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'GNU General Public License for more details.
+'You should have received a copy of the GNU General Public License k
+'along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'
 
+Imports instat.Translations
+''' <summary>
+''' Indicator Variable Class
+''' This Class contains the scripts that will handle the running and behavior of the Indicator Variable dialogue
+''' </summary>
+''' <Prerequisites>
+''' Needs to import the translation class that will handle the multilingual aspect
+''' For it to be fully functional, data must be loaded in the editor.
+''' Data loaded has to be sensible, in that the data has to have columns of type factor and non-factors
+''' </Prerequisites>
+''' <usage>
+''' This class will be used to generate Indicator variables
+''' This class will be instantiated when the Indicator Variable dialogue is opened and disposed of when the dialog is closed
+''' Not only will these module be able load the specific columns but also store the specific information about which data frame they originate from
+''' </usage>
+''' <Limitations>
+''' The available factors and non factors at the moment are just combo boxes, I propose they become user controls.
+''' Cannot yet write back to the data frame, will be implemented later
+''' It is still a work in progress
+''' </Limitations>
+''' <Future>
+''' To have a fully functional dialogue, it still the target, that works with multiple data frames.
+''' </Future>
+
+Public Class dlgIndicatorVariable
+    ''' <summary>
+    ''' Handles the Load event of the dlgIndicatorVariable control.
+    ''' </summary>
+    ''' <param name="sender">The source of the event.</param>
+    ''' <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+    ''' <code>
+    ''' autoTranslate(me) handles the multilingual capabilities for the dialogue.
+    ''' </code>
+    Private Sub dlgIndicatorVariable_Load(sender As Object, e As EventArgs) Handles Me.Load
+        autoTranslate(Me)
+    End Sub
+
+    ''' <summary>
+    ''' Handles the CheckedChanged event of the chkWithX control.
+    ''' </summary>
+    ''' <param name="sender">The source of the event.</param>
+    ''' <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+    ''' <code>
+    ''' The checkChanged event for chkWithX toggles on/off cboNonFactors enabled or not.
+    ''' </code>
+    Private Sub chkWithX_CheckedChanged(sender As Object, e As EventArgs) Handles chkWithX.CheckedChanged
+        If chkWithX.Checked = True Then
+            cboNonFactors.Enabled = True
+        ElseIf chkWithX.Checked = False
+            cboNonFactors.Enabled = False
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Handles the CheckedChanged event of the rdoLevelNumber control.
+    ''' </summary>
+    ''' <param name="sender">The source of the event.</param>
+    ''' <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+    ''' <code>
+    ''' The checkChanged event for rdoLevelNumber toggles on/off txtLevel being enabled or not.
+    ''' </code>
+    Private Sub rdoLevelNumber_CheckedChanged(sender As Object, e As EventArgs) Handles rdoLevelNumber.CheckedChanged
+        If rdoLevelNumber.Checked = True Then
+            txtLevel.Enabled = True
+        ElseIf rdoLevelNumber.Checked = False
+            txtLevel.Enabled = False
+        End If
+    End Sub
 End Class
