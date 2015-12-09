cdc38e6aea768bf5c37b486c549aed0386f37a6b
John Lunalo <johnlunalo95@gmail.com>
2015-12-09 12:42:54 +0300

    this code is just loading the font dialog. I would like someone to take it on as further task to implement its full functionality.

---
+++ instat/dlgFont.vb
@@ -0,0 +1,20 @@
+﻿Imports System
+Imports System.Data
+Imports System.Windows.Forms
+Imports System.Drawing
+Imports instat.Translations
+Public Class dlgFont
+    Private Sub dlgFont_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim dlgFont As System.Windows.Forms.FontDialog
+
+        autoTranslate(Me)
+        dlgFont = New System.Windows.Forms.FontDialog
+        'setting font
+        If dlgFont.ShowDialog() = DialogResult.OK Then
+            Debug.WriteLine(dlgFont.Font)
+            Debug.WriteLine(dlgFont.Color)
+
+        End If
+
+    End Sub
+End Class
