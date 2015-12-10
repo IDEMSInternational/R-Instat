d7f3de2e08fd5d9fff7c421db8b9e9af6b98c737
Mary Mutahi <mutahi.nyambura@outlook.com>
2015-12-10 10:49:12 +0300

    NP-One Way ANOVA dialog

--- instat/dlgNon-ParametricANOVA.Designer.vb
+++ instat/dlgNon-ParametricANOVA.Designer.vb
@@ -20,19 +20,88 @@ Partial Class dlgNon_ParametricANOVA
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.lblYVariate = New System.Windows.Forms.Label()
+        Me.lblFactor = New System.Windows.Forms.Label()
+        Me.ucrBase = New instat.ucrButtons()
+        Me.ucrReceiverFactor = New instat.ucrReceiverSingle()
+        Me.ucrReceiverYVariate = New instat.ucrReceiverSingle()
+        Me.ucrAddRemove = New instat.ucrSelectorAddRemove()
         Me.SuspendLayout()
+        '
+        'lblYVariate
+        '
+        Me.lblYVariate.AutoSize = True
+        Me.lblYVariate.Location = New System.Drawing.Point(267, 22)
+        Me.lblYVariate.Name = "lblYVariate"
+        Me.lblYVariate.Size = New System.Drawing.Size(49, 13)
+        Me.lblYVariate.TabIndex = 4
+        Me.lblYVariate.Tag = "Y_Variate"
+        Me.lblYVariate.Text = "Y variate"
+        '
+        'lblFactor
+        '
+        Me.lblFactor.AutoSize = True
+        Me.lblFactor.Location = New System.Drawing.Point(267, 76)
+        Me.lblFactor.Name = "lblFactor"
+        Me.lblFactor.Size = New System.Drawing.Size(37, 13)
+        Me.lblFactor.TabIndex = 5
+        Me.lblFactor.Tag = "Factor"
+        Me.lblFactor.Text = "Factor"
+        '
+        'ucrBase
+        '
+        Me.ucrBase.Location = New System.Drawing.Point(0, 172)
+        Me.ucrBase.Name = "ucrBase"
+        Me.ucrBase.Size = New System.Drawing.Size(410, 32)
+        Me.ucrBase.TabIndex = 3
+        '
+        'ucrReceiverFactor
+        '
+        Me.ucrReceiverFactor.Location = New System.Drawing.Point(261, 88)
+        Me.ucrReceiverFactor.Name = "ucrReceiverFactor"
+        Me.ucrReceiverFactor.Size = New System.Drawing.Size(106, 26)
+        Me.ucrReceiverFactor.TabIndex = 2
+        '
+        'ucrReceiverYVariate
+        '
+        Me.ucrReceiverYVariate.Location = New System.Drawing.Point(261, 35)
+        Me.ucrReceiverYVariate.Name = "ucrReceiverYVariate"
+        Me.ucrReceiverYVariate.Size = New System.Drawing.Size(106, 26)
+        Me.ucrReceiverYVariate.TabIndex = 1
+        '
+        'ucrAddRemove
+        '
+        Me.ucrAddRemove.Location = New System.Drawing.Point(16, 5)
+        Me.ucrAddRemove.Name = "ucrAddRemove"
+        Me.ucrAddRemove.Size = New System.Drawing.Size(203, 127)
+        Me.ucrAddRemove.TabIndex = 0
         '
         'dlgNon_ParametricANOVA
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(412, 204)
+        Me.Controls.Add(Me.lblFactor)
+        Me.Controls.Add(Me.lblYVariate)
+        Me.Controls.Add(Me.ucrBase)
+        Me.Controls.Add(Me.ucrReceiverFactor)
+        Me.Controls.Add(Me.ucrReceiverYVariate)
+        Me.Controls.Add(Me.ucrAddRemove)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.Name = "dlgNon_ParametricANOVA"
+        Me.Tag = "Non_Parametric_One_Way_ANOVA"
         Me.Text = "dlgNon-ParametricANOVA"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents ucrAddRemove As ucrSelectorAddRemove
+    Friend WithEvents ucrReceiverYVariate As ucrReceiverSingle
+    Friend WithEvents ucrReceiverFactor As ucrReceiverSingle
+    Friend WithEvents ucrBase As ucrButtons
+    Friend WithEvents lblYVariate As Label
+    Friend WithEvents lblFactor As Label
 End Class
