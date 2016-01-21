f4b0f67606e9c0cb6d3403d7a3ab2921b35d7bcb
John Lunalo <johnlunalo95@gmail.com>
2016-01-21 13:57:31 +0300

    adding basic controls and code on Merge dialog

--- instat/dlgMerge.Designer.vb
+++ instat/dlgMerge.Designer.vb
@@ -20,10 +20,66 @@ Partial Class dlgMerge
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.ucrBase = New instat.ucrButtons()
+        Me.ucrDataFrameAddRemove = New instat.ucrSelectorByDataFrameAddRemove()
+        Me.ucrReceiverMerge = New instat.ucrReceiverMultiple()
+        Me.lblVariables = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ucrBase
+        '
+        Me.ucrBase.Location = New System.Drawing.Point(12, 294)
+        Me.ucrBase.Name = "ucrBase"
+        Me.ucrBase.Size = New System.Drawing.Size(410, 53)
+        Me.ucrBase.TabIndex = 0
+        '
+        'ucrDataFrameAddRemove
+        '
+        Me.ucrDataFrameAddRemove.Location = New System.Drawing.Point(12, 9)
+        Me.ucrDataFrameAddRemove.Name = "ucrDataFrameAddRemove"
+        Me.ucrDataFrameAddRemove.Size = New System.Drawing.Size(242, 179)
+        Me.ucrDataFrameAddRemove.TabIndex = 1
+        '
+        'ucrReceiverMerge
+        '
+        Me.ucrReceiverMerge.Location = New System.Drawing.Point(276, 25)
+        Me.ucrReceiverMerge.Name = "ucrReceiverMerge"
+        Me.ucrReceiverMerge.Size = New System.Drawing.Size(121, 128)
+        Me.ucrReceiverMerge.TabIndex = 2
+        '
+        'lblVariables
+        '
+        Me.lblVariables.AutoSize = True
+        Me.lblVariables.Location = New System.Drawing.Point(273, 9)
+        Me.lblVariables.Name = "lblVariables"
+        Me.lblVariables.Size = New System.Drawing.Size(50, 13)
+        Me.lblVariables.TabIndex = 3
+        Me.lblVariables.Tag = "Variables"
+        Me.lblVariables.Text = "Variables"
+        '
+        'dlgMerge
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "dlgMerge"
+        Me.ClientSize = New System.Drawing.Size(433, 359)
+        Me.Controls.Add(Me.lblVariables)
+        Me.Controls.Add(Me.ucrReceiverMerge)
+        Me.Controls.Add(Me.ucrDataFrameAddRemove)
+        Me.Controls.Add(Me.ucrBase)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "dlgMerge"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Tag = "Merge"
+        Me.Text = "Merge"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents ucrBase As ucrButtons
+    Friend WithEvents ucrDataFrameAddRemove As ucrSelectorByDataFrameAddRemove
+    Friend WithEvents ucrReceiverMerge As ucrReceiverMultiple
+    Friend WithEvents lblVariables As Label
 End Class
