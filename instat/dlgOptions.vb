e61afec4d36142eb9dce007b9d6848a904ed0d7b
max <maxwellfundi@live.com>
2016-01-21 11:11:56 +0300

    Addeded language settings into options dialog

--- instat/dlgOptions.vb
+++ instat/dlgOptions.vb
@@ -14,9 +14,40 @@
 ' You should have received a copy of the GNU General Public License k
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+Imports System.Globalization
+Imports System.Threading
 Imports instat.Translations
+
 Public Class dlgOptions
     Private Sub dlgOptions_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         autoTranslate(Me)
+    End Sub
+
+    Private Sub rdoKiswahili_CheckedChanged(sender As Object, e As EventArgs) Handles rdoKiswahili.CheckedChanged
+        If rdoKiswahili.Checked = True Then
+            Thread.CurrentThread.CurrentCulture = New CultureInfo("sw-KE")
+            Thread.CurrentThread.CurrentUICulture = New CultureInfo("sw-KE")
+            autoTranslate(frmMain)
+            autoTranslate(Me)
+        End If
+    End Sub
+
+    Private Sub rdoFrench_CheckedChanged(sender As Object, e As EventArgs) Handles rdoFrench.CheckedChanged
+        If rdoFrench.Checked = True Then
+            Thread.CurrentThread.CurrentCulture = New CultureInfo("fr-FR")
+            Thread.CurrentThread.CurrentUICulture = New CultureInfo("fr-FR")
+            autoTranslate(frmMain)
+            autoTranslate(Me)
+
+        End If
+    End Sub
+
+    Private Sub rdoEnglish_CheckedChanged(sender As Object, e As EventArgs) Handles rdoEnglish.CheckedChanged
+        If rdoEnglish.Checked = True Then
+            Thread.CurrentThread.CurrentCulture = New CultureInfo("en-US")
+            Thread.CurrentThread.CurrentUICulture = New CultureInfo("en-US")
+            autoTranslate(frmMain)
+            autoTranslate(Me)
+        End If
     End Sub
 End Class
