11769f8962b9f8b40fe948f8678d384e588eb342
max <maxwellfundi@live.com>
2016-02-03 15:03:40 +0300

    adding parameters for dlgWaterBalance for climatic menu

--- instat/dlgWaterBalance.vb
+++ instat/dlgWaterBalance.vb
@@ -16,31 +16,46 @@
 
 Imports instat.Translations
 Public Class dlgWaterBalance
+
     Private Sub UcrButtons1_Load(sender As Object, e As EventArgs) Handles ucrBase.Load
         autoTranslate(Me)
         ucrBase.clsRsyntax.SetFunction("climate_obj$display_water_balance()")
-        ucrBase.clsRsyntax.iCallType = 0
+        ucrBase.clsRsyntax.iCallType = 1
         'frmMain.FillData("climate_obj$climate_data_objects[[1]]$data")
         'frmMain.FillData("climate_obj$used_data_objects[[1]]$data")
         Me.Hide()
-
+    End Sub
+    Private Sub dlgStartofRains_Load(sender As Object, e As EventArgs) Handles Me.Load
+        'UpdateVisible()
+        autoTranslate(Me)
     End Sub
 
     Private Sub txtColunmName_Leave(sender As Object, e As EventArgs) Handles txtColumnName.Leave
         ucrBase.clsRsyntax.AddParameter("col_name", Chr(34) & txtColumnName.Text & Chr(34))
-        ucrBase.clsRsyntax.AddParameter("evaporation", Chr(34) & txtColumnName.Text & Chr(34))
-        ucrBase.clsRsyntax.AddParameter("decimal_places", Chr(34) & txtColumnName.Text & Chr(34))
-        ucrBase.clsRsyntax.AddParameter("day_display", Chr(34) & txtColumnName.Text & Chr(34))
-        ucrBase.clsRsyntax.AddParameter("capacity_max", Chr(34) & txtColumnName.Text & Chr(34))
-        If chkPrintTable.Checked Then
-            ucrBase.clsRsyntax.AddParameter("print_tables", Chr(34) & txtColumnName.Text & Chr(34))
-
-        End If
     End Sub
 
-    Private Sub dlgStartofRains_Load(sender As Object, e As EventArgs) Handles Me.Load
-        'UpdateVisible()
-        autoTranslate(Me)
+    Private Sub nudEvapration_Leave(sender As Object, e As EventArgs) Handles nudEvaporation.Leave
+        ucrBase.clsRsyntax.AddParameter("evaporation", nudEvaporation.Value)
+    End Sub
+
+    Private Sub nudDecimalPlace_Leave(sender As Object, e As EventArgs) Handles nudDecimalPlace.Leave
+
+        ucrBase.clsRsyntax.AddParameter("decimal_places", nudDecimalPlace.Value)
+    End Sub
+    Private Sub txtDisplayDayName_Leave(sender As Object, e As EventArgs) Handles txtDisplayDayName.Leave
+
+        ucrBase.clsRsyntax.AddParameter("day_display", txtDisplayDayName.Text)
+    End Sub
+
+    Private Sub nudMaximumCapacity_Leave(sender As Object, e As EventArgs) Handles nudMaximumCapacity.Leave
+
+        ucrBase.clsRsyntax.AddParameter("capacity_max", nudMaximumCapacity.Value)
+    End Sub
+    Private Sub chkPrintTable_Leave(sender As Object, e As EventArgs) Handles chkPrintTable.Leave
+        If chkPrintTable.Checked Then
+            ucrBase.clsRsyntax.AddParameter("print_tables", chkPrintTable.Checked.ToString.ToUpper())
+
+        End If
     End Sub
 
 End Class
