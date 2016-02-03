461f296d08ce707fe84ac2fb256d60bd8185159b
Stephen Kogo <stevekogo@gmail.com>
2016-02-03 15:07:19 +0300

    edited dlgSpellLength dialogue

--- instat/dlgSpellLength.vb
+++ instat/dlgSpellLength.vb
@@ -14,8 +14,55 @@
 ' You should have received a copy of the GNU General Public License k
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 Imports instat.Translations
-Public Class dlgSpellLength
-    Private Sub dlgSpellLength_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+Public Class dlgSpellLengths
+    Private Sub dlgSpellLengths_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         autoTranslate(Me)
+        ucrBase.clsRsyntax.SetFunction("climate_obj$spell_lengths")
+        ucrBase.clsRsyntax.iCallType = 0
     End Sub
+
+    Private Sub nudThreshold_Leave(sender As Object, e As EventArgs) Handles nudThreshold.Leave
+        ucrBase.clsRsyntax.AddParameter("threshold", nudThreshold.Value.ToString())
+    End Sub
+
+    Private Sub chkPrintTable_Leave(sender As Object, e As EventArgs) Handles chkPrintTable.Leave
+        If chkPrintTable.Checked Then
+            ucrBase.clsRsyntax.AddParameter("print_table", chkPrintTable.Checked.ToString().ToUpper())
+        End If
+
+    End Sub
+
+    Private Sub chkMonthAbbreviations_Leave(sender As Object, e As EventArgs) Handles chkMonthAbbreviations.Leave
+        If chkMonthAbbreviations.Checked Then
+            ucrBase.clsRsyntax.AddParameter("month_list", chkMonthAbbreviations.Checked.ToString().ToUpper())
+        End If
+
+    End Sub
+
+    Private Sub chkSeparate_Leave(sender As Object, e As EventArgs) Handles chkSeparate.Leave
+        If chkSeparate.Checked Then
+            ucrBase.clsRsyntax.AddParameter("separate", chkSeparate.Checked.ToString().ToUpper())
+        End If
+
+    End Sub
+
+    Private Sub chkRemoveNA_Leave(sender As Object, e As EventArgs) Handles chkRemoveNA.Leave
+        If chkRemoveNA.Checked Then
+            ucrBase.clsRsyntax.AddParameter("na.rm", chkRemoveNA.Checked.ToString().ToUpper())
+        End If
+
+    End Sub
+
+    Private Sub txtDOYearList_Leave(sender As Object, e As EventArgs) Handles txtDOYearList.Leave
+        ucrBase.clsRsyntax.AddParameter("doy_m", Chr(34) & txtDOYearList.Text & Chr(34))
+        'this variable doy_m is a class list, however its a vector of the form c(m,n).
+    End Sub
+
+    Private Sub txtListYears_Leave(sender As Object, e As EventArgs) Handles txtListYears.Leave
+        ucrBase.clsRsyntax.AddParameter("years", Chr(34) & txtListYears.Text & Chr(34))
+        'this variable doy_m is a class list, however its a vector of the form c(m,n).
+
+    End Sub
+
 End Class
