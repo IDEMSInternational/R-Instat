d6134477f98186312d66376bbba4b10cd3f58c24
deaspo <polycarpokock@live.com>
2016-02-09 17:29:25 +0300

    Made the modifications to dlgSave as.

--- instat/dlgSaveAs.vb
+++ instat/dlgSaveAs.vb
@@ -1,52 +1,7 @@
 ﻿Imports System.IO
 Imports instat.Translations
 Public Class dlgSaveAs
-    Dim returnResults As KeyValuePair(Of String, String)
-    Public Function SaveDialog(strFilter As String) As KeyValuePair(Of String, String)
-        Dim dlgOpen As New SaveFileDialog
-        Dim strFilePath, strFileName As String
-        Select Case strFilter
-            Case ".txt"
-                dlgOpen.Filter = "Text file (*.txt)|*.txt"
-            Case ".rtf"
-                dlgOpen.Filter = "Rich Text file (*.rtf)|*.rtf"
-            Case ".dataFile"
-                dlgOpen.Filter = "Comma separated file (*.csv)|*.csv|RDS (*.RDS)|*.RDS"
-        End Select
-        dlgOpen.Title = "Save workbook"
-        If dlgOpen.ShowDialog() = DialogResult.OK Then
-            'checks if the file name is not blank'
-            If dlgOpen.FileName <> "" Then
-                strFileName = Path.GetFileNameWithoutExtension(dlgOpen.FileName)
-                strFilePath = Replace(dlgOpen.FileName, "\", "/")
-                Return New KeyValuePair(Of String, String)(strFileName, strFilePath)
-            End If
-        End If
-        Return New KeyValuePair(Of String, String)("", "")
-    End Function
-
-    Public Sub setFilePath(strFilePath As String)
-        ucrBase.clsRsyntax.AddParameter("file", Chr(34) & strFilePath & Chr(34))
-    End Sub
-
-    Public Sub setFileExtension(strFileExtension As String)
-        Select Case strFileExtension
-            Case ".RDS"
-                'set function
-                setFunction("saveRDS")
-                ucrBase.clsRsyntax.RemoveParameter("x")
-                ucrBase.clsRsyntax.AddParameter("object", frmMain.clsRLink.strInstatDataObject)
-            Case ".csv"
-                'set function
-                setFunction("write.csv")
-                ucrBase.clsRsyntax.RemoveParameter("object")
-                ucrBase.clsRsyntax.AddParameter("x", frmEditor.grdData.CurrentWorksheet.Name)
-        End Select
-    End Sub
-
-    Public Sub setFunction(strRfunction As String)
-        ucrBase.clsRsyntax.SetFunction(strRfunction)
-    End Sub
+    Dim dlgSave As New SaveFileDialog
 
     Private Sub chkLog_CheckedChanged(sender As Object, e As EventArgs) Handles chkLog.CheckedChanged
         If chkLog.Checked = True Then
@@ -72,39 +27,39 @@ Public Class dlgSaveAs
     End Sub
 
     Private Sub cmdLogSave_Click(sender As Object, e As EventArgs) Handles cmdLogSave.Click
-        returnResults = SaveDialog(".txt")
-        'Save and path
-        txtLogSave.Text = returnResults.Value
+        dlgSave.Title = "Save Log File"
+        dlgSave.Filter = "Text file (*.txt)|*.txt"
+        If dlgSave.ShowDialog() = DialogResult.OK Then
+            txtLogSave.Text = dlgSave.FileName.Replace("\", "/")
+        End If
     End Sub
 
     Private Sub cmdOutputSave_Click(sender As Object, e As EventArgs) Handles cmdOutputSave.Click
-        returnResults = SaveDialog(".rtf")
-        'Save path
-        txtOutputSave.Text = returnResults.Value
+        dlgSave.Title = "Save Output File"
+        dlgSave.Filter = "Rich Text file (*.rtf)|*.rtf"
+        If dlgSave.ShowDialog() = DialogResult.OK Then
+            txtOutputSave.Text = dlgSave.FileName.Replace("\", "/")
+        End If
     End Sub
 
     Private Sub ucrBase_ClickOk(sender As Object, e As EventArgs) Handles ucrBase.ClickOk
-        Dim sWriter As StreamWriter
-        If chkLog.Checked = True Then
+        If chkLog.Checked = True And txtLogSave.Text <> "" Then
             Try
-                sWriter = File.AppendText(txtLogSave.Text)
+                Dim sWriter As New StreamWriter(txtOutputSave.Text)
                 sWriter.Write(frmLog.txtLog.Text)
                 sWriter.Flush()
             Catch ex As Exception
             End Try
         End If
-        If chkOutput.Checked = True Then
+        If chkOutput.Checked = True And txtOutputSave.Text <> "" Then
             Try
-                'sWriter = File.AppendText(txtOutputSave.Text)
-                'sWriter.Write(frmLog.txtLog.Text)
-                'sWriter.Flush()
-                frmCommand.txtcommand.SaveFile(txtOutputSave.Text, RichTextBoxStreamType.RichText)
+                frmCommand.txtCommand.SaveFile(txtOutputSave.Text, RichTextBoxStreamType.RichText)
             Catch ex As Exception
             End Try
         End If
     End Sub
 
-    Private Sub txtEditorSave_TextChanged(sender As Object, e As EventArgs) Handles txtEditorSave.TextChanged
+    Private Sub txtEditorSave_TextChanged(sender As Object, e As EventArgs) Handles txtEditorSave.Click
         If txtEditorSave.Text <> "" Then
             ucrBase.OKEnabled(True)
         Else
@@ -113,11 +68,17 @@ Public Class dlgSaveAs
     End Sub
 
     Private Sub cmdEditorSave_Click(sender As Object, e As EventArgs) Handles cmdEditorSave.Click
-        returnResults = SaveDialog(".dataFile")
-        'Save path
-        txtEditorSave.Text = returnResults.Value
-        'Pass the File extension
-        setFileExtension(Path.GetExtension(returnResults.Value))
-        setFilePath(returnResults.Value)
+        dlgSave.Title = "Save Data File"
+        dlgSave.Filter = "RDS Data file (*.RDS)|*.RDS"
+        If dlgSave.ShowDialog() = DialogResult.OK Then
+            'Save path
+            txtEditorSave.Text = dlgSave.FileName.Replace("\", "/")
+        End If
+        'Pass the function
+        ucrBase.clsRsyntax.SetFunction("saveRDS")
+        'Pass the object
+        ucrBase.clsRsyntax.AddParameter("object", frmMain.clsRLink.strInstatDataObject)
+        'Pass the file
+        ucrBase.clsRsyntax.AddParameter("file", Chr(34) & txtEditorSave.Text & Chr(34))
     End Sub
 End Class
