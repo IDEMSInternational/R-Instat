bb19185c48dd79303da580e1319e4688a5343bf3
deaspo <polycarpokock@live.com>
2016-02-09 18:49:57 +0300

    Further modifications to the save as dialog and export data

--- instat/dlgSaveAs.vb
+++ instat/dlgSaveAs.vb
@@ -9,6 +9,7 @@ Public Class dlgSaveAs
         Else
             grpLog.Enabled = False
         End If
+        CheckOKEnable()
     End Sub
 
     Private Sub chkOutput_CheckedChanged(sender As Object, e As EventArgs) Handles chkOutput.CheckedChanged
@@ -17,31 +18,57 @@ Public Class dlgSaveAs
         Else
             grpOutput.Enabled = False
         End If
+        CheckOKEnable()
     End Sub
 
     Private Sub dlgSaveAs_Load(sender As Object, e As EventArgs) Handles Me.Load
         autoTranslate(Me)
         ucrBase.OKEnabled(False)
-        grpLog.Enabled = False
-        grpOutput.Enabled = False
+        If chkOutput.Checked Then
+            grpLog.Enabled = True
+        Else
+            grpLog.Enabled = False
+        End If
+        If chkLog.Checked Then
+            grpOutput.Enabled = True
+        Else
+            grpOutput.Enabled = False
+        End If
     End Sub
 
     Private Sub cmdLogSave_Click(sender As Object, e As EventArgs) Handles cmdLogSave.Click
+        dlgSave.Reset()
         dlgSave.Title = "Save Log File"
         dlgSave.Filter = "Text file (*.txt)|*.txt"
         If dlgSave.ShowDialog() = DialogResult.OK Then
             txtLogSave.Text = dlgSave.FileName.Replace("\", "/")
         End If
+        CheckOKEnable()
     End Sub
 
     Private Sub cmdOutputSave_Click(sender As Object, e As EventArgs) Handles cmdOutputSave.Click
+        dlgSave.Reset()
         dlgSave.Title = "Save Output File"
         dlgSave.Filter = "Rich Text file (*.rtf)|*.rtf"
         If dlgSave.ShowDialog() = DialogResult.OK Then
             txtOutputSave.Text = dlgSave.FileName.Replace("\", "/")
         End If
+        CheckOKEnable()
     End Sub
 
+    Public Sub CheckOKEnable()
+        Dim bEnable = False
+        If txtEditorSave.Text <> "" Then
+            bEnable = True
+            If chkLog.Checked And txtLogSave.Text = "" Then
+                bEnable = False
+            End If
+            If chkOutput.Checked And txtOutputSave.Text = "" Then
+                bEnable = False
+            End If
+        End If
+        ucrBase.OKEnabled(bEnable)
+    End Sub
     Private Sub ucrBase_ClickOk(sender As Object, e As EventArgs) Handles ucrBase.ClickOk
         If chkLog.Checked = True And txtLogSave.Text <> "" Then
             Try
@@ -59,14 +86,6 @@ Public Class dlgSaveAs
         End If
     End Sub
 
-    Private Sub txtEditorSave_TextChanged(sender As Object, e As EventArgs) Handles txtEditorSave.Click
-        If txtEditorSave.Text <> "" Then
-            ucrBase.OKEnabled(True)
-        Else
-            ucrBase.OKEnabled(False)
-        End If
-    End Sub
-
     Private Sub cmdEditorSave_Click(sender As Object, e As EventArgs) Handles cmdEditorSave.Click
         dlgSave.Title = "Save Data File"
         dlgSave.Filter = "RDS Data file (*.RDS)|*.RDS"
@@ -80,5 +99,7 @@ Public Class dlgSaveAs
         ucrBase.clsRsyntax.AddParameter("object", frmMain.clsRLink.strInstatDataObject)
         'Pass the file
         ucrBase.clsRsyntax.AddParameter("file", Chr(34) & txtEditorSave.Text & Chr(34))
+
+        CheckOKEnable()
     End Sub
 End Class
