50262e4b84bf6a09ee9c1221658c7081355ca281
max <maxwellfundi@live.com>
2016-02-09 17:28:20 +0300

    Removed plot and added sample size and number  of samples

--- instat/dlgRandomSample.designer.vb
+++ instat/dlgRandomSample.designer.vb
@@ -22,25 +22,17 @@ Partial Class dlgRandomSample
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.chkPlot = New System.Windows.Forms.CheckBox()
         Me.lblSeed = New System.Windows.Forms.Label()
         Me.txtSeed = New System.Windows.Forms.TextBox()
         Me.ucrDistWithParameters = New instat.ucrDistributionsWithParameters()
         Me.ucrBase = New instat.ucrButtons()
         Me.ucrDataFrameSelector = New instat.ucrDataFrame()
         Me.ucrNewColumnNameSelector = New instat.ucrNewColumnName()
+        Me.lblSampleSize = New System.Windows.Forms.Label()
+        Me.txtSampleSize = New System.Windows.Forms.TextBox()
+        Me.lblNumberofSamples = New System.Windows.Forms.Label()
+        Me.txtNumberofSamples = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
-        '
-        'chkPlot
-        '
-        Me.chkPlot.AutoSize = True
-        Me.chkPlot.Location = New System.Drawing.Point(190, 268)
-        Me.chkPlot.Name = "chkPlot"
-        Me.chkPlot.Size = New System.Drawing.Size(44, 17)
-        Me.chkPlot.TabIndex = 4
-        Me.chkPlot.Tag = "Plot"
-        Me.chkPlot.Text = "Plot"
-        Me.chkPlot.UseVisualStyleBackColor = True
         '
         'lblSeed
         '
@@ -86,18 +78,56 @@ Partial Class dlgRandomSample
         Me.ucrNewColumnNameSelector.Name = "ucrNewColumnNameSelector"
         Me.ucrNewColumnNameSelector.Size = New System.Drawing.Size(376, 35)
         Me.ucrNewColumnNameSelector.TabIndex = 9
+        Me.ucrNewColumnNameSelector.ucrDataFrameSelector = Nothing
+        '
+        'lblSampleSize
+        '
+        Me.lblSampleSize.AutoSize = True
+        Me.lblSampleSize.Location = New System.Drawing.Point(171, 3)
+        Me.lblSampleSize.Name = "lblSampleSize"
+        Me.lblSampleSize.Size = New System.Drawing.Size(65, 13)
+        Me.lblSampleSize.TabIndex = 10
+        Me.lblSampleSize.Tag = "Sample_Size"
+        Me.lblSampleSize.Text = "Sample Size"
+        '
+        'txtSampleSize
+        '
+        Me.txtSampleSize.Location = New System.Drawing.Point(174, 19)
+        Me.txtSampleSize.Name = "txtSampleSize"
+        Me.txtSampleSize.Size = New System.Drawing.Size(100, 20)
+        Me.txtSampleSize.TabIndex = 11
+        '
+        'lblNumberofSamples
+        '
+        Me.lblNumberofSamples.AutoSize = True
+        Me.lblNumberofSamples.Location = New System.Drawing.Point(288, 3)
+        Me.lblNumberofSamples.Name = "lblNumberofSamples"
+        Me.lblNumberofSamples.Size = New System.Drawing.Size(99, 13)
+        Me.lblNumberofSamples.TabIndex = 10
+        Me.lblNumberofSamples.Tag = "Number_of_Samples"
+        Me.lblNumberofSamples.Text = "Number of Samples"
+        '
+        'txtNumberofSamples
+        '
+        Me.txtNumberofSamples.Location = New System.Drawing.Point(291, 19)
+        Me.txtNumberofSamples.Name = "txtNumberofSamples"
+        Me.txtNumberofSamples.Size = New System.Drawing.Size(100, 20)
+        Me.txtNumberofSamples.TabIndex = 11
         '
         'dlgRandomSample
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(418, 342)
+        Me.Controls.Add(Me.txtNumberofSamples)
+        Me.Controls.Add(Me.lblNumberofSamples)
+        Me.Controls.Add(Me.txtSampleSize)
+        Me.Controls.Add(Me.lblSampleSize)
         Me.Controls.Add(Me.ucrNewColumnNameSelector)
         Me.Controls.Add(Me.ucrDataFrameSelector)
         Me.Controls.Add(Me.ucrDistWithParameters)
         Me.Controls.Add(Me.txtSeed)
         Me.Controls.Add(Me.lblSeed)
-        Me.Controls.Add(Me.chkPlot)
         Me.Controls.Add(Me.ucrBase)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.MaximizeBox = False
@@ -111,10 +141,13 @@ Partial Class dlgRandomSample
     End Sub
 
     Friend WithEvents ucrBase As ucrButtons
-    Friend WithEvents chkPlot As CheckBox
     Friend WithEvents lblSeed As Label
     Friend WithEvents txtSeed As TextBox
     Friend WithEvents ucrDistWithParameters As ucrDistributionsWithParameters
     Friend WithEvents ucrDataFrameSelector As ucrDataFrame
     Friend WithEvents ucrNewColumnNameSelector As ucrNewColumnName
+    Friend WithEvents lblSampleSize As Label
+    Friend WithEvents txtSampleSize As TextBox
+    Friend WithEvents lblNumberofSamples As Label
+    Friend WithEvents txtNumberofSamples As TextBox
 End Class
