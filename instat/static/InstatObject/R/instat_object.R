7263960c86b2e2d718e2147589c7a74155d3a1b4
Stephen Kogo <stevekogo@gmail.com>
2016-02-12 19:37:38 +0300

    implememted sort dataframe method

--- instat/static/InstatObject/R/instat_object.R
+++ instat/static/InstatObject/R/instat_object.R
@@ -386,10 +386,10 @@ instat_obj$methods(get_column_factor_levels = function(data_name,col_name = "") 
 } 
 )
 
-# instat_obj$methods(sort_dataframe = function(data_name, col_names = c(), decreasing = FALSE, na.last = TRUE) {
-#   if(!is.character(data_name)) stop("data_name must be of type character")
-#   if(!data_name %in% names(data_objects)) stop(paste("dataframe: ", data_name, " not found"))
-#   
-#   data_objects[[data_name]]$sort_dataframe(col_names = col_names, decreasing = decreasing, na.last = na.last)
-# } 
-# )
+instat_obj$methods(sort_dataframe = function(data_name, col_names = c(), decreasing = FALSE, na.last = TRUE) {
+  if(!is.character(data_name)) stop("data_name must be of type character")
+  if(!data_name %in% names(data_objects)) stop(paste("dataframe: ", data_name, " not found"))
+  
+  data_objects[[data_name]]$sort_dataframe(col_names = col_names, decreasing = decreasing, na.last = na.last)
+} 
+)
