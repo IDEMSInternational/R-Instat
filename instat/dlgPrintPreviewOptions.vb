8f1753d3f50b16ff82c76ca1fc59960194ab2a5e
deaspo <polycarpokock@live.com>
2016-02-15 16:36:01 +0300

    Added the preview and print options to Printpreview dialog, this dialog will be called from file>print preview or file>print

--- instat/dlgPrintPreviewOptions.vb
+++ instat/dlgPrintPreviewOptions.vb
@@ -1,3 +1,33 @@
 ﻿Public Class dlgPrintPreviewOptions
+    Private Sub cmdPrevSheet_Click(sender As Object, e As EventArgs) Handles cmdPrevSheet.Click
+        Dim sheetPreview = frmEditor.grdData.CurrentWorksheet
 
+        Using session = sheetPreview.CreatePrintSession()
+            Using previewPrint As New PrintPreviewDialog()
+                previewPrint.Document = session.PrintDocument
+                previewPrint.SetBounds(200, 200, 1024, 768)
+                previewPrint.PrintPreviewControl.Zoom = 1.0
+                previewPrint.ShowDialog(Me)
+            End Using
+        End Using
+    End Sub
+
+    Private Sub cmdPrtSheet_Click(sender As Object, e As EventArgs) Handles cmdPrtSheet.Click
+        Dim docToPrint As Printing.PrintDocument = Nothing
+        Try
+            docToPrint = frmEditor.grdData.CurrentWorksheet.CreatePrintSession().PrintDocument
+
+        Catch ex As Exception
+            MessageBox.Show(Me, ex.Message, Me.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Return
+        End Try
+        Using pd = New PrintDialog()
+            pd.Document = docToPrint
+            pd.UseEXDialog = True
+            If pd.ShowDialog() = DialogResult.OK Then
+                docToPrint.PrinterSettings = pd.PrinterSettings
+                docToPrint.Print()
+            End If
+        End Using
+    End Sub
 End Class
