1982862e6bbe745fe441030d8ec4217c16589fc1
deaspo <polycarpokock@live.com>
2016-02-15 16:58:34 +0300

    Removed all routines for the grid that are not going through R. Only methods that go through R will be added

--- instat/frmEditor.vb
+++ instat/frmEditor.vb
@@ -13,15 +13,11 @@
 '
 ' You should have received a copy of the GNU General Public License k
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
-Imports RDotNet
-Imports System.IO
-Imports System.Globalization
-Imports System.Threading
+
 Imports instat.Translations
 Imports unvell.ReoGrid.Events
 
 Public Class frmEditor
-    Public clearFilter As unvell.ReoGrid.Data.AutoColumnFilter
     Public WithEvents grdCurrSheet As unvell.ReoGrid.Worksheet
 
     Private Sub frmEditor_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -56,98 +52,6 @@ Public Class frmEditor
         frmMain.clsRLink.RunScript(strSctipt)
     End Sub
 
-    Private Sub resetToDefaultWidthToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles resetToDefaultWidthToolStripMenuItem.Click
-        grdData.DoAction(New unvell.ReoGrid.Actions.SetColumnsWidthAction(grdData.CurrentWorksheet.SelectionRange.Col, grdData.CurrentWorksheet.SelectionRange.Cols, unvell.ReoGrid.Worksheet.InitDefaultColumnWidth))
-    End Sub
-
-    Private Sub hideColumnsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles hideColumnsToolStripMenuItem.Click
-        grdData.DoAction(New unvell.ReoGrid.Actions.HideColumnsAction(grdData.CurrentWorksheet.SelectionRange.Col, grdData.CurrentWorksheet.SelectionRange.Cols))
-    End Sub
-
-    Private Sub unhideColumnsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles unhideColumnsToolStripMenuItem.Click
-        grdData.DoAction(New unvell.ReoGrid.Actions.UnhideColumnsAction(grdData.CurrentWorksheet.SelectionRange.Col, grdData.CurrentWorksheet.SelectionRange.Cols))
-    End Sub
-
-    Private Sub groupColumnsToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles groupColumnsToolStripMenuItem1.Click
-        Dim worksheet = grdData.CurrentWorksheet
-
-        Try
-            grdData.DoAction(New unvell.ReoGrid.Actions.AddOutlineAction(unvell.ReoGrid.RowOrColumn.Column, grdData.CurrentWorksheet.SelectionRange.Col, grdData.CurrentWorksheet.SelectionRange.Cols))
-        Catch generatedExceptionName As unvell.ReoGrid.OutlineOutOfRangeException
-            MessageBox.Show("Outline out of available range. The last column of spreadsheet cannot be grouped into outlines.")
-        Catch generatedExceptionName As unvell.ReoGrid.OutlineAlreadyDefinedException
-            MessageBox.Show("Another outline which same as selected one has already exist.")
-        Catch generatedExceptionName As unvell.ReoGrid.OutlineIntersectedException
-            MessageBox.Show("The outline to be added intersects with another existing one.")
-        Catch generatedExceptionName As unvell.ReoGrid.OutlineTooMuchException
-            MessageBox.Show("Level of outlines reached the maximum number of levels (10).")
-        End Try
-    End Sub
-
-    Private Sub columnFilterToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles columnFilterToolStripMenuItem.Click
-        If clearFilter IsNot Nothing Then
-            clearFilter.Detach()
-        End If
-        Dim filter As New unvell.ReoGrid.Actions.CreateAutoFilterAction(grdData.CurrentWorksheet.SelectionRange)
-        grdData.DoAction(filter)
-        clearFilter = filter.AutoColumnFilter
-    End Sub
-
-    Private Sub clearColumnFilterToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles clearColumnFilterToolStripMenuItem.Click
-        If clearFilter IsNot Nothing Then
-            clearFilter.Detach()
-        End If
-
-    End Sub
-
-    Private Sub ungroupColumnsToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles ungroupColumnsToolStripMenuItem1.Click
-        Dim removeOutlineAction = New unvell.ReoGrid.Actions.RemoveOutlineAction(unvell.ReoGrid.RowOrColumn.Column, grdData.CurrentWorksheet.SelectionRange.Col, grdData.CurrentWorksheet.SelectionRange.Cols)
-
-        Try
-            grdData.DoAction(removeOutlineAction)
-        Catch
-        End Try
-
-        If removeOutlineAction.RemovedOutline Is Nothing Then
-            MessageBox.Show("No grouped columns and outline found at specified position.")
-        End If
-    End Sub
-
-    Private Sub ungroupAllColumnsToolStripMenuItem_Click_1(sender As Object, e As EventArgs) Handles ungroupAllColumnsToolStripMenuItem.Click
-        grdData.DoAction(New unvell.ReoGrid.Actions.ClearOutlineAction(unvell.ReoGrid.RowOrColumn.Column))
-    End Sub
-
-    Private Sub groupRowsToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles groupRowsToolStripMenuItem1.Click
-        Try
-            grdData.DoAction(New unvell.ReoGrid.Actions.AddOutlineAction(unvell.ReoGrid.RowOrColumn.Row, grdData.CurrentWorksheet.SelectionRange.Row, grdData.CurrentWorksheet.SelectionRange.Rows))
-        Catch generatedExceptionName As unvell.ReoGrid.OutlineOutOfRangeException
-            MessageBox.Show("Outline out of available range. The last row of spreadsheet cannot be grouped into outlines.")
-        Catch generatedExceptionName As unvell.ReoGrid.OutlineAlreadyDefinedException
-            MessageBox.Show("Another same outline already exists.")
-        Catch generatedExceptionName As unvell.ReoGrid.OutlineIntersectedException
-            MessageBox.Show("The outline to be added intersects with another existing one.")
-        Catch generatedExceptionName As unvell.ReoGrid.OutlineTooMuchException
-            MessageBox.Show("Level of outlines reached the maximum number of levels (10).")
-        End Try
-    End Sub
-
-    Private Sub ungroupAllRowsToolStripMenuItem_Click_1(sender As Object, e As EventArgs) Handles ungroupAllRowsToolStripMenuItem.Click
-        grdData.DoAction(New unvell.ReoGrid.Actions.ClearOutlineAction(unvell.ReoGrid.RowOrColumn.Row))
-    End Sub
-
-    Private Sub ungroupRowsToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles ungroupRowsToolStripMenuItem1.Click
-        Dim removeOutlineAction = New unvell.ReoGrid.Actions.RemoveOutlineAction(unvell.ReoGrid.RowOrColumn.Row, grdData.CurrentWorksheet.SelectionRange.Row, grdData.CurrentWorksheet.SelectionRange.Rows)
-
-        Try
-            grdData.DoAction(removeOutlineAction)
-        Catch
-        End Try
-
-        If removeOutlineAction.RemovedOutline Is Nothing Then
-            MessageBox.Show("No grouped rows and outline found at specified position.")
-        End If
-    End Sub
-
     Private Sub insertRowToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles insertRowToolStripMenuItem.Click
         Dim strSctipt As String
         strSctipt = frmMain.clsRLink.strInstatDataObject & "$insert_row_in_data(data_name =" & Chr(34) & grdData.CurrentWorksheet.Name & Chr(34) & ",row_data = " & "c(), start_pos = " & grdData.CurrentWorksheet.SelectionRange.Row + 1 & ",number_rows =" & grdData.CurrentWorksheet.SelectionRange.Rows & ")"
@@ -160,49 +64,8 @@ Public Class frmEditor
         frmMain.clsRLink.RunScript(strSctipt)
     End Sub
 
-    Private Sub resetToDefaultHeightToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles resetToDefaultHeightToolStripMenuItem.Click
-        grdData.DoAction(New unvell.ReoGrid.Actions.SetRowsHeightAction(grdData.CurrentWorksheet.SelectionRange.Row, grdData.CurrentWorksheet.SelectionRange.Rows, unvell.ReoGrid.Worksheet.InitDefaultRowHeight))
-    End Sub
-
-    Private Sub hideRowsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles hideRowsToolStripMenuItem.Click
-        grdData.DoAction(New unvell.ReoGrid.Actions.HideRowsAction(grdData.CurrentWorksheet.SelectionRange.Row, grdData.CurrentWorksheet.SelectionRange.Rows))
-    End Sub
-
-    Private Sub unhideRowsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles unhideRowsToolStripMenuItem.Click
-        grdData.DoAction(New unvell.ReoGrid.Actions.UnhideRowsAction(grdData.CurrentWorksheet.SelectionRange.Row, grdData.CurrentWorksheet.SelectionRange.Rows))
-    End Sub
-
-    Private Sub cutRangeToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles cutRangeToolStripMenuItem.Click
-        Try
-            grdData.CurrentWorksheet.Cut()
-        Catch generatedExceptionName As unvell.ReoGrid.RangeIntersectionException
-            MessageBox.Show("Cannot cut a range that is a part of another merged cell.")
-        Catch
-            MessageBox.Show("We can't to do that for selected range.")
-        End Try
-    End Sub
-
-    Private Sub copyRangeToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles copyRangeToolStripMenuItem.Click
-        Try
-            grdData.CurrentWorksheet.Copy()
-        Catch generatedExceptionName As unvell.ReoGrid.RangeIntersectionException
-            MessageBox.Show("Cannot cut a range that is a part of another merged cell.")
-        Catch
-            MessageBox.Show("We can't to do that for selected range.")
-        End Try
-    End Sub
-
-    Private Sub pasteRangeToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles pasteRangeToolStripMenuItem.Click
-        Try
-            grdData.CurrentWorksheet.Paste()
-        Catch ex As Exception
-            MessageBox.Show(ex.Message)
-        End Try
-    End Sub
-
-
     Private Sub insertSheet_Click(sender As Object, e As EventArgs) Handles insertSheet.Click
-        grdData.InsertWorksheet(grdData.GetWorksheetIndex(grdData.CurrentWorksheet), grdData.CreateWorksheet())
+        dlgFileNew.ShowDialog()
     End Sub
 
     Private Sub deleteSheet_Click(sender As Object, e As EventArgs) Handles deleteSheet.Click
@@ -210,20 +73,16 @@ Public Class frmEditor
         If grdData.Worksheets.Count > 0 Then
             strSctipt = frmMain.clsRLink.strInstatDataObject & "$delete_dataframe(data_name =" & Chr(34) & grdData.CurrentWorksheet.Name & Chr(34) & ")"
             frmMain.clsRLink.RunScript(strSctipt)
-            'grdData.RemoveWorksheet(grdData.GetWorksheetIndex(grdData.CurrentWorksheet))
         End If
     End Sub
 
     Private Sub grdData_WorksheetRemoved(sender As Object, e As WorksheetRemovedEventArgs) Handles grdData.WorksheetRemoved
         If grdData.Worksheets.Count < 1 Then
             grdData.Hide()
-            MessageBox.Show("No more worksheets loaded", "Attention")
         End If
     End Sub
 
-
     Private Sub mnuColumnRename_Click(sender As Object, e As EventArgs) Handles mnuColumnRename.Click
-        'dlgName.setWorksheet(grdData.CurrentWorksheet.Name, (grdData.CurrentWorksheet.ColumnHeaders(grdData.CurrentWorksheet.SelectionRange.Col)).[Text])
         dlgName.ShowDialog()
     End Sub
 
