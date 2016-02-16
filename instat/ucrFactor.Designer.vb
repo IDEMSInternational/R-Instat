1e6b7c0f5a78e0b836b242525d7b5c7a21d034e6
dannyparsons <danny@aims.ac.za>
2016-02-16 22:54:19 +0000

    corrected problems with receivers and updated ucrFactor.

--- instat/ucrFactor.Designer.vb
+++ instat/ucrFactor.Designer.vb
@@ -22,8 +22,36 @@ Partial Class ucrFactor
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.grdFactorData = New unvell.ReoGrid.ReoGridControl()
+        Me.SuspendLayout()
+        '
+        'grdFactorData
+        '
+        Me.grdFactorData.BackColor = System.Drawing.Color.White
+        Me.grdFactorData.ColumnHeaderContextMenuStrip = Nothing
+        Me.grdFactorData.LeadHeaderContextMenuStrip = Nothing
+        Me.grdFactorData.Location = New System.Drawing.Point(3, 3)
+        Me.grdFactorData.Name = "grdFactorData"
+        Me.grdFactorData.Readonly = True
+        Me.grdFactorData.RowHeaderContextMenuStrip = Nothing
+        Me.grdFactorData.Script = Nothing
+        Me.grdFactorData.SheetTabContextMenuStrip = Nothing
+        Me.grdFactorData.SheetTabControlNewButtonVisible = False
+        Me.grdFactorData.SheetTabControlWidth = 60
+        Me.grdFactorData.SheetTabNewButtonVisible = False
+        Me.grdFactorData.Size = New System.Drawing.Size(422, 174)
+        Me.grdFactorData.TabIndex = 29
+        '
+        'ucrFactor
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.grdFactorData)
+        Me.Name = "ucrFactor"
+        Me.Size = New System.Drawing.Size(435, 186)
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents grdFactorData As unvell.ReoGrid.ReoGridControl
 End Class
