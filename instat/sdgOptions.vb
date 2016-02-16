503731fd9986bc33513c763ec4cb962b750bdc54
Mary Mutahi <mutahi.nyambura@outlook.com>
2016-02-16 13:13:33 +0300

    changes on the code view of sdgOptions

--- instat/sdgOptions.vb
+++ instat/sdgOptions.vb
@@ -1,37 +1,3 @@
 ﻿Public Class sdgOptions
-    Private Sub sdgOptions_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-    End Sub
-
-    Private Sub tbpDisplay_Click(sender As Object, e As EventArgs) Handles tbpDisplay.Click
-
-    End Sub
-
-    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles chkANOVATable.CheckedChanged
-
-    End Sub
-
-    Private Sub CheckBox3_CheckedChanged(sender As Object, e As EventArgs) Handles chkANOVAPValues.CheckedChanged
-
-    End Sub
-
-    Private Sub CheckBox4_CheckedChanged(sender As Object, e As EventArgs) Handles chkEstimates.CheckedChanged
-
-    End Sub
-
-    Private Sub CheckBox5_CheckedChanged(sender As Object, e As EventArgs) Handles chkEstimateValues.CheckedChanged
-
-    End Sub
-
-    Private Sub CheckBox6_CheckedChanged(sender As Object, e As EventArgs) Handles chkTableOfMeans.CheckedChanged
-
-    End Sub
-
-    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles chkModel.CheckedChanged
-
-    End Sub
-
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblConfidenceLevel.Click
-
-    End Sub
 End Class
