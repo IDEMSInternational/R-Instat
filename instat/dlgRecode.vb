c2a273eab7aff7a6d160563398c7af4c695aa206
John Lunalo <johnlunalo95@gmail.com>
2016-02-17 14:06:43 +0300

    Adding R code on dlgRecode

--- instat/dlgRecode.vb
+++ instat/dlgRecode.vb
@@ -21,6 +21,8 @@ Public Class dlgRecode
         ucrReceiverSingle.Selector = ucrSelectorDataFrameAddRemove
         ucrReceiverSingle.SetMeAsReceiver()
         autoTranslate(Me)
+        ucrBase.clsRsyntax.SetFunction("cut")
+        ucrBase.clsRsyntax.AddParameter("breaks")
 
         ucrSelectorNewColumnName.SetDataFrameSelector(ucrSelectorDataFrameAddRemove.ucrAvailableDataFrames)
         ucrSelectorNewColumnName.SetPrefix("Recode")
@@ -28,7 +30,29 @@ Public Class dlgRecode
 
     End Sub
     Private Sub ucrReceiverSingle_Leave(sender As Object, e As EventArgs) Handles ucrReceiverSingle.Leave
-        ucrBase.clsRsyntax.AddParameter("X", clsRFunctionParameter:=ucrReceiverSingle.GetVariables())
+        ucrBase.clsRsyntax.AddParameter("x", clsRFunctionParameter:=ucrReceiverSingle.GetVariables())
+
+    End Sub
+
+    Private Sub ucrMultipleNumeric_Leave(sender As Object, e As EventArgs) Handles ucrMultipleNumeric.Leave
+        ucrBase.clsRsyntax.AddParameter("breaks")
+    End Sub
+
+
+
+    Private Sub chkAddLabels_CheckedChanged(sender As Object, e As EventArgs) Handles chkAddLabels.CheckedChanged
+        If chkAddLabels.Checked = True Then
+            ucrBase.clsRsyntax.AddParameter("labels")
+        End If
+    End Sub
+
+    Private Sub grpClosedOn_CheckedChanged(sender As Object, e As EventArgs) Handles rdoLeft.CheckedChanged, rdoRight.CheckedChanged
+        If rdoLeft.Checked = True Then
+            ucrBase.clsRsyntax.AddParameter("right", "FALSE")
+        Else
+            ucrBase.clsRsyntax.AddParameter("right", "TRUE")
+
+        End If
 
     End Sub
 
