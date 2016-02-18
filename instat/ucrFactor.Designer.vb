58d86273fc0606eb9cdb546f577e749765cb3649
deaspo <polycarpokock@live.com>
2016-02-18 14:41:34 +0300

    Made the modifications as advised.Also am setting the default values to False when loading

--- instat/ucrFactor.Designer.vb
+++ instat/ucrFactor.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class ucrFactor
     Inherits System.Windows.Forms.UserControl
 
     'UserControl overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,28 +20,28 @@ Partial Class ucrFactor
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.lblNoFacrosLoaded = New System.Windows.Forms.Label()
+        Me.lblNoFactorsLoaded = New System.Windows.Forms.Label()
         Me.grdFactorData = New unvell.ReoGrid.ReoGridControl()
         Me.SuspendLayout()
         '
-        'lblNoFacrosLoaded
+        'lblNoFactorsLoaded
         '
-        Me.lblNoFacrosLoaded.AutoSize = True
-        Me.lblNoFacrosLoaded.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblNoFacrosLoaded.Location = New System.Drawing.Point(70, 83)
-        Me.lblNoFacrosLoaded.Name = "lblNoFacrosLoaded"
-        Me.lblNoFacrosLoaded.Size = New System.Drawing.Size(196, 16)
-        Me.lblNoFacrosLoaded.TabIndex = 0
-        Me.lblNoFacrosLoaded.Text = "No factor columns selected"
+        Me.lblNoFactorsLoaded.AutoSize = True
+        Me.lblNoFactorsLoaded.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNoFactorsLoaded.Location = New System.Drawing.Point(3, 18)
+        Me.lblNoFactorsLoaded.Name = "lblNoFactorsLoaded"
+        Me.lblNoFactorsLoaded.Size = New System.Drawing.Size(196, 16)
+        Me.lblNoFactorsLoaded.TabIndex = 0
+        Me.lblNoFactorsLoaded.Text = "No factor columns selected"
         '
         'grdFactorData
         '
         Me.grdFactorData.BackColor = System.Drawing.Color.White
         Me.grdFactorData.ColumnHeaderContextMenuStrip = Nothing
         Me.grdFactorData.LeadHeaderContextMenuStrip = Nothing
-        Me.grdFactorData.Location = New System.Drawing.Point(6, 6)
+        Me.grdFactorData.Location = New System.Drawing.Point(4, 4)
         Me.grdFactorData.Name = "grdFactorData"
         Me.grdFactorData.Readonly = True
         Me.grdFactorData.RowHeaderContextMenuStrip = Nothing
@@ -51,7 +51,7 @@ Partial Class ucrFactor
         Me.grdFactorData.SheetTabControlWidth = 60
         Me.grdFactorData.SheetTabNewButtonVisible = False
         Me.grdFactorData.Size = New System.Drawing.Size(260, 174)
-        Me.grdFactorData.TabIndex = 31
+        Me.grdFactorData.TabIndex = 32
         '
         'ucrFactor
         '
@@ -60,14 +60,14 @@ Partial Class ucrFactor
         Me.AutoSize = True
         Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
         Me.Controls.Add(Me.grdFactorData)
-        Me.Controls.Add(Me.lblNoFacrosLoaded)
+        Me.Controls.Add(Me.lblNoFactorsLoaded)
         Me.Name = "ucrFactor"
-        Me.Size = New System.Drawing.Size(269, 183)
+        Me.Size = New System.Drawing.Size(267, 181)
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents lblNoFacrosLoaded As Label
+    Friend WithEvents lblNoFactorsLoaded As Label
     Friend WithEvents grdFactorData As unvell.ReoGrid.ReoGridControl
 End Class
