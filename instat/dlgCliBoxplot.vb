1d47e676ea7052cc96cd399108dc3a91d0005b82
Stephen Kogo <stevekogo@gmail.com>
2016-02-19 14:19:12 +0300

    Appropriate calls to climate object methods

--- instat/dlgCliBoxplot.vb
+++ instat/dlgCliBoxplot.vb
@@ -19,7 +19,7 @@ Public Class dlgCliBoxplot
 
     Private Sub dlgCliBoxplot_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         autoTranslate(Me)
-        ucrBase.clsRsyntax.SetFunction("climate_obj$cliboxplot")
+        ucrBase.clsRsyntax.SetFunction(frmMain.clsRLink.strClimateObject & "$cliboxplot")
         ucrBase.clsRsyntax.iCallType = 0
     End Sub
 
