e8676b9a2cc7bf7769f4e77450d718a5f13e6e54
dannyparsons <danny@aims.ac.za>
2016-02-22 14:41:36 +0000

    added more remove options for receivers

--- instat/ucrReceiverMultiple.designer.vb
+++ instat/ucrReceiverMultiple.designer.vb
@@ -19,12 +19,18 @@ Partial Class ucrReceiverMultiple
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.lstSelectedVariables = New System.Windows.Forms.ListView()
+        Me.MenuStripDelete = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.DeleteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStripDelete.SuspendLayout()
         Me.SuspendLayout()
         '
         'lstSelectedVariables
         '
         Me.lstSelectedVariables.Activation = System.Windows.Forms.ItemActivation.TwoClick
+        Me.lstSelectedVariables.ContextMenuStrip = Me.MenuStripDelete
         Me.lstSelectedVariables.GridLines = True
         Me.lstSelectedVariables.Location = New System.Drawing.Point(3, 5)
         Me.lstSelectedVariables.Name = "lstSelectedVariables"
@@ -33,14 +39,37 @@ Partial Class ucrReceiverMultiple
         Me.lstSelectedVariables.UseCompatibleStateImageBehavior = False
         Me.lstSelectedVariables.View = System.Windows.Forms.View.Details
         '
+        'MenuStripDelete
+        '
+        Me.MenuStripDelete.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.DeleteToolStripMenuItem, Me.ClearToolStripMenuItem})
+        Me.MenuStripDelete.Name = "ContextMenuStrip1"
+        Me.MenuStripDelete.Size = New System.Drawing.Size(153, 70)
+        '
+        'DeleteToolStripMenuItem
+        '
+        Me.DeleteToolStripMenuItem.Name = "DeleteToolStripMenuItem"
+        Me.DeleteToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.DeleteToolStripMenuItem.Tag = "delete"
+        Me.DeleteToolStripMenuItem.Text = "Delete"
+        '
+        'ClearToolStripMenuItem
+        '
+        Me.ClearToolStripMenuItem.Name = "ClearToolStripMenuItem"
+        Me.ClearToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ClearToolStripMenuItem.Text = "Clear"
+        '
         'ucrReceiverMultiple
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.Controls.Add(Me.lstSelectedVariables)
         Me.Name = "ucrReceiverMultiple"
         Me.Size = New System.Drawing.Size(121, 104)
+        Me.MenuStripDelete.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
     Friend WithEvents lstSelectedVariables As ListView
+    Friend WithEvents MenuStripDelete As ContextMenuStrip
+    Friend WithEvents DeleteToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ClearToolStripMenuItem As ToolStripMenuItem
 End Class
