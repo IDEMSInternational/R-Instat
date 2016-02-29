74c0cd163f142a4a8529197d226dd5a9d04adfbf
deaspo <polycarpokock@live.com>
2016-02-29 18:25:10 +0300

    Updating to fix conflicts

--- instat/dlgName.vb
+++ instat/dlgName.vb
@@ -16,61 +16,54 @@
 
 Imports instat.Translations
 Public Class dlgName
-    Dim bFirstLoad As Boolean = True
+    Private bUseDefaultName As Boolean
     Private Sub dlgName_Load(sender As Object, e As EventArgs) Handles Me.Load
-        autoTranslate(Me)
+        ucrBase.iHelpTopicID = 33
+        ucrReceiverNames.Selector = ucrSelectorByDataFrameAddRemove
+        ucrReceiverNames.Selector = ucrSelectorByDataFrameAddRemove
+        defaultSettings()
         'set the function
         ucrBase.clsRsyntax.SetFunction(frmMain.clsRLink.strInstatDataObject & "$rename_column_in_data")
-        ucrReceiverName.Selector = ucrSelectVariables
-        If bFirstLoad Then
-            SetDefaults()
-            bFirstLoad = False
-        End If
-        TestOKEnabled()
+        ucrReceiverNames.txtReceiverSingle.Text = frmEditor.grdData.CurrentWorksheet.ColumnHeaders(frmEditor.grdData.CurrentWorksheet.SelectionRange.Col).[Text]
     End Sub
 
-    Private Sub SetDefaults()
-        txtName.Text = ""
-        ucrReceiverName.SetMeAsReceiver()
-        ucrSelectVariables.Reset()
+    Private Sub defaultSettings()
+        autoTranslate(Me)
+        ucrReceiverNames.Clear()
+        ucrReceiverNames.Enabled = False
+        ucrReceiverNames.Visible = False
+        ucrReceiverNames.Clear()
+        ucrReceiverNames.SetMeAsReceiver()
+        ucrReceiverNames.Enabled = True
+        ucrReceiverNames.Visible = True
+        bUseDefaultName = True
+        'txtName.Text = ""
     End Sub
 
     Private Sub ucrBase_ClickReset(sender As Object, e As EventArgs) Handles ucrBase.ClickReset
-        SetDefaults()
-        TestOKEnabled()
+        defaultSettings()
     End Sub
 
-    Private Sub ucrReceiverName_SelectionChanged(sender As Object, e As EventArgs) Handles ucrReceiverName.SelectionChanged
-        ucrBase.clsRsyntax.AddParameter("column_name", Chr(34) & ucrReceiverName.txtReceiverSingle.Text & Chr(34))
-        txtName.Text = ucrReceiverName.txtReceiverSingle.Text
-        TestOKEnabled()
+    Private Sub ucrSingle_ValueChanged(sender As Object, e As EventArgs) Handles ucrReceiverNames.ValueChanged
+        If bUseDefaultName Then
+            txtName.Text = ucrReceiverNames.txtReceiverSingle.Text
+        End If
     End Sub
 
-    Public Sub setSettings(strCurrentWorksheetName As String, strSelectedColumn As String)
-        'sets the defaults when calling the dialog
-        ucrReceiverName.txtReceiverSingle.Text = strSelectedColumn
-        ucrSelectVariables.ucrAvailableDataFrames.cboAvailableDataFrames.SelectedItem = strCurrentWorksheetName
-        txtName.Text = strSelectedColumn
+    Private Sub txtName_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtName.KeyPress
+        bUseDefaultName = False
+    End Sub
+
+    'Public Sub setWorksheet(strCurrentWorksheetName As String, strSelectedColumn As String)
+    '    ucrAddRemove.AddVariable(strCurrentWorksheetName, strSelectedColumn)
+    'End Sub
+
+    Private Sub ucrSingle_Leave(sender As Object, e As EventArgs) Handles ucrReceiverNames.Leave
+        ucrBase.clsRsyntax.AddParameter("data_name", Chr(34) & frmEditor.grdData.CurrentWorksheet.Name & Chr(34))
+        ucrBase.clsRsyntax.AddParameter("column_name", Chr(34) & frmEditor.grdData.CurrentWorksheet.ColumnHeaders(frmEditor.grdData.CurrentWorksheet.SelectionRange.Col).[Text] & Chr(34))
     End Sub
 
     Private Sub txtName_Leave(sender As Object, e As EventArgs) Handles txtName.Leave
         ucrBase.clsRsyntax.AddParameter("new_val", Chr(34) & txtName.Text & Chr(34))
     End Sub
-    Private Sub TestOKEnabled()
-        If Not ucrReceiverName.IsEmpty() And txtName.Text <> "" Then
-            ucrBase.OKEnabled(True)
-        Else
-            ucrBase.OKEnabled(False)
-        End If
-    End Sub
-
-    Private Sub ucrSelectVariables_DataFrameChanged() Handles ucrSelectVariables.DataFrameChanged
-        ucrBase.clsRsyntax.AddParameter("data_name", Chr(34) & frmEditor.grdData.CurrentWorksheet.Name & Chr(34))
-        TestOKEnabled()
-    End Sub
-
-    Private Sub txtName_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtName.KeyPress
-        TestOKEnabled()
-    End Sub
-
 End Class
