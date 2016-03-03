942becb551912fbac5a589f9830e7f55e816377b
John Lunalo <johnlunalo95@gmail.com>
2016-03-03 17:44:16 +0300

    dlgView
    Updating

--- instat/dlgView.Designer.vb
+++ instat/dlgView.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class dlgView
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,51 +20,19 @@ Partial Class dlgView
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.ucrSelectorForView = New instat.ucrSelectorByDataFrameAddRemove()
-        Me.ucrBase = New instat.ucrButtons()
-        Me.ucrReceiverView = New instat.ucrReceiverMultiple()
-        Me.lblSelected = New System.Windows.Forms.Label()
         Me.grpSelectedRows = New System.Windows.Forms.GroupBox()
         Me.txtBottom = New System.Windows.Forms.TextBox()
         Me.txtTop = New System.Windows.Forms.TextBox()
         Me.rdoBottom = New System.Windows.Forms.RadioButton()
         Me.rdoTop = New System.Windows.Forms.RadioButton()
+        Me.ucrBase = New instat.ucrButtons()
+        Me.ucrSelctorForView = New instat.ucrSelectorByDataFrameAddRemove()
+        Me.ucrReceiverView = New instat.ucrReceiverMultiple()
+        Me.lblSelected = New System.Windows.Forms.Label()
         Me.grpSelectedRows.SuspendLayout()
         Me.SuspendLayout()
-        '
-        'ucrSelectorForView
-        '
-        Me.ucrSelectorForView.Location = New System.Drawing.Point(13, 13)
-        Me.ucrSelectorForView.Name = "ucrSelectorForView"
-        Me.ucrSelectorForView.Size = New System.Drawing.Size(242, 179)
-        Me.ucrSelectorForView.TabIndex = 0
-        '
-        'ucrBase
-        '
-        Me.ucrBase.Location = New System.Drawing.Point(13, 269)
-        Me.ucrBase.Name = "ucrBase"
-        Me.ucrBase.Size = New System.Drawing.Size(401, 53)
-        Me.ucrBase.TabIndex = 1
-        '
-        'ucrReceiverView
-        '
-        Me.ucrReceiverView.Location = New System.Drawing.Point(261, 88)
-        Me.ucrReceiverView.Name = "ucrReceiverView"
-        Me.ucrReceiverView.Selector = Nothing
-        Me.ucrReceiverView.Size = New System.Drawing.Size(121, 104)
-        Me.ucrReceiverView.TabIndex = 2
-        '
-        'lblSelected
-        '
-        Me.lblSelected.AutoSize = True
-        Me.lblSelected.Location = New System.Drawing.Point(262, 69)
-        Me.lblSelected.Name = "lblSelected"
-        Me.lblSelected.Size = New System.Drawing.Size(49, 13)
-        Me.lblSelected.TabIndex = 3
-        Me.lblSelected.Tag = "Selected"
-        Me.lblSelected.Text = "Selected"
         '
         'grpSelectedRows
         '
@@ -72,7 +40,7 @@ Partial Class dlgView
         Me.grpSelectedRows.Controls.Add(Me.txtTop)
         Me.grpSelectedRows.Controls.Add(Me.rdoBottom)
         Me.grpSelectedRows.Controls.Add(Me.rdoTop)
-        Me.grpSelectedRows.Location = New System.Drawing.Point(13, 199)
+        Me.grpSelectedRows.Location = New System.Drawing.Point(12, 195)
         Me.grpSelectedRows.Name = "grpSelectedRows"
         Me.grpSelectedRows.Size = New System.Drawing.Size(401, 52)
         Me.grpSelectedRows.TabIndex = 4
@@ -118,16 +86,48 @@ Partial Class dlgView
         Me.rdoTop.Text = "Top"
         Me.rdoTop.UseVisualStyleBackColor = True
         '
+        'ucrBase
+        '
+        Me.ucrBase.Location = New System.Drawing.Point(12, 253)
+        Me.ucrBase.Name = "ucrBase"
+        Me.ucrBase.Size = New System.Drawing.Size(401, 53)
+        Me.ucrBase.TabIndex = 1
+        '
+        'ucrSelctorForView
+        '
+        Me.ucrSelctorForView.Location = New System.Drawing.Point(13, 13)
+        Me.ucrSelctorForView.Name = "ucrSelctorForView"
+        Me.ucrSelctorForView.Size = New System.Drawing.Size(242, 179)
+        Me.ucrSelctorForView.TabIndex = 5
+        '
+        'ucrReceiverView
+        '
+        Me.ucrReceiverView.Location = New System.Drawing.Point(292, 62)
+        Me.ucrReceiverView.Name = "ucrReceiverView"
+        Me.ucrReceiverView.Selector = Nothing
+        Me.ucrReceiverView.Size = New System.Drawing.Size(121, 104)
+        Me.ucrReceiverView.TabIndex = 6
+        '
+        'lblSelected
+        '
+        Me.lblSelected.AutoSize = True
+        Me.lblSelected.Location = New System.Drawing.Point(292, 43)
+        Me.lblSelected.Name = "lblSelected"
+        Me.lblSelected.Size = New System.Drawing.Size(49, 13)
+        Me.lblSelected.TabIndex = 7
+        Me.lblSelected.Tag = "Selected"
+        Me.lblSelected.Text = "Selected"
+        '
         'dlgView
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(468, 334)
-        Me.Controls.Add(Me.grpSelectedRows)
+        Me.ClientSize = New System.Drawing.Size(468, 318)
         Me.Controls.Add(Me.lblSelected)
         Me.Controls.Add(Me.ucrReceiverView)
+        Me.Controls.Add(Me.ucrSelctorForView)
+        Me.Controls.Add(Me.grpSelectedRows)
         Me.Controls.Add(Me.ucrBase)
-        Me.Controls.Add(Me.ucrSelectorForView)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.Name = "dlgView"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
@@ -139,14 +139,13 @@ Partial Class dlgView
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents ucrSelectorForView As ucrSelectorByDataFrameAddRemove
     Friend WithEvents ucrBase As ucrButtons
-    Friend WithEvents ucrReceiverView As ucrReceiverMultiple
-    Friend WithEvents lblSelected As Label
     Friend WithEvents grpSelectedRows As GroupBox
     Friend WithEvents rdoBottom As RadioButton
     Friend WithEvents rdoTop As RadioButton
     Friend WithEvents txtBottom As TextBox
     Friend WithEvents txtTop As TextBox
+    Friend WithEvents ucrSelctorForView As ucrSelectorByDataFrameAddRemove
+    Friend WithEvents ucrReceiverView As ucrReceiverMultiple
+    Friend WithEvents lblSelected As Label
 End Class
