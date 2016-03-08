2a31ccfa0f19cd0818c50cb4960751ea280e9993
max <maxwellfundi@live.com>
2016-03-08 15:50:49 +0300

    Initial boxplot tidying

--- instat/sdgBoxPlot.Designer.vb
+++ instat/sdgBoxPlot.Designer.vb
@@ -23,6 +23,9 @@ Partial Class sdgBoxPlot
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.ucrBaseSdgBoxplot = New instat.ucrButtonsSubdialogue()
+        Me.chkNotchedBoxplot = New System.Windows.Forms.CheckBox()
+        Me.chkHorizontalBoxplot = New System.Windows.Forms.CheckBox()
+        Me.chkVariableWidth = New System.Windows.Forms.CheckBox()
         Me.SuspendLayout()
         '
         'ucrBaseSdgBoxplot
@@ -32,11 +35,47 @@ Partial Class sdgBoxPlot
         Me.ucrBaseSdgBoxplot.Size = New System.Drawing.Size(142, 30)
         Me.ucrBaseSdgBoxplot.TabIndex = 0
         '
+        'chkNotchedBoxplot
+        '
+        Me.chkNotchedBoxplot.AutoSize = True
+        Me.chkNotchedBoxplot.Location = New System.Drawing.Point(12, 72)
+        Me.chkNotchedBoxplot.Name = "chkNotchedBoxplot"
+        Me.chkNotchedBoxplot.Size = New System.Drawing.Size(104, 17)
+        Me.chkNotchedBoxplot.TabIndex = 20
+        Me.chkNotchedBoxplot.Tag = "Notched_boxplot"
+        Me.chkNotchedBoxplot.Text = "Notched boxplot"
+        Me.chkNotchedBoxplot.UseVisualStyleBackColor = True
+        '
+        'chkHorizontalBoxplot
+        '
+        Me.chkHorizontalBoxplot.AutoSize = True
+        Me.chkHorizontalBoxplot.Location = New System.Drawing.Point(12, 43)
+        Me.chkHorizontalBoxplot.Name = "chkHorizontalBoxplot"
+        Me.chkHorizontalBoxplot.Size = New System.Drawing.Size(110, 17)
+        Me.chkHorizontalBoxplot.TabIndex = 19
+        Me.chkHorizontalBoxplot.Tag = "Horizontal_boxplot"
+        Me.chkHorizontalBoxplot.Text = "Horizontal boxplot"
+        Me.chkHorizontalBoxplot.UseVisualStyleBackColor = True
+        '
+        'chkVariableWidth
+        '
+        Me.chkVariableWidth.AutoSize = True
+        Me.chkVariableWidth.Location = New System.Drawing.Point(12, 12)
+        Me.chkVariableWidth.Name = "chkVariableWidth"
+        Me.chkVariableWidth.Size = New System.Drawing.Size(92, 17)
+        Me.chkVariableWidth.TabIndex = 18
+        Me.chkVariableWidth.Tag = "Variable_width"
+        Me.chkVariableWidth.Text = "Variable width"
+        Me.chkVariableWidth.UseVisualStyleBackColor = True
+        '
         'sdgBoxPlot
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.chkNotchedBoxplot)
+        Me.Controls.Add(Me.chkHorizontalBoxplot)
+        Me.Controls.Add(Me.chkVariableWidth)
         Me.Controls.Add(Me.ucrBaseSdgBoxplot)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.Name = "sdgBoxPlot"
@@ -44,8 +83,12 @@ Partial Class sdgBoxPlot
         Me.Tag = "BoxPlot_Options"
         Me.Text = "BoxPlot Options"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents ucrBaseSdgBoxplot As ucrButtonsSubdialogue
+    Friend WithEvents chkNotchedBoxplot As CheckBox
+    Friend WithEvents chkHorizontalBoxplot As CheckBox
+    Friend WithEvents chkVariableWidth As CheckBox
 End Class
