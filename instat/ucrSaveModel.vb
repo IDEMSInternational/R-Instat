f324133b04d008189ff70f06a861709e90693f57
dannyparsons <danny@aims.ac.za>
2016-03-08 16:03:00 +0000

    added SaveModel control and updated clsRLink

---
+++ instat/ucrSaveModel.vb
@@ -0,0 +1,12 @@
+﻿Public Class ucrSaveModel
+    Public Event CheckedChanged()
+    Private Sub chkSaveModel_CheckedChanged(sender As Object, e As EventArgs) Handles chkSaveModel.CheckedChanged
+        ucrInputModelName.Enabled = False
+        RaiseEvent CheckedChanged()
+    End Sub
+
+    Private Sub ucrSaveModel_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ucrInputModelName.SetDefaultTypeAsModel()
+        ucrInputModelName.SetItemsTypeAsModels()
+    End Sub
+End Class
