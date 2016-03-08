b66bcdacfbcf0e2465a688ac1d611464ecbcf886
John Lunalo <johnlunalo95@gmail.com>
2016-03-08 13:46:16 +0300

    dlgReoderDescriptives
    Changing file name

--- instat/dlgReoderDescriptives.Designer.vb
+++ instat/dlgReoderDescriptives.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class dlgReoderDescriptive
+Partial Class dlgReoderDescriptives
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
