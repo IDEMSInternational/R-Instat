8599efcca1a9f82d62b4eeae583c150d2741452c
John Lunalo <johnlunalo95@gmail.com>
2016-03-08 13:50:08 +0300

    dlgViwDescriptives
    Renaming file

--- instat/dlgViewDescriptives.Designer.vb
+++ instat/dlgViewDescriptives.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class dlgViewDescriptive
+Partial Class dlgViewDescriptives
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
