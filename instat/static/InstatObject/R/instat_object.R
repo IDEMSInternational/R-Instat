679bb1e51214fec2b1b5e77071fd75170b6c62c0
Stephen Kogo <stevekogo@gmail.com>
2016-03-08 15:03:36 +0300

    Extended functionality of move_columns_in_data method to move  than one column

--- instat/static/InstatObject/R/instat_object.R
+++ instat/static/InstatObject/R/instat_object.R
@@ -346,11 +346,11 @@ instat_obj$methods(insert_column_in_data = function(data_name, col_data =c(), st
 }
 )
 
-instat_obj$methods(move_column_in_data = function(data_name, col_name, col_number){
+instat_obj$methods(move_columns_in_data = function(data_name, col_names = "", col_number){
   if(!is.character(data_name)) stop("data_name must be of type character")
   if(!data_name %in% names(data_objects)) stop(paste("dataframe: ", data_name, " not found"))
   
-  data_objects[[data_name]]$move_column_in_data(col_name = col_name, col_number = col_number)
+  data_objects[[data_name]]$move_columns_in_data(col_names = col_names, col_number = col_number)
 }
 )
 
