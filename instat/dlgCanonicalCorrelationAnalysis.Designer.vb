1ccae05efda97b043c12ce483e340d04fd014b6c
Stephen Kogo <stevekogo@gmail.com>
2016-03-08 12:41:37 +0300

    Initial implementation of Canonical Correlations

--- instat/dlgCanonicalCorrelationAnalysis.Designer.vb
+++ instat/dlgCanonicalCorrelationAnalysis.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class dlgCanonicalCorrelationAnalysis
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,32 +20,104 @@ Partial Class dlgCanonicalCorrelationAnalysis
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.ucrBase = New instat.ucrButtons()
+        Me.ucrBaseCCA = New instat.ucrButtons()
+        Me.ucrSelectorCCA = New instat.ucrSelectorByDataFrameAddRemove()
+        Me.ucrReceiverYvariables = New instat.ucrReceiverMultiple()
+        Me.ucrReceiverXvariables = New instat.ucrReceiverMultiple()
+        Me.lblYVariables = New System.Windows.Forms.Label()
+        Me.lblXVariables = New System.Windows.Forms.Label()
+        Me.cmdCCAOptions = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'ucrBase
+        'ucrBaseCCA
         '
-        Me.ucrBase.Location = New System.Drawing.Point(9, 212)
-        Me.ucrBase.Name = "ucrBase"
-        Me.ucrBase.Size = New System.Drawing.Size(410, 53)
-        Me.ucrBase.TabIndex = 0
+        Me.ucrBaseCCA.Location = New System.Drawing.Point(9, 289)
+        Me.ucrBaseCCA.Name = "ucrBaseCCA"
+        Me.ucrBaseCCA.Size = New System.Drawing.Size(410, 53)
+        Me.ucrBaseCCA.TabIndex = 0
+        '
+        'ucrSelectorCCA
+        '
+        Me.ucrSelectorCCA.Location = New System.Drawing.Point(9, 36)
+        Me.ucrSelectorCCA.Name = "ucrSelectorCCA"
+        Me.ucrSelectorCCA.Size = New System.Drawing.Size(242, 179)
+        Me.ucrSelectorCCA.TabIndex = 1
+        '
+        'ucrReceiverYvariables
+        '
+        Me.ucrReceiverYvariables.Location = New System.Drawing.Point(307, 21)
+        Me.ucrReceiverYvariables.Name = "ucrReceiverYvariables"
+        Me.ucrReceiverYvariables.Selector = Nothing
+        Me.ucrReceiverYvariables.Size = New System.Drawing.Size(121, 104)
+        Me.ucrReceiverYvariables.TabIndex = 2
+        '
+        'ucrReceiverXvariables
+        '
+        Me.ucrReceiverXvariables.Location = New System.Drawing.Point(307, 150)
+        Me.ucrReceiverXvariables.Name = "ucrReceiverXvariables"
+        Me.ucrReceiverXvariables.Selector = Nothing
+        Me.ucrReceiverXvariables.Size = New System.Drawing.Size(121, 104)
+        Me.ucrReceiverXvariables.TabIndex = 3
+        '
+        'lblYVariables
+        '
+        Me.lblYVariables.AutoSize = True
+        Me.lblYVariables.Location = New System.Drawing.Point(334, 5)
+        Me.lblYVariables.Name = "lblYVariables"
+        Me.lblYVariables.Size = New System.Drawing.Size(60, 13)
+        Me.lblYVariables.TabIndex = 4
+        Me.lblYVariables.Tag = "Y_Variables"
+        Me.lblYVariables.Text = "Y Variables"
+        '
+        'lblXVariables
+        '
+        Me.lblXVariables.AutoSize = True
+        Me.lblXVariables.Location = New System.Drawing.Point(334, 134)
+        Me.lblXVariables.Name = "lblXVariables"
+        Me.lblXVariables.Size = New System.Drawing.Size(60, 13)
+        Me.lblXVariables.TabIndex = 5
+        Me.lblXVariables.Tag = "X_Variables"
+        Me.lblXVariables.Text = "X Variables"
+        '
+        'cmdCCAOptions
+        '
+        Me.cmdCCAOptions.Location = New System.Drawing.Point(316, 260)
+        Me.cmdCCAOptions.Name = "cmdCCAOptions"
+        Me.cmdCCAOptions.Size = New System.Drawing.Size(94, 23)
+        Me.cmdCCAOptions.TabIndex = 7
+        Me.cmdCCAOptions.Tag = "CCA_Options..."
+        Me.cmdCCAOptions.Text = "CCA Options..."
+        Me.cmdCCAOptions.UseVisualStyleBackColor = True
         '
         'dlgCanonicalCorrelationAnalysis
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(431, 277)
-        Me.Controls.Add(Me.ucrBase)
+        Me.ClientSize = New System.Drawing.Size(433, 339)
+        Me.Controls.Add(Me.cmdCCAOptions)
+        Me.Controls.Add(Me.lblXVariables)
+        Me.Controls.Add(Me.lblYVariables)
+        Me.Controls.Add(Me.ucrReceiverXvariables)
+        Me.Controls.Add(Me.ucrReceiverYvariables)
+        Me.Controls.Add(Me.ucrSelectorCCA)
+        Me.Controls.Add(Me.ucrBaseCCA)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.Name = "dlgCanonicalCorrelationAnalysis"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Tag = "Canonical_Correlation_Analysis"
         Me.Text = "Canonical Correlation Analysis"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents ucrBase As ucrButtons
+    Friend WithEvents ucrBaseCCA As ucrButtons
+    Friend WithEvents ucrSelectorCCA As ucrSelectorByDataFrameAddRemove
+    Friend WithEvents ucrReceiverYvariables As ucrReceiverMultiple
+    Friend WithEvents ucrReceiverXvariables As ucrReceiverMultiple
+    Friend WithEvents lblYVariables As Label
+    Friend WithEvents lblXVariables As Label
+    Friend WithEvents cmdCCAOptions As Button
 End Class
--- instat/dlgCanonicalCorrelationAnalysis.vb
+++ instat/dlgCanonicalCorrelationAnalysis.vb
@@ -15,8 +15,88 @@
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 Imports instat.Translations
 Public Class dlgCanonicalCorrelationAnalysis
+    Public bFirstLoad As Boolean = True
+    Dim clsModel As New ROperator
+    'Public Variables As New RSyntax
+    Public YVariables, XVariables, dataframe
     Private Sub dlgCanonicalCorrelationAnalysis_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        autoTranslate(Me)
+        If bFirstLoad Then
+            InitialiseDialog()
+            SetDefaults()
+            bFirstLoad = False
+        Else
+            ReopenDialog()
+        End If
 
+        autoTranslate(Me)
+    End Sub
+
+    Private Sub InitialiseDialog()
+        ucrReceiverYvariables.Selector = ucrSelectorCCA
+        ucrReceiverXvariables.Selector = ucrSelectorCCA
+        'ucrBaseCCA.iHelpTopicID = 
+    End Sub
+
+    Private Sub ReopenDialog()
+
+    End Sub
+
+    Private Sub SetDefaults()
+        ucrSelectorCCA.Reset()
+        ucrReceiverYvariables.SetMeAsReceiver()
+        ucrSelectorCCA.Focus()
+        TestOKEnabled()
+    End Sub
+
+    Private Sub TestOKEnabled()
+        If (Not ucrReceiverYvariables.IsEmpty()) And (Not ucrReceiverXvariables.IsEmpty()) Then
+            ucrBaseCCA.OKEnabled(True)
+        Else
+            ucrBaseCCA.OKEnabled(False)
+        End If
+    End Sub
+
+    Public Sub ucrSelectorCCA_DataFrameChanged() Handles ucrSelectorCCA.DataFrameChanged
+        dataframe = ucrSelectorCCA.ucrAvailableDataFrames.clsCurrDataFrame.ToString()
+    End Sub
+
+    Private Sub ucrBaseCCA_ClickReset(sender As Object, e As EventArgs) Handles ucrBaseCCA.ClickReset
+        SetDefaults()
+    End Sub
+
+    Private Sub ucrbaseCCA_clickok(sender As Object, e As EventArgs) Handles ucrBaseCCA.ClickOk
+        'sdgcanonicalcorrelation.regoptions
+        Options()
+    End Sub
+
+
+
+    Public Sub ucrReceiverYvariables_SelectionChanged() Handles ucrReceiverYvariables.SelectionChanged
+        TestOKEnabled()
+        YVariables = XYariables(ucrReceiverYvariables.GetVariableNames())
+    End Sub
+
+    Public Sub ucrReceiverXvariables_SelectionChanged() Handles ucrReceiverXvariables.SelectionChanged
+        TestOKEnabled()
+        XVariables = XYariables(ucrReceiverXvariables.GetVariableNames())
+    End Sub
+
+    Private Sub Options()
+        Cancor()
+    End Sub
+
+    Public Function XYariables(ByVal my_variable) As String
+        Dim Variables As New RSyntax
+        Variables.SetFunction(frmMain.clsRLink.strInstatDataObject & "$get_columns_from_data")
+        Variables.AddParameter("col_names", my_variable)
+        Variables.AddParameter("data_name", Chr(34) & ucrSelectorCCA.ucrAvailableDataFrames.cboAvailableDataFrames.SelectedItem & Chr(34))
+        Return (Variables.GetScript())
+    End Function
+
+    Private Sub Cancor()
+        ucrBaseCCA.clsRsyntax.SetFunction("cc") 'or matcor
+        ucrBaseCCA.clsRsyntax.AddParameter("Y", YVariables)
+        ucrBaseCCA.clsRsyntax.AddParameter("X", XVariables)
+        frmMain.clsRLink.RunScript(ucrBaseCCA.clsRsyntax.GetScript(), 2)
     End Sub
 End Class
