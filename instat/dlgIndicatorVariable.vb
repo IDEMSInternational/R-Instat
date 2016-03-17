1069f4b371c72130ab7eab90f7c0db5dcde48657
max <maxwellfundi@live.com>
2016-03-17 10:31:46 +0300

    Redesigning the Indicator Variables

--- instat/dlgIndicatorVariable.vb
+++ instat/dlgIndicatorVariable.vb
@@ -14,15 +14,81 @@
 '
 ' You should have received a copy of the GNU General Public License k
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 Imports instat.Translations
 Public Class dlgIndicatorVariable
+    Private bFirstLoad As Boolean = True
+
     Private Sub dlgIndicatorVariable_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If bFirstLoad Then
+            InitialiseDialog()
+            SetDefaults()
+            bFirstLoad = False
+        Else
+            ReopenDialog()
+
+        End If
+        autoTranslate(Me)
+    End Sub
+    Private Sub TestOkEnabled()
+        If Not ucrReceiverFactor.IsEmpty Then
+            ucrBase.OKEnabled(True)
+        Else
+            ucrBase.OKEnabled(True)
+        End If
+
+    End Sub
+    Private Sub SetDefaults()
+        ucrSelectorDummyVariable.Focus()
+        ucrSelectorDummyVariable.Reset()
+        ucrReceiverFactor.Selector = ucrSelectorDummyVariable
+        ucrReceiverFactor.SetMeAsReceiver()
+        ucrVariateReceiver.Visible = False
+        lblVariate.Visible = False
+        ucrInputColName.SetPrefix("Dummy")
+
+        TestOkEnabled()
+    End Sub
+
+    Public Sub InitialiseDialog()
         ucrBase.clsRsyntax.SetFunction("")
         ucrBase.clsRsyntax.iCallType = 2
-        ucrReceiverFactor.Selector = ucrAddRemove
+        ucrReceiverFactor.Selector = ucrSelectorDummyVariable
+        ucrVariateReceiver.Selector = ucrSelectorDummyVariable
         ucrReceiverFactor.SetMeAsReceiver()
+
         ucrReceiverFactor.SetDataType("factor")
-        autoTranslate(Me)
+
+        chkXvariable.Checked = False
+        ucrVariateReceiver.Visible = False
+        lblVariate.Visible = False
+
+        ucrInputColName.SetPrefix("Dummy")
+        ucrInputColName.SetItemsTypeAsColumns()
+        ucrInputColName.SetDefaultTypeAsColumn()
+        ucrInputColName.SetDataFrameSelector(ucrSelectorDummyVariable.ucrAvailableDataFrames)
+    End Sub
+
+    Private Sub ReopenDialog()
+        ucrInputColName.SetPrefix("Dummy")
+    End Sub
+
+    Private Sub chkXvariable_CheckedChanged(sender As Object, e As EventArgs) Handles chkXvariable.CheckedChanged
+        If chkXvariable.Checked Then
+            ucrVariateReceiver.Visible = True
+            lblVariate.Visible = True
+            ucrVariateReceiver.SetMeAsReceiver()
+        Else
+            ucrVariateReceiver.Visible = False
+            lblVariate.Visible = False
+            ucrReceiverFactor.SetMeAsReceiver()
+        End If
+    End Sub
+
+    Private Sub ucrBase_ClickReset(sender As Object, e As EventArgs) Handles ucrBase.ClickReset
+        SetDefaults()
+    End Sub
+
+    Private Sub ucrInputColName_NameChanged() Handles ucrInputColName.NameChanged
+        ucrBase.clsRsyntax.SetAssignTo(strAssignToName:=ucrInputColName.GetText, strTempDataframe:=ucrSelectorDummyVariable.ucrAvailableDataFrames.cboAvailableDataFrames.Text, strTempColumn:=ucrInputColName.GetText)
     End Sub
 End Class
