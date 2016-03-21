ec7a7c3a874422acf3d7d90ec6b292002f9beb55
max <maxwellfundi@live.com>
2016-03-21 17:33:20 +0300

    changing dlggroup to dlggroupTables

--- instat/dlgGroupTables.vb
+++ instat/dlgGroupTables.vb
@@ -15,7 +15,7 @@
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 Imports instat.Translations
 
-Public Class dlgGroup
+Public Class dlgGroupTables
     Private bFirstLoad As Boolean = True
     Private Sub dlgGroup_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         If bFirstLoad Then
