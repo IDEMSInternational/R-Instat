f8d8b2f5808551377098bdba7f7180d19759f013
John Lunalo <johnlunalo95@gmail.com>
2016-03-22 10:31:40 +0300

    Editing dlgExamine

--- instat/dlgExamine.vb
+++ instat/dlgExamine.vb
@@ -15,7 +15,30 @@
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 Imports instat.Translations
 Public Class dlgExamine
+    Public bFirstLoad As Boolean = True
     Private Sub dlgExamine_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         autoTranslate(Me)
+
+        If bFirstLoad Then
+            InitialiseDialog()
+            SetDefaults()
+            bFirstLoad = False
+        End If
+        TestOKEnabled()
+
+    End Sub
+
+    Private Sub TestOKEnabled()
+    End Sub
+
+    Private Sub InitialiseDialog()
+
+    End Sub
+    Private Sub SetDefaults()
+
+    End Sub
+
+    Private Sub ucrBase_ClickReset(sender As Object, e As EventArgs) Handles ucrBase.ClickReset
+        SetDefaults()
     End Sub
 End Class
