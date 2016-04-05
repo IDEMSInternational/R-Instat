fcadbd8c25e6e69c061bfe0274deba456e01c38a
deaspo <polycarpokock@live.com>
2016-04-05 11:56:26 +0300

    Added code for writing the files

--- instat/dlgExportDataset.vb
+++ instat/dlgExportDataset.vb
@@ -3,7 +3,15 @@ Imports instat.Translations
 Public Class dlgExportDataset
     Dim bFirstLoad As Boolean = True
     Private Sub cmdBrowse_Click(sender As Object, e As EventArgs) Handles cmdBrowse.Click
-        'ToAdd
+        Dim dlgSave As New SaveFileDialog
+        dlgSave.Title = "Export file dialog"
+        dlgSave.Filter = "Comma separated file (*.csv)|*.csv|Excel files (*xlsx)|*.xlsx"
+        If dlgSave.ShowDialog = DialogResult.OK Then
+            If dlgSave.FileName <> "" Then
+                txtExportFile.Text = Path.GetFileName(dlgSave.FileName)
+                SaveFileType(dlgSave.FileName.Replace("\", "/"))
+            End If
+        End If
     End Sub
 
     Private Sub txtExportFile_Click(sender As Object, e As EventArgs) Handles txtExportFile.Click
@@ -29,7 +37,7 @@ Public Class dlgExportDataset
     End Sub
 
     Private Sub TestOkEnabled()
-        If txtExportFile.Text <> "" AndAlso Directory.Exists(txtExportFile.Text) Then
+        If txtExportFile.Text <> "" Then
             ucrBase.OKEnabled(True)
         Else
             ucrBase.OKEnabled(False)
@@ -38,5 +46,19 @@ Public Class dlgExportDataset
 
     Private Sub txtExportFile_TextChanged(sender As Object, e As EventArgs) Handles txtExportFile.TextChanged
         TestOkEnabled()
+    End Sub
+
+    Private Sub SaveFileType(strFilePath As String)
+        Select Case Path.GetExtension(strFilePath)
+            Case ".csv"
+                ucrBase.clsRsyntax.SetFunction("write.csv")
+
+                'ToDo
+            Case ".xlsx"
+                ucrBase.clsRsyntax.SetFunction("write.xlsx")
+                'ToDo
+        End Select
+        ucrBase.clsRsyntax.AddParameter("file", Chr(34) & strFilePath & Chr(34))
+        ucrBase.clsRsyntax.AddParameter("x", ucrAvailableSheets.cboAvailableDataFrames.SelectedItem)
     End Sub
 End Class
