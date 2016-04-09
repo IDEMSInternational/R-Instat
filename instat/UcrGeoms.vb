8b054db945bb3fa82cf533aaaeb84e5d6f633cb8
maxwell Fundi <fundimaxwell@gmail.com>
2016-04-09 08:10:36 +0300

    geom changes

--- instat/UcrGeoms.vb
+++ instat/UcrGeoms.vb
@@ -2,6 +2,7 @@
     Public lstAllGeoms As New List(Of Geoms)
     Public lstCurrentGeoms As New List(Of Geoms)
     Public lstGgParameters As New List(Of RParameter)
+    Public clsCurrGeom As New Geoms
 
     Private Sub UcrGeoms_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         CreateGeomList()
@@ -13,36 +14,27 @@
         For Each Geom In lstAllGeoms
             cboGeomList.Items.Add(Geom.strGeomName)
         Next
-
+        cboGeomList.SelectedIndex = 0
     End Sub
-
-    Public Sub AddParameter(strArgumentName As String, strArgumentValue As String)
-        Dim i As Integer
-        Dim clsParam As New RParameter
-        i = lstGgParameters.FindIndex(Function(x) x.strArgumentName.Equals(strArgumentName))
-        If i = -1 Then
-            clsParam.SetArgumentName(strArgumentName)
-            clsParam.SetArgumentValue(strArgumentValue)
-            lstGgParameters.Add(clsParam)
-        Else
-            lstGgParameters(i).strArgumentValue = strArgumentValue
-        End If
-    End Sub
-
     Public Sub CreateGeomList()
         Dim clsgeom_boxplot As New Geoms
 
         clsgeom_boxplot.strGeomName = "geom_boxplot"
-        clsgeom_boxplot.AddGgParameter("x", " ")
-        clsgeom_boxplot.AddGgParameter("fill", " ")
-        clsgeom_boxplot.AddGgParameter("colour", " ")
-        clsgeom_boxplot.AddGgParameter("shape", " ")
-        clsgeom_boxplot.AddGgParameter("lower", " ")
-        clsgeom_boxplot.AddGgParameter("middle", " ")
-        clsgeom_boxplot.AddGgParameter("upper", " ")
-        clsgeom_boxplot.AddGgParameter("width", " ")
+        clsgeom_boxplot.AddGgParameter("x")
+        clsgeom_boxplot.AddGgParameter("fill")
+        clsgeom_boxplot.AddGgParameter("colour")
+        clsgeom_boxplot.AddGgParameter("shape")
+        clsgeom_boxplot.AddGgParameter("lower")
+        clsgeom_boxplot.AddGgParameter("middle")
+        clsgeom_boxplot.AddGgParameter("upper")
+        clsgeom_boxplot.AddGgParameter("width")
         lstAllGeoms.Add(clsgeom_boxplot)
     End Sub
 
+    'Public Event cboGeomListIndexChanged(sender As Object, e As EventArgs)
 
+    'Private Sub cboGeomList_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboGeomList.SelectedIndexChanged
+    '    clsCurrGeom = lstCurrentGeoms(cboGeomList.SelectedIndex)
+    '    RaiseEvent cboGeomListIndexChanged(sender, e)
+    'End Sub
 End Class
