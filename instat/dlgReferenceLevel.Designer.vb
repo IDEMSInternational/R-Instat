facb02201269118374d06515527535c49f0ad971
John Lunalo <johnlunalo95@gmail.com>
2016-04-20 09:56:27 +0300

    Editing dlgReferenceLevel

--- instat/dlgReferenceLevel.Designer.vb
+++ instat/dlgReferenceLevel.Designer.vb
@@ -31,7 +31,7 @@ Partial Class dlgReferenceLevel
         '
         'ucrBase
         '
-        Me.ucrBase.Location = New System.Drawing.Point(4, 295)
+        Me.ucrBase.Location = New System.Drawing.Point(12, 250)
         Me.ucrBase.Name = "ucrBase"
         Me.ucrBase.Size = New System.Drawing.Size(410, 53)
         Me.ucrBase.TabIndex = 0
@@ -39,12 +39,12 @@ Partial Class dlgReferenceLevel
         'lblFactorSelected
         '
         Me.lblFactorSelected.AutoSize = True
-        Me.lblFactorSelected.Location = New System.Drawing.Point(261, 42)
+        Me.lblFactorSelected.Location = New System.Drawing.Point(261, 15)
         Me.lblFactorSelected.Name = "lblFactorSelected"
-        Me.lblFactorSelected.Size = New System.Drawing.Size(80, 13)
+        Me.lblFactorSelected.Size = New System.Drawing.Size(82, 13)
         Me.lblFactorSelected.TabIndex = 4
-        Me.lblFactorSelected.Tag = "Factor_selected"
-        Me.lblFactorSelected.Text = "Factor selected"
+        Me.lblFactorSelected.Tag = "Factor_Selected"
+        Me.lblFactorSelected.Text = "Factor Selected"
         '
         'ucrSelectorForReferenceLevels
         '
@@ -55,7 +55,7 @@ Partial Class dlgReferenceLevel
         '
         'ucrReceiverReferenceLevels
         '
-        Me.ucrReceiverReferenceLevels.Location = New System.Drawing.Point(264, 58)
+        Me.ucrReceiverReferenceLevels.Location = New System.Drawing.Point(264, 31)
         Me.ucrReceiverReferenceLevels.Name = "ucrReceiverReferenceLevels"
         Me.ucrReceiverReferenceLevels.Selector = Nothing
         Me.ucrReceiverReferenceLevels.Size = New System.Drawing.Size(106, 26)
@@ -66,7 +66,7 @@ Partial Class dlgReferenceLevel
         Me.ucrFactorReferenceLevels.AutoSize = True
         Me.ucrFactorReferenceLevels.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
         Me.ucrFactorReferenceLevels.clsReceiver = Nothing
-        Me.ucrFactorReferenceLevels.Location = New System.Drawing.Point(251, 90)
+        Me.ucrFactorReferenceLevels.Location = New System.Drawing.Point(264, 63)
         Me.ucrFactorReferenceLevels.Name = "ucrFactorReferenceLevels"
         Me.ucrFactorReferenceLevels.shtCurrSheet = Nothing
         Me.ucrFactorReferenceLevels.Size = New System.Drawing.Size(267, 181)
@@ -76,7 +76,7 @@ Partial Class dlgReferenceLevel
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(543, 350)
+        Me.ClientSize = New System.Drawing.Size(543, 309)
         Me.Controls.Add(Me.ucrReceiverReferenceLevels)
         Me.Controls.Add(Me.ucrSelectorForReferenceLevels)
         Me.Controls.Add(Me.ucrFactorReferenceLevels)
