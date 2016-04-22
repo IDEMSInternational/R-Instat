206bf2fc606d371c2141a5e6bae9b35beaea3826
Stephen Kogo <stevekogo@gmail.com>
2016-04-22 16:23:47 +0300

    Implementing convert to variate in two variable modelling

--- instat/clsRLink.vb
+++ instat/clsRLink.vb
@@ -341,9 +341,9 @@ Public Class RLink
         Return lstModelNames
     End Function
 
-    Public Function GetDataType(strDataFrameName As String, strColumnName As String) As String
+    Public Function GetDataType(strDataFrameName As String, strColumnName As String, asnumeric As String) As String
         Dim strDataType As CharacterVector
-        strDataType = clsEngine.Evaluate(frmMain.clsRLink.strInstatDataObject & "$get_data_type(data_name = " & Chr(34) & strDataFrameName & Chr(34) & ",col_name=" & Chr(34) & strColumnName & Chr(34) & ")").AsCharacter
+        strDataType = clsEngine.Evaluate(frmMain.clsRLink.strInstatDataObject & "$get_data_type(data_name = " & Chr(34) & strDataFrameName & Chr(34) & ",col_name=" & Chr(34) & strColumnName & Chr(34) & ",asnumeric=" & asnumeric.ToUpper & ")").AsCharacter
         Return strDataType(0)
     End Function
 
