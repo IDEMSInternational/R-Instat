ee70dda2b5d548a864a32e74542d0ee5baea49c7
dannyparsons <danny@aims.ac.za>
2016-04-27 16:02:41 +0100

    initial changes on Reg Seq to get preview working

--- instat/dlgRegularSequence.vb
+++ instat/dlgRegularSequence.vb
@@ -14,15 +14,16 @@
 ' You should have received a copy of the GNU General Public License k
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 Imports instat.Translations
+Imports RDotNet
+
 Public Class dlgRegularSequence
     Dim bIsExtended As Boolean = False
     Public bFirstLoad As Boolean = True
+    Dim clsSeqFunction As New RFunction
+    Dim clsRepFunction As New RFunction
+
     Private Sub dlgRegularSequence_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
         autoTranslate(Me)
-
-
-
         If bFirstLoad Then
             InitialiseDialog()
             SetDefaults()
@@ -34,13 +35,21 @@ Public Class dlgRegularSequence
     End Sub
 
     Private Sub InitialiseDialog()
-        ucrBase.clsRsyntax.SetFunction("seq")
+        clsSeqFunction.SetRCommand("seq")
+        clsRepFunction.SetRCommand("rep")
+        clsRepFunction.AddParameter("x", clsRFunctionParameter:=clsSeqFunction)
+        nudFrom.Minimum = Integer.MinValue
+        nudFrom.Maximum = Integer.MaxValue
+        nudTo.Minimum = Integer.MinValue
+        nudTo.Maximum = Integer.MaxValue
+        nudInStepsOf.Maximum = Integer.MaxValue
+        nudRepeatValues.Maximum = Integer.MaxValue
         ucrBase.iHelpTopicID = 30
-        UcrDataFrameLengthForRegularSequence.SetDataFrameSelector(ucrSelectDataFrameRegularSequence)
-        UcrInputCboRegularSequence.SetPrefix("Regular")
-        UcrInputCboRegularSequence.SetItemsTypeAsColumns()
-        UcrInputCboRegularSequence.SetDefaultTypeAsColumn()
-        UcrInputCboRegularSequence.SetDataFrameSelector(ucrSelectDataFrameRegularSequence)
+        ucrDataFrameLengthForRegularSequence.SetDataFrameSelector(ucrSelectDataFrameRegularSequence)
+        ucrNewColumnName.SetPrefix("Regular")
+        ucrNewColumnName.SetItemsTypeAsColumns()
+        ucrNewColumnName.SetDefaultTypeAsColumn()
+        ucrNewColumnName.SetDataFrameSelector(ucrSelectDataFrameRegularSequence)
     End Sub
 
     Private Sub SetDefaults()
@@ -48,6 +57,10 @@ Public Class dlgRegularSequence
         rdoDates.Checked = False
         ucrSelectDataFrameRegularSequence.Reset()
         ucrSelectDataFrameRegularSequence.Focus()
+        nudFrom.Value = 1
+        nudTo.Value = ucrSelectDataFrameRegularSequence.iDataFrameLength
+        nudInStepsOf.Value = 1
+        CheckSequenceLength()
     End Sub
 
     Private Sub ReopenDialog()
@@ -57,29 +70,21 @@ Public Class dlgRegularSequence
     Private Sub ucrBase_ClickReset(sender As Object, e As EventArgs) Handles ucrBase.ClickReset
         SetDefaults()
         TestOKEnabled()
-
-
     End Sub
 
     Private Sub TestOKEnabled()
-        If rdoNumeric.Checked = True Then
-            If nudFrom.Text <> "" And nudTo.Text <> "" Then
+        If rdoNumeric.Checked Then
+            If nudFrom.Text <> "" AndAlso nudTo.Text <> "" AndAlso nudInStepsOf.Text <> "" AndAlso nudRepeatValues.Text <> "" Then
                 ucrBase.OKEnabled(True)
             Else
                 ucrBase.OKEnabled(False)
             End If
-        ElseIf rdoDates.Checked = True Then
-
+        ElseIf rdoDates.Checked Then
             ucrBase.OKEnabled(True)
         Else
             ucrBase.OKEnabled(False)
         End If
     End Sub
-
-    'Private Sub cmdRefreshPreview_Click(sender As Object, e As EventArgs) Handles cmdRefreshPreview.Click
-    '    frmMain.clsRLink.RunScript(ucrBase.clsRsyntax.clsBaseFunction.ToScript(), 2)
-    '    txtGetPreview.Refresh()
-    'End Sub
 
     Private Sub grpSequenceType_CheckedChanged(sender As Object, e As EventArgs) Handles rdoDates.CheckedChanged, rdoNumeric.CheckedChanged
         If rdoNumeric.Checked Then
@@ -87,46 +92,110 @@ Public Class dlgRegularSequence
             nudTo.Visible = True
             dtpSelectorA.Visible = False
             dtpSelectorB.Visible = False
-            nudRepeatValues.Enabled = False
         Else
             dtpSelectorA.Visible = True
             dtpSelectorB.Visible = True
             nudFrom.Visible = False
             nudTo.Visible = False
         End If
+        CheckSequenceLength()
         TestOKEnabled()
     End Sub
 
-    Private Sub nudInstepsOf_ValueChanged(sender As Object, e As EventArgs) Handles nudInstepsOf.ValueChanged
-        ucrBase.clsRsyntax.AddParameter("by", nudInstepsOf.Value)
-    End Sub
-
-    Private Sub nudRepeatValues_ValueChanged(sender As Object, e As EventArgs) Handles nudRepeatValues.ValueChanged
-        If rdoDates.Checked Then
-            ucrBase.clsRsyntax.AddParameter("length.out", "NULL")
+    Private Sub nudInstepsOf_ValueChanged(sender As Object, e As EventArgs) Handles nudInStepsOf.ValueChanged
+        If nudInStepsOf.Text <> "" Then
+            If (nudInStepsOf.Value = 1 AndAlso frmMain.clsInstatOptions.bIncludeRDefaultParameters) OrElse nudInStepsOf.Value <> 1 Then
+                clsSeqFunction.AddParameter("by", nudInStepsOf.Value)
+            Else
+                clsSeqFunction.RemoveParameterByName("by")
+            End If
         Else
-            ucrBase.clsRsyntax.AddParameter("length.out", nudRepeatValues.Value)
+            clsSeqFunction.RemoveParameterByName("by")
         End If
-
+        CheckSequenceLength()
     End Sub
 
-    Private Sub nudFrom_ValueChanged(sender As Object, e As EventArgs) Handles nudFrom.ValueChanged
-        ucrBase.clsRsyntax.AddParameter("from", nudFrom.Value)
+    Private Sub nudRepeatValues_ValueChanged(sender As Object, e As EventArgs) Handles nudRepeatValues.TextChanged
+        SetRepeatProperties()
     End Sub
 
-    Private Sub nudTo_ValueChanged(sender As Object, e As EventArgs) Handles nudTo.ValueChanged
-        ucrBase.clsRsyntax.AddParameter("to", nudTo.Value)
+    Private Sub SetRepeatProperties()
+        If nudRepeatValues.Text <> "" AndAlso nudRepeatValues.Value > 1 Then
+            ucrBase.clsRsyntax.SetBaseRFunction(clsRepFunction)
+            clsRepFunction.AddParameter("each", nudRepeatValues.Value)
+        Else
+            ucrBase.clsRsyntax.SetBaseRFunction(clsSeqFunction)
+        End If
+        CheckSequenceLength()
+    End Sub
+
+    Private Sub nudFrom_ValueChanged(sender As Object, e As EventArgs) Handles nudFrom.TextChanged
+        If nudFrom.Text <> "" Then
+            clsSeqFunction.AddParameter("from", nudFrom.Value)
+        Else
+            clsSeqFunction.RemoveParameterByName("from")
+        End If
+        CheckSequenceLength()
+    End Sub
+
+    Private Sub nudTo_ValueChanged(sender As Object, e As EventArgs) Handles nudTo.TextChanged
+        If nudTo.Text <> "" Then
+            clsSeqFunction.AddParameter("to", nudTo.Value)
+        Else
+            clsSeqFunction.RemoveParameterByName("to")
+        End If
+        CheckSequenceLength()
     End Sub
 
     Private Sub dtpSelectorA_ValueChanged(sender As Object, e As EventArgs) Handles dtpSelectorA.ValueChanged
         ucrBase.clsRsyntax.AddParameter("from", "as.Date('" & Format(dtpSelectorA.Value, "yyyy/MM/dd") & "')")
+        CheckSequenceLength()
     End Sub
 
     Private Sub dtpSelectorB_ValueChanged(sender As Object, e As EventArgs) Handles dtpSelectorB.ValueChanged
         ucrBase.clsRsyntax.AddParameter("to", "as.Date('" & Format(dtpSelectorB.Value, "yyyy/MM/dd") & "')")
+        CheckSequenceLength()
     End Sub
 
-    Private Sub UcrInputCboRegularSequence_NameChanged() Handles UcrInputCboRegularSequence.NameChanged
-        ucrBase.clsRsyntax.SetAssignTo(strAssignToName:=UcrInputCboRegularSequence.GetText, strTempDataframe:=ucrSelectDataFrameRegularSequence.cboAvailableDataFrames.Text, strTempColumn:=UcrInputCboRegularSequence.GetText)
+    Private Sub ucrInputCboRegularSequence_NameChanged() Handles ucrNewColumnName.NameChanged
+        ucrBase.clsRsyntax.SetAssignTo(strAssignToName:=ucrNewColumnName.GetText, strTempDataframe:=ucrSelectDataFrameRegularSequence.cboAvailableDataFrames.Text, strTempColumn:=ucrNewColumnName.GetText)
+    End Sub
+
+    Private Sub ucrSelectDataFrameRegularSequence_DataFrameChanged(sender As Object, e As EventArgs, strPrevDataFrame As String) Handles ucrSelectDataFrameRegularSequence.DataFrameChanged
+        nudTo.Value = ucrSelectDataFrameRegularSequence.iDataFrameLength
+        SetRepeatProperties()
+        nudFrom.Value = 1
+        nudTo.Value = ucrSelectDataFrameRegularSequence.iDataFrameLength
+        CheckSequenceLength()
+    End Sub
+
+    Private Sub CheckSequenceLength()
+        Dim iLength As Integer
+        Dim vecSequence As NumericVector
+        Dim strRCommand As String
+        lblMessage.Text = ""
+        Try
+            strRCommand = ucrBase.clsRsyntax.clsBaseFunction.ToScript()
+            vecSequence = frmMain.clsRLink.clsEngine.Evaluate(strRCommand).AsNumeric
+            iLength = vecSequence.Length
+            If iLength <> ucrSelectDataFrameRegularSequence.iDataFrameLength Then
+                ucrBase.clsRsyntax.SetBaseRFunction(clsRepFunction)
+                clsRepFunction.AddParameter("length.out", ucrSelectDataFrameRegularSequence.iDataFrameLength)
+                strRCommand = ucrBase.clsRsyntax.clsBaseFunction.ToScript()
+                vecSequence = frmMain.clsRLink.clsEngine.Evaluate(strRCommand).AsNumeric
+                If iLength < ucrSelectDataFrameRegularSequence.iDataFrameLength Then
+                    lblMessage.Text = "Sequence has been extended by repeating to match the length of the dataframe."
+                ElseIf iLength > ucrSelectDataFrameRegularSequence.iDataFrameLength Then
+                    lblMessage.Text = "Sequence has been truncated to match the length of the dataframe."
+                End If
+            Else
+                clsRepFunction.RemoveParameterByName("length.out")
+                lblMessage.Text = "Sequence matches the length of the dataframe."
+            End If
+            txtGetPreview.Text = vecSequence.ToString()
+        Catch ex As Exception
+            txtGetPreview.Text = ""
+            lblMessage.Text = "No preview avaiable"
+        End Try
     End Sub
 End Class
