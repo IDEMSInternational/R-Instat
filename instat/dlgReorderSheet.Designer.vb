152f305175650afa91dfcdcafa9b4116bdb5301a
John Lunalo <johnlunalo95@gmail.com>
2016-04-29 17:13:42 +0300

    Implementing dlgReoderSheets

--- instat/dlgReorderSheet.Designer.vb
+++ instat/dlgReorderSheet.Designer.vb
@@ -20,20 +20,66 @@ Partial Class dlgReorderSheet
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.ucrBase = New instat.ucrButtons()
+        Me.ucrDataFrameReoderSheets = New instat.ucrDataFrame()
+        Me.ucrSheetsToReoder = New instat.ucrReorder()
+        Me.lblSheetsToReoder = New System.Windows.Forms.Label()
         Me.SuspendLayout()
+        '
+        'ucrBase
+        '
+        Me.ucrBase.Location = New System.Drawing.Point(12, 175)
+        Me.ucrBase.Name = "ucrBase"
+        Me.ucrBase.Size = New System.Drawing.Size(411, 56)
+        Me.ucrBase.TabIndex = 0
+        '
+        'ucrDataFrameReoderSheets
+        '
+        Me.ucrDataFrameReoderSheets.Location = New System.Drawing.Point(13, 13)
+        Me.ucrDataFrameReoderSheets.Name = "ucrDataFrameReoderSheets"
+        Me.ucrDataFrameReoderSheets.Size = New System.Drawing.Size(127, 41)
+        Me.ucrDataFrameReoderSheets.TabIndex = 1
+        '
+        'ucrSheetsToReoder
+        '
+        Me.ucrSheetsToReoder.Location = New System.Drawing.Point(215, 30)
+        Me.ucrSheetsToReoder.Name = "ucrSheetsToReoder"
+        Me.ucrSheetsToReoder.Size = New System.Drawing.Size(154, 139)
+        Me.ucrSheetsToReoder.TabIndex = 2
+        Me.ucrSheetsToReoder.ucrDataFrameList = Nothing
+        Me.ucrSheetsToReoder.ucrReceiver = Nothing
+        '
+        'lblSheetsToReoder
+        '
+        Me.lblSheetsToReoder.AutoSize = True
+        Me.lblSheetsToReoder.Location = New System.Drawing.Point(212, 14)
+        Me.lblSheetsToReoder.Name = "lblSheetsToReoder"
+        Me.lblSheetsToReoder.Size = New System.Drawing.Size(90, 13)
+        Me.lblSheetsToReoder.TabIndex = 3
+        Me.lblSheetsToReoder.Text = "Sheets to Reoder"
         '
         'dlgReorderSheet
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(419, 232)
+        Me.Controls.Add(Me.lblSheetsToReoder)
+        Me.Controls.Add(Me.ucrSheetsToReoder)
+        Me.Controls.Add(Me.ucrDataFrameReoderSheets)
+        Me.Controls.Add(Me.ucrBase)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.Name = "dlgReorderSheet"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
         Me.Text = "Reorder Sheets"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents ucrBase As ucrButtons
+    Friend WithEvents ucrDataFrameReoderSheets As ucrDataFrame
+    Friend WithEvents ucrSheetsToReoder As ucrReorder
+    Friend WithEvents lblSheetsToReoder As Label
 End Class
--- instat/dlgReorderSheet.vb
+++ instat/dlgReorderSheet.vb
@@ -1,3 +1,52 @@
-﻿Public Class dlgReorderSheet
+﻿' Instat-R
+' Copyright (C) 2015
 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+'
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+
+' You should have received a copy of the GNU General Public License k
+' along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Imports instat.Translations
+Public Class dlgReorderSheet
+    Public bFirstLoad As Boolean = True
+    Private Sub dlgReorderSheet_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        autoTranslate(Me)
+        If bFirstLoad Then
+            initialiseDialog()
+        End If
+        TestOkEnabled()
+    End Sub
+
+    Private Sub initialiseDialog()
+        ucrBase.clsRsyntax.SetFunction(frmMain.clsRLink.strInstatDataObject & "$order_dataframes")
+        ucrSheetsToReoder.strDataType = "dataframe"
+        ucrSheetsToReoder.setDataframes(ucrDataFrameReoderSheets)
+    End Sub
+
+    Private Sub TestOkEnabled()
+        If Not ucrSheetsToReoder.isEmpty Then
+            ucrBase.OKEnabled(True)
+        Else
+            ucrBase.OKEnabled(False)
+        End If
+
+    End Sub
+
+    Private Sub ucrBase_ClickReset(sender As Object, e As EventArgs) Handles ucrBase.ClickReset
+        ucrDataFrameReoderSheets.Reset()
+        TestOkEnabled()
+    End Sub
+
+    Private Sub ucrDataFrameReoderSheets_DataFrameChanged(sender As Object, e As EventArgs, strPrevDataFrame As String) Handles ucrDataFrameReoderSheets.DataFrameChanged
+        ucrBase.clsRsyntax.AddParameter("data_name", Chr(34) & ucrDataFrameReoderSheets.cboAvailableDataFrames.Text & Chr(34))
+        TestOkEnabled()
+    End Sub
 End Class
