5f0ffc6f99e3123b6bc65afe51721d08ebce5cf6
maxwell fundi <maxwellfundi@live.com>
2016-05-11 17:45:19 +0300

    Addingopen graph dialog for use graph

--- instat/dlgUseGraph.Designer.vb
+++ instat/dlgUseGraph.Designer.vb
@@ -22,12 +22,27 @@ Partial Class dlgUseGraph
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.dlgOpenGraph = New System.Windows.Forms.OpenFileDialog()
+        Me.cmdOpenGraph = New System.Windows.Forms.Button()
         Me.ucrBase = New instat.ucrButtons()
         Me.SuspendLayout()
         '
+        'dlgOpenGraph
+        '
+        Me.dlgOpenGraph.FileName = "OpenFileDialog1"
+        '
+        'cmdOpenGraph
+        '
+        Me.cmdOpenGraph.Location = New System.Drawing.Point(101, 3)
+        Me.cmdOpenGraph.Name = "cmdOpenGraph"
+        Me.cmdOpenGraph.Size = New System.Drawing.Size(221, 32)
+        Me.cmdOpenGraph.TabIndex = 1
+        Me.cmdOpenGraph.Text = "Open Graph"
+        Me.cmdOpenGraph.UseVisualStyleBackColor = True
+        '
         'ucrBase
         '
-        Me.ucrBase.Location = New System.Drawing.Point(1, 285)
+        Me.ucrBase.Location = New System.Drawing.Point(6, 59)
         Me.ucrBase.Name = "ucrBase"
         Me.ucrBase.Size = New System.Drawing.Size(410, 53)
         Me.ucrBase.TabIndex = 0
@@ -36,7 +51,8 @@ Partial Class dlgUseGraph
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(410, 350)
+        Me.ClientSize = New System.Drawing.Size(414, 127)
+        Me.Controls.Add(Me.cmdOpenGraph)
         Me.Controls.Add(Me.ucrBase)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.Name = "dlgUseGraph"
@@ -48,4 +64,6 @@ Partial Class dlgUseGraph
     End Sub
 
     Friend WithEvents ucrBase As ucrButtons
+    Friend WithEvents dlgOpenGraph As OpenFileDialog
+    Friend WithEvents cmdOpenGraph As Button
 End Class
