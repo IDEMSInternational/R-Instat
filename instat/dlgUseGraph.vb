e51116541ab8cbb08c3c5d316bdbf6df93b21242
maxwell fundi <maxwellfundi@live.com>
2016-05-11 17:45:19 +0300

    Addingopen graph dialog for use graph

--- instat/dlgUseGraph.vb
+++ instat/dlgUseGraph.vb
@@ -18,7 +18,6 @@ Public Class dlgUseGraph
     Private bFirstLoad As Boolean = True
     Private Sub dlgUseGraph_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         autoTranslate(Me)
-
         If bFirstLoad Then
             SetDefaults()
             bFirstLoad = False
@@ -31,5 +30,22 @@ Public Class dlgUseGraph
 
     Private Sub ucrBase_ClickReset(sender As Object, e As EventArgs) Handles ucrBase.ClickReset
         SetDefaults()
+    End Sub
+
+    Private Sub OpenGraphBrowser()
+        Dim GraphBrowser As OpenFileDialog = New OpenFileDialog()
+        Dim strFileName As String
+
+        GraphBrowser.Title = "Open Saved Graph Dialog"
+        GraphBrowser.InitialDirectory = "C:\"
+        GraphBrowser.Filter = "All files (*.*)|*.*"
+
+        If GraphBrowser.ShowDialog() = DialogResult.OK Then
+            strFileName = GraphBrowser.FileName
+        End If
+    End Sub
+
+    Private Sub cmdOpenGraph_Click(sender As Object, e As EventArgs) Handles cmdOpenGraph.Click
+        OpenGraphBrowser()
     End Sub
 End Class
