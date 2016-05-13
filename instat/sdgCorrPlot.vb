45d05938ca2a0e97f577a7c77d9fd81cc4ac362e
Stephen Kogo <stevekogo@gmail.com>
2016-05-13 10:05:34 +0300

    Fixing correlation dialogue and subdialogue

--- instat/sdgCorrPlot.vb
+++ instat/sdgCorrPlot.vb
@@ -15,7 +15,9 @@
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 Imports instat.Translations
 Public Class sdgCorrPlot
+    Public clsRModelFunction As RFunction
     Public clsRGGPairsFunction As New RFunction
+    Public clsRGGscatmatrix As New RSyntax
     Public bFirstLoad As Boolean = True
 
     Private Sub sdgCorrPlot_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -27,18 +29,19 @@ Public Class sdgCorrPlot
         End If
     End Sub
 
+    Public Sub SetRModelFunction(clsRModelFunc As RFunction)
+        clsRModelFunction = clsRModelFunc
+    End Sub
+
+    Private Sub CorrelationMatrix()
+        frmMain.clsRLink.RunScript(dlgCorrelation.ucrBase.clsRsyntax.clsBaseFunction.ToScript(), 2)
+    End Sub
+
     Private Sub GGPairs()
         Dim clsRGraphics As New RSyntax
         clsRGraphics.SetFunction("ggpairs")
         clsRGraphics.AddParameter("data", clsRFunctionParameter:=dlgCorrelation.ucrSelectorDataFrameVarAddRemove.ucrAvailableDataFrames.clsCurrDataFrame)
         clsRGraphics.AddParameter("columns", dlgCorrelation.ucrReceiverMultipleColumns.GetVariableNames())
-
-        'clsRGraphics.SetOperatorParameter(True, clsRFunc:=clsRGGPairsFunction)
-        'clsRGraphics.SetOperation("")
-        'clsRGGPairsFunction.SetRCommand("ggpairs")
-        'clsRGGPairsFunction.AddParameter("data", clsRFunctionParameter:=dlgCorrelation.ucrSelectorDataFrameVarAddRemove.ucrAvailableDataFrames.clsCurrDataFrame)
-        'clsRGGPairsFunction.AddParameter("columns", dlgCorrelation.ucrReceiverMultipleColumns.GetVariableNames())
-        'clsRGraphics.SetOperatorParameter(True, clsRFunc:=clsRGGPairsFunction)
 
         'Calltype is not the right one but it works
         frmMain.clsRLink.RunScript(clsRGraphics.GetScript(), 2)
@@ -52,23 +55,83 @@ Public Class sdgCorrPlot
         clsRGGcorrGraphics.AddParameter("data", "NULL")
         clsRGGcorrGraphics.AddParameter("cor_matrix", dlgCorrelation.ucrBase.clsRsyntax.GetScript())
 
-
         'Calltype is not the right one but it works
         frmMain.clsRLink.RunScript(clsRGGcorrGraphics.GetScript(), 2)
     End Sub
 
+    Public Sub GGscatmatrix()
+        ' Dim clsRGGscatmatrix As New RSyntax
+        clsRGGscatmatrix.SetFunction("ggscatmat")
+        clsRGGscatmatrix.AddParameter("data", clsRFunctionParameter:=dlgCorrelation.ucrSelectorDataFrameVarAddRemove.ucrAvailableDataFrames.clsCurrDataFrame)
+        clsRGGscatmatrix.AddParameter("columns", dlgCorrelation.ucrReceiverMultipleColumns.GetVariableNames(bWithQuotes:=True))
+        'clsRGGscatmatrix.AddParameter("color", "NULL") 'This is the default option. We need to use factor column from data
+        'clsRGGscatmatrix.AddParameter("alpha", 1) 'This is the default option. We need a numeric up-down in the subdialogue
+
+        frmMain.clsRLink.RunScript(clsRGGscatmatrix.GetScript(), 2)
+    End Sub
 
     Public Sub SetDefaults()
-        chkGGPairs.Checked = False
-        chkGGcorr.Checked = False
+        chkCorrelationMatrix.Checked = True
+        rdoPairwisePlot.Checked = False
+        rdoCorrelationPlot.Checked = False
+        rdoScatterplotMatrix.Checked = False
+        tbSaveGraphs.Visible = False
+        chkColor.Checked = False
+        nudAlpha.Value = 1
+        lblAlpha.Enabled = False
+        nudAlpha.Enabled = False
+        chkColor.Enabled = False
+        'set save graph to unchecked by default
     End Sub
 
     Public Sub RegOptions()
-        If (chkGGPairs.Checked = True) Then
+        If (chkCorrelationMatrix.Checked = True) Then
+            CorrelationMatrix()
+        End If
+        If (rdoPairwisePlot.Checked = True) Then
             GGPairs()
         End If
-        If (chkGGcorr.Checked = True) Then
+        If (rdoScatterplotMatrix.Checked = True) Then
+            GGscatmatrix()
+        End If
+        If (rdoCorrelationPlot.Checked = True) Then
             GGcorr()
         End If
     End Sub
-End Class
+
+    Private Sub ucrSaveGraph_CheckedChanged(bChecked As Boolean) Handles ucrSaveGraph.CheckedChanged
+        If bChecked = True Then
+            tbSaveGraphs.Visible = True
+        Else
+            tbSaveGraphs.Visible = False
+        End If
+    End Sub
+
+    Private Sub nudAlpha_ValueChanged(sender As Object, e As EventArgs) Handles nudAlpha.ValueChanged
+        clsRGGscatmatrix.AddParameter("alpha", nudAlpha.Value.ToString) 'This is the default option. We need a numeric up-down in the subdialogue
+    End Sub
+
+    Private Sub chkColor_CheckedChanged(sender As Object, e As EventArgs) Handles chkColor.CheckedChanged
+        If chkColor.Checked = True Then
+            ' clsRGGscatmatrix.AddParameter("color", ....) 'We need to use factor column from data
+        ElseIf chkColor.Checked = False Then
+            clsRGGscatmatrix.AddParameter("color", "NULL")
+        Else
+            clsRGGscatmatrix.RemoveParameter("color")
+        End If
+    End Sub
+
+    Private Sub rdoScatterplotMatrix_CheckedChanged(sender As Object, e As EventArgs) Handles rdoScatterplotMatrix.CheckedChanged
+        If rdoScatterplotMatrix.Checked Then
+            lblAlpha.Enabled = True
+            nudAlpha.Enabled = True
+            chkColor.Enabled = True
+        Else
+            lblAlpha.Enabled = False
+            nudAlpha.Enabled = False
+            chkColor.Enabled = False
+        End If
+
+    End Sub
+
+ \End Class
