c755fa66e06c1fedb5934f41219e54ebce76f827
maxwell fundi <maxwellfundi@live.com>
2016-05-16 10:22:31 +0300

    Adding code to pass parameters to geomBoxplot

--- instat/sdgLayerOptions.vb
+++ instat/sdgLayerOptions.vb
@@ -15,7 +15,6 @@
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 Imports instat.Translations
 Public Class sdgLayerOptions
-
     Private bFirstLoad As Boolean = True
     Private Sub sdgLayers_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         If bFirstLoad Then
@@ -32,9 +31,64 @@ Public Class sdgLayerOptions
     End Sub
 
     Private Sub SetDefaults()
-
+        lblNotchWidth.Visible = False
+        nudNotchWidth.Visible = False
+        nudWhiskerLength.Visible = False
+        lblShape.Visible = False
+        nudShape.Visible = False
+        lblSize.Visible = False
+        nudSize.Visible = -False
+        lblStroke.Visible = False
+        nudStroke.Visible = False
     End Sub
     Private Sub ReopenDialog()
 
+    End Sub
+
+    Private Sub chkVariableWidth_CheckedChanged(sender As Object, e As EventArgs) Handles chkVariableWidth.CheckedChanged
+        If chkVariableWidth.Checked Then
+            ucrGeomWithAes.clsGeomFunction.AddParameter("varwidth", "TRUE")
+        Else
+            ucrGeomWithAes.clsGeomFunction.RemoveParameterByName("varwidth")
+        End If
+    End Sub
+
+    Private Sub chkNotchedBoxplot_CheckedChanged(sender As Object, e As EventArgs) Handles chkNotchedBoxplot.CheckedChanged
+        If chkNotchedBoxplot.Checked Then
+            ucrGeomWithAes.clsGeomFunction.AddParameter("notch", "TRUE")
+        Else
+            ucrGeomWithAes.clsGeomFunction.RemoveParameterByName("notch")
+        End If
+        lblNotchWidth.Visible = True
+        nudNotchWidth.Visible = True
+    End Sub
+
+    Private Sub nudWhiskerLength_ValueChanged(sender As Object, e As EventArgs) Handles nudWhiskerLength.ValueChanged
+        ucrGeomWithAes.clsGeomFunction.AddParameter("coef", nudWhiskerLength.Value)
+    End Sub
+
+    Private Sub nudShape_ValueChanged(sender As Object, e As EventArgs) Handles nudShape.ValueChanged
+        ucrGeomWithAes.clsGeomFunction.AddParameter("outlier.shape", nudShape.Value)
+    End Sub
+
+    Private Sub nudStroke_ValueChanged(sender As Object, e As EventArgs) Handles nudStroke.ValueChanged
+        ucrGeomWithAes.clsGeomFunction.AddParameter("outlier.stroke", nudStroke.Value)
+    End Sub
+
+    Private Sub nudSize_ValueChanged(sender As Object, e As EventArgs) Handles nudSize.ValueChanged
+        ucrGeomWithAes.clsGeomFunction.AddParameter("outlier.size", nudSize.Value)
+    End Sub
+
+    Private Sub chkWhiskerLength_CheckedChanged(sender As Object, e As EventArgs) Handles chkWhiskerLength.CheckedChanged
+        nudWhiskerLength.Visible = True
+    End Sub
+
+    Private Sub chkChangeOutliers_CheckedChanged(sender As Object, e As EventArgs) Handles chkChangeOutliers.CheckedChanged
+        lblShape.Visible = True
+        nudShape.Visible = True
+        lblSize.Visible = True
+        nudSize.Visible = -True
+        lblStroke.Visible = True
+        nudStroke.Visible = True
     End Sub
 End Class
