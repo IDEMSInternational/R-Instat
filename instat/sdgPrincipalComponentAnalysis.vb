ad444ade9f07cbfb9b83a68a3cdf5ffe18de7cf9
Stephen Kogo <stevekogo@gmail.com>
2016-05-23 22:15:49 +0300

    Plotting in PCA

--- instat/sdgPrincipalComponentAnalysis.vb
+++ instat/sdgPrincipalComponentAnalysis.vb
@@ -16,7 +16,8 @@
 Imports instat.Translations
 Public Class sdgPrincipalComponentAnalysis
     Public bFirstLoad As Boolean = True
-    Public clsREigenValues, clsREigenVectors, clsRScores As New RFunction
+    Public clsREigenValues, clsREigenVectors, clsRScores, clsPCAModel As New RFunction
+    Public clsRScreePlot As New RSyntax
 
     Private Sub sdgPrincipalComponentAnalysis_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         autoTranslate(Me)
@@ -54,11 +55,74 @@ Public Class sdgPrincipalComponentAnalysis
         'TODO
     End Sub
 
+    Private Sub ScreePlot()
+        clsRScreePlot.SetFunction("fviz_screeplot")
+        clsRScreePlot.AddParameter("X", clsRFunctionParameter:=dlgPrincipalComponentAnalysis.ucrBasePCA.clsRsyntax.clsBaseFunction)
+
+        frmMain.clsRLink.RunScript(clsRScreePlot.GetScript(), 0)
+    End Sub
+
+    Private Sub chkBar_CheckedChanged(sender As Object, e As EventArgs) Handles chkBar.CheckedChanged
+        If chkBar.Checked Then
+            If chkLine.Checked Then
+                clsRScreePlot.AddParameter("geom", "c(" & Chr(34) & "bar" & Chr(34) & "," & Chr(34) & "line" & Chr(34) & ")")
+            Else
+                clsRScreePlot.AddParameter("geom", Chr(34) & "bar" & Chr(34))
+            End If
+        Else
+            chkLine.Checked = True
+            clsRScreePlot.AddParameter("geom", Chr(34) & "line" & Chr(34))
+        End If
+    End Sub
+
+    Private Sub chkLine_CheckedChanged(sender As Object, e As EventArgs) Handles chkLine.CheckedChanged
+        If chkLine.Checked Then
+            If chkBar.Checked Then
+                clsRScreePlot.AddParameter("geom", "c(" & Chr(34) & "bar" & Chr(34) & "," & Chr(34) & "line" & Chr(34) & ")")
+            Else
+                clsRScreePlot.AddParameter("geom", Chr(34) & "line" & Chr(34))
+            End If
+        Else
+            chkBar.Checked = True
+            clsRScreePlot.AddParameter("geom", Chr(34) & "bar" & Chr(34))
+        End If
+    End Sub
+
+    Private Sub cmbChoice_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbChoice.SelectedIndexChanged
+        clsRScreePlot.AddParameter("choice", Chr(34) & cmbChoice.SelectedItem.ToString & Chr(34))
+    End Sub
+
+    Private Sub chkScreePlot_CheckedChanged(sender As Object, e As EventArgs) Handles chkScreePlot.CheckedChanged
+        If chkScreePlot.Checked Then
+            grpGeom.Enabled = True
+            chkBar.Enabled = True
+            chkLine.Enabled = True
+            lblChoice.Enabled = True
+            cmbChoice.Enabled = True
+        Else
+            chkBar.Enabled = False
+            chkLine.Enabled = False
+            grpGeom.Enabled = False
+            lblChoice.Enabled = False
+            cmbChoice.Enabled = False
+        End If
+    End Sub
+
+
     Public Sub SetDefaults()
         chkEigenValues.Checked = True
         chkEigenVectors.Checked = True
         chkScores.Checked = True
         chkResiduals.Checked = True
+        chkScreePlot.Checked = False
+        chkBar.Checked = True
+        chkLine.Checked = True
+        chkBar.Enabled = False
+        chkLine.Enabled = False
+        grpGeom.Enabled = False
+        lblChoice.Enabled = False
+        cmbChoice.SelectedItem = "variance"
+        cmbChoice.Enabled = False
     End Sub
 
     Public Sub PCAOptions()
@@ -73,6 +137,9 @@ Public Class sdgPrincipalComponentAnalysis
         End If
         If (chkResiduals.Checked) Then
             Residuals()
+        End If
+        If chkScreePlot.Checked Then
+            ScreePlot()
         End If
     End Sub
 
