f1b987b0d8918305bae7e82dce10951ad6d4b2bb
Mary Mutahi <mutahi.nyambura@outlook.com>
2016-05-26 05:29:02 +0300

    dlPCA– Organizing the designer and code

--- instat/dlgPrincipalComponentAnalysis.vb
+++ instat/dlgPrincipalComponentAnalysis.vb
@@ -81,11 +81,11 @@ Public Class dlgPrincipalComponentAnalysis
         ucrBasePCA.clsRsyntax.AddParameter("ncp", nudComponents.Value)
     End Sub
 
-    Private Sub nudComponents_ValueChanged(sender As Object, e As EventArgs) Handles nudComponents.ValueChanged
+    Private Sub nudComponents_TextChanged(sender As Object, e As EventArgs) Handles nudComponents.TextChanged
         ucrBasePCA.clsRsyntax.AddParameter("ncp", nudComponents.Value)
     End Sub
 
-    Private Sub chkScaleData_CheckedChanged(sender As Object, e As EventArgs) Handles chkScaleData.CheckedChanged
+    Private Sub chkScaleData_TextChanged(sender As Object, e As EventArgs) Handles chkScaleData.CheckedChanged
         If chkScaleData.Checked Then
             ucrBasePCA.clsRsyntax.AddParameter("scale.unit", "TRUE")
         Else
