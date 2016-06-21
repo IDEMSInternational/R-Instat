c6386c4b8dc48522a21976209c74ae75fcaceed3
deaspo <polycarpokock@live.com>
2016-06-21 04:52:58 +0300

    Added button to clear Script window

--- instat/frmScript.vb
+++ instat/frmScript.vb
@@ -18,4 +18,14 @@
     Private Sub frmScript_VisibleChanged(sender As Object, e As EventArgs) Handles Me.VisibleChanged
         frmMain.mnuViewScriptWindow.Checked = Me.Visible
     End Sub
+
+    Private Sub cmdClear_Click(sender As Object, e As EventArgs) Handles cmdClear.Click
+        Dim dlgResponse As DialogResult
+        If txtScript.Text <> "" Then
+            dlgResponse = MessageBox.Show("Are you sure you want to clear the " & Me.Text, "Clear " & Me.Text, MessageBoxButtons.YesNo)
+            If dlgResponse = DialogResult.Yes Then
+                txtScript.Clear()
+            End If
+        End If
+    End Sub
 End Class
