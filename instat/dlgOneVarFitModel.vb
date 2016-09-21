0b7b971a2b56fda7fb7c94ab7002f7326fd7ef18
Lily <lclements1004@outlook.com>
2016-09-21 12:30:49 +0100

    Updated One Var Fit Model dialog

--- instat/dlgOneVarFitModel.vb
+++ instat/dlgOneVarFitModel.vb
@@ -16,7 +16,6 @@
 
 Imports instat.Translations
 
-
 Public Class dlgOneVarFitModel
     Public clsRConvert, clsRCIFunction As New RFunction
     Public bfirstload As Boolean = True
@@ -33,43 +32,33 @@ Public Class dlgOneVarFitModel
     End Sub
 
     Private Sub InitialiseDialog()
-        UcrBase.clsRsyntax.SetFunction("")
+        UcrBase.clsRsyntax.SetFunction("fitdist")
         'ucrBase.iHelpTopicID = 
-
+        'UcrBase.clsRsyntax.iCallType = 2
         UcrReceiver.Selector = ucrSelectorOneVarFitMod
+        UcrReceiver.SetMeAsReceiver()
         ucrSaveModel.SetDataFrameSelector(ucrSelectorOneVarFitMod.ucrAvailableDataFrames)
-        UcrDistributions.SetGLMDistributions()
-
         ucrSaveModel.SetPrefix("dist")
-        AssignSaveModel()
+        ucrSaveModel.SetItemsTypeAsModels()
+        ucrSaveModel.SetDefaultTypeAsModel()
+        ucrSaveModel.SetValidationTypeAsRVariable()
     End Sub
 
     Private Sub SetDefaults()
-        ucrSelectorOneVarFitMod.Focus()
         ucrSelectorOneVarFitMod.Reset()
-        UcrReceiver.SetMeAsReceiver()
-        chkConvertToVariate.Checked = False
-        chkConvertToVariate.Visible = False
-        ucrSaveModel.Visible = True
+        ucrSelectorOneVarFitMod.Focus()
         chkSaveModel.Checked = True
         ucrSaveModel.Reset()
-        ConverttoVariate()
-        'sdgOneVarFitModel.SetDefaults()
-        '       sdgOneVarFitModDisplay.SetDefaults()
-        ucrSaveModel.Reset()
+        SetDataParameter()
+        AssignSaveModel()
         TestOKEnabled()
-        If (ucrSelectorOneVarFitMod.ucrAvailableDataFrames.cboAvailableDataFrames.Text <> "") Then
-            ucrSaveModel.SetName(ucrSelectorOneVarFitMod.ucrAvailableDataFrames.cboAvailableDataFrames.Text & "_dist")
-        Else
-            ucrSaveModel.SetName("")
-        End If
     End Sub
 
     Private Sub ReopenDialog()
     End Sub
 
     Private Sub TestOKEnabled()
-        If (chkSaveModel.Checked AndAlso Not ucrSaveModel.IsEmpty() OrElse Not chkSaveModel.Checked) AndAlso UcrDistributions.Enabled AndAlso Not UcrReceiver.IsEmpty Then
+        If (chkSaveModel.Checked AndAlso Not ucrSaveModel.IsEmpty() OrElse Not chkSaveModel.Checked) AndAlso Not UcrReceiver.IsEmpty Then
             UcrBase.OKEnabled(True)
         Else
             UcrBase.OKEnabled(False)
@@ -80,18 +69,9 @@ Public Class dlgOneVarFitModel
         SetDefaults()
     End Sub
 
-    Private Sub ucrBase_ClickOk(sender As Object, e As EventArgs) Handles UcrBase.ClickOk
-        'sdgOneVarFitModel.RegOptions()
-    End Sub
-
-
-
     Private Sub ucrSelectorOneVarFitMod_DataFrameChanged() Handles ucrSelectorOneVarFitMod.DataFrameChanged
-        UcrBase.clsRsyntax.AddParameter("data", clsRFunctionParameter:=ucrSelectorOneVarFitMod.ucrAvailableDataFrames.clsCurrDataFrame)
-        'the above line may not be correct..
         AssignSaveModel()
     End Sub
-
 
 
     Private Sub ucrSaveModel_NameChanged() Handles ucrSaveModel.NameChanged
@@ -100,41 +80,29 @@ Public Class dlgOneVarFitModel
     End Sub
 
 
-    Public Sub ConverttoVariate()
+    Public Sub SetDataParameter()
         If Not UcrReceiver.IsEmpty Then
-
-            ' UcrDistributions.ReceiverDataType(ucrSelectorOneVarFitMod.ucrAvailableDataFrames.cboAvailableDataFrames.Text, UcrReceiver.GetVariableNames(bWithQuotes:=False))
-
-            If UcrDistributions.strDataType = "numeric" Then
+            If UcrReceiver.strCurrDataType = "numeric" OrElse UcrReceiver.strCurrDataType = "integer" Then
                 chkConvertToVariate.Checked = False
                 chkConvertToVariate.Visible = False
             Else
                 chkConvertToVariate.Visible = True
             End If
-
             If chkConvertToVariate.Checked Then
                 clsRConvert.SetRCommand("as.numeric")
-                clsRConvert.AddParameter("x", UcrReceiver.GetVariableNames(bWithQuotes:=False))
-                'clsModel.SetParameter(True, clsRFunc:=clsRConvert)
-                '   UcrDistributions.ReceiverDataType("numeric")
+                clsRConvert.AddParameter("x", clsRFunctionParameter:=UcrReceiver.GetVariables())
+                UcrBase.clsRsyntax.AddParameter("data", clsRFunctionParameter:=clsRConvert)
             Else
-                '      clsmodel.setparameter(True, strValue:=UcrReceiver.GetVariableNames(bWithQuotes:=False))
-                '  UcrDistributions.receiverdatatype(ucrSelectorOneVarFitMod.ucrAvailableDataFrames.cboAvailableDataFrames.Text, UcrReceiver.GetVariableNames(bWithQuotes:=False))
-
+                'TODO This is needed because fitdist checks is.vector on data which is FALSE when data has attributes
+                clsRConvert.SetRCommand("as.vector")
+                clsRConvert.AddParameter("x", clsRFunctionParameter:=UcrReceiver.GetVariables())
+                UcrBase.clsRsyntax.AddParameter("data", clsRFunctionParameter:=clsRConvert)
             End If
-            ' sdgModelOptions.ucrDistributions.ReceiverDatatype(ucrDistributions.strDataType)
+        Else
+            chkConvertToVariate.Visible = False
+            UcrBase.clsRsyntax.RemoveParameter("data")
         End If
-
-        'If UcrDistributions.1stCurrentDistribution.Count=0 Or ucrreceiver.IsEmpty() Then
-        'UcrDistributions.Enabled = False
-        'UcrDistributions.cboDistributions.Text = ""
-        'cmdFittingOptions.Enabled = False
-        'Else
-        'UcrDistributions.Enabled = True
-        'cmdFittingOptions.Enabled = True
-        'End If
     End Sub
-
 
     Private Sub AssignSaveModel()
         If chkSaveModel.Checked AndAlso Not ucrSaveModel.IsEmpty Then
@@ -157,36 +125,25 @@ Public Class dlgOneVarFitModel
     End Sub
 
     Private Sub ucrDistributions_cboDistributionsIndexChanged(sender As Object, e As EventArgs) Handles UcrDistributions.cboDistributionsIndexChanged
-        '    sdgModelOptions.ucrDistributions.ReceiverDatatype(UcrDistributions.strDataType)
-        '   sdgModelOptions.ucrDistributions.cboDistributions.SelectedIndex = sdgModelOptions.ucrFamily.lstCurrentDistributions.FindIndex(Function(dist) dist.strNameTag = ucrFamily.clsCurrDistribution.strNameTag)
-        '  sdgModelOptions.RestrictLink()
-        ' 'TODO:   Include multinomial as an option And the appropriate function
-        'If (UcrDistributions.clsCurrDistribution.strNameTag = "Normal") Then
-        'UcrBase.clsRsyntax.SetFunction("lm")
-        'UcrBase.clsRsyntax.RemoveParameter("family")
-        'Else
-        '   clsRCIFunction.SetRCommand(UcrDistributions.clsCurrDistribution.strGLMFunctionName)
-        '  UcrBase.clsRsyntax.SetFunction("glm")
-        ' UcrBase.clsRsyntax.AddParameter("family", clsRFunctionParameter:=clsRCIFunction)
-        'End If
+        UcrBase.clsRsyntax.AddParameter("distr", Chr(34) & UcrDistributions.clsCurrDistribution.strRName & Chr(34))
+        SetDataParameter()
     End Sub
 
     Private Sub UcrReceiver_SelectionChanged(sender As Object, e As EventArgs) Handles UcrReceiver.SelectionChanged
-        ConverttoVariate()
-        TestOKEnabled()
-    End Sub
-
-    Private Sub chkConvertToVariate_CheckedChanged_1(sender As Object, e As EventArgs)
-        ConverttoVariate()
+        SetDataParameter()
         TestOKEnabled()
     End Sub
 
     Private Sub cmdFittingOptions_Click(sender As Object, e As EventArgs) Handles cmdFittingOptions.Click
         sdgOneVarFitModel.ShowDialog()
-        'UcrDistributions.cboDistributions.SelectedIndex = UcrDistributions.lstCurrentDistributions.FindIndex(Function(dist) dist.strNameTag = sdgFittingOptions.ucrDistributions.clsCurrDistribution.strNameTag)
     End Sub
 
     Private Sub cmdDisplayOptions_Click(sender As Object, e As EventArgs) Handles cmdDisplayOptions.Click
         sdgOneVarFitModDisplay.ShowDialog()
+    End Sub
+
+    Private Sub chkConvertToVariate_CheckedChanged(sender As Object, e As EventArgs) Handles chkConvertToVariate.CheckedChanged
+        SetDataParameter()
+        TestOKEnabled()
     End Sub
 End Class
