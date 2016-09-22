13a67b7d30ce20a3fcb8c003ba6686e663935931
Stephen Kogo <stevekogo@gmail.com>
2016-09-22 12:03:04 +0300

    Adding code to the designer

--- instat/dlgTwoVariableUseModel.vb
+++ instat/dlgTwoVariableUseModel.vb
@@ -14,8 +14,95 @@
 ' You should have received a copy of the GNU General Public License k
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 Imports instat.Translations
+Imports RDotNet
 Public Class dlgTwoVariableUseModel
+    Private bFirstLoad As Boolean = True
+    Public clsRModel, clsRCommand As New RFunction
+    Dim strModel As String
+
     Private Sub dlgTwoVariableUseModel_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         autoTranslate(Me)
+        If bFirstLoad Then
+            InitialiseDialog()
+            SetDefaults()
+            bFirstLoad = False
+        Else
+            ReOpenDialog()
+        End If
+        TestOkEnabled()
+    End Sub
+
+    Private Sub SetDefaults()
+        ucrReceiverUseModel.SetMeAsReceiver()
+        ucrSelectorUseModel.Reset()
+        ucrModel.SetName("")
+        TestOkEnabled()
+    End Sub
+
+    Private Sub InitialiseDialog()
+        'ucrBase.iHelpTopicID = 
+        ucrBaseUseModel.clsRsyntax.SetOperation("+")
+        ucrSelectorUseModel.SetItemType("model")
+        ucrReceiverUseModel.Selector = ucrSelectorUseModel
+        clsRCommand.SetRCommand(frmMain.clsRLink.strInstatDataObject & "$get_models")
+        ucrBaseUseModel.clsRsyntax.SetOperatorParameter(True, clsRFunc:=clsRCommand)
+        ucrModel.IsReadOnly = True
+        sdgSimpleRegOptions.SetRModelFunction(clsRCommand)
+        sdgSimpleRegOptions.SetRDataFrame(ucrSelectorUseModel.ucrAvailableDataFrames)
+    End Sub
+
+    Private Sub ReOpenDialog()
+
+    End Sub
+
+    Private Sub ucrBaseUseModel_ClickReset(sender As Object, e As EventArgs) Handles ucrBaseUseModel.ClickReset
+        SetDefaults()
+    End Sub
+
+    Private Sub ucrBaseUseModel_ClickOk(sender As Object, e As EventArgs) Handles ucrBaseUseModel.ClickOk
+        sdgSimpleRegOptions.RegOptions()
+    End Sub
+
+    Private Sub TestOkEnabled()
+        If Not ucrReceiverUseModel.IsEmpty Then
+            ucrBaseUseModel.OKEnabled(True)
+            Model()
+        Else
+            ucrBaseUseModel.OKEnabled(False)
+        End If
+    End Sub
+
+    Private Sub Model()
+        If Not ucrReceiverUseModel.IsEmpty And Not ucrSelectorUseModel.ucrAvailableDataFrames.cboAvailableDataFrames.SelectedItem = "" Then
+            clsRModel.SetRCommand("formula")
+            clsRModel.AddParameter("", clsRFunctionParameter:=clsRCommand)
+            'strDataTypes = frmMain.clsRLink.RunInternalScriptGetValue(clsRModel.ToScript()).AsCharacter.AsList(0).ToString
+        Else
+            strModel = ""
+        End If
+        ucrModel.SetName(strModel)
+    End Sub
+
+    Private Sub ucrModelReceiver_SelectionChanged(sender As Object, e As EventArgs) Handles ucrReceiverUseModel.SelectionChanged
+        If Not ucrReceiverUseModel.IsEmpty Then
+            clsRCommand.AddParameter("model_name", ucrReceiverUseModel.GetVariableNames())
+        Else
+            ucrBaseUseModel.clsRsyntax.RemoveParameter("model_name")
+        End If
+        TestOkEnabled()
+
+    End Sub
+
+    Private Sub ucrSelectorUseModel_DataFrameChanged() Handles ucrSelectorUseModel.DataFrameChanged
+        clsRCommand.AddParameter("data_name", Chr(34) & ucrSelectorUseModel.ucrAvailableDataFrames.cboAvailableDataFrames.SelectedItem & Chr(34))
+        TestOkEnabled()
+    End Sub
+
+    Private Sub cmdDisplayOptions_Click(sender As Object, e As EventArgs) Handles cmdDisplayOptions.Click
+        sdgSimpleRegOptions.ShowDialog()
+    End Sub
+
+    Private Sub cmdPredict_Click(sender As Object, e As EventArgs) Handles cmdPredict.Click
+        'this should lead to predict subdialogue
     End Sub
 End Class
