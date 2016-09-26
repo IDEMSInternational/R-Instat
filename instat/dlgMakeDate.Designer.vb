40d7c254c09f90c56e2229299b8b0f8291ee9c2e
steven ndung'u <stevenndungu4@outlook.com>
2016-09-26 16:18:46 +0300

    Make date dialogue box

---
+++ instat/dlgMakeDate.Designer.vb
@@ -0,0 +1,161 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class dlgMakeDate
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ucrSeclectorMakeDate = New instat.ucrSelectorByDataFrameAddRemove()
+        Me.ucrReceiverYear = New instat.ucrReceiverSingle()
+        Me.ucrReceiverMonth = New instat.ucrReceiverSingle()
+        Me.ucrReceiverDay = New instat.ucrReceiverSingle()
+        Me.lblYear = New System.Windows.Forms.Label()
+        Me.lblMonth = New System.Windows.Forms.Label()
+        Me.lblDay = New System.Windows.Forms.Label()
+        Me.UcrInputNewColumnName = New instat.ucrInputComboBox()
+        Me.lblNewColumnName = New System.Windows.Forms.Label()
+        Me.ucrBase = New instat.ucrButtons()
+        Me.SuspendLayout()
+        '
+        'ucrSeclectorMakeDate
+        '
+        Me.ucrSeclectorMakeDate.bShowHiddenColumns = False
+        Me.ucrSeclectorMakeDate.bUseCurrentFilter = False
+        Me.ucrSeclectorMakeDate.Location = New System.Drawing.Point(9, 9)
+        Me.ucrSeclectorMakeDate.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrSeclectorMakeDate.Name = "ucrSeclectorMakeDate"
+        Me.ucrSeclectorMakeDate.Size = New System.Drawing.Size(210, 180)
+        Me.ucrSeclectorMakeDate.TabIndex = 0
+        '
+        'ucrReceiverYear
+        '
+        Me.ucrReceiverYear.Location = New System.Drawing.Point(269, 43)
+        Me.ucrReceiverYear.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverYear.Name = "ucrReceiverYear"
+        Me.ucrReceiverYear.Selector = Nothing
+        Me.ucrReceiverYear.Size = New System.Drawing.Size(120, 20)
+        Me.ucrReceiverYear.TabIndex = 1
+        '
+        'ucrReceiverMonth
+        '
+        Me.ucrReceiverMonth.Location = New System.Drawing.Point(269, 93)
+        Me.ucrReceiverMonth.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverMonth.Name = "ucrReceiverMonth"
+        Me.ucrReceiverMonth.Selector = Nothing
+        Me.ucrReceiverMonth.Size = New System.Drawing.Size(120, 20)
+        Me.ucrReceiverMonth.TabIndex = 1
+        '
+        'ucrReceiverDay
+        '
+        Me.ucrReceiverDay.Location = New System.Drawing.Point(269, 144)
+        Me.ucrReceiverDay.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverDay.Name = "ucrReceiverDay"
+        Me.ucrReceiverDay.Selector = Nothing
+        Me.ucrReceiverDay.Size = New System.Drawing.Size(120, 20)
+        Me.ucrReceiverDay.TabIndex = 1
+        '
+        'lblYear
+        '
+        Me.lblYear.AutoSize = True
+        Me.lblYear.Location = New System.Drawing.Point(296, 30)
+        Me.lblYear.Name = "lblYear"
+        Me.lblYear.Size = New System.Drawing.Size(29, 13)
+        Me.lblYear.TabIndex = 2
+        Me.lblYear.Text = "Year"
+        '
+        'lblMonth
+        '
+        Me.lblMonth.AutoSize = True
+        Me.lblMonth.Location = New System.Drawing.Point(296, 78)
+        Me.lblMonth.Name = "lblMonth"
+        Me.lblMonth.Size = New System.Drawing.Size(37, 13)
+        Me.lblMonth.TabIndex = 2
+        Me.lblMonth.Text = "Month"
+        '
+        'lblDay
+        '
+        Me.lblDay.AutoSize = True
+        Me.lblDay.Location = New System.Drawing.Point(296, 131)
+        Me.lblDay.Name = "lblDay"
+        Me.lblDay.Size = New System.Drawing.Size(26, 13)
+        Me.lblDay.TabIndex = 2
+        Me.lblDay.Text = "Day"
+        '
+        'UcrInputNewColumnName
+        '
+        Me.UcrInputNewColumnName.IsReadOnly = False
+        Me.UcrInputNewColumnName.Location = New System.Drawing.Point(111, 208)
+        Me.UcrInputNewColumnName.Name = "UcrInputNewColumnName"
+        Me.UcrInputNewColumnName.Size = New System.Drawing.Size(159, 21)
+        Me.UcrInputNewColumnName.TabIndex = 3
+        '
+        'lblNewColumnName
+        '
+        Me.lblNewColumnName.AutoSize = True
+        Me.lblNewColumnName.Location = New System.Drawing.Point(12, 208)
+        Me.lblNewColumnName.Name = "lblNewColumnName"
+        Me.lblNewColumnName.Size = New System.Drawing.Size(98, 13)
+        Me.lblNewColumnName.TabIndex = 4
+        Me.lblNewColumnName.Text = "New Column Name"
+        '
+        'ucrBase
+        '
+        Me.ucrBase.Location = New System.Drawing.Point(9, 232)
+        Me.ucrBase.Name = "ucrBase"
+        Me.ucrBase.Size = New System.Drawing.Size(410, 52)
+        Me.ucrBase.TabIndex = 5
+        '
+        'dlgMakeDate
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(420, 298)
+        Me.Controls.Add(Me.ucrBase)
+        Me.Controls.Add(Me.lblNewColumnName)
+        Me.Controls.Add(Me.UcrInputNewColumnName)
+        Me.Controls.Add(Me.lblDay)
+        Me.Controls.Add(Me.lblMonth)
+        Me.Controls.Add(Me.lblYear)
+        Me.Controls.Add(Me.ucrReceiverDay)
+        Me.Controls.Add(Me.ucrReceiverMonth)
+        Me.Controls.Add(Me.ucrReceiverYear)
+        Me.Controls.Add(Me.ucrSeclectorMakeDate)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "dlgMakeDate"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Make Date"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ucrSeclectorMakeDate As ucrSelectorByDataFrameAddRemove
+    Friend WithEvents ucrReceiverYear As ucrReceiverSingle
+    Friend WithEvents ucrReceiverMonth As ucrReceiverSingle
+    Friend WithEvents ucrReceiverDay As ucrReceiverSingle
+    Friend WithEvents lblYear As Label
+    Friend WithEvents lblMonth As Label
+    Friend WithEvents lblDay As Label
+    Friend WithEvents UcrInputNewColumnName As ucrInputComboBox
+    Friend WithEvents lblNewColumnName As Label
+    Friend WithEvents ucrBase As ucrButtons
+End Class
---
+++ instat/dlgMakeDate.vb
@@ -0,0 +1,76 @@
+﻿' Instat-R
+' Copyright (C) 2015
+'
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+'
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License k
+' along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Imports instat.Translations
+Public Class dlgMakeDate
+    Public bFirstLoad As Boolean = True
+    Private Sub ucrSeclectorMakeDate_Load(sender As Object, e As EventArgs) Handles ucrSeclectorMakeDate.Load
+        If bFirstLoad Then
+            SetDefaults()
+            InitialiseDialog()
+            bFirstLoad = False
+        Else
+            ReopenDialog()
+        End If
+
+        autoTranslate(Me)
+
+    End Sub
+    Private Sub dlgMakeDate_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    Private Sub InitialiseDialog()
+        ucrReceiverYear.Selector = ucrSeclectorMakeDate
+        ucrReceiverMonth.Selector = ucrSeclectorMakeDate
+        ucrReceiverDay.Selector = ucrSeclectorMakeDate
+        ucrReceiverYear.SetIncludedDataTypes({"numeric", "factor"})
+        ucrReceiverMonth.SetIncludedDataTypes({"numeric", "factor"})
+        ucrReceiverDay.SetIncludedDataTypes({"numeric", "factor"})
+        ucrReceiverYear.SetMeAsReceiver()
+
+        UcrInputNewColumnName.SetPrefix("Date")
+        UcrInputNewColumnName.SetItemsTypeAsColumns()
+        UcrInputNewColumnName.SetDefaultTypeAsColumn()
+        UcrInputNewColumnName.SetDataFrameSelector(ucrSeclectorMakeDate.ucrAvailableDataFrames)
+        UcrInputNewColumnName.SetValidationTypeAsRVariable()
+
+
+    End Sub
+    Private Sub SetDefaults()
+
+    End Sub
+    Private Sub TestOkEnabled()
+        If Not ucrReceiverYear.IsEmpty AndAlso Not ucrReceiverDay.IsEmpty AndAlso Not ucrReceiverMonth.IsEmpty Then
+            ucrBase.OKEnabled(True)
+        Else
+            ucrBase.OKEnabled(False)
+        End If
+    End Sub
+    Private Sub ReopenDialog()
+    End Sub
+
+    Private Sub ucrReceiverYear_SelectionChanged(sender As Object, e As EventArgs) Handles ucrReceiverYear.SelectionChanged
+        TestOkEnabled()
+    End Sub
+
+    Private Sub ucrReceiverMonth_SelectionChanged(sender As Object, e As EventArgs) Handles ucrReceiverMonth.SelectionChanged
+        TestOkEnabled()
+    End Sub
+
+    Private Sub ucrReceiverDay_SelectionChanged(sender As Object, e As EventArgs) Handles ucrReceiverDay.SelectionChanged
+        TestOkEnabled()
+    End Sub
+End Class
