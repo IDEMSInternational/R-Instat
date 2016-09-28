f8bcdbb7c895768bec6081b5d0fcf481bc54e661
Lunalo <johnlunalo95@gmail.com>
2016-09-28 10:12:49 +0300

    Creating dlgTablesPlus

---
+++ instat/dlgTablesPlus.Designer.vb
@@ -0,0 +1,81 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class dlgTablesPlus
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ucrBase = New instat.ucrButtons()
+        Me.rdoProbabilities = New System.Windows.Forms.RadioButton()
+        Me.rdoQuantiles = New System.Windows.Forms.RadioButton()
+        Me.SuspendLayout()
+        '
+        'ucrBase
+        '
+        Me.ucrBase.Location = New System.Drawing.Point(12, 281)
+        Me.ucrBase.Name = "ucrBase"
+        Me.ucrBase.Size = New System.Drawing.Size(410, 52)
+        Me.ucrBase.TabIndex = 0
+        '
+        'rdoProbabilities
+        '
+        Me.rdoProbabilities.AutoSize = True
+        Me.rdoProbabilities.Location = New System.Drawing.Point(12, 12)
+        Me.rdoProbabilities.Name = "rdoProbabilities"
+        Me.rdoProbabilities.Size = New System.Drawing.Size(79, 17)
+        Me.rdoProbabilities.TabIndex = 1
+        Me.rdoProbabilities.TabStop = True
+        Me.rdoProbabilities.Text = "Probablities"
+        Me.rdoProbabilities.UseVisualStyleBackColor = True
+        '
+        'rdoQuantiles
+        '
+        Me.rdoQuantiles.AutoSize = True
+        Me.rdoQuantiles.Location = New System.Drawing.Point(213, 12)
+        Me.rdoQuantiles.Name = "rdoQuantiles"
+        Me.rdoQuantiles.Size = New System.Drawing.Size(69, 17)
+        Me.rdoQuantiles.TabIndex = 1
+        Me.rdoQuantiles.TabStop = True
+        Me.rdoQuantiles.Text = "Quantiles"
+        Me.rdoQuantiles.UseVisualStyleBackColor = True
+        '
+        'dlgTablesPlus
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(418, 345)
+        Me.Controls.Add(Me.rdoQuantiles)
+        Me.Controls.Add(Me.rdoProbabilities)
+        Me.Controls.Add(Me.ucrBase)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "dlgTablesPlus"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Tables Plus"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ucrBase As ucrButtons
+    Friend WithEvents rdoProbabilities As RadioButton
+    Friend WithEvents rdoQuantiles As RadioButton
+End Class
