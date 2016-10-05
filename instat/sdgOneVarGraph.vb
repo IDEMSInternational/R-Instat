b10d14d73325635ee6a720d7583ae12bc2ab8750
maxwell fundi <maxwellfundi@live.com>
2016-10-05 09:28:42 +0300

    More changes

--- instat/sdgOneVarGraph.vb
+++ instat/sdgOneVarGraph.vb
@@ -17,9 +17,8 @@ Imports instat.Translations
 Public Class sdgOneVarGraph
     Public bFirstLoad As Boolean = True
     Public clsRsyntax As New RSyntax
-    Public strNumericGeomFunction As String
+    Public strNumericGeomFunction As String = ""
     Public strCategoriacalGeomFunction As String = ""
-    Public strCategoricalGeomFunction As String = ""
 
     Private Sub sdgOneVarGraph_Load(sender As Object, e As EventArgs) Handles Me.Load
         If bFirstLoad Then
@@ -38,10 +37,10 @@ Public Class sdgOneVarGraph
         nudNumberofColumns.Value = 3
         ucrInputNumeric.Reset()
         ucrInputCategorical.Reset()
-        ucrInputNumeric.SetName("Boxplot")
-        ucrInputCategorical.SetName("Bar Chart")
         ucrInputNumeric.SetItems({"Boxplot", "Dot Plot", "Histogram", "Point Plot", "Density Plot", "Frequency Polygon"})
         ucrInputCategorical.SetItems({"Bar Chart", "Pie Chart", "Dot Plot"})
+        ucrInputNumeric.SetName("Boxplot")
+        ucrInputCategorical.SetName("Bar Chart")
     End Sub
 
     Public Sub InitialiseDialog()
@@ -51,13 +50,6 @@ Public Class sdgOneVarGraph
 
     Public Sub SetRSyntax(clsNewRSyntax As RSyntax)
         clsRsyntax = clsNewRSyntax
-    End Sub
-
-    Public Sub SetNumericGeomFunction(strNumeric As String)
-        strNumericGeomFunction = strNumeric
-    End Sub
-    Public Sub SetCategoricalGeomFunction(strCategorical As String)
-        strCategoriacalGeomFunction = strCategorical
     End Sub
 
     Private Sub SpecifyLayoutControl()
@@ -71,23 +63,20 @@ Public Class sdgOneVarGraph
     End Sub
 
     Public Sub SetNumericGeomFunction()
-        If ucrInputNumeric.cboInput.SelectedText = "Boxplot" Then
-            strNumericGeomFunction = "geom_boxplot"
-
-        ElseIf ucrInputNumeric.cboInput.SelectedText = "Histogram" Then
-            strNumericGeomFunction = "geom_histogram"
-
-        ElseIf ucrInputNumeric.cboInput.SelectedText = "Dot Plot" Then
-            strNumericGeomFunction = "geom_dotplot"
-
-        ElseIf ucrInputNumeric.cboInput.SelectedText = "Point Plot" Then
-            strNumericGeomFunction = "geom_point"
-
-        ElseIf ucrInputNumeric.cboInput.SelectedText = "Density Plot" Then
-            strNumericGeomFunction = "geom_density"
-        Else
-            strNumericGeomFunction = "geom_freqpoly"
-        End If
+        Select Case ucrInputNumeric.GetText
+            Case "Boxplot"
+                strNumericGeomFunction = "geom_boxplot"
+            Case "Histogram"
+                strNumericGeomFunction = "geom_histogram"
+            Case "Dot Plot"
+                strNumericGeomFunction = "geom_dotplot"
+            Case "Point Plot"
+                strNumericGeomFunction = "geom_point"
+            Case "Density Plot"
+                strNumericGeomFunction = "geom_density"
+            Case Else
+                strNumericGeomFunction = "geom_freqpoly"
+        End Select
     End Sub
 
     Private Sub ucrInputNumeric_NameChanged() Handles ucrInputNumeric.NameChanged
@@ -95,13 +84,15 @@ Public Class sdgOneVarGraph
     End Sub
 
     Public Sub SetCategoricalGeomFunction()
-        If ucrInputCategorical.cboInput.SelectedText = "bar chart" Then
-            strCategoriacalGeomFunction = "geom_bar"
-        ElseIf ucrInputCategorical.cboInput.SelectedText = "pie chart" Then
-            strCategoriacalGeomFunction = "geom_histogram"
-        Else
-            strCategoriacalGeomFunction = "geom_dotplot"
-        End If
+        Select Case ucrInputCategorical.GetText
+            Case "Bar Chart"
+                strCategoriacalGeomFunction = "geom_bar"
+            Case "Dot Plot"
+                strCategoriacalGeomFunction = "geom_dotplot"
+            Case Else
+                strCategoriacalGeomFunction = "geom_bar"
+        End Select
+
     End Sub
     Private Sub ucrInputCategorical_NameChanged() Handles ucrInputCategorical.NameChanged
         SetCategoricalGeomFunction()
