2751eb21b8f49accc7d648456d2e0ff8249d8eab
Lunalo <johnlunalo95@gmail.com>
2016-10-07 10:27:10 +0300

    Small changes on dlgContrasts dialog

--- instat/dlgContrasts.vb
+++ instat/dlgContrasts.vb
@@ -29,22 +29,64 @@ Public Class dlgContrasts
     End Sub
 
     Private Sub TestOKEnabled()
-
+        If Not ucrReceiverForContrasts.IsEmpty Then
+            If rdoCurrentContrasts.Checked OrElse rdoHemert.Checked OrElse rdoPolynomials.Checked OrElse rdoSumToZero.Checked OrElse rdoTreatControl.Checked OrElse rdoUserDefined.Checked Then
+                ucrBase.OKEnabled(True)
+            Else
+                ucrBase.OKEnabled(False)
+            End If
+        Else
+            ucrBase.OKEnabled(False)
+        End If
     End Sub
 
     Private Sub ReopenDialog()
 
     End Sub
     Private Sub InitialiseDialog()
+        ucrReceiverForContrasts.Selector = ucrSelectorForContrast
+        ucrReceiverForContrasts.SetMeAsReceiver()
+        ucrReceiverForContrasts.SetIncludedDataTypes({"factor"})
         ucrBase.iHelpTopicID = 353
+        ucrFactorLevelsAndLabels.SetReceiver(ucrReceiverForContrasts)
+        ucrFactorLevelsAndLabels.SetAsSingleSelector()
     End Sub
     Private Sub SetDefaults()
-
+        ucrSelectorForContrast.Reset()
     End Sub
 
     Private Sub ucrBase_ClickReset(sender As Object, e As EventArgs) Handles ucrBase.ClickReset
         SetDefaults()
     End Sub
 
+    Private Sub ucrReceiverForContrasts_SelectionChanged(sender As Object, e As EventArgs) Handles ucrReceiverForContrasts.SelectionChanged
+        If ucrReceiverForContrasts.IsEmpty = False Then
+            ucrBase.clsRsyntax.AddParameter("n", clsRFunctionParameter:=ucrReceiverForContrasts.GetVariables)
+        Else
+            ucrBase.clsRsyntax.RemoveParameter("n")
+        End If
+        TestOKEnabled()
+    End Sub
 
+    Private Sub rdoCurrentContrasts_CheckedChanged(sender As Object, e As EventArgs) Handles rdoCurrentContrasts.CheckedChanged, rdoHemert.CheckedChanged, rdoPolynomials.CheckedChanged, rdoTreatControl.CheckedChanged, rdoUserDefined.CheckedChanged, rdoSumToZero.CheckedChanged
+        ContrastsFunctions()
+        TestOKEnabled()
+    End Sub
+
+    Private Sub ContrastsFunctions()
+        If rdoSumToZero.Checked Then
+            ucrBase.clsRsyntax.SetFunction("contr.sum")
+        ElseIf rdoCurrentContrasts.Checked Then
+
+
+        ElseIf rdoHemert.Checked Then
+            ucrBase.clsRsyntax.SetFunction("contr.helmert")
+        ElseIf rdoTreatControl.Checked Then
+            ucrBase.clsRsyntax.SetFunction("contr.treatment")
+        ElseIf rdoUserDefined.Checked
+
+        Else
+            ucrBase.clsRsyntax.SetFunction("contr.poly")
+        End If
+    End Sub
 End Class
