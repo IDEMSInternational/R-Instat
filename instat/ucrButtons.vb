f9e307a571d107b92450bc4cbe2e77458f668566
FrancoisJRenaud <francois.j.renaud@gmail.com>
2016-10-07 14:19:01 +0100

    Some comments in ucrButtons, clsRSyntax and dlgColumnStructures
    in ucrSelectorByDataframe, Changed place of RaiseEvent DataFrameChanged as the next line was undoing the work in dlgColumnSrtuctures handle-ing that event.

--- instat/ucrButtons.vb
+++ instat/ucrButtons.vb
@@ -75,6 +75,11 @@ Public Class ucrButtons
         clsRsyntax.SetbIsAssigned(bIsAssigned)
         clsRsyntax.SetbToBeAssigned(bToBeAssigned)
         clsRsyntax.SetstrAssignTo(strAssignTo)
+        'Warning: these reinitializing processes of the RSyntax parameters should probably be integrated at the end of GetScript. 
+        'However, for the moment, RSyntax is not playing it's role of capturing the whole set of R-commands that the user wants to run when OK is Cklicked. 
+        'Indeed, the events BeforeClickOk and ClickOk enables for the moment to insert R-commands before and after the Base R-command handle. 
+        'In the process, we want the RSyntax parameters to be set as at the end of GetScript. Hence the reset needs to come after.
+        'Eventually, all this should be more neatly incorporated in the RSyntax machinery...
         ParentForm.Hide()
         For Each control In ParentForm.Controls
             control.Enabled = True
@@ -106,6 +111,7 @@ Public Class ucrButtons
     Private Sub cmdPaste_Click(sender As Object, e As EventArgs) Handles cmdPaste.Click
         frmScript.txtScript.Text = frmScript.txtScript.Text & vbCrLf & "# " & txtComment.Text
         frmScript.txtScript.Text = frmScript.txtScript.Text & vbCrLf & clsRsyntax.GetScript()
+        'here we getscript but we don't reinitialise the AssignTo etc. for when pressing OK button ? ...
         frmScript.Visible = True
         frmScript.BringToFront()
     End Sub
