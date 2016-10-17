bde734dc593e18dd810b9453e60559883d9aeaee
dannyparsons <danny@aims.ac.za>
2016-10-17 17:30:52 +0100

    initial implementation of saving results from new calcs class

--- instat/static/InstatObject/R/Backend_Components/calculations.R
+++ instat/static/InstatObject/R/Backend_Components/calculations.R
@@ -111,42 +111,71 @@ instat_calculation <- R6Class("instat_calculation",
 #                                     name = "prop", manipulations = list(village_group), 
 #                                     sub_calculations = list(total, val))
 
-# pass in and return link with curr_data as one argument
-instat_object$set("public", "apply_instat_calculation", function(calc, curr_data) {
+# pass in and return link with curr_data_list as one argument
+instat_object$set("public", "apply_instat_calculation", function(calc, curr_data_list) {
+  data_label <- "data"
+  link_label <- "link"
+  require_merge_label <- "require_merge"
+  
   for(manipulation in calc$manipulations) {
-    # now be pair (link + df)
-    curr_data <- self$apply_instat_calculation(manipulation, curr_data)
+    curr_data_list <- self$apply_instat_calculation(manipulation, curr_data_list)
   }
-  if(!missing(curr_data)) sub_calc_results <- curr_data
-  first_sub_calc = TRUE
+  if(!missing(curr_data_list)) sub_calc_results <- curr_data_list
+  first_sub_calc <- TRUE
   for(sub_calc in calc$sub_calculations) {
-    curr_sub_calc <- self$apply_instat_calculation(sub_calc, curr_data)
+    curr_sub_calc <- self$apply_instat_calculation(sub_calc, curr_data_list)
     if(first_sub_calc) sub_calc_results <- curr_sub_calc
     # change to joining all parts of list (data, link, boolean)
     # to merge links from all sub calculations, take link of the "biggest" data frame
     # below is joining the data frame only
     # this needs to change to do the joining correctly
     # through a similar process used by save_calculation to know how to add output
-    else sub_calc_results <- full_join(sub_calc_results, curr_sub_calc)
-    first_sub_calc = FALSE
+    else {
+      #### Set the data
+      sub_calc_results[[data_label]] <- full_join(sub_calc_results[[data_label]], curr_sub_calc[[data_label]])
+      
+      #### Set the link information
+      #TODO Is this ok to do? Not using a link object, but list that can be used to create a link at end
+      if(length(curr_sub_calc[[link_label]][[link_cols]]) > length(sub_calc_results[[link_label]][[link_cols]])) {
+        #Could these have different link columns? e.g. c("Village.") vs c("Variety.")
+        if(!all(sub_calc_results[[link_label]][[link_cols]] %in% curr_sub_calc[[link_label]][[link_cols]])) {
+          stop("sub calculations cannot have disjoint linking columns")
+        }
+        #TODO need to check they have the same to_data_frame?
+        sub_calc_results[[link_label]] <- curr_sub_calc[[link_label]]
+      }
+      else {
+        if(!all(curr_sub_calc[[link_label]][[link_cols]] %in% sub_calc_results[[link_label]][[link_cols]])) {
+          stop("sub calculations cannot have disjoint linking columns")
+        }
+      }
+      #### Set the require_merge boolean
+      sub_calc_results[[require_merge_label]] <- sub_calc_results[[require_merge_label]] || curr_sub_calc[[require_merge_label]]
+    }
+    first_sub_calc <- FALSE
   }
-  if(!first_sub_calc) curr_data <- sub_calc_results
+  if(!first_sub_calc) curr_data_list <- sub_calc_results
   
   data_names <- unique(as.vector(names(calc$calculated_from)))
-  if(missing(curr_data)) {
-    if(length(data_names) == 0) stop("No data frame for calculation.")
+  if(missing(curr_data_list)) {
+    if(length(data_names) == 0) stop("No data specified for calculation.")
     else {
       # change to set data part
       # create/get link from data to itself
-      curr_data <- self$get_data_frame(data_names[[1]])
+      curr_data_list <- list()
+      curr_data_list[[data_label]] <- self$get_data_frame(data_names[[1]])
+      link_list <- list(data_names[[1]], c())
+      names(link_list) <- c("to_data_frame", "link_cols")
+      curr_data_list[[list_label]] <- link_list
+      curr_data_list[[require_merge_label]] <- FALSE
     }
   }
   
   col_names_exp = c()
   i = 1
   for(col_name in calc$calculated_from) {
-    # change to names(curr_data[[data]])
-    if(!(col_name %in% names(curr_data))) {
+    # change to names(curr_data_list[[data]])
+    if(!(col_name %in% names(curr_data_list[[data_label]]))) {
       stop(col_name, " not found in data.")
     }
     col_names_exp[[i]] <- interp(~ var, var = as.name(col_name))
@@ -154,34 +183,43 @@ instat_object$set("public", "apply_instat_calculation", function(calc, curr_data
   }
   
   # updating link as well
-  # replace curr_data with curr_data[[data]]
+  # replace curr_data_list with curr_data_list[[data]]
   if(calc$type == "calculation") {
     # link unchanged
-    curr_data <- curr_data %>% mutate_(.dots = setNames(list(as.formula(paste0("~", calc$function_exp))), calc$name))
+    curr_data_list[[data_label]] <- curr_data_list[[data_label]] %>% mutate_(.dots = setNames(list(as.formula(paste0("~", calc$function_exp))), calc$name))
   }
   else if(calc$type == "summary") {
     # modify link to include linking columns
     # set requires_key = TRUE
-    curr_data <- curr_data %>% summarise_(.dots = setNames(list(as.formula(paste0("~", calc$function_exp))), calc$name))
+    # TODO Could to_data_frame ever change?
+    # TODO Is this always replacing the link_cols?
+    curr_data_list[[data_label]] <- curr_data_list[[data_label]] %>% summarise_(.dots = setNames(list(as.formula(paste0("~", calc$function_exp))), calc$name))
+    curr_data_list[[link_label]][["link_cols"]] <- as.character(groups(curr_data_list[[data_label]]))
+    curr_data_list[[require_merge_label]] <- TRUE
   }
   else if(calc$type == "by") {
     # link unchanged
-    curr_data <- curr_data %>% group_by_(.dots = col_names_exp, add = TRUE)
+    curr_data_list[[data_label]] <- curr_data_list[[data_label]] %>% group_by_(.dots = col_names_exp, add = TRUE)
   }
   else if(calc$type == "filter") {
     # link unchanged
     # set requires_key = TRUE
-    curr_data <- curr_data %>% filter_(.dots = setNames(list(as.formula(paste0("~", calc$function_exp))), calc$name))
+    curr_data_list[[data_label]] <- curr_data_list[[data_label]] %>% filter_(.dots = setNames(list(as.formula(paste0("~", calc$function_exp))), calc$name))
+    curr_data_list[[require_merge_label]] <- TRUE
   }
   else if(calc$type == "join") {
     # link unchanged?
     stop("join not yet implemented.")
   }
+  else if(calc$type == "combination") {
+    # No main calculation, but could have sub calculations
+  }
   # maybe change this to have empty main calculation with sub calculations type = "combination"
-  else stop("Cannot detect calculation type:", calc$type)
-  # curr_data needs to contain requires_key
-  # if(calc$save) self$save_calc_result(calc, curr_data)
+  else stop("Cannot detect calculation type: ", calc$type)
+  
+  # curr_data_list needs to contain requires_key
+  # if(calc$save) self$save_calc_result(calc, curr_data_list)
   # need to add metadata to new columns from calculation
-  return(curr_data)
+  return(curr_data_list)
 }
 )
