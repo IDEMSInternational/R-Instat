0ddeaba3b2adabda4e304facd38a22b72eac91a7
FrancoisJRenaud <francois.j.renaud@gmail.com>
2016-10-27 12:09:27 +0300

    Second draft for Geom_BoxPlot. Little suspicion about one change in the designer: ucrGeomListWithAes at lines 52 and 53...

--- instat/ucrGeom.vb
+++ instat/ucrGeom.vb
@@ -200,9 +200,9 @@ Public Class ucrGeom
 
         clsgeom_boxplot.SetGeomName("geom_boxplot")
         clsgeom_boxplot.AddAesParameter("x")
-        'Warning: When x is continuous, there needs to be a grouping variable... for example cut_width(x,0.25). Are there other grouping possibilities ?
-        clsgeom_boxplot.AddAesParameter("group", strIncludedDataTypes:={"factor"})
         clsgeom_boxplot.AddAesParameter("y", strIncludedDataTypes:={"numeric"}, bIsMandatory:=True)
+        'Warning: When x is continuous, there needs to be a grouping variable... for example cut_width(x,0.25). In our setting, a factor column can be used. If group is empty, R will send a warning message and ignore the x variable (still labels x though).
+        clsgeom_boxplot.AddAesParameter("group", strIncludedDataTypes:={"factor"})
 
         'Warning: The following aesthetics should be assigned to calculations for the different parameters of a boxplot. Only relevant for sort of summaries. 
         'clsgeom_boxplot.AddAesParameter("lower", strIncludedDataTypes:={"numeric"})
@@ -222,17 +222,17 @@ Public Class ucrGeom
         'adding layerParameters
         'Main geom_boxplot parameters
         clsgeom_boxplot.AddLayerParameter("notch", "boolean", "TRUE")
-        clsgeom_boxplot.AddLayerParameter("notchwidth", "numeric", "0.5", lstParameterStrings:={0.1, 0, 1}) 'Question to be discussed: this sets the width of the boxplot as a proportion of the boxplot width. values can be anything in ggplot but negative ones just give a silly looking thing, and I don't know if values above 1 make sense ? The notch would then be larger than the boxplot.
+        clsgeom_boxplot.AddLayerParameter("notchwidth", "numeric", "0.5", lstParameterStrings:={0.1}) 'Question to be discussed: this sets the width of the notch as a proportion of the boxplot width. Values can be anything in ggplot but negative ones just give a silly looking thing (I ve left it in for now, but would suggest we exclude ?), and I don't know if values above 1 make sense ? The notch would then be larger than the boxplot.
         clsgeom_boxplot.AddLayerParameter("varwidth", "boolean", "TRUE")
-        clsgeom_boxplot.AddLayerParameter("coef", "numeric", "1.5") 'Question to be discussed: This parameter is setting the length of the whiskers as a multiple of the IQR. Do we want to restrict the values ? Also the window doesn't adapt to the whiskers length, which means they are cut when too long.
-        clsgeom_boxplot.AddLayerParameter("outlier.shape", "numeric", "19", lstParameterStrings:={1, 0, 25}) 'there are other symbols that we can add here 
+        clsgeom_boxplot.AddLayerParameter("coef", "numeric", "1.5", lstParameterStrings:={0.1}) 'Question to be discussed: This parameter is setting the length of the whiskers as a multiple of the IQR. When giving a negative value, the whiskers are simply of length 0. Also the window showing the graph doesn't adapt to the whiskers' length, which means they are simply cut when too long.
+        clsgeom_boxplot.AddLayerParameter("outlier.shape", "numeric", "19", lstParameterStrings:={1, 0, 25}) 'Warning: there are other symbols that we can add here 
         clsgeom_boxplot.AddLayerParameter("outlier.colour", "colour", "NULL")
-        clsgeom_boxplot.AddLayerParameter("outlier.stroke", "numeric", "0.5", lstParameterStrings:={1, 0}) 'Outlier.stroke parameter gives the size of the outliers. It cannot be negative as this sends an error in R.
+        clsgeom_boxplot.AddLayerParameter("outlier.stroke", "numeric", "0.5", lstParameterStrings:={1, 0}) 'Outlier.stroke parameter gives the size of the outliers. It cannot be negative, this would trigger an error in R.
 
         'Global Layer parameters
-        clsgeom_boxplot.AddLayerParameter("show.legend", "boolean", "TRUE") 'Warning: the default value in R is NA, which only shows legend for that layer if aesthetics are mapped.
-        clsgeom_boxplot.AddLayerParameter("position", "list", Chr(34) & "dodge" & Chr(34), lstParameterStrings:={Chr(34) & "stack" & Chr(34), Chr(34) & "fill" & Chr(34), Chr(34) & "dodge" & Chr(34), Chr(34) & "jitter" & Chr(34), Chr(34) & "identity" & Chr(34)}) 'Could add the possiility to adjust the height and width parameters of these position functions... Sometimes useful to only jitter in one direction for example...
-        'Aesthetics as layer parameters...
+        clsgeom_boxplot.AddLayerParameter("show.legend", "boolean", "TRUE") 'Warning: The default value in R is NA, which only shows legend for that layer if aesthetics are mapped.
+        clsgeom_boxplot.AddLayerParameter("position", "list", Chr(34) & "dodge" & Chr(34), lstParameterStrings:={Chr(34) & "stack" & Chr(34), Chr(34) & "fill" & Chr(34), Chr(34) & "dodge" & Chr(34), Chr(34) & "jitter" & Chr(34), Chr(34) & "identity" & Chr(34)}) 'Warning: Could add the possiility to adjust the height and width parameters in the position function position_jitter, also width for dodge... Sometimes useful to only jitter in one direction for example...
+        'Aesthetics as layer parameters... Used fo fix colour, transparence, ... of the geom on that Layer.
         clsgeom_boxplot.AddLayerParameter("fill", "colour", "NULL")
         clsgeom_boxplot.AddLayerParameter("colour", "colour", "NULL")
         clsgeom_boxplot.AddLayerParameter("linetype", "numeric", "1", lstParameterStrings:={1, 0, 6})
