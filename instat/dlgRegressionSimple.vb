7c31c97d5abe6eea3b9fd281810f1b14cc16e573
Lily <lclements1004@outlook.com>
2016-10-28 14:58:47 +0100

    Amendments to Two Variable - Hyp Tests

--- instat/dlgRegressionSimple.vb
+++ instat/dlgRegressionSimple.vb
@@ -18,7 +18,7 @@ Imports instat.Translations
 Public Class dlgRegressionSimple
     Public bFirstLoad As Boolean = True
     Public clsModel, clsFunctionOperation As New ROperator
-    Public clsRConvert, clsRCIFunction, clsTwoVarModel, clsRCount, clsRCount2 As New RFunction
+    Public clsRConvert, clsRCIFunction, clsTwoVarModel, clsRPoisson, clsRTTest, clsRBinomial, clsRLength, clsRMean, clsRMean2, clsRLength2 As New RFunction
 
     Private Sub dlgRegressionSimple_Load(sender As Object, e As EventArgs) Handles Me.Load
         If bFirstLoad Then
@@ -83,8 +83,8 @@ Public Class dlgRegressionSimple
         ucrModelName.Reset()
         ucrModelPreview.SetName("")
         ResponseConvert()
+        SetRCode()
         TestOKEnabled()
-        PoissonChange()
     End Sub
 
     Private Sub SimpleRegression()
@@ -104,102 +104,80 @@ Public Class dlgRegressionSimple
         '        End If
         '       End If
         'TODO:   Include multinomial as an option And the appropriate function
-        If rdoGeneral.Checked Then
-            If (ucrFamily.clsCurrDistribution.strNameTag = "Normal") Then
-                ucrBase.clsRsyntax.SetFunction("lm")
-                ucrBase.clsRsyntax.RemoveParameter("family")
-            Else
-                clsRCIFunction.SetRCommand(ucrFamily.clsCurrDistribution.strGLMFunctionName)
-                ucrBase.clsRsyntax.SetFunction("glm")
-                clsTwoVarModel.AddParameter("family", clsRFunctionParameter:=clsRCIFunction)
-            End If
+        If (ucrFamily.clsCurrDistribution.strNameTag = "Normal") Then
+            ucrBase.clsRsyntax.SetFunction("lm")
+            ucrBase.clsRsyntax.RemoveParameter("family")
+        Else
+            clsRCIFunction.SetRCommand(ucrFamily.clsCurrDistribution.strGLMFunctionName)
+            ucrBase.clsRsyntax.SetFunction("glm")
+            clsTwoVarModel.AddParameter("family", clsRFunctionParameter:=clsRCIFunction)
         End If
     End Sub
 
     Private Sub SetTTest()
-        clsTwoVarModel.ClearParameters()
         ' only variables the response receiver can accept is numerical
-        clsTwoVarModel.SetRCommand("t.test")
-        clsTwoVarModel.AddParameter("conf.level", nudCI.Value.ToString())
-        clsTwoVarModel.AddParameter("mu", nudHypothesis.Value.ToString())
+        clsRTTest.SetRCommand("t.test")
+        clsRTTest.AddParameter("conf.level", nudCI.Value.ToString())
+        clsRTTest.AddParameter("mu", nudHypothesis.Value.ToString())
         If ucrExplanatory.strCurrDataType = "numeric" OrElse ucrExplanatory.strCurrDataType = "integer" Then 'is that the corerct way to write it - check
-            clsTwoVarModel.AddParameter("x", clsRFunctionParameter:=ucrResponse.GetVariables())
-            clsTwoVarModel.AddParameter("y", clsRFunctionParameter:=ucrExplanatory.GetVariables())
+            clsRTTest.AddParameter("x", clsRFunctionParameter:=ucrResponse.GetVariables())
+            clsRTTest.AddParameter("y", clsRFunctionParameter:=ucrExplanatory.GetVariables())
         Else
             clsModel.SetOperation("~")
             clsModel.SetParameter(True, clsRFunc:=ucrResponse.GetVariables())
             clsModel.SetParameter(False, clsRFunc:=ucrExplanatory.GetVariables())
-            clsTwoVarModel.AddParameter("x", clsROperatorParameter:=clsModel)
-            clsTwoVarModel.AddParameter("y", clsROperatorParameter:=clsModel)
+            clsRTTest.AddParameter("x", clsROperatorParameter:=clsModel)
+            clsRTTest.AddParameter("y", clsROperatorParameter:=clsModel)
         End If
 
         If chkPaired.Checked Then
-            clsTwoVarModel.AddParameter("paired", "TRUE")
+            clsRTTest.AddParameter("paired", "TRUE")
         Else
-            clsTwoVarModel.AddParameter("paired", "FALSE")
+            clsRTTest.RemoveParameterByName("paired")
         End If
     End Sub
 
     Private Sub SetBinomTest()
-        clsTwoVarModel.ClearParameters()
         ' only variables the response receiver can accept is numerical
         ' only variables the explanatory receiver can accept is factor
-        clsTwoVarModel.SetRCommand("prop.test")
-        clsTwoVarModel.AddParameter("conf.level", nudCI.Value.ToString())
-        clsTwoVarModel.AddParameter("p", nudHypothesis.Value.ToString())
+        clsRBinomial.SetRCommand("prop.test")
+        clsRBinomial.AddParameter("conf.level", nudCI.Value.ToString())
+        clsRBinomial.AddParameter("p", nudHypothesis.Value.ToString())
         clsModel.SetParameter(True, clsRFunc:=ucrResponse.GetVariables())
         clsModel.SetParameter(False, clsRFunc:=ucrExplanatory.GetVariables())
-        clsTwoVarModel.AddParameter("x", clsROperatorParameter:=clsModel)
-        clsTwoVarModel.AddParameter("y", clsROperatorParameter:=clsModel)
+        clsRBinomial.AddParameter("x", clsROperatorParameter:=clsModel)
+        clsRBinomial.AddParameter("y", clsROperatorParameter:=clsModel)
     End Sub
 
     Private Sub SetPoissonTest()
-        clsTwoVarModel.ClearParameters()
-        ' can accept any variables in any receiver
+        ' can accept only numerical in both receivers
 
-        clsTwoVarModel.SetRCommand("poisson.test")
-        clsTwoVarModel.AddParameter("conf.level", nudCI.Value.ToString())
-        clsTwoVarModel.AddParameter("r", nudHypothesis.Value.ToString())
-        clsRCount.SetRCommand("count")
-        If chkModifyPoisson1.Checked Then
-            If chkModifyPoisson2.Checked Then
-                '                clsTwoVarModel.AddParameter("x", "c(" & clsRCount & "," & clsRCount2 & ")")
-                clsRCount.AddParameter("x", clsROperatorParameter:=clsFunctionOperation)
-                clsFunctionOperation.SetOperation("==")
-                clsFunctionOperation.SetParameter(True, clsRFunc:=ucrResponse.GetVariables())
-                clsFunctionOperation.SetParameter(False, strValue:=Chr(34) & ucrTextforX.GetText.ToString & Chr(34))
-                clsRCount2.AddParameter("x", clsROperatorParameter:=clsFunctionOperation)
-                clsFunctionOperation.SetParameter(True, clsRFunc:=ucrExplanatory.GetVariables())
-                clsFunctionOperation.SetParameter(False, strValue:=Chr(34) & ucrTextforY.GetText.ToString & Chr(34))
-            ElseIf Not chkModifyPoisson2.Checked Then
-                ' receiver is altered, explanatory is not
-                '               clsTwoVarModel.AddParameter("x", "c(" & clsRCount & "," & clsRCount2 & ")")
-                clsRCount.AddParameter("x", clsROperatorParameter:=clsFunctionOperation)
-                clsFunctionOperation.SetOperation("==")
-                clsFunctionOperation.SetParameter(True, clsRFunc:=ucrResponse.GetVariables())
-                clsFunctionOperation.SetParameter(False, strValue:=Chr(34) & ucrTextforX.GetText.ToString & Chr(34))
-                clsRCount2.AddParameter("x", clsRFunctionParameter:=ucrExplanatory.GetVariables())
-            End If
-        Else
-            If chkModifyPoisson2.Checked Then
-                ' modify receiever is not altered but modify explanaatory is
-                '              clsTwoVarModel.AddParameter("x", "c(" & clsRCount & "," & clsRCount2 & ")")
-                clsRCount.AddParameter("x", clsRFunctionParameter:=ucrResponse.GetVariables())
-                clsFunctionOperation.SetOperation("==")
-                clsRCount2.AddParameter("x", clsROperatorParameter:=clsFunctionOperation)
-                clsFunctionOperation.SetParameter(True, clsRFunc:=ucrExplanatory.GetVariables())
-                clsFunctionOperation.SetParameter(False, strValue:=Chr(34) & ucrTextforY.GetText.ToString & Chr(34))
-            ElseIf Not chkModifyPoisson2.Checked Then
-                ' neither is altered, so we just have our values from the receiver
-                '             clsTwoVarModel.AddParameter("x", "c(" & clsRCount & "," & clsRCount2 & ")")
-                clsRCount.AddParameter("x", clsRFunctionParameter:=ucrResponse.GetVariables())
-                clsRCount2.AddParameter("x", clsRFunctionParameter:=ucrExplanatory.GetVariables())
-            End If
-        End If
+        clsRPoisson.SetRCommand("poisson.test")
+        clsRPoisson.AddParameter("conf.level", nudCI.Value.ToString())
+        clsRPoisson.AddParameter("r", nudHypothesis.Value.ToString())
+
+        clsRLength.SetRCommand("length")
+        clsRLength.AddParameter("x", clsRFunctionParameter:=ucrResponse.GetVariables())
+        clsRLength2.SetRCommand("length")
+        clsRLength2.AddParameter("x", clsRFunctionParameter:=ucrExplanatory.GetVariables())
+
+        clsRMean.SetRCommand("mean")
+        clsRMean.AddParameter("x", clsRFunctionParameter:=ucrResponse.GetVariables())
+        clsRMean2.SetRCommand("mean")
+        clsRMean2.AddParameter("x", clsRFunctionParameter:=ucrExplanatory.GetVariables())
+
+        '        clsRPoisson.AddParameter("x", "c(" & clsRLength & "," & clsRLength2 & ")")
+        '        clsRPoisson.AddParameter("T", "c(" & clsRMean & "," & clsRMean2 & ")")
     End Sub
 
     Private Sub SetRCode()
-        If rdoSpecific.Checked Then
+        clsRPoisson.ClearParameters()
+        clsRBinomial.ClearParameters()
+        clsRTTest.ClearParameters()
+        clsTwoVarModel.ClearParameters()
+        If rdoGeneral.Checked Then
+            SimpleRegression()
+        ElseIf rdoSpecific.Checked Then
             If ucrFamily.clsCurrDistribution.strNameTag = "Normal" Then
                 SetTTest()
             ElseIf ucrFamily.clsCurrDistribution.strNameTag = "Poisson" Then
@@ -207,11 +185,8 @@ Public Class dlgRegressionSimple
             ElseIf ucrFamily.clsCurrDistribution.strNameTag = "Binomial" Then
                 SetBinomTest()
             End If
-
-            'elseif rdoGeneral.checked
         End If
     End Sub
-
 
     Private Sub TestOKEnabled()
         If Not ucrResponse.IsEmpty() AndAlso Not ucrExplanatory.IsEmpty() AndAlso ucrFamily.Enabled AndAlso (chkSaveModel.Checked AndAlso Not ucrModelName.IsEmpty() OrElse Not chkSaveModel.Checked) Then
@@ -268,8 +243,7 @@ Public Class dlgRegressionSimple
         '        If rdoGeneral.Checked Then
         '        ucrFamily.SetGLMDistributions()
         '        Else
-        '        ucrFamily.bDistributionsSet = True
-        ' set only normal, poisson and binomial
+        '        only normal, poisson and binomial
         '       End If
     End Sub
 
@@ -349,7 +323,6 @@ Public Class dlgRegressionSimple
         sdgModelOptions.RestrictLink()
         DistributionsOffered()
         SimpleRegression()
-        PoissonChange()
     End Sub
 
     Private Sub cmdModelOptions_Click(sender As Object, e As EventArgs) Handles cmdModelOptions.Click
@@ -361,14 +334,6 @@ Public Class dlgRegressionSimple
     Private Sub chkConvertToVariate_CheckedChanged_1(sender As Object, e As EventArgs)
         ResponseConvert()
         TestOKEnabled()
-    End Sub
-
-    Private Sub ucrExplanatory_SelectionChanged(sender As Object, e As EventArgs) Handles ucrExplanatory.SelectionChanged
-
-    End Sub
-
-    Private Sub ucrResponse_SelectionChanged(sender As Object, e As EventArgs) Handles ucrResponse.SelectionChanged
-
     End Sub
 
     Private Sub chkFunction_CheckedChanged(sender As Object, e As EventArgs) Handles chkFunction.CheckedChanged
@@ -415,11 +380,12 @@ Public Class dlgRegressionSimple
                 lblMean.Visible = True
                 lblProbability.Visible = False
                 lblRate.Visible = False
-                chkPaired.Visible = True
                 nudHypothesis.Increment = 1
                 nudHypothesis.Maximum = Integer.MaxValue
                 nudHypothesis.Minimum = Integer.MinValue
                 nudHypothesis.Value = 0
+                '                If ucrExplanatory. GetVariables Is From same dataset
+                chkPaired.Visible = True
             ElseIf ucrFamily.clsCurrDistribution.strNameTag = "Poisson" Then
                 lblMean.Visible = False
                 lblProbability.Visible = False
@@ -459,42 +425,9 @@ Public Class dlgRegressionSimple
 
     Private Sub rdoTop_CheckedChanged(sender As Object, e As EventArgs) Handles rdoGeneral.CheckedChanged, rdoSpecific.CheckedChanged
         Display()
+        SetRCode()
         ResponseConvert()
         ExplanatoryFunctionSelect()
         DistributionsOffered()
-    End Sub
-
-    Private Sub PoissonChange()
-        If rdoSpecific.Checked AndAlso ucrFamily.clsCurrDistribution.strNameTag = "Poisson" Then
-            chkModifyPoisson1.Visible = True
-            chkModifyPoisson2.Visible = True
-            If chkModifyPoisson1.Checked Then
-                lblX.Visible = True
-                ucrTextforX.Visible = True
-            Else
-                lblX.Visible = False
-                ucrTextforX.Visible = False
-            End If
-            If chkModifyPoisson2.Checked Then
-                lblY.visible = True
-                ucrTextforY.Visible = True
-            Else
-                lblY.Visible = False
-                ucrTextforY.Visible = False
-            End If
-        Else
-            chkModifyPoisson1.Visible = False
-            chkModifyPoisson2.Visible = False
-            lblX.Visible = False
-            lblY.Visible = False
-            ucrTextforX.Visible = False
-            ucrTextforY.Visible = False
-        End If
-        Display()
-    End Sub
-
-    Private Sub PoissonObjects(sender As Object, e As EventArgs) Handles chkModifyPoisson1.VisibleChanged, chkModifyPoisson2.VisibleChanged, lblX.VisibleChanged, lblY.VisibleChanged, ucrTextforX.VisibleChanged, ucrTextforX.VisibleChanged
-        PoissonChange()
-        SetPoissonTest()
     End Sub
 End Class
