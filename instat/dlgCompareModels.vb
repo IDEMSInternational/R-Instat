2b9c95e8d72ac07166877ecba880166bb7c1b44d
Lunalo <johnlunalo95@gmail.com>
2016-11-04 09:27:32 +0300

    Adding functionalities

--- instat/dlgCompareModels.vb
+++ instat/dlgCompareModels.vb
@@ -15,6 +15,9 @@
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 Imports instat.Translations
 Public Class dlgCompareModels
+    Public lstCheckboxes As List(Of CheckBox)
+    Public lstVals As List(Of String)
+
     Public bFirstLoad As Boolean = True
     Private Sub dlgCompareModels_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         autoTranslate(Me)
@@ -65,4 +68,55 @@ Public Class dlgCompareModels
     Private Sub cmdAddNewDistributions_Click(sender As Object, e As EventArgs) Handles cmdAddNewDistributions.Click
         sdgAddNewDistribution.ShowDialog()
     End Sub
+
+    Private Sub nudXlimMin_ValueChanged(sender As Object, e As EventArgs) Handles nudXlimMin.ValueChanged, nudYlimMin.ValueChanged, nudNumberofColumns.ValueChanged, nudXlimMax.ValueChanged, nudYlimMax.ValueChanged
+        XLimits()
+        Ylimits()
+    End Sub
+
+    Private Sub grpPlotOptions_CheckedChanged(sender As Object, e As EventArgs) Handles chkCDF.CheckedChanged, chkDensity.CheckedChanged, chkHistogram.CheckedChanged
+        kindParameters()
+    End Sub
+    Private Sub Ylimits()
+        If nudYlimMax.Text <> "" AndAlso nudYlimMin.Text <> "" Then
+            ucrBase.clsRsyntax.AddParameter("ylim", "c(" & nudYlimMin.Text & "," & nudYlimMax.Text & ")")
+        Else
+            ucrBase.clsRsyntax.RemoveParameter("ylim")
+        End If
+    End Sub
+    Private Sub XLimits()
+        If nudXlimMax.Text <> "" AndAlso nudXlimMin.Text <> "" Then
+            ucrBase.clsRsyntax.AddParameter("xlim", "c(" & nudXlimMin.Text & "," & nudXlimMax.Text & ")")
+        Else
+            ucrBase.clsRsyntax.RemoveParameter("xlim")
+        End If
+    End Sub
+
+    Private Sub kindParameters()
+        lstCheckboxes.AddRange({chkCDF, chkDensity, chkqq, chkHistogram})
+        lstVals.AddRange({"cdf", "density", "qq", "histogram"})
+        Dim ival As Integer
+        Dim icheckedVal As Integer = lstCheckboxes(ival).Checked
+        Dim strTemp As String = ""
+
+        If lstCheckboxes(0).Checked Then
+            ucrBase.clsRsyntax.AddParameter("kind", lstVals(0))
+        ElseIf lstCheckboxes(1).Checked Then
+            ucrBase.clsRsyntax.AddParameter("kind", lstVals(1))
+        ElseIf lstCheckboxes(2).Checked = True Then
+            ucrBase.clsRsyntax.AddParameter("kind", lstVals(2))
+        ElseIf lstCheckboxes(3).Checked = True Then
+            ucrBase.clsRsyntax.AddParameter("kind", lstVals(3))
+        ElseIf icheckedVal > 1 Then
+            strTemp = "c" & "("
+            For icheckedVal = 0 To lstCheckboxes.Count - 1
+                If icheckedVal > 1 Then
+                    strTemp = strTemp & ","
+                End If
+            Next
+
+            strTemp = strTemp & ")"
+        End If
+    End Sub
+
 End Class
