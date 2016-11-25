12d0da2660d358e02849e788f0cbe4ca7353a133
Danny Parsons <danny@aims.ac.za>
2016-11-25 22:09:20 +0000

    fixing bugs in summaries and linking

--- instat/static/InstatObject/R/Backend_Components/link.R
+++ instat/static/InstatObject/R/Backend_Components/link.R
@@ -189,6 +189,38 @@ instat_object$set("public", "get_possible_linked_to_defintion", function(from_da
 }
 )
 
+instat_object$set("public", "get_equivalent_columns", function(from_data_name, columns, to_data_name) {
+  equivalent_columns <- self$link_between_containing(from_data_name, columns, to_data_name)
+  if(length(equivalent_columns) != 0) return(equivalent_columns)
+  else {
+    prev_data_links <- list(list(from_data_name, columns))
+    continue <- TRUE
+    while(continue) {
+      curr_data_links <- prev_data_links
+      curr_data_names <- sapply(curr_data_links, function(x) x[[1]])
+      for(temp_data_name in self$get_data_names()) {
+        i = 1
+        for(curr_from_data_frame in curr_data_names) {
+          curr_link_cols <- self$link_between_containing(curr_from_data_frame, curr_data_links[[i]][[2]], temp_data_name)
+          if(length(curr_link_cols) != 0) {
+            if(temp_data_name == to_data_name) {
+              return(curr_link_cols)
+            }
+            else if(!(temp_data_name %in% sapply(curr_data_links, function(x) x[[1]]))) {
+              curr_data_links[[length(curr_data_links) + 1]] <- list(temp_data_name, curr_link_cols)
+            }
+          }
+          i = i + 1
+        }
+      }
+      if(length(prev_data_links) == length(curr_data_links)) continue <- FALSE
+      else prev_data_links <- curr_data_links
+    }
+    return(c())
+  }
+}
+)
+
 instat_object$set("public", "link_exists_between", function(from_data_frame, to_data_frame) {
   return(any(sapply(private$.links, function(link) link$from_data_frame == from_data_frame && link$to_data_frame == to_data_frame))
          || any(sapply(private$.links, function(link) link$from_data_frame == to_data_frame && link$to_data_frame == from_data_frame)))
@@ -201,14 +233,22 @@ instat_object$set("public", "link_between_containing", function(from_data_frame,
     for(curr_link_pairs in curr_link$link_columns) {
       if(curr_link$from_data_frame == from_data_frame) {
         if(all(containing_columns %in% names(curr_link_pairs))) {
-          ind <- which(names(curr_link_pairs) %in% containing_columns)
-          return(as.vector(curr_link_pairs[ind]))
+          out <- c()
+          for(col in containing_columns) {
+            ind <- which(names(curr_link_pairs) == col)
+            out <- c(out, curr_link_pairs[[ind]])
+          }
+          return(out)
         }
       }
       else {
         if(all(containing_columns %in% curr_link_pairs)) {
-          ind <- which(curr_link_pairs %in% containing_columns)
-          return(names(curr_link_pairs)[ind])
+          out <- c()
+          for(col in containing_columns) {
+            ind <- which(curr_link_pairs == col)
+            out <- c(out, names(curr_link_pairs)[ind])
+          }
+          return(out)
         }
       }
     }
