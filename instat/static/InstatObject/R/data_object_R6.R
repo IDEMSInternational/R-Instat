6a7541ab35765e6cbcdb97ed628fdfffd85ac98b
Lunalo <johnlunalo95@gmail.com>
2016-11-29 10:53:15 +0300

    Improvement on split_date method

--- instat/static/InstatObject/R/data_object_R6.R
+++ instat/static/InstatObject/R/data_object_R6.R
@@ -1717,7 +1717,8 @@ data_object$set("public","set_contrasts_of_factor", function(col_name, new_contr
        contrasts(private$data[[col_name]]) <- new_contrasts
   }
 )
-data_object$set("public","split_date", function(data_name, col_name = "", week = FALSE, month = FALSE, year = FALSE, day = FALSE) {
+
+data_object$set("public","split_date", function(data_name, col_name = "", week = FALSE, month_val = FALSE, month_abbr = FALSE, month_name = FALSE, weekday_val = FALSE, weekday_abbr = FALSE, weekday_name = FALSE, year = FALSE, day = FALSE) {
   col_data <- self$get_columns_from_data(col_name, use_current_filter = FALSE)
   if(!is.Date(col_data)) stop("This column must be a date or time!")
   if(week) {
@@ -1725,10 +1726,35 @@ data_object$set("public","split_date", function(data_name, col_name = "", week =
 	  col_name <- next_default_item(prefix = "week", existing_names = self$get_column_names(), include_index = FALSE)
     self$add_columns_to_data(col_name = col_name, col_data = week)
   }
-  if(month) {
-    month <- month(col_data)
-	  col_name <- next_default_item(prefix = "month", existing_names = self$get_column_names(), include_index = FALSE)
-    self$add_columns_to_data(col_name = col_name, col_data = month)
+  if(month_val) {
+    month_val <- month(col_data)
+	  col_name <- next_default_item(prefix = "month_val", existing_names = self$get_column_names(), include_index = FALSE)
+    self$add_columns_to_data(col_name = col_name, col_data = month_val)
+  }
+   if(month_abbr) {
+    month_abbr <- month(col_data, label = TRUE)
+	  col_name <- next_default_item(prefix = "month_abbr", existing_names = self$get_column_names(), include_index = FALSE)
+    self$add_columns_to_data(col_name = col_name, col_data = month_abbr)
+  }
+   if(month_name) {
+    month_name <- month(col_data, label = TRUE, abbr = FALSE)
+	  col_name <- next_default_item(prefix = "month_name", existing_names = self$get_column_names(), include_index = FALSE)
+    self$add_columns_to_data(col_name = col_name, col_data = month_name)
+  }
+   if(weekday_val) {
+    weekday_val <- wday(col_data)
+	  col_name <- next_default_item(prefix = "weekday_val", existing_names = self$get_column_names(), include_index = FALSE)
+    self$add_columns_to_data(col_name = col_name, col_data = weekday_val)
+  }
+   if(weekday_abbr) {
+     weekday_abbr <- wday(col_data, label = TRUE)
+	  col_name <- next_default_item(prefix = "weekday_abbr", existing_names = self$get_column_names(), include_index = FALSE)
+    self$add_columns_to_data(col_name = col_name, col_data = weekday_abbr)
+  }
+   if(weekday_name) {
+     weekday_name <- wday(col_data, label = TRUE, abbr = FALSE)
+	  col_name <- next_default_item(prefix = "weekday_name", existing_names = self$get_column_names(), include_index = FALSE)
+    self$add_columns_to_data(col_name = col_name, col_data = weekday_name)
   }
   if(year) {
     year <- year(col_data)
