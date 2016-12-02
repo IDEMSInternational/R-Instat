a272c9b08d853496d4a1010f15f049e9d22a3528
steven ndung'u <stevenndungu4@outlook.com>
2016-12-02 15:07:56 +0300

    Define Climatic Data Dialogue

--- instat/DlgDefineClimaticData.vb
+++ instat/DlgDefineClimaticData.vb
@@ -1,3 +1,65 @@
-﻿Public Class DlgDefineClimaticData
+﻿' Instat-R
+' Copyright (C) 2015
+'
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+'
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License k
+' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+Imports instat.Translations
+Public Class DlgDefineClimaticData
+    Public bFirstLoad As Boolean = True
+    Private Sub DlgDefineClimaticData_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If bFirstLoad Then
+            InitialiseDialog()
+            SetDefaults()
+            bFirstLoad = False
+        End If
+        TestOKEnabled()
+        autoTranslate(Me)
+    End Sub
+
+    Private Sub InitialiseDialog()
+        ucrReceiverDate.Selector = ucrSelectorDefineClimaticData
+        ucrReceiverRain.Selector = ucrSelectorDefineClimaticData
+        ucrReceiverMaxTemp.Selector = ucrSelectorDefineClimaticData
+        ucrReceiverMinTemp.Selector = ucrSelectorDefineClimaticData
+        ucrReceiverSunshine.Selector = ucrSelectorDefineClimaticData
+        ucrReceiverWind.Selector = ucrSelectorDefineClimaticData
+
+    End Sub
+
+    Private Sub SetDefaults()
+        ucrReceiverDate.SetMeAsReceiver()
+        ucrSelectorDefineClimaticData.Reset()
+        TestOKEnabled()
+    End Sub
+
+    Private Sub TestOKEnabled()
+        If Not ucrReceiverDate.IsEmpty Then
+            ucrBase.OKEnabled(True)
+        Else
+            ucrBase.OKEnabled(False)
+        End If
+    End Sub
+
+    Private Sub ucrBase_ClickReset(sender As Object, e As EventArgs) Handles ucrBase.ClickReset
+        SetDefaults()
+    End Sub
+
+    Private Sub ucrReceiverDate_SelectionChanged(sender As Object, e As EventArgs) Handles ucrReceiverDate.SelectionChanged, ucrReceiverMaxTemp.SelectionChanged, ucrReceiverMinTemp.SelectionChanged, ucrReceiverRain.SelectionChanged, ucrReceiverSunshine.SelectionChanged, ucrReceiverWind.SelectionChanged
+        TestOKEnabled()
+    End Sub
+
+    Private Sub ucrSelectorDefineClimaticData_DataFrameChanged() Handles ucrSelectorDefineClimaticData.DataFrameChanged
+
+    End Sub
 End Class
