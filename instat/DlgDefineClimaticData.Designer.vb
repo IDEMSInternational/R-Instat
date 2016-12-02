bbf0f8c6f42ed40de02dc670a456c1b3c4995ab3
steven ndung'u <stevenndungu4@outlook.com>
2016-12-02 15:07:56 +0300

    Define Climatic Data Dialogue

--- instat/DlgDefineClimaticData.Designer.vb
+++ instat/DlgDefineClimaticData.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class DlgDefineClimaticData
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,15 +20,168 @@ Partial Class DlgDefineClimaticData
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.ucrSelectorDefineClimaticData = New instat.ucrSelectorByDataFrameAddRemove()
+        Me.ucrReceiverDate = New instat.ucrReceiverSingle()
+        Me.ucrReceiverWind = New instat.ucrReceiverSingle()
+        Me.ucrReceiverMaxTemp = New instat.ucrReceiverSingle()
+        Me.ucrReceiverMinTemp = New instat.ucrReceiverSingle()
+        Me.ucrReceiverRain = New instat.ucrReceiverSingle()
+        Me.lblDate = New System.Windows.Forms.Label()
+        Me.lblMaxTemp = New System.Windows.Forms.Label()
+        Me.lblMinTemp = New System.Windows.Forms.Label()
+        Me.lblRain = New System.Windows.Forms.Label()
+        Me.lblWind = New System.Windows.Forms.Label()
+        Me.lblSunshine = New System.Windows.Forms.Label()
+        Me.ucrReceiverSunshine = New instat.ucrReceiverSingle()
+        Me.ucrBase = New instat.ucrButtons()
         Me.SuspendLayout()
+        '
+        'ucrSelectorDefineClimaticData
+        '
+        Me.ucrSelectorDefineClimaticData.bShowHiddenColumns = False
+        Me.ucrSelectorDefineClimaticData.bUseCurrentFilter = True
+        Me.ucrSelectorDefineClimaticData.Location = New System.Drawing.Point(9, 9)
+        Me.ucrSelectorDefineClimaticData.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrSelectorDefineClimaticData.Name = "ucrSelectorDefineClimaticData"
+        Me.ucrSelectorDefineClimaticData.Size = New System.Drawing.Size(210, 180)
+        Me.ucrSelectorDefineClimaticData.TabIndex = 0
+        '
+        'ucrReceiverDate
+        '
+        Me.ucrReceiverDate.Location = New System.Drawing.Point(265, 28)
+        Me.ucrReceiverDate.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverDate.Name = "ucrReceiverDate"
+        Me.ucrReceiverDate.Selector = Nothing
+        Me.ucrReceiverDate.Size = New System.Drawing.Size(120, 20)
+        Me.ucrReceiverDate.TabIndex = 1
+        '
+        'ucrReceiverWind
+        '
+        Me.ucrReceiverWind.Location = New System.Drawing.Point(265, 208)
+        Me.ucrReceiverWind.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverWind.Name = "ucrReceiverWind"
+        Me.ucrReceiverWind.Selector = Nothing
+        Me.ucrReceiverWind.Size = New System.Drawing.Size(120, 20)
+        Me.ucrReceiverWind.TabIndex = 2
+        '
+        'ucrReceiverMaxTemp
+        '
+        Me.ucrReceiverMaxTemp.Location = New System.Drawing.Point(265, 163)
+        Me.ucrReceiverMaxTemp.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverMaxTemp.Name = "ucrReceiverMaxTemp"
+        Me.ucrReceiverMaxTemp.Selector = Nothing
+        Me.ucrReceiverMaxTemp.Size = New System.Drawing.Size(120, 20)
+        Me.ucrReceiverMaxTemp.TabIndex = 3
+        '
+        'ucrReceiverMinTemp
+        '
+        Me.ucrReceiverMinTemp.Location = New System.Drawing.Point(265, 118)
+        Me.ucrReceiverMinTemp.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverMinTemp.Name = "ucrReceiverMinTemp"
+        Me.ucrReceiverMinTemp.Selector = Nothing
+        Me.ucrReceiverMinTemp.Size = New System.Drawing.Size(120, 20)
+        Me.ucrReceiverMinTemp.TabIndex = 4
+        '
+        'ucrReceiverRain
+        '
+        Me.ucrReceiverRain.Location = New System.Drawing.Point(265, 73)
+        Me.ucrReceiverRain.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverRain.Name = "ucrReceiverRain"
+        Me.ucrReceiverRain.Selector = Nothing
+        Me.ucrReceiverRain.Size = New System.Drawing.Size(120, 20)
+        Me.ucrReceiverRain.TabIndex = 5
+        '
+        'lblDate
+        '
+        Me.lblDate.AutoSize = True
+        Me.lblDate.Location = New System.Drawing.Point(265, 9)
+        Me.lblDate.Name = "lblDate"
+        Me.lblDate.Size = New System.Drawing.Size(33, 13)
+        Me.lblDate.TabIndex = 6
+        Me.lblDate.Text = "Date:"
+        '
+        'lblMaxTemp
+        '
+        Me.lblMaxTemp.AutoSize = True
+        Me.lblMaxTemp.Location = New System.Drawing.Point(265, 144)
+        Me.lblMaxTemp.Name = "lblMaxTemp"
+        Me.lblMaxTemp.Size = New System.Drawing.Size(117, 13)
+        Me.lblMaxTemp.TabIndex = 7
+        Me.lblMaxTemp.Text = "Temperature Maximum:"
+        '
+        'lblMinTemp
+        '
+        Me.lblMinTemp.AutoSize = True
+        Me.lblMinTemp.Location = New System.Drawing.Point(265, 99)
+        Me.lblMinTemp.Name = "lblMinTemp"
+        Me.lblMinTemp.Size = New System.Drawing.Size(118, 13)
+        Me.lblMinTemp.TabIndex = 8
+        Me.lblMinTemp.Text = "Temperature Manimum:"
+        '
+        'lblRain
+        '
+        Me.lblRain.AutoSize = True
+        Me.lblRain.Location = New System.Drawing.Point(265, 54)
+        Me.lblRain.Name = "lblRain"
+        Me.lblRain.Size = New System.Drawing.Size(32, 13)
+        Me.lblRain.TabIndex = 9
+        Me.lblRain.Text = "Rain:"
+        '
+        'lblWind
+        '
+        Me.lblWind.AutoSize = True
+        Me.lblWind.Location = New System.Drawing.Point(265, 189)
+        Me.lblWind.Name = "lblWind"
+        Me.lblWind.Size = New System.Drawing.Size(35, 13)
+        Me.lblWind.TabIndex = 10
+        Me.lblWind.Text = "Wind:"
+        '
+        'lblSunshine
+        '
+        Me.lblSunshine.AutoSize = True
+        Me.lblSunshine.Location = New System.Drawing.Point(265, 234)
+        Me.lblSunshine.Name = "lblSunshine"
+        Me.lblSunshine.Size = New System.Drawing.Size(54, 13)
+        Me.lblSunshine.TabIndex = 12
+        Me.lblSunshine.Text = "Sunshine:"
+        '
+        'ucrReceiverSunshine
+        '
+        Me.ucrReceiverSunshine.Location = New System.Drawing.Point(265, 253)
+        Me.ucrReceiverSunshine.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverSunshine.Name = "ucrReceiverSunshine"
+        Me.ucrReceiverSunshine.Selector = Nothing
+        Me.ucrReceiverSunshine.Size = New System.Drawing.Size(120, 20)
+        Me.ucrReceiverSunshine.TabIndex = 11
+        '
+        'ucrBase
+        '
+        Me.ucrBase.Location = New System.Drawing.Point(9, 325)
+        Me.ucrBase.Name = "ucrBase"
+        Me.ucrBase.Size = New System.Drawing.Size(410, 52)
+        Me.ucrBase.TabIndex = 13
         '
         'DlgDefineClimaticData
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(418, 389)
+        Me.Controls.Add(Me.ucrBase)
+        Me.Controls.Add(Me.lblSunshine)
+        Me.Controls.Add(Me.ucrReceiverSunshine)
+        Me.Controls.Add(Me.lblWind)
+        Me.Controls.Add(Me.lblRain)
+        Me.Controls.Add(Me.lblMinTemp)
+        Me.Controls.Add(Me.lblMaxTemp)
+        Me.Controls.Add(Me.lblDate)
+        Me.Controls.Add(Me.ucrReceiverRain)
+        Me.Controls.Add(Me.ucrReceiverMinTemp)
+        Me.Controls.Add(Me.ucrReceiverMaxTemp)
+        Me.Controls.Add(Me.ucrReceiverWind)
+        Me.Controls.Add(Me.ucrReceiverDate)
+        Me.Controls.Add(Me.ucrSelectorDefineClimaticData)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
         Me.MaximizeBox = False
         Me.MinimizeBox = False
@@ -36,6 +189,24 @@ Partial Class DlgDefineClimaticData
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Define Climatic Data"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents ucrSelectorDefineClimaticData As ucrSelectorByDataFrameAddRemove
+    Friend WithEvents ucrReceiverDate As ucrReceiverSingle
+    Friend WithEvents ucrReceiverWind As ucrReceiverSingle
+    Friend WithEvents ucrReceiverMaxTemp As ucrReceiverSingle
+    Friend WithEvents ucrReceiverMinTemp As ucrReceiverSingle
+    Friend WithEvents ucrReceiverRain As ucrReceiverSingle
+    Friend WithEvents lblDate As Label
+    Friend WithEvents lblMaxTemp As Label
+    Friend WithEvents lblMinTemp As Label
+    Friend WithEvents lblRain As Label
+    Friend WithEvents lblWind As Label
+    Friend WithEvents lblSunshine As Label
+    Friend WithEvents ucrReceiverSunshine As ucrReceiverSingle
+    Friend WithEvents ucrBase As ucrButtons
+
+
 End Class
