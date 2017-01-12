5243dde9f8a2bbe40d6bae66dc99753ca8e4307e
Danny Parsons <danny@aims.ac.za>
2017-01-12 11:58:46 +0000

    added methods to graph display form

--- instat/frmGraphDisplay.vb
+++ instat/frmGraphDisplay.vb
@@ -2,4 +2,8 @@
     Private Sub frmGraphDisplay_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Public Sub SetImageFromFile(strFilePath As String)
+        pctGraph.Image = Image.FromFile(strFilePath)
+    End Sub
 End Class
