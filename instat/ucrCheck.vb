b964e8fea7acb933c184c6c99aad74138c80ef1d
Danny Parsons <danny@aims.ac.za>
2017-01-17 16:21:08 +0000

    updates on linking

--- instat/ucrCheck.vb
+++ instat/ucrCheck.vb
@@ -15,38 +15,38 @@
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 Public Class ucrCheck
-    'Is the checkbox linked to specific parameter values when checked/unchecked
-    Private bIsParameterValue As Boolean = True
+    ''Is the checkbox linked to specific parameter values when checked/unchecked
+    'Private bIsParameterValue As Boolean = True
 
     Private strValueIfChecked As String = "TRUE"
     Private strValueIfUnchecked As String = "FALSE"
 
-    'Is the checkbox linked to a parameter only by whether the parameter is present or not, irrespective of the parameter value
-    Private bIsParameterPresent As Boolean = False
+    ''Is the checkbox linked to a parameter only by whether the parameter is present or not, irrespective of the parameter value
+    'Private bIsParameterPresent As Boolean = False
 
-    'When bIsParameterPresent should the control be checked when the parameter is present (and unchecked when not present)
-    'If = False then the opposite.
-    Public bParameterIncludedWhenChecked As Boolean = True
+    ''When bIsParameterPresent should the control be checked when the parameter is present (and unchecked when not present)
+    ''If = False then the opposite.
+    'Private bParameterIncludedWhenChecked As Boolean = True
 
     Public Overrides Sub UpdateControl(clsRCodeObject As RCodeStructure)
 
         MyBase.UpdateControl(clsRCodeObject)
 
-        'TODO Add methods in RFunction/base class for RFunction/ROperator to do these checks better
         If clsParameter IsNot Nothing Then
-            If bIsParameterValue Then
+            If bChangeParameterValue Then
                 If clsParameter.strArgumentValue = strValueIfChecked OrElse clsParameter.strArgumentValue = strValueIfUnchecked Then
                     chkCheck.Checked = (clsParameter.strArgumentValue = strValueIfChecked)
                 Else
-                    MsgBox("Developer error: The value of parameter " & clsParameter.strArgumentName & ": " & clsParameter.strArgumentValue & " does not match strValueIfChecked or strValueIfUnchecked so cannot determine state for the checkbox. Setting as the default.")
+                    MsgBox("Developer error: The value of parameter " & clsParameter.strArgumentName & ": " & clsParameter.strArgumentValue & " does not match strValueIfChecked or strValueIfUnchecked so cannot determine state for the checkbox. Setting as the default instead.")
                     chkCheck.Checked = False
                 End If
-            ElseIf bIsParameterPresent Then
-                If clsRCodeObject.clsParameters.Contains(clsParameter) Then
-                    chkCheck.Checked = bParameterIncludedWhenChecked
-                Else
-                    chkCheck.Checked = Not bParameterIncludedWhenChecked
-                End If
+            ElseIf bAddRemoveParameter Then
+                'Commented out as not currently needed. Can be included if needed.
+                'If bParameterIncludedWhenChecked Then
+                chkCheck.Checked = clsRCodeObject.clsParameters.Contains(clsParameter)
+                'Else
+                'chkCheck.Checked = Not clsRCodeObject.clsParameters.Contains(clsParameter)
+                'End If
             End If
         End If
     End Sub
@@ -111,14 +111,7 @@ Public Class ucrCheck
         bIsParameterPresent = False
     End Sub
 
-    Public Overrides Sub SetLinkedControl(ucrNewLinkedControl As ucrCore)
-        MyBase.SetLinkedControl(ucrNewLinkedControl)
-        bIsParameterPresent = True
-        bIsParameterValue = False
-    End Sub
-
     Private Sub chkCheck_CheckedChanged(sender As Object, e As EventArgs) Handles chkCheck.CheckedChanged
-        OnControlContentsChanged()
         OnControlValueChanged()
     End Sub
 
