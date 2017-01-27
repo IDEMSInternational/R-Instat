3dd4059ae07ccf57335f10f2177e243acc7e838c
steven ndung'u <stevenndungu4@outlook.com>
2017-01-27 13:16:17 +0300

    Tyding up the code

--- instat/dlgRank.vb
+++ instat/dlgRank.vb
@@ -77,6 +77,10 @@ Public Class dlgRank
         End If
     End Sub
 
+    Private Sub ReopenDialog()
+
+    End Sub
+
     ' Sub that runs only the first time the dialog loads it sets default RFunction as the base function
     Private Sub SetDefaults()
         ucrBase.clsRsyntax.SetBaseRFunction(clsDefaultFunction.Clone())
@@ -86,55 +90,12 @@ Public Class dlgRank
         TestOKEnabled()
     End Sub
 
-    Private Sub ReopenDialog()
-
-    End Sub
-
     Private Sub Controls_ControlContentsChanged(ucrChangedControl As ucrCore) Handles ucrSelectorForRank.ControlContentsChanged, ucrReceiverRank.ControlContentsChanged, ucrSaveRank.ControlContentsChanged
         TestOKEnabled()
     End Sub
 
-    'Private Sub SetTiesValues()
-
-    '    If rdoAverage.Checked Then
-    '        If frmMain.clsInstatOptions.bIncludeRDefaultParameters Then
-    '            ucrBase.clsRsyntax.AddParameter("ties.method", Chr(34) & "average" & Chr(34))
-    '        Else
-    '            ucrBase.clsRsyntax.RemoveParameter("ties.method")
-    '        End If
-    '    ElseIf rdoMinimum.Checked Then
-    '        ucrBase.clsRsyntax.AddParameter("ties.method", Chr(34) & "min" & Chr(34))
-    '    ElseIf rdoMaximum.Checked Then
-    '        ucrBase.clsRsyntax.AddParameter("ties.method", Chr(34) & "max" & Chr(34))
-    '    ElseIf rdoFirst.Checked Then
-    '        ucrBase.clsRsyntax.AddParameter("ties.method", Chr(34) & "first" & Chr(34))
-    '    ElseIf rdoRandom.Checked Then
-    '        ucrBase.clsRsyntax.AddParameter("ties.method", Chr(34) & "random" & Chr(34))
-    '    Else
-    '        ucrBase.clsRsyntax.RemoveParameter("ties.method")
-    '    End If
-
-    'End Sub
-
-    'When the reset button is clicked, set the defaults again
     Private Sub ucrBase_ClickReset(sender As Object, e As EventArgs) Handles ucrBase.ClickReset
         SetDefaults()
     End Sub
-
-    'Private Sub setMissingValue()
-    '    If rdoKeptAsMissing.Checked Then
-    '        If frmMain.clsInstatOptions.bIncludeRDefaultParameters Then
-    '            ucrBase.clsRsyntax.AddParameter("na.last", Chr(34) & "keep" & Chr(34))
-    '        Else
-    '            ucrBase.clsRsyntax.RemoveParameter("na.last")
-    '        End If
-    '    ElseIf rdoFirstMissingValues.Checked Then
-    '        ucrBase.clsRsyntax.AddParameter("na.last", "FALSE")
-    '    ElseIf rdoLast.Checked Then
-    '        ucrBase.clsRsyntax.AddParameter("na.last", "TRUE")
-    '    Else
-    '        ucrBase.clsRsyntax.RemoveParameter("na.last")
-    '    End If
-    'End Sub
 
 End Class
