c546fee00a9ea886cc05c3319d377c595a7aa41c
Lily <Admin@NEW-KT5NM8N55N7>
2017-01-31 22:15:06 +0000

    Updated Code to new code

--- instat/dlgRenameSheet.vb
+++ instat/dlgRenameSheet.vb
@@ -18,18 +18,21 @@ Imports instat
 Imports instat.Translations
 Public Class dlgRenameSheet
     Public bFirstLoad As Boolean = True
+    Private bReset As Boolean = True
     Private clsDefaultFunction As New RFunction
     Private Sub dlgRenameSheet_Load(sender As Object, e As EventArgs) Handles Me.Load
-        autoTranslate(Me)
-
         If bFirstLoad Then
             InitialiseDialog()
-            SetDefaults()
             bFirstLoad = False
         Else
-            ReopenDialog()
+            ReopenDialog() ' Temp fix
         End If
-        TestOKEnabled()
+        If bReset Then
+            SetDefaults()
+        End If
+        SetRCodeForControls(bReset)
+        bReset = False
+        autoTranslate(Me)
     End Sub
 
     Private Sub ReopenDialog()
@@ -46,22 +49,26 @@ Public Class dlgRenameSheet
         ucrDataFrameToRename.SetParameter(New RParameter("data_name"))
         ucrDataFrameToRename.SetParameterIsString()
 
-        clsDefaultFunction.SetRCommand(frmMain.clsRLink.strInstatDataObject & "$rename_dataframe")
     End Sub
 
     ' check how changing dataframes affects it
 
     Private Sub SetDefaults()
+        clsDefaultFunction.SetRCommand(frmMain.clsRLink.strInstatDataObject & "$rename_dataframe")
         ucrBase.clsRsyntax.SetBaseRFunction(clsDefaultFunction.Clone())
-        SetRCode(Me, ucrBase.clsRsyntax.clsBaseFunction, True)
         ucrInputNewName.Reset()
         ucrDataFrameToRename.Reset()
         CheckAutoName()
+        TestOKEnabled()
+    End Sub
+
+    Private Sub SetRCodeforControls(bReset As Boolean)
+        SetRCode(Me, ucrBase.clsRsyntax.clsBaseFunction, bReset)
     End Sub
 
     Private Sub ucrBase_ClickReset(sender As Object, e As EventArgs) Handles ucrBase.ClickReset
         SetDefaults()
-        TestOKEnabled()
+        SetRCodeForControls(bReset)
     End Sub
 
     Private Sub TestOKEnabled()
