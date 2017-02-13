247e366b702972155d81376af4b2a86ee3e68bd8
john <johnlunalo95@gmail.com>
2017-02-13 14:38:12 +0300

    Editing methods to accept empty reciver with no columns.

--- instat/ucrReceiver.vb
+++ instat/ucrReceiver.vb
@@ -29,7 +29,7 @@ Public Class ucrReceiver
     Public bExcludeFromSelector As Boolean = False
     Public Event SelectionChanged(sender As Object, e As EventArgs)
     Public WithEvents frmParent As Form
-
+    Public bAddParameterIfEmpty As Boolean = False
     'If the control is used to set a parameter that is a string i.e. column = "ID"
     Private bParameterIsString As Boolean = False
     'If the control is used to set a parameter that is an RFunction i.e. x = InstatDataObject$get_columns_from_data()
@@ -386,6 +386,10 @@ Public Class ucrReceiver
     End Function
 
     Public Overrides Function IsRDefault() As Boolean
-        Return IsEmpty()
+        If bAddParameterIfEmpty Then
+            Return False
+        Else
+            Return IsEmpty()
+        End If
     End Function
 End Class
