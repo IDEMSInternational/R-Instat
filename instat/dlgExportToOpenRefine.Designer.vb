5220a9f88f487c1fcfeef0eaa6ad6ca03d5b75da
steven ndung'u <stevenndungu4@outlook.com>
2017-02-14 10:48:25 +0300

    DlgExportToRefine

--- instat/dlgExportToOpenRefine.Designer.vb
+++ instat/dlgExportToOpenRefine.Designer.vb
@@ -23,11 +23,10 @@ Partial Class dlgExportToOpenRefine
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.lblNameDataSet = New System.Windows.Forms.Label()
-        Me.chkRefineBrowser = New System.Windows.Forms.CheckBox()
         Me.ucrBase = New instat.ucrButtons()
         Me.ucrInputDatasetName = New instat.ucrInputTextBox()
         Me.ucrOpenRefineDataFrame = New instat.ucrDataFrame()
-        Me.UcrDialogDisabled1 = New instat.ucrDialogDisabled()
+        Me.ucrchkOpenBrowser = New instat.ucrCheck()
         Me.SuspendLayout()
         '
         'lblNameDataSet
@@ -39,16 +38,6 @@ Partial Class dlgExportToOpenRefine
         Me.lblNameDataSet.TabIndex = 8
         Me.lblNameDataSet.Text = "Dataset Name:"
         '
-        'chkRefineBrowser
-        '
-        Me.chkRefineBrowser.AutoSize = True
-        Me.chkRefineBrowser.Location = New System.Drawing.Point(10, 76)
-        Me.chkRefineBrowser.Name = "chkRefineBrowser"
-        Me.chkRefineBrowser.Size = New System.Drawing.Size(93, 17)
-        Me.chkRefineBrowser.TabIndex = 9
-        Me.chkRefineBrowser.Text = "Open Browser"
-        Me.chkRefineBrowser.UseVisualStyleBackColor = True
-        '
         'ucrBase
         '
         Me.ucrBase.Location = New System.Drawing.Point(10, 99)
@@ -58,6 +47,9 @@ Partial Class dlgExportToOpenRefine
         '
         'ucrInputDatasetName
         '
+        Me.ucrInputDatasetName.bAddRemoveParameter = True
+        Me.ucrInputDatasetName.bChangeParameterValue = True
+        Me.ucrInputDatasetName.IsMultiline = False
         Me.ucrInputDatasetName.IsReadOnly = False
         Me.ucrInputDatasetName.Location = New System.Drawing.Point(217, 29)
         Me.ucrInputDatasetName.Name = "ucrInputDatasetName"
@@ -66,6 +58,8 @@ Partial Class dlgExportToOpenRefine
         '
         'ucrOpenRefineDataFrame
         '
+        Me.ucrOpenRefineDataFrame.bAddRemoveParameter = True
+        Me.ucrOpenRefineDataFrame.bChangeParameterValue = True
         Me.ucrOpenRefineDataFrame.bUseCurrentFilter = True
         Me.ucrOpenRefineDataFrame.Location = New System.Drawing.Point(10, 10)
         Me.ucrOpenRefineDataFrame.Margin = New System.Windows.Forms.Padding(0)
@@ -73,21 +67,22 @@ Partial Class dlgExportToOpenRefine
         Me.ucrOpenRefineDataFrame.Size = New System.Drawing.Size(160, 40)
         Me.ucrOpenRefineDataFrame.TabIndex = 0
         '
-        'UcrDialogDisabled1
+        'ucrchkOpenBrowser
         '
-        Me.UcrDialogDisabled1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.UcrDialogDisabled1.Location = New System.Drawing.Point(91, 46)
-        Me.UcrDialogDisabled1.Name = "UcrDialogDisabled1"
-        Me.UcrDialogDisabled1.Size = New System.Drawing.Size(240, 50)
-        Me.UcrDialogDisabled1.TabIndex = 10
+        Me.ucrchkOpenBrowser.bAddRemoveParameter = True
+        Me.ucrchkOpenBrowser.bChangeParameterValue = True
+        Me.ucrchkOpenBrowser.Checked = False
+        Me.ucrchkOpenBrowser.Location = New System.Drawing.Point(13, 72)
+        Me.ucrchkOpenBrowser.Name = "ucrchkOpenBrowser"
+        Me.ucrchkOpenBrowser.Size = New System.Drawing.Size(100, 20)
+        Me.ucrchkOpenBrowser.TabIndex = 9
         '
         'dlgExportToOpenRefine
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(425, 162)
-        Me.Controls.Add(Me.UcrDialogDisabled1)
-        Me.Controls.Add(Me.chkRefineBrowser)
+        Me.Controls.Add(Me.ucrchkOpenBrowser)
         Me.Controls.Add(Me.lblNameDataSet)
         Me.Controls.Add(Me.ucrBase)
         Me.Controls.Add(Me.ucrInputDatasetName)
@@ -107,6 +102,5 @@ Partial Class dlgExportToOpenRefine
     Friend WithEvents ucrInputDatasetName As ucrInputTextBox
     Friend WithEvents ucrBase As ucrButtons
     Friend WithEvents lblNameDataSet As Label
-    Friend WithEvents chkRefineBrowser As CheckBox
-    Friend WithEvents UcrDialogDisabled1 As ucrDialogDisabled
+    Friend WithEvents ucrchkOpenBrowser As ucrCheck
 End Class
