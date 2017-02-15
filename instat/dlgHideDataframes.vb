3858a396b2953204a091d4b6228871bd2aa5ce4f
Lily <Admin@NEW-KT5NM8N55N7>
2017-02-15 12:11:42 +0000

    Update to branches

--- instat/dlgHideDataframes.vb
+++ instat/dlgHideDataframes.vb
@@ -13,7 +13,6 @@
 '
 ' You should have received a copy of the GNU General Public License k
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
-Imports instat
 Imports instat.Translations
 Public Class dlgHideDataframes
     Public bFirstLoad As Boolean = True
