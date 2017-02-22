07ed1d3f23cd60f2bd9deaa28dc955274c51eeed
Stephen Kogo <stevekogo@gmail.com>
2017-02-22 16:59:40 +0300

    minor edits

--- instat/dlgExportToCPT.vb
+++ instat/dlgExportToCPT.vb
@@ -14,7 +14,6 @@
 ' You should have received a copy of the GNU General Public License k
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 Imports System.IO
-Imports instat
 Imports instat.Translations
 Public Class dlgExportToCPT
     Dim bFirstLoad As Boolean = True
