6c4a748ff2eeccad6419f74d6eefd0e443a9092d
steven ndung'u <stevenndungu4@outlook.com>
2017-02-22 16:27:40 +0300

    changes on the dialogue

--- instat/dlgViewAndRemoveKeys.vb
+++ instat/dlgViewAndRemoveKeys.vb
@@ -14,10 +14,13 @@
 ' You should have received a copy of the GNU General Public License k
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+Imports instat
 Imports instat.Translations
 Public Class dlgViewAndRemoveKeys
-    Public bFirstLoad As Boolean = True
+    Private bFirstLoad As Boolean = True
     Private bReset As Boolean = True
+    Private clsGetKey As New RFunction
+    Private clsRemoveKey As New RFunction
     Private Sub dlgViewAndRemoveKeys_Load(sender As Object, e As EventArgs) Handles Me.Load
         autoTranslate(Me)
         If bFirstLoad Then
@@ -33,29 +36,32 @@ Public Class dlgViewAndRemoveKeys
         bReset = False
     End Sub
 
-    Private Sub ReopenDialog()
+    Private Sub ReopenDialog() 'Temporary fixes remove key error on reopen of the dialog
         ucrSelectorKeys.Reset()
     End Sub
 
     Private Sub SetDefaults()
-        Dim clsDefaultFunction As New RFunction
         'reset
         ucrSelectorKeys.Reset()
-        '  ucrChkRemoveKey.Checked = False
 
+        clsGetKey = New RFunction
+        clsRemoveKey = New RFunction
         ' Set default RFunction as the base function
-        clsDefaultFunction.SetRCommand(frmMain.clsRLink.strInstatDataObject & "$remove_key")
-        ucrBase.clsRsyntax.SetBaseRFunction(clsDefaultFunction.Clone())
+        clsGetKey.SetRCommand(frmMain.clsRLink.strInstatDataObject & "$get_keys")
+        clsRemoveKey.SetRCommand(frmMain.clsRLink.strInstatDataObject & "$remove_key")
+        ucrBase.clsRsyntax.SetBaseRFunction(clsGetKey)
+
     End Sub
 
-    Public Sub SetRCodeForControls(bReset As Boolean)
+    Private Sub SetRCodeForControls(bReset As Boolean)
         SetRCode(Me, ucrBase.clsRsyntax.clsBaseFunction, bReset)
+        ucrChkRemoveKey.SetRCode(ucrBase.clsRsyntax.clsBaseFunction, bReset)
     End Sub
 
     Private Sub InitialiseDialog()
 
         ucrBase.iHelpTopicID = 505
-        ucrChkRemoveKey.Checked = False
+        ucrBase.clsRsyntax.iCallType = 2
 
         'Setting Receiver
         ucrReceiverSelectedKey.Selector = ucrSelectorKeys
@@ -67,19 +73,21 @@ Public Class dlgViewAndRemoveKeys
         ucrSelectorKeys.SetParameter(New RParameter("data_name", 0))
         ucrSelectorKeys.SetParameterIsString()
 
+        ucrChkRemoveKey.AddFunctionNamesCondition(True, frmMain.clsRLink.strInstatDataObject & "$remove_key")
+        ucrChkRemoveKey.AddFunctionNamesCondition(False, frmMain.clsRLink.strInstatDataObject & "$get_keys")
         ucrChkRemoveKey.SetText("Remove Key")
 
     End Sub
 
     Private Sub TestOKEnabled()
-        If Not ucrReceiverSelectedKey.IsEmpty AndAlso ucrChkRemoveKey.Checked Then
+        If Not ucrReceiverSelectedKey.IsEmpty Then
             ucrBase.OKEnabled(True)
         Else
             ucrBase.OKEnabled(False)
         End If
     End Sub
 
-    Private Sub Controls_ControlContentsChanged(ucrChangedControl As ucrCore) Handles ucrReceiverSelectedKey.ControlContentsChanged, ucrChkRemoveKey.ControlContentsChanged
+    Private Sub Controls_ControlContentsChanged(ucrChangedControl As ucrCore) Handles ucrReceiverSelectedKey.ControlContentsChanged
         TestOKEnabled()
     End Sub
 
@@ -87,5 +95,18 @@ Public Class dlgViewAndRemoveKeys
         SetDefaults()
         SetRCodeForControls(True)
         TestOKEnabled()
+    End Sub
+
+    Private Sub ChangeBaseFunc()
+        If ucrChkRemoveKey.Checked Then
+            ucrBase.clsRsyntax.SetBaseRFunction(clsRemoveKey)
+        Else
+            ucrBase.clsRsyntax.SetBaseRFunction(clsGetKey)
+        End If
+        SetRCodeForControls(False)
+    End Sub
+
+    Private Sub ucrChkRemoveKey_ControlValueChanged(ucrChangedControl As ucrCore) Handles ucrChkRemoveKey.ControlValueChanged
+        ChangeBaseFunc()
     End Sub
 End Class
