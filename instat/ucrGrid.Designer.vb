f28d97ca5c66aae1e096ecc734c7857c3e5199fb
John Lunalo <johnlunalo95@gmail.com>
2017-02-23 17:17:41 +0300

    Basic Structure of ucrGrid

---
+++ instat/ucrGrid.Designer.vb
@@ -0,0 +1,57 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ucrGrid
+    Inherits instat.ucrCore
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.grdLayout = New unvell.ReoGrid.ReoGridControl()
+        Me.SuspendLayout()
+        '
+        'grdLayout
+        '
+        Me.grdLayout.BackColor = System.Drawing.Color.White
+        Me.grdLayout.ColumnHeaderContextMenuStrip = Nothing
+        Me.grdLayout.LeadHeaderContextMenuStrip = Nothing
+        Me.grdLayout.Location = New System.Drawing.Point(3, 3)
+        Me.grdLayout.Name = "grdLayout"
+        Me.grdLayout.RowHeaderContextMenuStrip = Nothing
+        Me.grdLayout.Script = Nothing
+        Me.grdLayout.SheetTabContextMenuStrip = Nothing
+        Me.grdLayout.SheetTabNewButtonVisible = True
+        Me.grdLayout.SheetTabVisible = True
+        Me.grdLayout.SheetTabWidth = 60
+        Me.grdLayout.Size = New System.Drawing.Size(289, 181)
+        Me.grdLayout.TabIndex = 6
+        Me.grdLayout.Text = "ReoGridControl1"
+        '
+        'ucrGrid
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.grdLayout)
+        Me.Name = "ucrGrid"
+        Me.Size = New System.Drawing.Size(295, 187)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents grdLayout As unvell.ReoGrid.ReoGridControl
+End Class
