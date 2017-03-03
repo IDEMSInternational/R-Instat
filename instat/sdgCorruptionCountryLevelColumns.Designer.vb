fa778e593c50afc84ade383f81fe3c94ee8e3c9f
Lily <Admin@NEW-KT5NM8N55N7>
2017-03-03 13:02:05 +0000

    Update dlgDefineCorruption

--- instat/sdgCorruptionCountryLevelColumns.Designer.vb
+++ instat/sdgCorruptionCountryLevelColumns.Designer.vb
@@ -22,13 +22,128 @@ Partial Class sdgCorruptionCountryLevelColumns
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.lblPPPConversionRate = New System.Windows.Forms.Label()
+        Me.ucrReceiverPPPConversionRate = New instat.ucrReceiverSingle()
+        Me.ucrReceiverForeignWinner = New instat.ucrReceiverSingle()
+        Me.lblForeignWinner = New System.Windows.Forms.Label()
+        Me.lblProcuringAuthorityID = New System.Windows.Forms.Label()
+        Me.ucrReceiverCountryISO = New instat.ucrReceiverSingle()
+        Me.lblCountryISO = New System.Windows.Forms.Label()
+        Me.ucrReceiverProcuringAuthorityID = New instat.ucrReceiverSingle()
+        Me.lblWinnerID = New System.Windows.Forms.Label()
+        Me.ucrReceiverWinnerID = New instat.ucrReceiverSingle()
         Me.SuspendLayout()
+        '
+        'lblPPPConversionRate
+        '
+        Me.lblPPPConversionRate.AutoSize = True
+        Me.lblPPPConversionRate.Location = New System.Drawing.Point(81, 199)
+        Me.lblPPPConversionRate.Name = "lblPPPConversionRate"
+        Me.lblPPPConversionRate.Size = New System.Drawing.Size(113, 13)
+        Me.lblPPPConversionRate.TabIndex = 34
+        Me.lblPPPConversionRate.Text = "PPP Conversion Rate:"
+        '
+        'ucrReceiverPPPConversionRate
+        '
+        Me.ucrReceiverPPPConversionRate.frmParent = Me
+        Me.ucrReceiverPPPConversionRate.Location = New System.Drawing.Point(81, 217)
+        Me.ucrReceiverPPPConversionRate.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverPPPConversionRate.Name = "ucrReceiverPPPConversionRate"
+        Me.ucrReceiverPPPConversionRate.Selector = Nothing
+        Me.ucrReceiverPPPConversionRate.Size = New System.Drawing.Size(120, 20)
+        Me.ucrReceiverPPPConversionRate.TabIndex = 35
+        '
+        'ucrReceiverForeignWinner
+        '
+        Me.ucrReceiverForeignWinner.frmParent = Me
+        Me.ucrReceiverForeignWinner.Location = New System.Drawing.Point(81, 174)
+        Me.ucrReceiverForeignWinner.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverForeignWinner.Name = "ucrReceiverForeignWinner"
+        Me.ucrReceiverForeignWinner.Selector = Nothing
+        Me.ucrReceiverForeignWinner.Size = New System.Drawing.Size(120, 20)
+        Me.ucrReceiverForeignWinner.TabIndex = 33
+        '
+        'lblForeignWinner
+        '
+        Me.lblForeignWinner.AutoSize = True
+        Me.lblForeignWinner.Location = New System.Drawing.Point(81, 156)
+        Me.lblForeignWinner.Name = "lblForeignWinner"
+        Me.lblForeignWinner.Size = New System.Drawing.Size(82, 13)
+        Me.lblForeignWinner.TabIndex = 32
+        Me.lblForeignWinner.Text = "Foreign Winner:"
+        '
+        'lblProcuringAuthorityID
+        '
+        Me.lblProcuringAuthorityID.AutoSize = True
+        Me.lblProcuringAuthorityID.Location = New System.Drawing.Point(83, 24)
+        Me.lblProcuringAuthorityID.Name = "lblProcuringAuthorityID"
+        Me.lblProcuringAuthorityID.Size = New System.Drawing.Size(113, 13)
+        Me.lblProcuringAuthorityID.TabIndex = 28
+        Me.lblProcuringAuthorityID.Text = "Procuring Authority ID:"
+        '
+        'ucrReceiverCountryISO
+        '
+        Me.ucrReceiverCountryISO.frmParent = Me
+        Me.ucrReceiverCountryISO.Location = New System.Drawing.Point(81, 131)
+        Me.ucrReceiverCountryISO.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverCountryISO.Name = "ucrReceiverCountryISO"
+        Me.ucrReceiverCountryISO.Selector = Nothing
+        Me.ucrReceiverCountryISO.Size = New System.Drawing.Size(120, 20)
+        Me.ucrReceiverCountryISO.TabIndex = 31
+        '
+        'lblCountryISO
+        '
+        Me.lblCountryISO.AutoSize = True
+        Me.lblCountryISO.Location = New System.Drawing.Point(81, 113)
+        Me.lblCountryISO.Name = "lblCountryISO"
+        Me.lblCountryISO.Size = New System.Drawing.Size(67, 13)
+        Me.lblCountryISO.TabIndex = 30
+        Me.lblCountryISO.Text = "Country ISO:"
+        '
+        'ucrReceiverProcuringAuthorityID
+        '
+        Me.ucrReceiverProcuringAuthorityID.frmParent = Me
+        Me.ucrReceiverProcuringAuthorityID.Location = New System.Drawing.Point(83, 42)
+        Me.ucrReceiverProcuringAuthorityID.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverProcuringAuthorityID.Name = "ucrReceiverProcuringAuthorityID"
+        Me.ucrReceiverProcuringAuthorityID.Selector = Nothing
+        Me.ucrReceiverProcuringAuthorityID.Size = New System.Drawing.Size(120, 20)
+        Me.ucrReceiverProcuringAuthorityID.TabIndex = 29
+        '
+        'lblWinnerID
+        '
+        Me.lblWinnerID.AutoSize = True
+        Me.lblWinnerID.Location = New System.Drawing.Point(81, 78)
+        Me.lblWinnerID.Name = "lblWinnerID"
+        Me.lblWinnerID.Size = New System.Drawing.Size(83, 13)
+        Me.lblWinnerID.TabIndex = 36
+        Me.lblWinnerID.Text = "Winner Country:"
+        '
+        'ucrReceiverWinnerID
+        '
+        Me.ucrReceiverWinnerID.frmParent = Me
+        Me.ucrReceiverWinnerID.Location = New System.Drawing.Point(81, 93)
+        Me.ucrReceiverWinnerID.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverWinnerID.Name = "ucrReceiverWinnerID"
+        Me.ucrReceiverWinnerID.Selector = Nothing
+        Me.ucrReceiverWinnerID.Size = New System.Drawing.Size(120, 20)
+        Me.ucrReceiverWinnerID.TabIndex = 37
         '
         'sdgCorruptionCountryLevelColumns
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.lblWinnerID)
+        Me.Controls.Add(Me.ucrReceiverWinnerID)
+        Me.Controls.Add(Me.lblPPPConversionRate)
+        Me.Controls.Add(Me.ucrReceiverPPPConversionRate)
+        Me.Controls.Add(Me.ucrReceiverForeignWinner)
+        Me.Controls.Add(Me.lblForeignWinner)
+        Me.Controls.Add(Me.lblProcuringAuthorityID)
+        Me.Controls.Add(Me.ucrReceiverCountryISO)
+        Me.Controls.Add(Me.lblCountryISO)
+        Me.Controls.Add(Me.ucrReceiverProcuringAuthorityID)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.MaximizeBox = False
         Me.MinimizeBox = False
@@ -36,6 +151,18 @@ Partial Class sdgCorruptionCountryLevelColumns
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
         Me.Text = "Country Level Columns"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents lblPPPConversionRate As Label
+    Friend WithEvents ucrReceiverPPPConversionRate As ucrReceiverSingle
+    Friend WithEvents ucrReceiverForeignWinner As ucrReceiverSingle
+    Friend WithEvents lblForeignWinner As Label
+    Friend WithEvents lblProcuringAuthorityID As Label
+    Friend WithEvents ucrReceiverCountryISO As ucrReceiverSingle
+    Friend WithEvents lblCountryISO As Label
+    Friend WithEvents ucrReceiverProcuringAuthorityID As ucrReceiverSingle
+    Friend WithEvents lblWinnerID As Label
+    Friend WithEvents ucrReceiverWinnerID As ucrReceiverSingle
 End Class
--- instat/sdgCorruptionCountryLevelColumns.vb
+++ instat/sdgCorruptionCountryLevelColumns.vb
@@ -1,3 +1,25 @@
 ﻿Public Class sdgCorruptionCountryLevelColumns
+    '           ucrReceiverProcuringAuthorityID.SetParameter(New RParameter("procuring_authority_id"))
+    '       ucrReceiverCountryISO.SetParameter(New RParameter("country_iso"))
+    '       ucrReceiverForeignWinner.SetParameter(New RParameter("foreign_winner"))
+    '       ucrReceiverPPPConversionRate.SetParameter(New RParameter("ppp_conversion_rate"))
+    '
+    ''
+    '   ucrReceiverWinnerID.SetParameter(New RParameter("winner_id"))
+
+
+    '        dctRecognisedCorruptionTypes.Add("procuring_authority_id", {"anb_id"})
+
+    '       dctRecognisedCorruptionTypes.Add()
+    '        dctRecognisedCorruptionTypes.Add()
+    '       dctRecognisedCorruptionTypes.Add()
+    '      dctRecognisedCorruptionTypes.Add("winner_id", {"w_id"})
+    '      dctRecognisedCorruptionTypes.Add()
+    ''     dctRecognisedCorruptionTypes.Add()
+    '   dctRecognisedCorruptionTypes.Add("procedure_type", {"ctf_methodtype"})
+    '  dctRecognisedCorruptionTypes.Add()
+    '' dctRecognisedCorruptionTypes.Add("country_iso", {"iso"})
+    'dctRecognisedCorruptionTypes.Add("foreign_winner", {"f_winner"})
+    'dctRecognisedCorruptionTypes.Add("ppp_conversion_rate", {"ppp"})
 
 End Class
