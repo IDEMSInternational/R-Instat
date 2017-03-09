f58b34acab636ade0a6f5b1c6d490db2e7cee106
steven ndung'u <stevenndungu4@outlook.com>
2017-03-09 19:28:38 +0300

    Setting up parameters.

--- instat/dlgOneWayFrequencies.Designer.vb
+++ instat/dlgOneWayFrequencies.Designer.vb
@@ -22,13 +22,6 @@ Partial Class dlgOneWayFrequencies
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.ucrSelectorOneWayFreq = New instat.ucrSelectorByDataFrameAddRemove()
-        Me.ucrReceiverOneWayFreq = New instat.ucrReceiverSingle()
-        Me.ucrSaveGraph = New instat.ucrSave()
-        Me.ucrBase = New instat.ucrButtons()
-        Me.ucrChkTable = New instat.ucrCheck()
-        Me.ucrChkweights = New instat.ucrCheck()
-        Me.ucrChkGraph = New instat.ucrCheck()
         Me.grpSort = New System.Windows.Forms.GroupBox()
         Me.rdoDescending = New System.Windows.Forms.RadioButton()
         Me.rdoAscending = New System.Windows.Forms.RadioButton()
@@ -37,66 +30,15 @@ Partial Class dlgOneWayFrequencies
         Me.cmdOptions = New System.Windows.Forms.Button()
         Me.lblSelectedVariable = New System.Windows.Forms.Label()
         Me.ucrChkFlip = New instat.ucrCheck()
+        Me.ucrChkGraph = New instat.ucrCheck()
+        Me.ucrChkTable = New instat.ucrCheck()
+        Me.ucrBase = New instat.ucrButtons()
+        Me.ucrSaveGraph = New instat.ucrSave()
+        Me.ucrReceiverOneWayFreq = New instat.ucrReceiverSingle()
+        Me.ucrSelectorOneWayFreq = New instat.ucrSelectorByDataFrameAddRemove()
+        Me.ucrChkweights = New instat.ucrCheck()
         Me.grpSort.SuspendLayout()
         Me.SuspendLayout()
-        '
-        'ucrSelectorOneWayFreq
-        '
-        Me.ucrSelectorOneWayFreq.bShowHiddenColumns = False
-        Me.ucrSelectorOneWayFreq.bUseCurrentFilter = True
-        Me.ucrSelectorOneWayFreq.Location = New System.Drawing.Point(10, 10)
-        Me.ucrSelectorOneWayFreq.Margin = New System.Windows.Forms.Padding(0)
-        Me.ucrSelectorOneWayFreq.Name = "ucrSelectorOneWayFreq"
-        Me.ucrSelectorOneWayFreq.Size = New System.Drawing.Size(210, 180)
-        Me.ucrSelectorOneWayFreq.TabIndex = 0
-        '
-        'ucrReceiverOneWayFreq
-        '
-        Me.ucrReceiverOneWayFreq.frmParent = Me
-        Me.ucrReceiverOneWayFreq.Location = New System.Drawing.Point(295, 60)
-        Me.ucrReceiverOneWayFreq.Margin = New System.Windows.Forms.Padding(0)
-        Me.ucrReceiverOneWayFreq.Name = "ucrReceiverOneWayFreq"
-        Me.ucrReceiverOneWayFreq.Selector = Nothing
-        Me.ucrReceiverOneWayFreq.Size = New System.Drawing.Size(120, 20)
-        Me.ucrReceiverOneWayFreq.TabIndex = 1
-        '
-        'ucrSaveGraph
-        '
-        Me.ucrSaveGraph.Location = New System.Drawing.Point(9, 265)
-        Me.ucrSaveGraph.Name = "ucrSaveGraph"
-        Me.ucrSaveGraph.Size = New System.Drawing.Size(294, 24)
-        Me.ucrSaveGraph.TabIndex = 5
-        '
-        'ucrBase
-        '
-        Me.ucrBase.Location = New System.Drawing.Point(9, 293)
-        Me.ucrBase.Name = "ucrBase"
-        Me.ucrBase.Size = New System.Drawing.Size(410, 52)
-        Me.ucrBase.TabIndex = 6
-        '
-        'ucrChkTable
-        '
-        Me.ucrChkTable.Checked = False
-        Me.ucrChkTable.Location = New System.Drawing.Point(10, 239)
-        Me.ucrChkTable.Name = "ucrChkTable"
-        Me.ucrChkTable.Size = New System.Drawing.Size(145, 20)
-        Me.ucrChkTable.TabIndex = 10
-        '
-        'ucrChkweights
-        '
-        Me.ucrChkweights.Checked = False
-        Me.ucrChkweights.Location = New System.Drawing.Point(10, 204)
-        Me.ucrChkweights.Name = "ucrChkweights"
-        Me.ucrChkweights.Size = New System.Drawing.Size(145, 20)
-        Me.ucrChkweights.TabIndex = 11
-        '
-        'ucrChkGraph
-        '
-        Me.ucrChkGraph.Checked = False
-        Me.ucrChkGraph.Location = New System.Drawing.Point(158, 239)
-        Me.ucrChkGraph.Name = "ucrChkGraph"
-        Me.ucrChkGraph.Size = New System.Drawing.Size(145, 20)
-        Me.ucrChkGraph.TabIndex = 12
         '
         'grpSort
         '
@@ -104,7 +46,7 @@ Partial Class dlgOneWayFrequencies
         Me.grpSort.Controls.Add(Me.rdoAscending)
         Me.grpSort.Controls.Add(Me.rdoNone)
         Me.grpSort.Controls.Add(Me.ucrPnlSort)
-        Me.grpSort.Location = New System.Drawing.Point(295, 99)
+        Me.grpSort.Location = New System.Drawing.Point(267, 114)
         Me.grpSort.Name = "grpSort"
         Me.grpSort.Size = New System.Drawing.Size(123, 91)
         Me.grpSort.TabIndex = 14
@@ -153,7 +95,7 @@ Partial Class dlgOneWayFrequencies
         '
         'cmdOptions
         '
-        Me.cmdOptions.Location = New System.Drawing.Point(318, 204)
+        Me.cmdOptions.Location = New System.Drawing.Point(280, 216)
         Me.cmdOptions.Name = "cmdOptions"
         Me.cmdOptions.Size = New System.Drawing.Size(69, 23)
         Me.cmdOptions.TabIndex = 15
@@ -163,7 +105,7 @@ Partial Class dlgOneWayFrequencies
         'lblSelectedVariable
         '
         Me.lblSelectedVariable.AutoSize = True
-        Me.lblSelectedVariable.Location = New System.Drawing.Point(292, 41)
+        Me.lblSelectedVariable.Location = New System.Drawing.Point(267, 61)
         Me.lblSelectedVariable.Name = "lblSelectedVariable"
         Me.lblSelectedVariable.Size = New System.Drawing.Size(93, 13)
         Me.lblSelectedVariable.TabIndex = 16
@@ -172,22 +114,80 @@ Partial Class dlgOneWayFrequencies
         'ucrChkFlip
         '
         Me.ucrChkFlip.Checked = False
-        Me.ucrChkFlip.Location = New System.Drawing.Point(158, 204)
+        Me.ucrChkFlip.Location = New System.Drawing.Point(10, 202)
         Me.ucrChkFlip.Name = "ucrChkFlip"
         Me.ucrChkFlip.Size = New System.Drawing.Size(145, 20)
         Me.ucrChkFlip.TabIndex = 17
+        '
+        'ucrChkGraph
+        '
+        Me.ucrChkGraph.Checked = False
+        Me.ucrChkGraph.Location = New System.Drawing.Point(9, 252)
+        Me.ucrChkGraph.Name = "ucrChkGraph"
+        Me.ucrChkGraph.Size = New System.Drawing.Size(145, 20)
+        Me.ucrChkGraph.TabIndex = 12
+        '
+        'ucrChkTable
+        '
+        Me.ucrChkTable.Checked = False
+        Me.ucrChkTable.Location = New System.Drawing.Point(10, 227)
+        Me.ucrChkTable.Name = "ucrChkTable"
+        Me.ucrChkTable.Size = New System.Drawing.Size(145, 20)
+        Me.ucrChkTable.TabIndex = 10
+        '
+        'ucrBase
+        '
+        Me.ucrBase.Location = New System.Drawing.Point(9, 302)
+        Me.ucrBase.Name = "ucrBase"
+        Me.ucrBase.Size = New System.Drawing.Size(410, 52)
+        Me.ucrBase.TabIndex = 6
+        '
+        'ucrSaveGraph
+        '
+        Me.ucrSaveGraph.Location = New System.Drawing.Point(9, 277)
+        Me.ucrSaveGraph.Name = "ucrSaveGraph"
+        Me.ucrSaveGraph.Size = New System.Drawing.Size(294, 24)
+        Me.ucrSaveGraph.TabIndex = 5
+        '
+        'ucrReceiverOneWayFreq
+        '
+        Me.ucrReceiverOneWayFreq.frmParent = Me
+        Me.ucrReceiverOneWayFreq.Location = New System.Drawing.Point(270, 77)
+        Me.ucrReceiverOneWayFreq.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverOneWayFreq.Name = "ucrReceiverOneWayFreq"
+        Me.ucrReceiverOneWayFreq.Selector = Nothing
+        Me.ucrReceiverOneWayFreq.Size = New System.Drawing.Size(120, 20)
+        Me.ucrReceiverOneWayFreq.TabIndex = 1
+        '
+        'ucrSelectorOneWayFreq
+        '
+        Me.ucrSelectorOneWayFreq.bShowHiddenColumns = False
+        Me.ucrSelectorOneWayFreq.bUseCurrentFilter = True
+        Me.ucrSelectorOneWayFreq.Location = New System.Drawing.Point(10, 10)
+        Me.ucrSelectorOneWayFreq.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrSelectorOneWayFreq.Name = "ucrSelectorOneWayFreq"
+        Me.ucrSelectorOneWayFreq.Size = New System.Drawing.Size(210, 180)
+        Me.ucrSelectorOneWayFreq.TabIndex = 0
+        '
+        'ucrChkweights
+        '
+        Me.ucrChkweights.Checked = False
+        Me.ucrChkweights.Location = New System.Drawing.Point(270, 27)
+        Me.ucrChkweights.Name = "ucrChkweights"
+        Me.ucrChkweights.Size = New System.Drawing.Size(145, 20)
+        Me.ucrChkweights.TabIndex = 18
         '
         'dlgOneWayFrequencies
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(440, 353)
+        Me.ClientSize = New System.Drawing.Size(427, 361)
+        Me.Controls.Add(Me.ucrChkweights)
         Me.Controls.Add(Me.ucrChkFlip)
         Me.Controls.Add(Me.lblSelectedVariable)
         Me.Controls.Add(Me.cmdOptions)
         Me.Controls.Add(Me.grpSort)
         Me.Controls.Add(Me.ucrChkGraph)
-        Me.Controls.Add(Me.ucrChkweights)
         Me.Controls.Add(Me.ucrChkTable)
         Me.Controls.Add(Me.ucrBase)
         Me.Controls.Add(Me.ucrSaveGraph)
@@ -211,7 +211,6 @@ Partial Class dlgOneWayFrequencies
     Friend WithEvents ucrSaveGraph As ucrSave
     Friend WithEvents ucrBase As ucrButtons
     Friend WithEvents ucrChkGraph As ucrCheck
-    Friend WithEvents ucrChkweights As ucrCheck
     Friend WithEvents ucrChkTable As ucrCheck
     Friend WithEvents grpSort As GroupBox
     Friend WithEvents rdoDescending As RadioButton
@@ -221,4 +220,5 @@ Partial Class dlgOneWayFrequencies
     Friend WithEvents cmdOptions As Button
     Friend WithEvents lblSelectedVariable As Label
     Friend WithEvents ucrChkFlip As ucrCheck
+    Friend WithEvents ucrChkweights As ucrCheck
 End Class
--- instat/dlgOneWayFrequencies.vb
+++ instat/dlgOneWayFrequencies.vb
@@ -20,6 +20,8 @@ Public Class dlgOneWayFrequencies
     Private bFirstLoad As Boolean = True
     Private bReset As Boolean = True
     Private bResetSubdialog As Boolean = False
+    Private clsSjtFreq As New RFunction
+    Private clsSjpFrq As New RFunction
     Private Sub dlgOneWayFrequencies_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         autoTranslate(Me)
         If bFirstLoad Then
@@ -34,36 +36,97 @@ Public Class dlgOneWayFrequencies
     End Sub
 
     Public Sub SetRCodeForControls(bReset As Boolean)
-        SetRCode(Me, ucrBase.clsRsyntax.clsBaseFunction, bReset)
+        ucrReceiverOneWayFreq.SetRCode(ucrBase.clsRsyntax.clsBaseFunction, bReset)
+        ucrPnlSort.SetRCode(ucrBase.clsRsyntax.clsBaseFunction, bReset)
+        ucrChkTable.SetRCode(ucrBase.clsRsyntax.clsBaseFunction, bReset)
+        ucrChkGraph.SetRCode(ucrBase.clsRsyntax.clsBaseFunction, bReset)
+
     End Sub
 
     Private Sub InitialiseDialog()
+        'HelpID
+        ' ucrBase.iHelpTopicID = 
+        ucrBase.clsRsyntax.bExcludeAssignedFunctionOutput = False
+        ucrBase.clsRsyntax.iCallType = 3
+
         ucrReceiverOneWayFreq.Selector = ucrSelectorOneWayFreq
         ucrReceiverOneWayFreq.SetMeAsReceiver()
-        ucrReceiverOneWayFreq.SetParameter(New RParameter("", 1))
-        ucrReceiverOneWayFreq.SetParameterIsString()
+        ucrReceiverOneWayFreq.SetParameter(New RParameter("data", 1))
+        ucrReceiverOneWayFreq.SetParameterIsRFunction()
+
+
+        ucrPnlSort.SetParameter(New RParameter("sort.frq"))
+        ucrPnlSort.AddRadioButton(rdoNone, Chr(34) & "none" & Chr(34))
+        ucrPnlSort.AddRadioButton(rdoAscending, Chr(34) & "asc" & Chr(34))
+        ucrPnlSort.AddRadioButton(rdoDescending, Chr(34) & "desc" & Chr(34))
+        ucrPnlSort.SetRDefault(Chr(34) & "none" & Chr(34))
 
 
         ucrChkTable.SetText("Table")
         ucrChkweights.SetText("Weights")
         ucrChkGraph.SetText("Graph")
+        ' ucrChkGraph.SetParameter(New RParameter("type"))
+
         ucrChkFlip.SetText("Flip Coordinates")
+        ucrChkFlip.SetParameter(New RParameter("coord.flip"), bNewChangeParameterValue:=True, bNewAddRemoveParameter:=True, strNewValueIfChecked:="TRUE", strNewValueIfUnchecked:="FALSE")
+        ucrChkFlip.SetRDefault("FALSE")
 
         ucrSaveGraph.SetPrefix("one_way_freq")
         ucrSaveGraph.SetSaveTypeAsGraph()
         ucrSaveGraph.SetDataFrameSelector(ucrSelectorOneWayFreq.ucrAvailableDataFrames)
         ucrSaveGraph.SetCheckBoxText("Save Graph")
         ucrSaveGraph.SetIsComboBox()
-        ucrSaveGraph.SetAssignToIfUncheckedValue("last_graph")
     End Sub
 
     Private Sub SetDefaults()
+        clsSjtFreq = New RFunction
+        clsSjpFrq = New RFunction
         ucrSelectorOneWayFreq.Reset()
         ucrSaveGraph.Reset()
+        ucrReceiverOneWayFreq.SetParameter(New RParameter("data", 1))
+        ucrReceiverOneWayFreq.SetParameterIsRFunction()
+        clsSjtFreq.SetRCommand("sjPlot::sjt.frq")
+        clsSjtFreq.AddParameter("data", clsRFunctionParameter:=ucrReceiverOneWayFreq.GetVariables)
+        clsSjtFreq.AddParameter("sort.frq", Chr(34) & "none" & Chr(34))
+        clsSjtFreq.AddParameter("use.viewer", "FALSE")
+        clsSjpFrq.SetRCommand("sjPlot::sjp.frq")
+        clsSjpFrq.AddParameter("var.cnt", clsRFunctionParameter:=ucrReceiverOneWayFreq.GetVariables)
+
+        ucrBase.clsRsyntax.SetBaseRFunction(clsSjtFreq)
     End Sub
 
     Private Sub TestOkEnabled()
+        If Not ucrReceiverOneWayFreq.IsEmpty() AndAlso ucrSaveGraph.IsComplete() Then
+            ucrBase.OKEnabled(True)
+        Else
+            ucrBase.OKEnabled(False)
+        End If
+    End Sub
 
+    Private Sub ChangeBaseFunction()
+        If ucrChkTable.Checked Then
+            ucrReceiverOneWayFreq.SetParameter(New RParameter("data", 1))
+            ucrReceiverOneWayFreq.SetParameterIsRFunction()
+            ucrBase.clsRsyntax.SetBaseRFunction(clsSjtFreq)
+        Else
+            ucrReceiverOneWayFreq.SetParameter(New RParameter("var.cnt", 1))
+            ucrReceiverOneWayFreq.SetParameterIsRFunction()
+            ucrBase.clsRsyntax.SetBaseRFunction(clsSjpFrq)
+        End If
+        SetRCodeForControls(False)
+    End Sub
+
+    Private Sub ucrBase_ClickReset(sender As Object, e As EventArgs) Handles ucrBase.ClickReset
+        SetDefaults()
+        SetRCodeForControls(True)
+        TestOkEnabled()
+    End Sub
+
+    Private Sub AllControls_ControlValueChanged(ucrChangedControl As ucrCore) Handles ucrReceiverOneWayFreq.ControlValueChanged, ucrChkTable.ControlValueChanged, ucrChkGraph.ControlValueChanged, ucrPnlSort.ControlValueChanged
+        If ucrChkweights.Checked Then
+            ucrReceiverOneWayFreq.SetDataType("numeric")
+        End If
+        ChangeBaseFunction()
     End Sub
 
     Private Sub Controls_ControlContentsChanged(ucrChangedControl As ucrCore) Handles ucrReceiverOneWayFreq.ControlContentsChanged, ucrSaveGraph.ControlContentsChanged
