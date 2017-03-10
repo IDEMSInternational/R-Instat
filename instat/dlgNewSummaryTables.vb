78ad346c0d755dfc4048c24eaae046bf273a8822
Alex Sananka <Alex Sananka@SANANKA>
2017-03-10 13:35:53 +0300

    Designer Changes and minor Edits

---
+++ instat/dlgNewSummaryTables.vb
@@ -0,0 +1,70 @@
+﻿' Instat-R
+' Copyright (C) 2015
+'
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+'
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License k
+' along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Imports instat
+Imports instat.Translations
+Public Class dlgNewSummaryTables
+    Private bFirstload As Boolean = True
+    Private bReset As Boolean = True
+    Private Sub dlgNewSummaryTables_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        autoTranslate(Me)
+        If bFirstload Then
+            InitialiseDialog()
+            bFirstload = False
+        End If
+        If bReset Then
+            SetDefaults()
+        End If
+        bReset = False
+        TestOKEnabled()
+    End Sub
+    Public Sub SetRCodeForControls(bReset As Boolean)
+
+    End Sub
+    Private Sub SetDefaults()
+        ucrFactorsSelector.Reset()
+        ucrReceiverFactor.SetMeAsReceiver()
+    End Sub
+
+    Private Sub initialiseDialog()
+        ucrReceiverFactor.Selector = ucrFactorsSelector
+        ucrReceiverNumeric.Selector = ucrFactorsSelector
+        ucrCheckWeight.SetText("Weights")
+        ucrCheckSummaries.SetText("Summary Columns")
+        ucrCheckDisplayMargins.SetText("Dispaly Margins")
+    End Sub
+
+    Private Sub TestOKEnabled()
+        If Not ucrReceiverFactor.IsEmpty Then
+            If ucrCheckWeight.Checked Then
+                If ucrReceiverNumberColn.IsEmpty() Then
+                    ucrBase.OKEnabled(True)
+                Else
+                    ucrBase.OKEnabled(False)
+                End If
+            End If
+
+        End If
+    End Sub
+    Private Sub cmdSummaries_Click(sender As Object, e As EventArgs) Handles cmdSummaries.Click
+        sdgSummaries.ShowDialog()
+    End Sub
+    Private Sub ucrBase_ClickReset(sender As Object, e As EventArgs) Handles ucrBase.ClickReset
+        SetDefaults()
+        SetRCodeForControls(True)
+        TestOKEnabled()
+    End Sub
+End Class
