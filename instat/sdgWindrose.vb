af85dec8fcef102d85dcec76b2ba634c961172bb
maxwell fundi <maxwellfundi@live.com>
2017-03-13 15:22:55 +0300

    Adding functionality into sdgWindrose

--- instat/sdgWindrose.vb
+++ instat/sdgWindrose.vb
@@ -13,8 +13,36 @@
 '
 ' You should have received a copy of the GNU General Public License k
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
-Public Class sdgWindrose
-    Private Sub sdgWindrose_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+Imports instat.Translations
 
+Public Class sdgWindrose
+    Public bControlsInitialised As Boolean = False
+    Public clsWindRoseFunc As New RFunction
+    Private Sub sdgWindrose_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        autoTranslate(Me)
+    End Sub
+
+    Public Sub InitialiseControls()
+        ucrNudCalmWind.SetParameter(New RParameter("calm_wind"))
+        ucrNudCalmWind.SetRDefault(0)
+
+        ucrNudNoOfDirections.SetParameter(New RParameter("n_directions"))
+        ucrNudNoOfDirections.SetRDefault(12)
+
+        ucrNudNoOfSpeeds.SetParameter(New RParameter("n_speeds "))
+        ucrNudNoOfSpeeds.SetRDefault(5)
+
+        ucrInputSpeedCuts.SetParameter(New RParameter("speed_cuts "))
+        ucrInputSpeedCuts.SetRDefault("NA")
+
+        bControlsInitialised = True
+    End Sub
+
+    Public Sub SetRFunction(clsNewRFunction As RFunction, Optional bReset As Boolean = False)
+        If Not bControlsInitialised Then
+            InitialiseControls()
+        End If
+        clsWindRoseFunc = clsNewRFunction
+        SetRCode(Me, clsWindRoseFunc, bReset)
     End Sub
 End Class
