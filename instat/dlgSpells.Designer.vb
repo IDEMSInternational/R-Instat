8e86c7893ca00b14d57e533e1c6c1cf0cf21bcdf
Lily <Admin@NEW-KT5NM8N55N7>
2017-03-15 22:03:28 +0000

    Update of code

--- instat/dlgSpells.Designer.vb
+++ instat/dlgSpells.Designer.vb
@@ -23,16 +23,6 @@ Partial Class dlgSpells
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.lblRainfall = New System.Windows.Forms.Label()
-        Me.grpRainParameters = New System.Windows.Forms.GroupBox()
-        Me.lblRain = New System.Windows.Forms.Label()
-        Me.ucrInputCondition = New instat.ucrInputComboBox()
-        Me.NudConditionRight = New System.Windows.Forms.NumericUpDown()
-        Me.nudConditionLeft = New System.Windows.Forms.NumericUpDown()
-        Me.nudFrom = New System.Windows.Forms.NumericUpDown()
-        Me.nudTo = New System.Windows.Forms.NumericUpDown()
-        Me.lblThreshold = New System.Windows.Forms.Label()
-        Me.lblFrom = New System.Windows.Forms.Label()
-        Me.lblTo = New System.Windows.Forms.Label()
         Me.lblDate = New System.Windows.Forms.Label()
         Me.lblYear = New System.Windows.Forms.Label()
         Me.lblDOY = New System.Windows.Forms.Label()
@@ -43,11 +33,20 @@ Partial Class dlgSpells
         Me.ucrSelectorForStartofRains = New instat.ucrSelectorByDataFrameAddRemove()
         Me.ucrBase = New instat.ucrButtons()
         Me.chkConditionRain = New System.Windows.Forms.CheckBox()
-        Me.grpRainParameters.SuspendLayout()
-        CType(Me.NudConditionRight, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.nudConditionLeft, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.nudFrom, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.lblTo = New System.Windows.Forms.Label()
+        Me.lblFrom = New System.Windows.Forms.Label()
+        Me.lblThreshold = New System.Windows.Forms.Label()
+        Me.nudTo = New System.Windows.Forms.NumericUpDown()
+        Me.nudFrom = New System.Windows.Forms.NumericUpDown()
+        Me.nudConditionLeft = New System.Windows.Forms.NumericUpDown()
+        Me.NudConditionRight = New System.Windows.Forms.NumericUpDown()
+        Me.ucrInputCondition = New instat.ucrInputComboBox()
+        Me.grpRainParameters = New System.Windows.Forms.GroupBox()
         CType(Me.nudTo, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.nudFrom, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.nudConditionLeft, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NudConditionRight, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.grpRainParameters.SuspendLayout()
         Me.SuspendLayout()
         '
         'lblRainfall
@@ -58,112 +57,6 @@ Partial Class dlgSpells
         Me.lblRainfall.Size = New System.Drawing.Size(70, 13)
         Me.lblRainfall.TabIndex = 3
         Me.lblRainfall.Text = "Rain Column:"
-        '
-        'grpRainParameters
-        '
-        Me.grpRainParameters.Controls.Add(Me.lblRain)
-        Me.grpRainParameters.Controls.Add(Me.ucrInputCondition)
-        Me.grpRainParameters.Controls.Add(Me.NudConditionRight)
-        Me.grpRainParameters.Controls.Add(Me.nudConditionLeft)
-        Me.grpRainParameters.Controls.Add(Me.nudFrom)
-        Me.grpRainParameters.Controls.Add(Me.nudTo)
-        Me.grpRainParameters.Controls.Add(Me.lblThreshold)
-        Me.grpRainParameters.Controls.Add(Me.lblFrom)
-        Me.grpRainParameters.Controls.Add(Me.lblTo)
-        Me.grpRainParameters.Location = New System.Drawing.Point(9, 192)
-        Me.grpRainParameters.Name = "grpRainParameters"
-        Me.grpRainParameters.Size = New System.Drawing.Size(380, 74)
-        Me.grpRainParameters.TabIndex = 9
-        Me.grpRainParameters.TabStop = False
-        '
-        'lblRain
-        '
-        Me.lblRain.AutoSize = True
-        Me.lblRain.Location = New System.Drawing.Point(239, 47)
-        Me.lblRain.Name = "lblRain"
-        Me.lblRain.Size = New System.Drawing.Size(42, 13)
-        Me.lblRain.TabIndex = 14
-        Me.lblRain.Text = "Rainfall"
-        '
-        'ucrInputCondition
-        '
-        Me.ucrInputCondition.AddQuotesIfUnrecognised = True
-        Me.ucrInputCondition.IsReadOnly = False
-        Me.ucrInputCondition.Location = New System.Drawing.Point(131, 44)
-        Me.ucrInputCondition.Name = "ucrInputCondition"
-        Me.ucrInputCondition.Size = New System.Drawing.Size(97, 21)
-        Me.ucrInputCondition.TabIndex = 10
-        '
-        'NudConditionRight
-        '
-        Me.NudConditionRight.DecimalPlaces = 2
-        Me.NudConditionRight.Increment = New Decimal(New Integer() {1, 0, 0, 65536})
-        Me.NudConditionRight.Location = New System.Drawing.Point(237, 45)
-        Me.NudConditionRight.Name = "NudConditionRight"
-        Me.NudConditionRight.Size = New System.Drawing.Size(53, 20)
-        Me.NudConditionRight.TabIndex = 9
-        Me.NudConditionRight.Value = New Decimal(New Integer() {85, 0, 0, 131072})
-        '
-        'nudConditionLeft
-        '
-        Me.nudConditionLeft.DecimalPlaces = 2
-        Me.nudConditionLeft.Increment = New Decimal(New Integer() {1, 0, 0, 65536})
-        Me.nudConditionLeft.Location = New System.Drawing.Point(67, 45)
-        Me.nudConditionLeft.Name = "nudConditionLeft"
-        Me.nudConditionLeft.Size = New System.Drawing.Size(53, 20)
-        Me.nudConditionLeft.TabIndex = 6
-        '
-        'nudFrom
-        '
-        Me.nudFrom.Location = New System.Drawing.Point(67, 14)
-        Me.nudFrom.Maximum = New Decimal(New Integer() {366, 0, 0, 0})
-        Me.nudFrom.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
-        Me.nudFrom.Name = "nudFrom"
-        Me.nudFrom.RightToLeft = System.Windows.Forms.RightToLeft.Yes
-        Me.nudFrom.Size = New System.Drawing.Size(53, 20)
-        Me.nudFrom.TabIndex = 7
-        Me.nudFrom.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
-        Me.nudFrom.UpDownAlign = System.Windows.Forms.LeftRightAlignment.Left
-        Me.nudFrom.Value = New Decimal(New Integer() {1, 0, 0, 0})
-        '
-        'nudTo
-        '
-        Me.nudTo.Location = New System.Drawing.Point(237, 14)
-        Me.nudTo.Maximum = New Decimal(New Integer() {366, 0, 0, 0})
-        Me.nudTo.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
-        Me.nudTo.Name = "nudTo"
-        Me.nudTo.Size = New System.Drawing.Size(53, 20)
-        Me.nudTo.TabIndex = 8
-        Me.nudTo.Value = New Decimal(New Integer() {1, 0, 0, 0})
-        '
-        'lblThreshold
-        '
-        Me.lblThreshold.AutoSize = True
-        Me.lblThreshold.Location = New System.Drawing.Point(6, 47)
-        Me.lblThreshold.Name = "lblThreshold"
-        Me.lblThreshold.Size = New System.Drawing.Size(54, 13)
-        Me.lblThreshold.TabIndex = 0
-        Me.lblThreshold.Tag = "Threshold"
-        Me.lblThreshold.Text = "Condition:"
-        '
-        'lblFrom
-        '
-        Me.lblFrom.AutoSize = True
-        Me.lblFrom.Location = New System.Drawing.Point(6, 16)
-        Me.lblFrom.Name = "lblFrom"
-        Me.lblFrom.Size = New System.Drawing.Size(55, 13)
-        Me.lblFrom.TabIndex = 2
-        Me.lblFrom.Text = "From Day:"
-        '
-        'lblTo
-        '
-        Me.lblTo.AutoSize = True
-        Me.lblTo.Location = New System.Drawing.Point(186, 16)
-        Me.lblTo.Name = "lblTo"
-        Me.lblTo.Size = New System.Drawing.Size(45, 13)
-        Me.lblTo.TabIndex = 4
-        Me.lblTo.Tag = ""
-        Me.lblTo.Text = "To Day:"
         '
         'lblDate
         '
@@ -259,6 +152,102 @@ Partial Class dlgSpells
         Me.chkConditionRain.Text = "Conditional on Rain at Start of Spell"
         Me.chkConditionRain.UseVisualStyleBackColor = True
         '
+        'lblTo
+        '
+        Me.lblTo.AutoSize = True
+        Me.lblTo.Location = New System.Drawing.Point(186, 16)
+        Me.lblTo.Name = "lblTo"
+        Me.lblTo.Size = New System.Drawing.Size(45, 13)
+        Me.lblTo.TabIndex = 4
+        Me.lblTo.Tag = ""
+        Me.lblTo.Text = "To Day:"
+        '
+        'lblFrom
+        '
+        Me.lblFrom.AutoSize = True
+        Me.lblFrom.Location = New System.Drawing.Point(6, 16)
+        Me.lblFrom.Name = "lblFrom"
+        Me.lblFrom.Size = New System.Drawing.Size(55, 13)
+        Me.lblFrom.TabIndex = 2
+        Me.lblFrom.Text = "From Day:"
+        '
+        'lblThreshold
+        '
+        Me.lblThreshold.AutoSize = True
+        Me.lblThreshold.Location = New System.Drawing.Point(6, 47)
+        Me.lblThreshold.Name = "lblThreshold"
+        Me.lblThreshold.Size = New System.Drawing.Size(54, 13)
+        Me.lblThreshold.TabIndex = 0
+        Me.lblThreshold.Tag = "Threshold"
+        Me.lblThreshold.Text = "Condition:"
+        '
+        'nudTo
+        '
+        Me.nudTo.Location = New System.Drawing.Point(237, 14)
+        Me.nudTo.Maximum = New Decimal(New Integer() {366, 0, 0, 0})
+        Me.nudTo.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.nudTo.Name = "nudTo"
+        Me.nudTo.Size = New System.Drawing.Size(53, 20)
+        Me.nudTo.TabIndex = 8
+        Me.nudTo.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'nudFrom
+        '
+        Me.nudFrom.Location = New System.Drawing.Point(67, 14)
+        Me.nudFrom.Maximum = New Decimal(New Integer() {366, 0, 0, 0})
+        Me.nudFrom.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.nudFrom.Name = "nudFrom"
+        Me.nudFrom.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.nudFrom.Size = New System.Drawing.Size(53, 20)
+        Me.nudFrom.TabIndex = 7
+        Me.nudFrom.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        Me.nudFrom.UpDownAlign = System.Windows.Forms.LeftRightAlignment.Left
+        Me.nudFrom.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'nudConditionLeft
+        '
+        Me.nudConditionLeft.DecimalPlaces = 2
+        Me.nudConditionLeft.Increment = New Decimal(New Integer() {1, 0, 0, 65536})
+        Me.nudConditionLeft.Location = New System.Drawing.Point(67, 45)
+        Me.nudConditionLeft.Name = "nudConditionLeft"
+        Me.nudConditionLeft.Size = New System.Drawing.Size(53, 20)
+        Me.nudConditionLeft.TabIndex = 6
+        '
+        'NudConditionRight
+        '
+        Me.NudConditionRight.DecimalPlaces = 2
+        Me.NudConditionRight.Increment = New Decimal(New Integer() {1, 0, 0, 65536})
+        Me.NudConditionRight.Location = New System.Drawing.Point(237, 45)
+        Me.NudConditionRight.Name = "NudConditionRight"
+        Me.NudConditionRight.Size = New System.Drawing.Size(53, 20)
+        Me.NudConditionRight.TabIndex = 9
+        Me.NudConditionRight.Value = New Decimal(New Integer() {85, 0, 0, 131072})
+        '
+        'ucrInputCondition
+        '
+        Me.ucrInputCondition.AddQuotesIfUnrecognised = True
+        Me.ucrInputCondition.IsReadOnly = False
+        Me.ucrInputCondition.Location = New System.Drawing.Point(131, 44)
+        Me.ucrInputCondition.Name = "ucrInputCondition"
+        Me.ucrInputCondition.Size = New System.Drawing.Size(97, 21)
+        Me.ucrInputCondition.TabIndex = 10
+        '
+        'grpRainParameters
+        '
+        Me.grpRainParameters.Controls.Add(Me.ucrInputCondition)
+        Me.grpRainParameters.Controls.Add(Me.NudConditionRight)
+        Me.grpRainParameters.Controls.Add(Me.nudConditionLeft)
+        Me.grpRainParameters.Controls.Add(Me.nudFrom)
+        Me.grpRainParameters.Controls.Add(Me.nudTo)
+        Me.grpRainParameters.Controls.Add(Me.lblThreshold)
+        Me.grpRainParameters.Controls.Add(Me.lblFrom)
+        Me.grpRainParameters.Controls.Add(Me.lblTo)
+        Me.grpRainParameters.Location = New System.Drawing.Point(9, 192)
+        Me.grpRainParameters.Name = "grpRainParameters"
+        Me.grpRainParameters.Size = New System.Drawing.Size(380, 74)
+        Me.grpRainParameters.TabIndex = 9
+        Me.grpRainParameters.TabStop = False
+        '
         'dlgSpells
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
@@ -283,12 +272,12 @@ Partial Class dlgSpells
         Me.ShowIcon = False
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Dry Spells"
+        CType(Me.nudTo, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.nudFrom, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.nudConditionLeft, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NudConditionRight, System.ComponentModel.ISupportInitialize).EndInit()
         Me.grpRainParameters.ResumeLayout(False)
         Me.grpRainParameters.PerformLayout()
-        CType(Me.NudConditionRight, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.nudConditionLeft, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.nudFrom, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.nudTo, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -298,21 +287,20 @@ Partial Class dlgSpells
     Friend WithEvents ucrSelectorForStartofRains As ucrSelectorByDataFrameAddRemove
     Friend WithEvents ucrReceiverRainfall As ucrReceiverSingle
     Friend WithEvents lblRainfall As Label
-    Friend WithEvents grpRainParameters As GroupBox
-    Friend WithEvents lblFrom As Label
-    Friend WithEvents lblTo As Label
-    Friend WithEvents lblThreshold As Label
     Friend WithEvents lblDate As Label
     Friend WithEvents ucrReceiverDate As ucrReceiverSingle
     Friend WithEvents ucrReceiverYear As ucrReceiverSingle
     Friend WithEvents lblYear As Label
     Friend WithEvents lblDOY As Label
     Friend WithEvents ucrReceiverDayOfYear As ucrReceiverSingle
+    Friend WithEvents chkConditionRain As CheckBox
+    Friend WithEvents grpRainParameters As GroupBox
+    Friend WithEvents ucrInputCondition As ucrInputComboBox
+    Friend WithEvents NudConditionRight As NumericUpDown
     Friend WithEvents nudConditionLeft As NumericUpDown
     Friend WithEvents nudFrom As NumericUpDown
     Friend WithEvents nudTo As NumericUpDown
-    Friend WithEvents ucrInputCondition As ucrInputComboBox
-    Friend WithEvents NudConditionRight As NumericUpDown
-    Friend WithEvents chkConditionRain As CheckBox
-    Friend WithEvents lblRain As Label
+    Friend WithEvents lblThreshold As Label
+    Friend WithEvents lblFrom As Label
+    Friend WithEvents lblTo As Label
 End Class
