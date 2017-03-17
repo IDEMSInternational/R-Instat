22857841caafc8e4a631b21b3450743c89d0d955
Stephen Kogo <stevekogo@gmail.com>
2017-03-17 10:56:31 +0300

    edits

--- instat/dlgOpenNetCDF.Designer.vb
+++ instat/dlgOpenNetCDF.Designer.vb
@@ -23,20 +23,103 @@ Partial Class dlgOpenNetCDF
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.ucrBase = New instat.ucrButtons()
+        Me.ucrInputNameLocation = New instat.ucrInputTextBox()
+        Me.lblLocationName = New System.Windows.Forms.Label()
+        Me.ucrInputFilePath = New instat.ucrInputTextBox()
+        Me.ucrInputName = New instat.ucrInputTextBox()
+        Me.cmdOpenDataSet = New System.Windows.Forms.Button()
+        Me.lblFileOpenPath = New System.Windows.Forms.Label()
+        Me.lblSSTName = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'ucrBase
         '
-        Me.ucrBase.Location = New System.Drawing.Point(12, 288)
+        Me.ucrBase.Location = New System.Drawing.Point(12, 124)
         Me.ucrBase.Name = "ucrBase"
-        Me.ucrBase.Size = New System.Drawing.Size(347, 53)
+        Me.ucrBase.Size = New System.Drawing.Size(410, 53)
         Me.ucrBase.TabIndex = 1
+        '
+        'ucrInputNameLocation
+        '
+        Me.ucrInputNameLocation.AddQuotesIfUnrecognised = True
+        Me.ucrInputNameLocation.IsMultiline = False
+        Me.ucrInputNameLocation.IsReadOnly = False
+        Me.ucrInputNameLocation.Location = New System.Drawing.Point(158, 77)
+        Me.ucrInputNameLocation.Name = "ucrInputNameLocation"
+        Me.ucrInputNameLocation.Size = New System.Drawing.Size(142, 21)
+        Me.ucrInputNameLocation.TabIndex = 15
+        '
+        'lblLocationName
+        '
+        Me.lblLocationName.AutoSize = True
+        Me.lblLocationName.Location = New System.Drawing.Point(14, 82)
+        Me.lblLocationName.Name = "lblLocationName"
+        Me.lblLocationName.Size = New System.Drawing.Size(134, 13)
+        Me.lblLocationName.TabIndex = 14
+        Me.lblLocationName.Tag = "Location_Dataframe_Name:"
+        Me.lblLocationName.Text = "Location Dataframe Name:"
+        '
+        'ucrInputFilePath
+        '
+        Me.ucrInputFilePath.AddQuotesIfUnrecognised = True
+        Me.ucrInputFilePath.IsMultiline = False
+        Me.ucrInputFilePath.IsReadOnly = False
+        Me.ucrInputFilePath.Location = New System.Drawing.Point(43, 15)
+        Me.ucrInputFilePath.Name = "ucrInputFilePath"
+        Me.ucrInputFilePath.Size = New System.Drawing.Size(161, 21)
+        Me.ucrInputFilePath.TabIndex = 10
+        '
+        'ucrInputName
+        '
+        Me.ucrInputName.AddQuotesIfUnrecognised = True
+        Me.ucrInputName.IsMultiline = False
+        Me.ucrInputName.IsReadOnly = False
+        Me.ucrInputName.Location = New System.Drawing.Point(142, 45)
+        Me.ucrInputName.Name = "ucrInputName"
+        Me.ucrInputName.Size = New System.Drawing.Size(142, 21)
+        Me.ucrInputName.TabIndex = 13
+        '
+        'cmdOpenDataSet
+        '
+        Me.cmdOpenDataSet.Location = New System.Drawing.Point(210, 15)
+        Me.cmdOpenDataSet.Name = "cmdOpenDataSet"
+        Me.cmdOpenDataSet.Size = New System.Drawing.Size(66, 23)
+        Me.cmdOpenDataSet.TabIndex = 11
+        Me.cmdOpenDataSet.Tag = "select_file"
+        Me.cmdOpenDataSet.Text = "Select File"
+        Me.cmdOpenDataSet.UseVisualStyleBackColor = True
+        '
+        'lblFileOpenPath
+        '
+        Me.lblFileOpenPath.AutoSize = True
+        Me.lblFileOpenPath.Location = New System.Drawing.Point(14, 18)
+        Me.lblFileOpenPath.Name = "lblFileOpenPath"
+        Me.lblFileOpenPath.Size = New System.Drawing.Size(26, 13)
+        Me.lblFileOpenPath.TabIndex = 9
+        Me.lblFileOpenPath.Text = "File:"
+        '
+        'lblSSTName
+        '
+        Me.lblSSTName.AutoSize = True
+        Me.lblSSTName.Location = New System.Drawing.Point(14, 49)
+        Me.lblSSTName.Name = "lblSSTName"
+        Me.lblSSTName.Size = New System.Drawing.Size(114, 13)
+        Me.lblSSTName.TabIndex = 12
+        Me.lblSSTName.Tag = "SST_Dataframe_Name:"
+        Me.lblSSTName.Text = "SST Dataframe Name:"
         '
         'dlgOpenNetCDF
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(371, 353)
+        Me.ClientSize = New System.Drawing.Size(434, 190)
+        Me.Controls.Add(Me.ucrInputNameLocation)
+        Me.Controls.Add(Me.lblLocationName)
+        Me.Controls.Add(Me.ucrInputFilePath)
+        Me.Controls.Add(Me.ucrInputName)
+        Me.Controls.Add(Me.cmdOpenDataSet)
+        Me.Controls.Add(Me.lblFileOpenPath)
+        Me.Controls.Add(Me.lblSSTName)
         Me.Controls.Add(Me.ucrBase)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.MaximizeBox = False
@@ -45,8 +128,16 @@ Partial Class dlgOpenNetCDF
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Open NetCDF"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents ucrBase As ucrButtons
+    Friend WithEvents ucrInputNameLocation As ucrInputTextBox
+    Friend WithEvents lblLocationName As Label
+    Friend WithEvents ucrInputFilePath As ucrInputTextBox
+    Friend WithEvents ucrInputName As ucrInputTextBox
+    Friend WithEvents cmdOpenDataSet As Button
+    Friend WithEvents lblFileOpenPath As Label
+    Friend WithEvents lblSSTName As Label
 End Class
--- instat/dlgOpenNetCDF.vb
+++ instat/dlgOpenNetCDF.vb
@@ -14,36 +14,112 @@
 ' You should have received a copy of the GNU General Public License k
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 Imports instat.Translations
+Imports System.IO
+
 Public Class dlgOpenNetCDF
-    Private bFirstLoad As Boolean = True
-    Private bReset As Boolean = True
-    Private Sub dlgOpenNetCDF_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Private clsBaseNetCDF, clsRCDF As New RFunction
+    Dim bFirstLoad As Boolean
+    Dim strFileType As String
+    Dim bCanImport As Boolean
+    Dim bComponentsInitialised As Boolean
+    Public bStartOpenDialog As Boolean
+
+    Public Sub New()
+        ' This call is required by the designer.
+        InitializeComponent()
+        clsBaseNetCDF.SetRCommand(frmMain.clsRLink.strInstatDataObject & "$import_NetCDF")
+        clsBaseNetCDF.AddParameter("nc_data", clsRFunctionParameter:=clsRCDF)
+        bFirstLoad = True
+        bCanImport = True
+        bComponentsInitialised = True
+        bStartOpenDialog = True
+        ucrInputName.bAutoChangeOnLeave = True
+    End Sub
+
+    Private Sub dlgImportDataset_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         autoTranslate(Me)
+        Me.Show()
         If bFirstLoad Then
             InitialiseDialog()
+            SetCSVDefault()
             bFirstLoad = False
         End If
-
-        If bReset Then
-            SetDefaults()
+        If bStartOpenDialog Then
+            GetFileFromOpenDialog()
+            bStartOpenDialog = False
         End If
-        SetRCodeForControls(bReset)
-        bReset = False
         TestOkEnabled()
     End Sub
 
-    Private Sub SetRCodeForControls(bReset As Boolean)
-
-    End Sub
-
-    Private Sub SetDefaults()
-
-    End Sub
-
     Private Sub InitialiseDialog()
+        ucrBase.iHelpTopicID = 102
+        ucrInputName.SetValidationTypeAsRVariable()
+        ucrInputNameLocation.SetValidationTypeAsRVariable()
+        ucrInputNameLocation.SetName("lat_lon_data")
+    End Sub
+
+    Private Sub ucrInputDataName_NameChanged() Handles ucrInputName.NameChanged, ucrInputNameLocation.NameChanged
+        ucrBase.clsRsyntax.AddParameter("data_names", "c(" & Chr(34) & ucrInputName.GetText() & Chr(34) & "," & Chr(34) & ucrInputNameLocation.GetText() & Chr(34) & ")")
+    End Sub
+
+
+    Private Sub ucrBase_ClickReset(sender As Object, e As EventArgs) Handles ucrBase.ClickReset
+        SetCSVDefault()
+    End Sub
+
+    Private Sub TestOkEnabled()
+        If bCanImport Then
+            ucrBase.OKEnabled(True)
+        Else
+            ucrBase.OKEnabled(False)
+        End If
+    End Sub
+
+    'Loads the open dialog on load and click
+    Public Sub GetFileFromOpenDialog()
+        Dim strFilePath As String = ""
+        Dim strFileName As String = ""
+        Dim strFileExt As String = ""
+        Using dlgOpen As New OpenFileDialog
+            dlgOpen.Filter = "All Data files|*.nc|NetCDF files|*.nc"
+            dlgOpen.Title = "Open Data from file"
+            If Not ucrInputFilePath.IsEmpty() Then
+                dlgOpen.InitialDirectory = Path.GetDirectoryName(Replace(ucrInputFilePath.GetText(), "/", "\"))
+            Else
+                dlgOpen.InitialDirectory = frmMain.clsInstatOptions.strWorkingDirectory
+            End If
+
+            If dlgOpen.ShowDialog() = DialogResult.OK Then
+                ucrInputName.SetName("")
+                ucrInputName.Reset()
+                'checks if the file name is not blank'
+                If dlgOpen.FileName <> "" Then
+                    strFileName = Path.GetFileNameWithoutExtension(dlgOpen.FileName)
+                    strFilePath = Replace(dlgOpen.FileName, "\", "/")
+                    strFileExt = Path.GetExtension(strFilePath)
+                    ucrInputFilePath.SetName(strFilePath)
+                    ucrInputName.Show()
+                    lblSSTName.Show()
+
+                    If strFileExt = ".nc" Then
+                        ucrBase.clsRsyntax.SetBaseRFunction(clsBaseNetCDF)
+                        clsRCDF.SetRCommand("nc_open")
+                        clsRCDF.AddParameter("filename", Chr(34) & strFilePath & Chr(34))
+                        strFileType = "nc"
+                        ucrInputName.SetName(strFileName, bSilent:=True)
+                        ucrInputName.Focus()
+                    End If
+                End If
+            End If
+            TestOkEnabled()
+        End Using
+    End Sub
+
+    Private Sub SetCSVDefault()
 
     End Sub
-    Private Sub TestOkEnabled()
 
+    Private Sub cmdOpenDataSet_Click(sender As Object, e As EventArgs) Handles cmdOpenDataSet.Click
+        GetFileFromOpenDialog()
     End Sub
 End Class
