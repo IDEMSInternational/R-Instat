ab6cc763b1aa12e7f502084db6650cc0e65523a0
Stephen Kogo <stevekogo@gmail.com>
2017-03-28 16:32:43 +0300

    Swapping the receivers

--- instat/dlgDescribeTwoVariable.Designer.vb
+++ instat/dlgDescribeTwoVariable.Designer.vb
@@ -26,10 +26,10 @@ Partial Class dlgDescribeTwoVariable
         Me.cmdDisplayOptions = New System.Windows.Forms.Button()
         Me.lblFirstVariables = New System.Windows.Forms.Label()
         Me.lbSecondVariable = New System.Windows.Forms.Label()
-        Me.ucrReceiverSecondVar = New instat.ucrReceiverSingle()
-        Me.ucrReceiverFirstVar = New instat.ucrReceiverMultiple()
+        Me.ucrReceiverFirstVar = New instat.ucrReceiverSingle()
+        Me.ucrReceiverSecondVar = New instat.ucrReceiverMultiple()
         Me.ucrSelectorDescribeTwoVar = New instat.ucrSelectorByDataFrameAddRemove()
-        Me.ucrBaseDescribeTwoVar = New instat.ucrButtons()
+        Me.ucrBase = New instat.ucrButtons()
         Me.ucrChkSaveResult = New instat.ucrCheck()
         Me.ucrChkOmitMissing = New instat.ucrCheck()
         Me.SuspendLayout()
@@ -57,7 +57,7 @@ Partial Class dlgDescribeTwoVariable
         'lblFirstVariables
         '
         Me.lblFirstVariables.AutoSize = True
-        Me.lblFirstVariables.Location = New System.Drawing.Point(248, 46)
+        Me.lblFirstVariables.Location = New System.Drawing.Point(248, 10)
         Me.lblFirstVariables.Name = "lblFirstVariables"
         Me.lblFirstVariables.Size = New System.Drawing.Size(81, 13)
         Me.lblFirstVariables.TabIndex = 1
@@ -66,32 +66,32 @@ Partial Class dlgDescribeTwoVariable
         '
         'lbSecondVariable
         '
-        Me.lbSecondVariable.Location = New System.Drawing.Point(248, 154)
+        Me.lbSecondVariable.Location = New System.Drawing.Point(257, 78)
         Me.lbSecondVariable.Name = "lbSecondVariable"
         Me.lbSecondVariable.Size = New System.Drawing.Size(100, 15)
         Me.lbSecondVariable.TabIndex = 3
         Me.lbSecondVariable.Tag = "Second_Variable:"
         Me.lbSecondVariable.Text = "Second Variable:"
         '
-        'ucrReceiverSecondVar
-        '
-        Me.ucrReceiverSecondVar.frmParent = Me
-        Me.ucrReceiverSecondVar.Location = New System.Drawing.Point(247, 170)
-        Me.ucrReceiverSecondVar.Margin = New System.Windows.Forms.Padding(0)
-        Me.ucrReceiverSecondVar.Name = "ucrReceiverSecondVar"
-        Me.ucrReceiverSecondVar.Selector = Nothing
-        Me.ucrReceiverSecondVar.Size = New System.Drawing.Size(120, 20)
-        Me.ucrReceiverSecondVar.TabIndex = 4
-        '
         'ucrReceiverFirstVar
         '
         Me.ucrReceiverFirstVar.frmParent = Me
-        Me.ucrReceiverFirstVar.Location = New System.Drawing.Point(247, 61)
+        Me.ucrReceiverFirstVar.Location = New System.Drawing.Point(251, 33)
         Me.ucrReceiverFirstVar.Margin = New System.Windows.Forms.Padding(0)
         Me.ucrReceiverFirstVar.Name = "ucrReceiverFirstVar"
         Me.ucrReceiverFirstVar.Selector = Nothing
-        Me.ucrReceiverFirstVar.Size = New System.Drawing.Size(120, 87)
-        Me.ucrReceiverFirstVar.TabIndex = 2
+        Me.ucrReceiverFirstVar.Size = New System.Drawing.Size(120, 20)
+        Me.ucrReceiverFirstVar.TabIndex = 4
+        '
+        'ucrReceiverSecondVar
+        '
+        Me.ucrReceiverSecondVar.frmParent = Me
+        Me.ucrReceiverSecondVar.Location = New System.Drawing.Point(251, 103)
+        Me.ucrReceiverSecondVar.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverSecondVar.Name = "ucrReceiverSecondVar"
+        Me.ucrReceiverSecondVar.Selector = Nothing
+        Me.ucrReceiverSecondVar.Size = New System.Drawing.Size(120, 87)
+        Me.ucrReceiverSecondVar.TabIndex = 2
         '
         'ucrSelectorDescribeTwoVar
         '
@@ -103,12 +103,12 @@ Partial Class dlgDescribeTwoVariable
         Me.ucrSelectorDescribeTwoVar.Size = New System.Drawing.Size(210, 180)
         Me.ucrSelectorDescribeTwoVar.TabIndex = 0
         '
-        'ucrBaseDescribeTwoVar
+        'ucrBase
         '
-        Me.ucrBaseDescribeTwoVar.Location = New System.Drawing.Point(10, 230)
-        Me.ucrBaseDescribeTwoVar.Name = "ucrBaseDescribeTwoVar"
-        Me.ucrBaseDescribeTwoVar.Size = New System.Drawing.Size(410, 52)
-        Me.ucrBaseDescribeTwoVar.TabIndex = 9
+        Me.ucrBase.Location = New System.Drawing.Point(10, 230)
+        Me.ucrBase.Name = "ucrBase"
+        Me.ucrBase.Size = New System.Drawing.Size(410, 52)
+        Me.ucrBase.TabIndex = 9
         '
         'ucrChkSaveResult
         '
@@ -136,11 +136,11 @@ Partial Class dlgDescribeTwoVariable
         Me.Controls.Add(Me.lbSecondVariable)
         Me.Controls.Add(Me.lblFirstVariables)
         Me.Controls.Add(Me.cmdDisplayOptions)
-        Me.Controls.Add(Me.ucrReceiverSecondVar)
         Me.Controls.Add(Me.ucrReceiverFirstVar)
+        Me.Controls.Add(Me.ucrReceiverSecondVar)
         Me.Controls.Add(Me.cmdSummaries)
         Me.Controls.Add(Me.ucrSelectorDescribeTwoVar)
-        Me.Controls.Add(Me.ucrBaseDescribeTwoVar)
+        Me.Controls.Add(Me.ucrBase)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
         Me.MaximizeBox = False
         Me.MinimizeBox = False
@@ -153,11 +153,11 @@ Partial Class dlgDescribeTwoVariable
 
     End Sub
 
-    Friend WithEvents ucrBaseDescribeTwoVar As ucrButtons
+    Friend WithEvents ucrBase As ucrButtons
     Friend WithEvents ucrSelectorDescribeTwoVar As ucrSelectorByDataFrameAddRemove
-    Friend WithEvents ucrReceiverFirstVar As ucrReceiverMultiple
+    Friend WithEvents ucrReceiverSecondVar As ucrReceiverMultiple
     Friend WithEvents cmdSummaries As Button
-    Friend WithEvents ucrReceiverSecondVar As ucrReceiverSingle
+    Friend WithEvents ucrReceiverFirstVar As ucrReceiverSingle
     Friend WithEvents cmdDisplayOptions As Button
     Friend WithEvents lblFirstVariables As Label
     Friend WithEvents lbSecondVariable As Label
