6d7db9f75087f6da994893eb66c3dc85ff4b9b1b
Alex Sananka <Alex Sananka@SANANKA>
2017-03-28 12:16:28 +0300

    Minor edit

--- instat/dlgFrequency.vb
+++ instat/dlgFrequency.vb
@@ -14,7 +14,7 @@
 ' You should have received a copy of the GNU General Public License k
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-Imports instat
+
 Imports instat.Translations
 Public Class dlgFrequency
     Private bFirstLoad As Boolean = True
