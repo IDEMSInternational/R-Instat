3c5eeb2602bce25450323cdae1e1fdf0a674f63d
Alex Sananka <Alex Sananka@SANANKA>
2017-03-29 17:08:29 +0300

    adding code

--- instat/dlgRatingScales.vb
+++ instat/dlgRatingScales.vb
@@ -1,5 +1,52 @@
-﻿Public Class dlgRatingScales
+﻿' Instat-R
+' Copyright (C) 2015
+'
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+'
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License k
+' along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Imports instat.Translations
+Public Class dlgRatingScales
+    Private bFirstLoad As Boolean = True
+    Private bReset As Boolean = True
+    Private clsSjplikert, clsSjpStackFrq, clsSjtStackFrq As New RFunction
     Private Sub dlgRatingScales_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        autoTranslate(Me)
+        If bFirstLoad Then
+            InitialiseDialog()
+            bFirstLoad = False
+        End If
+        If bReset Then
+            SetDefaults()
+        End If
+        SetRCodeForControls(bReset)
+        bReset = False
+    End Sub
+
+    Private Sub SetDefaults()
+        ucrReceiverOrderedFactors.SetMeAsReceiver()
+    End Sub
+
+    Private Sub SetRCodeForControls(bReset As Boolean)
+
+    End Sub
+
+    Private Sub InitialiseDialog()
+        ucrReceiverOrderedFactors.Selector = ucrSelectorRatingScale
+        ucrReceiverWeights.Selector = ucrSelectorRatingScale
+
+        ucrChkWeights.SetText("Weights")
+        ucrChkWeights.SetParameter(ucrReceiverWeights.GetParameter(), bNewChangeParameterValue:=False, bNewAddRemoveParameter:=True)
+        ucrChkWeights.AddToLinkedControls(ucrReceiverWeights, {True}, bNewLinkedAddRemoveParameter:=True, bNewLinkedHideIfParameterMissing:=True)
 
     End Sub
 End Class
