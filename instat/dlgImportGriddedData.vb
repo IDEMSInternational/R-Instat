0ddb3471010014a3a3ecf925c53474203aa21e18
Stephen Kogo <stevekogo@gmail.com>
2017-04-03 12:20:55 +0300

    added code to the controls(implemented import CHIRPS)

--- instat/dlgImportGriddedData.vb
+++ instat/dlgImportGriddedData.vb
@@ -18,7 +18,7 @@ Imports instat.Translations
 Public Class dlgImportGriddedData
     Private bFirstLoad As Boolean = True
     Private bReset As Boolean = True
-
+    Private clsRDefaultFunction As New RFunction
     Private Sub dlgImportGriddedData_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         If bFirstLoad Then
             InitialiseDialog()
@@ -33,24 +33,46 @@ Public Class dlgImportGriddedData
         autoTranslate(Me)
         TestOkEnabled()
     End Sub
-    Private Sub SetRCodeForControls(bReset As Boolean)
 
+    Private Sub SetRCodeForControls(bReset As Boolean)
+        SetRCode(Me, ucrBase.clsRsyntax.clsBaseFunction, bReset)
     End Sub
 
     Private Sub InitialiseDialog()
+        ucrInputDataName.SetParameter(New RParameter("data_names", 0))
+        ucrInputDataName.clsParameter.bIncludeArgumentName = False
+
+        ucrNudMinLat.SetParameter(New RParameter("X1", 1))
+        ucrNudMaxLat.SetParameter(New RParameter("X2", 2))
+        ucrNudMinLon.SetParameter(New RParameter("Y1", 3))
+        ucrNudMaxLon.SetParameter(New RParameter("Y2", 4))
 
     End Sub
-    Private Sub SetDefaults()
 
+    Private Sub SetDefaults()
+        clsRDefaultFunction = New RFunction
+        clsRDefaultFunction.SetRCommand(frmMain.clsRLink.strInstatDataObject & "$import_from_iri")
+        'clsRDefaultFunction.AddParameter("nc_data", clsRFunctionParameter:=clsRCDF)
+        'clsRDefaultFunction.AddParameter("data_names", clsRFunctionParameter:=clsRDatanames)
+        ucrBase.clsRsyntax.SetBaseRFunction(clsRDefaultFunction)
     End Sub
 
     Private Sub TestOkEnabled()
-
+        'If (ucrInputDataName.Text <> "" AndAlso ucrInputLocDataName.Text <> "" AndAlso ucrInputFilePath.Text <> "" AndAlso (Not ucrInputDataName.Text = ucrInputLocDataName.Text)) Then
+        If (ucrInputDataName.Text <> "") Then
+            ucrBase.OKEnabled(True)
+        Else
+            ucrBase.OKEnabled(False)
+        End If
     End Sub
 
     Private Sub ucrBase_ClickReset(sender As Object, e As EventArgs) Handles ucrBase.ClickReset
         SetDefaults()
         SetRCodeForControls(True)
+        TestOkEnabled()
+    End Sub
+
+    Private Sub Controls_ControlContentsChanged(ucrChangedControl As ucrCore) Handles ucrInputDataName.ControlContentsChanged
         TestOkEnabled()
     End Sub
 End Class
