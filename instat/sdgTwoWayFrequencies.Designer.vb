b900d954abe1c347d3873aea4d563af2bc613537
shadrack kibet <shadrackkibet36@outlook.com>
2017-04-03 15:27:02 +0300

    created a new subdialogue two Way Frequencies

---
+++ instat/sdgTwoWayFrequencies.Designer.vb
@@ -0,0 +1,52 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class sdgTwoWayFrequencies
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.UcrBase = New instat.ucrButtons()
+        Me.SuspendLayout()
+        '
+        'UcrBase
+        '
+        Me.UcrBase.Location = New System.Drawing.Point(10, 222)
+        Me.UcrBase.Name = "UcrBase"
+        Me.UcrBase.Size = New System.Drawing.Size(407, 52)
+        Me.UcrBase.TabIndex = 0
+        '
+        'sdgTwoWayFrequencies
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(433, 286)
+        Me.Controls.Add(Me.UcrBase)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "sdgTwoWayFrequencies"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "TwoWayFrequencies"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents UcrBase As ucrButtons
+End Class
