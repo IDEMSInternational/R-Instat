bf38113cb8bfb11d93f47a0567954e4cde5f837a
Stephen Kogo <stevekogo@gmail.com>
2017-04-04 12:46:24 +0300

    edits

--- instat/dlgImportGriddedData.vb
+++ instat/dlgImportGriddedData.vb
@@ -24,7 +24,6 @@ Public Class dlgImportGriddedData
             InitialiseDialog()
             bFirstLoad = False
         End If
-
         If bReset Then
             SetDefaults()
         End If
@@ -39,13 +38,12 @@ Public Class dlgImportGriddedData
     End Sub
 
     Private Sub InitialiseDialog()
+        Dim dctDownloadPairs As New Dictionary(Of String, String)
         'ucrBase.iHelpTopicID = 
 
         ucrInputDataName.SetParameter(New RParameter("data_names", 0))
         ucrInputDataName.clsParameter.bIncludeArgumentName = False
 
-
-        Dim dctDownloadPairs As New Dictionary(Of String, String)
         ucrInputDownloadFrom.SetParameter(New RParameter("download_from", 1))
         dctDownloadPairs.Add("CHIRPS", Chr(34) & "CHIRPS" & Chr(34))
         dctDownloadPairs.Add("TAMSAT", Chr(34) & "TAMSAT" & Chr(34))
@@ -55,36 +53,30 @@ Public Class dlgImportGriddedData
         dctDownloadPairs.Add("NASA_TRMM_3B42", Chr(34) & "NASA_TRMM_3B42" & Chr(34))
         ucrInputDownloadFrom.SetItems(dctDownloadPairs)
 
-
         ucrNudMinLat.SetParameter(New RParameter("X1", 2))
         ucrNudMinLat.SetMinMax(-50, 50)
         ucrNudMinLat.DecimalPlaces = 2
         ucrNudMinLat.Increment = 0.01
-        'ucrNudMinLat.bAddRemoveParameter = False
         ucrNudMinLat.SetLinkedDisplayControl(lblMinLat)
         ucrNudMaxLat.SetParameter(New RParameter("X2", 3))
         ucrNudMaxLat.SetMinMax(-50, 50)
         ucrNudMaxLat.DecimalPlaces = 2
         ucrNudMaxLat.Increment = 0.01
-        'ucrNudMaxLat.bAddRemoveParameter = False
         ucrNudMaxLat.SetLinkedDisplayControl(lblMaxLat)
         ucrNudMinLon.SetParameter(New RParameter("Y1", 4))
         ucrNudMinLon.SetMinMax(-180, 180)
         ucrNudMinLon.DecimalPlaces = 2
         ucrNudMinLon.Increment = 0.01
-        'ucrNudMinLon.bAddRemoveParameter = False
         ucrNudMinLon.SetLinkedDisplayControl(lblMinLon)
         ucrNudMaxLon.SetParameter(New RParameter("Y2", 5))
         ucrNudMaxLon.SetMinMax(-180, 180)
         ucrNudMaxLon.DecimalPlaces = 2
         ucrNudMaxLon.Increment = 0.01
-        'ucrNudMaxLon.bAddRemoveParameter = False
         ucrNudMaxLon.SetLinkedDisplayControl(lblMaxLon)
     End Sub
 
     Private Sub SetDefaults()
         clsRDefaultFunction = New RFunction
-
         clsRDefaultFunction.SetRCommand(frmMain.clsRLink.strInstatDataObject & "$import_from_iri")
         clsRDefaultFunction.AddParameter("download_from", Chr(34) & "CHIRPS" & Chr(34))
         clsRDefaultFunction.AddParameter("data_names", "CHIRPS")
