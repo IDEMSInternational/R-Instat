019682445dff31b83c6c59875ea2b8df3eda241e
John Lunalo <johnlunalo95@gmail.com>
2017-04-12 15:14:44 +0300

    Edits

--- instat/dlgName.vb
+++ instat/dlgName.vb
@@ -68,7 +68,7 @@ Public Class dlgName
         DefaultNewName()
         ucrInputNewName.SetParameter(New RParameter("new_val", 2))
         ucrInputNewName.SetValidationTypeAsRVariable()
-        ucrInputVariableLabel.SetParameter(New RParameter("value", 3))
+        ucrInputVariableLabel.SetParameter(New RParameter("label", 3))
     End Sub
 
     Private Sub DefaultNewName()
