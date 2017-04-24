66d297a9da90c5890a96f333d42022f32f279114
steven ndung'u <stevenndungu4@outlook.com>
2017-04-24 12:15:56 +0300

    edit

--- instat/dlgFactorLabels.vb
+++ instat/dlgFactorLabels.vb
@@ -15,7 +15,7 @@
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 Imports instat.Translations
-Public Class dlgFactorLabels
+Public Class dlgViewFactorLabels
     Private bFirstLoad As Boolean = True
     Private bReset As Boolean = True
     Private Sub dlgFactorLabels_Load(sender As Object, e As EventArgs) Handles MyBase.Load
