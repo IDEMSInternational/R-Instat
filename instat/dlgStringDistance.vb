c3673ff20704565a5346d91490b4a973be3683a3
shadrack kibet <shadrackkibet36@outlook.com>
2017-04-24 17:57:16 +0300

    adding code and more designer changes

--- instat/dlgStringDistance.vb
+++ instat/dlgStringDistance.vb
@@ -13,10 +13,12 @@
 '
 ' You should have received a copy of the GNU General Public License k
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
+Imports instat
 Imports instat.Translations
 Public Class dlgStringDistance
     Private bFirstload As Boolean = True
     Private bReset As Boolean = True
+    Private clsStringDistFunction As New RFunction
     Private Sub dlgStringDistance_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         If bFirstload Then
             InitialiseDialog()
@@ -33,24 +35,83 @@ Public Class dlgStringDistance
 
     Private Sub InitialiseDialog()
 
+        'ucrReceiver
+        ucrReceiverStringDistance.SetParameterIsRFunction()
+        ucrReceiverStringDistance.Selector = ucrSelectorStringDistance
+        ucrReceiverStringDistance.SetMeAsReceiver()
+        ucrReceiverStringDistance.SetParameter(New RParameter("a", 0))
+        ucrReceiverStringDistance.SetParameterIsString()
+        ucrInputPatternStringDistance.SetParameter(New RParameter("b", 1))
+        ucrInputPatternStringDistance.AddQuotesIfUnrecognised = True
+
+        ucrInputComboBoxMethod.SetParameter(New RParameter("method", 2))
+
+        Dim dctMethod As New Dictionary(Of String, String)
+        dctMethod.Add("Optimal string aligment", Chr(34) & "osa" & Chr(34))
+        dctMethod.Add("Levenshtein distance", Chr(34) & "lv" & Chr(34))
+        dctMethod.Add("Full Damerau-Levenshtein distance", Chr(34) & "dl" & Chr(34))
+        dctMethod.Add("Hamming distance", Chr(34) & "hamming" & Chr(34))
+        dctMethod.Add("Longest common substring distance", Chr(34) & "lcs" & Chr(34))
+        dctMethod.Add("q-gram distance", Chr(34) & "qgram" & Chr(34))
+        dctMethod.Add("cosine distance", Chr(34) & "cosine" & Chr(34))
+        dctMethod.Add("jaccard Distance", Chr(34) & "jaccard" & Chr(34))
+        dctMethod.Add("Jaro ,or Jaro-Winker distance", Chr(34) & "jw" & Chr(34))
+        dctMethod.Add("soundex encoding", Chr(34) & "soundex" & Chr(34))
+        ucrInputComboBoxMethod.SetItems(dctMethod)
+
+        'ucrSave
+        ucrSaveStringDistance.SetPrefix("string_distance")
+        ucrSaveStringDistance.SetSaveTypeAsColumn()
+        ucrSaveStringDistance.SetDataFrameSelector(ucrSelectorStringDistance.ucrAvailableDataFrames)
+        ucrSaveStringDistance.SetIsTextBox()
+        ucrSaveStringDistance.SetLabelText("Save Result")
+
+
+        clsStringDistFunction.SetAssignTo(ucrSaveStringDistance.GetText, strTempDataframe:=ucrSelectorStringDistance.ucrAvailableDataFrames.cboAvailableDataFrames.Text, strTempColumn:=ucrSaveStringDistance.GetText, bAssignToIsPrefix:=True)
+        ucrBase.clsRsyntax.SetBaseRFunction(clsStringDistFunction)
+
+
+
+
+
 
     End Sub
 
     Private Sub SetDefaults()
 
-    End Sub
-
-    Private Sub TestOkEnabled()
+        clsStringDistFunction = New RFunction
+        ucrSelectorStringDistance.Reset()
+        ucrInputPatternStringDistance.Reset()
+        ucrSaveStringDistance.Reset()
+        clsStringDistFunction.SetPackageName("stringdist")
+        clsStringDistFunction.SetRCommand("stringdist")
+        ucrBase.clsRsyntax.SetBaseRFunction(clsStringDistFunction)
 
     End Sub
 
     Private Sub SetRCodeForControls(bReset As Boolean)
+        SetRCode(Me, ucrBase.clsRsyntax.clsBaseFunction, bReset)
 
     End Sub
 
-    Private Sub ucrBase_ClickReset(sender As Object, e As EventArgs) Handles UcrBaseStringDistance.ClickReset
+    Private Sub TestOkEnabled()
+        If Not ucrReceiverStringDistance.IsEmpty() AndAlso Not ucrInputPatternStringDistance.IsEmpty() AndAlso Not ucrInputComboBoxMethod.IsEmpty AndAlso ucrSaveStringDistance.IsComplete() Then
+            ucrBase.OKEnabled(True)
+        Else
+            ucrBase.OKEnabled(False)
+        End If
+
+    End Sub
+
+
+    Private Sub ucrBase_ClickReset(sender As Object, e As EventArgs) Handles ucrBase.ClickReset
         SetDefaults()
         SetRCodeForControls(True)
         TestOkEnabled()
     End Sub
+
+    Private Sub UcrReceiverStringDistance_ControlContentsChanged(ucrChangedControl As ucrCore) Handles ucrReceiverStringDistance.ControlContentsChanged
+        TestOkEnabled()
+    End Sub
+
 End Class
