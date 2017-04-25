6544d7b90f04df5c4aae90f55d8a08466830f774
John Lunalo <johnlunalo95@gmail.com>
2017-04-25 15:23:36 +0300

    Updating Link dialog

--- instat/dlgAddLink.vb
+++ instat/dlgAddLink.vb
@@ -15,10 +15,11 @@
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 Imports instat.Translations
+Imports RDotNet
 Public Class dlgAddLink
     Public bFirstLoad As Boolean = True
     Private bReset As Boolean = True
-
+    Private clsGetKeyNames As RFunction
     Private Sub dlgAddLink_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         autoTranslate(Me)
         If bFirstLoad Then
@@ -35,13 +36,24 @@ Public Class dlgAddLink
 
     Private Sub InitialiseDialog()
         ucrBase.iHelpTopicID = 506
+        ucrDataSelectorTo.SetParameter(New RParameter("data_name", 0))
+        ucrBase.clsRsyntax.iCallType = 2
+
     End Sub
 
     Private Sub SetDefaults()
-        Dim clsDefaultFunction As New RFunction
+        Dim lstOutput As String
+        clsGetKeyNames = New RFunction
+        ucrDataSelectorFrom.Reset()
+        ucrDataSelectorTo.Reset()
+        clsGetKeyNames.AddParameter("data_name", Chr(34) & ucrDataSelectorTo.cboAvailableDataFrames.SelectedItem & Chr(34))
+        clsGetKeyNames.SetRCommand(frmMain.clsRLink.strInstatDataObject & "$get_keys")
+        lstOutput = frmMain.clsRLink.RunInternalScriptGetValue(clsGetKeyNames.ToScript).AsCharacter(0)
+        lvwLinkViewBox.Items.Add(lstOutput)
+        lvwLinkViewBox.Columns.Add("Key", 50, HorizontalAlignment.Left)
+        lvwLinkViewBox.Columns.Add("Column Name", 100, HorizontalAlignment.Left)
+        ucrBase.clsRsyntax.SetBaseRFunction(clsGetKeyNames)
 
-        'clsDefaultFunction.SetRCommand()
-        'ucrBase.clsRsyntax.SetBaseRFunction(clsDefaultFunction)
     End Sub
 
     Private Sub SetRCodeForControls(bReset As Boolean)
