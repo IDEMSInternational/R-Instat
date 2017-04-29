a23c94864acb0c1fbdabb79a727e435d71bccd5c
shadrack kibet <shadrackkibet36@outlook.com>
2017-04-29 22:45:04 +0300

    Designer changes with some  code additions

--- instat/dlgStringHandling.Designer.vb
+++ instat/dlgStringHandling.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class dlgStringHandling
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class dlgStringHandling
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.lblColumn = New System.Windows.Forms.Label()
         Me.lblReplaceBy = New System.Windows.Forms.Label()
@@ -157,9 +157,10 @@ Partial Class dlgStringHandling
         Me.rdoReplaceAll.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.rdoReplaceAll.Location = New System.Drawing.Point(313, 12)
         Me.rdoReplaceAll.Name = "rdoReplaceAll"
-        Me.rdoReplaceAll.Size = New System.Drawing.Size(85, 27)
+        Me.rdoReplaceAll.Size = New System.Drawing.Size(84, 27)
         Me.rdoReplaceAll.TabIndex = 15
         Me.rdoReplaceAll.TabStop = True
+        Me.rdoReplaceAll.Text = "Replace All"
         Me.rdoReplaceAll.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         Me.rdoReplaceAll.UseVisualStyleBackColor = True
         '
