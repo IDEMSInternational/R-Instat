0bc314bc4963f8cdde670d0936320ea2651a1898
steven ndung'u <stevenndungu4@outlook.com>
2017-05-03 19:06:00 +0300

    Changing name of the dialogues

--- instat/dlgCopyDataFrame.Designer.vb
+++ instat/dlgCopyDataFrame.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class dlgRenameSheet
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class dlgCopyDataFrame
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,14 +20,14 @@ Partial Class dlgRenameSheet
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.ucrBase = New instat.ucrButtons()
-        Me.ucrDataFrameToRename = New instat.ucrDataFrame()
-        Me.lblNewName = New System.Windows.Forms.Label()
-        Me.ucrInputNewName = New instat.ucrInputTextBox()
-        Me.lblLabel = New System.Windows.Forms.Label()
+        Me.ucrDataFrameCopySheets = New instat.ucrDataFrame()
         Me.ucrInputLabel = New instat.ucrInputTextBox()
+        Me.lblLabel = New System.Windows.Forms.Label()
+        Me.ucrInputNewName = New instat.ucrInputTextBox()
+        Me.lblNewName = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'ucrBase
@@ -37,42 +37,14 @@ Partial Class dlgRenameSheet
         Me.ucrBase.Size = New System.Drawing.Size(410, 52)
         Me.ucrBase.TabIndex = 5
         '
-        'ucrDataFrameToRename
+        'ucrDataFrameCopySheets
         '
-        Me.ucrDataFrameToRename.bUseCurrentFilter = True
-        Me.ucrDataFrameToRename.Location = New System.Drawing.Point(10, 10)
-        Me.ucrDataFrameToRename.Margin = New System.Windows.Forms.Padding(0)
-        Me.ucrDataFrameToRename.Name = "ucrDataFrameToRename"
-        Me.ucrDataFrameToRename.Size = New System.Drawing.Size(120, 40)
-        Me.ucrDataFrameToRename.TabIndex = 0
-        '
-        'lblNewName
-        '
-        Me.lblNewName.Location = New System.Drawing.Point(196, 14)
-        Me.lblNewName.Name = "lblNewName"
-        Me.lblNewName.Size = New System.Drawing.Size(100, 26)
-        Me.lblNewName.TabIndex = 1
-        Me.lblNewName.Tag = "New_Name"
-        Me.lblNewName.Text = "New Name:"
-        '
-        'ucrInputNewName
-        '
-        Me.ucrInputNewName.AddQuotesIfUnrecognised = True
-        Me.ucrInputNewName.IsMultiline = False
-        Me.ucrInputNewName.IsReadOnly = False
-        Me.ucrInputNewName.Location = New System.Drawing.Point(196, 29)
-        Me.ucrInputNewName.Name = "ucrInputNewName"
-        Me.ucrInputNewName.Size = New System.Drawing.Size(131, 33)
-        Me.ucrInputNewName.TabIndex = 2
-        '
-        'lblLabel
-        '
-        Me.lblLabel.AutoSize = True
-        Me.lblLabel.Location = New System.Drawing.Point(196, 64)
-        Me.lblLabel.Name = "lblLabel"
-        Me.lblLabel.Size = New System.Drawing.Size(94, 13)
-        Me.lblLabel.TabIndex = 3
-        Me.lblLabel.Text = "Data Frame Label:"
+        Me.ucrDataFrameCopySheets.bUseCurrentFilter = True
+        Me.ucrDataFrameCopySheets.Location = New System.Drawing.Point(10, 10)
+        Me.ucrDataFrameCopySheets.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrDataFrameCopySheets.Name = "ucrDataFrameCopySheets"
+        Me.ucrDataFrameCopySheets.Size = New System.Drawing.Size(120, 40)
+        Me.ucrDataFrameCopySheets.TabIndex = 0
         '
         'ucrInputLabel
         '
@@ -84,7 +56,35 @@ Partial Class dlgRenameSheet
         Me.ucrInputLabel.Size = New System.Drawing.Size(131, 32)
         Me.ucrInputLabel.TabIndex = 4
         '
-        'dlgRenameSheet
+        'lblLabel
+        '
+        Me.lblLabel.AutoSize = True
+        Me.lblLabel.Location = New System.Drawing.Point(196, 64)
+        Me.lblLabel.Name = "lblLabel"
+        Me.lblLabel.Size = New System.Drawing.Size(94, 13)
+        Me.lblLabel.TabIndex = 3
+        Me.lblLabel.Text = "Data Frame Label:"
+        '
+        'ucrInputNewName
+        '
+        Me.ucrInputNewName.AddQuotesIfUnrecognised = True
+        Me.ucrInputNewName.IsMultiline = False
+        Me.ucrInputNewName.IsReadOnly = False
+        Me.ucrInputNewName.Location = New System.Drawing.Point(196, 29)
+        Me.ucrInputNewName.Name = "ucrInputNewName"
+        Me.ucrInputNewName.Size = New System.Drawing.Size(131, 33)
+        Me.ucrInputNewName.TabIndex = 2
+        '
+        'lblNewName
+        '
+        Me.lblNewName.Location = New System.Drawing.Point(196, 14)
+        Me.lblNewName.Name = "lblNewName"
+        Me.lblNewName.Size = New System.Drawing.Size(100, 26)
+        Me.lblNewName.TabIndex = 1
+        Me.lblNewName.Tag = "New_Name"
+        Me.lblNewName.Text = "New Name:"
+        '
+        'dlgCopyDataFrame
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
@@ -92,24 +92,24 @@ Partial Class dlgRenameSheet
         Me.Controls.Add(Me.ucrInputLabel)
         Me.Controls.Add(Me.lblLabel)
         Me.Controls.Add(Me.ucrInputNewName)
-        Me.Controls.Add(Me.ucrBase)
         Me.Controls.Add(Me.lblNewName)
-        Me.Controls.Add(Me.ucrDataFrameToRename)
+        Me.Controls.Add(Me.ucrDataFrameCopySheets)
+        Me.Controls.Add(Me.ucrBase)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.MaximizeBox = False
         Me.MinimizeBox = False
-        Me.Name = "dlgRenameSheet"
+        Me.Name = "dlgCopyDataFrame"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "Rename Sheet"
+        Me.Text = "Copy Data Frame"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents ucrDataFrameToRename As ucrDataFrame
     Friend WithEvents ucrBase As ucrButtons
-    Friend WithEvents lblNewName As Label
-    Friend WithEvents ucrInputNewName As ucrInputTextBox
-    Friend WithEvents lblLabel As Label
+    Friend WithEvents ucrDataFrameCopySheets As ucrDataFrame
     Friend WithEvents ucrInputLabel As ucrInputTextBox
+    Friend WithEvents lblLabel As Label
+    Friend WithEvents ucrInputNewName As ucrInputTextBox
+    Friend WithEvents lblNewName As Label
 End Class
