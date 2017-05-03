c0eff1f4d9c656fc5717e9ed66146b31ebd19f97
Alex Sananka <sananka43@gmail.com>
2017-05-03 15:03:34 +0300

    minor layout changes

--- instat/dlgExportRObjects.Designer.vb
+++ instat/dlgExportRObjects.Designer.vb
@@ -23,20 +23,45 @@ Partial Class dlgExportRObjects
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.ucrBase = New instat.ucrButtons()
+        Me.ucrSelectorObjects = New instat.ucrSelectorByDataFrameAddRemove()
+        Me.ucrReceiverObjects = New instat.ucrReceiverMultiple()
         Me.SuspendLayout()
         '
         'ucrBase
         '
-        Me.ucrBase.Location = New System.Drawing.Point(7, 256)
+        Me.ucrBase.Location = New System.Drawing.Point(7, 320)
         Me.ucrBase.Name = "ucrBase"
         Me.ucrBase.Size = New System.Drawing.Size(398, 52)
         Me.ucrBase.TabIndex = 1
+        '
+        'ucrSelectorObjects
+        '
+        Me.ucrSelectorObjects.bShowHiddenColumns = False
+        Me.ucrSelectorObjects.bUseCurrentFilter = True
+        Me.ucrSelectorObjects.Location = New System.Drawing.Point(10, 10)
+        Me.ucrSelectorObjects.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrSelectorObjects.Name = "ucrSelectorObjects"
+        Me.ucrSelectorObjects.Size = New System.Drawing.Size(210, 180)
+        Me.ucrSelectorObjects.TabIndex = 2
+        '
+        'ucrReceiverObjects
+        '
+        Me.ucrReceiverObjects.frmParent = Me
+        Me.ucrReceiverObjects.Location = New System.Drawing.Point(264, 60)
+        Me.ucrReceiverObjects.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverObjects.Name = "ucrReceiverObjects"
+        Me.ucrReceiverObjects.Selector = Nothing
+        Me.ucrReceiverObjects.Size = New System.Drawing.Size(120, 100)
+        Me.ucrReceiverObjects.TabIndex = 3
+        Me.ucrReceiverObjects.ucrSelector = Nothing
         '
         'dlgExportRObjects
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(417, 320)
+        Me.ClientSize = New System.Drawing.Size(426, 384)
+        Me.Controls.Add(Me.ucrReceiverObjects)
+        Me.Controls.Add(Me.ucrSelectorObjects)
         Me.Controls.Add(Me.ucrBase)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.MaximizeBox = False
@@ -48,4 +73,6 @@ Partial Class dlgExportRObjects
 
     End Sub
     Friend WithEvents ucrBase As ucrButtons
+    Friend WithEvents ucrSelectorObjects As ucrSelectorByDataFrameAddRemove
+    Friend WithEvents ucrReceiverObjects As ucrReceiverMultiple
 End Class
