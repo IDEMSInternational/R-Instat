693ddb05315dbabca71a50d6b47da479c19b13bf
Alex Sananka <sananka43@gmail.com>
2017-05-03 16:42:44 +0300

    Adding code and more designer changes

--- instat/dlgExportRObjects.Designer.vb
+++ instat/dlgExportRObjects.Designer.vb
@@ -25,11 +25,17 @@ Partial Class dlgExportRObjects
         Me.ucrBase = New instat.ucrButtons()
         Me.ucrSelectorObjects = New instat.ucrSelectorByDataFrameAddRemove()
         Me.ucrReceiverObjects = New instat.ucrReceiverMultiple()
+        Me.grpSaveOptions = New System.Windows.Forms.GroupBox()
+        Me.ucrInputFile = New instat.ucrInputTextBox()
+        Me.rdoBrowse = New System.Windows.Forms.Button()
+        Me.lblFile = New System.Windows.Forms.Label()
+        Me.lblObjects = New System.Windows.Forms.Label()
+        Me.grpSaveOptions.SuspendLayout()
         Me.SuspendLayout()
         '
         'ucrBase
         '
-        Me.ucrBase.Location = New System.Drawing.Point(7, 320)
+        Me.ucrBase.Location = New System.Drawing.Point(12, 272)
         Me.ucrBase.Name = "ucrBase"
         Me.ucrBase.Size = New System.Drawing.Size(398, 52)
         Me.ucrBase.TabIndex = 1
@@ -55,11 +61,61 @@ Partial Class dlgExportRObjects
         Me.ucrReceiverObjects.TabIndex = 3
         Me.ucrReceiverObjects.ucrSelector = Nothing
         '
+        'grpSaveOptions
+        '
+        Me.grpSaveOptions.Controls.Add(Me.ucrInputFile)
+        Me.grpSaveOptions.Controls.Add(Me.rdoBrowse)
+        Me.grpSaveOptions.Controls.Add(Me.lblFile)
+        Me.grpSaveOptions.Location = New System.Drawing.Point(10, 204)
+        Me.grpSaveOptions.Name = "grpSaveOptions"
+        Me.grpSaveOptions.Size = New System.Drawing.Size(398, 56)
+        Me.grpSaveOptions.TabIndex = 6
+        Me.grpSaveOptions.TabStop = False
+        '
+        'ucrInputFile
+        '
+        Me.ucrInputFile.AddQuotesIfUnrecognised = True
+        Me.ucrInputFile.IsMultiline = False
+        Me.ucrInputFile.IsReadOnly = False
+        Me.ucrInputFile.Location = New System.Drawing.Point(40, 24)
+        Me.ucrInputFile.Name = "ucrInputFile"
+        Me.ucrInputFile.Size = New System.Drawing.Size(251, 21)
+        Me.ucrInputFile.TabIndex = 1
+        '
+        'rdoBrowse
+        '
+        Me.rdoBrowse.Location = New System.Drawing.Point(296, 23)
+        Me.rdoBrowse.Name = "rdoBrowse"
+        Me.rdoBrowse.Size = New System.Drawing.Size(62, 23)
+        Me.rdoBrowse.TabIndex = 2
+        Me.rdoBrowse.Text = "Browse"
+        Me.rdoBrowse.UseVisualStyleBackColor = True
+        '
+        'lblFile
+        '
+        Me.lblFile.AutoSize = True
+        Me.lblFile.Location = New System.Drawing.Point(11, 27)
+        Me.lblFile.Name = "lblFile"
+        Me.lblFile.Size = New System.Drawing.Size(26, 13)
+        Me.lblFile.TabIndex = 0
+        Me.lblFile.Text = "File:"
+        '
+        'lblObjects
+        '
+        Me.lblObjects.AutoSize = True
+        Me.lblObjects.Location = New System.Drawing.Point(264, 42)
+        Me.lblObjects.Name = "lblObjects"
+        Me.lblObjects.Size = New System.Drawing.Size(91, 13)
+        Me.lblObjects.TabIndex = 7
+        Me.lblObjects.Text = "Selected Objects:"
+        '
         'dlgExportRObjects
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(426, 384)
+        Me.ClientSize = New System.Drawing.Size(412, 328)
+        Me.Controls.Add(Me.lblObjects)
+        Me.Controls.Add(Me.grpSaveOptions)
         Me.Controls.Add(Me.ucrReceiverObjects)
         Me.Controls.Add(Me.ucrSelectorObjects)
         Me.Controls.Add(Me.ucrBase)
@@ -69,10 +125,18 @@ Partial Class dlgExportRObjects
         Me.Name = "dlgExportRObjects"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Export R Objects"
+        Me.grpSaveOptions.ResumeLayout(False)
+        Me.grpSaveOptions.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents ucrBase As ucrButtons
     Friend WithEvents ucrSelectorObjects As ucrSelectorByDataFrameAddRemove
     Friend WithEvents ucrReceiverObjects As ucrReceiverMultiple
+    Friend WithEvents grpSaveOptions As GroupBox
+    Friend WithEvents ucrInputFile As ucrInputTextBox
+    Friend WithEvents rdoBrowse As Button
+    Friend WithEvents lblFile As Label
+    Friend WithEvents lblObjects As Label
 End Class
