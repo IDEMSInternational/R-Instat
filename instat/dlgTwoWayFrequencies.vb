e5a3b6ef7cd7afeabc8284d9c2c424eb11918032
Alex Sananka <sananka43@gmail.com>
2017-05-09 14:58:04 +0300

    changing prefix

--- instat/dlgTwoWayFrequencies.vb
+++ instat/dlgTwoWayFrequencies.vb
@@ -128,7 +128,7 @@ Public Class dlgTwoWayFrequencies
         ucrPnlFreqDisplay.AddToLinkedControls(ucrSaveGraph, {rdoGraph, rdoBoth}, bNewLinkedHideIfParameterMissing:=True)
         ucrPnlFreqType.SetLinkedDisplayControl(grpFreqTypeGraph)
 
-        ucrSaveGraph.SetPrefix("Graph")
+        ucrSaveGraph.SetPrefix("Two_way_freq")
         ucrSaveGraph.SetSaveTypeAsGraph()
         ucrSaveGraph.SetDataFrameSelector(ucrSelectorTwoWayFrequencies.ucrAvailableDataFrames)
         ucrSaveGraph.SetCheckBoxText("Save Graph")
