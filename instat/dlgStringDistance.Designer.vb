0d01546c7960b67d02319cf342a9c520472aefde
shadrack kibet <shadrackkibet36@outlook.com>
2017-05-10 10:42:16 +0300

    designer enhancement

--- instat/dlgStringDistance.Designer.vb
+++ instat/dlgStringDistance.Designer.vb
@@ -31,6 +31,11 @@ Partial Class dlgStringDistance
         Me.ucrInputPatternStringDistance = New instat.ucrInputTextBox()
         Me.ucrReceiverStringDistance = New instat.ucrReceiverSingle()
         Me.ucrBase = New instat.ucrButtons()
+        Me.rdoString = New System.Windows.Forms.RadioButton()
+        Me.Column = New System.Windows.Forms.RadioButton()
+        Me.ucrPnlStringDist = New instat.UcrPanel()
+        Me.UcrReceiverSingle1 = New instat.ucrReceiverSingle()
+        Me.lblColumnString = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'lblColumn
@@ -114,11 +119,74 @@ Partial Class dlgStringDistance
         Me.ucrBase.Size = New System.Drawing.Size(399, 52)
         Me.ucrBase.TabIndex = 8
         '
+        'rdoString
+        '
+        Me.rdoString.Appearance = System.Windows.Forms.Appearance.Button
+        Me.rdoString.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoString.FlatAppearance.BorderSize = 2
+        Me.rdoString.FlatAppearance.CheckedBackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoString.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.rdoString.Location = New System.Drawing.Point(250, 11)
+        Me.rdoString.Name = "rdoString"
+        Me.rdoString.Size = New System.Drawing.Size(61, 27)
+        Me.rdoString.TabIndex = 9
+        Me.rdoString.TabStop = True
+        Me.rdoString.Text = "String"
+        Me.rdoString.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.rdoString.UseVisualStyleBackColor = True
+        '
+        'Column
+        '
+        Me.Column.Appearance = System.Windows.Forms.Appearance.Button
+        Me.Column.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Column.FlatAppearance.BorderSize = 2
+        Me.Column.FlatAppearance.CheckedBackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Column.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Column.Location = New System.Drawing.Point(309, 11)
+        Me.Column.Name = "Column"
+        Me.Column.Size = New System.Drawing.Size(61, 27)
+        Me.Column.TabIndex = 10
+        Me.Column.TabStop = True
+        Me.Column.Text = "Column"
+        Me.Column.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.Column.UseVisualStyleBackColor = True
+        '
+        'ucrPnlStringDist
+        '
+        Me.ucrPnlStringDist.Location = New System.Drawing.Point(242, 2)
+        Me.ucrPnlStringDist.Name = "ucrPnlStringDist"
+        Me.ucrPnlStringDist.Size = New System.Drawing.Size(145, 42)
+        Me.ucrPnlStringDist.TabIndex = 11
+        '
+        'UcrReceiverSingle1
+        '
+        Me.UcrReceiverSingle1.frmParent = Me
+        Me.UcrReceiverSingle1.Location = New System.Drawing.Point(250, 106)
+        Me.UcrReceiverSingle1.Margin = New System.Windows.Forms.Padding(0)
+        Me.UcrReceiverSingle1.Name = "UcrReceiverSingle1"
+        Me.UcrReceiverSingle1.Selector = Nothing
+        Me.UcrReceiverSingle1.Size = New System.Drawing.Size(137, 20)
+        Me.UcrReceiverSingle1.TabIndex = 12
+        Me.UcrReceiverSingle1.ucrSelector = Nothing
+        '
+        'lblColumnString
+        '
+        Me.lblColumnString.AutoSize = True
+        Me.lblColumnString.Location = New System.Drawing.Point(250, 90)
+        Me.lblColumnString.Name = "lblColumnString"
+        Me.lblColumnString.Size = New System.Drawing.Size(51, 13)
+        Me.lblColumnString.TabIndex = 13
+        Me.lblColumnString.Text = "Column 2"
+        '
         'dlgStringDistance
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(417, 285)
+        Me.Controls.Add(Me.lblColumnString)
+        Me.Controls.Add(Me.UcrReceiverSingle1)
+        Me.Controls.Add(Me.Column)
+        Me.Controls.Add(Me.rdoString)
         Me.Controls.Add(Me.ucrSelectorStringDistance)
         Me.Controls.Add(Me.lblMethod)
         Me.Controls.Add(Me.ucrInputComboBoxMethod)
@@ -128,6 +196,7 @@ Partial Class dlgStringDistance
         Me.Controls.Add(Me.ucrInputPatternStringDistance)
         Me.Controls.Add(Me.ucrReceiverStringDistance)
         Me.Controls.Add(Me.ucrBase)
+        Me.Controls.Add(Me.ucrPnlStringDist)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.MaximizeBox = False
         Me.MinimizeBox = False
@@ -148,4 +217,9 @@ Partial Class dlgStringDistance
     Friend WithEvents ucrSaveStringDistance As ucrSave
     Friend WithEvents ucrInputPatternStringDistance As ucrInputTextBox
     Friend WithEvents ucrSelectorStringDistance As ucrSelectorByDataFrameAddRemove
+    Friend WithEvents lblColumnString As Label
+    Friend WithEvents UcrReceiverSingle1 As ucrReceiverSingle
+    Friend WithEvents Column As RadioButton
+    Friend WithEvents rdoString As RadioButton
+    Friend WithEvents ucrPnlStringDist As UcrPanel
 End Class
