c9c4f1781362414751a377aa855afe66e1581e03
Danny Parsons <danny@aims.ac.za>
2017-05-17 23:10:58 +0100

    corrected display daily menu item

--- instat/frmMain.Designer.vb
+++ instat/frmMain.Designer.vb
@@ -3689,7 +3689,7 @@ Partial Class frmMain
         '
         Me.DispalyClimaticDataToolStripMenuItem.Name = "DispalyClimaticDataToolStripMenuItem"
         Me.DispalyClimaticDataToolStripMenuItem.Size = New System.Drawing.Size(195, 22)
-        Me.DispalyClimaticDataToolStripMenuItem.Text = "Dispaly Climatic Data..."
+        Me.DispalyClimaticDataToolStripMenuItem.Text = "Display Dailycorr Climatic Data..."
         '
         'frmMain
         '
