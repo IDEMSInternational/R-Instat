9dc74d376b63c632e4025b0808d18359984a049f
Steven <stevenndungu4@gmail.com>
2017-05-19 10:00:17 +0300

    edit

--- instat/dlgRowSummary.Designer.vb
+++ instat/dlgRowSummary.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class dlgRowSummaries
+Partial Class dlgRowSummary
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
