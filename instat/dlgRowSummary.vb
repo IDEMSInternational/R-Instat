5cf1f6f2d0ed3245e45d89ee2b4a1827560a7925
Steven <stevenndungu4@gmail.com>
2017-05-19 10:00:17 +0300

    edit

--- instat/dlgRowSummary.vb
+++ instat/dlgRowSummary.vb
@@ -14,12 +14,12 @@
 ' You should have received a copy of the GNU General Public License k
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 Imports instat.Translations
-Public Class dlgRowSummaries
+Public Class dlgRowSummary
     Private bFirstLoad As Boolean = True
     Private bReset As Boolean = True
     Private clsDefaultFunction As New RFunction
 
-    Private Sub dlgRowSummaries_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Private Sub dlgRowSummary_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         If bFirstLoad Then
             InitialiseDialog()
             bFirstLoad = False
