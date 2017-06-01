181c2313e5a23a11c9c8bddd6a3ea77daa056021
Danny Parsons <danny@aims.ac.za>
2017-06-01 16:22:47 +0100

    added missing code

--- instat/ucrInputComboBox.vb
+++ instat/ucrInputComboBox.vb
@@ -169,6 +169,10 @@ Public Class ucrInputComboBox
     Private Sub ucrInputComboBox_Load(sender As Object, e As EventArgs) Handles Me.Load
         bAllowNonConditionValues = False
         FillItemTypes()
+        If bFirstLoad Then
+            SetDropDownStyleAsEditable(True)
+            bFirstLoad = False
+        End If
     End Sub
 
     Private Sub cboInput_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboInput.SelectedIndexChanged
@@ -217,5 +221,23 @@ Public Class ucrInputComboBox
 
     Public Overrides Sub UpdateControl(Optional bReset As Boolean = False)
         MyBase.UpdateControl(bReset)
+    End Sub
+
+    Public Sub SetDropDownStyleAsNonEditable()
+        cboInput.DropDownStyle = ComboBoxStyle.DropDownList
+        cboInput.AutoCompleteMode = AutoCompleteMode.None
+        cboInput.AutoCompleteSource = AutoCompleteSource.None
+    End Sub
+
+    Public Sub SetDropDownStyleAsEditable(bAdditionsAllowed As Boolean)
+        cboInput.DropDownStyle = ComboBoxStyle.DropDown
+        cboInput.AutoCompleteMode = AutoCompleteMode.Append
+        cboInput.AutoCompleteSource = AutoCompleteSource.ListItems
+        'TODO implement validation settings for this		
+        If bAdditionsAllowed Then
+
+        Else
+
+        End If
     End Sub
 End Class
