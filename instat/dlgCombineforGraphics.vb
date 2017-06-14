98fbc5ff5aa267f6309c6dc2c14e0789dadaffcd
maxwell fundi <maxwellfundi@live.com>
2017-06-14 16:57:12 +0300

    Combine graph

--- instat/dlgCombineforGraphics.vb
+++ instat/dlgCombineforGraphics.vb
@@ -89,15 +89,11 @@ Public Class dlgCombineforGraphics
     Private Sub cmdLayout_Click(sender As Object, e As EventArgs) Handles cmdOptions.Click
         'this SetRFunction is still empty in subdialog but will be fixed soon.
         sdgCombineGraphOptions.SetRFunction(ucrBase.clsRsyntax.clsBaseFunction, bResetSubDialog)
-        bResetSubDialog = False
         sdgCombineGraphOptions.ShowDialog()
+        bResetSubDialog = False
     End Sub
 
     Private Sub AllControls_ControlContentsChanged() Handles ucrCombineGraphReceiver.ControlContentsChanged, ucrSave.ControlContentsChanged
         TestOkEnabled()
-    End Sub
-
-    Private Sub AllControls_ControlContentsChanged(ucrChangedControl As ucrCore) Handles ucrSave.ControlContentsChanged, ucrCombineGraphReceiver.ControlContentsChanged
-
     End Sub
 End Class
