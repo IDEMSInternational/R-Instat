4afdabfe2a68b065e7f4c9f1a17841cb5caf2577
Danny Parsons <danny@aims.ac.za>
2017-06-19 18:04:26 +0100

    adding multiple sets of columns option to stack dialog

--- instat/dlgStack.vb
+++ instat/dlgStack.vb
@@ -13,11 +13,15 @@
 ' You should have received a copy of the GNU General Public License k
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+Imports instat
 Imports instat.Translations
 Public Class dlgStack
     Private clsMeltFunction As New RFunction
+    Private clsReshapeFunction As RFunction
+    Private clsSplitColumnsInGroups As RFunction
     Private bFirstLoad As Boolean = True
     Private bReset As Boolean = True
+
     Private Sub dlgStack_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         autoTranslate(Me)
         If bFirstLoad Then
@@ -60,10 +64,23 @@ Public Class dlgStack
         ucrFactorInto.SetParameter(New RParameter("variable.name", 3))
         ucrFactorInto.SetValidationTypeAsRVariable()
         ucrFactorInto.SetRDefault(Chr(34) & "variable" & Chr(34))
+        ucrFactorInto.SetLinkedDisplayControl(lblFactorInto)
 
         ucrStackDataInto.SetParameter(New RParameter("value.name", 4))
         ucrStackDataInto.SetValidationTypeAsRVariable()
         ucrStackDataInto.SetRDefault(Chr(34) & "value" & Chr(34))
+        ucrStackDataInto.SetLinkedDisplayControl(lblStackDataInto)
+
+        ucrChkStackMultipleSets.SetText("Stack multiple column sets")
+        ucrChkStackMultipleSets.AddToLinkedControls(ucrNudNoSets, {True}, bNewLinkedHideIfParameterMissing:=True, bNewLinkedChangeToDefaultState:=True, objNewDefaultState:=2)
+        ucrChkStackMultipleSets.AddToLinkedControls(ucrChkCarryColumns, {False}, bNewLinkedHideIfParameterMissing:=True)
+        ucrChkStackMultipleSets.AddToLinkedControls(ucrStackDataInto, {False}, bNewLinkedHideIfParameterMissing:=True)
+        ucrChkStackMultipleSets.AddFunctionNamesCondition(True, "reshape")
+        ucrChkStackMultipleSets.AddFunctionNamesCondition(False, "melt")
+
+        ucrNudNoSets.SetParameter(New RParameter("num", 1))
+        ucrNudNoSets.Minimum = 2
+        ucrNudNoSets.SetLinkedDisplayControl(lblSets)
 
         ucrSaveNewDataName.SetIsTextBox()
         ucrSaveNewDataName.SetLabelText("New Data Frame Name:")
@@ -73,21 +90,49 @@ Public Class dlgStack
 
     Private Sub SetDefaults()
         clsMeltFunction = New RFunction
+        clsReshapeFunction = New RFunction
+        clsSplitColumnsInGroups = New RFunction
 
         ucrSelectorStack.Reset()
         ucrSaveNewDataName.Reset()
         ucrReceiverColumnsToBeStack.SetMeAsReceiver()
 
-        clsMeltFunction.SetPackageName("data.table")
+        clsMeltFunction.SetPackageName("reshape2")
         clsMeltFunction.SetRCommand("melt")
         clsMeltFunction.AddParameter("id.vars", "NULL")
+
         clsMeltFunction.SetAssignTo(ucrSelectorStack.ucrAvailableDataFrames.cboAvailableDataFrames.Text & "_stacked", strTempDataframe:=ucrSelectorStack.ucrAvailableDataFrames.cboAvailableDataFrames.Text & "_stacked")
+
+        clsReshapeFunction.SetPackageName("stats")
+        clsReshapeFunction.SetRCommand("reshape")
+        clsReshapeFunction.SetAssignTo(ucrSelectorStack.ucrAvailableDataFrames.cboAvailableDataFrames.Text & "_stacked", strTempDataframe:=ucrSelectorStack.ucrAvailableDataFrames.cboAvailableDataFrames.Text & "_stacked")
+        clsReshapeFunction.AddParameter("direction", Chr(34) & "long" & Chr(34))
+        clsReshapeFunction.AddParameter("timevar", Chr(34) & "variable" & Chr(34))
+
+        clsSplitColumnsInGroups.SetRCommand("split_items_in_groups")
+        clsSplitColumnsInGroups.AddParameter("num", 2, iPosition:=1)
+
+        clsReshapeFunction.AddParameter("varying", clsRFunctionParameter:=clsSplitColumnsInGroups, iPosition:=1)
 
         ucrBase.clsRsyntax.SetBaseRFunction(clsMeltFunction)
     End Sub
 
     Private Sub SetRCodeForControls(bReset As Boolean)
-        SetRCode(Me, ucrBase.clsRsyntax.clsBaseFunction, bReset)
+        ucrReceiverColumnsToBeStack.AddAdditionalCodeParameterPair(clsSplitColumnsInGroups, New RParameter("items", 0), iAdditionalPairNo:=1)
+        ucrFactorInto.AddAdditionalCodeParameterPair(clsReshapeFunction, New RParameter("timevar", 3), iAdditionalPairNo:=1)
+        ucrSelectorStack.AddAdditionalCodeParameterPair(clsReshapeFunction, ucrSelectorStack.GetParameter(), iAdditionalPairNo:=1)
+        ucrSaveNewDataName.AddAdditionalRCode(clsReshapeFunction, iAdditionalPairNo:=1)
+
+        ucrChkStackMultipleSets.SetRCode(ucrBase.clsRsyntax.clsBaseFunction, bReset)
+
+        ucrSelectorStack.SetRCode(clsMeltFunction, bReset)
+        ucrReceiverColumnsToBeStack.SetRCode(clsMeltFunction, bReset)
+        ucrColumnsToCarryReceiver.SetRCode(clsMeltFunction, bReset)
+        ucrChkCarryColumns.SetRCode(clsMeltFunction, bReset)
+        ucrFactorInto.SetRCode(clsMeltFunction, bReset)
+        ucrStackDataInto.SetRCode(clsMeltFunction, bReset)
+        ucrSaveNewDataName.SetRCode(clsMeltFunction, bReset)
+
         If bReset Then
             SetCarryColumnsOptions()
         End If
@@ -148,5 +193,16 @@ Public Class dlgStack
 
     Private Sub CoreControls_ControlContentesChanged(ucrChangedControl As ucrCore) Handles ucrReceiverColumnsToBeStack.ControlContentsChanged, ucrStackDataInto.ControlContentsChanged, ucrFactorInto.ControlContentsChanged, ucrSaveNewDataName.ControlContentsChanged, ucrChkCarryColumns.ControlContentsChanged, ucrColumnsToCarryReceiver.ControlContentsChanged
         TestOKEnabled()
+    End Sub
+
+    Private Sub ucrChkStackMultipleSets_ControlValueChanged(ucrChangedControl As ucrCore) Handles ucrChkStackMultipleSets.ControlValueChanged
+        If ucrChkStackMultipleSets.Checked Then
+            ucrBase.clsRsyntax.SetBaseRFunction(clsReshapeFunction)
+            If Not ucrFactorInto.IsEmpty Then
+                clsReshapeFunction.AddParameter("timevar", Chr(34) & ucrFactorInto.GetText() & Chr(34))
+            End If
+        Else
+            ucrBase.clsRsyntax.SetBaseRFunction(clsMeltFunction)
+        End If
     End Sub
 End Class
