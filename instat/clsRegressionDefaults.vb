26010b80de0b1441d5a57aecb9a2e1dfe7b160ea
Steven <stevenndungu4@gmail.com>
2017-06-22 06:49:29 +0300

    Display tab implemented

---
+++ instat/clsRegressionDefaults.vb
@@ -0,0 +1,115 @@
+﻿Public Class clsRegressionDefaults
+    Public Shared ReadOnly Property clsDefaultFitted As RFunction
+        Get
+            Dim clsRFittedValues As New RFunction
+            Dim clsRWriteFitted, clsRModelFunction As New RFunction
+
+            'ucrSave (sdgSimpleRegOptions) Fitted Values
+            clsRFittedValues.SetRCommand("fitted")
+            clsRModelFunction.SetRCommand("")
+            clsRFittedValues.AddParameter("object", clsRFunctionParameter:=clsDefaultRLmOrGLM)
+            clsRWriteFitted.SetRCommand(frmMain.clsRLink.strInstatDataObject & "$add_columns_to_data")
+            clsRWriteFitted.AddParameter("col_data", clsRFunctionParameter:=clsRFittedValues)
+            Return clsRWriteFitted
+        End Get
+    End Property
+
+    Public Shared ReadOnly Property clsDefaultRLmOrGLM As RFunction
+        Get
+
+            Dim clsRModelFunction As New RFunction
+            
+
+            clsRModelFunction.SetRCommand("lm")
+            'clsRModelFunction.AddParameter("formula", clsROperatorParameter:=clsDefaultFormulaOperator, iPosition:=1)
+            Return clsRModelFunction
+        End Get
+    End Property
+
+    Public Shared ReadOnly Property clsDefaultRGraphicsOperator As ROperator
+        Get
+
+            Dim clsRGraphicsOperator As New ROperator
+
+            clsRGraphicsOperator.SetOperation("+")
+            Return clsRGraphicsOperator
+        End Get
+    End Property
+
+    Public Shared ReadOnly Property clsDefaultFormulaOperator As ROperator
+        Get
+
+            Dim clsFormulaOperator As New ROperator
+
+            clsFormulaOperator.SetOperation("~")
+            Return clsFormulaOperator
+        End Get
+    End Property
+
+
+    Public Shared ReadOnly Property clsDefaultSummary As RFunction
+        Get
+            Dim clsDefaultRestpvalFunction As New RFunction
+
+            clsDefaultRestpvalFunction.SetPackageName("base")
+            clsDefaultRestpvalFunction.SetRCommand("summary")
+            Return clsDefaultRestpvalFunction
+        End Get
+    End Property
+
+    Public Shared ReadOnly Property clsDefaultRConfinterval As RFunction
+        Get
+            Dim clsDefaultRConfint As New RFunction
+            clsDefaultRConfint.SetPackageName("stats")
+            clsDefaultRConfint.SetRCommand("confint")
+            Return clsDefaultRConfint
+        End Get
+    End Property
+
+    Public Shared ReadOnly Property clsDefaultRaovPValueFunction As RFunction
+        Get
+            Dim clsDefaultRaovpvalFunction As New RFunction
+            clsDefaultRaovpvalFunction.SetPackageName("stats")
+            clsDefaultRaovpvalFunction.SetRCommand("anova")
+            Return clsDefaultRaovpvalFunction
+        End Get
+    End Property
+
+    Public Shared ReadOnly Property clsDefaultRanovaFunction As RFunction
+        Get
+            Dim clsDefaultRaovFunction As New RFunction
+            clsDefaultRaovFunction.SetPackageName("stats")
+            clsDefaultRaovFunction.SetRCommand("anova")
+            Return clsDefaultRaovFunction
+        End Get
+    End Property
+
+
+    Public Shared ReadOnly Property clsDefaultModel As RFunction
+        Get
+            Dim clsDefaultRModelsFunction As New RFunction
+            clsDefaultRModelsFunction.SetPackageName("stats")
+            clsDefaultRModelsFunction.SetRCommand("formula")
+            Return clsDefaultRModelsFunction
+        End Get
+    End Property
+
+    Public Shared ReadOnly Property clsDefaultRResidualPlots As RFunction
+        Get
+            Dim clsDefaultRResidualPlotsFunction As New RFunction
+            clsDefaultRResidualPlotsFunction.SetPackageName("ggplot2")
+            clsDefaultRResidualPlotsFunction.SetRCommand("autoplot")
+            Return clsDefaultRResidualPlotsFunction
+        End Get
+    End Property
+
+    Public Shared ReadOnly Property clsDefaultRgeom_pointFunction As RFunction
+        Get
+            Dim clsDefaultRgeom_point As New RFunction
+            clsDefaultRgeom_point.SetPackageName("ggplot2")
+            clsDefaultRgeom_point.SetRCommand("geom_point")
+            Return clsDefaultRgeom_point
+        End Get
+    End Property
+
+End Class
