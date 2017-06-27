01234b4b871f5b6ddca323e7b8fdd023c7057b7b
Steven <stevenndungu4@gmail.com>
2017-06-27 14:08:58 +0300

    more additions on corruption risks

--- instat/dlgStandardiseCountryNames.vb
+++ instat/dlgStandardiseCountryNames.vb
@@ -50,6 +50,7 @@ Public Class dlgStandardiseCountryNames
         ucrReceiverCountryNames.SetParameter(New RParameter("country_columns", 1))
         ucrReceiverCountryNames.SetParameterIsString()
         ucrReceiverCountryNames.SetIncludedDataTypes({"factor", "character"})
+        ucrReceiverCountryNames.strSelectorHeading = "Factors"
         ucrReceiverCountryNames.Selector = ucrSelectorStandardiseCountry
         ucrReceiverCountryNames.SetMeAsReceiver()
     End Sub
