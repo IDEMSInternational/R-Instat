40db1e76e801fe0206ea6a0c68f30756d2ff993f
Danny Parsons <danny@aims.ac.za>
2017-06-29 18:05:01 +0100

    three variable modelling working with link function

--- instat/sdgModelOptions.vb
+++ instat/sdgModelOptions.vb
@@ -14,60 +14,61 @@
 ' You should have received a copy of the GNU General Public License k
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 Imports instat.Translations
+
 Public Class sdgModelOptions
     Private bControlsInitialised As Boolean = False
-    Public clsRCIFunction As New RFunction
+    Public clsFamilyFunction As New RFunction
     Public bFirstLoad As Boolean = True
+    Public bRCodeSet As Boolean = True
+    Private lstRadioButtons As New List(Of RadioButton)
 
     Private Sub sdgModelOptions_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         autoTranslate(Me)
     End Sub
 
     Public Sub InitialiseControls()
-
         ucrPnlLinkFunctions.SetParameter(New RParameter("link", 0))
-
-
         ucrPnlLinkFunctions.AddRadioButton(rdoIdentity, Chr(34) & "identity" & Chr(34))
         ucrPnlLinkFunctions.AddRadioButton(rdoCauchit, Chr(34) & "cauchit" & Chr(34))
-        ucrPnlLinkFunctions.AddRadioButton(rdocloglog, Chr(34) & "cloglog" & Chr(34))
+        ucrPnlLinkFunctions.AddRadioButton(rdoCloglog, Chr(34) & "cloglog" & Chr(34))
         ucrPnlLinkFunctions.AddRadioButton(rdoLog, Chr(34) & "log" & Chr(34))
         ucrPnlLinkFunctions.AddRadioButton(rdoSqrt, Chr(34) & "sqrt" & Chr(34))
         ucrPnlLinkFunctions.AddRadioButton(rdoProbit, Chr(34) & "probit" & Chr(34))
         ucrPnlLinkFunctions.AddRadioButton(rdoLogit, Chr(34) & "logit" & Chr(34))
         ucrPnlLinkFunctions.AddRadioButton(rdoMuSquaredInverse, Chr(34) & "1/mu^2" & Chr(34))
         ucrPnlLinkFunctions.AddRadioButton(rdoInverse, Chr(34) & "inverse" & Chr(34))
+        'True because no fixed default (depends on distribution) and there may not be a link parameter present
+        ucrPnlLinkFunctions.bAllowNonConditionValues = True
+        ucrPnlLinkFunctions.bSetToFirstIfNoValue = False
 
+        ucrDistributionChoice.SetGLMDistributions()
+        ucrDistributionChoice.SetFunctionIsDistFunction()
+        'Override so that distributions don't reset on loading
+        ucrDistributionChoice.bFirstLoad = False
+
+        lstRadioButtons.AddRange({rdoIdentity, rdoCauchit, rdoCloglog, rdoLog, rdoSqrt, rdoProbit, rdoLogit, rdoMuSquaredInverse, rdoInverse})
         bControlsInitialised = True
-
     End Sub
 
-    Public Sub SetRFunction(clsNewRCIFunction As RFunction, Optional bReset As Boolean = False)
+    Public Sub SetRCodeForControls(ucrNewDistributionChoice As ucrDistributions, clsNewFamilyFunction As RFunction, Optional bReset As Boolean = False)
+        bRCodeSet = False
         If Not bControlsInitialised Then
             InitialiseControls()
         End If
-        clsRCIFunction = clsNewRCIFunction
-        ucrPnlLinkFunctions.SetRCode(clsRCIFunction, bReset)
-    End Sub
+        clsFamilyFunction = clsNewFamilyFunction
+        ucrDistributionChoice.SetDataType(ucrNewDistributionChoice.strDataType)
 
-    Public Sub SetDefaults()
-        RestrictLink()
+        ucrPnlLinkFunctions.SetRCode(clsFamilyFunction, bReset)
+        ucrDistributionChoice.SetRCode(clsFamilyFunction, bReset)
+
+        bRCodeSet = True
+        SetLinkFunctionsAvailable(Not clsFamilyFunction.ContainsParameter("link"))
     End Sub
 
     Private Sub ResetLinks()
-        rdoIdentity.Checked = False
-        rdoCauchit.Checked = False
-        rdocloglog.Checked = False
-        rdoInverse.Checked = False
-        rdoLog.Checked = False
-        rdoLogit.Checked = False
-        rdoMuSquaredInverse.Checked = False
-        rdoProbit.Checked = False
-        rdoSqrt.Checked = False
-
         rdoIdentity.Enabled = False
         rdoCauchit.Enabled = False
-        rdocloglog.Enabled = False
+        rdoCloglog.Enabled = False
         rdoInverse.Enabled = False
         rdoLog.Enabled = False
         rdoLogit.Enabled = False
@@ -77,57 +78,72 @@ Public Class sdgModelOptions
     End Sub
 
     Public Sub ucrFamily_cboDistributionsIndexChanged() Handles ucrDistributionChoice.DistributionsIndexChanged
-        RestrictLink()
+        If bRCodeSet Then
+            SetLinkFunctionsAvailable(True)
+        End If
     End Sub
 
-    Public Sub RestrictLink()
-        Dim strFamilyName As String
-        strFamilyName = ucrDistributionChoice.clsCurrDistribution.strNameTag
+    Public Sub SetLinkFunctionsAvailable(Optional bSetDefault As Boolean = True)
+        Dim strDistributionName As String
+
+        strDistributionName = ucrDistributionChoice.clsCurrDistribution.strNameTag
         ResetLinks()
-        If strFamilyName = "Normal" Then
+        If strDistributionName = "Normal" Then
             rdoIdentity.Enabled = True
-            ' rdoIdentity.Checked = True
             rdoInverse.Enabled = True
             rdoLog.Enabled = True
-        End If
-        If strFamilyName = "Binomial" Or strFamilyName = "Quasibinomial" Then
+            If bSetDefault Then
+                rdoIdentity.Checked = True
+            End If
+        ElseIf strDistributionName = "Binomial" Or strDistributionName = "Quasibinomial" Then
             rdoLogit.Enabled = True
-            ' rdoLogit.Checked = True
             rdoCauchit.Enabled = True
-            rdocloglog.Enabled = True
+            rdoCloglog.Enabled = True
             rdoLog.Enabled = True
             rdoProbit.Enabled = True
-        End If
-        If strFamilyName = "Gamma" Then
+            If bSetDefault Then
+                rdoLogit.Checked = True
+            End If
+        ElseIf strDistributionName = "Gamma" Then
             rdoInverse.Enabled = True
             rdoLog.Enabled = True
-            ' rdoLog.Checked = True
             rdoIdentity.Enabled = True
-        End If
-        If strFamilyName = "Poisson" Or strFamilyName = "Quasipoisson" Then
+            If bSetDefault Then
+                rdoInverse.Checked = True
+            End If
+        ElseIf strDistributionName = "Poisson" Or strDistributionName = "Quasipoisson" Then
             rdoLog.Enabled = True
-            'rdoLog.Checked = True
             rdoIdentity.Enabled = True
             rdoSqrt.Enabled = True
-        End If
-        If strFamilyName = "Inverse_Gaussian" Then
+            If bSetDefault Then
+                rdoLog.Checked = True
+            End If
+        ElseIf strDistributionName = "Inverse_Gaussian" Then
             rdoMuSquaredInverse.Enabled = True
-            ' rdoMuSquaredInverse.Checked = True
             rdoIdentity.Enabled = True
             rdoInverse.Enabled = True
             rdoLog.Enabled = True
-        End If
-        If strFamilyName = "Quasi" Then
+            If bSetDefault Then
+                rdoMuSquaredInverse.Checked = True
+            End If
+        ElseIf strDistributionName = "Quasi" Then
             rdoIdentity.Enabled = True
-            'rdoIdentity.Checked = True
-            rdocloglog.Enabled = True
+            rdoCloglog.Enabled = True
             rdoInverse.Enabled = True
             rdoLog.Enabled = True
             rdoLogit.Enabled = True
             rdoMuSquaredInverse.Enabled = True
             rdoProbit.Enabled = True
             rdoSqrt.Enabled = True
+            If bSetDefault Then
+                rdoIdentity.Checked = True
+            End If
         End If
+        'Ensures a disabled radio button cannot be checked
+        For i As Integer = 0 To lstRadioButtons.Count - 1
+            If lstRadioButtons(i).Checked AndAlso Not lstRadioButtons(i).Enabled Then
+                lstRadioButtons(i).Checked = False
+            End If
+        Next
     End Sub
-
 End Class
