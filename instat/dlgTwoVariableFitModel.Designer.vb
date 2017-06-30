c8a314c9e04b0dad590e1308163988eb7fa0a05d
Steven <stevenndungu4@gmail.com>
2017-06-30 15:06:25 +0300

    Renaming the dialogue

--- instat/dlgTwoVariableFitModel.Designer.vb
+++ instat/dlgTwoVariableFitModel.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class dlgRegressionSimple
+Partial Class dlgTwoVariableFitModel
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
