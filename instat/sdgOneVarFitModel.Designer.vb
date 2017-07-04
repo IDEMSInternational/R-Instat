87a3fced9ec4fb30d3b07bd0671ad82b03cb45a1
Lily <Admin@NEW-KT5NM8N55N7>
2017-07-04 10:59:02 +0200

    Rearrangements/adding control names

--- instat/sdgOneVarFitModel.Designer.vb
+++ instat/sdgOneVarFitModel.Designer.vb
@@ -38,100 +38,58 @@ Partial Class sdgOneVarFitModel
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.TabControl1 = New System.Windows.Forms.TabControl()
-        Me.TabPage2 = New System.Windows.Forms.TabPage()
-        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.ucrBase = New instat.ucrButtonsSubdialogue()
-        Me.ucrPnlFitMethod = New instat.UcrPanel()
-        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.tbFittingOptions = New System.Windows.Forms.TabControl()
+        Me.tbFittingMethod = New System.Windows.Forms.TabPage()
         Me.rdoMme = New System.Windows.Forms.RadioButton()
         Me.rdoQme = New System.Windows.Forms.RadioButton()
         Me.rdoMle = New System.Windows.Forms.RadioButton()
         Me.rdoMge = New System.Windows.Forms.RadioButton()
-        Me.ucrPnlOptimisation = New instat.UcrPanel()
+        Me.ucrPnlFitMethod = New instat.UcrPanel()
+        Me.tbOptimisationMethod = New System.Windows.Forms.TabPage()
         Me.rdoSANN = New System.Windows.Forms.RadioButton()
-        Me.rdoCG = New System.Windows.Forms.RadioButton()
-        Me.rdoBFGS = New System.Windows.Forms.RadioButton()
-        Me.rdoNelderMead = New System.Windows.Forms.RadioButton()
         Me.rdoDefault = New System.Windows.Forms.RadioButton()
-        Me.TabControl1.SuspendLayout()
-        Me.TabPage2.SuspendLayout()
-        Me.GroupBox1.SuspendLayout()
-        Me.TabPage1.SuspendLayout()
+        Me.rdoCG = New System.Windows.Forms.RadioButton()
+        Me.rdoNelderMead = New System.Windows.Forms.RadioButton()
+        Me.rdoBFGS = New System.Windows.Forms.RadioButton()
+        Me.ucrPnlOptimisation = New instat.UcrPanel()
+        Me.ucrBase = New instat.ucrButtonsSubdialogue()
+        Me.tbFittingOptions.SuspendLayout()
+        Me.tbFittingMethod.SuspendLayout()
+        Me.tbOptimisationMethod.SuspendLayout()
         Me.SuspendLayout()
         '
-        'TabControl1
+        'tbFittingOptions
         '
-        Me.TabControl1.Controls.Add(Me.TabPage1)
-        Me.TabControl1.Controls.Add(Me.TabPage2)
-        Me.TabControl1.Location = New System.Drawing.Point(12, 12)
-        Me.TabControl1.Name = "TabControl1"
-        Me.TabControl1.SelectedIndex = 0
-        Me.TabControl1.Size = New System.Drawing.Size(201, 177)
-        Me.TabControl1.TabIndex = 0
+        Me.tbFittingOptions.Controls.Add(Me.tbFittingMethod)
+        Me.tbFittingOptions.Controls.Add(Me.tbOptimisationMethod)
+        Me.tbFittingOptions.Location = New System.Drawing.Point(5, 12)
+        Me.tbFittingOptions.Name = "tbFittingOptions"
+        Me.tbFittingOptions.SelectedIndex = 0
+        Me.tbFittingOptions.Size = New System.Drawing.Size(201, 177)
+        Me.tbFittingOptions.TabIndex = 0
         '
-        'TabPage2
+        'tbFittingMethod
         '
-        Me.TabPage2.Controls.Add(Me.GroupBox1)
-        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
-        Me.TabPage2.Name = "TabPage2"
-        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage2.Size = New System.Drawing.Size(193, 151)
-        Me.TabPage2.TabIndex = 1
-        Me.TabPage2.Text = "Optimisation"
-        Me.TabPage2.UseVisualStyleBackColor = True
-        '
-        'GroupBox1
-        '
-        Me.GroupBox1.Controls.Add(Me.rdoSANN)
-        Me.GroupBox1.Controls.Add(Me.rdoCG)
-        Me.GroupBox1.Controls.Add(Me.rdoBFGS)
-        Me.GroupBox1.Controls.Add(Me.rdoNelderMead)
-        Me.GroupBox1.Controls.Add(Me.rdoDefault)
-        Me.GroupBox1.Controls.Add(Me.ucrPnlOptimisation)
-        Me.GroupBox1.Location = New System.Drawing.Point(6, 6)
-        Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(181, 139)
-        Me.GroupBox1.TabIndex = 0
-        Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "Optimisation Method"
-        '
-        'ucrBase
-        '
-        Me.ucrBase.Location = New System.Drawing.Point(16, 195)
-        Me.ucrBase.Name = "ucrBase"
-        Me.ucrBase.Size = New System.Drawing.Size(142, 30)
-        Me.ucrBase.TabIndex = 1
-        '
-        'ucrPnlFitMethod
-        '
-        Me.ucrPnlFitMethod.Location = New System.Drawing.Point(3, 6)
-        Me.ucrPnlFitMethod.Name = "ucrPnlFitMethod"
-        Me.ucrPnlFitMethod.Size = New System.Drawing.Size(171, 95)
-        Me.ucrPnlFitMethod.TabIndex = 5
-        '
-        'TabPage1
-        '
-        Me.TabPage1.Controls.Add(Me.rdoMme)
-        Me.TabPage1.Controls.Add(Me.rdoQme)
-        Me.TabPage1.Controls.Add(Me.rdoMle)
-        Me.TabPage1.Controls.Add(Me.rdoMge)
-        Me.TabPage1.Controls.Add(Me.ucrPnlFitMethod)
-        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
-        Me.TabPage1.Name = "TabPage1"
-        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage1.Size = New System.Drawing.Size(193, 151)
-        Me.TabPage1.TabIndex = 0
-        Me.TabPage1.Text = "Fitting Method"
-        Me.TabPage1.UseVisualStyleBackColor = True
+        Me.tbFittingMethod.Controls.Add(Me.rdoMme)
+        Me.tbFittingMethod.Controls.Add(Me.rdoQme)
+        Me.tbFittingMethod.Controls.Add(Me.rdoMle)
+        Me.tbFittingMethod.Controls.Add(Me.rdoMge)
+        Me.tbFittingMethod.Controls.Add(Me.ucrPnlFitMethod)
+        Me.tbFittingMethod.Location = New System.Drawing.Point(4, 22)
+        Me.tbFittingMethod.Name = "tbFittingMethod"
+        Me.tbFittingMethod.Padding = New System.Windows.Forms.Padding(3)
+        Me.tbFittingMethod.Size = New System.Drawing.Size(193, 151)
+        Me.tbFittingMethod.TabIndex = 0
+        Me.tbFittingMethod.Text = "Fitting Method"
+        Me.tbFittingMethod.UseVisualStyleBackColor = True
         '
         'rdoMme
         '
         Me.rdoMme.AutoSize = True
-        Me.rdoMme.Location = New System.Drawing.Point(3, 29)
+        Me.rdoMme.Location = New System.Drawing.Point(10, 33)
         Me.rdoMme.Name = "rdoMme"
         Me.rdoMme.Size = New System.Drawing.Size(102, 17)
-        Me.rdoMme.TabIndex = 7
+        Me.rdoMme.TabIndex = 2
         Me.rdoMme.TabStop = True
         Me.rdoMme.Text = "Moments (MME)"
         Me.rdoMme.UseVisualStyleBackColor = True
@@ -139,10 +97,10 @@ Partial Class sdgOneVarFitModel
         'rdoQme
         '
         Me.rdoQme.AutoSize = True
-        Me.rdoQme.Location = New System.Drawing.Point(3, 52)
+        Me.rdoQme.Location = New System.Drawing.Point(10, 56)
         Me.rdoQme.Name = "rdoQme"
         Me.rdoQme.Size = New System.Drawing.Size(99, 17)
-        Me.rdoQme.TabIndex = 8
+        Me.rdoQme.TabIndex = 3
         Me.rdoQme.TabStop = True
         Me.rdoQme.Text = "Quartiles (QME)"
         Me.rdoQme.UseVisualStyleBackColor = True
@@ -150,10 +108,10 @@ Partial Class sdgOneVarFitModel
         'rdoMle
         '
         Me.rdoMle.AutoSize = True
-        Me.rdoMle.Location = New System.Drawing.Point(3, 6)
+        Me.rdoMle.Location = New System.Drawing.Point(10, 10)
         Me.rdoMle.Name = "rdoMle"
         Me.rdoMle.Size = New System.Drawing.Size(151, 17)
-        Me.rdoMle.TabIndex = 6
+        Me.rdoMle.TabIndex = 1
         Me.rdoMle.TabStop = True
         Me.rdoMle.Text = "Maximum Likelihood (MLE)"
         Me.rdoMle.UseVisualStyleBackColor = True
@@ -161,109 +119,137 @@ Partial Class sdgOneVarFitModel
         'rdoMge
         '
         Me.rdoMge.AutoSize = True
-        Me.rdoMge.Location = New System.Drawing.Point(3, 75)
+        Me.rdoMge.Location = New System.Drawing.Point(10, 79)
         Me.rdoMge.Name = "rdoMge"
         Me.rdoMge.Size = New System.Drawing.Size(132, 17)
-        Me.rdoMge.TabIndex = 9
+        Me.rdoMge.TabIndex = 4
         Me.rdoMge.TabStop = True
         Me.rdoMge.Text = "Goodness of Fit (MGE)"
         Me.rdoMge.UseVisualStyleBackColor = True
         '
-        'ucrPnlOptimisation
+        'ucrPnlFitMethod
         '
-        Me.ucrPnlOptimisation.Location = New System.Drawing.Point(6, 13)
-        Me.ucrPnlOptimisation.Name = "ucrPnlOptimisation"
-        Me.ucrPnlOptimisation.Size = New System.Drawing.Size(165, 120)
-        Me.ucrPnlOptimisation.TabIndex = 6
+        Me.ucrPnlFitMethod.Location = New System.Drawing.Point(3, 6)
+        Me.ucrPnlFitMethod.Name = "ucrPnlFitMethod"
+        Me.ucrPnlFitMethod.Size = New System.Drawing.Size(184, 114)
+        Me.ucrPnlFitMethod.TabIndex = 0
+        '
+        'tbOptimisationMethod
+        '
+        Me.tbOptimisationMethod.Controls.Add(Me.rdoSANN)
+        Me.tbOptimisationMethod.Controls.Add(Me.rdoDefault)
+        Me.tbOptimisationMethod.Controls.Add(Me.rdoCG)
+        Me.tbOptimisationMethod.Controls.Add(Me.rdoNelderMead)
+        Me.tbOptimisationMethod.Controls.Add(Me.rdoBFGS)
+        Me.tbOptimisationMethod.Controls.Add(Me.ucrPnlOptimisation)
+        Me.tbOptimisationMethod.Location = New System.Drawing.Point(4, 22)
+        Me.tbOptimisationMethod.Name = "tbOptimisationMethod"
+        Me.tbOptimisationMethod.Padding = New System.Windows.Forms.Padding(3)
+        Me.tbOptimisationMethod.Size = New System.Drawing.Size(193, 151)
+        Me.tbOptimisationMethod.TabIndex = 1
+        Me.tbOptimisationMethod.Text = "Optimisation"
+        Me.tbOptimisationMethod.UseVisualStyleBackColor = True
         '
         'rdoSANN
         '
         Me.rdoSANN.AutoSize = True
-        Me.rdoSANN.Location = New System.Drawing.Point(21, 107)
+        Me.rdoSANN.Location = New System.Drawing.Point(10, 102)
         Me.rdoSANN.Name = "rdoSANN"
         Me.rdoSANN.Size = New System.Drawing.Size(55, 17)
-        Me.rdoSANN.TabIndex = 11
+        Me.rdoSANN.TabIndex = 5
         Me.rdoSANN.TabStop = True
         Me.rdoSANN.Text = "SANN"
         Me.rdoSANN.UseVisualStyleBackColor = True
         '
+        'rdoDefault
+        '
+        Me.rdoDefault.AutoSize = True
+        Me.rdoDefault.Location = New System.Drawing.Point(10, 10)
+        Me.rdoDefault.Name = "rdoDefault"
+        Me.rdoDefault.Size = New System.Drawing.Size(59, 17)
+        Me.rdoDefault.TabIndex = 1
+        Me.rdoDefault.TabStop = True
+        Me.rdoDefault.Text = "Default"
+        Me.rdoDefault.UseVisualStyleBackColor = True
+        '
         'rdoCG
         '
         Me.rdoCG.AutoSize = True
-        Me.rdoCG.Location = New System.Drawing.Point(21, 84)
+        Me.rdoCG.Location = New System.Drawing.Point(10, 79)
         Me.rdoCG.Name = "rdoCG"
         Me.rdoCG.Size = New System.Drawing.Size(138, 17)
-        Me.rdoCG.TabIndex = 10
+        Me.rdoCG.TabIndex = 4
         Me.rdoCG.TabStop = True
         Me.rdoCG.Text = "Conjugent Gradent (CG)"
         Me.rdoCG.UseVisualStyleBackColor = True
         '
-        'rdoBFGS
-        '
-        Me.rdoBFGS.AutoSize = True
-        Me.rdoBFGS.Location = New System.Drawing.Point(21, 61)
-        Me.rdoBFGS.Name = "rdoBFGS"
-        Me.rdoBFGS.Size = New System.Drawing.Size(53, 17)
-        Me.rdoBFGS.TabIndex = 9
-        Me.rdoBFGS.TabStop = True
-        Me.rdoBFGS.Text = "BFGS"
-        Me.rdoBFGS.UseVisualStyleBackColor = True
-        '
         'rdoNelderMead
         '
         Me.rdoNelderMead.AutoSize = True
-        Me.rdoNelderMead.Location = New System.Drawing.Point(21, 38)
+        Me.rdoNelderMead.Location = New System.Drawing.Point(10, 33)
         Me.rdoNelderMead.Name = "rdoNelderMead"
         Me.rdoNelderMead.Size = New System.Drawing.Size(86, 17)
-        Me.rdoNelderMead.TabIndex = 8
+        Me.rdoNelderMead.TabIndex = 2
         Me.rdoNelderMead.TabStop = True
         Me.rdoNelderMead.Text = "Nelder-Mead"
         Me.rdoNelderMead.UseVisualStyleBackColor = True
         '
-        'rdoDefault
+        'rdoBFGS
         '
-        Me.rdoDefault.AutoSize = True
-        Me.rdoDefault.Location = New System.Drawing.Point(21, 15)
-        Me.rdoDefault.Name = "rdoDefault"
-        Me.rdoDefault.Size = New System.Drawing.Size(59, 17)
-        Me.rdoDefault.TabIndex = 7
-        Me.rdoDefault.TabStop = True
-        Me.rdoDefault.Text = "Default"
-        Me.rdoDefault.UseVisualStyleBackColor = True
+        Me.rdoBFGS.AutoSize = True
+        Me.rdoBFGS.Location = New System.Drawing.Point(10, 56)
+        Me.rdoBFGS.Name = "rdoBFGS"
+        Me.rdoBFGS.Size = New System.Drawing.Size(53, 17)
+        Me.rdoBFGS.TabIndex = 3
+        Me.rdoBFGS.TabStop = True
+        Me.rdoBFGS.Text = "BFGS"
+        Me.rdoBFGS.UseVisualStyleBackColor = True
+        '
+        'ucrPnlOptimisation
+        '
+        Me.ucrPnlOptimisation.Location = New System.Drawing.Point(6, 6)
+        Me.ucrPnlOptimisation.Name = "ucrPnlOptimisation"
+        Me.ucrPnlOptimisation.Size = New System.Drawing.Size(165, 120)
+        Me.ucrPnlOptimisation.TabIndex = 0
+        '
+        'ucrBase
+        '
+        Me.ucrBase.Location = New System.Drawing.Point(33, 195)
+        Me.ucrBase.Name = "ucrBase"
+        Me.ucrBase.Size = New System.Drawing.Size(142, 30)
+        Me.ucrBase.TabIndex = 1
         '
         'sdgOneVarFitModel
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(216, 228)
+        Me.ClientSize = New System.Drawing.Size(209, 228)
         Me.Controls.Add(Me.ucrBase)
-        Me.Controls.Add(Me.TabControl1)
+        Me.Controls.Add(Me.tbFittingOptions)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "sdgOneVarFitModel"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
         Me.Text = "Fitting Options"
-        Me.TabControl1.ResumeLayout(False)
-        Me.TabPage2.ResumeLayout(False)
-        Me.GroupBox1.ResumeLayout(False)
-        Me.GroupBox1.PerformLayout()
-        Me.TabPage1.ResumeLayout(False)
-        Me.TabPage1.PerformLayout()
+        Me.tbFittingOptions.ResumeLayout(False)
+        Me.tbFittingMethod.ResumeLayout(False)
+        Me.tbFittingMethod.PerformLayout()
+        Me.tbOptimisationMethod.ResumeLayout(False)
+        Me.tbOptimisationMethod.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
 
-    Friend WithEvents TabControl1 As TabControl
-    Friend WithEvents TabPage2 As TabPage
-    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents tbFittingOptions As TabControl
+    Friend WithEvents tbOptimisationMethod As TabPage
     Friend WithEvents rdoSANN As RadioButton
     Friend WithEvents rdoCG As RadioButton
     Friend WithEvents rdoBFGS As RadioButton
     Friend WithEvents rdoNelderMead As RadioButton
     Friend WithEvents rdoDefault As RadioButton
     Friend WithEvents ucrBase As ucrButtonsSubdialogue
-    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents tbFittingMethod As TabPage
     Friend WithEvents rdoMme As RadioButton
     Friend WithEvents rdoQme As RadioButton
     Friend WithEvents rdoMle As RadioButton
