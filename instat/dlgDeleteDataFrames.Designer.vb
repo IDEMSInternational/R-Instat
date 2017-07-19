07feac7ceb3766b22c3df147219f9b682f132113
Steven <stevenndungu4@gmail.com>
2017-07-19 12:41:45 +0300

    Renaming from dlete sheet to delete Dataframe

--- instat/dlgDeleteDataFrames.Designer.vb
+++ instat/dlgDeleteDataFrames.Designer.vb
@@ -15,7 +15,7 @@
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 <Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class dlgDeleteSheet
+Partial Class dlgDeleteDataFrames
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
