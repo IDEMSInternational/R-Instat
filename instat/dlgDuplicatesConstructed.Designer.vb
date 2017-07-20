3eaa84ab2e9bbca89dc93296f2edd1e7fb3737ec
Alex Sananka <sananka43@gmail.com>
2017-07-20 17:28:03 +0300

    adding controls and code

--- instat/dlgDuplicatesConstructed.Designer.vb
+++ instat/dlgDuplicatesConstructed.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class dlgDuplicatesConstructed
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,24 +20,163 @@ Partial Class dlgDuplicatesConstructed
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.rdoDataframe = New System.Windows.Forms.RadioButton()
+        Me.rdoSelectedVariables = New System.Windows.Forms.RadioButton()
+        Me.rdoIndexnumberofduplicates = New System.Windows.Forms.RadioButton()
+        Me.rdoDuplicatesonly = New System.Windows.Forms.RadioButton()
+        Me.rdoAllduplicatecases = New System.Windows.Forms.RadioButton()
+        Me.ucrPnlDuplicates = New instat.UcrPanel()
+        Me.ucrNewColumnName = New instat.ucrSave()
+        Me.ucrReceiverMultipleForDuplicates = New instat.ucrReceiverMultiple()
+        Me.ucrPnlOptions = New instat.UcrPanel()
         Me.ucrBase = New instat.ucrButtons()
+        Me.ucrSelectorForDuplicates = New instat.ucrSelectorByDataFrameAddRemove()
+        Me.ucrSelectDataFrameDuplicates = New instat.ucrDataFrame()
         Me.SuspendLayout()
+        '
+        'rdoDataframe
+        '
+        Me.rdoDataframe.Appearance = System.Windows.Forms.Appearance.Button
+        Me.rdoDataframe.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoDataframe.FlatAppearance.BorderSize = 2
+        Me.rdoDataframe.FlatAppearance.CheckedBackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoDataframe.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.rdoDataframe.Location = New System.Drawing.Point(57, 12)
+        Me.rdoDataframe.Name = "rdoDataframe"
+        Me.rdoDataframe.Size = New System.Drawing.Size(118, 27)
+        Me.rdoDataframe.TabIndex = 15
+        Me.rdoDataframe.TabStop = True
+        Me.rdoDataframe.Text = "Data Frame"
+        Me.rdoDataframe.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.rdoDataframe.UseVisualStyleBackColor = True
+        '
+        'rdoSelectedVariables
+        '
+        Me.rdoSelectedVariables.Appearance = System.Windows.Forms.Appearance.Button
+        Me.rdoSelectedVariables.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoSelectedVariables.FlatAppearance.BorderSize = 2
+        Me.rdoSelectedVariables.FlatAppearance.CheckedBackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoSelectedVariables.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.rdoSelectedVariables.Location = New System.Drawing.Point(174, 12)
+        Me.rdoSelectedVariables.Name = "rdoSelectedVariables"
+        Me.rdoSelectedVariables.Size = New System.Drawing.Size(128, 27)
+        Me.rdoSelectedVariables.TabIndex = 16
+        Me.rdoSelectedVariables.TabStop = True
+        Me.rdoSelectedVariables.Text = "Selected Variables"
+        Me.rdoSelectedVariables.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.rdoSelectedVariables.UseVisualStyleBackColor = True
+        '
+        'rdoIndexnumberofduplicates
+        '
+        Me.rdoIndexnumberofduplicates.AutoSize = True
+        Me.rdoIndexnumberofduplicates.Location = New System.Drawing.Point(17, 284)
+        Me.rdoIndexnumberofduplicates.Name = "rdoIndexnumberofduplicates"
+        Me.rdoIndexnumberofduplicates.Size = New System.Drawing.Size(155, 17)
+        Me.rdoIndexnumberofduplicates.TabIndex = 25
+        Me.rdoIndexnumberofduplicates.TabStop = True
+        Me.rdoIndexnumberofduplicates.Text = " Index number of duplicates"
+        Me.rdoIndexnumberofduplicates.UseVisualStyleBackColor = True
+        '
+        'rdoDuplicatesonly
+        '
+        Me.rdoDuplicatesonly.AutoSize = True
+        Me.rdoDuplicatesonly.Location = New System.Drawing.Point(17, 262)
+        Me.rdoDuplicatesonly.Name = "rdoDuplicatesonly"
+        Me.rdoDuplicatesonly.Size = New System.Drawing.Size(97, 17)
+        Me.rdoDuplicatesonly.TabIndex = 24
+        Me.rdoDuplicatesonly.TabStop = True
+        Me.rdoDuplicatesonly.Text = "Duplicates only"
+        Me.rdoDuplicatesonly.UseVisualStyleBackColor = True
+        '
+        'rdoAllduplicatecases
+        '
+        Me.rdoAllduplicatecases.AutoSize = True
+        Me.rdoAllduplicatecases.Location = New System.Drawing.Point(17, 240)
+        Me.rdoAllduplicatecases.Name = "rdoAllduplicatecases"
+        Me.rdoAllduplicatecases.Size = New System.Drawing.Size(113, 17)
+        Me.rdoAllduplicatecases.TabIndex = 23
+        Me.rdoAllduplicatecases.TabStop = True
+        Me.rdoAllduplicatecases.Text = "All duplicate cases"
+        Me.rdoAllduplicatecases.UseVisualStyleBackColor = True
+        '
+        'ucrPnlDuplicates
+        '
+        Me.ucrPnlDuplicates.Location = New System.Drawing.Point(10, 235)
+        Me.ucrPnlDuplicates.Name = "ucrPnlDuplicates"
+        Me.ucrPnlDuplicates.Size = New System.Drawing.Size(154, 69)
+        Me.ucrPnlDuplicates.TabIndex = 22
+        '
+        'ucrNewColumnName
+        '
+        Me.ucrNewColumnName.Location = New System.Drawing.Point(10, 324)
+        Me.ucrNewColumnName.Name = "ucrNewColumnName"
+        Me.ucrNewColumnName.Size = New System.Drawing.Size(294, 24)
+        Me.ucrNewColumnName.TabIndex = 21
+        '
+        'ucrReceiverMultipleForDuplicates
+        '
+        Me.ucrReceiverMultipleForDuplicates.frmParent = Me
+        Me.ucrReceiverMultipleForDuplicates.Location = New System.Drawing.Point(241, 67)
+        Me.ucrReceiverMultipleForDuplicates.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverMultipleForDuplicates.Name = "ucrReceiverMultipleForDuplicates"
+        Me.ucrReceiverMultipleForDuplicates.Selector = Nothing
+        Me.ucrReceiverMultipleForDuplicates.Size = New System.Drawing.Size(120, 100)
+        Me.ucrReceiverMultipleForDuplicates.strNcFilePath = ""
+        Me.ucrReceiverMultipleForDuplicates.TabIndex = 20
+        Me.ucrReceiverMultipleForDuplicates.ucrSelector = Nothing
+        '
+        'ucrPnlOptions
+        '
+        Me.ucrPnlOptions.Location = New System.Drawing.Point(57, 8)
+        Me.ucrPnlOptions.Name = "ucrPnlOptions"
+        Me.ucrPnlOptions.Size = New System.Drawing.Size(254, 36)
+        Me.ucrPnlOptions.TabIndex = 17
         '
         'ucrBase
         '
-        Me.ucrBase.Location = New System.Drawing.Point(5, 197)
+        Me.ucrBase.Location = New System.Drawing.Point(10, 352)
         Me.ucrBase.Name = "ucrBase"
-        Me.ucrBase.Size = New System.Drawing.Size(335, 52)
+        Me.ucrBase.Size = New System.Drawing.Size(388, 52)
         Me.ucrBase.TabIndex = 12
+        '
+        'ucrSelectorForDuplicates
+        '
+        Me.ucrSelectorForDuplicates.bShowHiddenColumns = False
+        Me.ucrSelectorForDuplicates.bUseCurrentFilter = True
+        Me.ucrSelectorForDuplicates.Location = New System.Drawing.Point(10, 50)
+        Me.ucrSelectorForDuplicates.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrSelectorForDuplicates.Name = "ucrSelectorForDuplicates"
+        Me.ucrSelectorForDuplicates.Size = New System.Drawing.Size(210, 180)
+        Me.ucrSelectorForDuplicates.TabIndex = 18
+        '
+        'ucrSelectDataFrameDuplicates
+        '
+        Me.ucrSelectDataFrameDuplicates.bUseCurrentFilter = True
+        Me.ucrSelectDataFrameDuplicates.Location = New System.Drawing.Point(10, 50)
+        Me.ucrSelectDataFrameDuplicates.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrSelectDataFrameDuplicates.Name = "ucrSelectDataFrameDuplicates"
+        Me.ucrSelectDataFrameDuplicates.Size = New System.Drawing.Size(120, 40)
+        Me.ucrSelectDataFrameDuplicates.TabIndex = 19
         '
         'dlgDuplicatesConstructed
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(348, 261)
+        Me.ClientSize = New System.Drawing.Size(410, 412)
+        Me.Controls.Add(Me.rdoIndexnumberofduplicates)
+        Me.Controls.Add(Me.rdoDuplicatesonly)
+        Me.Controls.Add(Me.rdoAllduplicatecases)
+        Me.Controls.Add(Me.ucrPnlDuplicates)
+        Me.Controls.Add(Me.ucrNewColumnName)
+        Me.Controls.Add(Me.ucrReceiverMultipleForDuplicates)
+        Me.Controls.Add(Me.rdoDataframe)
+        Me.Controls.Add(Me.rdoSelectedVariables)
+        Me.Controls.Add(Me.ucrPnlOptions)
         Me.Controls.Add(Me.ucrBase)
+        Me.Controls.Add(Me.ucrSelectorForDuplicates)
+        Me.Controls.Add(Me.ucrSelectDataFrameDuplicates)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.MaximizeBox = False
         Me.MinimizeBox = False
@@ -45,8 +184,20 @@ Partial Class dlgDuplicatesConstructed
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Duplicates Constructed"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents ucrBase As ucrButtons
+    Friend WithEvents rdoDataframe As RadioButton
+    Friend WithEvents rdoSelectedVariables As RadioButton
+    Friend WithEvents ucrPnlOptions As UcrPanel
+    Friend WithEvents ucrSelectorForDuplicates As ucrSelectorByDataFrameAddRemove
+    Friend WithEvents ucrReceiverMultipleForDuplicates As ucrReceiverMultiple
+    Friend WithEvents ucrNewColumnName As ucrSave
+    Friend WithEvents rdoIndexnumberofduplicates As RadioButton
+    Friend WithEvents rdoDuplicatesonly As RadioButton
+    Friend WithEvents rdoAllduplicatecases As RadioButton
+    Friend WithEvents ucrPnlDuplicates As UcrPanel
+    Friend WithEvents ucrSelectDataFrameDuplicates As ucrDataFrame
 End Class
--- instat/dlgDuplicatesConstructed.vb
+++ instat/dlgDuplicatesConstructed.vb
@@ -13,11 +13,13 @@
 '
 ' You should have received a copy of the GNU General Public License 
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
+Imports instat
 Imports instat.Translations
 
 Public Class dlgDuplicatesConstructed
     Private bReset As Boolean = True
     Private bFirstLoad As Boolean = True
+    Private clsDuplicate2, clsDuplicated As New RFunction
     Private Sub dlgDuplicatesConstructed_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         If bFirstLoad Then
             InitialiseDialog()
@@ -33,10 +35,28 @@ Public Class dlgDuplicatesConstructed
     End Sub
 
     Private Sub InitialiseDialog()
+        ucrReceiverMultipleForDuplicates.Selector = ucrSelectorForDuplicates
+        ucrReceiverMultipleForDuplicates.SetMeAsReceiver()
+
+        ucrPnlDuplicates.AddRadioButton(rdoAllduplicatecases)
+        ucrPnlDuplicates.AddRadioButton(rdoDuplicatesonly)
+        ucrPnlDuplicates.AddRadioButton(rdoIndexnumberofduplicates)
+
+        ucrPnlDuplicates.AddFunctionNamesCondition(rdoAllduplicatecases, "duplicated2")
+        ucrPnlDuplicates.AddFunctionNamesCondition(rdoDuplicatesonly, "duplicated")
+        rdoIndexnumberofduplicates.Enabled = False 'for now until it's working properly
+
 
     End Sub
 
     Private Sub SetDefaults()
+        clsDuplicated = New RFunction
+        clsDuplicate2 = New RFunction
+
+        clsDuplicated.SetRCommand("duplicated")
+        clsDuplicate2.SetPackageName("questionr")
+        clsDuplicate2.SetRCommand("duplicated2")
+        Dataframe()
 
     End Sub
 
@@ -46,5 +66,18 @@ Public Class dlgDuplicatesConstructed
 
     Private Sub TestOKEnabled()
 
+    End Sub
+
+    Private Sub Dataframe()
+        If rdoDataframe.Checked Then
+            ucrSelectorForDuplicates.SetVariablesVisible(False)
+            ucrSelectorForDuplicates.Reset()
+        Else
+            ucrSelectorForDuplicates.SetVariablesVisible(True)
+        End If
+    End Sub
+
+    Private Sub ucrPnlOptions_ControlContentsChanged(ucrChangedControl As ucrCore) Handles ucrPnlOptions.ControlContentsChanged
+        Dataframe()
     End Sub
 End Class
