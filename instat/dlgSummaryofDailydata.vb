682003fde8a01e8994e456aec24d8ffab9d79f51
Steven <stevenndungu4@gmail.com>
2017-07-25 16:52:54 +0300

    more additions

--- instat/dlgSummaryofDailydata.vb
+++ instat/dlgSummaryofDailydata.vb
@@ -18,6 +18,8 @@ Imports instat.Translations
 Public Class dlgSummaryofDailydata
     Public bFirstLoad As Boolean = True
     Private bReset As Boolean = True
+    Private clsSummariesList As New RFunction
+    Private clsDefaultFunction As New RFunction
     Private bResetSubdialog As Boolean = False
     Private Sub dlgSummaryofDailydata_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         If bFirstLoad Then
@@ -35,9 +37,83 @@ Public Class dlgSummaryofDailydata
 
     Private Sub InitialiseDialog()
 
+        ' setting receivers
+        ucrReceiverStation.SetParameter(New RParameter("station", 1, False))
+        ucrReceiverStation.SetParameterIsString()
+        ucrReceiverStation.Selector = ucrSelectorSummaryofDailyData
+        ucrReceiverStation.AddIncludedMetadataProperty("Climatic_Type", {Chr(34) & "station" & Chr(34)})
+        ucrReceiverStation.bAutoFill = True
+        ucrReceiverStation.strSelectorHeading = "Station Variables"
+
+        ucrReceiverDate.SetParameter(New RParameter("date", 0, False))
+        ucrReceiverDate.SetParameterIsString()
+        ucrReceiverDate.Selector = ucrSelectorSummaryofDailyData
+        ucrReceiverDate.AddIncludedMetadataProperty("Climatic_Type", {Chr(34) & "date" & Chr(34)})
+        ucrReceiverDate.bAutoFill = True
+        ucrReceiverDate.strSelectorHeading = "Date Variables"
+
+        ucrReceiverWithinYear.SetParameter(New RParameter("day", 0))
+        ucrReceiverWithinYear.SetParameterIsString()
+        ucrReceiverWithinYear.bWithQuotes = False
+        ucrReceiverWithinYear.Selector = ucrSelectorSummaryofDailyData
+        ucrReceiverWithinYear.AddIncludedMetadataProperty("Climatic_Type", {Chr(34) & "doy" & Chr(34)})
+        ucrReceiverWithinYear.bAutoFill = True
+        ucrReceiverWithinYear.strSelectorHeading = "Within year Variables"
+
+        ucrReceiverData.SetParameter(New RParameter("data", 0))
+        ucrReceiverData.SetParameterIsRFunction()
+        ucrReceiverData.bWithQuotes = False
+        ucrReceiverData.strSelectorHeading = "Data Variables"
+        ucrReceiverData.Selector = ucrSelectorSummaryofDailyData
+        ucrReceiverData.AddIncludedMetadataProperty("Climatic_Type", {Chr(34) & "rain" & Chr(34)})
+        ucrReceiverData.bAutoFill = True
+
+        ucrReceiverYear.Selector = ucrSelectorSummaryofDailyData
+        ucrReceiverYear.AddIncludedMetadataProperty("Climatic_Type", {Chr(34) & "year" & Chr(34)})
+        ucrReceiverYear.bAutoFill = True
+        ucrReceiverYear.strSelectorHeading = "Year Variables"
+
+        ucrSelectorSummaryofDailyData.SetParameter(New RParameter("data_name", 0))
+        ucrSelectorSummaryofDailyData.SetParameterIsString()
+
+        ucrChkStoreResults.SetParameter(New RParameter("store_results", 3))
+        ucrChkStoreResults.SetText("Store Results in Data Frame")
+        ucrChkStoreResults.SetValuesCheckedAndUnchecked("TRUE", "FALSE")
+        ucrChkStoreResults.SetRDefault("TRUE")
+
+        ucrChkPrintOutput.SetParameter(New RParameter("return_output", 4))
+        ucrChkPrintOutput.SetText("Print Results to Output Window")
+        ucrChkPrintOutput.SetValuesCheckedAndUnchecked("TRUE", "FALSE")
+        ucrChkPrintOutput.SetRDefault("FALSE")
+
+        ucrChkDropUnusedLevels.SetParameter(New RParameter("drop", 5))
+        ucrChkDropUnusedLevels.SetText("Drop Unused Levels")
+        ucrChkDropUnusedLevels.SetValuesCheckedAndUnchecked("TRUE", "FALSE")
+        ucrChkDropUnusedLevels.SetRDefault("FALSE")
+
+        ucrChkOmitMissing.SetParameter(New RParameter("na.rm", 6))
+        ucrChkOmitMissing.SetText("Omit Missing Values")
+        ucrChkOmitMissing.SetValuesCheckedAndUnchecked("TRUE", "FALSE")
+        ucrChkOmitMissing.SetRDefault("FALSE")
     End Sub
 
     Private Sub SetDefaults()
+        clsDefaultFunction = New RFunction
+        clsSummariesList = New RFunction
+
+        ucrSelectorSummaryofDailyData.Reset()
+        ucrReceiverStation.SetMeAsReceiver()
+
+        clsSummariesList.SetRCommand("c")
+        clsSummariesList.AddParameter("summary_count_non_missing", Chr(34) & "summary_count_non_missing" & Chr(34), bIncludeArgumentName:=False)
+        clsSummariesList.AddParameter("summary_count", Chr(34) & "summary_count" & Chr(34), bIncludeArgumentName:=False)
+        clsSummariesList.AddParameter("summary_sum", Chr(34) & "summary_sum" & Chr(34), bIncludeArgumentName:=False)
+
+        clsDefaultFunction.SetRCommand(frmMain.clsRLink.strInstatDataObject & "$calculate_summary")
+        clsDefaultFunction.AddParameter("summaries", clsRFunctionParameter:=clsSummariesList)
+        'Prevents an error if user chooses non count summaries with no columns to summarise
+        clsDefaultFunction.AddParameter("silent", "TRUE")
+        ucrBase.clsRsyntax.SetBaseRFunction(clsDefaultFunction)
         bResetSubdialog = True
     End Sub
 
@@ -62,4 +138,7 @@ Public Class dlgSummaryofDailydata
         TestOKEnabled()
     End Sub
 
+    Private Sub CoreControls_ControlContentsChanged(ucrChangedControl As ucrCore) Handles ucrReceiverStation.ControlContentsChanged, ucrReceiverDate.ControlContentsChanged, ucrReceiverWithinYear.ControlContentsChanged, ucrReceiverYear.ControlContentsChanged
+        TestOKEnabled()
+    End Sub
 End Class
