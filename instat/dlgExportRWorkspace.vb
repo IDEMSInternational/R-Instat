e81790ed1581f6aa65ae3601eb3094ce33c3c8af
Alex Sananka <sananka43@gmail.com>
2017-07-27 14:23:43 +0300

    adding selector heading to dlgexportR workspace.

--- instat/dlgExportRWorkspace.vb
+++ instat/dlgExportRWorkspace.vb
@@ -40,6 +40,7 @@ Public Class dlgExportRWorkspace
         ucrReceiverMultiple.SetParameter(New RParameter("data_names", 0))
         ucrReceiverMultiple.SetParameterIsString()
         ucrReceiverMultiple.Selector = ucrSelectorForDataFrames
+        ucrReceiverMultiple.strSelectorHeading = "data frame(s)"
         ucrReceiverMultiple.SetItemType("dataframe")
 
         ucrInputExportFile.SetParameter(New RParameter("file", 1))
