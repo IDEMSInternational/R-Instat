19d3722c0bb3e2afdab4c0b2c02e8cd64ba05260
maxwell fundi <maxwellfundi@live.com>
2017-08-01 17:19:44 +0300

    redesigning the dialog

--- instat/dlgClimaticBoxPlot.Designer.vb
+++ instat/dlgClimaticBoxPlot.Designer.vb
@@ -28,13 +28,15 @@ Partial Class dlgClimaticBoxPlot
         Me.lblDOY = New System.Windows.Forms.Label()
         Me.lblRainfall = New System.Windows.Forms.Label()
         Me.grpLayout = New System.Windows.Forms.GroupBox()
-        Me.ucrChkMargins = New instat.ucrCheck()
-        Me.ucrChkStation = New instat.ucrCheck()
-        Me.ucrChkMonth = New instat.ucrCheck()
-        Me.ucrChk2ndXVariable = New instat.ucrCheck()
-        Me.ucrChkXVariable = New instat.ucrCheck()
         Me.cmdBoxPlotOptions = New System.Windows.Forms.Button()
         Me.cmdOptions = New System.Windows.Forms.Button()
+        Me.rdoViolin = New System.Windows.Forms.RadioButton()
+        Me.rdoJitter = New System.Windows.Forms.RadioButton()
+        Me.rdoBoxplot = New System.Windows.Forms.RadioButton()
+        Me.ucrPnlPlots = New instat.UcrPanel()
+        Me.ucrChkMargins = New instat.ucrCheck()
+        Me.ucrChk2ndFacet = New instat.ucrCheck()
+        Me.ucrChkFacet = New instat.ucrCheck()
         Me.ucrReceiverMoreData = New instat.ucrReceiverSingle()
         Me.ucrChkMoreData = New instat.ucrCheck()
         Me.ucrReceiverStation = New instat.ucrReceiverSingle()
@@ -47,10 +49,11 @@ Partial Class dlgClimaticBoxPlot
         Me.ucrChkHorizontalBoxplot = New instat.ucrCheck()
         Me.ucrChkVarWidth = New instat.ucrCheck()
         Me.ucrSelectorClimaticBoxPlot = New instat.ucrSelectorByDataFrameAddRemove()
-        Me.rdoViolin = New System.Windows.Forms.RadioButton()
-        Me.rdoJitter = New System.Windows.Forms.RadioButton()
-        Me.rdoBoxplot = New System.Windows.Forms.RadioButton()
-        Me.ucrPnlPlots = New instat.UcrPanel()
+        Me.ucrPnlLayoutOptions = New instat.UcrPanel()
+        Me.rdoYearThenData = New System.Windows.Forms.RadioButton()
+        Me.rdoDataThenYear = New System.Windows.Forms.RadioButton()
+        Me.ucrReceiverFacet = New instat.ucrReceiverSingle()
+        Me.ucrReceiver2ndFacet = New instat.ucrReceiverSingle()
         Me.grpLayout.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -68,9 +71,9 @@ Partial Class dlgClimaticBoxPlot
         Me.lblYear.AutoSize = True
         Me.lblYear.Location = New System.Drawing.Point(263, 206)
         Me.lblYear.Name = "lblYear"
-        Me.lblYear.Size = New System.Drawing.Size(40, 13)
+        Me.lblYear.Size = New System.Drawing.Size(65, 13)
         Me.lblYear.TabIndex = 28
-        Me.lblYear.Text = "Month:"
+        Me.lblYear.Text = "Within Year:"
         '
         'lblDate
         '
@@ -101,57 +104,20 @@ Partial Class dlgClimaticBoxPlot
         '
         'grpLayout
         '
+        Me.grpLayout.Controls.Add(Me.ucrReceiver2ndFacet)
+        Me.grpLayout.Controls.Add(Me.ucrReceiverFacet)
+        Me.grpLayout.Controls.Add(Me.rdoDataThenYear)
+        Me.grpLayout.Controls.Add(Me.rdoYearThenData)
+        Me.grpLayout.Controls.Add(Me.ucrPnlLayoutOptions)
         Me.grpLayout.Controls.Add(Me.ucrChkMargins)
-        Me.grpLayout.Controls.Add(Me.ucrChkStation)
-        Me.grpLayout.Controls.Add(Me.ucrChkMonth)
-        Me.grpLayout.Controls.Add(Me.ucrChk2ndXVariable)
-        Me.grpLayout.Controls.Add(Me.ucrChkXVariable)
+        Me.grpLayout.Controls.Add(Me.ucrChk2ndFacet)
+        Me.grpLayout.Controls.Add(Me.ucrChkFacet)
         Me.grpLayout.Location = New System.Drawing.Point(10, 355)
         Me.grpLayout.Name = "grpLayout"
-        Me.grpLayout.Size = New System.Drawing.Size(396, 100)
+        Me.grpLayout.Size = New System.Drawing.Size(396, 135)
         Me.grpLayout.TabIndex = 34
         Me.grpLayout.TabStop = False
         Me.grpLayout.Text = "Layout"
-        '
-        'ucrChkMargins
-        '
-        Me.ucrChkMargins.Checked = False
-        Me.ucrChkMargins.Location = New System.Drawing.Point(16, 78)
-        Me.ucrChkMargins.Name = "ucrChkMargins"
-        Me.ucrChkMargins.Size = New System.Drawing.Size(135, 20)
-        Me.ucrChkMargins.TabIndex = 37
-        '
-        'ucrChkStation
-        '
-        Me.ucrChkStation.Checked = False
-        Me.ucrChkStation.Location = New System.Drawing.Point(176, 55)
-        Me.ucrChkStation.Name = "ucrChkStation"
-        Me.ucrChkStation.Size = New System.Drawing.Size(135, 20)
-        Me.ucrChkStation.TabIndex = 36
-        '
-        'ucrChkMonth
-        '
-        Me.ucrChkMonth.Checked = False
-        Me.ucrChkMonth.Location = New System.Drawing.Point(16, 52)
-        Me.ucrChkMonth.Name = "ucrChkMonth"
-        Me.ucrChkMonth.Size = New System.Drawing.Size(135, 20)
-        Me.ucrChkMonth.TabIndex = 35
-        '
-        'ucrChk2ndXVariable
-        '
-        Me.ucrChk2ndXVariable.Checked = False
-        Me.ucrChk2ndXVariable.Location = New System.Drawing.Point(176, 26)
-        Me.ucrChk2ndXVariable.Name = "ucrChk2ndXVariable"
-        Me.ucrChk2ndXVariable.Size = New System.Drawing.Size(135, 20)
-        Me.ucrChk2ndXVariable.TabIndex = 34
-        '
-        'ucrChkXVariable
-        '
-        Me.ucrChkXVariable.Checked = False
-        Me.ucrChkXVariable.Location = New System.Drawing.Point(16, 26)
-        Me.ucrChkXVariable.Name = "ucrChkXVariable"
-        Me.ucrChkXVariable.Size = New System.Drawing.Size(135, 20)
-        Me.ucrChkXVariable.TabIndex = 33
         '
         'cmdBoxPlotOptions
         '
@@ -172,6 +138,85 @@ Partial Class dlgClimaticBoxPlot
         Me.cmdOptions.Tag = "Options"
         Me.cmdOptions.Text = "Plot Options"
         Me.cmdOptions.UseVisualStyleBackColor = True
+        '
+        'rdoViolin
+        '
+        Me.rdoViolin.Appearance = System.Windows.Forms.Appearance.Button
+        Me.rdoViolin.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoViolin.FlatAppearance.BorderSize = 2
+        Me.rdoViolin.FlatAppearance.CheckedBackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoViolin.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.rdoViolin.Location = New System.Drawing.Point(262, 12)
+        Me.rdoViolin.Name = "rdoViolin"
+        Me.rdoViolin.Size = New System.Drawing.Size(98, 27)
+        Me.rdoViolin.TabIndex = 40
+        Me.rdoViolin.TabStop = True
+        Me.rdoViolin.Text = "Violin Plot"
+        Me.rdoViolin.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.rdoViolin.UseVisualStyleBackColor = True
+        '
+        'rdoJitter
+        '
+        Me.rdoJitter.Appearance = System.Windows.Forms.Appearance.Button
+        Me.rdoJitter.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoJitter.FlatAppearance.BorderSize = 2
+        Me.rdoJitter.FlatAppearance.CheckedBackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoJitter.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.rdoJitter.Location = New System.Drawing.Point(166, 12)
+        Me.rdoJitter.Name = "rdoJitter"
+        Me.rdoJitter.Size = New System.Drawing.Size(98, 27)
+        Me.rdoJitter.TabIndex = 39
+        Me.rdoJitter.TabStop = True
+        Me.rdoJitter.Text = "Jitter Plot"
+        Me.rdoJitter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.rdoJitter.UseVisualStyleBackColor = True
+        '
+        'rdoBoxplot
+        '
+        Me.rdoBoxplot.Appearance = System.Windows.Forms.Appearance.Button
+        Me.rdoBoxplot.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoBoxplot.FlatAppearance.BorderSize = 2
+        Me.rdoBoxplot.FlatAppearance.CheckedBackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoBoxplot.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.rdoBoxplot.Location = New System.Drawing.Point(70, 12)
+        Me.rdoBoxplot.Name = "rdoBoxplot"
+        Me.rdoBoxplot.Size = New System.Drawing.Size(98, 27)
+        Me.rdoBoxplot.TabIndex = 38
+        Me.rdoBoxplot.TabStop = True
+        Me.rdoBoxplot.Text = "Boxplot"
+        Me.rdoBoxplot.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.rdoBoxplot.UseVisualStyleBackColor = True
+        '
+        'ucrPnlPlots
+        '
+        Me.ucrPnlPlots.Location = New System.Drawing.Point(57, 8)
+        Me.ucrPnlPlots.Name = "ucrPnlPlots"
+        Me.ucrPnlPlots.Size = New System.Drawing.Size(322, 35)
+        Me.ucrPnlPlots.TabIndex = 37
+        '
+        'ucrChkMargins
+        '
+        Me.ucrChkMargins.Checked = False
+        Me.ucrChkMargins.Location = New System.Drawing.Point(16, 106)
+        Me.ucrChkMargins.Name = "ucrChkMargins"
+        Me.ucrChkMargins.Size = New System.Drawing.Size(109, 20)
+        Me.ucrChkMargins.TabIndex = 37
+        '
+        'ucrChk2ndFacet
+        '
+        Me.ucrChk2ndFacet.Checked = False
+        Me.ucrChk2ndFacet.Location = New System.Drawing.Point(16, 80)
+        Me.ucrChk2ndFacet.Name = "ucrChk2ndFacet"
+        Me.ucrChk2ndFacet.Size = New System.Drawing.Size(135, 20)
+        Me.ucrChk2ndFacet.TabIndex = 36
+        '
+        'ucrChkFacet
+        '
+        Me.ucrChkFacet.Checked = False
+        Me.ucrChkFacet.Location = New System.Drawing.Point(16, 52)
+        Me.ucrChkFacet.Name = "ucrChkFacet"
+        Me.ucrChkFacet.Size = New System.Drawing.Size(135, 20)
+        Me.ucrChkFacet.TabIndex = 35
         '
         'ucrReceiverMoreData
         '
@@ -255,14 +300,14 @@ Partial Class dlgClimaticBoxPlot
         '
         'ucrBase
         '
-        Me.ucrBase.Location = New System.Drawing.Point(10, 501)
+        Me.ucrBase.Location = New System.Drawing.Point(10, 526)
         Me.ucrBase.Name = "ucrBase"
         Me.ucrBase.Size = New System.Drawing.Size(405, 52)
         Me.ucrBase.TabIndex = 21
         '
         'ucrSavePlot
         '
-        Me.ucrSavePlot.Location = New System.Drawing.Point(10, 466)
+        Me.ucrSavePlot.Location = New System.Drawing.Point(10, 496)
         Me.ucrSavePlot.Name = "ucrSavePlot"
         Me.ucrSavePlot.Size = New System.Drawing.Size(254, 24)
         Me.ucrSavePlot.TabIndex = 20
@@ -293,66 +338,64 @@ Partial Class dlgClimaticBoxPlot
         Me.ucrSelectorClimaticBoxPlot.Size = New System.Drawing.Size(210, 180)
         Me.ucrSelectorClimaticBoxPlot.TabIndex = 15
         '
-        'rdoViolin
+        'ucrPnlLayoutOptions
         '
-        Me.rdoViolin.Appearance = System.Windows.Forms.Appearance.Button
-        Me.rdoViolin.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaption
-        Me.rdoViolin.FlatAppearance.BorderSize = 2
-        Me.rdoViolin.FlatAppearance.CheckedBackColor = System.Drawing.SystemColors.ActiveCaption
-        Me.rdoViolin.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.rdoViolin.Location = New System.Drawing.Point(262, 12)
-        Me.rdoViolin.Name = "rdoViolin"
-        Me.rdoViolin.Size = New System.Drawing.Size(98, 27)
-        Me.rdoViolin.TabIndex = 40
-        Me.rdoViolin.TabStop = True
-        Me.rdoViolin.Text = "Violin Plot"
-        Me.rdoViolin.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        Me.rdoViolin.UseVisualStyleBackColor = True
+        Me.ucrPnlLayoutOptions.Location = New System.Drawing.Point(6, 17)
+        Me.ucrPnlLayoutOptions.Name = "ucrPnlLayoutOptions"
+        Me.ucrPnlLayoutOptions.Size = New System.Drawing.Size(248, 29)
+        Me.ucrPnlLayoutOptions.TabIndex = 38
         '
-        'rdoJitter
+        'rdoYearThenData
         '
-        Me.rdoJitter.Appearance = System.Windows.Forms.Appearance.Button
-        Me.rdoJitter.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaption
-        Me.rdoJitter.FlatAppearance.BorderSize = 2
-        Me.rdoJitter.FlatAppearance.CheckedBackColor = System.Drawing.SystemColors.ActiveCaption
-        Me.rdoJitter.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.rdoJitter.Location = New System.Drawing.Point(166, 12)
-        Me.rdoJitter.Name = "rdoJitter"
-        Me.rdoJitter.Size = New System.Drawing.Size(98, 27)
-        Me.rdoJitter.TabIndex = 39
-        Me.rdoJitter.TabStop = True
-        Me.rdoJitter.Text = "Jitter Plot"
-        Me.rdoJitter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        Me.rdoJitter.UseVisualStyleBackColor = True
+        Me.rdoYearThenData.AutoSize = True
+        Me.rdoYearThenData.Location = New System.Drawing.Point(16, 19)
+        Me.rdoYearThenData.Name = "rdoYearThenData"
+        Me.rdoYearThenData.Size = New System.Drawing.Size(101, 17)
+        Me.rdoYearThenData.TabIndex = 39
+        Me.rdoYearThenData.TabStop = True
+        Me.rdoYearThenData.Text = "Year Then Data"
+        Me.rdoYearThenData.UseVisualStyleBackColor = True
         '
-        'rdoBoxplot
+        'rdoDataThenYear
         '
-        Me.rdoBoxplot.Appearance = System.Windows.Forms.Appearance.Button
-        Me.rdoBoxplot.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaption
-        Me.rdoBoxplot.FlatAppearance.BorderSize = 2
-        Me.rdoBoxplot.FlatAppearance.CheckedBackColor = System.Drawing.SystemColors.ActiveCaption
-        Me.rdoBoxplot.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.rdoBoxplot.Location = New System.Drawing.Point(70, 12)
-        Me.rdoBoxplot.Name = "rdoBoxplot"
-        Me.rdoBoxplot.Size = New System.Drawing.Size(98, 27)
-        Me.rdoBoxplot.TabIndex = 38
-        Me.rdoBoxplot.TabStop = True
-        Me.rdoBoxplot.Text = "Boxplot"
-        Me.rdoBoxplot.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        Me.rdoBoxplot.UseVisualStyleBackColor = True
+        Me.rdoDataThenYear.AutoSize = True
+        Me.rdoDataThenYear.Location = New System.Drawing.Point(158, 19)
+        Me.rdoDataThenYear.Name = "rdoDataThenYear"
+        Me.rdoDataThenYear.Size = New System.Drawing.Size(101, 17)
+        Me.rdoDataThenYear.TabIndex = 39
+        Me.rdoDataThenYear.TabStop = True
+        Me.rdoDataThenYear.Text = "Data Then Year"
+        Me.rdoDataThenYear.UseVisualStyleBackColor = True
         '
-        'ucrPnlPlots
+        'ucrReceiverFacet
         '
-        Me.ucrPnlPlots.Location = New System.Drawing.Point(57, 8)
-        Me.ucrPnlPlots.Name = "ucrPnlPlots"
-        Me.ucrPnlPlots.Size = New System.Drawing.Size(322, 35)
-        Me.ucrPnlPlots.TabIndex = 37
+        Me.ucrReceiverFacet.frmParent = Me
+        Me.ucrReceiverFacet.Location = New System.Drawing.Point(158, 53)
+        Me.ucrReceiverFacet.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverFacet.Name = "ucrReceiverFacet"
+        Me.ucrReceiverFacet.Selector = Nothing
+        Me.ucrReceiverFacet.Size = New System.Drawing.Size(120, 20)
+        Me.ucrReceiverFacet.strNcFilePath = ""
+        Me.ucrReceiverFacet.TabIndex = 40
+        Me.ucrReceiverFacet.ucrSelector = Nothing
+        '
+        'ucrReceiver2ndFacet
+        '
+        Me.ucrReceiver2ndFacet.frmParent = Me
+        Me.ucrReceiver2ndFacet.Location = New System.Drawing.Point(158, 77)
+        Me.ucrReceiver2ndFacet.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiver2ndFacet.Name = "ucrReceiver2ndFacet"
+        Me.ucrReceiver2ndFacet.Selector = Nothing
+        Me.ucrReceiver2ndFacet.Size = New System.Drawing.Size(120, 20)
+        Me.ucrReceiver2ndFacet.strNcFilePath = ""
+        Me.ucrReceiver2ndFacet.TabIndex = 40
+        Me.ucrReceiver2ndFacet.ucrSelector = Nothing
         '
         'dlgClimaticBoxPlot
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(435, 564)
+        Me.ClientSize = New System.Drawing.Size(435, 577)
         Me.Controls.Add(Me.rdoViolin)
         Me.Controls.Add(Me.rdoJitter)
         Me.Controls.Add(Me.rdoBoxplot)
@@ -384,6 +427,7 @@ Partial Class dlgClimaticBoxPlot
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Climatic BoxPlot"
         Me.grpLayout.ResumeLayout(False)
+        Me.grpLayout.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -407,14 +451,17 @@ Partial Class dlgClimaticBoxPlot
     Friend WithEvents ucrReceiverMoreData As ucrReceiverSingle
     Friend WithEvents grpLayout As GroupBox
     Friend WithEvents ucrChkMargins As ucrCheck
-    Friend WithEvents ucrChkStation As ucrCheck
-    Friend WithEvents ucrChkMonth As ucrCheck
-    Friend WithEvents ucrChk2ndXVariable As ucrCheck
-    Friend WithEvents ucrChkXVariable As ucrCheck
+    Friend WithEvents ucrChk2ndFacet As ucrCheck
+    Friend WithEvents ucrChkFacet As ucrCheck
     Friend WithEvents cmdBoxPlotOptions As Button
     Friend WithEvents cmdOptions As Button
     Friend WithEvents rdoViolin As RadioButton
     Friend WithEvents rdoJitter As RadioButton
     Friend WithEvents rdoBoxplot As RadioButton
     Friend WithEvents ucrPnlPlots As UcrPanel
+    Friend WithEvents rdoYearThenData As RadioButton
+    Friend WithEvents ucrPnlLayoutOptions As UcrPanel
+    Friend WithEvents rdoDataThenYear As RadioButton
+    Friend WithEvents ucrReceiverFacet As ucrReceiverSingle
+    Friend WithEvents ucrReceiver2ndFacet As ucrReceiverSingle
 End Class
