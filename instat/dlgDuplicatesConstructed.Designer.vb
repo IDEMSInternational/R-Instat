89195fc102d6e548ee765dee7186454c64ff33ae
Lily <Admin@NEW-KT5NM8N55N7>
2017-08-31 16:50:25 +0100

    tab order

--- instat/dlgDuplicatesConstructed.Designer.vb
+++ instat/dlgDuplicatesConstructed.Designer.vb
@@ -31,20 +31,20 @@ Partial Class dlgDuplicatesConstructed
         Me.ttDuplicates = New System.Windows.Forms.ToolTip(Me.components)
         Me.rdoSuccessiveValues = New System.Windows.Forms.RadioButton()
         Me.grpOptions = New System.Windows.Forms.GroupBox()
-        Me.ucrInputTolerance = New instat.ucrInputTextBox()
-        Me.ucrChkTolerance = New instat.ucrCheck()
-        Me.ucrNudOmit = New instat.ucrNud()
-        Me.ucrChkOmitValues = New instat.ucrCheck()
-        Me.ucrInputConditions = New instat.ucrInputComboBox()
         Me.lblSelectedVariable = New System.Windows.Forms.Label()
+        Me.lblVariablesToDuplicate = New System.Windows.Forms.Label()
+        Me.ucrReceiverForSelectedVariables = New instat.ucrReceiverMultiple()
         Me.ucrReceiverForSuccessiveValues = New instat.ucrReceiverSingle()
         Me.ucrPnlDuplicates = New instat.UcrPanel()
         Me.ucrNewColumnName = New instat.ucrSave()
         Me.ucrPnlOptions = New instat.UcrPanel()
         Me.ucrBase = New instat.ucrButtons()
         Me.ucrSelectorDuplicateswithVariables = New instat.ucrSelectorByDataFrameAddRemove()
-        Me.ucrReceiverForSelectedVariables = New instat.ucrReceiverMultiple()
-        Me.lblVariablesToDuplicate = New System.Windows.Forms.Label()
+        Me.ucrInputTolerance = New instat.ucrInputTextBox()
+        Me.ucrChkTolerance = New instat.ucrCheck()
+        Me.ucrNudOmit = New instat.ucrNud()
+        Me.ucrChkOmitValues = New instat.ucrCheck()
+        Me.ucrInputConditions = New instat.ucrInputComboBox()
         Me.grpOptions.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -86,7 +86,7 @@ Partial Class dlgDuplicatesConstructed
         Me.rdoIndexNumberOfDuplicates.Location = New System.Drawing.Point(10, 285)
         Me.rdoIndexNumberOfDuplicates.Name = "rdoIndexNumberOfDuplicates"
         Me.rdoIndexNumberOfDuplicates.Size = New System.Drawing.Size(156, 17)
-        Me.rdoIndexNumberOfDuplicates.TabIndex = 10
+        Me.rdoIndexNumberOfDuplicates.TabIndex = 12
         Me.rdoIndexNumberOfDuplicates.TabStop = True
         Me.rdoIndexNumberOfDuplicates.Text = "Index Number of Duplicates"
         Me.rdoIndexNumberOfDuplicates.UseVisualStyleBackColor = True
@@ -97,7 +97,7 @@ Partial Class dlgDuplicatesConstructed
         Me.rdoDuplicatesOnly.Location = New System.Drawing.Point(10, 262)
         Me.rdoDuplicatesOnly.Name = "rdoDuplicatesOnly"
         Me.rdoDuplicatesOnly.Size = New System.Drawing.Size(99, 17)
-        Me.rdoDuplicatesOnly.TabIndex = 9
+        Me.rdoDuplicatesOnly.TabIndex = 11
         Me.rdoDuplicatesOnly.TabStop = True
         Me.rdoDuplicatesOnly.Text = "Duplicates Only"
         Me.rdoDuplicatesOnly.UseVisualStyleBackColor = True
@@ -108,7 +108,7 @@ Partial Class dlgDuplicatesConstructed
         Me.rdoAllDuplicateCases.Location = New System.Drawing.Point(10, 239)
         Me.rdoAllDuplicateCases.Name = "rdoAllDuplicateCases"
         Me.rdoAllDuplicateCases.Size = New System.Drawing.Size(116, 17)
-        Me.rdoAllDuplicateCases.TabIndex = 8
+        Me.rdoAllDuplicateCases.TabIndex = 10
         Me.rdoAllDuplicateCases.TabStop = True
         Me.rdoAllDuplicateCases.Text = "All Duplicate Cases"
         Me.rdoAllDuplicateCases.UseVisualStyleBackColor = True
@@ -136,12 +136,94 @@ Partial Class dlgDuplicatesConstructed
         Me.grpOptions.Controls.Add(Me.ucrNudOmit)
         Me.grpOptions.Controls.Add(Me.ucrChkOmitValues)
         Me.grpOptions.Controls.Add(Me.ucrInputConditions)
-        Me.grpOptions.Location = New System.Drawing.Point(10, 239)
+        Me.grpOptions.Location = New System.Drawing.Point(10, 236)
         Me.grpOptions.Name = "grpOptions"
         Me.grpOptions.Size = New System.Drawing.Size(221, 124)
-        Me.grpOptions.TabIndex = 11
+        Me.grpOptions.TabIndex = 13
         Me.grpOptions.TabStop = False
         Me.grpOptions.Text = "Options"
+        '
+        'lblSelectedVariable
+        '
+        Me.lblSelectedVariable.AutoSize = True
+        Me.lblSelectedVariable.Location = New System.Drawing.Point(263, 82)
+        Me.lblSelectedVariable.Name = "lblSelectedVariable"
+        Me.lblSelectedVariable.Size = New System.Drawing.Size(108, 13)
+        Me.lblSelectedVariable.TabIndex = 5
+        Me.lblSelectedVariable.Tag = ""
+        Me.lblSelectedVariable.Text = "Variable to Duplicate:"
+        '
+        'lblVariablesToDuplicate
+        '
+        Me.lblVariablesToDuplicate.AutoSize = True
+        Me.lblVariablesToDuplicate.Location = New System.Drawing.Point(263, 82)
+        Me.lblVariablesToDuplicate.Name = "lblVariablesToDuplicate"
+        Me.lblVariablesToDuplicate.Size = New System.Drawing.Size(113, 13)
+        Me.lblVariablesToDuplicate.TabIndex = 7
+        Me.lblVariablesToDuplicate.Tag = ""
+        Me.lblVariablesToDuplicate.Text = "Variables to Duplicate:"
+        '
+        'ucrReceiverForSelectedVariables
+        '
+        Me.ucrReceiverForSelectedVariables.frmParent = Me
+        Me.ucrReceiverForSelectedVariables.Location = New System.Drawing.Point(263, 97)
+        Me.ucrReceiverForSelectedVariables.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverForSelectedVariables.Name = "ucrReceiverForSelectedVariables"
+        Me.ucrReceiverForSelectedVariables.Selector = Nothing
+        Me.ucrReceiverForSelectedVariables.Size = New System.Drawing.Size(120, 100)
+        Me.ucrReceiverForSelectedVariables.strNcFilePath = ""
+        Me.ucrReceiverForSelectedVariables.TabIndex = 8
+        Me.ucrReceiverForSelectedVariables.ucrSelector = Nothing
+        '
+        'ucrReceiverForSuccessiveValues
+        '
+        Me.ucrReceiverForSuccessiveValues.frmParent = Me
+        Me.ucrReceiverForSuccessiveValues.Location = New System.Drawing.Point(263, 97)
+        Me.ucrReceiverForSuccessiveValues.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverForSuccessiveValues.Name = "ucrReceiverForSuccessiveValues"
+        Me.ucrReceiverForSuccessiveValues.Selector = Nothing
+        Me.ucrReceiverForSuccessiveValues.Size = New System.Drawing.Size(120, 20)
+        Me.ucrReceiverForSuccessiveValues.strNcFilePath = ""
+        Me.ucrReceiverForSuccessiveValues.TabIndex = 6
+        Me.ucrReceiverForSuccessiveValues.ucrSelector = Nothing
+        '
+        'ucrPnlDuplicates
+        '
+        Me.ucrPnlDuplicates.Location = New System.Drawing.Point(3, 235)
+        Me.ucrPnlDuplicates.Name = "ucrPnlDuplicates"
+        Me.ucrPnlDuplicates.Size = New System.Drawing.Size(179, 77)
+        Me.ucrPnlDuplicates.TabIndex = 9
+        '
+        'ucrNewColumnName
+        '
+        Me.ucrNewColumnName.Location = New System.Drawing.Point(10, 372)
+        Me.ucrNewColumnName.Name = "ucrNewColumnName"
+        Me.ucrNewColumnName.Size = New System.Drawing.Size(281, 24)
+        Me.ucrNewColumnName.TabIndex = 14
+        '
+        'ucrPnlOptions
+        '
+        Me.ucrPnlOptions.Location = New System.Drawing.Point(51, 8)
+        Me.ucrPnlOptions.Name = "ucrPnlOptions"
+        Me.ucrPnlOptions.Size = New System.Drawing.Size(360, 36)
+        Me.ucrPnlOptions.TabIndex = 0
+        '
+        'ucrBase
+        '
+        Me.ucrBase.Location = New System.Drawing.Point(10, 398)
+        Me.ucrBase.Name = "ucrBase"
+        Me.ucrBase.Size = New System.Drawing.Size(401, 52)
+        Me.ucrBase.TabIndex = 15
+        '
+        'ucrSelectorDuplicateswithVariables
+        '
+        Me.ucrSelectorDuplicateswithVariables.bShowHiddenColumns = False
+        Me.ucrSelectorDuplicateswithVariables.bUseCurrentFilter = True
+        Me.ucrSelectorDuplicateswithVariables.Location = New System.Drawing.Point(10, 47)
+        Me.ucrSelectorDuplicateswithVariables.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrSelectorDuplicateswithVariables.Name = "ucrSelectorDuplicateswithVariables"
+        Me.ucrSelectorDuplicateswithVariables.Size = New System.Drawing.Size(210, 180)
+        Me.ucrSelectorDuplicateswithVariables.TabIndex = 4
         '
         'ucrInputTolerance
         '
@@ -190,93 +272,11 @@ Partial Class dlgDuplicatesConstructed
         Me.ucrInputConditions.Size = New System.Drawing.Size(137, 21)
         Me.ucrInputConditions.TabIndex = 1
         '
-        'lblSelectedVariable
-        '
-        Me.lblSelectedVariable.AutoSize = True
-        Me.lblSelectedVariable.Location = New System.Drawing.Point(263, 82)
-        Me.lblSelectedVariable.Name = "lblSelectedVariable"
-        Me.lblSelectedVariable.Size = New System.Drawing.Size(108, 13)
-        Me.lblSelectedVariable.TabIndex = 5
-        Me.lblSelectedVariable.Tag = ""
-        Me.lblSelectedVariable.Text = "Variable to Duplicate:"
-        '
-        'ucrReceiverForSuccessiveValues
-        '
-        Me.ucrReceiverForSuccessiveValues.frmParent = Me
-        Me.ucrReceiverForSuccessiveValues.Location = New System.Drawing.Point(263, 97)
-        Me.ucrReceiverForSuccessiveValues.Margin = New System.Windows.Forms.Padding(0)
-        Me.ucrReceiverForSuccessiveValues.Name = "ucrReceiverForSuccessiveValues"
-        Me.ucrReceiverForSuccessiveValues.Selector = Nothing
-        Me.ucrReceiverForSuccessiveValues.Size = New System.Drawing.Size(120, 20)
-        Me.ucrReceiverForSuccessiveValues.strNcFilePath = ""
-        Me.ucrReceiverForSuccessiveValues.TabIndex = 6
-        Me.ucrReceiverForSuccessiveValues.ucrSelector = Nothing
-        '
-        'ucrPnlDuplicates
-        '
-        Me.ucrPnlDuplicates.Location = New System.Drawing.Point(3, 235)
-        Me.ucrPnlDuplicates.Name = "ucrPnlDuplicates"
-        Me.ucrPnlDuplicates.Size = New System.Drawing.Size(179, 77)
-        Me.ucrPnlDuplicates.TabIndex = 7
-        '
-        'ucrNewColumnName
-        '
-        Me.ucrNewColumnName.Location = New System.Drawing.Point(10, 378)
-        Me.ucrNewColumnName.Name = "ucrNewColumnName"
-        Me.ucrNewColumnName.Size = New System.Drawing.Size(281, 24)
-        Me.ucrNewColumnName.TabIndex = 12
-        '
-        'ucrPnlOptions
-        '
-        Me.ucrPnlOptions.Location = New System.Drawing.Point(51, 8)
-        Me.ucrPnlOptions.Name = "ucrPnlOptions"
-        Me.ucrPnlOptions.Size = New System.Drawing.Size(360, 36)
-        Me.ucrPnlOptions.TabIndex = 0
-        '
-        'ucrBase
-        '
-        Me.ucrBase.Location = New System.Drawing.Point(10, 402)
-        Me.ucrBase.Name = "ucrBase"
-        Me.ucrBase.Size = New System.Drawing.Size(401, 52)
-        Me.ucrBase.TabIndex = 13
-        '
-        'ucrSelectorDuplicateswithVariables
-        '
-        Me.ucrSelectorDuplicateswithVariables.bShowHiddenColumns = False
-        Me.ucrSelectorDuplicateswithVariables.bUseCurrentFilter = True
-        Me.ucrSelectorDuplicateswithVariables.Location = New System.Drawing.Point(10, 47)
-        Me.ucrSelectorDuplicateswithVariables.Margin = New System.Windows.Forms.Padding(0)
-        Me.ucrSelectorDuplicateswithVariables.Name = "ucrSelectorDuplicateswithVariables"
-        Me.ucrSelectorDuplicateswithVariables.Size = New System.Drawing.Size(210, 180)
-        Me.ucrSelectorDuplicateswithVariables.TabIndex = 4
-        '
-        'ucrReceiverForSelectedVariables
-        '
-        Me.ucrReceiverForSelectedVariables.frmParent = Me
-        Me.ucrReceiverForSelectedVariables.Location = New System.Drawing.Point(263, 97)
-        Me.ucrReceiverForSelectedVariables.Margin = New System.Windows.Forms.Padding(0)
-        Me.ucrReceiverForSelectedVariables.Name = "ucrReceiverForSelectedVariables"
-        Me.ucrReceiverForSelectedVariables.Selector = Nothing
-        Me.ucrReceiverForSelectedVariables.Size = New System.Drawing.Size(120, 100)
-        Me.ucrReceiverForSelectedVariables.strNcFilePath = ""
-        Me.ucrReceiverForSelectedVariables.TabIndex = 14
-        Me.ucrReceiverForSelectedVariables.ucrSelector = Nothing
-        '
-        'lblVariablesToDuplicate
-        '
-        Me.lblVariablesToDuplicate.AutoSize = True
-        Me.lblVariablesToDuplicate.Location = New System.Drawing.Point(263, 82)
-        Me.lblVariablesToDuplicate.Name = "lblVariablesToDuplicate"
-        Me.lblVariablesToDuplicate.Size = New System.Drawing.Size(113, 13)
-        Me.lblVariablesToDuplicate.TabIndex = 15
-        Me.lblVariablesToDuplicate.Tag = ""
-        Me.lblVariablesToDuplicate.Text = "Variables to Duplicate:"
-        '
         'dlgDuplicatesConstructed
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(434, 464)
+        Me.ClientSize = New System.Drawing.Size(434, 456)
         Me.Controls.Add(Me.lblVariablesToDuplicate)
         Me.Controls.Add(Me.ucrReceiverForSelectedVariables)
         Me.Controls.Add(Me.lblSelectedVariable)
