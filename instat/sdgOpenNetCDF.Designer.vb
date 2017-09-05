80e1f2109ad5f7a60adfb26c437a3546e139a5c4
Lily <Admin@NEW-KT5NM8N55N7>
2017-09-05 13:04:46 +0100

    Updating netCDF

--- instat/sdgOpenNetCDF.Designer.vb
+++ instat/sdgOpenNetCDF.Designer.vb
@@ -22,13 +22,209 @@ Partial Class sdgOpenNetCDF
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.ucrInputMinLong = New instat.ucrInputTextBox()
+        Me.ucrInputMinLat = New instat.ucrInputTextBox()
+        Me.ucrInputMinZ = New instat.ucrInputTextBox()
+        Me.ucrInputMinTime = New instat.ucrInputTextBox()
+        Me.ucrInputMaxLong = New instat.ucrInputTextBox()
+        Me.ucrInputMaxLat = New instat.ucrInputTextBox()
+        Me.ucrInputMaxZ = New instat.ucrInputTextBox()
+        Me.ucrInputMaxTime = New instat.ucrInputTextBox()
+        Me.ucrInputFileDetails = New instat.ucrInputTextBox()
+        Me.lblMinimumLongitude = New System.Windows.Forms.Label()
+        Me.lblMinimumLatitude = New System.Windows.Forms.Label()
+        Me.lblMinZ = New System.Windows.Forms.Label()
+        Me.lblMinTime = New System.Windows.Forms.Label()
+        Me.lblMaxTime = New System.Windows.Forms.Label()
+        Me.lblMaxZ = New System.Windows.Forms.Label()
+        Me.lblMaxLatitude = New System.Windows.Forms.Label()
+        Me.lblMaxLongitude = New System.Windows.Forms.Label()
         Me.SuspendLayout()
+        '
+        'ucrInputMinLong
+        '
+        Me.ucrInputMinLong.AddQuotesIfUnrecognised = True
+        Me.ucrInputMinLong.IsMultiline = False
+        Me.ucrInputMinLong.IsReadOnly = False
+        Me.ucrInputMinLong.Location = New System.Drawing.Point(106, 200)
+        Me.ucrInputMinLong.Name = "ucrInputMinLong"
+        Me.ucrInputMinLong.Size = New System.Drawing.Size(52, 21)
+        Me.ucrInputMinLong.TabIndex = 22
+        '
+        'ucrInputMinLat
+        '
+        Me.ucrInputMinLat.AddQuotesIfUnrecognised = True
+        Me.ucrInputMinLat.IsMultiline = False
+        Me.ucrInputMinLat.IsReadOnly = False
+        Me.ucrInputMinLat.Location = New System.Drawing.Point(106, 227)
+        Me.ucrInputMinLat.Name = "ucrInputMinLat"
+        Me.ucrInputMinLat.Size = New System.Drawing.Size(52, 21)
+        Me.ucrInputMinLat.TabIndex = 21
+        '
+        'ucrInputMinZ
+        '
+        Me.ucrInputMinZ.AddQuotesIfUnrecognised = True
+        Me.ucrInputMinZ.IsMultiline = False
+        Me.ucrInputMinZ.IsReadOnly = False
+        Me.ucrInputMinZ.Location = New System.Drawing.Point(106, 254)
+        Me.ucrInputMinZ.Name = "ucrInputMinZ"
+        Me.ucrInputMinZ.Size = New System.Drawing.Size(52, 21)
+        Me.ucrInputMinZ.TabIndex = 20
+        '
+        'ucrInputMinTime
+        '
+        Me.ucrInputMinTime.AddQuotesIfUnrecognised = True
+        Me.ucrInputMinTime.IsMultiline = False
+        Me.ucrInputMinTime.IsReadOnly = False
+        Me.ucrInputMinTime.Location = New System.Drawing.Point(106, 281)
+        Me.ucrInputMinTime.Name = "ucrInputMinTime"
+        Me.ucrInputMinTime.Size = New System.Drawing.Size(52, 21)
+        Me.ucrInputMinTime.TabIndex = 19
+        '
+        'ucrInputMaxLong
+        '
+        Me.ucrInputMaxLong.AddQuotesIfUnrecognised = True
+        Me.ucrInputMaxLong.IsMultiline = False
+        Me.ucrInputMaxLong.IsReadOnly = False
+        Me.ucrInputMaxLong.Location = New System.Drawing.Point(271, 200)
+        Me.ucrInputMaxLong.Name = "ucrInputMaxLong"
+        Me.ucrInputMaxLong.Size = New System.Drawing.Size(52, 21)
+        Me.ucrInputMaxLong.TabIndex = 18
+        '
+        'ucrInputMaxLat
+        '
+        Me.ucrInputMaxLat.AddQuotesIfUnrecognised = True
+        Me.ucrInputMaxLat.IsMultiline = False
+        Me.ucrInputMaxLat.IsReadOnly = False
+        Me.ucrInputMaxLat.Location = New System.Drawing.Point(271, 227)
+        Me.ucrInputMaxLat.Name = "ucrInputMaxLat"
+        Me.ucrInputMaxLat.Size = New System.Drawing.Size(52, 21)
+        Me.ucrInputMaxLat.TabIndex = 17
+        '
+        'ucrInputMaxZ
+        '
+        Me.ucrInputMaxZ.AddQuotesIfUnrecognised = True
+        Me.ucrInputMaxZ.IsMultiline = False
+        Me.ucrInputMaxZ.IsReadOnly = False
+        Me.ucrInputMaxZ.Location = New System.Drawing.Point(271, 254)
+        Me.ucrInputMaxZ.Name = "ucrInputMaxZ"
+        Me.ucrInputMaxZ.Size = New System.Drawing.Size(52, 21)
+        Me.ucrInputMaxZ.TabIndex = 16
+        '
+        'ucrInputMaxTime
+        '
+        Me.ucrInputMaxTime.AddQuotesIfUnrecognised = True
+        Me.ucrInputMaxTime.IsMultiline = False
+        Me.ucrInputMaxTime.IsReadOnly = False
+        Me.ucrInputMaxTime.Location = New System.Drawing.Point(271, 281)
+        Me.ucrInputMaxTime.Name = "ucrInputMaxTime"
+        Me.ucrInputMaxTime.Size = New System.Drawing.Size(52, 21)
+        Me.ucrInputMaxTime.TabIndex = 15
+        '
+        'ucrInputFileDetails
+        '
+        Me.ucrInputFileDetails.AddQuotesIfUnrecognised = True
+        Me.ucrInputFileDetails.IsMultiline = True
+        Me.ucrInputFileDetails.IsReadOnly = True
+        Me.ucrInputFileDetails.Location = New System.Drawing.Point(7, 35)
+        Me.ucrInputFileDetails.Name = "ucrInputFileDetails"
+        Me.ucrInputFileDetails.Size = New System.Drawing.Size(320, 159)
+        Me.ucrInputFileDetails.TabIndex = 14
+        '
+        'lblMinimumLongitude
+        '
+        Me.lblMinimumLongitude.AutoSize = True
+        Me.lblMinimumLongitude.Location = New System.Drawing.Point(4, 203)
+        Me.lblMinimumLongitude.Name = "lblMinimumLongitude"
+        Me.lblMinimumLongitude.Size = New System.Drawing.Size(101, 13)
+        Me.lblMinimumLongitude.TabIndex = 23
+        Me.lblMinimumLongitude.Text = "Minimum Longitude:"
+        '
+        'lblMinimumLatitude
+        '
+        Me.lblMinimumLatitude.AutoSize = True
+        Me.lblMinimumLatitude.Location = New System.Drawing.Point(4, 230)
+        Me.lblMinimumLatitude.Name = "lblMinimumLatitude"
+        Me.lblMinimumLatitude.Size = New System.Drawing.Size(92, 13)
+        Me.lblMinimumLatitude.TabIndex = 24
+        Me.lblMinimumLatitude.Text = "Minimum Latitude:"
+        '
+        'lblMinZ
+        '
+        Me.lblMinZ.AutoSize = True
+        Me.lblMinZ.Location = New System.Drawing.Point(4, 257)
+        Me.lblMinZ.Name = "lblMinZ"
+        Me.lblMinZ.Size = New System.Drawing.Size(61, 13)
+        Me.lblMinZ.TabIndex = 25
+        Me.lblMinZ.Text = "Minimum Z:"
+        '
+        'lblMinTime
+        '
+        Me.lblMinTime.AutoSize = True
+        Me.lblMinTime.Location = New System.Drawing.Point(4, 284)
+        Me.lblMinTime.Name = "lblMinTime"
+        Me.lblMinTime.Size = New System.Drawing.Size(77, 13)
+        Me.lblMinTime.TabIndex = 26
+        Me.lblMinTime.Text = "Minimum Time:"
+        '
+        'lblMaxTime
+        '
+        Me.lblMaxTime.AutoSize = True
+        Me.lblMaxTime.Location = New System.Drawing.Point(164, 284)
+        Me.lblMaxTime.Name = "lblMaxTime"
+        Me.lblMaxTime.Size = New System.Drawing.Size(80, 13)
+        Me.lblMaxTime.TabIndex = 30
+        Me.lblMaxTime.Text = "Maximum Time:"
+        '
+        'lblMaxZ
+        '
+        Me.lblMaxZ.AutoSize = True
+        Me.lblMaxZ.Location = New System.Drawing.Point(164, 257)
+        Me.lblMaxZ.Name = "lblMaxZ"
+        Me.lblMaxZ.Size = New System.Drawing.Size(64, 13)
+        Me.lblMaxZ.TabIndex = 29
+        Me.lblMaxZ.Text = "Maximum Z:"
+        '
+        'lblMaxLatitude
+        '
+        Me.lblMaxLatitude.AutoSize = True
+        Me.lblMaxLatitude.Location = New System.Drawing.Point(164, 230)
+        Me.lblMaxLatitude.Name = "lblMaxLatitude"
+        Me.lblMaxLatitude.Size = New System.Drawing.Size(95, 13)
+        Me.lblMaxLatitude.TabIndex = 28
+        Me.lblMaxLatitude.Text = "Maximum Latitude:"
+        '
+        'lblMaxLongitude
+        '
+        Me.lblMaxLongitude.AutoSize = True
+        Me.lblMaxLongitude.Location = New System.Drawing.Point(164, 203)
+        Me.lblMaxLongitude.Name = "lblMaxLongitude"
+        Me.lblMaxLongitude.Size = New System.Drawing.Size(104, 13)
+        Me.lblMaxLongitude.TabIndex = 27
+        Me.lblMaxLongitude.Text = "Maximum Longitude:"
         '
         'sdgOpenNetCDF
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(791, 306)
+        Me.Controls.Add(Me.lblMaxTime)
+        Me.Controls.Add(Me.lblMaxZ)
+        Me.Controls.Add(Me.lblMaxLatitude)
+        Me.Controls.Add(Me.lblMaxLongitude)
+        Me.Controls.Add(Me.lblMinTime)
+        Me.Controls.Add(Me.lblMinZ)
+        Me.Controls.Add(Me.lblMinimumLatitude)
+        Me.Controls.Add(Me.lblMinimumLongitude)
+        Me.Controls.Add(Me.ucrInputMinLong)
+        Me.Controls.Add(Me.ucrInputMinLat)
+        Me.Controls.Add(Me.ucrInputMinZ)
+        Me.Controls.Add(Me.ucrInputMinTime)
+        Me.Controls.Add(Me.ucrInputMaxLong)
+        Me.Controls.Add(Me.ucrInputMaxLat)
+        Me.Controls.Add(Me.ucrInputMaxZ)
+        Me.Controls.Add(Me.ucrInputMaxTime)
+        Me.Controls.Add(Me.ucrInputFileDetails)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.MaximizeBox = False
         Me.MinimizeBox = False
@@ -36,6 +232,25 @@ Partial Class sdgOpenNetCDF
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
         Me.Text = "NetCDF Options"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents ucrInputFileDetails As ucrInputTextBox
+    Friend WithEvents ucrInputMaxTime As ucrInputTextBox
+    Friend WithEvents ucrInputMaxZ As ucrInputTextBox
+    Friend WithEvents ucrInputMaxLat As ucrInputTextBox
+    Friend WithEvents ucrInputMaxLong As ucrInputTextBox
+    Friend WithEvents ucrInputMinTime As ucrInputTextBox
+    Friend WithEvents ucrInputMinZ As ucrInputTextBox
+    Friend WithEvents ucrInputMinLat As ucrInputTextBox
+    Friend WithEvents ucrInputMinLong As ucrInputTextBox
+    Friend WithEvents lblMinimumLongitude As Label
+    Friend WithEvents lblMinimumLatitude As Label
+    Friend WithEvents lblMinZ As Label
+    Friend WithEvents lblMinTime As Label
+    Friend WithEvents lblMaxTime As Label
+    Friend WithEvents lblMaxZ As Label
+    Friend WithEvents lblMaxLatitude As Label
+    Friend WithEvents lblMaxLongitude As Label
 End Class
--- instat/sdgOpenNetCDF.vb
+++ instat/sdgOpenNetCDF.vb
@@ -1,3 +1,86 @@
-﻿Public Class sdgOpenNetCDF
+﻿' R-Instat
+' Copyright (C) 2015-2017
+'
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+'
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License 
+' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+Imports instat.Translations
+Public Class sdgOpenNetCDF
+    Private clsRCDF, clsRSubsetFunction, clsRLatFunction, clsRLongFunction, clsRZFunction, clsRTimeFunction As New RFunction
+    Private strShort As String
+    Private Sub sdgOpenNetCDF_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        autoTranslate(Me)
+    End Sub
+
+    Public Sub InitialiseControls()
+        ' subset = list(lat = c(a,b), long = c(a, b), z = c(a, b), time = c(a, b))
+
+        ucrInputMinLong.SetParameter(New RParameter("long1", 0, bNewIncludeArgumentName:=False))
+        ucrInputMinLong.SetValidationTypeAsNumeric()
+        ucrInputMinLong.AddQuotesIfUnrecognised = False
+        ucrInputMinLong.Focus()
+
+        ucrInputMaxLong.SetParameter(New RParameter("long2", 1, bNewIncludeArgumentName:=False))
+        ucrInputMaxLong.SetValidationTypeAsNumeric()
+        ucrInputMaxLong.AddQuotesIfUnrecognised = False
+
+        ucrInputMinLat.SetParameter(New RParameter("lat1", 0, bNewIncludeArgumentName:=False))
+        ucrInputMinLat.SetValidationTypeAsNumeric()
+        ucrInputMinLat.AddQuotesIfUnrecognised = False
+
+        ucrInputMaxLat.SetParameter(New RParameter("lat2", 1, bNewIncludeArgumentName:=False))
+        ucrInputMaxLat.SetValidationTypeAsNumeric()
+        ucrInputMaxLat.AddQuotesIfUnrecognised = False
+
+        ucrInputMinZ.SetParameter(New RParameter("Z1", 0, bNewIncludeArgumentName:=False))
+        ucrInputMinZ.SetValidationTypeAsNumeric()
+        ucrInputMinZ.AddQuotesIfUnrecognised = False
+
+        ucrInputMaxZ.SetParameter(New RParameter("Z2", 1, bNewIncludeArgumentName:=False))
+        ucrInputMaxZ.SetValidationTypeAsNumeric()
+        ucrInputMaxZ.AddQuotesIfUnrecognised = False
+
+        ucrInputMinTime.SetParameter(New RParameter("time1", 0, bNewIncludeArgumentName:=False))
+        ucrInputMinTime.SetValidationTypeAsNumeric()
+        ucrInputMinTime.AddQuotesIfUnrecognised = False
+
+        ucrInputMaxTime.SetParameter(New RParameter("time2", 1, bNewIncludeArgumentName:=False))
+        ucrInputMaxTime.SetValidationTypeAsNumeric()
+        ucrInputMaxTime.AddQuotesIfUnrecognised = False
+    End Sub
+
+    Public Sub SetRFunction(clsRNewCDF As RFunction, clsRNewLatFunction As RFunction, clsRNewLongFunction As RFunction, clsRNewZFunction As RFunction, clsRNewTimeFunction As RFunction, strNewShort As String, Optional bReset As Boolean = False)
+        'If Not bControlsInitialised Then
+        '    InitialiseControls()
+        'End If
+        clsRCDF = clsRNewCDF
+        clsRLatFunction = clsRNewLatFunction
+        clsRLongFunction = clsRNewLongFunction
+        clsRZFunction = clsRNewZFunction
+        clsRTimeFunction = clsRNewTimeFunction
+        strShort = strNewShort
+
+        ucrInputFileDetails.Text = strShort
+
+        ucrInputMinLong.SetRCode(clsRLongFunction, bReset, bCloneIfNeeded:=True)
+        ucrInputMaxLong.SetRCode(clsRLongFunction, bReset, bCloneIfNeeded:=True)
+        ucrInputMinLat.SetRCode(clsRLatFunction, bReset, bCloneIfNeeded:=True)
+        ucrInputMaxLat.SetRCode(clsRLatFunction, bReset, bCloneIfNeeded:=True)
+        ucrInputMinZ.SetRCode(clsRZFunction, bReset, bCloneIfNeeded:=True)
+        ucrInputMaxZ.SetRCode(clsRZFunction, bReset, bCloneIfNeeded:=True)
+        ucrInputMinTime.SetRCode(clsRTimeFunction, bReset, bCloneIfNeeded:=True)
+        ucrInputMaxTime.SetRCode(clsRTimeFunction, bReset, bCloneIfNeeded:=True)
+
+        ucrInputMinLong.Focus()
+    End Sub
 End Class
