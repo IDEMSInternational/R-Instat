4648db466d85ee08c68a1b647b52808412e90a3f
Danny Parsons <danny@aims.ac.za>
2017-09-06 14:38:09 +0100

    fixed bugs creating data frame with subset

--- instat/static/InstatObject/R/instat_object_R6.R
+++ instat/static/InstatObject/R/instat_object_R6.R
@@ -1149,7 +1149,8 @@ instat_object$set("public", "import_NetCDF", function(nc, name, only_data_vars =
     if(use_prefix) curr_name <- paste0(name, "_", i)
     else curr_name <- name
     data_names <- c(data_names, curr_name)
-    curr_boundary <- boundary[names(boundary) %in% dim_groups[[i]]]
+    if(!missing(boundary)) curr_boundary <- boundary[names(boundary) %in% dim_groups[[i]]]
+    else curr_boundary <- NULL
     data_list[[curr_name]] <- nc_as_data_frame(nc, var_groups[[i]], keep_raw_time = keep_raw_time, include_metadata = include_metadata, boundary = curr_boundary)
     tmp_list <- list()
     tmp_list[[curr_name]] <- data_list[[curr_name]]
