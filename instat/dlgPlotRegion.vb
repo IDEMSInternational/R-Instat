129f8a11a5d1e70665e80b6e8300b72f2bebc0f4
Danny Parsons <danny@aims.ac.za>
2017-09-12 11:08:25 +0100

    fixing bugs in plot region dialog

--- instat/dlgPlotRegion.vb
+++ instat/dlgPlotRegion.vb
@@ -20,6 +20,7 @@ Public Class dlgPlotRegion
     Private bReset As Boolean = True
     Private clsPlotRegionFunction As RFunction
     Private clsAsDateTimeFunction As RFunction
+
     Private Sub dlgPlotRegion_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         If bFirstLoad Then
             InitialiseDialog()
@@ -35,6 +36,8 @@ Public Class dlgPlotRegion
     End Sub
 
     Private Sub InitialiseDialog()
+        ucrBase.clsRsyntax.iCallType = 3
+
         ucrReceiverLongitude.SetParameter(New RParameter("lon", 1))
         ucrReceiverLongitude.Selector = ucrSelectorPlotRegion
         ucrReceiverLongitude.SetParameterIsRFunction()
@@ -47,21 +50,25 @@ Public Class dlgPlotRegion
         ucrReceiverElement.Selector = ucrSelectorPlotRegion
         ucrReceiverElement.SetParameterIsRFunction()
 
-        ucrReceiverLatitude.SetParameter(New RParameter("time", 4))
-        ucrReceiverLatitude.Selector = ucrSelectorPlotRegion
-        ucrReceiverLatitude.SetParameterIsRFunction()
-
+        ucrReceiverYear.SetParameter(New RParameter("time", 4))
+        ucrReceiverYear.Selector = ucrSelectorPlotRegion
+        ucrReceiverYear.SetParameterIsRFunction()
+        ucrReceiverYear.SetIncludedDataTypes({"Date"})
     End Sub
 
     Private Sub SetDefaults()
         clsPlotRegionFunction = New RFunction
         clsAsDateTimeFunction = New RFunction
+
         ucrSelectorPlotRegion.Reset()
         ucrReceiverLongitude.SetMeAsReceiver()
-        clsPlotRegionFunction.SetRCommand("plot_region")
-        clsPlotRegionFunction.iCallType = 3
-        clsAsDateTimeFunction.SetRCommand("as.Date")
         clsPlotRegionFunction.AddParameter("time_point", clsRFunctionParameter:=clsAsDateTimeFunction)
+
+        clsPlotRegionFunction.SetRCommand("plot.region")
+        clsAsDateTimeFunction.SetRCommand("as.Date")
+        clsAsDateTimeFunction.AddParameter("format", Chr(34) & "%Y-%m-%d" & Chr(34), iPosition:=1)
+        dtpDateTimePicker.Value = New DateTime(2015, 1, 1)
+
         AsDate()
         ucrBase.clsRsyntax.SetBaseRFunction(clsPlotRegionFunction)
     End Sub
@@ -81,8 +88,7 @@ Public Class dlgPlotRegion
     End Sub
 
     Private Sub AsDate()
-        clsAsDateTimeFunction.AddParameter("x", Chr(34) & dtpDateTimePicker.Value.Year & "-" & dtpDateTimePicker.Value.Month & "-" & dtpDateTimePicker.Value.Day & Chr(34), iPosition:=0, bIncludeArgumentName:=False)
-        clsAsDateTimeFunction.AddParameter("format", Chr(34) & "%Y-%m-%d" & Chr(34), iPosition:=1, bIncludeArgumentName:=False)
+        clsAsDateTimeFunction.AddParameter("x", Chr(34) & dtpDateTimePicker.Value.Year & "-" & dtpDateTimePicker.Value.Month & "-" & dtpDateTimePicker.Value.Day & Chr(34), iPosition:=0)
     End Sub
 
     Private Sub dtpDateTimePicker_ValueChanged(sender As Object, e As EventArgs) Handles dtpDateTimePicker.ValueChanged
