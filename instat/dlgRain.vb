97a0f787ab8a08e607d1283c904b9290f38ade18
Lazarus Kioko Muthenya <klmuthenya@outlook.com>
2017-09-22 12:26:31 +0300

    Renamed dialogue and added control.

---
+++ instat/dlgRain.vb
@@ -0,0 +1,9 @@
+﻿Public Class dlgRain
+    Private Sub UcrSelectorByDataFrameAddRemove1_Load(sender As Object, e As EventArgs) Handles ucrSelectorRain.Load
+
+    End Sub
+
+    Private Sub dlgRain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
