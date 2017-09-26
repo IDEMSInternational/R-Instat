0385384eea55f698489e8b1992ed9b5832ce9021
Lazarus Kioko Muthenya <klmuthenya@outlook.com>
2017-09-26 17:02:51 +0300

    Removing unneccesary code.

--- instat/dlgClimaticCheckDataTemperature.vb
+++ instat/dlgClimaticCheckDataTemperature.vb
@@ -1,25 +1,3 @@
 ﻿Public Class dlgClimaticCheckDataTemperature
-    Private Sub dlgClimaticCheckDataTemperature_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-    End Sub
-
-    Private Sub ucrBase_Load(sender As Object, e As EventArgs) Handles ucrBase.Load
-
-    End Sub
-
-    Private Sub UcrNud7_Load(sender As Object, e As EventArgs) Handles ucrNudRangeElement1Max.Load
-
-    End Sub
-
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblRangeElement1.Click
-
-    End Sub
-
-    Private Sub lblElement2_Click(sender As Object, e As EventArgs) Handles lblRangeElement2.Click
-
-    End Sub
-
-    Private Sub grpRange_Enter(sender As Object, e As EventArgs)
-
-    End Sub
 End Class
