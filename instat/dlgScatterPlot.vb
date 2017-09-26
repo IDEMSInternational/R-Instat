a16fe2ca4f9926c14613ed3535c1e42744641e98
maxwell fundi <maxwellfundi@live.com>
2017-09-26 10:34:03 +0300

    Swapping the conditions

--- instat/dlgScatterPlot.vb
+++ instat/dlgScatterPlot.vb
@@ -95,6 +95,7 @@ Public Class dlgScatterPlot
         clsGeomSmoothFunc.SetPackageName("ggplot2")
         clsGeomSmoothFunc.SetRCommand("geom_smooth")
         clsGeomSmoothFunc.AddParameter("method", Chr(34) & "lm" & Chr(34), iPosition:=0)
+        clsGeomSmoothFunc.AddParameter("se", "FALSE", iPosition:=1)
         clsGeomSmoothParameter.SetArgumentName("geom_smooth")
         clsGeomSmoothParameter.SetArgument(clsGeomSmoothFunc)
         ucrChkLineofBestFit.SetText("Add Line of Best Fit")
@@ -103,9 +104,9 @@ Public Class dlgScatterPlot
 
         ucrChkWithSE.SetText("With Standard Error")
         ucrChkWithSE.SetParameter(New RParameter("se", 1))
-        ucrChkWithSE.SetValuesCheckedAndUnchecked("FALSE", "TRUE")
-        ucrChkWithSE.SetRDefault("TRUE")
-        ucrChkWithSE.AddParameterValuesCondition(True, "se", "FALSE")
+        ucrChkWithSE.SetValuesCheckedAndUnchecked("TRUE", "FALSE")
+        ucrChkWithSE.AddParameterValuesCondition(True, "se", "TRUE")
+        ucrChkWithSE.AddParameterValuesCondition(False, "se", "FALSE")
 
         ucrSaveScatterPlot.SetPrefix("scatterplot")
         ucrSaveScatterPlot.SetSaveTypeAsGraph()
