e291c1969ec9eaabda5696f081cd3e740e8e3045
Lazarus Kioko Muthenya <klmuthenya@outlook.com>
2017-09-27 18:11:59 +0300

    Setting controls and code.

--- instat/dlgCountryColouredMap.Designer.vb
+++ instat/dlgCountryColouredMap.Designer.vb
@@ -23,20 +23,91 @@ Partial Class dlgCountryColouredMap
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.ucrBase = New instat.ucrButtons()
+        Me.ucrSelectorCountryColouredMap = New instat.ucrSelectorByDataFrameAddRemove()
+        Me.ucrReceiverCountry = New instat.ucrReceiverSingle()
+        Me.ucrReceiverColourBy = New instat.ucrReceiverSingle()
+        Me.lblCountry = New System.Windows.Forms.Label()
+        Me.lblColourBy = New System.Windows.Forms.Label()
+        Me.ucrSaveMap = New instat.ucrSave()
         Me.SuspendLayout()
         '
         'ucrBase
         '
-        Me.ucrBase.Location = New System.Drawing.Point(0, 209)
+        Me.ucrBase.Location = New System.Drawing.Point(10, 239)
         Me.ucrBase.Name = "ucrBase"
         Me.ucrBase.Size = New System.Drawing.Size(410, 52)
         Me.ucrBase.TabIndex = 0
+        '
+        'ucrSelectorCountryColouredMap
+        '
+        Me.ucrSelectorCountryColouredMap.bShowHiddenColumns = False
+        Me.ucrSelectorCountryColouredMap.bUseCurrentFilter = True
+        Me.ucrSelectorCountryColouredMap.Location = New System.Drawing.Point(10, 10)
+        Me.ucrSelectorCountryColouredMap.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrSelectorCountryColouredMap.Name = "ucrSelectorCountryColouredMap"
+        Me.ucrSelectorCountryColouredMap.Size = New System.Drawing.Size(210, 180)
+        Me.ucrSelectorCountryColouredMap.TabIndex = 1
+        '
+        'ucrReceiverCountry
+        '
+        Me.ucrReceiverCountry.frmParent = Me
+        Me.ucrReceiverCountry.Location = New System.Drawing.Point(238, 60)
+        Me.ucrReceiverCountry.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverCountry.Name = "ucrReceiverCountry"
+        Me.ucrReceiverCountry.Selector = Nothing
+        Me.ucrReceiverCountry.Size = New System.Drawing.Size(120, 20)
+        Me.ucrReceiverCountry.strNcFilePath = ""
+        Me.ucrReceiverCountry.TabIndex = 2
+        Me.ucrReceiverCountry.ucrSelector = Nothing
+        '
+        'ucrReceiverColourBy
+        '
+        Me.ucrReceiverColourBy.frmParent = Me
+        Me.ucrReceiverColourBy.Location = New System.Drawing.Point(238, 108)
+        Me.ucrReceiverColourBy.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverColourBy.Name = "ucrReceiverColourBy"
+        Me.ucrReceiverColourBy.Selector = Nothing
+        Me.ucrReceiverColourBy.Size = New System.Drawing.Size(120, 20)
+        Me.ucrReceiverColourBy.strNcFilePath = ""
+        Me.ucrReceiverColourBy.TabIndex = 3
+        Me.ucrReceiverColourBy.ucrSelector = Nothing
+        '
+        'lblCountry
+        '
+        Me.lblCountry.AutoSize = True
+        Me.lblCountry.Location = New System.Drawing.Point(235, 44)
+        Me.lblCountry.Name = "lblCountry"
+        Me.lblCountry.Size = New System.Drawing.Size(43, 13)
+        Me.lblCountry.TabIndex = 4
+        Me.lblCountry.Text = "Country"
+        '
+        'lblColourBy
+        '
+        Me.lblColourBy.AutoSize = True
+        Me.lblColourBy.Location = New System.Drawing.Point(235, 95)
+        Me.lblColourBy.Name = "lblColourBy"
+        Me.lblColourBy.Size = New System.Drawing.Size(55, 13)
+        Me.lblColourBy.TabIndex = 5
+        Me.lblColourBy.Text = "Colour By:"
+        '
+        'ucrSaveMap
+        '
+        Me.ucrSaveMap.Location = New System.Drawing.Point(10, 209)
+        Me.ucrSaveMap.Name = "ucrSaveMap"
+        Me.ucrSaveMap.Size = New System.Drawing.Size(199, 24)
+        Me.ucrSaveMap.TabIndex = 6
         '
         'dlgCountryColouredMap
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(402, 262)
+        Me.ClientSize = New System.Drawing.Size(430, 300)
+        Me.Controls.Add(Me.ucrSaveMap)
+        Me.Controls.Add(Me.lblColourBy)
+        Me.Controls.Add(Me.lblCountry)
+        Me.Controls.Add(Me.ucrReceiverColourBy)
+        Me.Controls.Add(Me.ucrReceiverCountry)
+        Me.Controls.Add(Me.ucrSelectorCountryColouredMap)
         Me.Controls.Add(Me.ucrBase)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.MaximizeBox = False
@@ -45,8 +116,15 @@ Partial Class dlgCountryColouredMap
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Country Coloured Map"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents ucrBase As ucrButtons
+    Friend WithEvents ucrSelectorCountryColouredMap As ucrSelectorByDataFrameAddRemove
+    Friend WithEvents ucrReceiverCountry As ucrReceiverSingle
+    Friend WithEvents ucrReceiverColourBy As ucrReceiverSingle
+    Friend WithEvents lblColourBy As Label
+    Friend WithEvents lblCountry As Label
+    Friend WithEvents ucrSaveMap As ucrSave
 End Class
--- instat/dlgCountryColouredMap.vb
+++ instat/dlgCountryColouredMap.vb
@@ -1,3 +1,135 @@
-﻿Public Class dlgCountryColouredMap
+﻿' R- Instat
+' Copyright (C) 2015-2017
+'
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+'
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License 
+' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+Imports instat.Translations
+Public Class dlgCountryColouredMap
+    Private bFirstLoad As Boolean = True
+    Private bReset As Boolean = True
+    Private clsMapdataFunction, clsRightjoinfunction, clsconcatenateFunction As New RFunction
+    Private clsBaseOperator As New ROperator
+    Private clsGgplot As New RFunction
+    Private clsRGeomPolygon As New RFunction
+    Private clsRaesFunc As New RFunction
+    Private clsCoordMap As New RFunction
+
+    Private Sub dlgCountryColouredMap_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If bFirstLoad Then
+            InitialiseDialog()
+            bFirstLoad = False
+        End If
+
+        If bReset Then
+            SetDefaults()
+        End If
+        SetRCodeForControls(bReset)
+        bReset = False
+        autoTranslate(Me)
+        TestOKEnabled()
+    End Sub
+
+    Private Sub InitialiseDialog()
+        ucrReceiverCountry.Selector = ucrSelectorCountryColouredMap
+        ucrReceiverColourBy.Selector = ucrSelectorCountryColouredMap
+
+        ucrSelectorCountryColouredMap.SetParameter(New RParameter("", 1))
+        ucrSelectorCountryColouredMap.SetParameterIsrfunction()
+
+        ucrReceiverCountry.SetParameter(New RParameter("region", 2))
+        ucrReceiverCountry.SetParameterIsString()
+
+        ucrReceiverColourBy.SetParameter(New RParameter("group", 3))
+        ucrReceiverCountry.SetParameterIsString()
+
+        ucrSaveMap.SetPrefix("Map")
+        ucrSaveMap.SetSaveTypeAsGraph()
+        ucrSaveMap.SetIsComboBox()
+        ucrSaveMap.SetCheckBoxText("Save Graph")
+        ucrSaveMap.SetAssignToIfUncheckedValue("last_graph")
+        ucrSaveMap.SetDataFrameSelector(ucrSelectorCountryColouredMap.ucrAvailableDataFrames)
+    End Sub
+
+    Private Sub SetDefaults()
+        clsMapdataFunction = New RFunction
+        clsRightjoinfunction = New RFunction
+        clsconcatenateFunction = New RFunction
+
+        clsBaseOperator = New ROperator
+        clsGgplot = New RFunction
+        clsRGeomPolygon = New RFunction
+        clsCoordMap = New RFunction
+        clsRaesFunc = New RFunction
+
+        ucrSelectorCountryColouredMap.Reset()
+        ucrReceiverCountry.SetMeAsReceiver()
+        clsMapdataFunction.SetPackageName("ggplot2")
+        clsMapdataFunction.SetRCommand("map_data")
+        clsMapdataFunction.SetAssignTo("world")
+        clsMapdataFunction.AddParameter("map", strParameterValue:=Chr(34) & "world" & Chr(34))
+
+        clsRightjoinfunction.SetPackageName("dplyr")
+        clsRightjoinfunction.SetRCommand("right_join")
+        clsRightjoinfunction.SetAssignTo("merged_country")
+        clsRightjoinfunction.AddParameter("x", clsRFunctionParameter:=clsMapdataFunction, bIncludeArgumentName:=False)
+        clsRightjoinfunction.AddParameter("by", clsRFunctionParameter:=clsconcatenateFunction)
+        clsconcatenateFunction.SetRCommand("c")
+
+
+        clsBaseOperator.SetOperation("+")
+        clsGgplot.SetPackageName("ggplot2")
+        clsGgplot.SetRCommand("ggplot")
+        clsBaseOperator.AddParameter("ggplot", clsRFunctionParameter:=clsGgplot, iPosition:=0)
+
+        clsRGeomPolygon.SetPackageName("ggplot2")
+        clsRGeomPolygon.SetRCommand("geom_polygon")
+        clsRGeomPolygon.AddParameter("data", clsRFunctionParameter:=clsRightjoinfunction, iPosition:=0)
+
+        clsRaesFunc.SetPackageName("ggplot2")
+        clsRaesFunc.SetRCommand("aes")
+        clsRGeomPolygon.AddParameter("aes", clsRFunctionParameter:=clsRaesFunc, iPosition:=1, bIncludeArgumentName:=False)
+        clsRaesFunc.AddParameter("x", "long", iPosition:=0)
+        clsRaesFunc.AddParameter("y", "lat", iPosition:=1)
+        clsRaesFunc.AddParameter("fill", "count")
+        clsBaseOperator.AddParameter("polygon", clsRFunctionParameter:=clsRGeomPolygon, iPosition:=1)
+
+        clsCoordMap.SetPackageName("ggplot2")
+        clsCoordMap.SetRCommand("coord_map")
+        clsBaseOperator.AddParameter("coordmap", clsRFunctionParameter:=clsCoordMap, iPosition:=2)
+
+
+        ucrBase.clsRsyntax.AddToBeforeCodes(clsMapdataFunction)
+        ucrBase.clsRsyntax.AddToBeforeCodes(clsRightjoinfunction)
+        clsBaseOperator.SetAssignTo("last_graph", strTempDataframe:=ucrSelectorCountryColouredMap.ucrAvailableDataFrames.cboAvailableDataFrames.Text, strTempGraph:="last_graph")
+        ucrBase.clsRsyntax.SetBaseROperator(clsBaseOperator)
+    End Sub
+
+    Private Sub SetRCodeForControls(bReset As Boolean)
+        ucrSelectorCountryColouredMap.SetRCode(clsMapdataFunction, bReset)
+        ucrReceiverCountry.SetRCode(clsconcatenateFunction, bReset)
+        ucrReceiverColourBy.SetRCode(clsRaesFunc, bReset)
+        ucrSaveMap.SetRCode(clsBaseOperator, bReset)
+
+    End Sub
+
+    Private Sub TestOKEnabled()
+
+    End Sub
+
+    Private Sub ucrBase_ClickReset(sender As Object, e As EventArgs) Handles ucrBase.ClickReset
+        SetDefaults()
+        SetRCodeForControls(True)
+        TestOKEnabled()
+    End Sub
 End Class
