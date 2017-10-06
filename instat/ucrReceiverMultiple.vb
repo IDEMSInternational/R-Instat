dacd7f066a6e8f65c8f00a5c33fb6c97218a0f89
maxwell fundi <maxwellfundi@live.com>
2017-10-06 10:35:30 +0300

    Adding bFixReceiver to receivers

--- instat/ucrReceiverMultiple.vb
+++ instat/ucrReceiverMultiple.vb
@@ -328,7 +328,7 @@ Public Class ucrReceiverMultiple
         OnSelectionChanged()
     End Sub
 
-    Public Overrides Sub Add(strItem As String, Optional strDataFrame As String = "")
+    Public Overrides Sub Add(strItem As String, Optional strDataFrame As String = "", Optional bFixreceiver As Boolean = False)
         Dim kvpItems(0) As KeyValuePair(Of String, String)
         If strDataFrame = "" Then
             For i = 0 To Selector.lstAvailableVariable.Items.Count - 1
@@ -339,6 +339,8 @@ Public Class ucrReceiverMultiple
         End If
         kvpItems(0) = New KeyValuePair(Of String, String)(strDataFrame, strItem)
         AddMultiple(kvpItems)
+
+        lstSelectedVariables.Enabled = Not bFixreceiver
     End Sub
 
     Public Sub AddItemsWithMetadataProperty(strCurrentDataFrame As String, strProperty As String, strValues As String())
