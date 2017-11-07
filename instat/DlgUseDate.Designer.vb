401097a79eaa0ae7ea3bede391ff3d92ab848ea0
shadrack kibet <shadrackkibet@outlook.com>
2017-11-07 15:17:49 +0300

    localisable=True

--- instat/DlgUseDate.Designer.vb
+++ instat/DlgUseDate.Designer.vb
@@ -38,6 +38,7 @@ Partial Class dlgUseDate
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(dlgUseDate))
         Me.lblDateVariable = New System.Windows.Forms.Label()
         Me.grpShifted = New System.Windows.Forms.GroupBox()
         Me.lblMonth = New System.Windows.Forms.Label()
@@ -76,12 +77,8 @@ Partial Class dlgUseDate
         '
         'lblDateVariable
         '
-        Me.lblDateVariable.AutoSize = True
-        Me.lblDateVariable.Location = New System.Drawing.Point(254, 44)
+        resources.ApplyResources(Me.lblDateVariable, "lblDateVariable")
         Me.lblDateVariable.Name = "lblDateVariable"
-        Me.lblDateVariable.Size = New System.Drawing.Size(71, 13)
-        Me.lblDateVariable.TabIndex = 1
-        Me.lblDateVariable.Text = "Date Column:"
         '
         'grpShifted
         '
@@ -91,157 +88,115 @@ Partial Class dlgUseDate
         Me.grpShifted.Controls.Add(Me.ucrChkShiftYear)
         Me.grpShifted.Controls.Add(Me.ucrNudShiftStartDay)
         Me.grpShifted.Controls.Add(Me.ucrChkShiftDay)
-        Me.grpShifted.Location = New System.Drawing.Point(236, 201)
+        resources.ApplyResources(Me.grpShifted, "grpShifted")
         Me.grpShifted.Name = "grpShifted"
-        Me.grpShifted.Size = New System.Drawing.Size(143, 125)
-        Me.grpShifted.TabIndex = 4
         Me.grpShifted.TabStop = False
-        Me.grpShifted.Text = "Shifted"
         '
         'lblMonth
         '
-        Me.lblMonth.AutoSize = True
-        Me.lblMonth.Location = New System.Drawing.Point(10, 73)
+        resources.ApplyResources(Me.lblMonth, "lblMonth")
         Me.lblMonth.Name = "lblMonth"
-        Me.lblMonth.Size = New System.Drawing.Size(40, 13)
-        Me.lblMonth.TabIndex = 2
-        Me.lblMonth.Text = "Month:"
         '
         'ucrInputComboBoxMonth
         '
         Me.ucrInputComboBoxMonth.AddQuotesIfUnrecognised = True
         Me.ucrInputComboBoxMonth.IsReadOnly = False
-        Me.ucrInputComboBoxMonth.Location = New System.Drawing.Point(56, 70)
+        resources.ApplyResources(Me.ucrInputComboBoxMonth, "ucrInputComboBoxMonth")
         Me.ucrInputComboBoxMonth.Name = "ucrInputComboBoxMonth"
-        Me.ucrInputComboBoxMonth.Size = New System.Drawing.Size(81, 21)
-        Me.ucrInputComboBoxMonth.TabIndex = 3
         '
         'lblDay
         '
-        Me.lblDay.AutoSize = True
-        Me.lblDay.Location = New System.Drawing.Point(10, 98)
+        resources.ApplyResources(Me.lblDay, "lblDay")
         Me.lblDay.Name = "lblDay"
-        Me.lblDay.Size = New System.Drawing.Size(29, 13)
-        Me.lblDay.TabIndex = 4
-        Me.lblDay.Text = "Day:"
         '
         'ucrChkShiftYear
         '
         Me.ucrChkShiftYear.Checked = False
-        Me.ucrChkShiftYear.Location = New System.Drawing.Point(13, 46)
+        resources.ApplyResources(Me.ucrChkShiftYear, "ucrChkShiftYear")
         Me.ucrChkShiftYear.Name = "ucrChkShiftYear"
-        Me.ucrChkShiftYear.Size = New System.Drawing.Size(100, 20)
-        Me.ucrChkShiftYear.TabIndex = 1
         '
         'ucrNudShiftStartDay
         '
         Me.ucrNudShiftStartDay.DecimalPlaces = New Decimal(New Integer() {0, 0, 0, 0})
         Me.ucrNudShiftStartDay.Increment = New Decimal(New Integer() {1, 0, 0, 0})
-        Me.ucrNudShiftStartDay.Location = New System.Drawing.Point(57, 95)
+        resources.ApplyResources(Me.ucrNudShiftStartDay, "ucrNudShiftStartDay")
         Me.ucrNudShiftStartDay.Maximum = New Decimal(New Integer() {100, 0, 0, 0})
         Me.ucrNudShiftStartDay.Minimum = New Decimal(New Integer() {0, 0, 0, 0})
         Me.ucrNudShiftStartDay.Name = "ucrNudShiftStartDay"
-        Me.ucrNudShiftStartDay.Size = New System.Drawing.Size(50, 20)
-        Me.ucrNudShiftStartDay.TabIndex = 5
         Me.ucrNudShiftStartDay.Value = New Decimal(New Integer() {0, 0, 0, 0})
         '
         'ucrChkShiftDay
         '
         Me.ucrChkShiftDay.Checked = False
-        Me.ucrChkShiftDay.Location = New System.Drawing.Point(13, 19)
+        resources.ApplyResources(Me.ucrChkShiftDay, "ucrChkShiftDay")
         Me.ucrChkShiftDay.Name = "ucrChkShiftDay"
-        Me.ucrChkShiftDay.Size = New System.Drawing.Size(94, 20)
-        Me.ucrChkShiftDay.TabIndex = 0
         '
         'grpOthers
         '
         Me.grpOthers.Controls.Add(Me.ucrChkLeapYear)
         Me.grpOthers.Controls.Add(Me.ucrChkDekad)
         Me.grpOthers.Controls.Add(Me.ucrChkPentad)
-        Me.grpOthers.Location = New System.Drawing.Point(10, 330)
+        resources.ApplyResources(Me.grpOthers, "grpOthers")
         Me.grpOthers.Name = "grpOthers"
-        Me.grpOthers.Size = New System.Drawing.Size(369, 48)
-        Me.grpOthers.TabIndex = 7
         Me.grpOthers.TabStop = False
-        Me.grpOthers.Text = "Other Functions"
         '
         'ucrChkLeapYear
         '
         Me.ucrChkLeapYear.Checked = False
-        Me.ucrChkLeapYear.Location = New System.Drawing.Point(6, 19)
+        resources.ApplyResources(Me.ucrChkLeapYear, "ucrChkLeapYear")
         Me.ucrChkLeapYear.Name = "ucrChkLeapYear"
-        Me.ucrChkLeapYear.Size = New System.Drawing.Size(100, 20)
-        Me.ucrChkLeapYear.TabIndex = 0
         '
         'ucrChkDekad
         '
         Me.ucrChkDekad.Checked = False
-        Me.ucrChkDekad.Location = New System.Drawing.Point(221, 19)
+        resources.ApplyResources(Me.ucrChkDekad, "ucrChkDekad")
         Me.ucrChkDekad.Name = "ucrChkDekad"
-        Me.ucrChkDekad.Size = New System.Drawing.Size(100, 20)
-        Me.ucrChkDekad.TabIndex = 2
         '
         'ucrChkPentad
         '
         Me.ucrChkPentad.Checked = False
-        Me.ucrChkPentad.Location = New System.Drawing.Point(112, 19)
+        resources.ApplyResources(Me.ucrChkPentad, "ucrChkPentad")
         Me.ucrChkPentad.Name = "ucrChkPentad"
-        Me.ucrChkPentad.Size = New System.Drawing.Size(100, 20)
-        Me.ucrChkPentad.TabIndex = 1
         '
         'grpFullName
         '
         Me.grpFullName.Controls.Add(Me.ucrChkFullWeekday)
         Me.grpFullName.Controls.Add(Me.ucrChkFullMonth)
-        Me.grpFullName.Location = New System.Drawing.Point(385, 201)
+        resources.ApplyResources(Me.grpFullName, "grpFullName")
         Me.grpFullName.Name = "grpFullName"
-        Me.grpFullName.Size = New System.Drawing.Size(106, 89)
-        Me.grpFullName.TabIndex = 5
         Me.grpFullName.TabStop = False
-        Me.grpFullName.Text = "Full Name"
         '
         'ucrChkFullWeekday
         '
         Me.ucrChkFullWeekday.Checked = False
-        Me.ucrChkFullWeekday.Location = New System.Drawing.Point(6, 17)
+        resources.ApplyResources(Me.ucrChkFullWeekday, "ucrChkFullWeekday")
         Me.ucrChkFullWeekday.Name = "ucrChkFullWeekday"
-        Me.ucrChkFullWeekday.Size = New System.Drawing.Size(90, 20)
-        Me.ucrChkFullWeekday.TabIndex = 0
         '
         'ucrChkFullMonth
         '
         Me.ucrChkFullMonth.Checked = False
-        Me.ucrChkFullMonth.Location = New System.Drawing.Point(6, 43)
+        resources.ApplyResources(Me.ucrChkFullMonth, "ucrChkFullMonth")
         Me.ucrChkFullMonth.Name = "ucrChkFullMonth"
-        Me.ucrChkFullMonth.Size = New System.Drawing.Size(90, 20)
-        Me.ucrChkFullMonth.TabIndex = 1
         '
         'grpAbbreviation
         '
         Me.grpAbbreviation.Controls.Add(Me.ucrChkAbbrMonth)
         Me.grpAbbreviation.Controls.Add(Me.ucrChkAbbrWeekday)
-        Me.grpAbbreviation.Location = New System.Drawing.Point(386, 290)
+        resources.ApplyResources(Me.grpAbbreviation, "grpAbbreviation")
         Me.grpAbbreviation.Name = "grpAbbreviation"
-        Me.grpAbbreviation.Size = New System.Drawing.Size(105, 88)
-        Me.grpAbbreviation.TabIndex = 6
         Me.grpAbbreviation.TabStop = False
-        Me.grpAbbreviation.Text = "Abbreviation"
         '
         'ucrChkAbbrMonth
         '
         Me.ucrChkAbbrMonth.Checked = False
-        Me.ucrChkAbbrMonth.Location = New System.Drawing.Point(5, 43)
+        resources.ApplyResources(Me.ucrChkAbbrMonth, "ucrChkAbbrMonth")
         Me.ucrChkAbbrMonth.Name = "ucrChkAbbrMonth"
-        Me.ucrChkAbbrMonth.Size = New System.Drawing.Size(94, 20)
-        Me.ucrChkAbbrMonth.TabIndex = 1
         '
         'ucrChkAbbrWeekday
         '
         Me.ucrChkAbbrWeekday.Checked = False
-        Me.ucrChkAbbrWeekday.Location = New System.Drawing.Point(5, 17)
+        resources.ApplyResources(Me.ucrChkAbbrWeekday, "ucrChkAbbrWeekday")
         Me.ucrChkAbbrWeekday.Name = "ucrChkAbbrWeekday"
-        Me.ucrChkAbbrWeekday.Size = New System.Drawing.Size(94, 20)
-        Me.ucrChkAbbrWeekday.TabIndex = 0
         '
         'grpValues
         '
@@ -252,103 +207,77 @@ Partial Class dlgUseDate
         Me.grpValues.Controls.Add(Me.ucrChkDay)
         Me.grpValues.Controls.Add(Me.ucrChkWeekday)
         Me.grpValues.Controls.Add(Me.ucrChkWeek)
-        Me.grpValues.Location = New System.Drawing.Point(10, 199)
+        resources.ApplyResources(Me.grpValues, "grpValues")
         Me.grpValues.Name = "grpValues"
-        Me.grpValues.Size = New System.Drawing.Size(220, 125)
-        Me.grpValues.TabIndex = 3
         Me.grpValues.TabStop = False
-        Me.grpValues.Text = "Values"
         '
         'ucrChkDayYear366
         '
         Me.ucrChkDayYear366.Checked = False
-        Me.ucrChkDayYear366.Location = New System.Drawing.Point(108, 69)
+        resources.ApplyResources(Me.ucrChkDayYear366, "ucrChkDayYear366")
         Me.ucrChkDayYear366.Name = "ucrChkDayYear366"
-        Me.ucrChkDayYear366.Size = New System.Drawing.Size(109, 20)
-        Me.ucrChkDayYear366.TabIndex = 6
         '
         'ucrChkYear
         '
         Me.ucrChkYear.Checked = False
-        Me.ucrChkYear.Location = New System.Drawing.Point(6, 17)
+        resources.ApplyResources(Me.ucrChkYear, "ucrChkYear")
         Me.ucrChkYear.Name = "ucrChkYear"
-        Me.ucrChkYear.Size = New System.Drawing.Size(100, 20)
-        Me.ucrChkYear.TabIndex = 0
         '
         'ucrChkDayInYear
         '
         Me.ucrChkDayInYear.Checked = False
-        Me.ucrChkDayInYear.Location = New System.Drawing.Point(108, 43)
+        resources.ApplyResources(Me.ucrChkDayInYear, "ucrChkDayInYear")
         Me.ucrChkDayInYear.Name = "ucrChkDayInYear"
-        Me.ucrChkDayInYear.Size = New System.Drawing.Size(94, 20)
-        Me.ucrChkDayInYear.TabIndex = 5
         '
         'ucrChkMonth
         '
         Me.ucrChkMonth.Checked = False
-        Me.ucrChkMonth.Location = New System.Drawing.Point(6, 43)
+        resources.ApplyResources(Me.ucrChkMonth, "ucrChkMonth")
         Me.ucrChkMonth.Name = "ucrChkMonth"
-        Me.ucrChkMonth.Size = New System.Drawing.Size(100, 20)
-        Me.ucrChkMonth.TabIndex = 1
         '
         'ucrChkDay
         '
         Me.ucrChkDay.Checked = False
-        Me.ucrChkDay.Location = New System.Drawing.Point(6, 69)
+        resources.ApplyResources(Me.ucrChkDay, "ucrChkDay")
         Me.ucrChkDay.Name = "ucrChkDay"
-        Me.ucrChkDay.Size = New System.Drawing.Size(100, 20)
-        Me.ucrChkDay.TabIndex = 2
         '
         'ucrChkWeekday
         '
         Me.ucrChkWeekday.Checked = False
-        Me.ucrChkWeekday.Location = New System.Drawing.Point(108, 17)
+        resources.ApplyResources(Me.ucrChkWeekday, "ucrChkWeekday")
         Me.ucrChkWeekday.Name = "ucrChkWeekday"
-        Me.ucrChkWeekday.Size = New System.Drawing.Size(94, 20)
-        Me.ucrChkWeekday.TabIndex = 4
         '
         'ucrChkWeek
         '
         Me.ucrChkWeek.Checked = False
-        Me.ucrChkWeek.Location = New System.Drawing.Point(6, 95)
+        resources.ApplyResources(Me.ucrChkWeek, "ucrChkWeek")
         Me.ucrChkWeek.Name = "ucrChkWeek"
-        Me.ucrChkWeek.Size = New System.Drawing.Size(100, 20)
-        Me.ucrChkWeek.TabIndex = 3
         '
         'ucrReceiverUseDate
         '
         Me.ucrReceiverUseDate.frmParent = Me
-        Me.ucrReceiverUseDate.Location = New System.Drawing.Point(254, 59)
-        Me.ucrReceiverUseDate.Margin = New System.Windows.Forms.Padding(0)
+        resources.ApplyResources(Me.ucrReceiverUseDate, "ucrReceiverUseDate")
         Me.ucrReceiverUseDate.Name = "ucrReceiverUseDate"
         Me.ucrReceiverUseDate.Selector = Nothing
-        Me.ucrReceiverUseDate.Size = New System.Drawing.Size(151, 20)
         Me.ucrReceiverUseDate.strNcFilePath = ""
-        Me.ucrReceiverUseDate.TabIndex = 2
         Me.ucrReceiverUseDate.ucrSelector = Nothing
         '
         'ucrBase
         '
-        Me.ucrBase.Location = New System.Drawing.Point(10, 384)
+        resources.ApplyResources(Me.ucrBase, "ucrBase")
         Me.ucrBase.Name = "ucrBase"
-        Me.ucrBase.Size = New System.Drawing.Size(407, 52)
-        Me.ucrBase.TabIndex = 8
         '
         'ucrSelectorUseDate
         '
         Me.ucrSelectorUseDate.bShowHiddenColumns = False
         Me.ucrSelectorUseDate.bUseCurrentFilter = True
-        Me.ucrSelectorUseDate.Location = New System.Drawing.Point(10, 10)
-        Me.ucrSelectorUseDate.Margin = New System.Windows.Forms.Padding(0)
+        resources.ApplyResources(Me.ucrSelectorUseDate, "ucrSelectorUseDate")
         Me.ucrSelectorUseDate.Name = "ucrSelectorUseDate"
-        Me.ucrSelectorUseDate.Size = New System.Drawing.Size(210, 180)
-        Me.ucrSelectorUseDate.TabIndex = 0
         '
         'dlgUseDate
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        resources.ApplyResources(Me, "$this")
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(502, 444)
         Me.Controls.Add(Me.grpOthers)
         Me.Controls.Add(Me.grpShifted)
         Me.Controls.Add(Me.ucrReceiverUseDate)
@@ -362,8 +291,6 @@ Partial Class dlgUseDate
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "dlgUseDate"
-        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "Use Date"
         Me.grpShifted.ResumeLayout(False)
         Me.grpShifted.PerformLayout()
         Me.grpOthers.ResumeLayout(False)
--- instat/dlgExtremes.Designer.vb
+++ instat/dlgExtremes.Designer.vb
@@ -38,6 +38,7 @@ Partial Class dlgExtremes
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(dlgExtremes))
         Me.ucrBase = New instat.ucrButtons()
         Me.ucrAddRemove = New instat.ucrSelectorAddRemove()
         Me.ucrReceiverDataToFit = New instat.ucrReceiverSingle()
@@ -64,73 +65,53 @@ Partial Class dlgExtremes
         '
         'ucrBase
         '
-        Me.ucrBase.Location = New System.Drawing.Point(9, 290)
+        resources.ApplyResources(Me.ucrBase, "ucrBase")
         Me.ucrBase.Name = "ucrBase"
-        Me.ucrBase.Size = New System.Drawing.Size(405, 51)
-        Me.ucrBase.TabIndex = 0
         '
         'ucrAddRemove
         '
         Me.ucrAddRemove.bShowHiddenColumns = False
-        Me.ucrAddRemove.Location = New System.Drawing.Point(9, 24)
-        Me.ucrAddRemove.Margin = New System.Windows.Forms.Padding(0)
+        resources.ApplyResources(Me.ucrAddRemove, "ucrAddRemove")
         Me.ucrAddRemove.Name = "ucrAddRemove"
-        Me.ucrAddRemove.Size = New System.Drawing.Size(206, 127)
-        Me.ucrAddRemove.TabIndex = 1
         '
         'ucrReceiverDataToFit
         '
-        Me.ucrReceiverDataToFit.Location = New System.Drawing.Point(224, 39)
-        Me.ucrReceiverDataToFit.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverDataToFit.frmParent = Me
+        resources.ApplyResources(Me.ucrReceiverDataToFit, "ucrReceiverDataToFit")
         Me.ucrReceiverDataToFit.Name = "ucrReceiverDataToFit"
         Me.ucrReceiverDataToFit.Selector = Nothing
-        Me.ucrReceiverDataToFit.Size = New System.Drawing.Size(106, 26)
-        Me.ucrReceiverDataToFit.TabIndex = 2
+        Me.ucrReceiverDataToFit.strNcFilePath = ""
+        Me.ucrReceiverDataToFit.ucrSelector = Nothing
         '
         'lblDataToFit
         '
-        Me.lblDataToFit.AutoSize = True
-        Me.lblDataToFit.Location = New System.Drawing.Point(224, 24)
+        resources.ApplyResources(Me.lblDataToFit, "lblDataToFit")
         Me.lblDataToFit.Name = "lblDataToFit"
-        Me.lblDataToFit.Size = New System.Drawing.Size(89, 13)
-        Me.lblDataToFit.TabIndex = 3
         Me.lblDataToFit.Tag = "Data_to_be_fitted"
-        Me.lblDataToFit.Text = "Data to be Fitted:"
         '
         'grpDistributions
         '
         Me.grpDistributions.Controls.Add(Me.rdoGEV)
         Me.grpDistributions.Controls.Add(Me.rdoGumbel)
-        Me.grpDistributions.Location = New System.Drawing.Point(267, 194)
+        resources.ApplyResources(Me.grpDistributions, "grpDistributions")
         Me.grpDistributions.Name = "grpDistributions"
-        Me.grpDistributions.Size = New System.Drawing.Size(100, 91)
-        Me.grpDistributions.TabIndex = 4
         Me.grpDistributions.TabStop = False
         Me.grpDistributions.Tag = "Distributions"
-        Me.grpDistributions.Text = "Distributions"
         '
         'rdoGEV
         '
-        Me.rdoGEV.AutoSize = True
-        Me.rdoGEV.Location = New System.Drawing.Point(6, 48)
+        resources.ApplyResources(Me.rdoGEV, "rdoGEV")
         Me.rdoGEV.Name = "rdoGEV"
-        Me.rdoGEV.Size = New System.Drawing.Size(47, 17)
-        Me.rdoGEV.TabIndex = 1
         Me.rdoGEV.TabStop = True
         Me.rdoGEV.Tag = "GEV"
-        Me.rdoGEV.Text = "GEV"
         Me.rdoGEV.UseVisualStyleBackColor = True
         '
         'rdoGumbel
         '
-        Me.rdoGumbel.AutoSize = True
-        Me.rdoGumbel.Location = New System.Drawing.Point(6, 25)
+        resources.ApplyResources(Me.rdoGumbel, "rdoGumbel")
         Me.rdoGumbel.Name = "rdoGumbel"
-        Me.rdoGumbel.Size = New System.Drawing.Size(61, 17)
-        Me.rdoGumbel.TabIndex = 0
         Me.rdoGumbel.TabStop = True
         Me.rdoGumbel.Tag = "Gumbel"
-        Me.rdoGumbel.Text = "Gumbel"
         Me.rdoGumbel.UseVisualStyleBackColor = True
         '
         'grpEstimationMethods
@@ -140,132 +121,89 @@ Partial Class dlgExtremes
         Me.grpEstimationMethods.Controls.Add(Me.lblSaveEstimates)
         Me.grpEstimationMethods.Controls.Add(Me.chkMethodOfMoments)
         Me.grpEstimationMethods.Controls.Add(Me.chkMaximumLikelihood)
-        Me.grpEstimationMethods.Location = New System.Drawing.Point(9, 193)
+        resources.ApplyResources(Me.grpEstimationMethods, "grpEstimationMethods")
         Me.grpEstimationMethods.Name = "grpEstimationMethods"
-        Me.grpEstimationMethods.Size = New System.Drawing.Size(241, 91)
-        Me.grpEstimationMethods.TabIndex = 6
         Me.grpEstimationMethods.TabStop = False
         Me.grpEstimationMethods.Tag = "Estimation_methods"
-        Me.grpEstimationMethods.Text = "Estimation methods"
         '
         'cboMethodOfMoments
         '
         Me.cboMethodOfMoments.FormattingEnabled = True
-        Me.cboMethodOfMoments.Location = New System.Drawing.Point(147, 59)
+        resources.ApplyResources(Me.cboMethodOfMoments, "cboMethodOfMoments")
         Me.cboMethodOfMoments.Name = "cboMethodOfMoments"
-        Me.cboMethodOfMoments.Size = New System.Drawing.Size(84, 21)
-        Me.cboMethodOfMoments.TabIndex = 4
         '
         'cboMaximumLikelihood
         '
         Me.cboMaximumLikelihood.FormattingEnabled = True
-        Me.cboMaximumLikelihood.Location = New System.Drawing.Point(147, 32)
+        resources.ApplyResources(Me.cboMaximumLikelihood, "cboMaximumLikelihood")
         Me.cboMaximumLikelihood.Name = "cboMaximumLikelihood"
-        Me.cboMaximumLikelihood.Size = New System.Drawing.Size(84, 21)
-        Me.cboMaximumLikelihood.TabIndex = 3
         '
         'lblSaveEstimates
         '
-        Me.lblSaveEstimates.AutoSize = True
-        Me.lblSaveEstimates.Location = New System.Drawing.Point(143, 16)
+        resources.ApplyResources(Me.lblSaveEstimates, "lblSaveEstimates")
         Me.lblSaveEstimates.Name = "lblSaveEstimates"
-        Me.lblSaveEstimates.Size = New System.Drawing.Size(94, 13)
-        Me.lblSaveEstimates.TabIndex = 2
         Me.lblSaveEstimates.Tag = "Save_estimates_in"
-        Me.lblSaveEstimates.Text = "Save Estimates in:"
         '
         'chkMethodOfMoments
         '
-        Me.chkMethodOfMoments.AutoSize = True
-        Me.chkMethodOfMoments.Location = New System.Drawing.Point(6, 62)
+        resources.ApplyResources(Me.chkMethodOfMoments, "chkMethodOfMoments")
         Me.chkMethodOfMoments.Name = "chkMethodOfMoments"
-        Me.chkMethodOfMoments.Size = New System.Drawing.Size(120, 17)
-        Me.chkMethodOfMoments.TabIndex = 1
         Me.chkMethodOfMoments.Tag = "Method_of_moments"
-        Me.chkMethodOfMoments.Text = "Method of Moments"
         Me.chkMethodOfMoments.UseVisualStyleBackColor = True
         '
         'chkMaximumLikelihood
         '
-        Me.chkMaximumLikelihood.AutoSize = True
-        Me.chkMaximumLikelihood.Location = New System.Drawing.Point(6, 34)
+        resources.ApplyResources(Me.chkMaximumLikelihood, "chkMaximumLikelihood")
         Me.chkMaximumLikelihood.Name = "chkMaximumLikelihood"
-        Me.chkMaximumLikelihood.Size = New System.Drawing.Size(121, 17)
-        Me.chkMaximumLikelihood.TabIndex = 0
         Me.chkMaximumLikelihood.Tag = "Maximum_likelihood_"
-        Me.chkMaximumLikelihood.Text = "Maximum Likelihood"
         Me.chkMaximumLikelihood.UseVisualStyleBackColor = True
         '
         'chkRestrictValues
         '
-        Me.chkRestrictValues.AutoSize = True
-        Me.chkRestrictValues.Location = New System.Drawing.Point(224, 77)
+        resources.ApplyResources(Me.chkRestrictValues, "chkRestrictValues")
         Me.chkRestrictValues.Name = "chkRestrictValues"
-        Me.chkRestrictValues.Size = New System.Drawing.Size(123, 17)
-        Me.chkRestrictValues.TabIndex = 7
         Me.chkRestrictValues.Tag = "Restrict_data_values"
-        Me.chkRestrictValues.Text = "Restrict Data Values"
         Me.chkRestrictValues.UseVisualStyleBackColor = True
         '
         'lblBetween
         '
-        Me.lblBetween.AutoSize = True
-        Me.lblBetween.Location = New System.Drawing.Point(232, 101)
+        resources.ApplyResources(Me.lblBetween, "lblBetween")
         Me.lblBetween.Name = "lblBetween"
-        Me.lblBetween.Size = New System.Drawing.Size(30, 13)
-        Me.lblBetween.TabIndex = 8
         Me.lblBetween.Tag = "between"
-        Me.lblBetween.Text = "From"
-        Me.lblBetween.TextAlign = System.Drawing.ContentAlignment.TopRight
         '
         'txtBetween
         '
-        Me.txtBetween.Location = New System.Drawing.Point(267, 97)
+        resources.ApplyResources(Me.txtBetween, "txtBetween")
         Me.txtBetween.Name = "txtBetween"
-        Me.txtBetween.Size = New System.Drawing.Size(33, 20)
-        Me.txtBetween.TabIndex = 10
         '
         'lblAnd
         '
-        Me.lblAnd.AutoSize = True
-        Me.lblAnd.Location = New System.Drawing.Point(234, 124)
+        resources.ApplyResources(Me.lblAnd, "lblAnd")
         Me.lblAnd.Name = "lblAnd"
-        Me.lblAnd.Size = New System.Drawing.Size(20, 13)
-        Me.lblAnd.TabIndex = 11
         Me.lblAnd.Tag = "and"
-        Me.lblAnd.Text = "To"
         '
         'txtAnd
         '
-        Me.txtAnd.Location = New System.Drawing.Point(267, 123)
+        resources.ApplyResources(Me.txtAnd, "txtAnd")
         Me.txtAnd.Name = "txtAnd"
-        Me.txtAnd.Size = New System.Drawing.Size(33, 20)
-        Me.txtAnd.TabIndex = 12
         '
         'chkProbabilityPlot
         '
-        Me.chkProbabilityPlot.AutoSize = True
-        Me.chkProbabilityPlot.Location = New System.Drawing.Point(9, 168)
+        resources.ApplyResources(Me.chkProbabilityPlot, "chkProbabilityPlot")
         Me.chkProbabilityPlot.Name = "chkProbabilityPlot"
-        Me.chkProbabilityPlot.Size = New System.Drawing.Size(94, 17)
-        Me.chkProbabilityPlot.TabIndex = 13
         Me.chkProbabilityPlot.Tag = "Probability_plot"
-        Me.chkProbabilityPlot.Text = "Probability plot"
         Me.chkProbabilityPlot.UseVisualStyleBackColor = True
         '
         'UcrDialogDisabled1
         '
         Me.UcrDialogDisabled1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.UcrDialogDisabled1.Location = New System.Drawing.Point(80, 140)
+        resources.ApplyResources(Me.UcrDialogDisabled1, "UcrDialogDisabled1")
         Me.UcrDialogDisabled1.Name = "UcrDialogDisabled1"
-        Me.UcrDialogDisabled1.Size = New System.Drawing.Size(240, 50)
-        Me.UcrDialogDisabled1.TabIndex = 14
         '
         'dlgExtremes
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        resources.ApplyResources(Me, "$this")
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(416, 353)
         Me.Controls.Add(Me.UcrDialogDisabled1)
         Me.Controls.Add(Me.chkProbabilityPlot)
         Me.Controls.Add(Me.txtAnd)
@@ -283,9 +221,7 @@ Partial Class dlgExtremes
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "dlgExtremes"
-        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Tag = "Fit_an_Extreme_distribution"
-        Me.Text = "Fit an Extreme Distribution"
         Me.grpDistributions.ResumeLayout(False)
         Me.grpDistributions.PerformLayout()
         Me.grpEstimationMethods.ResumeLayout(False)
