a69d6a7b16676f344b768598990e90672be88aba
maxwell fundi <maxwellfundi@live.com>
2017-11-09 11:57:22 +0300

    Adding positions

--- instat/ucrGeom.vb
+++ instat/ucrGeom.vb
@@ -1026,8 +1026,9 @@ Public Class ucrGeom
         clsgeom_smooth.AddAesParameter("weight")
 
         'add  layer parameter
-        'clsgeom_smooth.AddLayerParameter("position") ****Not sure how many positions exist for geom_smooth
-
+        ''****Not sure how many positions exist for geom_smooth
+        clsgeom_smooth.AddLayerParameter("position", "list", Chr(34) & "identity" & Chr(34), lstParameterStrings:={Chr(34) & "identity" & Chr(34), Chr(34) & "jitter" & Chr(34)})
+        'Can we have  stack, DoDragDrop, fill positions here)
         clsgeom_smooth.AddLayerParameter("method", "list", Chr(34) & "lm" & Chr(34), lstParameterStrings:={Chr(34) & "lm" & Chr(34), Chr(34) & "glm" & Chr(34), Chr(34) & "gam" & Chr(34), Chr(34) & "loess" & Chr(34), Chr(34) & "rlm" & Chr(34)})
         'formula has to be an input and we dont have that currently. its passed in like this formula= y ~ x or  formula= y ~ poly(x, 2) or formula= y ~ log(x) so the user has to type in stuff
         'clsgeom_smooth.AddLayerParameter("formula",)
@@ -1047,6 +1048,7 @@ Public Class ucrGeom
         'method.args is a list of methods passed into the moddeling function
         'its passed in like this** method.args = list(family = "binomial") we currently dont have this too. 
         'clsgeom_smooth.AddLayerParameter("method.args")
+
         lstAllGeoms.Add(clsgeom_smooth)
 
         'clsgeom_spoke.strGeomName = "geom_spoke"
