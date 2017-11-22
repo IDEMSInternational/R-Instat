2cd592eb4813f5e50bfda473c8da42a7ae5f5b4b
patowhiz <conlooptechnologies@gmail.com>
2017-11-22 03:42:59 +0300

    dlgBackupManager

---
+++ instat/dlgBackupManager.vb
@@ -0,0 +1,219 @@
+﻿' R- Instat
+' Copyright (C) 2015-2017
+'
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+'
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License 
+' along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Imports instat.Translations
+Imports System.IO
+Public Class dlgBackupManager
+    Private strAutoSaveDataFolderPath As String 'holds the folder path for data files. Set by the parent form
+    Private strAutoSavedDataFilePaths As List(Of String) 'holds the file paths including the file name
+    Private strSelectedDataFilePath As String 'holds the selected file path
+
+    Private Sub dlgBackupManager_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'set selected data file path to empty string
+        strSelectedDataFilePath = ""
+        loadFilesInfo()
+        testOk()
+        autoTranslate(Me)
+
+    End Sub
+
+    'get the folder path from the default declared data save folder path in frmMain
+    'this should be set by the parent, in this case frmMain
+    Public Property AutoSaveDataFolderPath() As String
+        Get
+            Return strAutoSaveDataFolderPath
+        End Get
+        Set(value As String)
+            strAutoSaveDataFolderPath = value
+        End Set
+    End Property
+
+    'this is accessed by frmMain. To get the selected data file name & path to open
+    Public ReadOnly Property SelectedDataFilePath() As String
+        Get
+            Return strSelectedDataFilePath
+        End Get
+    End Property
+
+    'loads the files info's into the listview
+    Private Sub loadFilesInfo()
+        'clear any previous items
+        ucrLstViewDataBackups.Items.Clear()
+
+        'countercheck if the folder really exists then get all the file paths inside the folder
+        If Directory.Exists(strAutoSaveDataFolderPath) Then
+            strAutoSavedDataFilePaths = My.Computer.FileSystem.GetFiles(frmMain.strAutoSaveDataFolderPath).ToList
+        End If
+
+        'if there are files then loop thro to get their info
+        If strAutoSavedDataFilePaths IsNot Nothing AndAlso strAutoSavedDataFilePaths.Count > 0 Then
+            Dim autoSavedFileInfo As FileInfo
+            'Iterate through the path list add the file infos to the listview
+            For Each filePath As String In strAutoSavedDataFilePaths
+                'create a file info object from the file path, to get file information
+                autoSavedFileInfo = New FileInfo(filePath)
+                ' Create the listviewitem and initialise it with values for the 3 columns add it to the listview 
+                With autoSavedFileInfo
+                    ucrLstViewDataBackups.Items.Add(New ListViewItem(New String() { .Name, .LastWriteTime.ToString, getConvertedFileSize(.Length)}))
+                End With
+            Next
+        End If
+
+    End Sub
+
+    Private Sub ucrLstViewDataBackups_Click(sender As Object, e As EventArgs) Handles ucrLstViewDataBackups.Click
+        testOk()
+    End Sub
+
+    Private Sub cmdOpen_Click(sender As Object, e As EventArgs) Handles cmdOpen.Click
+
+        If (Not testOk()) Then
+            MsgBox("Select the file to open")
+            Return
+        End If
+
+        If MsgBox("Are you sure you want to open this data file?" & Environment.NewLine & "This will replace the current data.", MessageBoxButtons.YesNo, "Back up Manager") = MsgBoxResult.No Then
+            Return
+        End If
+
+        If (ucrLstViewDataBackups.SelectedIndices.Count = 1) Then
+            'get the selected file path. To be used in opening the file name in form Main
+            strSelectedDataFilePath = strAutoSavedDataFilePaths(ucrLstViewDataBackups.SelectedIndices(0))
+        Else
+            'don't allow opening of more than 1 file at a time
+            'NOTE in future this can be changed
+            MsgBox("You can only save one data file at a time")
+        End If
+
+        Close()
+
+    End Sub
+
+    Private Sub cmdSave_Click(sender As Object, e As EventArgs) Handles cmdSave.Click
+
+        If (Not testOk()) Then
+            MsgBox("Select the file to save")
+            Return
+        End If
+
+        'if its only one file that got selected then use the SaveFileDialog
+        If (ucrLstViewDataBackups.SelectedIndices.Count = 1) Then
+            Using dlgSave As New SaveFileDialog
+                dlgSave.Title = "Save Data File"
+                dlgSave.Filter = "RDS Data file (*.RDS)|*.RDS"
+                If dlgSave.ShowDialog() = DialogResult.OK Then
+                    'save the selected file
+                    saveFile(strAutoSavedDataFilePaths(ucrLstViewDataBackups.SelectedIndices(0)), dlgSave.FileName)
+                    'display success message
+                    MsgBox("Data file successfully saved to " & dlgSave.FileName)
+                End If
+            End Using
+        Else
+            'don't allow saving of more than 1 file at a time
+            'NOTE in future this can be changed
+            MsgBox("You can only save one data file at a time")
+        End If
+
+
+    End Sub
+
+    Private Sub cmdDelete_Click(sender As Object, e As EventArgs) Handles cmdDelete.Click
+
+        If (Not testOk()) Then
+            MsgBox("Select the file to delete")
+            Return
+        End If
+        If MsgBox("Are you sure you want to delete this file?" & Environment.NewLine & "You cannot undo this action and all its data will be lost.", MessageBoxButtons.YesNo, "Back up Manager") = MsgBoxResult.No Then
+            Return
+        End If
+
+        'loop thro deleting the selected files
+        For i As Integer = 0 To ucrLstViewDataBackups.SelectedIndices.Count - 1
+            'delete the file
+            File.Delete(strAutoSavedDataFilePaths(ucrLstViewDataBackups.SelectedIndices(i)))
+
+        Next
+
+        'then reload file info's
+        loadFilesInfo()
+        'set the button states accordingly
+        testOk()
+
+    End Sub
+
+    Private Sub cmdHelp_Click(sender As Object, e As EventArgs) Handles cmdHelp.Click
+        'to do 
+    End Sub
+
+    Private Sub cmdClose_Click(sender As Object, e As EventArgs) Handles cmdClose.Click
+        strSelectedDataFilePath = ""
+        Close()
+    End Sub
+
+    'function to return bytes as KB,MB,GB,TB in a 2 dp string format
+    Public Function getConvertedFileSize(fileSize As ULong) As String
+        Try
+            'to hold the div result as a double for formatting
+            Dim doubleBytes As Double
+            Select Case fileSize
+                Case Is >= 1099511627776
+                    doubleBytes = fileSize / 1099511627776 'TB
+                    Return FormatNumber(doubleBytes, 2) & " TB"
+                Case 1073741824 To 1099511627775
+                    doubleBytes = fileSize / 1073741824 'GB
+                    Return FormatNumber(doubleBytes, 2) & " GB"
+                Case 1048576 To 1073741823
+                    doubleBytes = fileSize / 1048576 'MB
+                    Return FormatNumber(doubleBytes, 2) & " MB"
+                Case 1024 To 1048575
+                    doubleBytes = fileSize / 1024 'KB
+                    Return FormatNumber(doubleBytes, 2) & " KB"
+                Case 0 To 1023
+                    doubleBytes = fileSize ' bytes
+                    Return FormatNumber(doubleBytes, 2) & " bytes"
+                Case Else
+                    Return ""
+            End Select
+        Catch ex As Exception
+            Return ""
+        End Try
+    End Function
+
+    'serves 2 purposes. checks whether the open,save,delete operation are allowed and also sets the controls states
+    Private Function testOk() As Boolean
+        If (ucrLstViewDataBackups.Items.Count > 0 AndAlso ucrLstViewDataBackups.SelectedIndices.Count > 0) Then
+            cmdSave.Enabled = True
+            cmdOpen.Enabled = True
+            cmdDelete.Enabled = True
+            Return True
+        Else
+            cmdSave.Enabled = False
+            cmdOpen.Enabled = False
+            cmdDelete.Enabled = False
+            Return False
+        End If
+    End Function
+
+    'copies the file to the user destination location
+    Private Sub saveFile(sourceFileName As String, destFilename As String)
+        Try
+            File.Copy(sourceFileName, destFilename, True)
+        Catch ex As Exception
+            MsgBox("Could not copy and/or delete data file." & Environment.NewLine & ex.Message, "Error copying/deleting file")
+        End Try
+    End Sub
+
+End Class
