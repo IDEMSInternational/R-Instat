f7ef41614948d87642efe10edc1103ada4b2f863
maxwell fundi <maxwellfundi@live.com>
2017-11-22 08:00:42 +0300

    Adding code for opening script

--- instat/ucrScript.vb
+++ instat/ucrScript.vb
@@ -14,8 +14,10 @@
 ' You should have received a copy of the GNU General Public License 
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+Imports System.IO
 Public Class ucrScript
     Private strComment As String = "Code run from Script Window"
+    Public strRInstatLogFilesFolderPath As String = Path.Combine(Path.GetFullPath(FileIO.SpecialDirectories.MyDocuments), "R-Instat_Log_files")
 
     Public Sub CopyText()
         txtScript.Copy()
@@ -58,5 +60,28 @@ Public Class ucrScript
                 frmMain.clsRLink.RunScriptFromWindow(strNewScript:=strText, strNewComment:=strComment)
             End If
         End If
+    End Sub
+
+    Private Sub cmdOpenScript_Click(sender As Object, e As EventArgs) Handles cmdOpenScript.Click
+        Dim clsProcessStart As New RFunction
+        Dim strScriptFilename As String = ""
+        Dim i As Integer
+
+        Try
+            If Not Directory.Exists(strRInstatLogFilesFolderPath) Then
+                Directory.CreateDirectory(strRInstatLogFilesFolderPath)
+            End If
+            strScriptFilename = "RInstatScript.R"
+
+            While File.Exists(Path.Combine(strRInstatLogFilesFolderPath, strScriptFilename))
+                i = i + 1
+                strScriptFilename = "RInstatScript" & i & ".R"
+            End While
+            File.WriteAllText(Path.Combine(strRInstatLogFilesFolderPath, strScriptFilename), frmMain.clsRLink.GetRSetupScript() & txtScript.Text)
+            Process.Start(Path.Combine(strRInstatLogFilesFolderPath, strScriptFilename))
+        Catch
+            MsgBox("Could not save the script file." & Environment.NewLine & "The file may be in use by another program or you may not have access to write to the specified location.", MsgBoxStyle.Critical)
+            End
+        End Try
     End Sub
 End Class
