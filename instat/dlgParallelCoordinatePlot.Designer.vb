f5d655c7f42cd606d60a76ff03e0bbf3326edaec
Lazarus Kioko Muthenya <klmuthenya@outlook.com>
2017-11-27 11:25:55 +0300

    dlgParallelCoordinatePlot Designer Additions.

--- instat/dlgParallelCoordinatePlot.Designer.vb
+++ instat/dlgParallelCoordinatePlot.Designer.vb
@@ -23,17 +23,169 @@ Partial Class dlgParallelCoordinatePlot
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(dlgParallelCoordinatePlot))
+        Me.ucrSelectorParallelCoordinatePlot = New instat.ucrSelectorByDataFrameAddRemove()
+        Me.cmdParcoordOptions = New System.Windows.Forms.Button()
+        Me.cmdPlotOptions = New System.Windows.Forms.Button()
+        Me.UcrReceiverMultiple1 = New instat.ucrReceiverMultiple()
+        Me.lblXVariables = New System.Windows.Forms.Label()
+        Me.ucrReceiverFactor = New instat.ucrReceiverSingle()
+        Me.lblFactor = New System.Windows.Forms.Label()
+        Me.ucrBase = New instat.ucrButtons()
+        Me.ucrSaveGraph = New instat.ucrSaveGraph()
+        Me.lblScale = New System.Windows.Forms.Label()
+        Me.ucrInputScale = New instat.ucrInputComboBox()
+        Me.ucrChkBoxplots = New instat.ucrCheck()
+        Me.ucrChkPoints = New instat.ucrCheck()
+        Me.lblTransparency = New System.Windows.Forms.Label()
+        Me.ucrNudTransparency = New instat.ucrNud()
+        Me.ucrChkHorizontalPlot = New instat.ucrCheck()
         Me.SuspendLayout()
+        '
+        'ucrSelectorParallelCoordinatePlot
+        '
+        Me.ucrSelectorParallelCoordinatePlot.bShowHiddenColumns = False
+        Me.ucrSelectorParallelCoordinatePlot.bUseCurrentFilter = True
+        resources.ApplyResources(Me.ucrSelectorParallelCoordinatePlot, "ucrSelectorParallelCoordinatePlot")
+        Me.ucrSelectorParallelCoordinatePlot.Name = "ucrSelectorParallelCoordinatePlot"
+        '
+        'cmdParcoordOptions
+        '
+        resources.ApplyResources(Me.cmdParcoordOptions, "cmdParcoordOptions")
+        Me.cmdParcoordOptions.Name = "cmdParcoordOptions"
+        Me.cmdParcoordOptions.UseVisualStyleBackColor = True
+        '
+        'cmdPlotOptions
+        '
+        resources.ApplyResources(Me.cmdPlotOptions, "cmdPlotOptions")
+        Me.cmdPlotOptions.Name = "cmdPlotOptions"
+        Me.cmdPlotOptions.UseVisualStyleBackColor = True
+        '
+        'UcrReceiverMultiple1
+        '
+        Me.UcrReceiverMultiple1.frmParent = Me
+        resources.ApplyResources(Me.UcrReceiverMultiple1, "UcrReceiverMultiple1")
+        Me.UcrReceiverMultiple1.Name = "UcrReceiverMultiple1"
+        Me.UcrReceiverMultiple1.Selector = Nothing
+        Me.UcrReceiverMultiple1.strNcFilePath = ""
+        Me.UcrReceiverMultiple1.ucrSelector = Nothing
+        '
+        'lblXVariables
+        '
+        resources.ApplyResources(Me.lblXVariables, "lblXVariables")
+        Me.lblXVariables.Name = "lblXVariables"
+        '
+        'ucrReceiverFactor
+        '
+        Me.ucrReceiverFactor.frmParent = Me
+        resources.ApplyResources(Me.ucrReceiverFactor, "ucrReceiverFactor")
+        Me.ucrReceiverFactor.Name = "ucrReceiverFactor"
+        Me.ucrReceiverFactor.Selector = Nothing
+        Me.ucrReceiverFactor.strNcFilePath = ""
+        Me.ucrReceiverFactor.ucrSelector = Nothing
+        '
+        'lblFactor
+        '
+        resources.ApplyResources(Me.lblFactor, "lblFactor")
+        Me.lblFactor.Name = "lblFactor"
+        '
+        'ucrBase
+        '
+        resources.ApplyResources(Me.ucrBase, "ucrBase")
+        Me.ucrBase.Name = "ucrBase"
+        '
+        'ucrSaveGraph
+        '
+        resources.ApplyResources(Me.ucrSaveGraph, "ucrSaveGraph")
+        Me.ucrSaveGraph.Name = "ucrSaveGraph"
+        '
+        'lblScale
+        '
+        resources.ApplyResources(Me.lblScale, "lblScale")
+        Me.lblScale.Name = "lblScale"
+        '
+        'ucrInputScale
+        '
+        Me.ucrInputScale.AddQuotesIfUnrecognised = True
+        Me.ucrInputScale.IsReadOnly = False
+        resources.ApplyResources(Me.ucrInputScale, "ucrInputScale")
+        Me.ucrInputScale.Name = "ucrInputScale"
+        '
+        'ucrChkBoxplots
+        '
+        Me.ucrChkBoxplots.Checked = False
+        resources.ApplyResources(Me.ucrChkBoxplots, "ucrChkBoxplots")
+        Me.ucrChkBoxplots.Name = "ucrChkBoxplots"
+        '
+        'ucrChkPoints
+        '
+        Me.ucrChkPoints.Checked = False
+        resources.ApplyResources(Me.ucrChkPoints, "ucrChkPoints")
+        Me.ucrChkPoints.Name = "ucrChkPoints"
+        '
+        'lblTransparency
+        '
+        resources.ApplyResources(Me.lblTransparency, "lblTransparency")
+        Me.lblTransparency.Name = "lblTransparency"
+        '
+        'ucrNudTransparency
+        '
+        Me.ucrNudTransparency.DecimalPlaces = New Decimal(New Integer() {0, 0, 0, 0})
+        Me.ucrNudTransparency.Increment = New Decimal(New Integer() {1, 0, 0, 0})
+        resources.ApplyResources(Me.ucrNudTransparency, "ucrNudTransparency")
+        Me.ucrNudTransparency.Maximum = New Decimal(New Integer() {100, 0, 0, 0})
+        Me.ucrNudTransparency.Minimum = New Decimal(New Integer() {0, 0, 0, 0})
+        Me.ucrNudTransparency.Name = "ucrNudTransparency"
+        Me.ucrNudTransparency.Value = New Decimal(New Integer() {0, 0, 0, 0})
+        '
+        'ucrChkHorizontalPlot
+        '
+        Me.ucrChkHorizontalPlot.Checked = False
+        resources.ApplyResources(Me.ucrChkHorizontalPlot, "ucrChkHorizontalPlot")
+        Me.ucrChkHorizontalPlot.Name = "ucrChkHorizontalPlot"
         '
         'dlgParallelCoordinatePlot
         '
         resources.ApplyResources(Me, "$this")
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.ucrChkHorizontalPlot)
+        Me.Controls.Add(Me.ucrNudTransparency)
+        Me.Controls.Add(Me.lblTransparency)
+        Me.Controls.Add(Me.ucrChkPoints)
+        Me.Controls.Add(Me.ucrChkBoxplots)
+        Me.Controls.Add(Me.ucrInputScale)
+        Me.Controls.Add(Me.lblScale)
+        Me.Controls.Add(Me.ucrSaveGraph)
+        Me.Controls.Add(Me.ucrBase)
+        Me.Controls.Add(Me.lblFactor)
+        Me.Controls.Add(Me.ucrReceiverFactor)
+        Me.Controls.Add(Me.lblXVariables)
+        Me.Controls.Add(Me.UcrReceiverMultiple1)
+        Me.Controls.Add(Me.cmdPlotOptions)
+        Me.Controls.Add(Me.cmdParcoordOptions)
+        Me.Controls.Add(Me.ucrSelectorParallelCoordinatePlot)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "dlgParallelCoordinatePlot"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents ucrSelectorParallelCoordinatePlot As ucrSelectorByDataFrameAddRemove
+    Friend WithEvents cmdParcoordOptions As Button
+    Friend WithEvents cmdPlotOptions As Button
+    Friend WithEvents UcrReceiverMultiple1 As ucrReceiverMultiple
+    Friend WithEvents lblScale As Label
+    Friend WithEvents ucrSaveGraph As ucrSaveGraph
+    Friend WithEvents ucrBase As ucrButtons
+    Friend WithEvents lblFactor As Label
+    Friend WithEvents ucrReceiverFactor As ucrReceiverSingle
+    Friend WithEvents lblXVariables As Label
+    Friend WithEvents ucrInputScale As ucrInputComboBox
+    Friend WithEvents lblTransparency As Label
+    Friend WithEvents ucrChkPoints As ucrCheck
+    Friend WithEvents ucrChkBoxplots As ucrCheck
+    Friend WithEvents ucrNudTransparency As ucrNud
+    Friend WithEvents ucrChkHorizontalPlot As ucrCheck
 End Class
--- instat/dlgParallelCoordinatePlot.vb
+++ instat/dlgParallelCoordinatePlot.vb
@@ -1,3 +1,2 @@
 ﻿Public Class dlgParallelCoordinatePlot
-
 End Class
