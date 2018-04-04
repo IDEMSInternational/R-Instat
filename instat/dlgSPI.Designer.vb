2fccca30032c15c18c81422b7fc428eab16fb0ac
thushari93 <thushari.hapuarachchi@gmail.com>
2018-04-04 13:58:14 +0530

    Progress in SPI

--- instat/dlgSPI.Designer.vb
+++ instat/dlgSPI.Designer.vb
@@ -23,51 +23,38 @@ Partial Class dlgSPI
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(dlgSPI))
-        Me.rdoSPISPEI = New System.Windows.Forms.RadioButton()
-        Me.rdoKernal = New System.Windows.Forms.RadioButton()
-        Me.rdoPET = New System.Windows.Forms.RadioButton()
         Me.lblDate = New System.Windows.Forms.Label()
         Me.lblYear = New System.Windows.Forms.Label()
         Me.lblMonth = New System.Windows.Forms.Label()
         Me.lblData = New System.Windows.Forms.Label()
-        Me.GrpIndex = New System.Windows.Forms.GroupBox()
+        Me.grpIndex = New System.Windows.Forms.GroupBox()
         Me.rdoSPEI = New System.Windows.Forms.RadioButton()
         Me.rdoSPI = New System.Windows.Forms.RadioButton()
         Me.lblTime = New System.Windows.Forms.Label()
-        Me.lblsaveindex = New System.Windows.Forms.Label()
         Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
-        Me.UcrtimeScale = New instat.ucrNud()
+        Me.grpKernal = New System.Windows.Forms.GroupBox()
+        Me.RdoRect = New System.Windows.Forms.RadioButton()
+        Me.Rdocirc = New System.Windows.Forms.RadioButton()
+        Me.RdoGaus = New System.Windows.Forms.RadioButton()
+        Me.RdoTri = New System.Windows.Forms.RadioButton()
+        Me.lblShift = New System.Windows.Forms.Label()
+        Me.grpKernType = New System.Windows.Forms.GroupBox()
+        Me.ucrSaveIndex = New instat.ucrSave()
+        Me.ucrPnlType = New instat.UcrPanel()
+        Me.ucrKernShift = New instat.ucrNud()
+        Me.ucrTimeScale = New instat.ucrNud()
         Me.ucrChkOmitMissingValues = New instat.ucrCheck()
         Me.ucrBase = New instat.ucrButtons()
-        Me.ucrInputIndex = New instat.ucrInputTextBox()
+        Me.ucrPnlInd = New instat.UcrPanel()
         Me.ucrReceiverData = New instat.ucrReceiverSingle()
         Me.ucrReceiverMonth = New instat.ucrReceiverSingle()
         Me.ucrReceiverYear = New instat.ucrReceiverSingle()
         Me.ucrReceiverDate = New instat.ucrReceiverSingle()
         Me.ucrSelectorVariable = New instat.ucrSelectorByDataFrameAddRemove()
-        Me.GrpIndex.SuspendLayout()
+        Me.grpIndex.SuspendLayout()
+        Me.grpKernal.SuspendLayout()
+        Me.grpKernType.SuspendLayout()
         Me.SuspendLayout()
-        '
-        'rdoSPISPEI
-        '
-        resources.ApplyResources(Me.rdoSPISPEI, "rdoSPISPEI")
-        Me.rdoSPISPEI.Name = "rdoSPISPEI"
-        Me.rdoSPISPEI.TabStop = True
-        Me.rdoSPISPEI.UseVisualStyleBackColor = True
-        '
-        'rdoKernal
-        '
-        resources.ApplyResources(Me.rdoKernal, "rdoKernal")
-        Me.rdoKernal.Name = "rdoKernal"
-        Me.rdoKernal.TabStop = True
-        Me.rdoKernal.UseVisualStyleBackColor = True
-        '
-        'rdoPET
-        '
-        resources.ApplyResources(Me.rdoPET, "rdoPET")
-        Me.rdoPET.Name = "rdoPET"
-        Me.rdoPET.TabStop = True
-        Me.rdoPET.UseVisualStyleBackColor = True
         '
         'lblDate
         '
@@ -89,13 +76,14 @@ Partial Class dlgSPI
         resources.ApplyResources(Me.lblData, "lblData")
         Me.lblData.Name = "lblData"
         '
-        'GrpIndex
+        'grpIndex
         '
-        Me.GrpIndex.Controls.Add(Me.rdoSPEI)
-        Me.GrpIndex.Controls.Add(Me.rdoSPI)
-        resources.ApplyResources(Me.GrpIndex, "GrpIndex")
-        Me.GrpIndex.Name = "GrpIndex"
-        Me.GrpIndex.TabStop = False
+        Me.grpIndex.Controls.Add(Me.ucrPnlInd)
+        Me.grpIndex.Controls.Add(Me.rdoSPEI)
+        Me.grpIndex.Controls.Add(Me.rdoSPI)
+        resources.ApplyResources(Me.grpIndex, "grpIndex")
+        Me.grpIndex.Name = "grpIndex"
+        Me.grpIndex.TabStop = False
         '
         'rdoSPEI
         '
@@ -116,20 +104,88 @@ Partial Class dlgSPI
         resources.ApplyResources(Me.lblTime, "lblTime")
         Me.lblTime.Name = "lblTime"
         '
-        'lblsaveindex
+        'grpKernal
         '
-        resources.ApplyResources(Me.lblsaveindex, "lblsaveindex")
-        Me.lblsaveindex.Name = "lblsaveindex"
+        Me.grpKernal.Controls.Add(Me.lblShift)
+        Me.grpKernal.Controls.Add(Me.grpKernType)
+        Me.grpKernal.Controls.Add(Me.ucrKernShift)
+        resources.ApplyResources(Me.grpKernal, "grpKernal")
+        Me.grpKernal.Name = "grpKernal"
+        Me.grpKernal.TabStop = False
         '
-        'UcrtimeScale
+        'RdoRect
         '
-        Me.UcrtimeScale.DecimalPlaces = New Decimal(New Integer() {0, 0, 0, 0})
-        Me.UcrtimeScale.Increment = New Decimal(New Integer() {1, 0, 0, 0})
-        resources.ApplyResources(Me.UcrtimeScale, "UcrtimeScale")
-        Me.UcrtimeScale.Maximum = New Decimal(New Integer() {24, 0, 0, 0})
-        Me.UcrtimeScale.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
-        Me.UcrtimeScale.Name = "UcrtimeScale"
-        Me.UcrtimeScale.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        resources.ApplyResources(Me.RdoRect, "RdoRect")
+        Me.RdoRect.Name = "RdoRect"
+        Me.RdoRect.TabStop = True
+        Me.RdoRect.UseVisualStyleBackColor = True
+        '
+        'Rdocirc
+        '
+        resources.ApplyResources(Me.Rdocirc, "Rdocirc")
+        Me.Rdocirc.Name = "Rdocirc"
+        Me.Rdocirc.TabStop = True
+        Me.Rdocirc.UseVisualStyleBackColor = True
+        '
+        'RdoGaus
+        '
+        resources.ApplyResources(Me.RdoGaus, "RdoGaus")
+        Me.RdoGaus.Name = "RdoGaus"
+        Me.RdoGaus.TabStop = True
+        Me.RdoGaus.UseVisualStyleBackColor = True
+        '
+        'RdoTri
+        '
+        resources.ApplyResources(Me.RdoTri, "RdoTri")
+        Me.RdoTri.Name = "RdoTri"
+        Me.RdoTri.TabStop = True
+        Me.RdoTri.UseVisualStyleBackColor = True
+        '
+        'lblShift
+        '
+        resources.ApplyResources(Me.lblShift, "lblShift")
+        Me.lblShift.Name = "lblShift"
+        '
+        'grpKernType
+        '
+        Me.grpKernType.Controls.Add(Me.ucrPnlType)
+        Me.grpKernType.Controls.Add(Me.RdoRect)
+        Me.grpKernType.Controls.Add(Me.RdoGaus)
+        Me.grpKernType.Controls.Add(Me.Rdocirc)
+        Me.grpKernType.Controls.Add(Me.RdoTri)
+        resources.ApplyResources(Me.grpKernType, "grpKernType")
+        Me.grpKernType.Name = "grpKernType"
+        Me.grpKernType.TabStop = False
+        '
+        'ucrSaveIndex
+        '
+        resources.ApplyResources(Me.ucrSaveIndex, "ucrSaveIndex")
+        Me.ucrSaveIndex.Name = "ucrSaveIndex"
+        '
+        'ucrPnlType
+        '
+        resources.ApplyResources(Me.ucrPnlType, "ucrPnlType")
+        Me.ucrPnlType.Name = "ucrPnlType"
+        '
+        'ucrKernShift
+        '
+        Me.ucrKernShift.DecimalPlaces = New Decimal(New Integer() {0, 0, 0, 0})
+        Me.ucrKernShift.Increment = New Decimal(New Integer() {1, 0, 0, 0})
+        resources.ApplyResources(Me.ucrKernShift, "ucrKernShift")
+        Me.ucrKernShift.Maximum = New Decimal(New Integer() {24, 0, 0, 0})
+        Me.ucrKernShift.Minimum = New Decimal(New Integer() {0, 0, 0, 0})
+        Me.ucrKernShift.Name = "ucrKernShift"
+        Me.ucrKernShift.Value = New Decimal(New Integer() {2, 0, 0, 0})
+        '
+        'ucrTimeScale
+        '
+        Me.ucrTimeScale.DecimalPlaces = New Decimal(New Integer() {0, 0, 0, 0})
+        Me.ucrTimeScale.Increment = New Decimal(New Integer() {1, 0, 0, 0})
+        resources.ApplyResources(Me.ucrTimeScale, "ucrTimeScale")
+        Me.ucrTimeScale.Maximum = New Decimal(New Integer() {24, 0, 0, 0})
+        Me.ucrTimeScale.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.ucrTimeScale.Name = "ucrTimeScale"
+        Me.ucrTimeScale.Value = New Decimal(New Integer() {1, 0, 0, 0})
         '
         'ucrChkOmitMissingValues
         '
@@ -142,13 +198,10 @@ Partial Class dlgSPI
         resources.ApplyResources(Me.ucrBase, "ucrBase")
         Me.ucrBase.Name = "ucrBase"
         '
-        'ucrInputIndex
+        'ucrPnlInd
         '
-        Me.ucrInputIndex.AddQuotesIfUnrecognised = True
-        Me.ucrInputIndex.IsMultiline = False
-        Me.ucrInputIndex.IsReadOnly = False
-        resources.ApplyResources(Me.ucrInputIndex, "ucrInputIndex")
-        Me.ucrInputIndex.Name = "ucrInputIndex"
+        resources.ApplyResources(Me.ucrPnlInd, "ucrPnlInd")
+        Me.ucrPnlInd.Name = "ucrPnlInd"
         '
         'ucrReceiverData
         '
@@ -197,13 +250,13 @@ Partial Class dlgSPI
         '
         resources.ApplyResources(Me, "$this")
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Controls.Add(Me.UcrtimeScale)
+        Me.Controls.Add(Me.ucrSaveIndex)
+        Me.Controls.Add(Me.grpKernal)
+        Me.Controls.Add(Me.ucrTimeScale)
         Me.Controls.Add(Me.ucrChkOmitMissingValues)
         Me.Controls.Add(Me.ucrBase)
-        Me.Controls.Add(Me.ucrInputIndex)
-        Me.Controls.Add(Me.lblsaveindex)
         Me.Controls.Add(Me.lblTime)
-        Me.Controls.Add(Me.GrpIndex)
+        Me.Controls.Add(Me.grpIndex)
         Me.Controls.Add(Me.lblData)
         Me.Controls.Add(Me.lblMonth)
         Me.Controls.Add(Me.lblYear)
@@ -213,24 +266,20 @@ Partial Class dlgSPI
         Me.Controls.Add(Me.ucrReceiverYear)
         Me.Controls.Add(Me.ucrReceiverDate)
         Me.Controls.Add(Me.ucrSelectorVariable)
-        Me.Controls.Add(Me.rdoPET)
-        Me.Controls.Add(Me.rdoKernal)
-        Me.Controls.Add(Me.rdoSPISPEI)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "dlgSPI"
-        Me.GrpIndex.ResumeLayout(False)
-        Me.GrpIndex.PerformLayout()
+        Me.grpIndex.ResumeLayout(False)
+        Me.grpIndex.PerformLayout()
+        Me.grpKernal.ResumeLayout(False)
+        Me.grpKernal.PerformLayout()
+        Me.grpKernType.ResumeLayout(False)
+        Me.grpKernType.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-
-
-    Friend WithEvents rdoSPISPEI As RadioButton
-    Friend WithEvents rdoKernal As RadioButton
-    Friend WithEvents rdoPET As RadioButton
     Friend WithEvents ucrSelectorVariable As ucrSelectorByDataFrameAddRemove
     Friend WithEvents ucrReceiverDate As ucrReceiverSingle
     Friend WithEvents lblDate As Label
@@ -240,14 +289,23 @@ Partial Class dlgSPI
     Friend WithEvents lblData As Label
     Friend WithEvents lblMonth As Label
     Friend WithEvents lblYear As Label
-    Friend WithEvents GrpIndex As GroupBox
+    Friend WithEvents grpIndex As GroupBox
     Friend WithEvents rdoSPEI As RadioButton
     Friend WithEvents rdoSPI As RadioButton
-    Friend WithEvents lblsaveindex As Label
     Friend WithEvents lblTime As Label
-    Friend WithEvents ucrInputIndex As ucrInputTextBox
     Friend WithEvents ucrBase As ucrButtons
     Friend WithEvents ucrChkOmitMissingValues As ucrCheck
     Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
-    Friend WithEvents UcrtimeScale As ucrNud
+    Friend WithEvents ucrTimeScale As ucrNud
+    Friend WithEvents grpKernal As GroupBox
+    Friend WithEvents RdoRect As RadioButton
+    Friend WithEvents RdoTri As RadioButton
+    Friend WithEvents ucrPnlType As UcrPanel
+    Friend WithEvents Rdocirc As RadioButton
+    Friend WithEvents RdoGaus As RadioButton
+    Friend WithEvents lblShift As Label
+    Friend WithEvents ucrKernShift As ucrNud
+    Friend WithEvents ucrPnlInd As UcrPanel
+    Friend WithEvents ucrSaveIndex As ucrSave
+    Friend WithEvents grpKernType As GroupBox
 End Class
