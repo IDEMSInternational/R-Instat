930240a2fb71ef18f301cff6884f27264faf4702
Bethan Clarke <bethanclarke@live.co.uk>
2018-06-14 12:00:35 +0100

    chnages to day_in_year

--- instat/static/InstatObject/R/data_object_R6.R
+++ instat/static/InstatObject/R/data_object_R6.R
@@ -2143,7 +2143,8 @@ data_object$set("public","split_date", function(col_name = "", year = FALSE, lea
   }
   
   if(day_in_year) {
-    day_in_year_vector <- as.integer(lubridate::yday(col_data))
+    day_in_year_vector <- (as.integer(lubridate::yday(col_data))) + (367 - s_start_month) %% 366
+	day_in_year_vector <- ifelse(day_in_year_vector == 0, 12, day_in_year_vector)
     col_name <- next_default_item(prefix = "doy_365", existing_names = self$get_column_names(), include_index = FALSE)
     self$add_columns_to_data(col_name = col_name, col_data = day_in_year_vector)
     if(self$is_climatic_data()) self$set_climatic_types(types = c(doy = col_name))
