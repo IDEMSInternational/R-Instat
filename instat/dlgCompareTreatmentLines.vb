fb8f2269e95eb32f3a302f2edc969e21ccad6568
Lazarus Kioko Muthenya <muthenyakioko@gmail.com>
2018-06-19 15:34:22 +0300

    Enabling autoTranslate(Me)

--- instat/dlgCompareTreatmentLines.vb
+++ instat/dlgCompareTreatmentLines.vb
@@ -31,7 +31,7 @@ Public Class dlgCompareTreatmentLines
         End If
         SetRCodeforControls(bReset)
         bReset = False
-        'autoTranslate(Me)
+        autoTranslate(Me)
     End Sub
 
     Private Sub InitialiseDialog()
