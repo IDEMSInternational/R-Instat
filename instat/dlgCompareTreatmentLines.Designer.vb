ed49369e80f72ace00d389febcffe575a61d3de7
Danny Parsons <danny@aims.ac.za>
2018-06-19 18:27:14 +0100

    added dialog code for treatment lines comparison created input factor control

--- instat/dlgCompareTreatmentLines.Designer.vb
+++ instat/dlgCompareTreatmentLines.Designer.vb
@@ -23,26 +23,142 @@ Partial Class dlgCompareTreatmentLines
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(dlgCompareTreatmentLines))
+        Me.lblMeasurement = New System.Windows.Forms.Label()
+        Me.lblTreamtments = New System.Windows.Forms.Label()
+        Me.lblID = New System.Windows.Forms.Label()
+        Me.ucrReceiverID = New instat.ucrReceiverSingle()
+        Me.ucrReceiverTreatments = New instat.ucrReceiverSingle()
+        Me.ucrReceiverMeasurement = New instat.ucrReceiverSingle()
+        Me.ucrSelectorPlot = New instat.ucrSelectorByDataFrameAddRemove()
+        Me.ucrSavePlot = New instat.ucrSave()
         Me.ucrBase = New instat.ucrButtons()
+        Me.ucrInputFactorTreatment1 = New instat.ucrInputFactorLevels()
+        Me.ucrInputFactorTreatment2 = New instat.ucrInputFactorLevels()
+        Me.lblTreatment1 = New System.Windows.Forms.Label()
+        Me.lblTreatment2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
+        '
+        'lblMeasurement
+        '
+        resources.ApplyResources(Me.lblMeasurement, "lblMeasurement")
+        Me.lblMeasurement.Name = "lblMeasurement"
+        '
+        'lblTreamtments
+        '
+        resources.ApplyResources(Me.lblTreamtments, "lblTreamtments")
+        Me.lblTreamtments.Name = "lblTreamtments"
+        '
+        'lblID
+        '
+        resources.ApplyResources(Me.lblID, "lblID")
+        Me.lblID.Name = "lblID"
+        '
+        'ucrReceiverID
+        '
+        Me.ucrReceiverID.frmParent = Me
+        resources.ApplyResources(Me.ucrReceiverID, "ucrReceiverID")
+        Me.ucrReceiverID.Name = "ucrReceiverID"
+        Me.ucrReceiverID.Selector = Nothing
+        Me.ucrReceiverID.strNcFilePath = ""
+        Me.ucrReceiverID.ucrSelector = Nothing
+        '
+        'ucrReceiverTreatments
+        '
+        Me.ucrReceiverTreatments.frmParent = Me
+        resources.ApplyResources(Me.ucrReceiverTreatments, "ucrReceiverTreatments")
+        Me.ucrReceiverTreatments.Name = "ucrReceiverTreatments"
+        Me.ucrReceiverTreatments.Selector = Nothing
+        Me.ucrReceiverTreatments.strNcFilePath = ""
+        Me.ucrReceiverTreatments.ucrSelector = Nothing
+        '
+        'ucrReceiverMeasurement
+        '
+        Me.ucrReceiverMeasurement.frmParent = Me
+        resources.ApplyResources(Me.ucrReceiverMeasurement, "ucrReceiverMeasurement")
+        Me.ucrReceiverMeasurement.Name = "ucrReceiverMeasurement"
+        Me.ucrReceiverMeasurement.Selector = Nothing
+        Me.ucrReceiverMeasurement.strNcFilePath = ""
+        Me.ucrReceiverMeasurement.ucrSelector = Nothing
+        '
+        'ucrSelectorPlot
+        '
+        Me.ucrSelectorPlot.bDropUnusedFilterLevels = False
+        Me.ucrSelectorPlot.bShowHiddenColumns = False
+        Me.ucrSelectorPlot.bUseCurrentFilter = True
+        resources.ApplyResources(Me.ucrSelectorPlot, "ucrSelectorPlot")
+        Me.ucrSelectorPlot.Name = "ucrSelectorPlot"
+        '
+        'ucrSavePlot
+        '
+        resources.ApplyResources(Me.ucrSavePlot, "ucrSavePlot")
+        Me.ucrSavePlot.Name = "ucrSavePlot"
         '
         'ucrBase
         '
         resources.ApplyResources(Me.ucrBase, "ucrBase")
         Me.ucrBase.Name = "ucrBase"
         '
+        'ucrInputFactorTreatment1
+        '
+        Me.ucrInputFactorTreatment1.AddQuotesIfUnrecognised = True
+        Me.ucrInputFactorTreatment1.IsReadOnly = False
+        resources.ApplyResources(Me.ucrInputFactorTreatment1, "ucrInputFactorTreatment1")
+        Me.ucrInputFactorTreatment1.Name = "ucrInputFactorTreatment1"
+        '
+        'ucrInputFactorTreatment2
+        '
+        Me.ucrInputFactorTreatment2.AddQuotesIfUnrecognised = True
+        Me.ucrInputFactorTreatment2.IsReadOnly = False
+        resources.ApplyResources(Me.ucrInputFactorTreatment2, "ucrInputFactorTreatment2")
+        Me.ucrInputFactorTreatment2.Name = "ucrInputFactorTreatment2"
+        '
+        'lblTreatment1
+        '
+        resources.ApplyResources(Me.lblTreatment1, "lblTreatment1")
+        Me.lblTreatment1.Name = "lblTreatment1"
+        '
+        'lblTreatment2
+        '
+        resources.ApplyResources(Me.lblTreatment2, "lblTreatment2")
+        Me.lblTreatment2.Name = "lblTreatment2"
+        '
         'dlgCompareTreatmentLines
         '
         resources.ApplyResources(Me, "$this")
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.lblTreatment2)
+        Me.Controls.Add(Me.lblTreatment1)
+        Me.Controls.Add(Me.ucrInputFactorTreatment2)
+        Me.Controls.Add(Me.ucrInputFactorTreatment1)
+        Me.Controls.Add(Me.ucrReceiverID)
+        Me.Controls.Add(Me.lblID)
+        Me.Controls.Add(Me.ucrReceiverTreatments)
+        Me.Controls.Add(Me.lblTreamtments)
+        Me.Controls.Add(Me.ucrReceiverMeasurement)
+        Me.Controls.Add(Me.lblMeasurement)
+        Me.Controls.Add(Me.ucrSelectorPlot)
+        Me.Controls.Add(Me.ucrSavePlot)
         Me.Controls.Add(Me.ucrBase)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "dlgCompareTreatmentLines"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents ucrBase As ucrButtons
+    Friend WithEvents ucrSavePlot As ucrSave
+    Friend WithEvents ucrSelectorPlot As ucrSelectorByDataFrameAddRemove
+    Friend WithEvents lblMeasurement As Label
+    Friend WithEvents ucrReceiverMeasurement As ucrReceiverSingle
+    Friend WithEvents ucrReceiverTreatments As ucrReceiverSingle
+    Friend WithEvents lblTreamtments As Label
+    Friend WithEvents ucrReceiverID As ucrReceiverSingle
+    Friend WithEvents lblID As Label
+    Friend WithEvents lblTreatment2 As Label
+    Friend WithEvents lblTreatment1 As Label
+    Friend WithEvents ucrInputFactorTreatment2 As ucrInputFactorLevels
+    Friend WithEvents ucrInputFactorTreatment1 As ucrInputFactorLevels
 End Class
--- instat/dlgCompareTreatmentLines.vb
+++ instat/dlgCompareTreatmentLines.vb
@@ -14,12 +14,49 @@
 ' You should have received a copy of the GNU General Public License 
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+Imports instat
 Imports instat.Translations
 
 Public Class dlgCompareTreatmentLines
     Private bFirstLoad As Boolean = True
     Private bReset As Boolean = True
+    Private bResetSubdialog As Boolean = True
+    Private bResetBoxLayerSubdialog As Boolean = True
 
+    ' Main functions and operator for ggplot
+    Private clsBaseOperator As New ROperator
+    Private clsRggplotFunction As New RFunction
+    Private clsRaesGlobalFunction As New RFunction
+    Private clsRaesLineFunction As New RFunction
+
+    ' Geoms (layers)
+    Private clsLineGeom As New RFunction
+    Private clsBoxplotGeom As New RFunction
+    Private clsHlineGeom As New RFunction
+
+    ' Additional functions for plot subdialog
+    Private clsLabsFunction As New RFunction
+    Private clsXlabsFunction As New RFunction
+    Private clsYlabFunction As New RFunction
+    Private clsXScaleContinuousFunction As New RFunction
+    Private clsYScaleContinuousFunction As New RFunction
+    Private clsRFacetFunction As New RFunction
+    Private clsThemeFunction As New RFunction
+    Private dctThemeFunctions As Dictionary(Of String, RFunction)
+
+    ' Data unstacking, diff calculation and merge codes
+    Private clsLeftJoin As New RFunction
+    Private clsDCast As New RFunction
+    Private clsDCastFormula As New ROperator
+    Private clsPipeOperator As New ROperator
+    Private clsMutate As New RFunction
+    Private clsDiffOperator As New ROperator
+    Private clsCut As New RFunction
+    Private clsCutBreaks As New RFunction
+    Private clsCutLabels As New RFunction
+
+    Private strDiff As String = "diff"
+    Private strDiffCode As String = "diff_code"
 
     Private Sub dlgCompareTreatmentLines_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         If bFirstLoad Then
@@ -36,13 +73,169 @@ Public Class dlgCompareTreatmentLines
 
     Private Sub InitialiseDialog()
 
+        ucrBase.clsRsyntax.bExcludeAssignedFunctionOutput = False
+        'ucrBase.iHelpTopicID = 
+        ucrBase.clsRsyntax.iCallType = 3
+
+        ucrSelectorPlot.SetParameter(New RParameter("x", 0))
+        ucrSelectorPlot.SetParameterIsrfunction()
+
+        ucrReceiverMeasurement.SetParameter(New RParameter("y", 1))
+        ucrReceiverMeasurement.Selector = ucrSelectorPlot
+        ucrReceiverMeasurement.SetIncludedDataTypes({"numeric"})
+        ucrReceiverMeasurement.strSelectorHeading = "Numerics"
+        ucrReceiverMeasurement.SetParameterIsString()
+        ucrReceiverMeasurement.bWithQuotes = False
+
+        ucrReceiverTreatments.SetParameter(New RParameter("right", 1))
+        ucrReceiverTreatments.Selector = ucrSelectorPlot
+        ucrReceiverTreatments.SetIncludedDataTypes({"factor"})
+        ucrReceiverTreatments.strSelectorHeading = "Factors"
+        ucrReceiverTreatments.SetParameterIsString()
+        ucrReceiverTreatments.bWithQuotes = False
+
+        ucrInputFactorTreatment1.SetParameter(New RParameter("left", 0))
+        ucrInputFactorTreatment1.SetFactorReceiver(ucrReceiverTreatments)
+        ucrInputFactorTreatment1.bLevelsWithQuotes = False
+
+        ucrInputFactorTreatment2.SetParameter(New RParameter("right", 1))
+        ucrInputFactorTreatment2.SetFactorReceiver(ucrReceiverTreatments)
+        ucrInputFactorTreatment2.AddQuotesIfUnrecognised = False
+        ucrInputFactorTreatment2.bLevelsWithQuotes = False
+
+        ucrReceiverID.SetParameter(New RParameter("left", 0))
+        ucrReceiverID.Selector = ucrSelectorPlot
+        ucrReceiverID.SetParameterIsString()
+        ucrReceiverID.bWithQuotes = False
+
+        ucrSavePlot.SetPrefix("line")
+        ucrSavePlot.SetIsComboBox()
+        ucrSavePlot.SetCheckBoxText("Save Graph")
+        ucrSavePlot.SetSaveTypeAsGraph()
+        ucrSavePlot.SetDataFrameSelector(ucrSelectorPlot.ucrAvailableDataFrames)
+        ucrSavePlot.SetAssignToIfUncheckedValue("last_graph")
     End Sub
 
     Private Sub SetDefaults()
+        clsBaseOperator = New ROperator
+        clsRggplotFunction = New RFunction
+        clsRaesGlobalFunction = New RFunction
+        clsRaesLineFunction = New RFunction
 
+        clsLineGeom = New RFunction
+        clsBoxplotGeom = New RFunction
+        clsHlineGeom = New RFunction
+
+        ucrSelectorPlot.Reset()
+        ucrSelectorPlot.SetGgplotFunction(clsBaseOperator)
+        ucrReceiverMeasurement.SetMeAsReceiver()
+
+        ucrSavePlot.Reset()
+        sdgPlots.Reset()
+
+        bResetSubdialog = True
+        bResetBoxLayerSubdialog = True
+
+        ' Data manipulation code
+        clsLeftJoin = New RFunction
+        clsPipeOperator = New ROperator
+        clsDCast = New RFunction
+        clsDCastFormula = New ROperator
+        clsMutate = New RFunction
+        clsDiffOperator = New ROperator
+        clsCut = New RFunction
+        clsCutBreaks = New RFunction
+        clsCutLabels = New RFunction
+
+        clsLeftJoin.SetPackageName("dplyr")
+        clsLeftJoin.SetRCommand("left_join")
+        SetLeftJoinAssignTo()
+
+        clsLeftJoin.AddParameter("y", clsROperatorParameter:=clsPipeOperator, iPosition:=1)
+
+        clsPipeOperator.SetOperation("%>%")
+
+        clsPipeOperator.AddParameter("1", clsRFunctionParameter:=clsDCast, iPosition:=1)
+        clsPipeOperator.AddParameter("2", clsRFunctionParameter:=clsMutate, iPosition:=2)
+
+        clsDCast.SetPackageName("reshape2")
+        clsDCast.SetRCommand("dcast")
+        clsDCast.AddParameter("formula", clsROperatorParameter:=clsDCastFormula, iPosition:=1)
+
+        clsDCastFormula.SetOperation("~")
+
+        clsMutate.SetPackageName("dplyr")
+        clsMutate.SetRCommand("mutate")
+
+        clsMutate.AddParameter(strDiff, clsROperatorParameter:=clsDiffOperator, iPosition:=0)
+        clsMutate.AddParameter(strDiffCode, clsRFunctionParameter:=clsCut, iPosition:=1)
+
+        clsDiffOperator.SetOperation("-")
+
+        clsCut.SetRCommand("cut")
+        clsCut.AddParameter("x", strDiff, iPosition:=0)
+        clsCut.AddParameter("breaks", clsRFunctionParameter:=clsCutBreaks, iPosition:=1)
+        clsCut.AddParameter("labels", clsRFunctionParameter:=clsCutLabels, iPosition:=2)
+
+        clsCutBreaks.SetRCommand("c")
+        clsCutBreaks.AddParameter("0", "-Inf, -10, 10, Inf", bIncludeArgumentName:=False)
+
+        clsCutLabels.SetRCommand("c")
+        clsCutLabels.AddParameter("0", Chr(34) & "Decrease" & Chr(34) & ", " & Chr(34) & "Same" & Chr(34) & ", " & Chr(34) & "Increase" & Chr(34), bIncludeArgumentName:=False)
+
+        ' ggplot code
+        clsBaseOperator.SetOperation("+")
+        clsBaseOperator.AddParameter("ggplot", clsRFunctionParameter:=clsRggplotFunction, iPosition:=0)
+        clsBaseOperator.AddParameter("geom_line", clsRFunctionParameter:=clsLineGeom, iPosition:=2)
+
+        clsRggplotFunction.SetPackageName("ggplot2")
+        clsRggplotFunction.SetRCommand("ggplot")
+        clsRggplotFunction.AddParameter("data", clsRFunctionParameter:=clsLeftJoin, iPosition:=0)
+        clsRggplotFunction.AddParameter("mapping", clsRFunctionParameter:=clsRaesGlobalFunction, iPosition:=1)
+
+        clsRaesGlobalFunction.SetPackageName("ggplot2")
+        clsRaesGlobalFunction.SetRCommand("aes")
+
+        clsLineGeom.SetPackageName("ggplot2")
+        clsLineGeom.SetRCommand("geom_line")
+        clsLineGeom.AddParameter("mapping", clsRFunctionParameter:=clsRaesLineFunction, iPosition:=0)
+
+        clsRaesLineFunction.SetPackageName("ggplot2")
+        clsRaesLineFunction.SetRCommand("aes")
+        clsRaesLineFunction.AddParameter("colour", strDiffCode)
+
+        clsBoxplotGeom.SetPackageName("ggplot2")
+        clsBoxplotGeom.SetRCommand("geom_boxplot")
+        clsBoxplotGeom.AddParameter("outlier.colour", Chr(34) & "red" & Chr(34), iPosition:=1)
+
+        clsHlineGeom.SetPackageName("ggplot2")
+        clsHlineGeom.SetRCommand("geom_hline")
+
+        clsBaseOperator.AddParameter(GgplotDefaults.clsDefaultThemeParameter.Clone())
+        clsXlabsFunction = GgplotDefaults.clsXlabTitleFunction.Clone()
+        clsLabsFunction = GgplotDefaults.clsDefaultLabs.Clone()
+        clsXScaleContinuousFunction = GgplotDefaults.clsXScalecontinuousFunction.Clone()
+        clsYScaleContinuousFunction = GgplotDefaults.clsYScalecontinuousFunction.Clone()
+        clsRFacetFunction = GgplotDefaults.clsFacetFunction.Clone()
+        clsYlabFunction = GgplotDefaults.clsYlabTitleFunction.Clone
+        clsThemeFunction = GgplotDefaults.clsDefaultThemeFunction.Clone()
+        dctThemeFunctions = New Dictionary(Of String, RFunction)(GgplotDefaults.dctThemeFunctions)
+
+        clsBaseOperator.SetAssignTo("last_graph", strTempDataframe:=ucrSelectorPlot.ucrAvailableDataFrames.cboAvailableDataFrames.Text, strTempGraph:="last_graph")
+        ucrBase.clsRsyntax.SetBaseROperator(clsBaseOperator)
     End Sub
 
-    Private Sub SetRCodeforControls(bReset As Boolean)
+    Private Sub SetRCodeforControls(bResetControls As Boolean)
+        ucrSelectorPlot.AddAdditionalCodeParameterPair(clsPipeOperator, ucrSelectorPlot.GetParameter(), iAdditionalPairNo:=1)
+        ucrReceiverTreatments.AddAdditionalCodeParameterPair(clsRaesGlobalFunction, New RParameter("x", iNewPosition:=0), iAdditionalPairNo:=1)
+        ucrReceiverID.AddAdditionalCodeParameterPair(clsRaesLineFunction, New RParameter("group", iNewPosition:=0), iAdditionalPairNo:=1)
+
+        ucrInputFactorTreatment1.SetRCode(clsDiffOperator, bReset)
+        ucrInputFactorTreatment2.SetRCode(clsDiffOperator, bReset)
+        ucrReceiverMeasurement.SetRCode(clsRaesGlobalFunction, bResetControls)
+        ucrReceiverTreatments.SetRCode(clsDCastFormula, bResetControls)
+        ucrReceiverID.SetRCode(clsDCastFormula, bResetControls)
+        ucrSelectorPlot.SetRCode(clsLeftJoin, bResetControls)
     End Sub
 
     Private Sub TestOkEnabled()
@@ -55,4 +248,31 @@ Public Class dlgCompareTreatmentLines
         TestOkEnabled()
     End Sub
 
+    Private Sub ucrSelectorPlot_ControlValueChanged(ucrChangedControl As ucrCore) Handles ucrSelectorPlot.ControlValueChanged
+        SetLeftJoinAssignTo()
+    End Sub
+
+    Private Sub SetLeftJoinAssignTo()
+        If ucrSelectorPlot.ucrAvailableDataFrames.cboAvailableDataFrames.Text <> "" Then
+            clsLeftJoin.SetAssignTo(ucrSelectorPlot.ucrAvailableDataFrames.cboAvailableDataFrames.Text & "_with_diff")
+        Else
+            clsPipeOperator.RemoveAssignTo()
+        End If
+    End Sub
+
+    Private Sub ucrReceiverMeasurement_ControlValueChanged(ucrChangedControl As ucrCore) Handles ucrReceiverMeasurement.ControlValueChanged
+        If ucrReceiverMeasurement.IsEmpty() Then
+            clsDCast.RemoveParameterByName("value.var")
+        Else
+            clsDCast.AddParameter("value.var", ucrReceiverMeasurement.GetVariableNames(), iPosition:=7)
+        End If
+    End Sub
+
+    Private Sub ucrReceiverID_ControlValueChanged(ucrChangedControl As ucrCore) Handles ucrReceiverID.ControlValueChanged
+        If ucrReceiverID.IsEmpty() Then
+            clsLeftJoin.RemoveParameterByName("by")
+        Else
+            clsLeftJoin.AddParameter("by", ucrReceiverID.GetVariableNames(), iPosition:=2)
+        End If
+    End Sub
 End Class
