f0936bfd9a61c03f53714bebe17ae8ed7b4b505b
shadrack kibet <shadrackkibet36@outlook.com>
2018-07-23 14:06:28 +0300

    code addition

--- instat/dlgExportToCMSAF.vb
+++ instat/dlgExportToCMSAF.vb
@@ -1,3 +1,135 @@
-﻿Public Class dlgExportToCMSAF
+﻿' R- Instat
+' Copyright (C) 2015-2017
+'
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+'
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License 
+' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+Imports System.IO
+Imports instat.Translations
+Public Class dlgExportToCMSAF
+    Dim bFirstLoad As Boolean = True
+    Private bReset As Boolean = True
+
+    Private clsAsDataFrameFunction, clsAtrributesFunction, clsExportFunction As New RFunction
+    Private clsAssignOperator As New ROperator
+    Private Sub dlgExportToCMSAF_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        autoTranslate(Me)
+        If bFirstLoad Then
+            InitialiseDialog()
+            bFirstLoad = False
+        End If
+        If bReset Then
+            SetDefaults()
+        End If
+        SetRCodeForControls(bReset)
+        bReset = False
+        TestOkEnabled()
+    End Sub
+
+    Private Sub InitialiseDialog()
+        ucrReceiverStation.Selector = ucrSelectorImportToCMSAF
+        ucrReceiverDate.Selector = ucrSelectorImportToCMSAF
+        ucrReceiverLongitude.Selector = ucrSelectorImportToCMSAF
+        ucrReceiverLatitude.Selector = ucrSelectorImportToCMSAF
+        ucrReceiverElement.Selector = ucrSelectorImportToCMSAF
+
+        ucrReceiverStation.SetParameter(New RParameter("station_name ", 0))
+        ucrReceiverStation.SetParameterIsRFunction()
+        ucrReceiverStation.SetClimaticType("station")
+        ucrReceiverStation.bAutoFill = True
+
+        ucrReceiverDate.SetParameter(New RParameter("date", 1))
+        ucrReceiverDate.SetParameterIsRFunction()
+        ucrReceiverDate.SetClimaticType("date")
+        ucrReceiverDate.bAutoFill = True
+
+        ucrReceiverLongitude.SetParameter(New RParameter("lon", 2))
+        ucrReceiverLongitude.SetParameterIsRFunction()
+
+        ucrReceiverLatitude.SetParameter(New RParameter("lat", 3))
+        ucrReceiverLatitude.SetParameterIsRFunction()
+
+        ucrReceiverElement.SetParameter(New RParameter("SIS", 4))
+        ucrReceiverElement.SetParameterIsRFunction()
+
+        ucrInputExportFile.SetParameter(New RParameter("file", 1))
+        ucrInputExportFile.IsReadOnly = True
+
+    End Sub
+
+    Private Sub SetDefaults()
+        Dim strSIS As String = "SIS"
+
+        clsAsDataFrameFunction = New RFunction
+        clsAtrributesFunction = New RFunction
+        clsExportFunction = New RFunction
+
+        clsAssignOperator = New ROperator
+
+        ucrBase.clsRsyntax.ClearCodes()
+        ucrReceiverElement.SetMeAsReceiver()
+        ucrSelectorImportToCMSAF.Reset()
+        ucrInputExportFile.SetName("")
+
+        clsAsDataFrameFunction.SetRCommand("as.data.frame")
+        clsAsDataFrameFunction.SetAssignTo("x")
+
+        clsAtrributesFunction.SetRCommand("attr")
+        clsAtrributesFunction.AddParameter("x", clsRFunctionParameter:=clsAsDataFrameFunction, bIncludeArgumentName:=False, iPosition:=0)
+        clsAtrributesFunction.AddParameter("element_name", Chr(34) & "element_name" & Chr(34), bIncludeArgumentName:=False, iPosition:=1)
+
+        clsAssignOperator.SetOperation("<-")
+        clsAssignOperator.AddParameter("left", clsRFunctionParameter:=clsAtrributesFunction, iPosition:=0)
+        clsAssignOperator.AddParameter("right", strParameterValue:=Chr(34) & strSIS & Chr(34), iPosition:=1)
+
+        clsExportFunction.SetPackageName("rio")
+        clsExportFunction.SetRCommand("export")
+        clsExportFunction.AddParameter("x", clsRFunctionParameter:=clsAsDataFrameFunction, bIncludeArgumentName:=False, iPosition:=0)
+
+        ucrBase.clsRsyntax.AddToBeforeCodes(clsAsDataFrameFunction, iPosition:=0)
+        ucrBase.clsRsyntax.AddToBeforeCodes(clsAssignOperator, iPosition:=1)
+        ucrBase.clsRsyntax.SetBaseRFunction(clsExportFunction)
+    End Sub
+
+    Private Sub SetRCodeForControls(bRese As Boolean)
+        ucrReceiverStation.SetRCode(clsAsDataFrameFunction, bReset)
+        ucrReceiverDate.SetRCode(clsAsDataFrameFunction, bReset)
+        ucrReceiverLongitude.SetRCode(clsAsDataFrameFunction, bReset)
+        ucrReceiverLatitude.SetRCode(clsAsDataFrameFunction, bReset)
+        ucrReceiverElement.SetRCode(clsAsDataFrameFunction, bReset)
+        ucrInputExportFile.SetRCode(clsExportFunction, bReset)
+    End Sub
+
+    Private Sub cmdBrowse_Click(sender As Object, e As EventArgs) Handles cmdBrowse.Click
+        Dim dlgSave As New SaveFileDialog
+
+        dlgSave.Title = "Export R Objects"
+        dlgSave.InitialDirectory = frmMain.clsInstatOptions.strWorkingDirectory
+        dlgSave.Filter = "Serialized R Objects (*.rds)|*.rds"
+        If dlgSave.ShowDialog = DialogResult.OK Then
+            If dlgSave.FileName <> "" Then
+                ucrInputExportFile.SetName(Path.GetFullPath(dlgSave.FileName).ToString.Replace("\", "/"))
+            End If
+        End If
+    End Sub
+
+    Private Sub TestOkEnabled()
+
+    End Sub
+
+    Private Sub ucrBase_ClickReset(sender As Object, e As EventArgs) Handles ucrBase.ClickReset
+        SetDefaults()
+        SetRCodeForControls(True)
+        TestOkEnabled()
+    End Sub
 End Class
