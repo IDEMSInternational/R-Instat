dead74f210af452f3967be07f5a3ce02ea4a9e0d
jackykemboi <jackykemboi@gmail.com>
2018-08-02 11:44:45 +0300

    Adding extension .dbf

--- instat/dlgImportShapeFiles.vb
+++ instat/dlgImportShapeFiles.vb
@@ -67,7 +67,7 @@ Public Class dlgImportShapeFiles
         Dim strTemp As String = ""
 
         Using dlgOpen As New OpenFileDialog
-            dlgOpen.Filter = "All Shape files|*.shp|Shape files|*.shp"
+            dlgOpen.Filter = "All Shape files|*.shp;*.dbf"
             dlgOpen.Title = "Import Shape File"
 
             If bFromLibrary Then
@@ -85,7 +85,7 @@ Public Class dlgImportShapeFiles
                     strFilePath = dlgOpen.FileName
                     strFileExt = Path.GetExtension(strFilePath)
                     ucrInputFilePath.SetName(Replace(strFilePath, "\", "/"))
-                    If strFileExt = ".shp" Then
+                    If strFileExt = ".shp" OrElse strFileExt = ".dbf" Then
                         ucrSaveDataframeName.SetName(frmMain.clsRLink.MakeValidText(strFileName))
                         clsReadShapeFiles.AddParameter("dsn", Chr(34) & Replace(strFilePath, "\", "/") & Chr(34))
                     End If
