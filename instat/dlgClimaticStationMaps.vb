95a351ca19ab6c9c0473869c0f9a22b35dcf9010
jackykemboi <jackykemboi@gmail.com>
2018-09-28 12:22:47 +0300

    Adding Help ID

--- instat/dlgClimaticStationMaps.vb
+++ instat/dlgClimaticStationMaps.vb
@@ -53,6 +53,7 @@ Public Class dlgClimaticStationMaps
     End Sub
 
     Private Sub InitialiseDialog()
+        ucrBase.iHelpTopicID = 558
         ucrBase.clsRsyntax.iCallType = 3
         ucrBase.clsRsyntax.bExcludeAssignedFunctionOutput = False
 
