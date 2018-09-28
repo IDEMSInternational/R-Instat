e112daedb912fb2d882dfdf1fdeefa8b6054ab92
patowhiz <conlooptechnologies@gmail.com>
2018-09-28 11:54:12 +0300

    Changed the command control and other modifications

--- instat/dlgNewDataFrame.vb
+++ instat/dlgNewDataFrame.vb
@@ -25,6 +25,9 @@ Public Class dlgNewDataFrame
     Private Sub dlgNewDataFrame_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         autoTranslate(Me)
         If bFirstLoad Then
+            ucrInputCommand.txtInput.WordWrap = False
+            ucrInputCommand.txtInput.ScrollBars = ScrollBars.Both
+            'ucrInputCommand.txtInput.Multiline = True
             InitialiseDialog()
             bFirstLoad = False
         End If
@@ -85,12 +88,11 @@ Public Class dlgNewDataFrame
 
         'reset the controls
         ucrNewDFName.Reset()
-        txtCommand.Text = "data.frame(data=matrix(data=NA, nrow=10, ncol=2))"
-        'empty and create 6 default rows
+        ucrInputCommand.SetText("data.frame(data=matrix(data=NA, nrow=10, ncol=2))")
+        'empty and create 6 (+ 1) default rows
         dataGridView.Rows.Clear()
-        For i As Integer = 1 To 6
-            dataGridView.Rows.Add()
-        Next
+        dataGridView.Rows.Add(6)
+
 
         rdoConstruct.Checked = True
         rdoRandom.Enabled = False 'TODO remove this later
@@ -101,6 +103,7 @@ Public Class dlgNewDataFrame
             'validate the datagrid rows 
             ucrBase.OKEnabled(False)
             If ucrNewDFName.IsComplete Then
+                Dim bDuplicate As Boolean
                 Dim inputValidation As New ucrInput 'temporarily use validation functions for ucrInput
                 inputValidation.SetValidationTypeAsRVariable()
                 For Each row As DataGridViewRow In dataGridView.Rows
@@ -111,6 +114,23 @@ Public Class dlgNewDataFrame
                             ucrBase.OKEnabled(False)
                             Exit For ' if not valid text then exit for
                         End If
+
+                        'check for duplicates
+                        bDuplicate = False
+                        For i As Integer = dataGridView.Rows.IndexOf(row) - 1 To 0 Step -1
+                            If dataGridView.Rows(i).Cells("colName").Value = row.Cells("colName").Value Then
+                                bDuplicate = True
+                                MessageBox.Show(Me, "Duplicate column names are not allowed", "Construct", MessageBoxButtons.OK)
+                                Exit For
+                            End If
+                        Next
+
+                        If bDuplicate Then
+                            row.Selected = True
+                            ucrBase.OKEnabled(False)
+                            Exit For
+                        End If
+
                     End If
 
                     'if both the column name and expression exist then enable
@@ -122,7 +142,7 @@ Public Class dlgNewDataFrame
 
         ElseIf rdoCommand.Checked Then
             'enable if there is text in the input textbox
-            ucrBase.OKEnabled(ucrNewDFName.IsComplete AndAlso txtCommand.TextLength > 0)
+            ucrBase.OKEnabled(ucrNewDFName.IsComplete AndAlso Not ucrInputCommand.IsEmpty)
         ElseIf rdoRandom.Checked Then
             'TODO
             ucrBase.OKEnabled(False)
@@ -156,7 +176,7 @@ Public Class dlgNewDataFrame
         If rdoConstruct.Checked Then
             ucrNudCols.Visible = False
             ucrNudRows.Visible = False
-            txtCommand.Visible = False
+            ucrInputCommand.Visible = False
             dataGridView.Visible = True
             lblCommand.Visible = True
             btnExample.Visible = False
@@ -165,19 +185,19 @@ Public Class dlgNewDataFrame
         ElseIf rdoCommand.Checked Then
             ucrNudCols.Visible = False
             ucrNudRows.Visible = False
-            txtCommand.Visible = True
+            ucrInputCommand.Visible = True
             dataGridView.Visible = False
             lblCommand.Visible = True
             btnExample.Visible = True
             lblCommand.Text = "Command:"
-            ucrBase.clsRsyntax.SetCommandString(txtCommand.Text)
+            ucrBase.clsRsyntax.SetCommandString(ucrInputCommand.GetText())
             ucrBase.clsRsyntax.SetAssignTo(ucrNewDFName.GetText(), strTempDataframe:=ucrNewDFName.GetText())
         ElseIf rdoRandom.Checked Then
             'TODO 
         ElseIf rdoEmpty.Checked Then
             ucrNudCols.Visible = True
             ucrNudRows.Visible = True
-            txtCommand.Visible = False
+            ucrInputCommand.Visible = False
             dataGridView.Visible = False
             lblCommand.Visible = False
             btnExample.Visible = False
@@ -192,12 +212,13 @@ Public Class dlgNewDataFrame
         End If
     End Sub
 
-    Private Sub txtCommandAndDataGridView_ValueChanged(sender As Object, e As EventArgs) Handles txtCommand.TextChanged, dataGridView.CellValueChanged
+    Private Sub ucrInputCommand_ContentsChanged() Handles ucrInputCommand.ContentsChanged
+        ucrBase.clsRsyntax.SetCommandString(ucrInputCommand.GetText())
         TestOKEnabled()
     End Sub
 
-    Private Sub txtCommand_TextChanged(sender As Object, e As EventArgs) Handles txtCommand.TextChanged
-        ucrBase.clsRsyntax.SetCommandString(txtCommand.Text)
+    Private Sub dataGridView_ValueChanged(sender As Object, e As EventArgs) Handles dataGridView.CellValueChanged
+        TestOKEnabled()
     End Sub
 
     Private Sub dataGridView_CellEndEdit(sender As Object, e As DataGridViewCellEventArgs) Handles dataGridView.CellEndEdit
@@ -219,39 +240,13 @@ Public Class dlgNewDataFrame
         Next
     End Sub
 
-    Private Sub mnuItemCut_Click(sender As Object, e As EventArgs) Handles mnuItemCut.Click
-        txtCommand.Cut()
-    End Sub
-
-    Private Sub mnuItemCopy_Click(sender As Object, e As EventArgs) Handles mnuItemCopy.Click
-        txtCommand.Copy()
-    End Sub
-
-    Private Sub mnuItemPaste_Click(sender As Object, e As EventArgs) Handles mnuItemPaste.Click
-        txtCommand.Paste()
-    End Sub
-
-    Private Sub mnuItemUndo_Click(sender As Object, e As EventArgs) Handles mnuItemUndo.Click
-        'Determine if last operation can be undone in text box.   
-        If txtCommand.CanUndo Then
-            'Undo the last operation.
-            txtCommand.Undo()
-            'Clear the undo buffer to prevent last action from being redone.
-            txtCommand.ClearUndo()
-        End If
-    End Sub
-
-    Private Sub SelectAllToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SelectAllToolStripMenuItem.Click
-        txtCommand.SelectAll()
-    End Sub
-
     Private Sub btnExample_Click(sender As Object, e As EventArgs) Handles btnExample.Click
         'shows a popup that displays the example commands
         Dim frm As New Form
         Dim lstView As New ListView
         frm.ShowInTaskbar = False
         frm.FormBorderStyle = FormBorderStyle.None
-        frm.Size = New Size(txtCommand.Width, txtCommand.Height)
+        frm.Size = New Size(ucrInputCommand.Width, ucrInputCommand.Height)
         frm.Controls.Add(lstView)
 
         'Set the listview properties
@@ -265,7 +260,7 @@ Public Class dlgNewDataFrame
         lstView.Columns.Add("Comment", 200)
 
         lstView.Items.Add(New ListViewItem({"data.frame()", "Empty data frame"}))
-        lstView.Items.Add(New ListViewItem({"data.frame(data = matrix(data = NA, nrow = 10, ncol = 2)", "10 rows and 2 columns filled with missing values"}))
+        lstView.Items.Add(New ListViewItem({"data.frame(data = matrix(data = NA, nrow = 10, ncol = 2))", "10 rows and 2 columns filled with missing values"}))
         lstView.Items.Add(New ListViewItem({"data.frame(x=1:30, y=rnorm(30,mean = 100, sd = 15), z = runif(30, min = 10, max = 30))", " "}))
         lstView.Items.Add(New ListViewItem({"data.frame( block = gl(4,3), treat = c(""C"",""A"",""B"",""B"",""C"",""A"",""A"",""B"",""C"",""A"",""C"",""B""), yield = c(74, 68, 50,62,68,57,70,56,83, 67, 67,59))", " "}))
         'lstView.Items.Add(New ListViewItem({"wakefield::r_data_theme(n = 100, data_theme = ""the_works"")", " "}))
@@ -275,12 +270,12 @@ Public Class dlgNewDataFrame
         AddHandler lstView.LostFocus, Sub()
                                           frm.Close()
                                       End Sub
-        AddHandler lstView.Click, Sub()
-                                      If lstView.SelectedItems.Count > 0 Then
-                                          txtCommand.Text = lstView.SelectedItems.Item(0).SubItems(0).Text
-                                          frm.Close()
-                                      End If
-                                  End Sub
+        AddHandler lstView.DoubleClick, Sub()
+                                            If lstView.SelectedItems.Count > 0 Then
+                                                ucrInputCommand.SetText(lstView.SelectedItems.Item(0).SubItems(0).Text)
+                                                frm.Close()
+                                            End If
+                                        End Sub
 
         Dim ctlpos As Point = btnExample.PointToScreen(New Point(0, 0)) 'Point.Empty is not function so use Point(0, 0)
         frm.StartPosition = FormStartPosition.Manual 'set it to manual
