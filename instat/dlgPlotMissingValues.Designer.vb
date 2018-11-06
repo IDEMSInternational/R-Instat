496b35afc276e273508ccd1c2d51022d479cce6b
Ogik99 <ogikachieng@outlook.com>
2018-11-06 11:02:48 +0300

    minor edit

---
+++ instat/dlgPlotMissingValues.Designer.vb
@@ -0,0 +1,216 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class dlgPlotMissingValues
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.UcrRadio1 = New instat.ucrRadio()
+        Me.rdoVisMiss = New instat.ucrRadio()
+        Me.rdoVisGuess = New instat.ucrRadio()
+        Me.UcrRadio4 = New instat.ucrRadio()
+        Me.UcrRadio5 = New instat.ucrRadio()
+        Me.UcrRadio6 = New instat.ucrRadio()
+        Me.UcrPanel1 = New instat.UcrPanel()
+        Me.UcrRadio7 = New instat.ucrRadio()
+        Me.UcrRadio8 = New instat.ucrRadio()
+        Me.UcrRadio9 = New instat.ucrRadio()
+        Me.UcrSelector1 = New instat.ucrSelector()
+        Me.ucrBase = New instat.ucrButtons()
+        Me.UcrSaveGraph1 = New instat.ucrSaveGraph()
+        Me.UcrPanel2 = New instat.UcrPanel()
+        Me.rdoWholeDataFrame = New instat.ucrRadio()
+        Me.UcrRadio11 = New instat.ucrRadio()
+        Me.rdoVisDat = New instat.ucrButtons()
+        Me.SuspendLayout()
+        '
+        'UcrRadio1
+        '
+        Me.UcrRadio1.Location = New System.Drawing.Point(12, 12)
+        Me.UcrRadio1.Name = "UcrRadio1"
+        Me.UcrRadio1.Size = New System.Drawing.Size(100, 20)
+        Me.UcrRadio1.TabIndex = 0
+        '
+        'rdoVisMiss
+        '
+        Me.rdoVisMiss.Location = New System.Drawing.Point(97, 12)
+        Me.rdoVisMiss.Name = "rdoVisMiss"
+        Me.rdoVisMiss.Size = New System.Drawing.Size(100, 20)
+        Me.rdoVisMiss.TabIndex = 1
+        '
+        'rdoVisGuess
+        '
+        Me.rdoVisGuess.Location = New System.Drawing.Point(189, 12)
+        Me.rdoVisGuess.Name = "rdoVisGuess"
+        Me.rdoVisGuess.Size = New System.Drawing.Size(100, 20)
+        Me.rdoVisGuess.TabIndex = 2
+        '
+        'UcrRadio4
+        '
+        Me.UcrRadio4.Location = New System.Drawing.Point(12, 38)
+        Me.UcrRadio4.Name = "UcrRadio4"
+        Me.UcrRadio4.Size = New System.Drawing.Size(100, 20)
+        Me.UcrRadio4.TabIndex = 3
+        '
+        'UcrRadio5
+        '
+        Me.UcrRadio5.Location = New System.Drawing.Point(97, 38)
+        Me.UcrRadio5.Name = "UcrRadio5"
+        Me.UcrRadio5.Size = New System.Drawing.Size(100, 20)
+        Me.UcrRadio5.TabIndex = 4
+        '
+        'UcrRadio6
+        '
+        Me.UcrRadio6.Location = New System.Drawing.Point(189, 38)
+        Me.UcrRadio6.Name = "UcrRadio6"
+        Me.UcrRadio6.Size = New System.Drawing.Size(100, 20)
+        Me.UcrRadio6.TabIndex = 5
+        '
+        'UcrPanel1
+        '
+        Me.UcrPanel1.Location = New System.Drawing.Point(12, -2)
+        Me.UcrPanel1.Name = "UcrPanel1"
+        Me.UcrPanel1.Size = New System.Drawing.Size(264, 101)
+        Me.UcrPanel1.TabIndex = 8
+        '
+        'UcrRadio7
+        '
+        Me.UcrRadio7.Location = New System.Drawing.Point(12, 64)
+        Me.UcrRadio7.Name = "UcrRadio7"
+        Me.UcrRadio7.Size = New System.Drawing.Size(100, 20)
+        Me.UcrRadio7.TabIndex = 9
+        '
+        'UcrRadio8
+        '
+        Me.UcrRadio8.Location = New System.Drawing.Point(97, 64)
+        Me.UcrRadio8.Name = "UcrRadio8"
+        Me.UcrRadio8.Size = New System.Drawing.Size(100, 20)
+        Me.UcrRadio8.TabIndex = 10
+        '
+        'UcrRadio9
+        '
+        Me.UcrRadio9.Location = New System.Drawing.Point(189, 64)
+        Me.UcrRadio9.Name = "UcrRadio9"
+        Me.UcrRadio9.Size = New System.Drawing.Size(100, 20)
+        Me.UcrRadio9.TabIndex = 11
+        '
+        'UcrSelector1
+        '
+        Me.UcrSelector1.bShowHiddenColumns = False
+        Me.UcrSelector1.Location = New System.Drawing.Point(13, 106)
+        Me.UcrSelector1.Margin = New System.Windows.Forms.Padding(0)
+        Me.UcrSelector1.Name = "UcrSelector1"
+        Me.UcrSelector1.Size = New System.Drawing.Size(148, 130)
+        Me.UcrSelector1.TabIndex = 12
+        '
+        'ucrBase
+        '
+        Me.ucrBase.Location = New System.Drawing.Point(12, 293)
+        Me.ucrBase.Name = "ucrBase"
+        Me.ucrBase.Size = New System.Drawing.Size(447, 52)
+        Me.ucrBase.TabIndex = 13
+        '
+        'UcrSaveGraph1
+        '
+        Me.UcrSaveGraph1.Location = New System.Drawing.Point(26, 254)
+        Me.UcrSaveGraph1.Name = "UcrSaveGraph1"
+        Me.UcrSaveGraph1.Size = New System.Drawing.Size(86, 20)
+        Me.UcrSaveGraph1.TabIndex = 14
+        '
+        'UcrPanel2
+        '
+        Me.UcrPanel2.Location = New System.Drawing.Point(250, 106)
+        Me.UcrPanel2.Name = "UcrPanel2"
+        Me.UcrPanel2.Size = New System.Drawing.Size(187, 106)
+        Me.UcrPanel2.TabIndex = 15
+        '
+        'rdoWholeDataFrame
+        '
+        Me.rdoWholeDataFrame.Location = New System.Drawing.Point(260, 117)
+        Me.rdoWholeDataFrame.Name = "rdoWholeDataFrame"
+        Me.rdoWholeDataFrame.Size = New System.Drawing.Size(100, 20)
+        Me.rdoWholeDataFrame.TabIndex = 16
+        '
+        'UcrRadio11
+        '
+        Me.UcrRadio11.Location = New System.Drawing.Point(260, 143)
+        Me.UcrRadio11.Name = "UcrRadio11"
+        Me.UcrRadio11.Size = New System.Drawing.Size(100, 20)
+        Me.UcrRadio11.TabIndex = 17
+        '
+        'rdoVisDat
+        '
+        Me.rdoVisDat.Location = New System.Drawing.Point(13, 293)
+        Me.rdoVisDat.Name = "rdoVisDat"
+        Me.rdoVisDat.Size = New System.Drawing.Size(410, 52)
+        Me.rdoVisDat.TabIndex = 18
+        '
+        'dlgPlotMissingValues
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(502, 357)
+        Me.Controls.Add(Me.rdoVisDat)
+        Me.Controls.Add(Me.UcrRadio11)
+        Me.Controls.Add(Me.rdoWholeDataFrame)
+        Me.Controls.Add(Me.UcrPanel2)
+        Me.Controls.Add(Me.UcrSaveGraph1)
+        Me.Controls.Add(Me.ucrBase)
+        Me.Controls.Add(Me.UcrSelector1)
+        Me.Controls.Add(Me.UcrRadio9)
+        Me.Controls.Add(Me.UcrRadio8)
+        Me.Controls.Add(Me.UcrRadio7)
+        Me.Controls.Add(Me.UcrRadio6)
+        Me.Controls.Add(Me.UcrRadio5)
+        Me.Controls.Add(Me.UcrRadio4)
+        Me.Controls.Add(Me.rdoVisGuess)
+        Me.Controls.Add(Me.rdoVisMiss)
+        Me.Controls.Add(Me.UcrRadio1)
+        Me.Controls.Add(Me.UcrPanel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "dlgPlotMissingValues"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Plot Missing Values"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents UcrRadio1 As ucrRadio
+    Friend WithEvents rdoVisMiss As ucrRadio
+    Friend WithEvents rdoVisGuess As ucrRadio
+    Friend WithEvents UcrRadio4 As ucrRadio
+    Friend WithEvents UcrRadio5 As ucrRadio
+    Friend WithEvents UcrRadio6 As ucrRadio
+    Friend WithEvents UcrPanel1 As UcrPanel
+    Friend WithEvents UcrRadio7 As ucrRadio
+    Friend WithEvents UcrRadio8 As ucrRadio
+    Friend WithEvents UcrRadio9 As ucrRadio
+    Friend WithEvents UcrSelector1 As ucrSelector
+    Friend WithEvents ucrBase As ucrButtons
+    Friend WithEvents UcrSaveGraph1 As ucrSaveGraph
+    Friend WithEvents UcrPanel2 As UcrPanel
+    Friend WithEvents rdoWholeDataFrame As ucrRadio
+    Friend WithEvents UcrRadio11 As ucrRadio
+    Friend WithEvents rdoVisDat As ucrButtons
+End Class
---
+++ instat/dlgPlotMissingValues.vb
@@ -0,0 +1,61 @@
+﻿' R- Instat
+' Copyright (C) 2015-2017
+'
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+'
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License 
+' along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Imports instat.Translations
+Public Class dlgPlotMissingValues
+    Private bFirstLoad As Boolean = True
+    Private bReset As Boolean = True
+    Private bResetSubdialog As Boolean = False
+    Private clsPlotMissingValues As New RFunction
+    Public strDefaultDataFrame As String = ""
+    Public strDefaultColumns() As String = Nothing
+    Private Sub dlgPlotMissingValues_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        autoTranslate(Me)
+        If bFirstLoad Then
+            InitialiseDialog()
+            bFirstLoad = False
+        End If
+        If bReset Then
+            SetDefaults()
+        End If
+        SetRCodeForControls(bReset)
+        SetDefaultColumn()
+        bReset = False
+        TestOkEnabled()
+    End Sub
+
+    Private Sub InitialiseDialog()
+
+    End Sub
+
+    Private Sub SetDefaults()
+
+    End Sub
+    Private Sub SetRCodeForControls(bReset)
+
+    End Sub
+
+    Public Sub TestOkEnabled()
+
+    End Sub
+    Private Sub UcrSelector1_Load(sender As Object, e As EventArgs) Handles UcrSelector1.Load
+
+    End Sub
+
+    Private Sub rdoVisGuess_Load(sender As Object, e As EventArgs) Handles rdoVisGuess.Load
+
+    End Sub
+End Class
