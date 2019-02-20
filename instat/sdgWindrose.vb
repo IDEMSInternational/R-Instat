32a68b64d63624db4c1b7bc2bfe2073b5825bbc6
maxwell fundi <maxwellfundi@live.com>
2019-02-20 12:42:14 +0300

    Adding sequential color pallete

--- instat/sdgWindrose.vb
+++ instat/sdgWindrose.vb
@@ -25,6 +25,8 @@ Public Class sdgWindrose
 
     Public Sub InitialiseControls()
         Dim dctThemePairs As New Dictionary(Of String, String)
+        Dim dctSequatailPairs As New Dictionary(Of String, String)
+
 
         ucrNudNoOfDirections.SetParameter(New RParameter("n_directions", 3))
         ucrNudNoOfDirections.SetRDefault(12)
@@ -49,6 +51,26 @@ Public Class sdgWindrose
         ucrInputTheme.SetItems(dctThemePairs)
         ucrInputTheme.SetDropDownStyleAsNonEditable()
         ucrInputSpeedCuts.AddToLinkedControls(ucrNudNoOfSpeeds, {"NA"}, bNewLinkedAddRemoveParameter:=True, bNewLinkedDisabledIfParameterMissing:=True)
+
+        dctSequatailPairs.Add("Blues", Chr(34) & "Blues" & Chr(34))
+        dctSequatailPairs.Add("Greens", Chr(34) & "Greens" & Chr(34))
+        dctSequatailPairs.Add("Greys", Chr(34) & "Greys" & Chr(34))
+        dctSequatailPairs.Add("Oranges", Chr(34) & "Oranges" & Chr(34))
+        dctSequatailPairs.Add("Purples", Chr(34) & "Purples" & Chr(34))
+        dctSequatailPairs.Add("Reds", Chr(34) & "Reds" & Chr(34))
+        dctSequatailPairs.Add("BuGn", Chr(34) & "BuGn" & Chr(34))
+        dctSequatailPairs.Add("BuPu", Chr(34) & "BuPu" & Chr(34))
+        dctSequatailPairs.Add("GnBu", Chr(34) & "GnBu" & Chr(34))
+        dctSequatailPairs.Add("OrRd", Chr(34) & "OrRd" & Chr(34))
+        dctSequatailPairs.Add("PuBu", Chr(34) & "PuBu" & Chr(34))
+        dctSequatailPairs.Add("PuBuGn", Chr(34) & "PuBuGn" & Chr(34))
+        dctSequatailPairs.Add("PuRd", Chr(34) & "PuRd" & Chr(34))
+        dctSequatailPairs.Add("RdPu", Chr(34) & "RdPu" & Chr(34))
+        dctSequatailPairs.Add("YlGn", Chr(34) & "YlGn" & Chr(34))
+        dctSequatailPairs.Add("YlGnBu", Chr(34) & "YlGnBu" & Chr(34))
+        dctSequatailPairs.Add("YlOrBr", Chr(34) & "YlOrBr" & Chr(34))
+        dctSequatailPairs.Add("YlOrRd", Chr(34) & "YlOrRd" & Chr(34))
+        ucrInputSequential.SetItems(dctSequatailPairs)
 
         bControlsInitialised = True
     End Sub
