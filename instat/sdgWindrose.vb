0bbe9e338dd7dee8b004a805fa072cba1679f471
maxwell fundi <maxwellfundi@live.com>
2019-02-21 15:53:03 +0300

    More additons to windrose

--- instat/sdgWindrose.vb
+++ instat/sdgWindrose.vb
@@ -35,14 +35,6 @@ Public Class sdgWindrose
         ucrPnlColourPalette.AddRadioButton(rdoQualitative)
         ucrPnlColourPalette.SetParameter(New RParameter("col_pal"))
 
-        ucrPnlColourPalette.AddToLinkedControls(ucrInputSequential, {rdoSequential}, bNewLinkedAddRemoveParameter:=False, bNewLinkedChangeParameterValue:=True)
-        ucrPnlColourPalette.AddParameterValuesCondition(rdoSequential, "col_pal", {Chr(34) & "Blues" & Chr(34), Chr(34) & "Blues" & Chr(34), Chr(34) & "Greens" & Chr(34), Chr(34) & "Greys" & Chr(34), Chr(34) & "Oranges" & Chr(34), Chr(34) & "Purples" & Chr(34), Chr(34) & "Reds" & Chr(34), Chr(34) & "BuGn" & Chr(34), Chr(34) & "BuPu" & Chr(34), Chr(34) & "GnBu" & Chr(34), Chr(34) & "OrRd" & Chr(34), Chr(34) & "PuBu" & Chr(34), Chr(34) & "PuBuGn" & Chr(34), Chr(34) & "PuRd" & Chr(34), Chr(34) & "RdPu" & Chr(34), Chr(34) & "YlGn" & Chr(34), Chr(34) & "YlGnBu" & Chr(34), Chr(34) & "YlOrBr" & Chr(34), Chr(34) & "YlOrRd" & Chr(34)}, True)
-
-        ucrPnlColourPalette.AddToLinkedControls(ucrInputSequential, {rdoDiverging}, bNewLinkedAddRemoveParameter:=False, bNewLinkedChangeParameterValue:=True)
-        ucrPnlColourPalette.AddParameterValuesCondition(rdoDiverging, "col_pal", {Chr(34) & "Spectral" & Chr(34), Chr(34) & "BrBG" & Chr(34), Chr(34) & "PiYG" & Chr(34), Chr(34) & "PRGn" & Chr(34), Chr(34) & "PuOr" & Chr(34), Chr(34) & "RdBu" & Chr(34), Chr(34) & "RdGy" & Chr(34), Chr(34) & "RdYlBu" & Chr(34), Chr(34) & "RdYlGn" & Chr(34)}, True)
-
-        ucrPnlColourPalette.AddToLinkedControls(ucrInputSequential, {rdoQualitative}, bNewLinkedAddRemoveParameter:=False, bNewLinkedChangeParameterValue:=True)
-        ucrPnlColourPalette.AddParameterValuesCondition(rdoQualitative, "col_pal", {Chr(34) & "Accent" & Chr(34), Chr(34) & "Dark2" & Chr(34), Chr(34) & "Pastel1" & Chr(34), Chr(34) & "Pastel2" & Chr(34), Chr(34) & "Set1" & Chr(34), Chr(34) & "Set2" & Chr(34), Chr(34) & "Set3" & Chr(34)}, True)
 
         ucrNudNoOfDirections.SetParameter(New RParameter("n_directions", 3))
         ucrNudNoOfDirections.SetRDefault(12)
@@ -56,6 +48,7 @@ Public Class sdgWindrose
         ucrNudCalmWind.SetParameter(New RParameter("calm_wind", 9))
         ucrNudCalmWind.SetRDefault(0)
 
+        ucrInputTheme.SetParameter(New RParameter("col_pal"))
         ucrInputTheme.SetParameter(New RParameter("ggtheme", 7))
         dctThemePairs.Add("grey", Chr(34) & "grey" & Chr(34))
         dctThemePairs.Add("gray", Chr(34) & "gray" & Chr(34))
@@ -68,6 +61,7 @@ Public Class sdgWindrose
         ucrInputTheme.SetDropDownStyleAsNonEditable()
         ucrInputSpeedCuts.AddToLinkedControls(ucrNudNoOfSpeeds, {"NA"}, bNewLinkedAddRemoveParameter:=True, bNewLinkedDisabledIfParameterMissing:=True)
 
+        ucrInputSequential.SetParameter(New RParameter("col_pal"))
         dctSequatailPairs.Add("Blues", Chr(34) & "Blues" & Chr(34))
         dctSequatailPairs.Add("Greens", Chr(34) & "Greens" & Chr(34))
         dctSequatailPairs.Add("Greys", Chr(34) & "Greys" & Chr(34))
@@ -89,6 +83,7 @@ Public Class sdgWindrose
         ucrInputSequential.SetItems(dctSequatailPairs)
         ucrInputSequential.SetDropDownStyleAsNonEditable()
 
+        ucrInputDiverging.SetParameter(New RParameter("col_pal"))
         dctDivergingPairs.Add("Spectral", Chr(34) & "Spectral" & Chr(34))
         dctDivergingPairs.Add("BrBG", Chr(34) & "BrBG" & Chr(34))
         dctDivergingPairs.Add("PiYG", Chr(34) & "PiYG" & Chr(34))
@@ -101,6 +96,7 @@ Public Class sdgWindrose
         ucrInputDiverging.SetItems(dctDivergingPairs)
         ucrInputDiverging.SetDropDownStyleAsNonEditable()
 
+        ucrInputQualitative.SetParameter(New RParameter("col_pal"))
         dctQualititivePairs.Add("Accent", Chr(34) & "Accent" & Chr(34))
         dctQualititivePairs.Add("Dark2", Chr(34) & "Dark2" & Chr(34))
         dctQualititivePairs.Add("Pastel1", Chr(34) & "Pastel1" & Chr(34))
@@ -110,6 +106,11 @@ Public Class sdgWindrose
         dctQualititivePairs.Add("Set3", Chr(34) & "Set3" & Chr(34))
         ucrInputQualitative.SetItems(dctQualititivePairs)
         ucrInputQualitative.SetDropDownStyleAsNonEditable()
+
+        ucrPnlColourPalette.AddParameterValuesCondition(rdoSequential, "col_pal", dctSequatailPairs.Values.ToArray, True)
+        ucrPnlColourPalette.AddParameterValuesCondition(rdoDiverging, "col_pal", dctDivergingPairs.Values.ToArray, True)
+        ucrPnlColourPalette.AddParameterValuesCondition(rdoQualitative, "col_pal", dctQualititivePairs.Values.ToArray, True)
+
         bControlsInitialised = True
     End Sub
 
@@ -119,5 +120,25 @@ Public Class sdgWindrose
         End If
         clsWindRoseFunc = clsNewRFunction
         SetRCode(Me, clsWindRoseFunc, bReset)
+    End Sub
+
+    Private Sub ShowHideCombos()
+        If rdoDiverging.Checked Then
+            ucrInputDiverging.Visible = True
+            ucrInputSequential.Visible = False
+            ucrInputQualitative.Visible = False
+        ElseIf rdoQualitative.Checked Then
+            ucrInputDiverging.Visible = False
+            ucrInputSequential.Visible = False
+            ucrInputQualitative.Visible = True
+        Else
+            ucrInputDiverging.Visible = False
+            ucrInputSequential.Visible = True
+            ucrInputQualitative.Visible = False
+        End If
+    End Sub
+
+    Private Sub ucrPnlColourPalette_ControlValueChanged(ucrChangedControl As ucrCore) Handles ucrPnlColourPalette.ControlValueChanged
+        ShowHideCombos()
     End Sub
 End Class
