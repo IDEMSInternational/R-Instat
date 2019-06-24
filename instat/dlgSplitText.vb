4b4aead418ba39337b601a07a1ca0ad497a1b557
ivanluv <luvtheart93@gmail.com>
2019-06-24 13:06:09 +0300

    Removing split text function from controlcontent changed

--- instat/dlgSplitText.vb
+++ instat/dlgSplitText.vb
@@ -10,6 +10,8 @@
 ' but WITHOUT ANY WARRANTY; without even the implied warranty of
 ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ' GNU General Public License for more details.
+' You should have received a copy of the GNU General Public License 
+' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 
@@ -143,7 +145,6 @@ Public Class dlgSplitText
             ucrBase.clsRsyntax.SetBaseRFunction(clsBinaryColumns)
             clsBinaryColumns.SetAssignTo("Split", strTempDataframe:=ucrSelectorSplitTextColumn.ucrAvailableDataFrames.cboAvailableDataFrames.Text, bAssignToColumnWithoutNames:=True)
         ElseIf rdoTextComponents.Checked Then
-            SplitTextOptions()
             ucrSaveColumn.SetName("Split")
         Else
         End If
