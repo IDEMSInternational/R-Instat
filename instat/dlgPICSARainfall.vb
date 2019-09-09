fa36c32a2b191d3f505cc32249e1449981ce906d
ogikachieng <ogikachieng@outlook.com>
2019-09-09 12:34:28 +0300

    code changes

--- instat/dlgPICSARainfall.vb
+++ instat/dlgPICSARainfall.vb
@@ -42,8 +42,6 @@ Public Class dlgPICSARainfall
     Private clsGeomPoint As New RFunction
     Private clsPointsFunc As New RFunction
     Private clsPointsParam As New RParameter
-    Private clsGeomRug As New RFunction
-    Private clsRugParam As New RParameter
     Private clsYLabsFunction, clsXLabsFunction, clsLabsFunction As RFunction
     Private clsXAxisLabels, clsYAxisLabels As New RFunction
     Private clsPnlBackgroundFunction, clsPnlGridLinesFunction As RFunction
@@ -167,16 +165,6 @@ Public Class dlgPICSARainfall
         clsPointsFunc.AddParameter("size", "3")
         clsPointsFunc.AddParameter("colour", Chr(34) & "red" & Chr(34))
         ucrChkPoints.SetParameter(clsPointsParam, bNewChangeParameterValue:=False, bNewAddRemoveParameter:=True)
-
-        ucrChkRug.SetText("Add Rug Plot")
-        ucrChkRug.AddParameterPresentCondition(True, "rug")
-        ucrChkRug.AddParameterPresentCondition(False, "rug", False)
-        clsGeomRug.SetPackageName("ggplot2")
-        clsGeomRug.SetRCommand("geom_rug")
-        clsRugParam.SetArgumentName("rug")
-        clsRugParam.SetArgument(clsGeomRug)
-        ucrChkRug.SetParameter(clsRugParam, bNewChangeParameterValue:=False, bNewAddRemoveParameter:=True)
-
 
         ucrSave.SetPrefix("PICSA_Rainfall_Graph")
         ucrSave.SetIsComboBox()
@@ -601,7 +589,6 @@ Public Class dlgPICSARainfall
         ucrReceiverFacetBy.SetRCode(clsFacetOperator, bReset)
         ucrSave.SetRCode(clsBaseOperator, bReset)
         ucrChkPoints.SetRCode(clsBaseOperator, bReset)
-        ucrChkRug.SetRCode(clsBaseOperator, bReset)
         ucrVariablesAsFactorForPicsa.SetRCode(clsAsNumeric, bReset)
 
         If bReset Then
