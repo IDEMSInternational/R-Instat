96734263ee1e1606db11e73d1a855fa338d4f3f9
Wycklife Bonyo <bonyowycklife@gmail.com>
2019-10-18 16:47:59 +0300

    Tooltips for Transform keyboard

--- instat/ucrCalculator.vb
+++ instat/ucrCalculator.vb
@@ -66,6 +66,8 @@ Public Class ucrCalculator
         bControlsInitialised = True
         ttCalculator.SetToolTip(cmdRound, "round(x) to round to whole numbers, round(x,2) to round to 2 decimal places, round(x,-2) to round to the nearest 100")
         ttCalculator.SetToolTip(cmdSiginf, "signif(x,3) to round to 3 significant figures")
+        ttCalculator.SetToolTip(cmdLag, "Shift a variable down. For example lag(1:5) = (NA,1,2,3,4); lag(1:5,3) = (NA,NA,NA, 1,2)")
+        ttCalculator.SetToolTip(cmdLead, "Shift a variable up. For example lead(1:5) = (2,3,4,5,NA); lead(1:5;3) = (4,5, NA,NA,NA)")
     End Sub
 
     Public Sub Reset()
@@ -1124,7 +1126,7 @@ Public Class ucrCalculator
 
     Private Sub cmMovMed_Click(sender As Object, e As EventArgs) Handles cmMovMed.Click
         If chkShowArguments.Checked Then
-            ucrReceiverForCalculation.AddToReceiverAtCursorPosition("zoo::rollmedian(x= , k=3)", 6)
+            ucrReceiverForCalculation.AddToReceiverAtCursorPosition("zoo::rollmedian(x= , k=3, fill = if (na.pad) NA, na.pad = FALSE, align = c(""center"", ""left"", ""right""))", 89)
         Else
             ucrReceiverForCalculation.AddToReceiverAtCursorPosition("zoo::rollmedian()", 1)
         End If
