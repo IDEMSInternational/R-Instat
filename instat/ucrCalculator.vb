e26210724bcea8d18acded939e2d90b39c136171
Wycklife Bonyo <bonyowycklife@gmail.com>
2019-10-28 16:58:00 +0300

    Tooltips

--- instat/ucrCalculator.vb
+++ instat/ucrCalculator.vb
@@ -78,6 +78,7 @@ Public Class ucrCalculator
         ttCalculator.SetToolTip(cmdRad, "change from degrees to radians. For example rad(90) = 2*pi * 90/360 = 1.57 ( = pi/2)")
         ttCalculator.SetToolTip(cmdLog, "logarithm to base 10. For example log10(1000) =3 (=10^3)")
         ttCalculator.SetToolTip(cmdTan, " tangent of angle in radians. For example tan(pi/4) = sin(pi/4)/cos(pi/4) = tan(rad(45)) = 1")
+        ttCalculator.SetToolTip(cmdAtan, "angle corresponding to a given tangent (in the range 0 to pi). For example atan(1) = 0..7854 (= pi/4); deg(atan(1)) = 45.")
     End Sub
 
     Public Sub Reset()
