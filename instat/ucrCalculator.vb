f8c3675b5d9876fdfa64024c88d1a5df78cff983
Wycklife Bonyo <bonyowycklife@gmail.com>
2019-10-28 16:51:05 +0300

    Tooltips

--- instat/ucrCalculator.vb
+++ instat/ucrCalculator.vb
@@ -71,7 +71,7 @@ Public Class ucrCalculator
         ttCalculator.SetToolTip(cmdAcos, "angle corresponding to a given cosine (in the range 0 to pi). For example acos(0) = 1.57 = pi/2; deg(acos(-1))=180.")
         ttCalculator.SetToolTip(cmdCeiling, " Integer above the given value. For example ceiling(3.5) = 4; ceiling(-3.5) = -3")
         ttCalculator.SetToolTip(cmdPi, "pi = 3.14159")
-
+        ttCalculator.SetToolTip(cmdLog, "(natural) logarithm. For example log(512) = 6.238; log(512,2) = 9 (log to base 2, i.e. 2 ^ 9 =512)")
     End Sub
 
     Public Sub Reset()
