aec4470a727479cb98fdb5d4cf4ac00e377f43bd
Wycklife Bonyo <bonyowycklife@gmail.com>
2019-10-29 14:27:50 +0300

    Tooltips

--- instat/ucrCalculator.vb
+++ instat/ucrCalculator.vb
@@ -75,6 +75,7 @@ Public Class ucrCalculator
         ttCalculator.SetToolTip(cmdMovMax, "moving (or rolling) maxima. For example rollmax(x=c(3,2,1,4,0) ,3,fill=NA, align=""right"") = (NA,NA, 3,4,4)")
         ttCalculator.SetToolTip(cmdcumsum, "cumulative sums. For example cumsum(c(3,2,1,4,0)) = (3,5,6,10,10)")
         ttCalculator.SetToolTip(cmdCumMean, "cumulative means. For example cummean(c(3,2,1,4,0)) = (3,2.5,2,2.5,2)")
+        ttCalculator.SetToolTip(cmdcummin, "cumulative minima. For example cummin(c(3,2,1,4,0)) = (3,2.,1,1,0)")
 
     End Sub
 
