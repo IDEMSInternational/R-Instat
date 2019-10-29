4e82024771594a5edaa099f4196141e69250c2d1
Wycklife Bonyo <bonyowycklife@gmail.com>
2019-10-29 14:47:10 +0300

    Tooltips

--- instat/ucrCalculator.vb
+++ instat/ucrCalculator.vb
@@ -88,6 +88,7 @@ Public Class ucrCalculator
         ttCalculator.SetToolTip(cmdMRank, " minimum ranks. For example m_rank(c(15,11,13,12,NA,12)) = (5,1,4,2,NA,2)")
         ttCalculator.SetToolTip(cmdNafill, "fills missing values at the start, middle and end. For example na.fill(c(NA,2,NA,4,5,NA),fill=""extend"") = (2,2,3,4,5,5); while fill=c(15,""extend"",NA) = (15,2,3,4,5,NA)")
         ttCalculator.SetToolTip(cmdNaapprox, "inear interpolation of missing values. For example na.approx(c(5,NA,NA,2,2,NA,4,7,NA),maxgap=1,na.rm=FALSE) = (5,NA,NA,2,2,3,4,7,NA)")
+        ttCalculator.SetToolTip(cmdNasplin, "Spline interpolation of missing values. For example na.spline(c(NA,NA,NA,2,2,NA,4,7,NA),maxgap=2,na.rm=FALSE) = (NA,NA,NA,2,2,2.5,4,7,12)")
 
     End Sub
 
