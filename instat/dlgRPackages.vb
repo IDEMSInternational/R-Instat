26e78183dffc2c2c49fff7fd4184fa56ba5e5ea5
ivanluv <luvtheart93@gmail.com>
2020-01-27 16:33:02 +0300

    Adding the R Package dialogue

---
+++ instat/dlgRPackages.vb
@@ -0,0 +1,44 @@
+﻿Imports instat
+
+Public Class dlgRPackages
+    Private bReset As Boolean = True
+    Private bFirstLoad As Boolean = True
+    Private clsInstallPackage As New RFunction
+    Private Sub dlgRPackages_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If bFirstLoad Then
+            InitialiseDialog()
+            bFirstLoad = False
+        End If
+        If bReset Then
+            SetDefaults()
+        End If
+        SetRCodeForControls(bReset)
+        bReset = False
+        TestOkEnabled()
+    End Sub
+    Private Sub InitialiseDialog()
+        ucrInputTextBoxRPackage.SetParameter(New RParameter("package", 1))
+
+    End Sub
+
+    Private Sub SetDefaults()
+        clsInstallPackage = New RFunction
+        clsInstallPackage.SetRCommand("install.packages")
+        ucrBase.clsRsyntax.SetBaseRFunction(clsInstallPackage)
+    End Sub
+    Private Sub SetRCodeForControls(bReset As Boolean)
+        ucrInputTextBoxRPackage.SetRCode(clsInstallPackage, bReset)
+    End Sub
+
+    Private Sub TestOkEnabled()
+        If ucrInputTextBoxRPackage.IsEmpty Then
+            ucrBase.OKEnabled(False)
+        Else
+            ucrBase.OKEnabled(True)
+        End If
+    End Sub
+
+    Private Sub ucrInputTextBoxRPackage_ControlContentsChanged(ucrChangedControl As ucrCore) Handles ucrInputTextBoxRPackage.ControlContentsChanged
+        TestOkEnabled()
+    End Sub
+End Class
