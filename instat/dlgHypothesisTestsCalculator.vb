b25dccc299f1637e6e6f83cd66b5955609fdeeec
Wycklife Bonyo <bonyowycklife@gmail.com>
2020-02-04 18:56:44 +0300

    More addition

--- instat/dlgHypothesisTestsCalculator.vb
+++ instat/dlgHypothesisTestsCalculator.vb
@@ -48,7 +48,7 @@ Public Class dlgHypothesisTestsCalculator
         ucrSaveResult.SetAssignToIfUncheckedValue("Last_Test")
         ucrSaveResult.SetDataFrameSelector(ucrSelectorColumn.ucrAvailableDataFrames)
 
-        ucrInputComboRPackage.SetItems({"Stats1", "Stats2", "Agricolae"})
+        ucrInputComboRPackage.SetItems({"Stats1", "Stats2", "Agricolae", "Verify"})
         ucrInputComboRPackage.SetDropDownStyleAsNonEditable()
         'Tooltips for conf & and Alt Buttons
         tpConf.SetToolTip(cmdConf, "The confidence level can be changed for some tests to 0.9 or 0.99 etc")
