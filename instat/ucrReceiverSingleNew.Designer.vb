ef29c1717b31da8c9e8cb1d633cf8e2670ce806c
patowhiz <conlooptechnologies@gmail.com>
2020-03-03 09:11:59 +0300

    more changes

--- instat/ucrReceiverSingleNew.Designer.vb
+++ instat/ucrReceiverSingleNew.Designer.vb
@@ -22,8 +22,27 @@ Partial Class ucrReceiverSingleNew
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.cboBox = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'cboBox
+        '
+        Me.cboBox.FormattingEnabled = True
+        Me.cboBox.Location = New System.Drawing.Point(3, 3)
+        Me.cboBox.Name = "cboBox"
+        Me.cboBox.Size = New System.Drawing.Size(144, 21)
+        Me.cboBox.TabIndex = 0
+        '
+        'ucrReceiverSingleNew
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.cboBox)
+        Me.Name = "ucrReceiverSingleNew"
+        Me.Size = New System.Drawing.Size(150, 29)
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents cboBox As ComboBox
 End Class
