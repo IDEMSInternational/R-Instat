850ef3e65713f87c71a12416031479ca75063d38
patowhiz <conlooptechnologies@gmail.com>
2020-03-03 19:39:45 +0300

    more changes

--- instat/ucrReceiverSingleNew.Designer.vb
+++ instat/ucrReceiverSingleNew.Designer.vb
@@ -1,6 +1,6 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class ucrReceiverSingleNew
-    Inherits instat.ucrReceiver
+    Inherits instat.ucrCore
 
     'UserControl overrides dispose to clean up the component list.
     <System.Diagnostics.DebuggerNonUserCode()>
