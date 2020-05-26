15170347ba0b0d79057e4c6b38ea3076c60cf38e
Wycklife <bonyowycklife@gmail.com>
2020-05-26 12:25:20 +0300

    More addition to the  keys

--- instat/dlgHypothesisTestsCalculator.vb
+++ instat/dlgHypothesisTestsCalculator.vb
@@ -775,4 +775,12 @@ Public Class dlgHypothesisTestsCalculator
             ucrReceiverForTestColumn.AddToReceiverAtCursorPosition("coin::mood_test( )", 1)
         End If
     End Sub
+
+    Private Sub cmdAnsari1_Click(sender As Object, e As EventArgs) Handles cmdAnsari1.Click
+        If ucrChkIncludeArguments.Checked Then
+            ucrReceiverForTestColumn.AddToReceiverAtCursorPosition("coin::ansari_test(formula = , data = , subset = NULL, weights = NULL)", 42)
+        Else
+            ucrReceiverForTestColumn.AddToReceiverAtCursorPosition("coin::ansari_test( )", 1)
+        End If
+    End Sub
 End Class
