24823cf8b7499de8429533f8cec848dd9915bd8c
Wycklife <bonyowycklife@gmail.com>
2020-05-26 12:43:27 +0300

    More addition to the keys

--- instat/dlgHypothesisTestsCalculator.vb
+++ instat/dlgHypothesisTestsCalculator.vb
@@ -839,4 +839,12 @@ Public Class dlgHypothesisTestsCalculator
             ucrReceiverForTestColumn.AddToReceiverAtCursorPosition("coin::chisq_test( )", 1)
         End If
     End Sub
+
+    Private Sub cmdCmh_Click(sender As Object, e As EventArgs) Handles cmdCmh.Click
+        If ucrChkIncludeArguments.Checked Then
+            ucrReceiverForTestColumn.AddToReceiverAtCursorPosition("coin::cmh_test(formula = , data = , subset = NULL, weights = NULL)", 42)
+        Else
+            ucrReceiverForTestColumn.AddToReceiverAtCursorPosition("coin::cmh_test( )", 1)
+        End If
+    End Sub
 End Class
