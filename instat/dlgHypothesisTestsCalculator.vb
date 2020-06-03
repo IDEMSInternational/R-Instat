18b1fa15c28a494f399ad87ca4515c2c63dffd10
Wycklife <bonyowycklife@gmail.com>
2020-06-03 12:04:54 +0300

    Added tooltip for the keys

--- instat/dlgHypothesisTestsCalculator.vb
+++ instat/dlgHypothesisTestsCalculator.vb
@@ -64,6 +64,7 @@ Public Class dlgHypothesisTestsCalculator
         ttHypothesisTests.SetToolTip(cmdCsmk, "The seasonal Mann-Kendall test under the presence of correlated seasons")
         ttHypothesisTests.SetToolTip(cmdLanzante, "The non-parametric Lanzante test for a shift in the central tendency of a time series")
         ttHypothesisTests.SetToolTip(cmdMk, "The Mann-Kendall Trend Test")
+        ttHypothesisTests.SetToolTip(cmdMmk, "The multivariate (multisite) Mann-Kendall test")
         'Temporarily disabled: Not yet functioning.
         ucrChkBy.Enabled = False
         ucrReceiverMultiple.Enabled = False
