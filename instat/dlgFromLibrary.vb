c267bc4107370cc068a0937b3bfa21e752350899
patowhiz <conlooptechnologies@gmail.com>
2020-06-13 07:45:22 +0300

    reading of datasets of different class types

--- instat/dlgFromLibrary.vb
+++ instat/dlgFromLibrary.vb
@@ -14,6 +14,7 @@
 ' You should have received a copy of the GNU General Public License 
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+Imports instat
 Imports instat.Translations
 Imports RDotNet
 
@@ -25,9 +26,13 @@ Public Class dlgFromLibrary
     Private bReset As Boolean = True
     Private clsDataFunction As New RFunction
     Private dctPackages As New Dictionary(Of String, String)
-    Private strAvailablePackages() As String
+
+    'a string array that holds the packages displayed in the combobox 
+    'todo. this property can be removed once the PR that enhances the inputCombobox control is merged
+    Private arrAvailablePackages() As String
 
     Private strSelectedPackage As String = ""
+    Private clsImportFunction As New RFunction 'the base function that call on import R-Instat function
 
     Private Sub dlgFromLibrary_Load(sender As Object, e As EventArgs) Handles Me.Load
         autoTranslate(Me)
@@ -56,7 +61,6 @@ Public Class dlgFromLibrary
         Dim clsGetPackages As New RFunction
         Dim expPackageNames As SymbolicExpression
         Dim chrPackageNames As CharacterVector
-        Dim iDataSets As Integer = 0
 
         ucrBase.iHelpTopicID = 156
 
@@ -66,16 +70,14 @@ Public Class dlgFromLibrary
         expPackageNames = frmMain.clsRLink.RunInternalScriptGetValue(clsGetPackages.ToScript(), bSilent:=True)
         If expPackageNames IsNot Nothing AndAlso expPackageNames.Type <> Internals.SymbolicExpressionType.Null Then
             chrPackageNames = expPackageNames.AsCharacter
-            strAvailablePackages = chrPackageNames.ToArray
-            System.Array.Sort(Of String)(strAvailablePackages)
+            arrAvailablePackages = chrPackageNames.ToArray
+            Array.Sort(arrAvailablePackages)
+            ucrInputPackages.SetParameter(New RParameter("package")) 'for combobox, can't set items without setting parameter first
+            ucrInputPackages.SetItems(arrAvailablePackages, bAddConditions:=True)
         End If
 
-        If strAvailablePackages IsNot Nothing Then
-            ucrInputPackages.SetParameter(New RParameter("package"))
-            ucrInputPackages.SetItems(strAvailablePackages, bAddConditions:=True)
-            ucrInputPackages.SetDropDownStyleAsNonEditable()
-            ucrInputPackages.SetLinkedDisplayControl(lblFromPackage)
-        End If
+        ucrInputPackages.SetDropDownStyleAsNonEditable()
+        ucrInputPackages.SetLinkedDisplayControl(lblFromPackage)
 
         ucrPnlOptions.AddRadioButton(rdoDefaultDatasets)
         ucrPnlOptions.AddRadioButton(rdoInstatCollection)
@@ -91,22 +93,29 @@ Public Class dlgFromLibrary
     End Sub
 
     Private Sub SetDefaults()
-        'TODO this should be the new clear method
-        clsDataFunction.ClearParameters()
-
+        'reset the functions and the custom controls
+        ucrNewDataFrameName.SetName("") 'called here cause its not called in the control's reset method
         ucrNewDataFrameName.Reset()
+        clsDataFunction.Clear()
+        clsImportFunction.Clear()
 
+        'set up the data function
         clsDataFunction.SetPackageName("utils")
         clsDataFunction.SetRCommand("data")
         clsDataFunction.AddParameter("package", Chr(34) & "datasets" & Chr(34))
+
+        'set up the import data function
+        clsImportFunction.SetRCommand(frmMain.clsRLink.strInstatDataObject & "$import_data")
+
+        'add the before codes (the data() R base function) and the base function(import_data() R-Instat function)
         ucrBase.clsRsyntax.AddToBeforeCodes(clsDataFunction)
+        ucrBase.clsRsyntax.SetBaseRFunction(clsImportFunction)
     End Sub
 
     Private Sub SetRCodeforControls(bReset As Boolean)
-        If strAvailablePackages IsNot Nothing Then
+        If arrAvailablePackages IsNot Nothing Then
             ucrInputPackages.SetRCode(clsDataFunction, bReset)
         End If
-        ucrNewDataFrameName.SetRCode(ucrBase.clsRsyntax.clsBaseFunction, bReset)
         ucrPnlOptions.SetRSyntax(ucrBase.clsRsyntax, bReset)
     End Sub
 
@@ -180,15 +189,14 @@ Public Class dlgFromLibrary
     End Sub
 
     Private Sub lstCollection_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstCollection.SelectedIndexChanged
-        Dim strDataName As String
 
         If lstCollection.SelectedItems.Count > 0 Then
-            strDataName = CheckString(lstCollection.SelectedItems(0).SubItems(0).Text)
-            ucrBase.clsRsyntax.SetCommandString(strDataName)
             If Not ucrNewDataFrameName.bUserTyped Then
-                ucrNewDataFrameName.SetName(strDataName)
+                'this will raise event ControValueChanged for this control, which will inturn call SetParameterValues()
+                ucrNewDataFrameName.SetName(CheckString(lstCollection.SelectedItems(0).SubItems(0).Text))
+            Else
+                SetParameterValues()
             End If
-            clsDataFunction.AddParameter("X", strDataName)
         End If
         TestOkEnabled()
         EnableHelp()
@@ -222,7 +230,75 @@ Public Class dlgFromLibrary
     End Sub
 
     Private Sub ucrNewDataFrameName_ControlContentsChanged(ucrChangedControl As ucrCore) Handles ucrNewDataFrameName.ControlContentsChanged
-        ucrBase.clsRsyntax.SetAssignTo(ucrNewDataFrameName.GetText, strTempDataframe:=ucrNewDataFrameName.GetText)
         TestOkEnabled()
     End Sub
+
+    Private Sub ucrNewDataFrameName_ControlValueChanged(ucrChangedControl As ucrCore) Handles ucrNewDataFrameName.ControlValueChanged
+        SetParameterValues()
+    End Sub
+
+    Private Sub SetParameterValues()
+        Dim strSelectedDataName As String
+        Dim strVecOutput As CharacterVector
+        Dim strRClass As String = ""
+
+        If lstCollection.SelectedItems.Count < 1 Then
+            Exit Sub
+        End If
+
+        strSelectedDataName = CheckString(lstCollection.SelectedItems(0).SubItems(0).Text)
+        clsDataFunction.AddParameter("X", strSelectedDataName)
+
+        'calling RunInternalScriptGetOutput() twice because currently it can't execute multiple lines
+        frmMain.clsRLink.RunInternalScriptGetOutput(clsDataFunction.Clone.ToScript(), bSilent:=True)
+        strVecOutput = frmMain.clsRLink.RunInternalScriptGetOutput("class(" + strSelectedDataName + ")", bSilent:=True)
+        If strVecOutput IsNot Nothing AndAlso strVecOutput.Length > 0 Then
+            strRClass = Mid(strVecOutput(0), 5).Replace("""", "").ToLower
+        End If
+
+        If strRClass = "list" Then
+            clsImportFunction.AddParameter("data_tables", strParameterValue:=strSelectedDataName)
+        Else
+            Dim clsListFunction As New RFunction 'defines the list function. list(x=x)
+            Dim clsListParameterFunction As New RFunction 'defines the function that act as list parameters e.g list(y=fortify.zoo(x))
+            clsListFunction.SetRCommand("list")
+            If strRClass = "zoo" Then
+                'this is the recommended command for converting zoo object types to data frames.
+                'In R-Instat the data.frame doesn't convert this object type well. See issue #5649
+                clsListParameterFunction.SetPackageName("zoo")
+                clsListParameterFunction.SetRCommand("fortify.zoo")
+                clsListParameterFunction.AddParameter("model", strParameterValue:=strSelectedDataName)
+                clsListFunction.AddParameter(ucrNewDataFrameName.GetText, clsRFunctionParameter:=clsListParameterFunction)
+            ElseIf strRClass = "spatialpolygonsdataframe" Then
+                'for some reason, objects of this type have to be explicitly coerced to data.frame 
+                'before being imported by import R-Instat function
+                clsListParameterFunction.SetRCommand("data.frame")
+                clsListParameterFunction.AddParameter("x", strParameterValue:=strSelectedDataName)
+                clsListFunction.AddParameter(ucrNewDataFrameName.GetText, clsRFunctionParameter:=clsListParameterFunction)
+            ElseIf strRClass = "dgcmatrix" OrElse strRClass = "dscmatrix" Then
+                'this if block is used for dgcmatrix,dscmatrix.
+                'The R summary() function returns an object of type data.frame if given a matrix. Used here to coerce it to data.frame
+                'todo. this needs to be investigated further on the best command for coercing this type of data
+                clsListParameterFunction.SetRCommand("summary")
+                clsListParameterFunction.AddParameter("object", strParameterValue:=strSelectedDataName)
+                clsListFunction.AddParameter(ucrNewDataFrameName.GetText, clsRFunctionParameter:=clsListParameterFunction)
+            ElseIf strRClass = "matrix" Then
+                'this if block is used for dgcmatrix,dscmatrix and any other matrix types 
+                'this has been done in it's own block in anticipation of a correct way of coercing matrix to data frame
+                'currently this command loses data(some columns) of the matrix once it's coerced. See issue #5649
+                clsListParameterFunction.SetRCommand("data.frame")
+                clsListParameterFunction.AddParameter("x", strParameterValue:=strSelectedDataName)
+                clsListFunction.AddParameter(ucrNewDataFrameName.GetText, clsRFunctionParameter:=clsListParameterFunction)
+            Else
+                clsListFunction.AddParameter(ucrNewDataFrameName.GetText, strParameterValue:=strSelectedDataName)
+            End If
+            clsImportFunction.AddParameter("data_tables", clsRFunctionParameter:=clsListFunction)
+        End If
+    End Sub
+
+    Private Sub ucrInputPackages_SelectionIndexChanged() Handles ucrInputPackages.SelectionIndexChanged
+
+    End Sub
+
+
 End Class
