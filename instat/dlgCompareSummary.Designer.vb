074560cae907878c80ed6d8f1e0da1a04513fdc5
shadrack kibet <shadrackkibet36@outlook.com>
2020-06-20 15:54:13 +0300

    adding controls

--- instat/dlgCompareSummary.Designer.vb
+++ instat/dlgCompareSummary.Designer.vb
@@ -22,18 +22,207 @@ Partial Class dlgCompareSummary
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.lblStation = New System.Windows.Forms.Label()
+        Me.lblFactors = New System.Windows.Forms.Label()
+        Me.lblSatellite = New System.Windows.Forms.Label()
+        Me.rdoContinuous = New System.Windows.Forms.RadioButton()
+        Me.rdoCategorical = New System.Windows.Forms.RadioButton()
+        Me.rdoBinary = New System.Windows.Forms.RadioButton()
+        Me.ucrPnlObservationType = New instat.UcrPanel()
+        Me.ucrReceiverMultipleFactors = New instat.ucrReceiverMultiple()
+        Me.ucrReceiverSatellite = New instat.ucrReceiverSingle()
+        Me.ucrReceiverStation = New instat.ucrReceiverSingle()
+        Me.ucrSelectorVerificationSummary = New instat.ucrSelectorByDataFrameAddRemove()
+        Me.ucrBase = New instat.ucrButtons()
+        Me.cmdSummaries = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'lblStation
+        '
+        Me.lblStation.AutoSize = True
+        Me.lblStation.Location = New System.Drawing.Point(273, 76)
+        Me.lblStation.Name = "lblStation"
+        Me.lblStation.Size = New System.Drawing.Size(84, 13)
+        Me.lblStation.TabIndex = 5
+        Me.lblStation.Text = "Station Element:"
+        '
+        'lblFactors
+        '
+        Me.lblFactors.AutoSize = True
+        Me.lblFactors.Location = New System.Drawing.Point(273, 172)
+        Me.lblFactors.Name = "lblFactors"
+        Me.lblFactors.Size = New System.Drawing.Size(45, 13)
+        Me.lblFactors.TabIndex = 6
+        Me.lblFactors.Text = "Factors:"
+        '
+        'lblSatellite
+        '
+        Me.lblSatellite.AutoSize = True
+        Me.lblSatellite.Location = New System.Drawing.Point(273, 126)
+        Me.lblSatellite.Name = "lblSatellite"
+        Me.lblSatellite.Size = New System.Drawing.Size(88, 13)
+        Me.lblSatellite.TabIndex = 7
+        Me.lblSatellite.Text = "Satellite Element:"
+        '
+        'rdoContinuous
+        '
+        Me.rdoContinuous.Appearance = System.Windows.Forms.Appearance.Button
+        Me.rdoContinuous.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoContinuous.FlatAppearance.BorderSize = 2
+        Me.rdoContinuous.FlatAppearance.CheckedBackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoContinuous.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.rdoContinuous.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.rdoContinuous.Location = New System.Drawing.Point(62, 12)
+        Me.rdoContinuous.Name = "rdoContinuous"
+        Me.rdoContinuous.Size = New System.Drawing.Size(98, 27)
+        Me.rdoContinuous.TabIndex = 9
+        Me.rdoContinuous.TabStop = True
+        Me.rdoContinuous.Text = "Continuous"
+        Me.rdoContinuous.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.rdoContinuous.UseVisualStyleBackColor = True
+        '
+        'rdoCategorical
+        '
+        Me.rdoCategorical.Appearance = System.Windows.Forms.Appearance.Button
+        Me.rdoCategorical.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoCategorical.FlatAppearance.BorderSize = 2
+        Me.rdoCategorical.FlatAppearance.CheckedBackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoCategorical.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.rdoCategorical.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.rdoCategorical.Location = New System.Drawing.Point(254, 12)
+        Me.rdoCategorical.Name = "rdoCategorical"
+        Me.rdoCategorical.Size = New System.Drawing.Size(98, 27)
+        Me.rdoCategorical.TabIndex = 10
+        Me.rdoCategorical.TabStop = True
+        Me.rdoCategorical.Text = "Categorical"
+        Me.rdoCategorical.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.rdoCategorical.UseVisualStyleBackColor = True
+        '
+        'rdoBinary
+        '
+        Me.rdoBinary.Appearance = System.Windows.Forms.Appearance.Button
+        Me.rdoBinary.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoBinary.FlatAppearance.BorderSize = 2
+        Me.rdoBinary.FlatAppearance.CheckedBackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoBinary.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.rdoBinary.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.rdoBinary.Location = New System.Drawing.Point(158, 12)
+        Me.rdoBinary.Name = "rdoBinary"
+        Me.rdoBinary.Size = New System.Drawing.Size(98, 27)
+        Me.rdoBinary.TabIndex = 11
+        Me.rdoBinary.TabStop = True
+        Me.rdoBinary.Text = "Binary"
+        Me.rdoBinary.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.rdoBinary.UseVisualStyleBackColor = True
+        '
+        'ucrPnlObservationType
+        '
+        Me.ucrPnlObservationType.Location = New System.Drawing.Point(58, 4)
+        Me.ucrPnlObservationType.Name = "ucrPnlObservationType"
+        Me.ucrPnlObservationType.Size = New System.Drawing.Size(300, 50)
+        Me.ucrPnlObservationType.TabIndex = 8
+        '
+        'ucrReceiverMultipleFactors
+        '
+        Me.ucrReceiverMultipleFactors.frmParent = Me
+        Me.ucrReceiverMultipleFactors.Location = New System.Drawing.Point(273, 188)
+        Me.ucrReceiverMultipleFactors.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverMultipleFactors.Name = "ucrReceiverMultipleFactors"
+        Me.ucrReceiverMultipleFactors.Selector = Nothing
+        Me.ucrReceiverMultipleFactors.Size = New System.Drawing.Size(120, 100)
+        Me.ucrReceiverMultipleFactors.strNcFilePath = ""
+        Me.ucrReceiverMultipleFactors.TabIndex = 4
+        Me.ucrReceiverMultipleFactors.ucrSelector = Nothing
+        '
+        'ucrReceiverSatellite
+        '
+        Me.ucrReceiverSatellite.frmParent = Me
+        Me.ucrReceiverSatellite.Location = New System.Drawing.Point(273, 142)
+        Me.ucrReceiverSatellite.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverSatellite.Name = "ucrReceiverSatellite"
+        Me.ucrReceiverSatellite.Selector = Nothing
+        Me.ucrReceiverSatellite.Size = New System.Drawing.Size(120, 20)
+        Me.ucrReceiverSatellite.strNcFilePath = ""
+        Me.ucrReceiverSatellite.TabIndex = 3
+        Me.ucrReceiverSatellite.ucrSelector = Nothing
+        '
+        'ucrReceiverStation
+        '
+        Me.ucrReceiverStation.frmParent = Me
+        Me.ucrReceiverStation.Location = New System.Drawing.Point(273, 93)
+        Me.ucrReceiverStation.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverStation.Name = "ucrReceiverStation"
+        Me.ucrReceiverStation.Selector = Nothing
+        Me.ucrReceiverStation.Size = New System.Drawing.Size(120, 20)
+        Me.ucrReceiverStation.strNcFilePath = ""
+        Me.ucrReceiverStation.TabIndex = 2
+        Me.ucrReceiverStation.ucrSelector = Nothing
+        '
+        'ucrSelectorVerificationSummary
+        '
+        Me.ucrSelectorVerificationSummary.bDropUnusedFilterLevels = False
+        Me.ucrSelectorVerificationSummary.bShowHiddenColumns = False
+        Me.ucrSelectorVerificationSummary.bUseCurrentFilter = True
+        Me.ucrSelectorVerificationSummary.Location = New System.Drawing.Point(10, 57)
+        Me.ucrSelectorVerificationSummary.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrSelectorVerificationSummary.Name = "ucrSelectorVerificationSummary"
+        Me.ucrSelectorVerificationSummary.Size = New System.Drawing.Size(210, 180)
+        Me.ucrSelectorVerificationSummary.TabIndex = 1
+        '
+        'ucrBase
+        '
+        Me.ucrBase.Location = New System.Drawing.Point(10, 320)
+        Me.ucrBase.Name = "ucrBase"
+        Me.ucrBase.Size = New System.Drawing.Size(410, 52)
+        Me.ucrBase.TabIndex = 0
+        '
+        'cmdSummaries
+        '
+        Me.cmdSummaries.Location = New System.Drawing.Point(273, 291)
+        Me.cmdSummaries.Name = "cmdSummaries"
+        Me.cmdSummaries.Size = New System.Drawing.Size(120, 23)
+        Me.cmdSummaries.TabIndex = 12
+        Me.cmdSummaries.Text = "Summaries"
+        Me.cmdSummaries.UseVisualStyleBackColor = True
         '
         'dlgCompareSummary
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(432, 384)
+        Me.Controls.Add(Me.cmdSummaries)
+        Me.Controls.Add(Me.rdoBinary)
+        Me.Controls.Add(Me.rdoCategorical)
+        Me.Controls.Add(Me.rdoContinuous)
+        Me.Controls.Add(Me.ucrPnlObservationType)
+        Me.Controls.Add(Me.lblSatellite)
+        Me.Controls.Add(Me.lblFactors)
+        Me.Controls.Add(Me.lblStation)
+        Me.Controls.Add(Me.ucrReceiverMultipleFactors)
+        Me.Controls.Add(Me.ucrReceiverSatellite)
+        Me.Controls.Add(Me.ucrReceiverStation)
+        Me.Controls.Add(Me.ucrSelectorVerificationSummary)
+        Me.Controls.Add(Me.ucrBase)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.Name = "dlgCompareSummary"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Summary"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents ucrBase As ucrButtons
+    Friend WithEvents ucrSelectorVerificationSummary As ucrSelectorByDataFrameAddRemove
+    Friend WithEvents ucrReceiverStation As ucrReceiverSingle
+    Friend WithEvents ucrReceiverMultipleFactors As ucrReceiverMultiple
+    Friend WithEvents ucrReceiverSatellite As ucrReceiverSingle
+    Friend WithEvents lblSatellite As Label
+    Friend WithEvents lblFactors As Label
+    Friend WithEvents lblStation As Label
+    Friend WithEvents ucrPnlObservationType As UcrPanel
+    Friend WithEvents rdoBinary As RadioButton
+    Friend WithEvents rdoCategorical As RadioButton
+    Friend WithEvents rdoContinuous As RadioButton
+    Friend WithEvents cmdSummaries As Button
 End Class
--- instat/sdgVerificationSummaries.Designer.vb
+++ instat/sdgVerificationSummaries.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class sdgVerificationSummaries
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,23 +20,152 @@ Partial Class sdgVerificationSummaries
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.ucrBase = New instat.ucrButtonsSubdialogue()
+        Me.tbSummaries = New System.Windows.Forms.TabControl()
+        Me.tbContinuous = New System.Windows.Forms.TabPage()
+        Me.tbBinary = New System.Windows.Forms.TabPage()
+        Me.UcrCheck9 = New instat.ucrCheck()
+        Me.UcrCheck8 = New instat.ucrCheck()
+        Me.UcrCheck7 = New instat.ucrCheck()
+        Me.UcrCheck6 = New instat.ucrCheck()
+        Me.tbCategorical = New System.Windows.Forms.TabPage()
+        Me.UcrCheck13 = New instat.ucrCheck()
+        Me.UcrCheck12 = New instat.ucrCheck()
+        Me.UcrCheck11 = New instat.ucrCheck()
+        Me.UcrCheck10 = New instat.ucrCheck()
+        Me.tbSummaries.SuspendLayout()
+        Me.tbBinary.SuspendLayout()
+        Me.tbCategorical.SuspendLayout()
         Me.SuspendLayout()
         '
         'ucrBase
         '
-        Me.ucrBase.Location = New System.Drawing.Point(114, 298)
+        Me.ucrBase.Location = New System.Drawing.Point(114, 333)
         Me.ucrBase.Name = "ucrBase"
-        Me.ucrBase.Size = New System.Drawing.Size(142, 30)
+        Me.ucrBase.Size = New System.Drawing.Size(154, 39)
         Me.ucrBase.TabIndex = 0
+        '
+        'tbSummaries
+        '
+        Me.tbSummaries.Controls.Add(Me.tbContinuous)
+        Me.tbSummaries.Controls.Add(Me.tbBinary)
+        Me.tbSummaries.Controls.Add(Me.tbCategorical)
+        Me.tbSummaries.Location = New System.Drawing.Point(12, 12)
+        Me.tbSummaries.Name = "tbSummaries"
+        Me.tbSummaries.SelectedIndex = 0
+        Me.tbSummaries.Size = New System.Drawing.Size(443, 305)
+        Me.tbSummaries.TabIndex = 1
+        '
+        'tbContinuous
+        '
+        Me.tbContinuous.Location = New System.Drawing.Point(4, 22)
+        Me.tbContinuous.Name = "tbContinuous"
+        Me.tbContinuous.Padding = New System.Windows.Forms.Padding(3)
+        Me.tbContinuous.Size = New System.Drawing.Size(435, 279)
+        Me.tbContinuous.TabIndex = 0
+        Me.tbContinuous.Text = "Continuous"
+        Me.tbContinuous.UseVisualStyleBackColor = True
+        '
+        'tbBinary
+        '
+        Me.tbBinary.Controls.Add(Me.UcrCheck9)
+        Me.tbBinary.Controls.Add(Me.UcrCheck8)
+        Me.tbBinary.Controls.Add(Me.UcrCheck7)
+        Me.tbBinary.Controls.Add(Me.UcrCheck6)
+        Me.tbBinary.Location = New System.Drawing.Point(4, 22)
+        Me.tbBinary.Name = "tbBinary"
+        Me.tbBinary.Padding = New System.Windows.Forms.Padding(3)
+        Me.tbBinary.Size = New System.Drawing.Size(435, 279)
+        Me.tbBinary.TabIndex = 1
+        Me.tbBinary.Text = "Binary"
+        Me.tbBinary.UseVisualStyleBackColor = True
+        '
+        'UcrCheck9
+        '
+        Me.UcrCheck9.Checked = False
+        Me.UcrCheck9.Location = New System.Drawing.Point(6, 20)
+        Me.UcrCheck9.Name = "UcrCheck9"
+        Me.UcrCheck9.Size = New System.Drawing.Size(100, 20)
+        Me.UcrCheck9.TabIndex = 4
+        '
+        'UcrCheck8
+        '
+        Me.UcrCheck8.Checked = False
+        Me.UcrCheck8.Location = New System.Drawing.Point(6, 46)
+        Me.UcrCheck8.Name = "UcrCheck8"
+        Me.UcrCheck8.Size = New System.Drawing.Size(100, 20)
+        Me.UcrCheck8.TabIndex = 3
+        '
+        'UcrCheck7
+        '
+        Me.UcrCheck7.Checked = False
+        Me.UcrCheck7.Location = New System.Drawing.Point(127, 20)
+        Me.UcrCheck7.Name = "UcrCheck7"
+        Me.UcrCheck7.Size = New System.Drawing.Size(100, 20)
+        Me.UcrCheck7.TabIndex = 2
+        '
+        'UcrCheck6
+        '
+        Me.UcrCheck6.Checked = False
+        Me.UcrCheck6.Location = New System.Drawing.Point(127, 46)
+        Me.UcrCheck6.Name = "UcrCheck6"
+        Me.UcrCheck6.Size = New System.Drawing.Size(100, 20)
+        Me.UcrCheck6.TabIndex = 1
+        '
+        'tbCategorical
+        '
+        Me.tbCategorical.Controls.Add(Me.UcrCheck13)
+        Me.tbCategorical.Controls.Add(Me.UcrCheck12)
+        Me.tbCategorical.Controls.Add(Me.UcrCheck11)
+        Me.tbCategorical.Controls.Add(Me.UcrCheck10)
+        Me.tbCategorical.Location = New System.Drawing.Point(4, 22)
+        Me.tbCategorical.Name = "tbCategorical"
+        Me.tbCategorical.Padding = New System.Windows.Forms.Padding(3)
+        Me.tbCategorical.Size = New System.Drawing.Size(435, 279)
+        Me.tbCategorical.TabIndex = 2
+        Me.tbCategorical.Text = "Categorical"
+        Me.tbCategorical.UseVisualStyleBackColor = True
+        '
+        'UcrCheck13
+        '
+        Me.UcrCheck13.Checked = False
+        Me.UcrCheck13.Location = New System.Drawing.Point(6, 18)
+        Me.UcrCheck13.Name = "UcrCheck13"
+        Me.UcrCheck13.Size = New System.Drawing.Size(100, 20)
+        Me.UcrCheck13.TabIndex = 4
+        '
+        'UcrCheck12
+        '
+        Me.UcrCheck12.Checked = False
+        Me.UcrCheck12.Location = New System.Drawing.Point(6, 44)
+        Me.UcrCheck12.Name = "UcrCheck12"
+        Me.UcrCheck12.Size = New System.Drawing.Size(100, 20)
+        Me.UcrCheck12.TabIndex = 3
+        '
+        'UcrCheck11
+        '
+        Me.UcrCheck11.Checked = False
+        Me.UcrCheck11.Location = New System.Drawing.Point(127, 18)
+        Me.UcrCheck11.Name = "UcrCheck11"
+        Me.UcrCheck11.Size = New System.Drawing.Size(100, 20)
+        Me.UcrCheck11.TabIndex = 2
+        '
+        'UcrCheck10
+        '
+        Me.UcrCheck10.Checked = False
+        Me.UcrCheck10.Location = New System.Drawing.Point(127, 44)
+        Me.UcrCheck10.Name = "UcrCheck10"
+        Me.UcrCheck10.Size = New System.Drawing.Size(100, 20)
+        Me.UcrCheck10.TabIndex = 1
         '
         'sdgVerificationSummaries
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(387, 340)
+        Me.ClientSize = New System.Drawing.Size(476, 373)
+        Me.Controls.Add(Me.tbSummaries)
         Me.Controls.Add(Me.ucrBase)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.MaximizeBox = False
@@ -44,9 +173,24 @@ Partial Class sdgVerificationSummaries
         Me.Name = "sdgVerificationSummaries"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Summaries"
+        Me.tbSummaries.ResumeLayout(False)
+        Me.tbBinary.ResumeLayout(False)
+        Me.tbCategorical.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents ucrBase As ucrButtonsSubdialogue
+    Friend WithEvents tbSummaries As TabControl
+    Friend WithEvents tbContinuous As TabPage
+    Friend WithEvents tbBinary As TabPage
+    Friend WithEvents tbCategorical As TabPage
+    Friend WithEvents UcrCheck9 As ucrCheck
+    Friend WithEvents UcrCheck8 As ucrCheck
+    Friend WithEvents UcrCheck7 As ucrCheck
+    Friend WithEvents UcrCheck6 As ucrCheck
+    Friend WithEvents UcrCheck13 As ucrCheck
+    Friend WithEvents UcrCheck12 As ucrCheck
+    Friend WithEvents UcrCheck11 As ucrCheck
+    Friend WithEvents UcrCheck10 As ucrCheck
 End Class
