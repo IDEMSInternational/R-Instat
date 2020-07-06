b6fc1d2d026d8f5fd9e8155192630f39e65fe69f
Wycklife <bonyowycklife@gmail.com>
2020-07-06 10:49:43 +0300

    Added more tests

--- instat/dlgOneVarFitModel.vb
+++ instat/dlgOneVarFitModel.vb
@@ -76,7 +76,7 @@ Public Class dlgOneVarFitModel
         ucrSaveModel.SetAssignToIfUncheckedValue("last_model")
         ucrSaveModel.SetPrefix("Normal")
 
-        ucrInputComboTests.SetItems({"Binomial", "Proportion", "Sign", "T", "Wilcoxon", "Z", "Bartel", "Br"})
+        ucrInputComboTests.SetItems({"Binomial", "Proportion", "Sign", "T", "Wilcoxon", "Z", "Bartel", "Br", "Runs", "Sen", "Serial Corr", "Snh", ""})
         ucrInputComboTests.SetDropDownStyleAsNonEditable()
 
         ucrInputComboEstimate.SetItems({"mean", "median", "normal", "poisson", "quantile", "sd", "variance"})
