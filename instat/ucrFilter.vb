d717577bbd270552f27d297121780e1b105c1c0c
shadrack kibet <shadrackkibet36@outlook.com>
2020-08-04 09:44:12 +0300

    no clearing of expression receiver

--- instat/ucrFilter.vb
+++ instat/ucrFilter.vb
@@ -229,7 +229,6 @@ Public Class ucrFilter
         lstFilters.Columns(1).Width = -2
         ucrFilterPreview.SetName(clsFilterView.ToScript())
         ucrFilterByReceiver.Clear()
-        ucrReceiverExpression.Clear()
         RaiseEvent FilterChanged()
     End Sub
 
