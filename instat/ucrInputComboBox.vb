9b5c5fd5e5e8cc8274e4ea4c8ecdac43457e0112
shadrack kibet <shadrackkibet36@outlook.com>
2020-08-06 16:16:16 +0300

    methods to get count of items, setting and getting selected index

--- instat/ucrInputComboBox.vb
+++ instat/ucrInputComboBox.vb
@@ -161,6 +161,19 @@ Public Class ucrInputComboBox
         End If
     End Function
 
+    Public Property GetSetSelectedIndex As Integer
+        Get
+            Return cboInput.SelectedIndex
+        End Get
+        Set(value As Integer)
+            cboInput.SelectedIndex = value
+        End Set
+    End Property
+
+    Public Function GetItemsCount() As Integer
+        Return cboInput.Items.Count
+    End Function
+
     Public Sub SetItems(Optional strItems As String() = Nothing, Optional bClearExisting As Boolean = True, Optional bAddConditions As Boolean = False, Optional bAddQuotes As Boolean = True)
         Dim dctValues As New Dictionary(Of String, String)
         If bAddConditions Then
