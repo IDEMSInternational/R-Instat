48a3bbd39025e94de0a8c6e5ab1520b196867424
ivanluv <luvisiaivan@gmail.com>
2020-08-17 15:37:24 +0300

    Code for extremes  dialogue  and extremes method subDialogue

--- instat/dlgExtremes.vb
+++ instat/dlgExtremes.vb
@@ -14,64 +14,301 @@
 ' You should have received a copy of the GNU General Public License 
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+Imports instat
 Imports instat.Translations
 
 Public Class dlgExtremes
+    Private clsAttach As New RFunction
+    Private clsDetach As New RFunction
+    Private clsFevdFunction, clsNaExclude, clsConvertVector, clsFevdPlotsFunction As New RFunction
+    'clsLocationScaleResetOperator is not run but affects reset of the check box.Any better method of implementation?
+    Private clsLocationScaleResetOperator As New ROperator
+    Private clsLocationParamOperator As New ROperator
+    Private bFirstLoad As Boolean = True
+    Private bReset As Boolean = True
+    Private bResetFittingOptions As Boolean = False
+    Private bResetDisplayOptions As Boolean = False
+    Private bResettingDialogue As Boolean = False
+
     Private Sub dlgExtremes_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        chkRestrictValues.Checked = False
-        lblBetween.Visible = False
-        txtBetween.Visible = False
-        lblAnd.Visible = False
-        txtAnd.Visible = False
-        chkMaximumLikelihood.Checked = False
-        chkMethodOfMoments.Checked = False
-        cboMaximumLikelihood.Visible = True
-
-        cboMethodOfMoments.Visible = False
-        chkProbabilityPlot.Checked = False
-
-        ucrBase.OKEnabled(False) ' temporarily for beta
-
-        ucrBase.clsRsyntax.SetFunction("gev.fit")
-        ucrBase.clsRsyntax.iCallType = 1
+        If bFirstLoad Then
+            InitialiseDialog()
+            bFirstLoad = False
+        End If
+        If bReset Then
+            SetDefaults()
+        End If
+        SetRCodeForControls(bReset)
+        bReset = False
         autoTranslate(Me)
-        ucrReceiverDataToFit.Selector = ucrAddRemove
-        ucrReceiverDataToFit.SetMeAsReceiver()
-        ucrBase.iHelpTopicID = 488
+        TestOkEnabled()
+    End Sub
+
+    Private Sub InitialiseDialog()
+        grpFirstCalc.Visible = False
+        grpSecondCalc.Visible = False
+
+        Dim dctFevdTypes As New Dictionary(Of String, String)
+
+        ucrReceiverVariable.Selector = ucrSelectorExtremes
+        ucrReceiverVariable.strSelectorHeading = "Variables"
+        ucrReceiverVariable.SetParameter(New RParameter("x", 0))
+        ucrReceiverVariable.SetParameterIsRFunction()
+
+        ucrInputExtremes.SetParameter(New RParameter("type", 1))
+        dctFevdTypes.Add("GEV", Chr(34) & "GEV" & Chr(34))
+        dctFevdTypes.Add("GP", Chr(34) & "GP" & Chr(34))
+        dctFevdTypes.Add("PP", Chr(34) & "PP" & Chr(34))
+        dctFevdTypes.Add("Gumbel", Chr(34) & "Gumbel" & Chr(34))
+        dctFevdTypes.Add("Exponential", Chr(34) & "Exponential" & Chr(34))
+        ucrInputExtremes.SetItems(dctFevdTypes)
+        ucrInputExtremes.SetDropDownStyleAsNonEditable()
+
+        ucrBase.clsRsyntax.iCallType = 2
+        ucrBase.clsRsyntax.bExcludeAssignedFunctionOutput = False
+
+        ucrReceiverVariable.Selector = ucrSelectorExtremes
+        ucrReceiverVariable.strSelectorHeading = "Variables"
+        ucrReceiverVariable.SetMeAsReceiver()
+        ucrReceiverVariable.SetParameter(New RParameter("object", 0))
+        ucrReceiverVariable.SetParameterIsRFunction()
+
+        ucrChkExplanatoryModelForLocationParameter.SetText("Explanatory Model for Location or Scale")
+        ucrChkExplanatoryModelForLocationParameter.AddParameterPresentCondition(True, "scaleLocation", True)
+        ucrChkExplanatoryModelForLocationParameter.AddParameterPresentCondition(False, "scaleLocation", False)
+        ucrChkExplanatoryModelForLocationParameter.AddToLinkedControls(ucrReceiverExpressionExplanatoryModelForLocParam, {True}, bNewLinkedHideIfParameterMissing:=True, bNewLinkedAddRemoveParameter:=True)
+
+        ucrInputThresholdforLocation.SetValidationTypeAsNumeric()
+        ucrInputThresholdforLocation.AddQuotesIfUnrecognised = False
+        ucrInputThresholdforLocation.SetLinkedDisplayControl(lblThreshold)
+
+        ucrReceiverExpressionExplanatoryModelForLocParam.SetParameter(New RParameter("locationParam", 1, bNewIncludeArgumentName:=False))
+        ucrReceiverExpressionExplanatoryModelForLocParam.SetParameterIsString()
+        ucrReceiverExpressionExplanatoryModelForLocParam.SetRDefault(1)
+        ucrReceiverExpressionExplanatoryModelForLocParam.bWithQuotes = False
+
+        ucrTryModelling.SetReceiver(ucrReceiverExpressionExplanatoryModelForLocParam)
+        ucrTryModelling.SetIsModel()
+
+        ucrSaveExtremes.SetPrefix("extreme")
+        ucrSaveExtremes.SetIsComboBox()
+        ucrSaveExtremes.SetCheckBoxText("Save Model")
+        ucrSaveExtremes.SetSaveTypeAsModel()
+        ucrSaveExtremes.SetDataFrameSelector(ucrSelectorExtremes.ucrAvailableDataFrames)
+        ucrSaveExtremes.SetAssignToIfUncheckedValue("last_model")
     End Sub
 
     Private Sub SetDefaults()
+        clsFevdFunction = New RFunction
+        clsFevdPlotsFunction = New RFunction
+        clsNaExclude = New RFunction
+        clsConvertVector = New RFunction
+        clsLocationParamOperator = New ROperator
+        clsLocationScaleResetOperator = New ROperator
+
+        ucrBase.clsRsyntax.ClearCodes()
+
+        ucrReceiverVariable.SetMeAsReceiver()
+        ucrSelectorExtremes.Reset()
+        ucrInputThresholdforLocation.SetText("0")
+        ucrReceiverExpressionExplanatoryModelForLocParam.Selector = ucrSelectorExtremes
+
+        clsLocationScaleResetOperator.SetOperation("")
+        clsLocationScaleResetOperator.bBrackets = False
+
+        clsLocationParamOperator.SetOperation("~")
+        clsLocationParamOperator.AddParameter(strParameterValue:="", iPosition:=0, bIncludeArgumentName:=False)
+        clsLocationParamOperator.bSpaceAroundOperation = False
+
+        clsNaExclude.SetPackageName("stats")
+        clsNaExclude.SetRCommand("na.exclude")
+
+        clsFevdPlotsFunction.SetPackageName("extRemes")
+        clsFevdPlotsFunction.SetRCommand("plot.fevd")
+        clsFevdPlotsFunction.iCallType = 3
+        clsFevdPlotsFunction.bExcludeAssignedFunctionOutput = False
+
+        clsFevdFunction.SetPackageName("extRemes")
+        clsFevdFunction.SetRCommand("fevd")
+
+
+        clsConvertVector.SetRCommand("as.vector")
+        clsConvertVector.AddParameter("x", clsRFunctionParameter:=clsNaExclude)
+
+        clsFevdFunction.AddParameter("x", clsRFunctionParameter:=clsConvertVector, iPosition:=0)
+        clsFevdFunction.AddParameter("type", Chr(34) & "GEV" & Chr(34), iPosition:=1)
+        clsFevdFunction.AddParameter("method", Chr(34) & "MLE" & Chr(34), iPosition:=2)
+
+        clsFevdFunction.SetAssignTo(ucrSaveExtremes.GetText(), strTempDataframe:=ucrSelectorExtremes.ucrAvailableDataFrames.cboAvailableDataFrames.Text, strTempModel:="last_model", bAssignToIsPrefix:=True)
+        clsFevdPlotsFunction.SetAssignTo("last_graph", strTempDataframe:=ucrSelectorExtremes.ucrAvailableDataFrames.cboAvailableDataFrames.Text, strTempGraph:="last_graph")
+        clsFevdPlotsFunction.AddParameter("x", clsRFunctionParameter:=clsFevdFunction, iPosition:=0)
+
+        clsAttach.SetRCommand("attach")
+        clsDetach.SetRCommand("detach")
+        clsAttach.AddParameter("what", clsRFunctionParameter:=ucrSelectorExtremes.ucrAvailableDataFrames.clsCurrDataFrame)
+        clsDetach.AddParameter("name", clsRFunctionParameter:=ucrSelectorExtremes.ucrAvailableDataFrames.clsCurrDataFrame)
+        clsDetach.AddParameter("unload", "TRUE")
+
+        ucrBase.clsRsyntax.AddToBeforeCodes(clsAttach)
+        ucrBase.clsRsyntax.AddToAfterCodes(clsDetach, iPosition:=1)
+        ucrBase.clsRsyntax.SetBaseRFunction(clsFevdFunction)
+        ucrTryModelling.SetRSyntax(ucrBase.clsRsyntax)
+
+        bResetDisplayOptions = True
+        bResetFittingOptions = True
     End Sub
 
-    Private Sub chkRestrictValues_CheckedChanged(sender As Object, e As EventArgs) 
-        If chkRestrictValues.Checked = True Then
-            lblBetween.Visible = True
-            txtBetween.Visible = True
-            lblAnd.Visible = True
-            txtAnd.Visible = True
+    Private Sub SetRCodeForControls(bReset As Boolean)
+        ucrInputExtremes.SetRCode(clsFevdFunction, bReset)
+        ucrReceiverVariable.SetRCode(clsNaExclude, bReset)
+        ucrSaveExtremes.SetRCode(clsFevdFunction, bReset)
+        ucrChkExplanatoryModelForLocationParameter.SetRCode(clsLocationScaleResetOperator, bReset)
+        ucrReceiverExpressionExplanatoryModelForLocParam.SetRCode(clsLocationParamOperator, bReset)
+    End Sub
+    Private Sub ucrBase_ClickReset(sender As Object, e As EventArgs) Handles ucrBase.ClickReset
+        bResettingDialogue = True
+        SetDefaults()
+        SetRCodeForControls(True)
+        TestOkEnabled()
+        bResettingDialogue = False
+    End Sub
+
+    Private Sub cmdDisplayOptions_Click(sender As Object, e As EventArgs) Handles cmdDisplayOptions.Click
+        sdgExtremesDisplayOptions.SetRCode(clsNewFevdPlotFunction:=clsFevdPlotsFunction, clsNewRSyntax:=ucrBase.clsRsyntax)
+        bResetDisplayOptions = False
+        sdgExtremesDisplayOptions.ShowDialog()
+    End Sub
+
+    Private Sub TestOkEnabled()
+        If ucrReceiverVariable.IsEmpty Then
+            ucrBase.OKEnabled(False)
         Else
-            lblBetween.Visible = False
-            txtBetween.Visible = False
-            lblAnd.Visible = False
-            txtAnd.Visible = False
+            ucrBase.OKEnabled(True)
         End If
     End Sub
 
-    Private Sub chkMaximumLikelihood_CheckedChanged(sender As Object, e As EventArgs) 
-        If chkMaximumLikelihood.Checked = True Then
-            cboMaximumLikelihood.Visible = True
-        End If
+    Private Sub cmdFittingOptions_Click(sender As Object, e As EventArgs) Handles cmdFittingOptions.Click
+        sdgExtremesMethod.SetRCode(clsNewFevdFunction:=clsFevdFunction, bReset:=bResetFittingOptions)
+        bResetFittingOptions = False
+        sdgExtremesMethod.ShowDialog()
     End Sub
 
-    Private Sub chkMethodofMoments_CheckedChanged(sender As Object, e As EventArgs) 
-        If chkMethodOfMoments.Checked = True Then
-            cboMethodOfMoments.Visible = True
+    Private Sub cmdPlus_Click(sender As Object, e As EventArgs) Handles cmdPlus.Click
+        ucrReceiverExpressionExplanatoryModelForLocParam.AddToReceiverAtCursorPosition("+")
+    End Sub
+
+    Private Sub cmdColon_Click(sender As Object, e As EventArgs) Handles cmdColon.Click
+        ucrReceiverExpressionExplanatoryModelForLocParam.AddToReceiverAtCursorPosition(":")
+    End Sub
+
+    Private Sub cmdMultiply_Click(sender As Object, e As EventArgs) Handles cmdMultiply.Click
+        ucrReceiverExpressionExplanatoryModelForLocParam.AddToReceiverAtCursorPosition("*")
+    End Sub
+
+    Private Sub cmdDiv_Click(sender As Object, e As EventArgs) Handles cmdDiv.Click
+        ucrReceiverExpressionExplanatoryModelForLocParam.AddToReceiverAtCursorPosition("/")
+    End Sub
+
+    Private Sub cmdDoubleBracket_Click(sender As Object, e As EventArgs) Handles cmdDoubleBracket.Click
+        ucrReceiverExpressionExplanatoryModelForLocParam.AddToReceiverAtCursorPosition("( )", 2)
+    End Sub
+
+    Private Sub cmdOpeningBracket_Click(sender As Object, e As EventArgs) Handles cmdOpeningBracket.Click
+        ucrReceiverExpressionExplanatoryModelForLocParam.AddToReceiverAtCursorPosition("(")
+    End Sub
+
+    Private Sub cmdClosingBracket_Click(sender As Object, e As EventArgs) Handles cmdClosingBracket.Click
+        ucrReceiverExpressionExplanatoryModelForLocParam.AddToReceiverAtCursorPosition(")")
+    End Sub
+
+    Private Sub cmdPower_Click(sender As Object, e As EventArgs) Handles cmdPower.Click
+        ucrReceiverExpressionExplanatoryModelForLocParam.AddToReceiverAtCursorPosition("^")
+    End Sub
+
+    Private Sub cmdMinus_Click(sender As Object, e As EventArgs) Handles cmdMinus.Click
+        ucrReceiverExpressionExplanatoryModelForLocParam.AddToReceiverAtCursorPosition("-")
+    End Sub
+
+    Private Sub cmdZero_Click(sender As Object, e As EventArgs) Handles cmdZero.Click
+        ucrReceiverExpressionExplanatoryModelForLocParam.AddToReceiverAtCursorPosition("I()", 1)
+    End Sub
+
+    Private Sub cmdClear_Click(sender As Object, e As EventArgs) Handles cmdClear.Click
+        ucrReceiverExpressionExplanatoryModelForLocParam.Clear()
+    End Sub
+
+    Private Sub cmdSqrt_Click(sender As Object, e As EventArgs) Handles cmdSqrt.Click
+        ucrReceiverExpressionExplanatoryModelForLocParam.AddToReceiverAtCursorPosition("sqrt( )", 2)
+    End Sub
+
+    Private Sub cmdCos_Click(sender As Object, e As EventArgs) Handles cmdCos.Click
+        ucrReceiverExpressionExplanatoryModelForLocParam.AddToReceiverAtCursorPosition("cos( )", 2)
+    End Sub
+
+    Private Sub cmdLog_Click(sender As Object, e As EventArgs) Handles cmdLog.Click
+        ucrReceiverExpressionExplanatoryModelForLocParam.AddToReceiverAtCursorPosition("log( )", 2)
+    End Sub
+
+    Private Sub cmdSin_Click(sender As Object, e As EventArgs) Handles cmdSin.Click
+        ucrReceiverExpressionExplanatoryModelForLocParam.AddToReceiverAtCursorPosition("sin( )", 2)
+    End Sub
+
+    Private Sub cmdExp_Click(sender As Object, e As EventArgs) Handles cmdExp.Click
+        ucrReceiverExpressionExplanatoryModelForLocParam.AddToReceiverAtCursorPosition("exp( )", 2)
+    End Sub
+
+    Private Sub cmdTan_Click(sender As Object, e As EventArgs) Handles cmdTan.Click
+        ucrReceiverExpressionExplanatoryModelForLocParam.AddToReceiverAtCursorPosition("tan( )", 2)
+    End Sub
+
+
+
+    Private Sub ucrReceiverVariable_ControlValueChanged(ucrChangedControl As ucrCore) Handles ucrReceiverVariable.ControlValueChanged
+        TestOkEnabled()
+    End Sub
+
+    Private Sub control_ControlValueChanged(ucrChangedControl As ucrCore) Handles ucrInputExtremes.ControlValueChanged, ucrReceiverVariable.ControlValueChanged, ucrChkExplanatoryModelForLocationParameter.ControlValueChanged, ucrInputThresholdforLocation.ControlValueChanged
+        ParameterControl()
+        ucrTryModelling.ucrInputTryMessage.SetName("")
+        ucrTryModelling.ucrInputTryMessage.txtInput.BackColor = Color.White
+        ucrTryModelling.ucrInputTryMessage.txtInput.Controls.Clear()
+    End Sub
+
+    Private Sub ParameterControl()
+        If ucrInputExtremes.GetText() = "GP" OrElse ucrInputExtremes.GetText() = "PP" OrElse ucrInputExtremes.GetText() = "Exponential" Then
+            ucrInputThresholdforLocation.Visible = True
+            clsFevdFunction.AddParameter("threshold", ucrInputThresholdforLocation.GetText(), iPosition:=4)
         Else
-            cboMethodOfMoments.Visible = False
+            ucrInputThresholdforLocation.Visible = False
+            clsFevdFunction.RemoveParameterByName("threshold")
         End If
+        If ucrChkExplanatoryModelForLocationParameter.Checked Then
+            If (ucrInputExtremes.GetText() = "GP" OrElse ucrInputExtremes.GetText() = "PP" OrElse ucrInputExtremes.GetText() = "Exponential") Then
+                clsFevdFunction.AddParameter("scale.fun", clsROperatorParameter:=clsLocationParamOperator, iPosition:=3)
+                clsFevdFunction.RemoveParameterByName("location.fun")
+            ElseIf (ucrInputExtremes.GetText() = "Gumbel" OrElse ucrInputExtremes.GetText() = "GEV") Then
+                clsFevdFunction.AddParameter("location.fun", clsROperatorParameter:=clsLocationParamOperator, iPosition:=5)
+                clsFevdFunction.RemoveParameterByName("scale.fun")
+            End If
+            If Not bResettingDialogue Then
+                clsLocationScaleResetOperator.AddParameter("scaleLocation", clsROperatorParameter:=clsLocationParamOperator, iPosition:=0)
+            End If
+        Else
+            ucrReceiverVariable.SetMeAsReceiver()
+            clsFevdFunction.RemoveParameterByName("scale.fun")
+            clsFevdFunction.RemoveParameterByName("location.fun")
+            clsLocationScaleResetOperator.RemoveParameterByName("scaleLocation")
+        End If
+        HideAndShowKeyboard()
     End Sub
 
-    Private Sub ucrReceiverDataToFit_Leave(sender As Object, e As EventArgs) 
-        ucrBase.clsRsyntax.AddParameter("xdat", clsRFunctionParameter:=ucrReceiverDataToFit.GetVariables())
+    Private Sub HideAndShowKeyboard()
+        If ucrChkExplanatoryModelForLocationParameter.Checked Then
+            grpFirstCalc.Visible = True
+            grpSecondCalc.Visible = True
+        Else
+            grpFirstCalc.Visible = False
+            grpSecondCalc.Visible = False
+        End If
     End Sub
 End Class
