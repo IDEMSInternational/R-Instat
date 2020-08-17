eb821dec42e89a51148b1118ec5f85b5f1b48225
ivanluv <luvisiaivan@gmail.com>
2020-08-17 15:18:15 +0300

    Design changes for ExtremesDisplay Sub dialogue

--- instat/sdgExtremesDisplayOptions.Designer.vb
+++ instat/sdgExtremesDisplayOptions.Designer.vb
@@ -22,20 +22,207 @@ Partial Class sdgExtremesDisplayOptions
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.rdoHist = New System.Windows.Forms.RadioButton()
+        Me.rdoZPlot = New System.Windows.Forms.RadioButton()
+        Me.rdoProbprob = New System.Windows.Forms.RadioButton()
+        Me.rdoQQPlot = New System.Windows.Forms.RadioButton()
+        Me.rdoQQ2 = New System.Windows.Forms.RadioButton()
+        Me.rdoPrimary = New System.Windows.Forms.RadioButton()
+        Me.grpPlots = New System.Windows.Forms.GroupBox()
+        Me.ucrSavePlots = New instat.ucrSave()
+        Me.rdoNoPlot = New System.Windows.Forms.RadioButton()
+        Me.rdoTrace = New System.Windows.Forms.RadioButton()
+        Me.rdoRlplot = New System.Windows.Forms.RadioButton()
+        Me.rdoDensity = New System.Windows.Forms.RadioButton()
+        Me.ucrPnlExtreme = New instat.UcrPanel()
+        Me.ucrBase = New instat.ucrButtonsSubdialogue()
+        Me.grpPlots.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'rdoHist
+        '
+        Me.rdoHist.AutoSize = True
+        Me.rdoHist.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.rdoHist.Location = New System.Drawing.Point(149, 71)
+        Me.rdoHist.Name = "rdoHist"
+        Me.rdoHist.Size = New System.Drawing.Size(64, 17)
+        Me.rdoHist.TabIndex = 14
+        Me.rdoHist.Text = "Hist Plot"
+        Me.rdoHist.UseVisualStyleBackColor = True
+        '
+        'rdoZPlot
+        '
+        Me.rdoZPlot.AutoSize = True
+        Me.rdoZPlot.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.rdoZPlot.Location = New System.Drawing.Point(149, 48)
+        Me.rdoZPlot.Name = "rdoZPlot"
+        Me.rdoZPlot.Size = New System.Drawing.Size(70, 17)
+        Me.rdoZPlot.TabIndex = 12
+        Me.rdoZPlot.Text = "Zplot Plot"
+        Me.rdoZPlot.UseVisualStyleBackColor = True
+        '
+        'rdoProbprob
+        '
+        Me.rdoProbprob.AutoSize = True
+        Me.rdoProbprob.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.rdoProbprob.Location = New System.Drawing.Point(17, 71)
+        Me.rdoProbprob.Name = "rdoProbprob"
+        Me.rdoProbprob.Size = New System.Drawing.Size(89, 17)
+        Me.rdoProbprob.TabIndex = 9
+        Me.rdoProbprob.Text = "Probprob Plot"
+        Me.rdoProbprob.UseVisualStyleBackColor = True
+        '
+        'rdoQQPlot
+        '
+        Me.rdoQQPlot.AutoSize = True
+        Me.rdoQQPlot.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.rdoQQPlot.Location = New System.Drawing.Point(149, 25)
+        Me.rdoQQPlot.Name = "rdoQQPlot"
+        Me.rdoQQPlot.Size = New System.Drawing.Size(62, 17)
+        Me.rdoQQPlot.TabIndex = 11
+        Me.rdoQQPlot.Text = "QQ-Plot"
+        Me.rdoQQPlot.UseVisualStyleBackColor = True
+        '
+        'rdoQQ2
+        '
+        Me.rdoQQ2.AutoSize = True
+        Me.rdoQQ2.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.rdoQQ2.Location = New System.Drawing.Point(149, 117)
+        Me.rdoQQ2.Name = "rdoQQ2"
+        Me.rdoQQ2.Size = New System.Drawing.Size(68, 17)
+        Me.rdoQQ2.TabIndex = 10
+        Me.rdoQQ2.Text = "QQ2 Plot"
+        Me.rdoQQ2.UseVisualStyleBackColor = True
+        '
+        'rdoPrimary
+        '
+        Me.rdoPrimary.AutoSize = True
+        Me.rdoPrimary.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.rdoPrimary.Location = New System.Drawing.Point(17, 25)
+        Me.rdoPrimary.Name = "rdoPrimary"
+        Me.rdoPrimary.Size = New System.Drawing.Size(59, 17)
+        Me.rdoPrimary.TabIndex = 8
+        Me.rdoPrimary.Text = "Primary"
+        Me.rdoPrimary.UseVisualStyleBackColor = True
+        '
+        'grpPlots
+        '
+        Me.grpPlots.Controls.Add(Me.ucrSavePlots)
+        Me.grpPlots.Controls.Add(Me.rdoNoPlot)
+        Me.grpPlots.Controls.Add(Me.rdoTrace)
+        Me.grpPlots.Controls.Add(Me.rdoRlplot)
+        Me.grpPlots.Controls.Add(Me.rdoHist)
+        Me.grpPlots.Controls.Add(Me.rdoDensity)
+        Me.grpPlots.Controls.Add(Me.rdoZPlot)
+        Me.grpPlots.Controls.Add(Me.rdoProbprob)
+        Me.grpPlots.Controls.Add(Me.rdoQQPlot)
+        Me.grpPlots.Controls.Add(Me.rdoQQ2)
+        Me.grpPlots.Controls.Add(Me.rdoPrimary)
+        Me.grpPlots.Controls.Add(Me.ucrPnlExtreme)
+        Me.grpPlots.Location = New System.Drawing.Point(12, 12)
+        Me.grpPlots.Name = "grpPlots"
+        Me.grpPlots.Size = New System.Drawing.Size(296, 171)
+        Me.grpPlots.TabIndex = 15
+        Me.grpPlots.TabStop = False
+        Me.grpPlots.Text = "Plots"
+        '
+        'ucrSavePlots
+        '
+        Me.ucrSavePlots.Location = New System.Drawing.Point(17, 139)
+        Me.ucrSavePlots.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.ucrSavePlots.Name = "ucrSavePlots"
+        Me.ucrSavePlots.Size = New System.Drawing.Size(261, 24)
+        Me.ucrSavePlots.TabIndex = 18
+        '
+        'rdoNoPlot
+        '
+        Me.rdoNoPlot.AutoSize = True
+        Me.rdoNoPlot.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.rdoNoPlot.Location = New System.Drawing.Point(17, 48)
+        Me.rdoNoPlot.Name = "rdoNoPlot"
+        Me.rdoNoPlot.Size = New System.Drawing.Size(60, 17)
+        Me.rdoNoPlot.TabIndex = 17
+        Me.rdoNoPlot.Text = "No Plot"
+        Me.rdoNoPlot.UseVisualStyleBackColor = True
+        '
+        'rdoTrace
+        '
+        Me.rdoTrace.AutoSize = True
+        Me.rdoTrace.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.rdoTrace.Location = New System.Drawing.Point(17, 117)
+        Me.rdoTrace.Name = "rdoTrace"
+        Me.rdoTrace.Size = New System.Drawing.Size(74, 17)
+        Me.rdoTrace.TabIndex = 16
+        Me.rdoTrace.Text = "Trace Plot"
+        Me.rdoTrace.UseVisualStyleBackColor = True
+        '
+        'rdoRlplot
+        '
+        Me.rdoRlplot.AutoSize = True
+        Me.rdoRlplot.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.rdoRlplot.Location = New System.Drawing.Point(17, 94)
+        Me.rdoRlplot.Name = "rdoRlplot"
+        Me.rdoRlplot.Size = New System.Drawing.Size(56, 17)
+        Me.rdoRlplot.TabIndex = 15
+        Me.rdoRlplot.Text = "Rl Plot"
+        Me.rdoRlplot.UseVisualStyleBackColor = True
+        '
+        'rdoDensity
+        '
+        Me.rdoDensity.AutoSize = True
+        Me.rdoDensity.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.rdoDensity.Location = New System.Drawing.Point(149, 94)
+        Me.rdoDensity.Name = "rdoDensity"
+        Me.rdoDensity.Size = New System.Drawing.Size(81, 17)
+        Me.rdoDensity.TabIndex = 13
+        Me.rdoDensity.Text = "Density Plot"
+        Me.rdoDensity.UseVisualStyleBackColor = True
+        '
+        'ucrPnlExtreme
+        '
+        Me.ucrPnlExtreme.Location = New System.Drawing.Point(6, 14)
+        Me.ucrPnlExtreme.Name = "ucrPnlExtreme"
+        Me.ucrPnlExtreme.Size = New System.Drawing.Size(276, 151)
+        Me.ucrPnlExtreme.TabIndex = 0
+        '
+        'ucrBase
+        '
+        Me.ucrBase.Location = New System.Drawing.Point(81, 189)
+        Me.ucrBase.Name = "ucrBase"
+        Me.ucrBase.Size = New System.Drawing.Size(142, 30)
+        Me.ucrBase.TabIndex = 16
         '
         'sdgExtremesDisplayOptions
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(323, 385)
+        Me.ClientSize = New System.Drawing.Size(323, 235)
+        Me.Controls.Add(Me.grpPlots)
+        Me.Controls.Add(Me.ucrBase)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "sdgExtremesDisplayOptions"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "sdgExtremesDisplayOptions"
+        Me.grpPlots.ResumeLayout(False)
+        Me.grpPlots.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents rdoHist As RadioButton
+    Friend WithEvents rdoZPlot As RadioButton
+    Friend WithEvents rdoProbprob As RadioButton
+    Friend WithEvents rdoQQPlot As RadioButton
+    Friend WithEvents rdoQQ2 As RadioButton
+    Friend WithEvents rdoPrimary As RadioButton
+    Friend WithEvents grpPlots As GroupBox
+    Friend WithEvents ucrSavePlots As ucrSave
+    Friend WithEvents rdoNoPlot As RadioButton
+    Friend WithEvents rdoTrace As RadioButton
+    Friend WithEvents rdoRlplot As RadioButton
+    Friend WithEvents rdoDensity As RadioButton
+    Friend WithEvents ucrPnlExtreme As UcrPanel
+    Friend WithEvents ucrBase As ucrButtonsSubdialogue
 End Class
