98206c9624fec0887e093bcdeb0b59288fdcc363
Wycklife Bonyo <bonyowycklife@gmail.com>
2020-08-27 17:28:24 +0300

    Setting data types for the receiver element

--- instat/dlgInventoryPlot.vb
+++ instat/dlgInventoryPlot.vb
@@ -52,6 +52,7 @@ Public Class dlgInventoryPlot
         ucrReceiverElements.Selector = ucrInventoryPlotSelector
         ucrReceiverElements.SetParameterIsString()
         ucrReceiverElements.strSelectorHeading = "Numerics"
+        ucrReceiverElements.SetIncludedDataTypes({"numeric"})
         ucrReceiverElements.SetClimaticType({"rain", "temp_min", "temp_max", "sunshine_hours", "cloud_cover", "wind_speed", "wind_direction", "radiation"})
         ucrReceiverElements.bAutoFill = True
         ucrReceiverElements.bWithQuotes = False
