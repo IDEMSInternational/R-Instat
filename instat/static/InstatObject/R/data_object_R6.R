50c6a14baf68fe8668510cadc5539e53799d307d
shadrack kibet <shadrackkibet36@outlook.com>
2020-09-06 14:19:16 +0300

    adding wrapper to imputeTS graphics options

--- instat/static/InstatObject/R/data_object_R6.R
+++ instat/static/InstatObject/R/data_object_R6.R
@@ -3650,4 +3650,51 @@ DataSheet$set("public", "patch_climate_element", function(date_col_name = "", va
 }
 )
 
+DataSheet$set("public", "visualize_element_na", function(element_col_name, element_col_name_imputed, station_col_name, x_axis_labels_col_name, ncol = 2, type = "distribution", xlab = NULL, ylab = NULL, legend = TRUE, orientation = "horizontal", interval_size = 1461){
+  curr_data <- self$get_data_frame()
+  if(!missing(station_col_name)){station_col <- self$get_columns_from_data(station_col_name);station_names <- unique(station_col)}
+  if(!missing(element_col_name)){element_col <- self$get_columns_from_data(element_col_name)}
+  if(!missing(element_col_name_imputed)){element_imputed_col <- self$get_columns_from_data(element_col_name_imputed)}
+  if(!(type %in% c("distribution", "gapsize", "interval", "imputation"))) stop(type, " must be either distribution, gapsize or imputation")
+  plt_list <- list()
+  if(type == "distribution"){
+    if(!missing(station_col_name)){
+      for (i in seq_along(station_names)) {
+        temp_data <- curr_data[station_col==station_names[i],] 
+        plt_list[[i]] <- imputeTS::ggplot_na_distribution(x = temp_data[,element_col_name], x_axis_labels = temp_data[,x_axis_labels_col_name], title = station_names[i], xlab = xlab, ylab = ylab) 
+      }
+    }else{
+      imputeTS::ggplot_na_distribution(x = element_col, x_axis_labels = curr_data[,x_axis_labels_col_name], xlab = xlab, ylab = ylab) 
+    }
+  }else if (type == "gapsize"){
+    if(!missing(station_col_name)){
+      for (i in seq_along(station_names)) {
+        temp_data <- curr_data[station_col==station_names[i],] 
+        plt_list[[i]] <- imputeTS::ggplot_na_gapsize(x = temp_data[,element_col_name], include_total = TRUE, title =paste0(station_names[i], ":Occurrence of gap sizes"), xlab = xlab, ylab = ylab, legend = legend, orientation = orientation) 
+      }
+    }else{
+      imputeTS::ggplot_na_gapsize(x = element_col, include_total = TRUE, xlab = xlab, ylab = ylab, legend = legend, orientation = orientation) 
+    }
+  }else if(type == "interval"){
+      if(!missing(station_col_name)){
+        for (i in seq_along(station_names)) {
+          temp_data <- curr_data[station_col==station_names[i],] 
+          plt_list[[i]] <- imputeTS::ggplot_na_intervals(x = temp_data[,element_col_name], title = paste0(station_names[i], ":Missing Values per Interval"), ylab = ylab, interval_size = interval_size) 
+        }
+      }else{
+        imputeTS::ggplot_na_intervals(x = element_col, ylab = ylab, interval_size = interval_size) 
+      }
+  }else if (type == "imputation"){
+    if(!missing(station_col_name)){
+      for (i in seq_along(station_names)) {
+        temp_data <- curr_data[station_col==station_names[i],] 
+        plt_list[[i]] <- imputeTS::ggplot_na_imputations(x_with_na = temp_data[,element_col_name], x_with_imputations = temp_data[,element_col_name_imputed], x_axis_labels = temp_data[,x_axis_labels_col_name], title = station_names[i], xlab = xlab, ylab = ylab, legend = legend) 
+      }
+    }else{
+      imputeTS::ggplot_na_imputations(x_with_na = element_col, x_with_imputations = element_imputed_col, x_axis_labels = curr_data[,x_axis_labels_col_name], xlab = xlab, ylab = ylab, legend = legend) 
+    }
+  }
+  if(!missing(station_col_name)) gridExtra::grid.arrange(grobs = plt_list, ncol = ncol)
+}
+)
 
