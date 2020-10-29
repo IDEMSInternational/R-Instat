60927c23845d82188e934efda65188f9eb669931
haward ketoyo <hketoyo@gmail.com>
2020-10-29 11:25:55 +0000

    Added controls to dlgPolarFrequency and implemented R code.

--- instat/dlgPolarFrequency.Designer.vb
+++ instat/dlgPolarFrequency.Designer.vb
@@ -22,22 +22,187 @@ Partial Class dlgPolarFrequency
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.UcrButtons1 = New instat.ucrButtons()
+        Me.ucrBase = New instat.ucrButtons()
+        Me.ucrSelectorPolarFrequency = New instat.ucrSelectorByDataFrameAddRemove()
+        Me.lblPollutant = New System.Windows.Forms.Label()
+        Me.lblColor = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.ucrReceiverPollutant = New instat.ucrReceiverSingle()
+        Me.ucrInputStatistic = New instat.ucrInputComboBox()
+        Me.ucrInputType = New instat.ucrInputComboBox()
+        Me.ucrInputPosition = New instat.ucrInputComboBox()
+        Me.ucrInputColor = New instat.ucrInputComboBox()
+        Me.ucrNudMinimumBins = New instat.ucrNud()
+        Me.ucrChkTransform = New instat.ucrCheck()
         Me.SuspendLayout()
         '
-        'UcrButtons1
+        'ucrBase
         '
-        Me.UcrButtons1.Location = New System.Drawing.Point(10, 329)
-        Me.UcrButtons1.Name = "UcrButtons1"
-        Me.UcrButtons1.Size = New System.Drawing.Size(410, 52)
-        Me.UcrButtons1.TabIndex = 0
+        Me.ucrBase.Location = New System.Drawing.Point(10, 329)
+        Me.ucrBase.Name = "ucrBase"
+        Me.ucrBase.Size = New System.Drawing.Size(410, 52)
+        Me.ucrBase.TabIndex = 0
+        '
+        'ucrSelectorPolarFrequency
+        '
+        Me.ucrSelectorPolarFrequency.bDropUnusedFilterLevels = False
+        Me.ucrSelectorPolarFrequency.bShowHiddenColumns = False
+        Me.ucrSelectorPolarFrequency.bUseCurrentFilter = True
+        Me.ucrSelectorPolarFrequency.Location = New System.Drawing.Point(14, 1)
+        Me.ucrSelectorPolarFrequency.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrSelectorPolarFrequency.Name = "ucrSelectorPolarFrequency"
+        Me.ucrSelectorPolarFrequency.Size = New System.Drawing.Size(210, 180)
+        Me.ucrSelectorPolarFrequency.TabIndex = 1
+        '
+        'lblPollutant
+        '
+        Me.lblPollutant.AutoSize = True
+        Me.lblPollutant.Location = New System.Drawing.Point(280, 36)
+        Me.lblPollutant.Name = "lblPollutant"
+        Me.lblPollutant.Size = New System.Drawing.Size(48, 13)
+        Me.lblPollutant.TabIndex = 2
+        Me.lblPollutant.Text = "Pollutant"
+        '
+        'lblColor
+        '
+        Me.lblColor.AutoSize = True
+        Me.lblColor.Location = New System.Drawing.Point(12, 210)
+        Me.lblColor.Name = "lblColor"
+        Me.lblColor.Size = New System.Drawing.Size(34, 13)
+        Me.lblColor.TabIndex = 3
+        Me.lblColor.Text = "Color:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(11, 231)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(68, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Key Position:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(254, 210)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(34, 13)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Type:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(254, 257)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(74, 13)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "Minimum Bins:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(254, 233)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(47, 13)
+        Me.Label6.TabIndex = 7
+        Me.Label6.Text = "Statistic:"
+        '
+        'ucrReceiverPollutant
+        '
+        Me.ucrReceiverPollutant.frmParent = Me
+        Me.ucrReceiverPollutant.Location = New System.Drawing.Point(283, 60)
+        Me.ucrReceiverPollutant.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverPollutant.Name = "ucrReceiverPollutant"
+        Me.ucrReceiverPollutant.Selector = Nothing
+        Me.ucrReceiverPollutant.Size = New System.Drawing.Size(120, 20)
+        Me.ucrReceiverPollutant.strNcFilePath = ""
+        Me.ucrReceiverPollutant.TabIndex = 8
+        Me.ucrReceiverPollutant.ucrSelector = Nothing
+        '
+        'ucrInputStatistic
+        '
+        Me.ucrInputStatistic.AddQuotesIfUnrecognised = True
+        Me.ucrInputStatistic.GetSetSelectedIndex = -1
+        Me.ucrInputStatistic.IsReadOnly = False
+        Me.ucrInputStatistic.Location = New System.Drawing.Point(314, 229)
+        Me.ucrInputStatistic.Name = "ucrInputStatistic"
+        Me.ucrInputStatistic.Size = New System.Drawing.Size(85, 21)
+        Me.ucrInputStatistic.TabIndex = 9
+        '
+        'ucrInputType
+        '
+        Me.ucrInputType.AddQuotesIfUnrecognised = True
+        Me.ucrInputType.GetSetSelectedIndex = -1
+        Me.ucrInputType.IsReadOnly = False
+        Me.ucrInputType.Location = New System.Drawing.Point(314, 202)
+        Me.ucrInputType.Name = "ucrInputType"
+        Me.ucrInputType.Size = New System.Drawing.Size(85, 21)
+        Me.ucrInputType.TabIndex = 10
+        '
+        'ucrInputPosition
+        '
+        Me.ucrInputPosition.AddQuotesIfUnrecognised = True
+        Me.ucrInputPosition.GetSetSelectedIndex = -1
+        Me.ucrInputPosition.IsReadOnly = False
+        Me.ucrInputPosition.Location = New System.Drawing.Point(94, 228)
+        Me.ucrInputPosition.Name = "ucrInputPosition"
+        Me.ucrInputPosition.Size = New System.Drawing.Size(68, 21)
+        Me.ucrInputPosition.TabIndex = 11
+        '
+        'ucrInputColor
+        '
+        Me.ucrInputColor.AddQuotesIfUnrecognised = True
+        Me.ucrInputColor.GetSetSelectedIndex = -1
+        Me.ucrInputColor.IsReadOnly = False
+        Me.ucrInputColor.Location = New System.Drawing.Point(94, 201)
+        Me.ucrInputColor.Name = "ucrInputColor"
+        Me.ucrInputColor.Size = New System.Drawing.Size(68, 21)
+        Me.ucrInputColor.TabIndex = 12
+        '
+        'ucrNudMinimumBins
+        '
+        Me.ucrNudMinimumBins.DecimalPlaces = New Decimal(New Integer() {0, 0, 0, 0})
+        Me.ucrNudMinimumBins.Increment = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.ucrNudMinimumBins.Location = New System.Drawing.Point(348, 257)
+        Me.ucrNudMinimumBins.Maximum = New Decimal(New Integer() {100, 0, 0, 0})
+        Me.ucrNudMinimumBins.Minimum = New Decimal(New Integer() {0, 0, 0, 0})
+        Me.ucrNudMinimumBins.Name = "ucrNudMinimumBins"
+        Me.ucrNudMinimumBins.Size = New System.Drawing.Size(50, 20)
+        Me.ucrNudMinimumBins.TabIndex = 16
+        Me.ucrNudMinimumBins.Value = New Decimal(New Integer() {0, 0, 0, 0})
+        '
+        'ucrChkTransform
+        '
+        Me.ucrChkTransform.Checked = False
+        Me.ucrChkTransform.Location = New System.Drawing.Point(14, 257)
+        Me.ucrChkTransform.Name = "ucrChkTransform"
+        Me.ucrChkTransform.Size = New System.Drawing.Size(100, 20)
+        Me.ucrChkTransform.TabIndex = 17
         '
         'dlgPolarFrequency
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(426, 396)
-        Me.Controls.Add(Me.UcrButtons1)
+        Me.Controls.Add(Me.ucrChkTransform)
+        Me.Controls.Add(Me.ucrNudMinimumBins)
+        Me.Controls.Add(Me.ucrInputColor)
+        Me.Controls.Add(Me.ucrInputPosition)
+        Me.Controls.Add(Me.ucrInputType)
+        Me.Controls.Add(Me.ucrInputStatistic)
+        Me.Controls.Add(Me.ucrReceiverPollutant)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblColor)
+        Me.Controls.Add(Me.lblPollutant)
+        Me.Controls.Add(Me.ucrSelectorPolarFrequency)
+        Me.Controls.Add(Me.ucrBase)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.MaximizeBox = False
         Me.MinimizeBox = False
@@ -45,8 +210,23 @@ Partial Class dlgPolarFrequency
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Polar Frequency"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents UcrButtons1 As ucrButtons
+    Friend WithEvents ucrBase As ucrButtons
+    Friend WithEvents ucrSelectorPolarFrequency As ucrSelectorByDataFrameAddRemove
+    Friend WithEvents lblPollutant As Label
+    Friend WithEvents lblColor As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents ucrReceiverPollutant As ucrReceiverSingle
+    Friend WithEvents ucrChkTransform As ucrCheck
+    Friend WithEvents ucrNudMinimumBins As ucrNud
+    Friend WithEvents ucrInputColor As ucrInputComboBox
+    Friend WithEvents ucrInputPosition As ucrInputComboBox
+    Friend WithEvents ucrInputType As ucrInputComboBox
+    Friend WithEvents ucrInputStatistic As ucrInputComboBox
 End Class
--- instat/dlgPolarFrequency.vb
+++ instat/dlgPolarFrequency.vb
@@ -1,3 +1,142 @@
-﻿Public Class dlgPolarFrequency
+﻿'R-Instat
+' Copyright (C) 2015-2017
+'
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+'
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License 
+' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+Imports instat.Translations
+Public Class dlgPolarFrequency
+    Private bFirstLoad As Boolean = True
+    Private bReset As Boolean = True
+
+    'Functions 
+    Private clsPolarFrequencyFunction As RFunction
+
+    Private Sub dlgPolarFrequency_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If bFirstLoad Then
+            InitiliseDialog()
+            bFirstLoad = False
+        End If
+
+        If bReset Then
+            SetDefaults()
+        End If
+        SetRCodeForControls(bReset)
+        bReset = False
+        TestOkEnabled()
+    End Sub
+
+    Private Sub InitiliseDialog()
+        Dim dctStatistic As New Dictionary(Of String, String)
+        Dim dctType As New Dictionary(Of String, String)
+        Dim dctPosition As New Dictionary(Of String, String)
+        Dim dctColor As New Dictionary(Of String, String)
+
+        ucrBase.clsRsyntax.iCallType = 3
+
+        ucrSelectorPolarFrequency.SetParameter(New RParameter("mydata", 0))
+        ucrSelectorPolarFrequency.SetParameterIsrfunction()
+
+        ucrReceiverPollutant.SetParameter(New RParameter("pollutant", 1))
+        ucrReceiverPollutant.Selector = ucrSelectorPolarFrequency
+        ucrReceiverPollutant.SetParameterIsString()
+
+        ucrChkTransform.SetText("Transform")
+        ucrChkTransform.SetParameter(New RParameter("trans", 2))
+        'ucrChkTransform.SetValuesCheckedAndUnchecked("TRUE", "FALSE")
+        ucrChkTransform.AddParameterPresentCondition(True, "trans")
+        ucrChkTransform.AddParameterPresentCondition(False, "trans", False)
+
+        ucrInputType.SetParameter(New RParameter("type", 3))
+        dctType.Add("Default", Chr(34) & "default" & Chr(34))
+        dctType.Add("Year", Chr(34) & "year" & Chr(34))
+        dctType.Add("Hour", Chr(34) & "hour" & Chr(34))
+        dctType.Add("Month", Chr(34) & "month" & Chr(34))
+        dctType.Add("Season", Chr(34) & "season" & Chr(34))
+        dctType.Add("Weekday", Chr(34) & "weekday" & Chr(34))
+        dctType.Add("Weekend", Chr(34) & "weekend" & Chr(34))
+        dctType.Add("Monthyear", Chr(34) & "monthyear" & Chr(34))
+        dctType.Add("Daylight", Chr(34) & "daylight" & Chr(34))
+        dctType.Add("DST", Chr(34) & "dst" & Chr(34))
+        ucrInputType.SetItems(dctType)
+        ucrInputType.SetDropDownStyleAsNonEditable()
+
+        ucrNudMinimumBins.SetParameter(New RParameter("min.bin", 4))
+        ucrNudMinimumBins.SetMinMax(1,)
+
+        ucrInputPosition.SetParameter(New RParameter("key.position", 5))
+        dctPosition.Add("Top", Chr(34) & "top" & Chr(34))
+        dctPosition.Add("Right", Chr(34) & "right" & Chr(34))
+        dctPosition.Add("Bottom", Chr(34) & "bottom" & Chr(34))
+        dctPosition.Add("Left", Chr(34) & "left" & Chr(34))
+        ucrInputPosition.SetItems(dctPosition)
+        ucrInputPosition.SetDropDownStyleAsNonEditable()
+
+        ucrInputStatistic.SetParameter(New RParameter("statistic", 6))
+        dctStatistic.Add("Frequency", Chr(34) & "frequency" & Chr(34))
+        dctStatistic.Add("Mean", Chr(34) & "mean" & Chr(34))
+        dctStatistic.Add("Median", Chr(34) & "median" & Chr(34))
+        dctStatistic.Add("Max", Chr(34) & "max" & Chr(34))
+        dctStatistic.Add("Stdev", Chr(34) & "stdev" & Chr(34))
+        dctStatistic.Add("Weighted mean", Chr(34) & "weighted.mean" & Chr(34))
+        ucrInputStatistic.SetItems(dctStatistic)
+        ucrInputStatistic.SetDropDownStyleAsNonEditable()
+
+        ucrInputColor.SetParameter(New RParameter("cols", 7))
+        dctColor.Add("Default", Chr(34) & "default" & Chr(34))
+        dctColor.Add("Increment", Chr(34) & "increment" & Chr(34))
+        dctColor.Add("Heat", Chr(34) & "heat" & Chr(34))
+        dctColor.Add("Jet", Chr(34) & "jet" & Chr(34))
+        ucrInputColor.SetItems(dctColor)
+        ucrInputColor.SetDropDownStyleAsNonEditable()
+    End Sub
+
+    Private Sub SetDefaults()
+        clsPolarFrequencyFunction = New RFunction
+
+        clsPolarFrequencyFunction.AddParameter("statistic", Chr(34) & "frequency" & Chr(34), iPosition:=6)
+        clsPolarFrequencyFunction.AddParameter("type", Chr(34) & "default" & Chr(34), iPosition:=3)
+        clsPolarFrequencyFunction.AddParameter("key.position", Chr(34) & "right" & Chr(34), iPosition:=5)
+        clsPolarFrequencyFunction.AddParameter("cols", Chr(34) & "default" & Chr(34), iPosition:=7)
+
+        ucrSelectorPolarFrequency.Reset()
+        ucrReceiverPollutant.SetMeAsReceiver()
+
+        clsPolarFrequencyFunction.SetPackageName("openair")
+        clsPolarFrequencyFunction.SetRCommand("polarFreq")
+
+        ucrBase.clsRsyntax.SetBaseRFunction(clsPolarFrequencyFunction)
+    End Sub
+
+    Private Sub SetRCodeForControls(bReset As Boolean)
+        SetRCode(Me, ucrBase.clsRsyntax.clsBaseFunction, bReset)
+    End Sub
+
+    Private Sub TestOkEnabled()
+        If ucrReceiverPollutant.IsEmpty Then
+            ucrBase.OKEnabled(False)
+        Else
+            ucrBase.OKEnabled(True)
+        End If
+    End Sub
+
+    Private Sub ucrBase_ClickReset(sender As Object, e As EventArgs) Handles ucrBase.ClickReset
+        SetDefaults()
+        SetRCodeForControls(True)
+        TestOkEnabled()
+    End Sub
+
+    Private Sub ucrReceiverPollutant_ControlValueChanged(ucrChangedControl As ucrCore) Handles ucrReceiverPollutant.ControlValueChanged
+        TestOkEnabled()
+    End Sub
 End Class
