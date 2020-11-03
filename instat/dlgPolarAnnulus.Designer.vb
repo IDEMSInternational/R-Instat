1d26f762e84b895698ecc3671b106df1fc4a6002
haward ketoyo <hketoyo@gmail.com>
2020-11-03 07:52:18 +0000

    Code Implementation Added controls to the dlgPolarAnnulus

--- instat/dlgPolarAnnulus.Designer.vb
+++ instat/dlgPolarAnnulus.Designer.vb
@@ -22,22 +22,317 @@ Partial Class dlgPolarAnnulus
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.UcrButtons1 = New instat.ucrButtons()
+        Me.lblMultiplePollutant = New System.Windows.Forms.Label()
+        Me.lblColor = New System.Windows.Forms.Label()
+        Me.lblType = New System.Windows.Forms.Label()
+        Me.lblStatistic = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.lblWindDirection = New System.Windows.Forms.Label()
+        Me.lbldate = New System.Windows.Forms.Label()
+        Me.lblPercentile = New System.Windows.Forms.Label()
+        Me.lblSinglePollutant = New System.Windows.Forms.Label()
+        Me.ucrReceiverSinglePollutant = New instat.ucrReceiverSingle()
+        Me.ucrChkMultiplePollutants = New instat.ucrCheck()
+        Me.ucrChkNormalize = New instat.ucrCheck()
+        Me.ucrNudPercentile = New instat.ucrNud()
+        Me.ucrReceiverMultiplePollutant = New instat.ucrReceiverMultiple()
+        Me.ucrReceiverDate = New instat.ucrReceiverSingle()
+        Me.ucrReceiverWindDirection = New instat.ucrReceiverSingle()
+        Me.ucrNudMinimumBins = New instat.ucrNud()
+        Me.ucrChckPadDate = New instat.ucrCheck()
+        Me.ucrChckExcludeMissing = New instat.ucrCheck()
+        Me.ucrInputStatistic = New instat.ucrInputComboBox()
+        Me.ucrInputType = New instat.ucrInputComboBox()
+        Me.ucrInputColor = New instat.ucrInputComboBox()
+        Me.ucrInputKeyPosition = New instat.ucrInputComboBox()
+        Me.ucrSelectorPolarAnnulus = New instat.ucrSelectorByDataFrameAddRemove()
+        Me.ucrBase = New instat.ucrButtons()
         Me.SuspendLayout()
         '
-        'UcrButtons1
+        'lblMultiplePollutant
         '
-        Me.UcrButtons1.Location = New System.Drawing.Point(3, 262)
-        Me.UcrButtons1.Name = "UcrButtons1"
-        Me.UcrButtons1.Size = New System.Drawing.Size(410, 52)
-        Me.UcrButtons1.TabIndex = 0
+        Me.lblMultiplePollutant.AutoSize = True
+        Me.lblMultiplePollutant.Location = New System.Drawing.Point(262, 18)
+        Me.lblMultiplePollutant.Name = "lblMultiplePollutant"
+        Me.lblMultiplePollutant.Size = New System.Drawing.Size(51, 13)
+        Me.lblMultiplePollutant.TabIndex = 2
+        Me.lblMultiplePollutant.Text = "Pollutant:"
+        '
+        'lblColor
+        '
+        Me.lblColor.AutoSize = True
+        Me.lblColor.Location = New System.Drawing.Point(12, 263)
+        Me.lblColor.Name = "lblColor"
+        Me.lblColor.Size = New System.Drawing.Size(36, 13)
+        Me.lblColor.TabIndex = 9
+        Me.lblColor.Text = "Collor:"
+        '
+        'lblType
+        '
+        Me.lblType.AutoSize = True
+        Me.lblType.Location = New System.Drawing.Point(12, 289)
+        Me.lblType.Name = "lblType"
+        Me.lblType.Size = New System.Drawing.Size(34, 13)
+        Me.lblType.TabIndex = 11
+        Me.lblType.Text = "Type:"
+        '
+        'lblStatistic
+        '
+        Me.lblStatistic.AutoSize = True
+        Me.lblStatistic.Location = New System.Drawing.Point(12, 314)
+        Me.lblStatistic.Name = "lblStatistic"
+        Me.lblStatistic.Size = New System.Drawing.Size(47, 13)
+        Me.lblStatistic.TabIndex = 13
+        Me.lblStatistic.Text = "Statistic:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(227, 286)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(74, 13)
+        Me.Label5.TabIndex = 20
+        Me.Label5.Text = "Minimum Bins:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(227, 261)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(68, 13)
+        Me.Label6.TabIndex = 18
+        Me.Label6.Text = "Key Position:"
+        '
+        'lblWindDirection
+        '
+        Me.lblWindDirection.AutoSize = True
+        Me.lblWindDirection.Location = New System.Drawing.Point(262, 131)
+        Me.lblWindDirection.Name = "lblWindDirection"
+        Me.lblWindDirection.Size = New System.Drawing.Size(80, 13)
+        Me.lblWindDirection.TabIndex = 6
+        Me.lblWindDirection.Text = "Wind Direction:"
+        '
+        'lbldate
+        '
+        Me.lbldate.AutoSize = True
+        Me.lbldate.Location = New System.Drawing.Point(262, 174)
+        Me.lbldate.Name = "lbldate"
+        Me.lbldate.Size = New System.Drawing.Size(33, 13)
+        Me.lbldate.TabIndex = 7
+        Me.lbldate.Text = "Date:"
+        '
+        'lblPercentile
+        '
+        Me.lblPercentile.AutoSize = True
+        Me.lblPercentile.Location = New System.Drawing.Point(11, 340)
+        Me.lblPercentile.Name = "lblPercentile"
+        Me.lblPercentile.Size = New System.Drawing.Size(57, 13)
+        Me.lblPercentile.TabIndex = 15
+        Me.lblPercentile.Text = "Percentile:"
+        '
+        'lblSinglePollutant
+        '
+        Me.lblSinglePollutant.AutoSize = True
+        Me.lblSinglePollutant.Location = New System.Drawing.Point(262, 18)
+        Me.lblSinglePollutant.Name = "lblSinglePollutant"
+        Me.lblSinglePollutant.Size = New System.Drawing.Size(51, 13)
+        Me.lblSinglePollutant.TabIndex = 5
+        Me.lblSinglePollutant.Text = "Pollutant:"
+        '
+        'ucrReceiverSinglePollutant
+        '
+        Me.ucrReceiverSinglePollutant.frmParent = Me
+        Me.ucrReceiverSinglePollutant.Location = New System.Drawing.Point(264, 37)
+        Me.ucrReceiverSinglePollutant.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverSinglePollutant.Name = "ucrReceiverSinglePollutant"
+        Me.ucrReceiverSinglePollutant.Selector = Nothing
+        Me.ucrReceiverSinglePollutant.Size = New System.Drawing.Size(120, 20)
+        Me.ucrReceiverSinglePollutant.strNcFilePath = ""
+        Me.ucrReceiverSinglePollutant.TabIndex = 1
+        Me.ucrReceiverSinglePollutant.ucrSelector = Nothing
+        '
+        'ucrChkMultiplePollutants
+        '
+        Me.ucrChkMultiplePollutants.Checked = False
+        Me.ucrChkMultiplePollutants.Location = New System.Drawing.Point(15, 231)
+        Me.ucrChkMultiplePollutants.Name = "ucrChkMultiplePollutants"
+        Me.ucrChkMultiplePollutants.Size = New System.Drawing.Size(209, 20)
+        Me.ucrChkMultiplePollutants.TabIndex = 8
+        '
+        'ucrChkNormalize
+        '
+        Me.ucrChkNormalize.Checked = False
+        Me.ucrChkNormalize.Location = New System.Drawing.Point(230, 235)
+        Me.ucrChkNormalize.Name = "ucrChkNormalize"
+        Me.ucrChkNormalize.Size = New System.Drawing.Size(100, 20)
+        Me.ucrChkNormalize.TabIndex = 17
+        '
+        'ucrNudPercentile
+        '
+        Me.ucrNudPercentile.DecimalPlaces = New Decimal(New Integer() {0, 0, 0, 0})
+        Me.ucrNudPercentile.Increment = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.ucrNudPercentile.Location = New System.Drawing.Point(100, 336)
+        Me.ucrNudPercentile.Maximum = New Decimal(New Integer() {100, 0, 0, 0})
+        Me.ucrNudPercentile.Minimum = New Decimal(New Integer() {0, 0, 0, 0})
+        Me.ucrNudPercentile.Name = "ucrNudPercentile"
+        Me.ucrNudPercentile.Size = New System.Drawing.Size(50, 20)
+        Me.ucrNudPercentile.TabIndex = 16
+        Me.ucrNudPercentile.Value = New Decimal(New Integer() {0, 0, 0, 0})
+        '
+        'ucrReceiverMultiplePollutant
+        '
+        Me.ucrReceiverMultiplePollutant.frmParent = Me
+        Me.ucrReceiverMultiplePollutant.Location = New System.Drawing.Point(265, 36)
+        Me.ucrReceiverMultiplePollutant.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverMultiplePollutant.Name = "ucrReceiverMultiplePollutant"
+        Me.ucrReceiverMultiplePollutant.Selector = Nothing
+        Me.ucrReceiverMultiplePollutant.Size = New System.Drawing.Size(120, 95)
+        Me.ucrReceiverMultiplePollutant.strNcFilePath = ""
+        Me.ucrReceiverMultiplePollutant.TabIndex = 2
+        Me.ucrReceiverMultiplePollutant.ucrSelector = Nothing
+        '
+        'ucrReceiverDate
+        '
+        Me.ucrReceiverDate.frmParent = Me
+        Me.ucrReceiverDate.Location = New System.Drawing.Point(265, 189)
+        Me.ucrReceiverDate.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverDate.Name = "ucrReceiverDate"
+        Me.ucrReceiverDate.Selector = Nothing
+        Me.ucrReceiverDate.Size = New System.Drawing.Size(120, 20)
+        Me.ucrReceiverDate.strNcFilePath = ""
+        Me.ucrReceiverDate.TabIndex = 4
+        Me.ucrReceiverDate.ucrSelector = Nothing
+        '
+        'ucrReceiverWindDirection
+        '
+        Me.ucrReceiverWindDirection.frmParent = Me
+        Me.ucrReceiverWindDirection.Location = New System.Drawing.Point(265, 151)
+        Me.ucrReceiverWindDirection.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverWindDirection.Name = "ucrReceiverWindDirection"
+        Me.ucrReceiverWindDirection.Selector = Nothing
+        Me.ucrReceiverWindDirection.Size = New System.Drawing.Size(120, 20)
+        Me.ucrReceiverWindDirection.strNcFilePath = ""
+        Me.ucrReceiverWindDirection.TabIndex = 3
+        Me.ucrReceiverWindDirection.ucrSelector = Nothing
+        '
+        'ucrNudMinimumBins
+        '
+        Me.ucrNudMinimumBins.DecimalPlaces = New Decimal(New Integer() {0, 0, 0, 0})
+        Me.ucrNudMinimumBins.Increment = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.ucrNudMinimumBins.Location = New System.Drawing.Point(319, 283)
+        Me.ucrNudMinimumBins.Maximum = New Decimal(New Integer() {100, 0, 0, 0})
+        Me.ucrNudMinimumBins.Minimum = New Decimal(New Integer() {0, 0, 0, 0})
+        Me.ucrNudMinimumBins.Name = "ucrNudMinimumBins"
+        Me.ucrNudMinimumBins.Size = New System.Drawing.Size(50, 20)
+        Me.ucrNudMinimumBins.TabIndex = 21
+        Me.ucrNudMinimumBins.Value = New Decimal(New Integer() {0, 0, 0, 0})
+        '
+        'ucrChckPadDate
+        '
+        Me.ucrChckPadDate.Checked = False
+        Me.ucrChckPadDate.Location = New System.Drawing.Point(230, 333)
+        Me.ucrChckPadDate.Name = "ucrChckPadDate"
+        Me.ucrChckPadDate.Size = New System.Drawing.Size(154, 20)
+        Me.ucrChckPadDate.TabIndex = 23
+        '
+        'ucrChckExcludeMissing
+        '
+        Me.ucrChckExcludeMissing.Checked = False
+        Me.ucrChckExcludeMissing.Location = New System.Drawing.Point(230, 307)
+        Me.ucrChckExcludeMissing.Name = "ucrChckExcludeMissing"
+        Me.ucrChckExcludeMissing.Size = New System.Drawing.Size(155, 20)
+        Me.ucrChckExcludeMissing.TabIndex = 22
+        '
+        'ucrInputStatistic
+        '
+        Me.ucrInputStatistic.AddQuotesIfUnrecognised = True
+        Me.ucrInputStatistic.GetSetSelectedIndex = -1
+        Me.ucrInputStatistic.IsReadOnly = False
+        Me.ucrInputStatistic.Location = New System.Drawing.Point(67, 311)
+        Me.ucrInputStatistic.Name = "ucrInputStatistic"
+        Me.ucrInputStatistic.Size = New System.Drawing.Size(83, 21)
+        Me.ucrInputStatistic.TabIndex = 14
+        '
+        'ucrInputType
+        '
+        Me.ucrInputType.AddQuotesIfUnrecognised = True
+        Me.ucrInputType.GetSetSelectedIndex = -1
+        Me.ucrInputType.IsReadOnly = False
+        Me.ucrInputType.Location = New System.Drawing.Point(67, 285)
+        Me.ucrInputType.Name = "ucrInputType"
+        Me.ucrInputType.Size = New System.Drawing.Size(83, 21)
+        Me.ucrInputType.TabIndex = 12
+        '
+        'ucrInputColor
+        '
+        Me.ucrInputColor.AddQuotesIfUnrecognised = True
+        Me.ucrInputColor.GetSetSelectedIndex = -1
+        Me.ucrInputColor.IsReadOnly = False
+        Me.ucrInputColor.Location = New System.Drawing.Point(67, 259)
+        Me.ucrInputColor.Name = "ucrInputColor"
+        Me.ucrInputColor.Size = New System.Drawing.Size(83, 21)
+        Me.ucrInputColor.TabIndex = 10
+        '
+        'ucrInputKeyPosition
+        '
+        Me.ucrInputKeyPosition.AddQuotesIfUnrecognised = True
+        Me.ucrInputKeyPosition.GetSetSelectedIndex = -1
+        Me.ucrInputKeyPosition.IsReadOnly = False
+        Me.ucrInputKeyPosition.Location = New System.Drawing.Point(301, 258)
+        Me.ucrInputKeyPosition.Name = "ucrInputKeyPosition"
+        Me.ucrInputKeyPosition.Size = New System.Drawing.Size(68, 21)
+        Me.ucrInputKeyPosition.TabIndex = 19
+        '
+        'ucrSelectorPolarAnnulus
+        '
+        Me.ucrSelectorPolarAnnulus.bDropUnusedFilterLevels = False
+        Me.ucrSelectorPolarAnnulus.bShowHiddenColumns = False
+        Me.ucrSelectorPolarAnnulus.bUseCurrentFilter = True
+        Me.ucrSelectorPolarAnnulus.Location = New System.Drawing.Point(15, 9)
+        Me.ucrSelectorPolarAnnulus.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrSelectorPolarAnnulus.Name = "ucrSelectorPolarAnnulus"
+        Me.ucrSelectorPolarAnnulus.Size = New System.Drawing.Size(210, 180)
+        Me.ucrSelectorPolarAnnulus.TabIndex = 0
+        '
+        'ucrBase
+        '
+        Me.ucrBase.Location = New System.Drawing.Point(7, 361)
+        Me.ucrBase.Name = "ucrBase"
+        Me.ucrBase.Size = New System.Drawing.Size(410, 52)
+        Me.ucrBase.TabIndex = 24
         '
         'dlgPolarAnnulus
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(410, 349)
-        Me.Controls.Add(Me.UcrButtons1)
+        Me.ClientSize = New System.Drawing.Size(413, 418)
+        Me.Controls.Add(Me.lblSinglePollutant)
+        Me.Controls.Add(Me.ucrReceiverSinglePollutant)
+        Me.Controls.Add(Me.ucrChkMultiplePollutants)
+        Me.Controls.Add(Me.ucrChkNormalize)
+        Me.Controls.Add(Me.ucrNudPercentile)
+        Me.Controls.Add(Me.lblPercentile)
+        Me.Controls.Add(Me.ucrReceiverMultiplePollutant)
+        Me.Controls.Add(Me.ucrReceiverDate)
+        Me.Controls.Add(Me.lbldate)
+        Me.Controls.Add(Me.ucrReceiverWindDirection)
+        Me.Controls.Add(Me.lblWindDirection)
+        Me.Controls.Add(Me.ucrNudMinimumBins)
+        Me.Controls.Add(Me.ucrChckPadDate)
+        Me.Controls.Add(Me.ucrChckExcludeMissing)
+        Me.Controls.Add(Me.ucrInputStatistic)
+        Me.Controls.Add(Me.ucrInputType)
+        Me.Controls.Add(Me.ucrInputColor)
+        Me.Controls.Add(Me.ucrInputKeyPosition)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.lblStatistic)
+        Me.Controls.Add(Me.lblType)
+        Me.Controls.Add(Me.lblColor)
+        Me.Controls.Add(Me.lblMultiplePollutant)
+        Me.Controls.Add(Me.ucrSelectorPolarAnnulus)
+        Me.Controls.Add(Me.ucrBase)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.MaximizeBox = False
         Me.MinimizeBox = False
@@ -45,8 +340,34 @@ Partial Class dlgPolarAnnulus
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Polar Annulus"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents UcrButtons1 As ucrButtons
+    Friend WithEvents ucrBase As ucrButtons
+    Friend WithEvents ucrSelectorPolarAnnulus As ucrSelectorByDataFrameAddRemove
+    Friend WithEvents lblMultiplePollutant As Label
+    Friend WithEvents lblColor As Label
+    Friend WithEvents lblType As Label
+    Friend WithEvents lblStatistic As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents ucrInputColor As ucrInputComboBox
+    Friend WithEvents ucrInputType As ucrInputComboBox
+    Friend WithEvents ucrInputStatistic As ucrInputComboBox
+    Friend WithEvents ucrNudMinimumBins As ucrNud
+    Friend WithEvents ucrChckPadDate As ucrCheck
+    Friend WithEvents ucrChckExcludeMissing As ucrCheck
+    Friend WithEvents ucrInputKeyPosition As ucrInputComboBox
+    Friend WithEvents ucrReceiverWindDirection As ucrReceiverSingle
+    Friend WithEvents lblWindDirection As Label
+    Friend WithEvents ucrReceiverDate As ucrReceiverSingle
+    Friend WithEvents lbldate As Label
+    Friend WithEvents ucrReceiverMultiplePollutant As ucrReceiverMultiple
+    Friend WithEvents lblPercentile As Label
+    Friend WithEvents ucrNudPercentile As ucrNud
+    Friend WithEvents ucrChkMultiplePollutants As ucrCheck
+    Friend WithEvents ucrChkNormalize As ucrCheck
+    Friend WithEvents lblSinglePollutant As Label
+    Friend WithEvents ucrReceiverSinglePollutant As ucrReceiverSingle
 End Class
--- instat/dlgPolarAnnulus.vb
+++ instat/dlgPolarAnnulus.vb
@@ -1,3 +1,177 @@
-﻿Public Class dlgPolarAnnulus
+﻿'R-Instat
+' Copyright (C) 2015-2017
+'
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+'
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License 
+' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+Imports instat.Translations
+Public Class dlgPolarAnnulus
+    Private bFirstLoad As Boolean = True
+    Private bReset As Boolean = True
+
+    'Functions 
+    Private clsPolarAnnulusFunction As RFunction
+    Private Sub dlgPolarAnnulus_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If bFirstLoad Then
+            InitiliseDialog()
+            bFirstLoad = False
+        End If
+
+        If bReset Then
+            SetDefaults()
+        End If
+        SetRcodeForControls(bReset)
+        bReset = False
+        TestOkEnabled()
+    End Sub
+
+    Private Sub InitiliseDialog()
+        Dim dctStatistic As New Dictionary(Of String, String)
+        Dim dctType As New Dictionary(Of String, String)
+        Dim dctPosition As New Dictionary(Of String, String)
+        Dim dctColor As New Dictionary(Of String, String)
+        Dim dctPercentile As New Dictionary(Of String, String)
+
+        ucrBase.clsRsyntax.iCallType = 3
+
+        ucrSelectorPolarAnnulus.SetParameter(New RParameter("mydata", 0))
+        ucrSelectorPolarAnnulus.SetParameterIsrfunction()
+
+        ucrReceiverMultiplePollutant.SetParameter(New RParameter("pollutant", 1))
+        ucrReceiverMultiplePollutant.Selector = ucrSelectorPolarAnnulus
+        ucrReceiverMultiplePollutant.SetParameterIsString()
+
+        ucrReceiverSinglePollutant.SetParameter(New RParameter("pollutant", 2))
+        ucrReceiverSinglePollutant.Selector = ucrSelectorPolarAnnulus
+        ucrReceiverSinglePollutant.SetParameterIsString()
+
+        ucrReceiverWindDirection.SetParameter(New RParameter("wd", 3))
+        ucrReceiverWindDirection.Selector = ucrSelectorPolarAnnulus
+        ucrReceiverWindDirection.SetParameterIsString()
+
+        ucrReceiverDate.SetParameter(New RParameter("date", 4))
+        ucrReceiverDate.Selector = ucrSelectorPolarAnnulus
+        ucrReceiverDate.SetParameterIsString()
+
+        ucrChckExcludeMissing.SetText("Exclude Missing")
+        ucrChckExcludeMissing.SetParameter(New RParameter("exclude.missing", 5))
+        ucrChckExcludeMissing.SetValuesCheckedAndUnchecked("TRUE", "FALSE")
+
+        ucrChckPadDate.SetText("Pad Date")
+        ucrChckPadDate.SetParameter(New RParameter("date.pad", 6))
+        ucrChckPadDate.SetValuesCheckedAndUnchecked("TRUE", "FALSE")
+
+        ucrInputType.SetParameter(New RParameter("type", 7))
+        dctType.Add("Default", Chr(34) & "default" & Chr(34))
+        dctType.Add("Year", Chr(34) & "year" & Chr(34))
+        dctType.Add("Month", Chr(34) & "month" & Chr(34))
+        dctType.Add("Season", Chr(34) & "season" & Chr(34))
+        dctType.Add("Weekday", Chr(34) & "weekday" & Chr(34))
+        dctType.Add("Weekend", Chr(34) & "weekend" & Chr(34))
+        dctType.Add("DST", Chr(34) & "dst" & Chr(34))
+        ucrInputType.SetItems(dctType)
+        ucrInputType.SetDropDownStyleAsNonEditable()
+
+        ucrInputKeyPosition.SetParameter(New RParameter("key.position", 8))
+        dctPosition.Add("Top", Chr(34) & "top" & Chr(34))
+        dctPosition.Add("Tight", Chr(34) & "right" & Chr(34))
+        dctPosition.Add("Bottom", Chr(34) & "bottom" & Chr(34))
+        dctPosition.Add("Left", Chr(34) & "left" & Chr(34))
+        ucrInputKeyPosition.SetItems(dctPosition)
+        ucrInputKeyPosition.SetDropDownStyleAsNonEditable()
+
+        ucrInputStatistic.SetParameter(New RParameter("statistic", 9))
+        dctStatistic.Add("Mean", Chr(34) & "mean" & Chr(34))
+        dctStatistic.Add("Percentile", Chr(34) & "percentile" & Chr(34))
+        dctStatistic.Add("Median", Chr(34) & "median" & Chr(34))
+        dctStatistic.Add("Max", Chr(34) & "max" & Chr(34))
+        dctStatistic.Add("Frequency", Chr(34) & "frequency" & Chr(34))
+        dctStatistic.Add("Standard deviation", Chr(34) & "stdev" & Chr(34))
+        dctStatistic.Add("Weighted Mean", Chr(34) & "weighted.mean" & Chr(34))
+        dctStatistic.Add("CPF", Chr(34) & "cpf" & Chr(34))
+        ucrInputStatistic.SetItems(dctStatistic)
+        ucrInputStatistic.SetDropDownStyleAsNonEditable()
+
+        ucrInputStatistic.AddToLinkedControls(ucrNudPercentile, {"Percentile", "CPF"}, bNewLinkedHideIfParameterMissing:=True)
+        ucrNudPercentile.SetLinkedDisplayControl(lblPercentile)
+
+        ucrChkMultiplePollutants.SetText("Multiple Pollutants")
+        ucrChkMultiplePollutants.AddToLinkedControls(ucrReceiverMultiplePollutant, {True}, bNewLinkedAddRemoveParameter:=True, bNewLinkedHideIfParameterMissing:=True)
+        ucrReceiverMultiplePollutant.SetLinkedDisplayControl(lblMultiplePollutant)
+        ucrChkMultiplePollutants.AddToLinkedControls(ucrReceiverSinglePollutant, {False}, bNewLinkedAddRemoveParameter:=True, bNewLinkedHideIfParameterMissing:=True)
+        ucrReceiverSinglePollutant.SetLinkedDisplayControl(lblSinglePollutant)
+        ucrChkMultiplePollutants.AddToLinkedControls(ucrChkNormalize, {True}, bNewLinkedAddRemoveParameter:=True, bNewLinkedHideIfParameterMissing:=True)
+
+        ucrInputColor.SetParameter(New RParameter("cols", 10))
+        dctColor.Add("Default", Chr(34) & "default" & Chr(34))
+        dctColor.Add("Increment", Chr(34) & "increment" & Chr(34))
+        dctColor.Add("Heat", Chr(34) & "heat" & Chr(34))
+        dctColor.Add("Jet", Chr(34) & "jet" & Chr(34))
+        ucrInputColor.SetItems(dctColor)
+        ucrInputColor.SetDropDownStyleAsNonEditable()
+
+        ucrNudMinimumBins.SetParameter(New RParameter("min.bin", 11))
+        ucrNudMinimumBins.Minimum = 1
+
+        ucrNudPercentile.SetParameter(New RParameter("percentile", 12))
+        ucrNudPercentile.Minimum = 1
+
+        ucrChkNormalize.SetText("Normalise")
+        ucrChkNormalize.SetParameter(New RParameter("normalise", 13))
+        ucrChkNormalize.SetValuesCheckedAndUnchecked("TRUE", "FALSE")
+    End Sub
+
+    Private Sub SetRcodeForControls(bReset As Boolean)
+        SetRCode(Me, ucrBase.clsRsyntax.clsBaseFunction, bReset)
+    End Sub
+
+    Private Sub SetDefaults()
+        clsPolarAnnulusFunction = New RFunction
+
+        clsPolarAnnulusFunction.AddParameter("exclude.missing", "TRUE", iPosition:=5)
+        clsPolarAnnulusFunction.AddParameter("date.pad", "TRUE", iPosition:=6)
+        clsPolarAnnulusFunction.AddParameter("type", Chr(34) & "default" & Chr(34), iPosition:=7)
+        clsPolarAnnulusFunction.AddParameter("key.position", Chr(34) & "right" & Chr(34), iPosition:=8)
+        clsPolarAnnulusFunction.AddParameter("statistic", Chr(34) & "mean" & Chr(34), iPosition:=9)
+        clsPolarAnnulusFunction.AddParameter("cols", Chr(34) & "default" & Chr(34), iPosition:=10)
+        clsPolarAnnulusFunction.AddParameter("min.bin", "2", iPosition:=11)
+        clsPolarAnnulusFunction.AddParameter("percentile", "80", iPosition:=12)
+        clsPolarAnnulusFunction.AddParameter("normalise", "TRUE", iPosition:=13)
+
+        ucrSelectorPolarAnnulus.Reset()
+        ucrReceiverMultiplePollutant.SetMeAsReceiver()
+
+        clsPolarAnnulusFunction.SetPackageName("openair")
+        clsPolarAnnulusFunction.SetRCommand("polarAnnulus")
+
+        ucrBase.clsRsyntax.SetBaseRFunction(clsPolarAnnulusFunction)
+    End Sub
+
+    Private Sub TestOkEnabled()
+        If ucrReceiverMultiplePollutant.IsEmpty OrElse ucrReceiverWindDirection.IsEmpty OrElse ucrReceiverDate.IsEmpty Then
+            ucrBase.OKEnabled(False)
+        Else
+            ucrBase.OKEnabled(True)
+        End If
+    End Sub
+
+    Private Sub ucrBase_ClickReset(sender As Object, e As EventArgs) Handles ucrBase.ClickReset
+        SetDefaults()
+        SetRcodeForControls(True)
+        TestOkEnabled()
+    End Sub
+
+    Private Sub ucrReceiverPollutant_ControlValueChanged(ucrChangedControl As ucrCore) Handles ucrReceiverWindDirection.ControlValueChanged, ucrReceiverDate.ControlValueChanged
+        TestOkEnabled()
+    End Sub
 End Class
