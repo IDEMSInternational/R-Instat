326e8e73de25434a1d8689a01fb1fd03bb1e9e50
Wycklife Bonyo <bonyowycklife@gmail.com>
2020-11-10 12:39:45 +0300

    Added a selector and a single receiver to the Scatter plot dialog

--- instat/dlgCircularScatterPlot.Designer.vb
+++ instat/dlgCircularScatterPlot.Designer.vb
@@ -23,20 +23,58 @@ Partial Class dlgCircularScatterPlot
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.ucrBase = New instat.ucrButtons()
+        Me.ucrSelectorCircularDataFrame = New instat.ucrSelectorByDataFrameAddRemove()
+        Me.ucrReceiverCircularData = New instat.ucrReceiverSingle()
+        Me.lblElement = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'ucrBase
         '
-        Me.ucrBase.Location = New System.Drawing.Point(5, 239)
+        Me.ucrBase.Location = New System.Drawing.Point(5, 266)
         Me.ucrBase.Name = "ucrBase"
         Me.ucrBase.Size = New System.Drawing.Size(410, 52)
         Me.ucrBase.TabIndex = 0
+        '
+        'ucrSelectorCircularDataFrame
+        '
+        Me.ucrSelectorCircularDataFrame.bDropUnusedFilterLevels = False
+        Me.ucrSelectorCircularDataFrame.bShowHiddenColumns = False
+        Me.ucrSelectorCircularDataFrame.bUseCurrentFilter = True
+        Me.ucrSelectorCircularDataFrame.Location = New System.Drawing.Point(9, 9)
+        Me.ucrSelectorCircularDataFrame.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrSelectorCircularDataFrame.Name = "ucrSelectorCircularDataFrame"
+        Me.ucrSelectorCircularDataFrame.Size = New System.Drawing.Size(210, 180)
+        Me.ucrSelectorCircularDataFrame.TabIndex = 1
+        '
+        'ucrReceiverCircularData
+        '
+        Me.ucrReceiverCircularData.frmParent = Me
+        Me.ucrReceiverCircularData.Location = New System.Drawing.Point(276, 41)
+        Me.ucrReceiverCircularData.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverCircularData.Name = "ucrReceiverCircularData"
+        Me.ucrReceiverCircularData.Selector = Nothing
+        Me.ucrReceiverCircularData.Size = New System.Drawing.Size(120, 20)
+        Me.ucrReceiverCircularData.strNcFilePath = ""
+        Me.ucrReceiverCircularData.TabIndex = 2
+        Me.ucrReceiverCircularData.ucrSelector = Nothing
+        '
+        'lblElement
+        '
+        Me.lblElement.AutoSize = True
+        Me.lblElement.Location = New System.Drawing.Point(279, 25)
+        Me.lblElement.Name = "lblElement"
+        Me.lblElement.Size = New System.Drawing.Size(48, 13)
+        Me.lblElement.TabIndex = 3
+        Me.lblElement.Text = "Element:"
         '
         'dlgCircularScatterPlot
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(420, 296)
+        Me.ClientSize = New System.Drawing.Size(420, 323)
+        Me.Controls.Add(Me.lblElement)
+        Me.Controls.Add(Me.ucrReceiverCircularData)
+        Me.Controls.Add(Me.ucrSelectorCircularDataFrame)
         Me.Controls.Add(Me.ucrBase)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.MaximizeBox = False
@@ -45,8 +83,12 @@ Partial Class dlgCircularScatterPlot
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Scatter Plot"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents ucrBase As ucrButtons
+    Friend WithEvents ucrSelectorCircularDataFrame As ucrSelectorByDataFrameAddRemove
+    Friend WithEvents ucrReceiverCircularData As ucrReceiverSingle
+    Friend WithEvents lblElement As Label
 End Class
