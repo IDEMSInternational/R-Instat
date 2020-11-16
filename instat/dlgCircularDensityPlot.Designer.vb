94dfadd526c298bbb776d9be82e7a638f15d6f69
Wycklife Bonyo <bonyowycklife@gmail.com>
2020-11-16 19:03:24 +0300

    Adding controls to the density dialog

--- instat/dlgCircularDensityPlot.Designer.vb
+++ instat/dlgCircularDensityPlot.Designer.vb
@@ -23,20 +23,68 @@ Partial Class dlgCircularDensityPlot
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.ucrBase = New instat.ucrButtons()
+        Me.ucrSelectorDataFrame = New instat.ucrSelectorByDataFrameAddRemove()
+        Me.ucrReceiverVariable = New instat.ucrReceiverSingle()
+        Me.lblVariable = New System.Windows.Forms.Label()
+        Me.ucrSaveDensity = New instat.ucrSave()
         Me.SuspendLayout()
         '
         'ucrBase
         '
-        Me.ucrBase.Location = New System.Drawing.Point(5, 238)
+        Me.ucrBase.Location = New System.Drawing.Point(5, 274)
         Me.ucrBase.Name = "ucrBase"
         Me.ucrBase.Size = New System.Drawing.Size(410, 52)
         Me.ucrBase.TabIndex = 0
+        '
+        'ucrSelectorDataFrame
+        '
+        Me.ucrSelectorDataFrame.bDropUnusedFilterLevels = False
+        Me.ucrSelectorDataFrame.bShowHiddenColumns = False
+        Me.ucrSelectorDataFrame.bUseCurrentFilter = True
+        Me.ucrSelectorDataFrame.Location = New System.Drawing.Point(13, 13)
+        Me.ucrSelectorDataFrame.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrSelectorDataFrame.Name = "ucrSelectorDataFrame"
+        Me.ucrSelectorDataFrame.Size = New System.Drawing.Size(210, 180)
+        Me.ucrSelectorDataFrame.TabIndex = 1
+        '
+        'ucrReceiverVariable
+        '
+        Me.ucrReceiverVariable.frmParent = Me
+        Me.ucrReceiverVariable.Location = New System.Drawing.Point(295, 37)
+        Me.ucrReceiverVariable.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverVariable.Name = "ucrReceiverVariable"
+        Me.ucrReceiverVariable.Selector = Nothing
+        Me.ucrReceiverVariable.Size = New System.Drawing.Size(120, 20)
+        Me.ucrReceiverVariable.strNcFilePath = ""
+        Me.ucrReceiverVariable.TabIndex = 2
+        Me.ucrReceiverVariable.ucrSelector = Nothing
+        '
+        'lblVariable
+        '
+        Me.lblVariable.AutoSize = True
+        Me.lblVariable.Location = New System.Drawing.Point(290, 22)
+        Me.lblVariable.Name = "lblVariable"
+        Me.lblVariable.Size = New System.Drawing.Size(48, 13)
+        Me.lblVariable.TabIndex = 3
+        Me.lblVariable.Text = "Variable:"
+        '
+        'ucrSaveDensity
+        '
+        Me.ucrSaveDensity.Location = New System.Drawing.Point(13, 225)
+        Me.ucrSaveDensity.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.ucrSaveDensity.Name = "ucrSaveDensity"
+        Me.ucrSaveDensity.Size = New System.Drawing.Size(249, 21)
+        Me.ucrSaveDensity.TabIndex = 4
         '
         'dlgCircularDensityPlot
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(420, 296)
+        Me.ClientSize = New System.Drawing.Size(420, 331)
+        Me.Controls.Add(Me.ucrSaveDensity)
+        Me.Controls.Add(Me.lblVariable)
+        Me.Controls.Add(Me.ucrReceiverVariable)
+        Me.Controls.Add(Me.ucrSelectorDataFrame)
         Me.Controls.Add(Me.ucrBase)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.MaximizeBox = False
@@ -45,8 +93,13 @@ Partial Class dlgCircularDensityPlot
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Density Plot"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents ucrBase As ucrButtons
+    Friend WithEvents ucrSelectorDataFrame As ucrSelectorByDataFrameAddRemove
+    Friend WithEvents ucrReceiverVariable As ucrReceiverSingle
+    Friend WithEvents ucrSaveDensity As ucrSave
+    Friend WithEvents lblVariable As Label
 End Class
