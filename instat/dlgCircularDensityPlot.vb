efe4e38262b1dad7ed85c9e39733e6c92c6ff8fc
Wycklife Bonyo <bonyowycklife@gmail.com>
2020-11-17 09:59:16 +0300

    Implemented the functionality of the density plot dialog

--- instat/dlgCircularDensityPlot.vb
+++ instat/dlgCircularDensityPlot.vb
@@ -35,11 +35,18 @@ Public Class dlgCircularDensityPlot
     End Sub
 
     Private Sub InitialiseDialog()
+        ucrBase.clsRsyntax.iCallType = 3
+        ucrBase.clsRsyntax.bExcludeAssignedFunctionOutput = False
+
         ucrSelectorDataFrame.SetParameter(New RParameter("data", 0))
         ucrSelectorDataFrame.SetParameterIsrfunction()
 
         ucrReceiverVariable.SetParameter(New RParameter("x", 0))
         ucrReceiverVariable.Selector = ucrSelectorDataFrame
+        'ucrReceiverVariable.SetParameterIsString()
+        ucrReceiverVariable.SetIncludedDataTypes({"numeric"})
+        ucrReceiverVariable.strSelectorHeading = "Numerics"
+        ucrReceiverVariable.bWithQuotes = False
 
         ucrSaveDensity.SetPrefix("circular_density")
         ucrSaveDensity.SetDataFrameSelector(ucrSelectorDataFrame.ucrAvailableDataFrames)
@@ -50,14 +57,34 @@ Public Class dlgCircularDensityPlot
     End Sub
 
     Private Sub SetDefaults()
+        clsDensityFunction = New RFunction
 
+        ucrSaveDensity.Reset()
+        ucrReceiverVariable.SetMeAsReceiver()
+        ucrSelectorDataFrame.Reset()
+
+        clsDensityFunction.SetPackageName("circular")
+        clsDensityFunction.SetRCommand("density.circular")
+        clsDensityFunction.AddParameter("adjust", "1", iPosition:=1)
+
+        ucrBase.clsRsyntax.SetBaseRFunction(clsDensityFunction)
     End Sub
 
     Private Sub SetRCodeForControls(bReset As Boolean)
-
+        ucrReceiverVariable.SetRCode(clsDensityFunction, bReset)
     End Sub
 
     Private Sub TestOkEnabled()
+        'If ucrReceiverVariable.IsEmpty Then
+        '    ucrBase.OKEnabled(False)
+        'Else
+        '    ucrBase.OKEnabled(True)
+        'End If
+    End Sub
 
+    Private Sub ucrBase_ClickReset(sender As Object, e As EventArgs) Handles ucrBase.ClickReset
+        SetDefaults()
+        SetRCodeForControls(True)
+        TestOkEnabled()
     End Sub
 End Class
