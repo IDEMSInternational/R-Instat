ca46f9aece4ab4096aa85edca49ba0007dcae9c4
Wycklife Bonyo <bonyowycklife@gmail.com>
2020-11-17 22:47:35 +0300

    Implementing circular rose plot dialog

--- instat/dlgCircularRosePlot.Designer.vb
+++ instat/dlgCircularRosePlot.Designer.vb
@@ -23,20 +23,68 @@ Partial Class dlgCircularRosePlot
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.ucrBase = New instat.ucrButtons()
+        Me.ucrSelctorCircularDataFrame = New instat.ucrSelectorByDataFrameAddRemove()
+        Me.ucrSaveCircularRosePlot = New instat.ucrSave()
+        Me.ucrReceiverVariable = New instat.ucrReceiverSingle()
+        Me.lblVariable = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'ucrBase
         '
-        Me.ucrBase.Location = New System.Drawing.Point(6, 240)
+        Me.ucrBase.Location = New System.Drawing.Point(6, 273)
         Me.ucrBase.Name = "ucrBase"
         Me.ucrBase.Size = New System.Drawing.Size(406, 52)
         Me.ucrBase.TabIndex = 0
+        '
+        'ucrSelctorCircularDataFrame
+        '
+        Me.ucrSelctorCircularDataFrame.bDropUnusedFilterLevels = False
+        Me.ucrSelctorCircularDataFrame.bShowHiddenColumns = False
+        Me.ucrSelctorCircularDataFrame.bUseCurrentFilter = True
+        Me.ucrSelctorCircularDataFrame.Location = New System.Drawing.Point(6, 9)
+        Me.ucrSelctorCircularDataFrame.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrSelctorCircularDataFrame.Name = "ucrSelctorCircularDataFrame"
+        Me.ucrSelctorCircularDataFrame.Size = New System.Drawing.Size(210, 180)
+        Me.ucrSelctorCircularDataFrame.TabIndex = 1
+        '
+        'ucrSaveCircularRosePlot
+        '
+        Me.ucrSaveCircularRosePlot.Location = New System.Drawing.Point(6, 232)
+        Me.ucrSaveCircularRosePlot.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.ucrSaveCircularRosePlot.Name = "ucrSaveCircularRosePlot"
+        Me.ucrSaveCircularRosePlot.Size = New System.Drawing.Size(279, 28)
+        Me.ucrSaveCircularRosePlot.TabIndex = 2
+        '
+        'ucrReceiverVariable
+        '
+        Me.ucrReceiverVariable.frmParent = Me
+        Me.ucrReceiverVariable.Location = New System.Drawing.Point(292, 64)
+        Me.ucrReceiverVariable.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverVariable.Name = "ucrReceiverVariable"
+        Me.ucrReceiverVariable.Selector = Nothing
+        Me.ucrReceiverVariable.Size = New System.Drawing.Size(120, 20)
+        Me.ucrReceiverVariable.strNcFilePath = ""
+        Me.ucrReceiverVariable.TabIndex = 3
+        Me.ucrReceiverVariable.ucrSelector = Nothing
+        '
+        'lblVariable
+        '
+        Me.lblVariable.AutoSize = True
+        Me.lblVariable.Location = New System.Drawing.Point(289, 42)
+        Me.lblVariable.Name = "lblVariable"
+        Me.lblVariable.Size = New System.Drawing.Size(48, 13)
+        Me.lblVariable.TabIndex = 4
+        Me.lblVariable.Text = "Variable:"
         '
         'dlgCircularRosePlot
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(420, 296)
+        Me.ClientSize = New System.Drawing.Size(420, 330)
+        Me.Controls.Add(Me.lblVariable)
+        Me.Controls.Add(Me.ucrReceiverVariable)
+        Me.Controls.Add(Me.ucrSaveCircularRosePlot)
+        Me.Controls.Add(Me.ucrSelctorCircularDataFrame)
         Me.Controls.Add(Me.ucrBase)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.MaximizeBox = False
@@ -45,8 +93,13 @@ Partial Class dlgCircularRosePlot
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Rose Plot"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents ucrBase As ucrButtons
+    Friend WithEvents ucrSelctorCircularDataFrame As ucrSelectorByDataFrameAddRemove
+    Friend WithEvents ucrSaveCircularRosePlot As ucrSave
+    Friend WithEvents ucrReceiverVariable As ucrReceiverSingle
+    Friend WithEvents lblVariable As Label
 End Class
--- instat/dlgCircularRosePlot.vb
+++ instat/dlgCircularRosePlot.vb
@@ -1,3 +1,84 @@
-﻿Public Class dlgCircularRosePlot
+﻿'R-Instat
+' Copyright (C) 2015-2017
+'
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+'
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License 
+' along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Imports instat.Translations
+
+Public Class dlgCircularRosePlot
+    Public bfirstload As Boolean = True
+    Public bReset As Boolean = True
+    Private clsRosePlotFunction As RFunction
+    Private Sub dlgCircularRosePlot_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If bFirstLoad Then
+            InitialiseDialog()
+            bFirstLoad = False
+        End If
+
+        If bReset Then
+            SetDefaults()
+        End If
+
+        SetRCodeForControls(bReset)
+        bReset = False
+        autoTranslate(Me)
+        TestOkEnabled()
+    End Sub
+
+    Private Sub InitialiseDialog()
+
+        ucrSelctorCircularDataFrame.SetParameter(New RParameter("data", 0))
+        ucrSelctorCircularDataFrame.SetParameterIsrfunction()
+
+        ucrReceiverVariable.SetParameter(New RParameter("x", 0))
+        ucrReceiverVariable.Selector = ucrSelctorCircularDataFrame
+        ucrReceiverVariable.SetParameterIsRFunction()
+
+        ucrSaveCircularRosePlot.SetPrefix("circular_rose_plot")
+        ucrSaveCircularRosePlot.SetDataFrameSelector(ucrSelctorCircularDataFrame.ucrAvailableDataFrames)
+        ucrSaveCircularRosePlot.SetIsComboBox()
+        ucrSaveCircularRosePlot.SetCheckBoxText("Save Graph")
+        ucrSaveCircularRosePlot.SetSaveTypeAsGraph()
+        ucrSaveCircularRosePlot.SetAssignToIfUncheckedValue("last_graph")
+    End Sub
+
+    Private Sub SetDefaults()
+        clsRosePlotFunction = New RFunction
+
+        ucrSelctorCircularDataFrame.Reset()
+        ucrSaveCircularRosePlot.Reset()
+        ucrReceiverVariable.SetMeAsReceiver()
+
+        clsRosePlotFunction.SetPackageName("circular")
+        clsRosePlotFunction.SetRCommand("rose.diag")
+
+    End Sub
+
+    Public Sub SetRCodeForControls(bReset As Boolean)
+        ucrReceiverVariable.SetRCode(clsRosePlotFunction, bReset)
+    End Sub
+
+    Private Sub TestOkEnabled()
+
+    End Sub
+
+    Private Sub ucrBase_ClickReset(sender As Object, e As EventArgs) Handles ucrBase.ClickReset
+
+    End Sub
+
+    Private Sub CoreControls_ControlContentsChanged() Handles ucrReceiverVariable.ControlContentsChanged, ucrSaveCircularRosePlot.ControlContentsChanged
+
+    End Sub
 
 End Class
