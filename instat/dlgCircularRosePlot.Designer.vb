4ccc7527bd7e4b613993de0a08efd1cfb98d5a41
Wycklife Bonyo <bonyowycklife@gmail.com>
2020-11-20 22:06:31 +0300

    Added circular options button and disabled

--- instat/dlgCircularRosePlot.Designer.vb
+++ instat/dlgCircularRosePlot.Designer.vb
@@ -31,6 +31,7 @@ Partial Class dlgCircularRosePlot
         Me.lblBins = New System.Windows.Forms.Label()
         Me.ucrInputComboRadius = New instat.ucrInputComboBox()
         Me.lblRadiusScale = New System.Windows.Forms.Label()
+        Me.cmdCircularOptions = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'ucrBase
@@ -85,7 +86,7 @@ Partial Class dlgCircularRosePlot
         Me.ucrInputBins.AddQuotesIfUnrecognised = True
         Me.ucrInputBins.IsMultiline = False
         Me.ucrInputBins.IsReadOnly = False
-        Me.ucrInputBins.Location = New System.Drawing.Point(346, 128)
+        Me.ucrInputBins.Location = New System.Drawing.Point(359, 128)
         Me.ucrInputBins.Name = "ucrInputBins"
         Me.ucrInputBins.Size = New System.Drawing.Size(66, 21)
         Me.ucrInputBins.TabIndex = 5
@@ -93,7 +94,7 @@ Partial Class dlgCircularRosePlot
         'lblBins
         '
         Me.lblBins.AutoSize = True
-        Me.lblBins.Location = New System.Drawing.Point(310, 132)
+        Me.lblBins.Location = New System.Drawing.Point(324, 132)
         Me.lblBins.Name = "lblBins"
         Me.lblBins.Size = New System.Drawing.Size(30, 13)
         Me.lblBins.TabIndex = 6
@@ -104,7 +105,7 @@ Partial Class dlgCircularRosePlot
         Me.ucrInputComboRadius.AddQuotesIfUnrecognised = True
         Me.ucrInputComboRadius.GetSetSelectedIndex = -1
         Me.ucrInputComboRadius.IsReadOnly = False
-        Me.ucrInputComboRadius.Location = New System.Drawing.Point(346, 168)
+        Me.ucrInputComboRadius.Location = New System.Drawing.Point(361, 168)
         Me.ucrInputComboRadius.Name = "ucrInputComboRadius"
         Me.ucrInputComboRadius.Size = New System.Drawing.Size(66, 21)
         Me.ucrInputComboRadius.TabIndex = 7
@@ -112,17 +113,30 @@ Partial Class dlgCircularRosePlot
         'lblRadiusScale
         '
         Me.lblRadiusScale.AutoSize = True
-        Me.lblRadiusScale.Location = New System.Drawing.Point(267, 173)
+        Me.lblRadiusScale.Location = New System.Drawing.Point(283, 172)
         Me.lblRadiusScale.Name = "lblRadiusScale"
         Me.lblRadiusScale.Size = New System.Drawing.Size(73, 13)
         Me.lblRadiusScale.TabIndex = 8
         Me.lblRadiusScale.Text = "Radius Scale:"
         '
+        'cmdCircularOptions
+        '
+        Me.cmdCircularOptions.Enabled = False
+        Me.cmdCircularOptions.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.cmdCircularOptions.Location = New System.Drawing.Point(6, 199)
+        Me.cmdCircularOptions.Name = "cmdCircularOptions"
+        Me.cmdCircularOptions.Size = New System.Drawing.Size(120, 25)
+        Me.cmdCircularOptions.TabIndex = 9
+        Me.cmdCircularOptions.Tag = "Circular_Options"
+        Me.cmdCircularOptions.Text = "Circular Options"
+        Me.cmdCircularOptions.UseVisualStyleBackColor = True
+        '
         'dlgCircularRosePlot
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(420, 330)
+        Me.ClientSize = New System.Drawing.Size(433, 330)
+        Me.Controls.Add(Me.cmdCircularOptions)
         Me.Controls.Add(Me.lblRadiusScale)
         Me.Controls.Add(Me.ucrInputComboRadius)
         Me.Controls.Add(Me.lblBins)
@@ -152,4 +166,5 @@ Partial Class dlgCircularRosePlot
     Friend WithEvents lblBins As Label
     Friend WithEvents lblRadiusScale As Label
     Friend WithEvents ucrInputComboRadius As ucrInputComboBox
+    Friend WithEvents cmdCircularOptions As Button
 End Class
