6a21f85f436ac99c1b747cb47efa4e34222ecdde
N-thony <antoine.ntalumeso@aims.ac.rw>
2021-02-03 12:06:38 +0200

    Update instat/ApplicationEvents.vb
    Co-authored-by: lloyddewit <57253949+lloyddewit@users.noreply.github.com>

--- instat/ApplicationEvents.vb
+++ instat/ApplicationEvents.vb
@@ -6,6 +6,15 @@
     ''' https://docs.microsoft.com/en-us/dotnet/visual-basic/developing-apps/customizing-extending-my/extending-the-my-namespace 
     ''' </summary>
     Partial Friend Class MyApplication
+        ''' <summary>   Handles the event raised when when launching a single-instance application 
+        '''             and the application is already active. 
+        '''             If the event was triggered by double-clicking on a data file, then the sub 
+        '''             displays a message box asking if the user would like to open the file in 
+        '''             the currently running R-Instat instance. </summary>
+        '''
+        ''' <param name="sender">   Source of the event. </param>
+        ''' <param name="e">        Startup next instance event information. </param>
+        '''----------------------------------------------------------------------------------------
         Private Sub MyApplication_StartupNextInstance(ByVal sender As Object, ByVal e As Microsoft.VisualBasic.ApplicationServices.StartupNextInstanceEventArgs) Handles Me.StartupNextInstance
             If e.CommandLine.Count > 0 Then
                 If MsgBox("Would you like to open this file?", MsgBoxStyle.YesNo) = MsgBoxResult.Yes Then
