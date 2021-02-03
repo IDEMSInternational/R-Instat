9e7940bd52ae95a5618966354f5eae0484120dce
N-thony <antoine.ntalumeso@aims.ac.rw>
2021-02-03 12:04:54 +0200

    Update instat/ApplicationEvents.vb
    Co-authored-by: lloyddewit <57253949+lloyddewit@users.noreply.github.com>

--- instat/ApplicationEvents.vb
+++ instat/ApplicationEvents.vb
@@ -1,10 +1,10 @@
 ﻿Namespace My
-    ' The following events are available for MyApplication:
-    ' Startup: Raised when the application starts, before the startup form is created.
-    ' Shutdown: Raised after all application forms are closed.  This event is not raised if the application terminates abnormally.
-    ' UnhandledException: Raised if the application encounters an unhandled exception.
-    ' StartupNextInstance: Raised when launching a single-instance application and the application is already active. 
-    ' NetworkAvailabilityChanged: Raised when the network connection is connected or disconnected.
+    ''' The 'My' namespace and the 'My.Application' objects are part of the dotnet framework.
+    ''' This class extends the 'My.Application' object by creating a Partial class named MyApplication.
+    ''' This class handles the event raised when R-Instat is launched when it's already running.
+    ''' For more details on extending 'My.Application', please see: 
+    ''' https://docs.microsoft.com/en-us/dotnet/visual-basic/developing-apps/customizing-extending-my/extending-the-my-namespace 
+    ''' </summary>
     Partial Friend Class MyApplication
         Private Sub MyApplication_StartupNextInstance(ByVal sender As Object, ByVal e As Microsoft.VisualBasic.ApplicationServices.StartupNextInstanceEventArgs) Handles Me.StartupNextInstance
             If e.CommandLine.Count > 0 Then
