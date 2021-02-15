511dbabdf31fd8341e899188d09fdc215ad5efc8
shadrack kibet <shadrackkibet36@outlook.com>
2021-02-15 13:47:19 +0300

    designer realignments

--- instat/sdgDistributionOptions.Designer.vb
+++ instat/sdgDistributionOptions.Designer.vb
@@ -26,26 +26,34 @@ Partial Class sdgDistributionOptions
         Me.ucrBase = New instat.ucrButtonsSubdialogue()
         Me.tbpPlotsOptions = New System.Windows.Forms.TabControl()
         Me.tbpTitles = New System.Windows.Forms.TabPage()
-        Me.grpGraph = New System.Windows.Forms.GroupBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.ucrInputGraphTitle = New instat.ucrInputTextBox()
-        Me.urChkSelectTheme = New instat.ucrCheck()
-        Me.ucrInputThemes = New instat.ucrInputComboBox()
-        Me.lblYAxis = New System.Windows.Forms.Label()
-        Me.UcrInputTextBox2 = New instat.ucrInputTextBox()
-        Me.lblXAxis = New System.Windows.Forms.Label()
-        Me.UcrInputTextBox1 = New instat.ucrInputTextBox()
-        Me.lblCaption = New System.Windows.Forms.Label()
         Me.lblSubTitle = New System.Windows.Forms.Label()
-        Me.ucrInputGraphCaption = New instat.ucrInputTextBox()
         Me.ucrInputGraphSubTitle = New instat.ucrInputTextBox()
+        Me.lblCaption = New System.Windows.Forms.Label()
+        Me.ucrInputGraphCaption = New instat.ucrInputTextBox()
         Me.tbpXAxis = New System.Windows.Forms.TabPage()
         Me.tbpYAxis = New System.Windows.Forms.TabPage()
+        Me.lblYAxisTitle = New System.Windows.Forms.Label()
+        Me.rdoYAxisSpecifyTitle = New System.Windows.Forms.RadioButton()
+        Me.rdoYAxisNoTitle = New System.Windows.Forms.RadioButton()
+        Me.rdoYAxisAuto = New System.Windows.Forms.RadioButton()
+        Me.ucrInputYAxisLabel = New instat.ucrInputTextBox()
+        Me.ucrPnlYAxis = New instat.UcrPanel()
         Me.tbpTheme = New System.Windows.Forms.TabPage()
+        Me.lblSelectTheme = New System.Windows.Forms.Label()
+        Me.ucrInputThemes = New instat.ucrInputComboBox()
         Me.lblFont = New System.Windows.Forms.Label()
+        Me.lblXAxisTitle = New System.Windows.Forms.Label()
+        Me.rdoXAxisSpecifyTitle = New System.Windows.Forms.RadioButton()
+        Me.rdoXAxisNoTitle = New System.Windows.Forms.RadioButton()
+        Me.rdoXAxisAuto = New System.Windows.Forms.RadioButton()
+        Me.ucrInputXAxisLabel = New instat.ucrInputTextBox()
+        Me.ucrPnlXAxis = New instat.UcrPanel()
         Me.tbpPlotsOptions.SuspendLayout()
         Me.tbpTitles.SuspendLayout()
-        Me.grpGraph.SuspendLayout()
+        Me.tbpXAxis.SuspendLayout()
+        Me.tbpYAxis.SuspendLayout()
         Me.tbpTheme.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -66,29 +74,16 @@ Partial Class sdgDistributionOptions
         '
         'tbpTitles
         '
-        Me.tbpTitles.Controls.Add(Me.grpGraph)
-        Me.tbpTitles.Controls.Add(Me.urChkSelectTheme)
-        Me.tbpTitles.Controls.Add(Me.ucrInputThemes)
-        Me.tbpTitles.Controls.Add(Me.lblYAxis)
-        Me.tbpTitles.Controls.Add(Me.UcrInputTextBox2)
-        Me.tbpTitles.Controls.Add(Me.lblXAxis)
-        Me.tbpTitles.Controls.Add(Me.UcrInputTextBox1)
+        Me.tbpTitles.Controls.Add(Me.Label1)
+        Me.tbpTitles.Controls.Add(Me.ucrInputGraphTitle)
+        Me.tbpTitles.Controls.Add(Me.lblSubTitle)
+        Me.tbpTitles.Controls.Add(Me.ucrInputGraphSubTitle)
+        Me.tbpTitles.Controls.Add(Me.lblCaption)
+        Me.tbpTitles.Controls.Add(Me.ucrInputGraphCaption)
         resources.ApplyResources(Me.tbpTitles, "tbpTitles")
         Me.tbpTitles.Name = "tbpTitles"
         Me.tbpTitles.Tag = "Titles"
         Me.tbpTitles.UseVisualStyleBackColor = True
-        '
-        'grpGraph
-        '
-        Me.grpGraph.Controls.Add(Me.Label1)
-        Me.grpGraph.Controls.Add(Me.ucrInputGraphTitle)
-        Me.grpGraph.Controls.Add(Me.lblSubTitle)
-        Me.grpGraph.Controls.Add(Me.ucrInputGraphSubTitle)
-        Me.grpGraph.Controls.Add(Me.lblCaption)
-        Me.grpGraph.Controls.Add(Me.ucrInputGraphCaption)
-        resources.ApplyResources(Me.grpGraph, "grpGraph")
-        Me.grpGraph.Name = "grpGraph"
-        Me.grpGraph.TabStop = False
         '
         'Label1
         '
@@ -103,63 +98,10 @@ Partial Class sdgDistributionOptions
         resources.ApplyResources(Me.ucrInputGraphTitle, "ucrInputGraphTitle")
         Me.ucrInputGraphTitle.Name = "ucrInputGraphTitle"
         '
-        'urChkSelectTheme
-        '
-        Me.urChkSelectTheme.Checked = False
-        resources.ApplyResources(Me.urChkSelectTheme, "urChkSelectTheme")
-        Me.urChkSelectTheme.Name = "urChkSelectTheme"
-        '
-        'ucrInputThemes
-        '
-        Me.ucrInputThemes.AddQuotesIfUnrecognised = True
-        Me.ucrInputThemes.GetSetSelectedIndex = -1
-        Me.ucrInputThemes.IsReadOnly = False
-        resources.ApplyResources(Me.ucrInputThemes, "ucrInputThemes")
-        Me.ucrInputThemes.Name = "ucrInputThemes"
-        '
-        'lblYAxis
-        '
-        resources.ApplyResources(Me.lblYAxis, "lblYAxis")
-        Me.lblYAxis.Name = "lblYAxis"
-        '
-        'UcrInputTextBox2
-        '
-        Me.UcrInputTextBox2.AddQuotesIfUnrecognised = True
-        Me.UcrInputTextBox2.IsMultiline = False
-        Me.UcrInputTextBox2.IsReadOnly = False
-        resources.ApplyResources(Me.UcrInputTextBox2, "UcrInputTextBox2")
-        Me.UcrInputTextBox2.Name = "UcrInputTextBox2"
-        '
-        'lblXAxis
-        '
-        resources.ApplyResources(Me.lblXAxis, "lblXAxis")
-        Me.lblXAxis.Name = "lblXAxis"
-        '
-        'UcrInputTextBox1
-        '
-        Me.UcrInputTextBox1.AddQuotesIfUnrecognised = True
-        Me.UcrInputTextBox1.IsMultiline = False
-        Me.UcrInputTextBox1.IsReadOnly = False
-        resources.ApplyResources(Me.UcrInputTextBox1, "UcrInputTextBox1")
-        Me.UcrInputTextBox1.Name = "UcrInputTextBox1"
-        '
-        'lblCaption
-        '
-        resources.ApplyResources(Me.lblCaption, "lblCaption")
-        Me.lblCaption.Name = "lblCaption"
-        '
         'lblSubTitle
         '
         resources.ApplyResources(Me.lblSubTitle, "lblSubTitle")
         Me.lblSubTitle.Name = "lblSubTitle"
-        '
-        'ucrInputGraphCaption
-        '
-        Me.ucrInputGraphCaption.AddQuotesIfUnrecognised = True
-        Me.ucrInputGraphCaption.IsMultiline = False
-        Me.ucrInputGraphCaption.IsReadOnly = False
-        resources.ApplyResources(Me.ucrInputGraphCaption, "ucrInputGraphCaption")
-        Me.ucrInputGraphCaption.Name = "ucrInputGraphCaption"
         '
         'ucrInputGraphSubTitle
         '
@@ -169,30 +111,148 @@ Partial Class sdgDistributionOptions
         resources.ApplyResources(Me.ucrInputGraphSubTitle, "ucrInputGraphSubTitle")
         Me.ucrInputGraphSubTitle.Name = "ucrInputGraphSubTitle"
         '
+        'lblCaption
+        '
+        resources.ApplyResources(Me.lblCaption, "lblCaption")
+        Me.lblCaption.Name = "lblCaption"
+        '
+        'ucrInputGraphCaption
+        '
+        Me.ucrInputGraphCaption.AddQuotesIfUnrecognised = True
+        Me.ucrInputGraphCaption.IsMultiline = False
+        Me.ucrInputGraphCaption.IsReadOnly = False
+        resources.ApplyResources(Me.ucrInputGraphCaption, "ucrInputGraphCaption")
+        Me.ucrInputGraphCaption.Name = "ucrInputGraphCaption"
+        '
         'tbpXAxis
         '
+        Me.tbpXAxis.Controls.Add(Me.lblXAxisTitle)
+        Me.tbpXAxis.Controls.Add(Me.rdoXAxisSpecifyTitle)
+        Me.tbpXAxis.Controls.Add(Me.rdoXAxisNoTitle)
+        Me.tbpXAxis.Controls.Add(Me.rdoXAxisAuto)
+        Me.tbpXAxis.Controls.Add(Me.ucrInputXAxisLabel)
+        Me.tbpXAxis.Controls.Add(Me.ucrPnlXAxis)
         resources.ApplyResources(Me.tbpXAxis, "tbpXAxis")
         Me.tbpXAxis.Name = "tbpXAxis"
         Me.tbpXAxis.UseVisualStyleBackColor = True
         '
         'tbpYAxis
         '
+        Me.tbpYAxis.Controls.Add(Me.lblYAxisTitle)
+        Me.tbpYAxis.Controls.Add(Me.rdoYAxisSpecifyTitle)
+        Me.tbpYAxis.Controls.Add(Me.rdoYAxisNoTitle)
+        Me.tbpYAxis.Controls.Add(Me.rdoYAxisAuto)
+        Me.tbpYAxis.Controls.Add(Me.ucrInputYAxisLabel)
+        Me.tbpYAxis.Controls.Add(Me.ucrPnlYAxis)
         resources.ApplyResources(Me.tbpYAxis, "tbpYAxis")
         Me.tbpYAxis.Name = "tbpYAxis"
         Me.tbpYAxis.UseVisualStyleBackColor = True
         '
+        'lblYAxisTitle
+        '
+        resources.ApplyResources(Me.lblYAxisTitle, "lblYAxisTitle")
+        Me.lblYAxisTitle.Name = "lblYAxisTitle"
+        '
+        'rdoYAxisSpecifyTitle
+        '
+        resources.ApplyResources(Me.rdoYAxisSpecifyTitle, "rdoYAxisSpecifyTitle")
+        Me.rdoYAxisSpecifyTitle.Name = "rdoYAxisSpecifyTitle"
+        Me.rdoYAxisSpecifyTitle.TabStop = True
+        Me.rdoYAxisSpecifyTitle.UseVisualStyleBackColor = True
+        '
+        'rdoYAxisNoTitle
+        '
+        resources.ApplyResources(Me.rdoYAxisNoTitle, "rdoYAxisNoTitle")
+        Me.rdoYAxisNoTitle.Name = "rdoYAxisNoTitle"
+        Me.rdoYAxisNoTitle.TabStop = True
+        Me.rdoYAxisNoTitle.UseVisualStyleBackColor = True
+        '
+        'rdoYAxisAuto
+        '
+        resources.ApplyResources(Me.rdoYAxisAuto, "rdoYAxisAuto")
+        Me.rdoYAxisAuto.Name = "rdoYAxisAuto"
+        Me.rdoYAxisAuto.TabStop = True
+        Me.rdoYAxisAuto.UseVisualStyleBackColor = True
+        '
+        'ucrInputYAxisLabel
+        '
+        Me.ucrInputYAxisLabel.AddQuotesIfUnrecognised = True
+        Me.ucrInputYAxisLabel.IsMultiline = False
+        Me.ucrInputYAxisLabel.IsReadOnly = False
+        resources.ApplyResources(Me.ucrInputYAxisLabel, "ucrInputYAxisLabel")
+        Me.ucrInputYAxisLabel.Name = "ucrInputYAxisLabel"
+        '
+        'ucrPnlYAxis
+        '
+        resources.ApplyResources(Me.ucrPnlYAxis, "ucrPnlYAxis")
+        Me.ucrPnlYAxis.Name = "ucrPnlYAxis"
+        '
         'tbpTheme
         '
+        Me.tbpTheme.Controls.Add(Me.lblSelectTheme)
+        Me.tbpTheme.Controls.Add(Me.ucrInputThemes)
         Me.tbpTheme.Controls.Add(Me.lblFont)
         resources.ApplyResources(Me.tbpTheme, "tbpTheme")
         Me.tbpTheme.Name = "tbpTheme"
         Me.tbpTheme.Tag = "Theme"
         Me.tbpTheme.UseVisualStyleBackColor = True
         '
+        'lblSelectTheme
+        '
+        resources.ApplyResources(Me.lblSelectTheme, "lblSelectTheme")
+        Me.lblSelectTheme.Name = "lblSelectTheme"
+        '
+        'ucrInputThemes
+        '
+        Me.ucrInputThemes.AddQuotesIfUnrecognised = True
+        Me.ucrInputThemes.GetSetSelectedIndex = -1
+        Me.ucrInputThemes.IsReadOnly = False
+        resources.ApplyResources(Me.ucrInputThemes, "ucrInputThemes")
+        Me.ucrInputThemes.Name = "ucrInputThemes"
+        '
         'lblFont
         '
         resources.ApplyResources(Me.lblFont, "lblFont")
         Me.lblFont.Name = "lblFont"
+        '
+        'lblXAxisTitle
+        '
+        resources.ApplyResources(Me.lblXAxisTitle, "lblXAxisTitle")
+        Me.lblXAxisTitle.Name = "lblXAxisTitle"
+        '
+        'rdoXAxisSpecifyTitle
+        '
+        resources.ApplyResources(Me.rdoXAxisSpecifyTitle, "rdoXAxisSpecifyTitle")
+        Me.rdoXAxisSpecifyTitle.Name = "rdoXAxisSpecifyTitle"
+        Me.rdoXAxisSpecifyTitle.TabStop = True
+        Me.rdoXAxisSpecifyTitle.UseVisualStyleBackColor = True
+        '
+        'rdoXAxisNoTitle
+        '
+        resources.ApplyResources(Me.rdoXAxisNoTitle, "rdoXAxisNoTitle")
+        Me.rdoXAxisNoTitle.Name = "rdoXAxisNoTitle"
+        Me.rdoXAxisNoTitle.TabStop = True
+        Me.rdoXAxisNoTitle.UseVisualStyleBackColor = True
+        '
+        'rdoXAxisAuto
+        '
+        resources.ApplyResources(Me.rdoXAxisAuto, "rdoXAxisAuto")
+        Me.rdoXAxisAuto.Name = "rdoXAxisAuto"
+        Me.rdoXAxisAuto.TabStop = True
+        Me.rdoXAxisAuto.UseVisualStyleBackColor = True
+        '
+        'ucrInputXAxisLabel
+        '
+        Me.ucrInputXAxisLabel.AddQuotesIfUnrecognised = True
+        Me.ucrInputXAxisLabel.IsMultiline = False
+        Me.ucrInputXAxisLabel.IsReadOnly = False
+        resources.ApplyResources(Me.ucrInputXAxisLabel, "ucrInputXAxisLabel")
+        Me.ucrInputXAxisLabel.Name = "ucrInputXAxisLabel"
+        '
+        'ucrPnlXAxis
+        '
+        resources.ApplyResources(Me.ucrPnlXAxis, "ucrPnlXAxis")
+        Me.ucrPnlXAxis.Name = "ucrPnlXAxis"
         '
         'sdgDistributionOptions
         '
@@ -207,8 +267,10 @@ Partial Class sdgDistributionOptions
         Me.tbpPlotsOptions.ResumeLayout(False)
         Me.tbpTitles.ResumeLayout(False)
         Me.tbpTitles.PerformLayout()
-        Me.grpGraph.ResumeLayout(False)
-        Me.grpGraph.PerformLayout()
+        Me.tbpXAxis.ResumeLayout(False)
+        Me.tbpXAxis.PerformLayout()
+        Me.tbpYAxis.ResumeLayout(False)
+        Me.tbpYAxis.PerformLayout()
         Me.tbpTheme.ResumeLayout(False)
         Me.tbpTheme.PerformLayout()
         Me.ResumeLayout(False)
@@ -228,11 +290,18 @@ Partial Class sdgDistributionOptions
     Friend WithEvents tbpYAxis As TabPage
     Friend WithEvents tbpTheme As TabPage
     Friend WithEvents lblFont As Label
-    Friend WithEvents urChkSelectTheme As ucrCheck
     Friend WithEvents ucrInputThemes As ucrInputComboBox
-    Friend WithEvents lblYAxis As Label
-    Friend WithEvents UcrInputTextBox2 As ucrInputTextBox
-    Friend WithEvents lblXAxis As Label
-    Friend WithEvents UcrInputTextBox1 As ucrInputTextBox
-    Friend WithEvents grpGraph As GroupBox
+    Friend WithEvents lblSelectTheme As Label
+    Friend WithEvents lblYAxisTitle As Label
+    Friend WithEvents rdoYAxisSpecifyTitle As RadioButton
+    Friend WithEvents rdoYAxisNoTitle As RadioButton
+    Friend WithEvents rdoYAxisAuto As RadioButton
+    Friend WithEvents ucrInputYAxisLabel As ucrInputTextBox
+    Friend WithEvents ucrPnlYAxis As UcrPanel
+    Friend WithEvents lblXAxisTitle As Label
+    Friend WithEvents rdoXAxisSpecifyTitle As RadioButton
+    Friend WithEvents rdoXAxisNoTitle As RadioButton
+    Friend WithEvents rdoXAxisAuto As RadioButton
+    Friend WithEvents ucrInputXAxisLabel As ucrInputTextBox
+    Friend WithEvents ucrPnlXAxis As UcrPanel
 End Class
