d4d224755cf6457b48d031197c6cee79d487b5b6
Haward Ketoyo <hketoyo@gmail.com>
2021-03-12 15:53:43 +0300

    Implemented code for dlgCircularDensity plot. The dialog implements, circular Density, Rose plot and Scatter plot.

--- instat/dlgCircularDensityPlot.Designer.vb
+++ instat/dlgCircularDensityPlot.Designer.vb
@@ -23,30 +23,243 @@ Partial Class dlgCircularDensityPlot
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.ucrBase = New instat.ucrButtons()
+        Me.lblUnits = New System.Windows.Forms.Label()
+        Me.ucrInputUnits = New instat.ucrInputComboBox()
+        Me.rdoScatterPlot = New System.Windows.Forms.RadioButton()
+        Me.rdoDensity = New System.Windows.Forms.RadioButton()
+        Me.rdoRosePlot = New System.Windows.Forms.RadioButton()
+        Me.ucrPnlOptions = New instat.UcrPanel()
+        Me.lblKernel = New System.Windows.Forms.Label()
+        Me.ucrInputComboKernel = New instat.ucrInputComboBox()
+        Me.lblBandWidth = New System.Windows.Forms.Label()
+        Me.ucrInputBandWidth = New instat.ucrInputTextBox()
+        Me.ucrChkOmitMissing = New instat.ucrCheck()
+        Me.ucrSaveDensity = New instat.ucrSave()
+        Me.lblVariable = New System.Windows.Forms.Label()
+        Me.ucrReceiverVariable = New instat.ucrReceiverSingle()
+        Me.ucrSelectorDataFrame = New instat.ucrSelectorByDataFrameAddRemove()
         Me.SuspendLayout()
         '
         'ucrBase
         '
-        Me.ucrBase.Location = New System.Drawing.Point(5, 238)
+        Me.ucrBase.Location = New System.Drawing.Point(5, 326)
         Me.ucrBase.Name = "ucrBase"
         Me.ucrBase.Size = New System.Drawing.Size(410, 52)
         Me.ucrBase.TabIndex = 0
+        '
+        'lblUnits
+        '
+        Me.lblUnits.AutoSize = True
+        Me.lblUnits.Location = New System.Drawing.Point(269, 269)
+        Me.lblUnits.Name = "lblUnits"
+        Me.lblUnits.Size = New System.Drawing.Size(34, 13)
+        Me.lblUnits.TabIndex = 30
+        Me.lblUnits.Text = "Units:"
+        '
+        'ucrInputUnits
+        '
+        Me.ucrInputUnits.AddQuotesIfUnrecognised = True
+        Me.ucrInputUnits.GetSetSelectedIndex = -1
+        Me.ucrInputUnits.IsReadOnly = False
+        Me.ucrInputUnits.Location = New System.Drawing.Point(317, 265)
+        Me.ucrInputUnits.Name = "ucrInputUnits"
+        Me.ucrInputUnits.Size = New System.Drawing.Size(93, 21)
+        Me.ucrInputUnits.TabIndex = 29
+        '
+        'rdoScatterPlot
+        '
+        Me.rdoScatterPlot.Appearance = System.Windows.Forms.Appearance.Button
+        Me.rdoScatterPlot.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoScatterPlot.FlatAppearance.BorderSize = 2
+        Me.rdoScatterPlot.FlatAppearance.CheckedBackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoScatterPlot.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.rdoScatterPlot.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.rdoScatterPlot.Location = New System.Drawing.Point(268, 11)
+        Me.rdoScatterPlot.Name = "rdoScatterPlot"
+        Me.rdoScatterPlot.Size = New System.Drawing.Size(125, 28)
+        Me.rdoScatterPlot.TabIndex = 28
+        Me.rdoScatterPlot.TabStop = True
+        Me.rdoScatterPlot.Tag = "ScatterOPlot"
+        Me.rdoScatterPlot.Text = "Scatter Plot"
+        Me.rdoScatterPlot.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.rdoScatterPlot.UseVisualStyleBackColor = True
+        '
+        'rdoDensity
+        '
+        Me.rdoDensity.Appearance = System.Windows.Forms.Appearance.Button
+        Me.rdoDensity.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoDensity.FlatAppearance.BorderSize = 2
+        Me.rdoDensity.FlatAppearance.CheckedBackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoDensity.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.rdoDensity.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.rdoDensity.Location = New System.Drawing.Point(145, 11)
+        Me.rdoDensity.Name = "rdoDensity"
+        Me.rdoDensity.Size = New System.Drawing.Size(125, 28)
+        Me.rdoDensity.TabIndex = 27
+        Me.rdoDensity.TabStop = True
+        Me.rdoDensity.Tag = "Density"
+        Me.rdoDensity.Text = "Density"
+        Me.rdoDensity.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.rdoDensity.UseVisualStyleBackColor = True
+        '
+        'rdoRosePlot
+        '
+        Me.rdoRosePlot.Appearance = System.Windows.Forms.Appearance.Button
+        Me.rdoRosePlot.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoRosePlot.FlatAppearance.BorderSize = 2
+        Me.rdoRosePlot.FlatAppearance.CheckedBackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoRosePlot.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.rdoRosePlot.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.rdoRosePlot.Location = New System.Drawing.Point(22, 11)
+        Me.rdoRosePlot.Name = "rdoRosePlot"
+        Me.rdoRosePlot.Size = New System.Drawing.Size(125, 28)
+        Me.rdoRosePlot.TabIndex = 26
+        Me.rdoRosePlot.TabStop = True
+        Me.rdoRosePlot.Tag = "RosePlot"
+        Me.rdoRosePlot.Text = "Roseplot"
+        Me.rdoRosePlot.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.rdoRosePlot.UseVisualStyleBackColor = True
+        '
+        'ucrPnlOptions
+        '
+        Me.ucrPnlOptions.Location = New System.Drawing.Point(21, 10)
+        Me.ucrPnlOptions.Margin = New System.Windows.Forms.Padding(5)
+        Me.ucrPnlOptions.Name = "ucrPnlOptions"
+        Me.ucrPnlOptions.Size = New System.Drawing.Size(373, 30)
+        Me.ucrPnlOptions.TabIndex = 25
+        '
+        'lblKernel
+        '
+        Me.lblKernel.AutoSize = True
+        Me.lblKernel.Location = New System.Drawing.Point(269, 245)
+        Me.lblKernel.Name = "lblKernel"
+        Me.lblKernel.Size = New System.Drawing.Size(43, 13)
+        Me.lblKernel.TabIndex = 24
+        Me.lblKernel.Text = "Kernel :"
+        '
+        'ucrInputComboKernel
+        '
+        Me.ucrInputComboKernel.AddQuotesIfUnrecognised = True
+        Me.ucrInputComboKernel.GetSetSelectedIndex = -1
+        Me.ucrInputComboKernel.IsReadOnly = False
+        Me.ucrInputComboKernel.Location = New System.Drawing.Point(317, 241)
+        Me.ucrInputComboKernel.Name = "ucrInputComboKernel"
+        Me.ucrInputComboKernel.Size = New System.Drawing.Size(93, 21)
+        Me.ucrInputComboKernel.TabIndex = 23
+        '
+        'lblBandWidth
+        '
+        Me.lblBandWidth.AutoSize = True
+        Me.lblBandWidth.Location = New System.Drawing.Point(270, 212)
+        Me.lblBandWidth.Name = "lblBandWidth"
+        Me.lblBandWidth.Size = New System.Drawing.Size(63, 13)
+        Me.lblBandWidth.TabIndex = 22
+        Me.lblBandWidth.Text = "Bandwidth: "
+        '
+        'ucrInputBandWidth
+        '
+        Me.ucrInputBandWidth.AddQuotesIfUnrecognised = True
+        Me.ucrInputBandWidth.IsMultiline = False
+        Me.ucrInputBandWidth.IsReadOnly = False
+        Me.ucrInputBandWidth.Location = New System.Drawing.Point(352, 209)
+        Me.ucrInputBandWidth.Name = "ucrInputBandWidth"
+        Me.ucrInputBandWidth.Size = New System.Drawing.Size(56, 21)
+        Me.ucrInputBandWidth.TabIndex = 21
+        '
+        'ucrChkOmitMissing
+        '
+        Me.ucrChkOmitMissing.Checked = False
+        Me.ucrChkOmitMissing.Location = New System.Drawing.Point(10, 269)
+        Me.ucrChkOmitMissing.Name = "ucrChkOmitMissing"
+        Me.ucrChkOmitMissing.Size = New System.Drawing.Size(170, 20)
+        Me.ucrChkOmitMissing.TabIndex = 20
+        '
+        'ucrSaveDensity
+        '
+        Me.ucrSaveDensity.Location = New System.Drawing.Point(10, 297)
+        Me.ucrSaveDensity.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.ucrSaveDensity.Name = "ucrSaveDensity"
+        Me.ucrSaveDensity.Size = New System.Drawing.Size(249, 21)
+        Me.ucrSaveDensity.TabIndex = 19
+        '
+        'lblVariable
+        '
+        Me.lblVariable.AutoSize = True
+        Me.lblVariable.Location = New System.Drawing.Point(285, 50)
+        Me.lblVariable.Name = "lblVariable"
+        Me.lblVariable.Size = New System.Drawing.Size(48, 13)
+        Me.lblVariable.TabIndex = 18
+        Me.lblVariable.Text = "Variable:"
+        '
+        'ucrReceiverVariable
+        '
+        Me.ucrReceiverVariable.frmParent = Me
+        Me.ucrReceiverVariable.Location = New System.Drawing.Point(289, 64)
+        Me.ucrReceiverVariable.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverVariable.Name = "ucrReceiverVariable"
+        Me.ucrReceiverVariable.Selector = Nothing
+        Me.ucrReceiverVariable.Size = New System.Drawing.Size(120, 20)
+        Me.ucrReceiverVariable.strNcFilePath = ""
+        Me.ucrReceiverVariable.TabIndex = 17
+        Me.ucrReceiverVariable.ucrSelector = Nothing
+        '
+        'ucrSelectorDataFrame
+        '
+        Me.ucrSelectorDataFrame.bDropUnusedFilterLevels = False
+        Me.ucrSelectorDataFrame.bShowHiddenColumns = False
+        Me.ucrSelectorDataFrame.bUseCurrentFilter = True
+        Me.ucrSelectorDataFrame.Location = New System.Drawing.Point(10, 50)
+        Me.ucrSelectorDataFrame.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrSelectorDataFrame.Name = "ucrSelectorDataFrame"
+        Me.ucrSelectorDataFrame.Size = New System.Drawing.Size(210, 180)
+        Me.ucrSelectorDataFrame.TabIndex = 16
         '
         'dlgCircularDensityPlot
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(420, 296)
+        Me.ClientSize = New System.Drawing.Size(420, 389)
+        Me.Controls.Add(Me.lblUnits)
+        Me.Controls.Add(Me.ucrInputUnits)
+        Me.Controls.Add(Me.rdoScatterPlot)
+        Me.Controls.Add(Me.rdoDensity)
+        Me.Controls.Add(Me.rdoRosePlot)
+        Me.Controls.Add(Me.ucrPnlOptions)
+        Me.Controls.Add(Me.lblKernel)
+        Me.Controls.Add(Me.ucrInputComboKernel)
+        Me.Controls.Add(Me.lblBandWidth)
+        Me.Controls.Add(Me.ucrInputBandWidth)
+        Me.Controls.Add(Me.ucrChkOmitMissing)
+        Me.Controls.Add(Me.ucrSaveDensity)
+        Me.Controls.Add(Me.lblVariable)
+        Me.Controls.Add(Me.ucrReceiverVariable)
+        Me.Controls.Add(Me.ucrSelectorDataFrame)
         Me.Controls.Add(Me.ucrBase)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "dlgCircularDensityPlot"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "Density Plot"
+        Me.Text = "Circular Plots"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents ucrBase As ucrButtons
+    Friend WithEvents lblUnits As Label
+    Friend WithEvents ucrInputUnits As ucrInputComboBox
+    Friend WithEvents rdoScatterPlot As RadioButton
+    Friend WithEvents rdoDensity As RadioButton
+    Friend WithEvents rdoRosePlot As RadioButton
+    Friend WithEvents ucrPnlOptions As UcrPanel
+    Friend WithEvents lblKernel As Label
+    Friend WithEvents ucrInputComboKernel As ucrInputComboBox
+    Friend WithEvents lblBandWidth As Label
+    Friend WithEvents ucrInputBandWidth As ucrInputTextBox
+    Friend WithEvents ucrChkOmitMissing As ucrCheck
+    Friend WithEvents ucrSaveDensity As ucrSave
+    Friend WithEvents lblVariable As Label
+    Friend WithEvents ucrReceiverVariable As ucrReceiverSingle
+    Friend WithEvents ucrSelectorDataFrame As ucrSelectorByDataFrameAddRemove
 End Class
--- instat/dlgCircularDensityPlot.vb
+++ instat/dlgCircularDensityPlot.vb
@@ -1,3 +1,209 @@
-﻿Public Class dlgCircularDensityPlot
+﻿' R- Instat
+' Copyright (C) 2015-2017
+'
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+'
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License 
+' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+Imports instat.Translations
+
+Public Class dlgCircularDensityPlot
+    Private bFirstLoad As Boolean = True
+    Private bReset As Boolean = True
+    Private clsDensityFunction As RFunction
+    Private clsRecordPlotFunction As RFunction
+    Private clsDensityPlotFunction, clsRosePlotFunction, clsScatterPlotFunction As RFunction
+    Private Sub dlgCircularDensityPlot_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If bFirstLoad Then
+            InitialiseDialog()
+            bFirstLoad = False
+        End If
+        If bReset Then
+            SetDefaults()
+        End If
+        SetRCodeForControls(bReset)
+        bReset = False
+        autoTranslate(Me)
+        TestOkEnabled()
+    End Sub
+
+    Private Sub InitialiseDialog()
+        Dim dctKernel As New Dictionary(Of String, String)
+        Dim dctUnits As New Dictionary(Of String, String)
+
+        ucrBase.clsRsyntax.iCallType = 3
+        ucrBase.clsRsyntax.bExcludeAssignedFunctionOutput = False
+
+        ucrPnlOptions.AddRadioButton(rdoRosePlot)
+        ucrPnlOptions.AddRadioButton(rdoDensity)
+        ucrPnlOptions.AddRadioButton(rdoScatterPlot)
+
+        ucrPnlOptions.AddFunctionNamesCondition(rdoRosePlot, "rose.diag")
+        ucrPnlOptions.AddFunctionNamesCondition(rdoDensity, "plot")
+        ucrPnlOptions.AddFunctionNamesCondition(rdoScatterPlot, "plot.circular")
+
+        ucrPnlOptions.AddToLinkedControls(ucrInputBandWidth, {rdoDensity}, bNewLinkedHideIfParameterMissing:=True)
+
+        ucrSelectorDataFrame.SetParameter(New RParameter("data", 0))
+        ucrSelectorDataFrame.SetParameterIsrfunction()
+
+        ucrReceiverVariable.SetParameter(New RParameter("x", 0))
+        ucrReceiverVariable.Selector = ucrSelectorDataFrame
+        ucrReceiverVariable.SetParameterIsRFunction()
+        ucrReceiverVariable.SetIncludedDataTypes({"numeric"})
+        ucrReceiverVariable.strSelectorHeading = "Numerics"
+
+        ucrInputBandWidth.SetParameter(New RParameter("bw", 1))
+        ucrInputBandWidth.SetValidationTypeAsNumeric()
+        ucrInputBandWidth.AddQuotesIfUnrecognised = False
+        ucrInputBandWidth.SetLinkedDisplayControl(lblBandWidth)
+
+        ucrInputComboKernel.SetParameter(New RParameter("kernel", 2))
+        dctKernel.Add("vonmises", Chr(34) & "vonmises" & Chr(34))
+        dctKernel.Add("wrappednormal", Chr(34) & "wrappednormal" & Chr(34))
+        ucrInputComboKernel.SetItems(dctKernel)
+        ucrInputComboKernel.SetDropDownStyleAsNonEditable()
+        ucrInputComboKernel.SetLinkedDisplayControl(lblKernel)
+
+        ucrChkOmitMissing.SetText("Omit Missing Values")
+        ucrChkOmitMissing.SetParameter(New RParameter("na.rm", 3))
+        ucrChkOmitMissing.SetText("Omit Missing Values")
+        ucrChkOmitMissing.SetValuesCheckedAndUnchecked("TRUE", "FALSE")
+
+        ucrInputUnits.SetParameter(New RParameter("units", 5))
+        dctUnits.Add("degrees", Chr(34) & "degrees" & Chr(34))
+        dctUnits.Add("radinas", Chr(34) & "radians" & Chr(34))
+        dctUnits.Add("hours", Chr(34) & "hours" & Chr(34))
+        ucrInputUnits.SetItems(dctUnits)
+        ucrInputUnits.SetDropDownStyleAsNonEditable()
+        ucrInputUnits.SetLinkedDisplayControl(lblUnits)
+
+        ucrSaveDensity.SetPrefix("circular_density")
+        ucrSaveDensity.SetDataFrameSelector(ucrSelectorDataFrame.ucrAvailableDataFrames)
+        ucrSaveDensity.SetIsComboBox()
+        ucrSaveDensity.SetCheckBoxText("Save Graph")
+        ucrSaveDensity.SetSaveTypeAsGraph()
+        ucrSaveDensity.SetAssignToIfUncheckedValue("last_graph")
+
+    End Sub
+
+    Private Sub SetDefaults()
+        clsDensityFunction = New RFunction
+        clsDensityPlotFunction = New RFunction
+        clsRecordPlotFunction = New RFunction
+        clsRosePlotFunction = New RFunction
+        clsScatterPlotFunction = New RFunction
+
+        ucrSaveDensity.Reset()
+        ucrReceiverVariable.SetMeAsReceiver()
+        ucrSelectorDataFrame.Reset()
+
+        clsDensityFunction.SetPackageName("circular")
+        clsDensityFunction.SetRCommand("density.circular")
+        clsDensityFunction.AddParameter("bw", "25", iPosition:=1)
+        clsDensityFunction.AddParameter("kernel", Chr(34) & "vonmises" & Chr(34), iPosition:=2)
+        clsDensityFunction.AddParameter("na.rm", "TRUE", iPosition:=3)
+
+        clsRosePlotFunction.SetPackageName("circular")
+        clsRosePlotFunction.SetRCommand("rose.diag")
+        clsRosePlotFunction.AddParameter("na.rm", "TRUE", iPosition:=3)
+        clsRosePlotFunction.AddParameter("prop", 2.9, bIncludeArgumentName:=True, iPosition:=4)
+        clsRosePlotFunction.AddParameter("bin", 36, bIncludeArgumentName:=True, iPosition:=5)
+        clsRosePlotFunction.AddParameter("col", Chr(34) & "blue" & Chr(34), bIncludeArgumentName:=True, iPosition:=6)
+        clsRosePlotFunction.AddParameter("kernel", Chr(34) & "vonmises" & Chr(34), iPosition:=7)
+        clsRosePlotFunction.AddParameter("units", Chr(34) & "degrees" & Chr(34), iPosition:=8)
+
+        clsScatterPlotFunction.SetPackageName("circular")
+        clsScatterPlotFunction.SetRCommand("plot.circular")
+        clsScatterPlotFunction.AddParameter("na.rm", "TRUE", iPosition:=3)
+        clsScatterPlotFunction.AddParameter("stack", "TRUE", iPosition:=4)
+        clsScatterPlotFunction.AddParameter("sep", 0.0003, bIncludeArgumentName:=True, iPosition:=5)
+        clsScatterPlotFunction.AddParameter("shrink", 2, bIncludeArgumentName:=True, iPosition:=6)
+        clsScatterPlotFunction.AddParameter("kernel", Chr(34) & "vonmises" & Chr(34), iPosition:=7)
+        clsScatterPlotFunction.AddParameter("units", Chr(34) & "degrees" & Chr(34), iPosition:=8)
+
+
+        clsDensityPlotFunction.SetRCommand("plot")
+        clsDensityPlotFunction.AddParameter("x", clsRFunctionParameter:=clsDensityFunction, bIncludeArgumentName:=False, iPosition:=0)
+
+        clsDensityPlotFunction.AddParameter("main", Chr(34) & " " & Chr(34), bIncludeArgumentName:=True, iPosition:=1)
+        clsDensityPlotFunction.AddParameter("xlab", Chr(34) & " " & Chr(34), bIncludeArgumentName:=True, iPosition:=2)
+        clsDensityPlotFunction.AddParameter("ylab", Chr(34) & " " & Chr(34), bIncludeArgumentName:=True, iPosition:=3)
+        clsDensityPlotFunction.AddParameter("shrink", 1.3, bIncludeArgumentName:=True, iPosition:=4)
+        clsDensityPlotFunction.AddParameter("units", Chr(34) & "degrees" & Chr(34), iPosition:=5)
+
+        clsRecordPlotFunction.SetPackageName("grDevices")
+        clsRecordPlotFunction.SetRCommand("recordPlot")
+
+        ucrBase.clsRsyntax.SetBaseRFunction(clsDensityPlotFunction)
+    End Sub
+
+    Private Sub SetRCodeForControls(bReset As Boolean)
+        'ucrInputBandWidth.AddAdditionalCodeParameterPair(clsDensityFunction, ucrInputBandWidth.GetParameter(), iAdditionalPairNo:=1)
+        'ucrChkOmitMissing.AddAdditionalCodeParameterPair(clsDensityFunction, ucrChkOmitMissing.GetParameter(), iAdditionalPairNo:=1)
+        'ucrInputComboKernel.AddAdditionalCodeParameterPair(clsDensityFunction, ucrInputComboKernel.GetParameter(), iAdditionalPairNo:=1)
+        ucrPnlOptions.SetRCode(ucrBase.clsRsyntax.clsBaseFunction, bReset)
+
+        ucrSaveDensity.SetRCode(clsDensityPlotFunction, bReset)
+        ucrSaveDensity.AddAdditionalRCode(clsRosePlotFunction, iAdditionalPairNo:=1)
+        ucrSaveDensity.AddAdditionalRCode(clsScatterPlotFunction, iAdditionalPairNo:=2)
+
+        ucrReceiverVariable.SetRCode(clsDensityFunction, bReset)
+        ucrReceiverVariable.AddAdditionalCodeParameterPair(clsRosePlotFunction, New RParameter("x", 0), iAdditionalPairNo:=1)
+        ucrReceiverVariable.AddAdditionalCodeParameterPair(clsScatterPlotFunction, New RParameter("x", 0), iAdditionalPairNo:=2)
+        ucrInputBandWidth.SetRCode(clsDensityFunction, bReset)
+        ucrInputComboKernel.SetRCode(clsDensityFunction, bReset)
+        ucrInputComboKernel.AddAdditionalCodeParameterPair(clsRosePlotFunction, New RParameter("kernel", 7), iAdditionalPairNo:=1)
+        ucrInputComboKernel.AddAdditionalCodeParameterPair(clsScatterPlotFunction, New RParameter("kernel", 7), iAdditionalPairNo:=2)
+
+
+        ucrInputUnits.SetRCode(clsDensityPlotFunction, bReset)
+        ucrInputUnits.AddAdditionalCodeParameterPair(clsRosePlotFunction, New RParameter("units", 2), iAdditionalPairNo:=1)
+        ucrInputUnits.AddAdditionalCodeParameterPair(clsScatterPlotFunction, New RParameter("units", 2), iAdditionalPairNo:=2)
+
+        ucrChkOmitMissing.SetRCode(clsDensityFunction, bReset)
+        ucrChkOmitMissing.AddAdditionalCodeParameterPair(clsRosePlotFunction, New RParameter("na.rm", 1), iAdditionalPairNo:=1)
+        ucrChkOmitMissing.AddAdditionalCodeParameterPair(clsScatterPlotFunction, New RParameter("na.rm", 1), iAdditionalPairNo:=2)
+
+    End Sub
+
+    Private Sub TestOkEnabled()
+        If ucrSaveDensity.IsComplete AndAlso Not ucrReceiverVariable.IsEmpty Then
+            ucrBase.OKEnabled(True)
+        Else
+            ucrBase.OKEnabled(False)
+        End If
+    End Sub
+
+    Private Sub ucrBase_ClickReset(sender As Object, e As EventArgs) Handles ucrBase.ClickReset
+        SetDefaults()
+        SetRCodeForControls(True)
+        TestOkEnabled()
+    End Sub
+
+    Private Sub CoreControls_ControlContentsChanged() Handles ucrReceiverVariable.ControlContentsChanged, ucrSaveDensity.ControlContentsChanged
+        TestOkEnabled()
+    End Sub
+
+    Private Sub ucrPnlOptions_ControlValueChanged(ucrChangedControl As ucrCore) Handles ucrPnlOptions.ControlValueChanged
+        If rdoDensity.Checked Then
+            ucrBase.clsRsyntax.SetBaseRFunction(clsDensityPlotFunction)
+            ucrSaveDensity.SetPrefix("density_plot")
+        ElseIf rdoRosePlot.Checked Then
+            ucrBase.clsRsyntax.SetBaseRFunction(clsRosePlotFunction)
+            ucrSaveDensity.SetPrefix("rose_plot")
+        ElseIf rdoScatterPlot.Checked Then
+            ucrBase.clsRsyntax.SetBaseRFunction(clsScatterPlotFunction)
+            ucrSaveDensity.SetPrefix("scatter_plot")
+        End If
+    End Sub
 End Class
