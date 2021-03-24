a5c514d1876efe0c0567b0fe3b8acf4dbc42d4e4
Lily Clements <lclements1004@outlook.com>
2021-03-24 13:04:19 +0000

    Remove line from defaults

--- instat/dlgDuplicates.vb
+++ instat/dlgDuplicates.vb
@@ -158,7 +158,6 @@ Public Class dlgDuplicates
         clsDupCountIndex.SetRCommand("duplicated_count_index")
 
         clsGetColumnsFunction.SetRCommand(frmMain.clsRLink.strInstatDataObject & "$get_columns_from_data")
-        clsGetColumnsFunction.AddParameter(strParameterName:="data_name", strParameterValue:=Chr(34) & ucrSelectorDuplicateswithVariables.ucrAvailableDataFrames.cboAvailableDataFrames.SelectedItem & Chr(34), iPosition:=0)
 
         clsSummaryFunction.SetRCommand("summary")
         clsSummaryFunction.AddParameter("object", clsRFunctionParameter:=clsGetColumnsFunction)
