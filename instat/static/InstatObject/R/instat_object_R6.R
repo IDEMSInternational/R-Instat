784da721a89cda3a310d2b07d2c7a65fc44a4881
shadrack kibet <shadrackkibet36@outlook.com>
2021-03-29 19:25:23 +0300

    removed redundant rcode

--- instat/static/InstatObject/R/instat_object_R6.R
+++ instat/static/InstatObject/R/instat_object_R6.R
@@ -2498,8 +2498,6 @@ DataBook$set("public", "save_data_entry_data", function(data_name, new_data, row
 DataBook$set("public", "import_from_cds", function(user, dataset, elements, start_date, end_date, lon, lat, path, import = FALSE, new_name) {
   all_dates <- seq(start_date, end_date, by = 1)
   all_periods <- unique(paste(lubridate::year(all_dates), sprintf("%02d", lubridate::month(all_dates)), sep = "-"))
-  all_days <- c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31")
-  all_hours <- c("00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00")
   area <- c(lat[2], lon[1], lat[1], lon[2])
   pb <- winProgressBar(title = "Requesting data from CDS", min = 0, max = length(all_periods))
   nc_files <- vector(mode = "character", length = length(all_periods))
