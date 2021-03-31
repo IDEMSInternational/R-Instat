686841336ec9bef609bdd7063bdc6314433d7ef6
Haward Ketoyo <haward.k.msatsi@aims-senegal.org>
2021-03-31 09:23:54 +0100

    Update instat/static/InstatObject/R/stand_alone_functions.R
    Co-authored-by: Shadrack Kibet <shadrackkibet36@outlook.com>

--- instat/static/InstatObject/R/stand_alone_functions.R
+++ instat/static/InstatObject/R/stand_alone_functions.R
@@ -2282,7 +2282,7 @@ plot_declustered <- function(data, station_col_name, element_col_name, threshold
 
 #This function creates awrarper around functions from openair package
 other_rose_plots <- function(data, type1_col_name, type2_col_name, date_col_name, wd_col_name, ws_col_name, main_method, single_pollutant, multiple_pollutant, ...) {
-  type = "default"
+  type <- "default"
   if (!missing(type1_col_name) && !missing(type2_col_name)) {
     type <- c(type1_col_name, type2_col_name)
   }
