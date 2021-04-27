a538ed1218cd3d2fbb533960a5060d66d74b2f88
Antoine Ntalumeso <antoine.ntalumeso@gmail.com>
2021-04-27 10:31:57 +0200

    Resolved comment about bug in add key

--- instat/dlgSetupForDataEntry.Designer.vb
+++ instat/dlgSetupForDataEntry.Designer.vb
@@ -20,7 +20,7 @@ Partial Class dlgSetupForDataEntry
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(dlgSetupForDataEntry))
 
@@ -365,8 +365,6 @@ Partial Class dlgSetupForDataEntry
     Friend WithEvents lblDate As Label
     Friend WithEvents ucrChkAddKey As ucrCheck
 
-        Me.ResumeLayout(False)
 
-    End Sub
 
 End Class
