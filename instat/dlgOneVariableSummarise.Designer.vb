e304fde6c7599fe2f9520eae213664611bdcc869
lloyddewit <stephen.lloyd03@gmail.com>
2021-08-06 12:37:49 +0200

    Removed all conflicting files. I used the following Git commands: $ git merge-base --fork-point master 17a9e24d5d854010d54813efbff83415b8605436 $ git checkout 17a9e24d5d854010d54813efbff83415b8605436 -- dlgOneVariableSummarise.Designer.vb dlgOneVariableSummarise.resx dlgOneWayFrequencies.resx

--- instat/dlgOneVariableSummarise.Designer.vb
+++ instat/dlgOneVariableSummarise.Designer.vb
@@ -140,15 +140,15 @@ Partial Class dlgOneVariableSummarise
         Me.Controls.Add(Me.rdoCustomised)
         Me.Controls.Add(Me.rdoDefault)
         Me.Controls.Add(Me.lblSummaries)
+        Me.Controls.Add(Me.lblMaxSum)
         Me.Controls.Add(Me.ucrNudMaxSum)
+        Me.Controls.Add(Me.ucrChkOmitMissing)
         Me.Controls.Add(Me.ucrSelectorOneVarSummarise)
         Me.Controls.Add(Me.ucrReceiverOneVarSummarise)
         Me.Controls.Add(Me.ucrBase)
         Me.Controls.Add(Me.lblSelectedVariable)
         Me.Controls.Add(Me.cmdSummaries)
         Me.Controls.Add(Me.ucrPnlSummaries)
-        Me.Controls.Add(Me.lblMaxSum)
-        Me.Controls.Add(Me.ucrChkOmitMissing)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.MaximizeBox = False
         Me.MinimizeBox = False
