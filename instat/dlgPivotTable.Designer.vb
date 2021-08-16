34c838a989a5945e5dae3fc5c08e656b750b92e3
ivanluv <luvisiaivan@gmail.com>
2021-08-16 16:15:47 +0300

    Code additions

--- instat/dlgPivotTable.Designer.vb
+++ instat/dlgPivotTable.Designer.vb
@@ -22,19 +22,169 @@ Partial Class dlgPivotTable
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(dlgPivotTable))
+        Me.lblInitialRowFactor = New System.Windows.Forms.Label()
+        Me.lblInitialColumnFactor = New System.Windows.Forms.Label()
+        Me.lblAdditionalRowFactor = New System.Windows.Forms.Label()
+        Me.ttSelectedVariable = New System.Windows.Forms.ToolTip(Me.components)
+        Me.ucrReceiverSelectedVariable = New instat.ucrReceiverMultiple()
+        Me.ucrChkSelectedVariable = New instat.ucrCheck()
+        Me.UcrInputComboBox2 = New instat.ucrInputComboBox()
+        Me.UcrInputComboBox1 = New instat.ucrInputComboBox()
+        Me.ucrInputSummaries = New instat.ucrInputComboBox()
+        Me.ucrReceiveradditionalRowFactor = New instat.ucrReceiverSingle()
+        Me.ucrReceiverInitialColumnFactor = New instat.ucrReceiverSingle()
+        Me.ucrReceiverInitialRowFactor = New instat.ucrReceiverSingle()
+        Me.ucrBase = New instat.ucrButtons()
+        Me.ucrSelectorPivot = New instat.ucrSelectorByDataFrameAddRemove()
+        Me.ucrChkIncludeSubTotals = New instat.ucrCheck()
         Me.SuspendLayout()
+        '
+        'lblInitialRowFactor
+        '
+        resources.ApplyResources(Me.lblInitialRowFactor, "lblInitialRowFactor")
+        Me.lblInitialRowFactor.Name = "lblInitialRowFactor"
+        Me.lblInitialRowFactor.Tag = "Initial Row Factor:"
+        '
+        'lblInitialColumnFactor
+        '
+        resources.ApplyResources(Me.lblInitialColumnFactor, "lblInitialColumnFactor")
+        Me.lblInitialColumnFactor.Name = "lblInitialColumnFactor"
+        Me.lblInitialColumnFactor.Tag = "Initial Column Factor:"
+        '
+        'lblAdditionalRowFactor
+        '
+        resources.ApplyResources(Me.lblAdditionalRowFactor, "lblAdditionalRowFactor")
+        Me.lblAdditionalRowFactor.Name = "lblAdditionalRowFactor"
+        Me.lblAdditionalRowFactor.Tag = "Additional Row Factor"
+        '
+        'ucrReceiverSelectedVariable
+        '
+        Me.ucrReceiverSelectedVariable.frmParent = Me
+        resources.ApplyResources(Me.ucrReceiverSelectedVariable, "ucrReceiverSelectedVariable")
+        Me.ucrReceiverSelectedVariable.Name = "ucrReceiverSelectedVariable"
+        Me.ucrReceiverSelectedVariable.Selector = Nothing
+        Me.ucrReceiverSelectedVariable.strNcFilePath = ""
+        Me.ucrReceiverSelectedVariable.ucrSelector = Nothing
+        '
+        'ucrChkSelectedVariable
+        '
+        Me.ucrChkSelectedVariable.Checked = False
+        resources.ApplyResources(Me.ucrChkSelectedVariable, "ucrChkSelectedVariable")
+        Me.ucrChkSelectedVariable.Name = "ucrChkSelectedVariable"
+        '
+        'UcrInputComboBox2
+        '
+        Me.UcrInputComboBox2.AddQuotesIfUnrecognised = True
+        Me.UcrInputComboBox2.GetSetSelectedIndex = -1
+        Me.UcrInputComboBox2.IsReadOnly = False
+        resources.ApplyResources(Me.UcrInputComboBox2, "UcrInputComboBox2")
+        Me.UcrInputComboBox2.Name = "UcrInputComboBox2"
+        '
+        'UcrInputComboBox1
+        '
+        Me.UcrInputComboBox1.AddQuotesIfUnrecognised = True
+        Me.UcrInputComboBox1.GetSetSelectedIndex = -1
+        Me.UcrInputComboBox1.IsReadOnly = False
+        resources.ApplyResources(Me.UcrInputComboBox1, "UcrInputComboBox1")
+        Me.UcrInputComboBox1.Name = "UcrInputComboBox1"
+        '
+        'ucrInputSummaries
+        '
+        Me.ucrInputSummaries.AddQuotesIfUnrecognised = True
+        Me.ucrInputSummaries.GetSetSelectedIndex = -1
+        Me.ucrInputSummaries.IsReadOnly = False
+        resources.ApplyResources(Me.ucrInputSummaries, "ucrInputSummaries")
+        Me.ucrInputSummaries.Name = "ucrInputSummaries"
+        '
+        'ucrReceiveradditionalRowFactor
+        '
+        Me.ucrReceiveradditionalRowFactor.frmParent = Me
+        resources.ApplyResources(Me.ucrReceiveradditionalRowFactor, "ucrReceiveradditionalRowFactor")
+        Me.ucrReceiveradditionalRowFactor.Name = "ucrReceiveradditionalRowFactor"
+        Me.ucrReceiveradditionalRowFactor.Selector = Nothing
+        Me.ucrReceiveradditionalRowFactor.strNcFilePath = ""
+        Me.ucrReceiveradditionalRowFactor.ucrSelector = Nothing
+        '
+        'ucrReceiverInitialColumnFactor
+        '
+        Me.ucrReceiverInitialColumnFactor.frmParent = Me
+        resources.ApplyResources(Me.ucrReceiverInitialColumnFactor, "ucrReceiverInitialColumnFactor")
+        Me.ucrReceiverInitialColumnFactor.Name = "ucrReceiverInitialColumnFactor"
+        Me.ucrReceiverInitialColumnFactor.Selector = Nothing
+        Me.ucrReceiverInitialColumnFactor.strNcFilePath = ""
+        Me.ucrReceiverInitialColumnFactor.ucrSelector = Nothing
+        '
+        'ucrReceiverInitialRowFactor
+        '
+        Me.ucrReceiverInitialRowFactor.frmParent = Me
+        resources.ApplyResources(Me.ucrReceiverInitialRowFactor, "ucrReceiverInitialRowFactor")
+        Me.ucrReceiverInitialRowFactor.Name = "ucrReceiverInitialRowFactor"
+        Me.ucrReceiverInitialRowFactor.Selector = Nothing
+        Me.ucrReceiverInitialRowFactor.strNcFilePath = ""
+        Me.ucrReceiverInitialRowFactor.ucrSelector = Nothing
+        '
+        'ucrBase
+        '
+        resources.ApplyResources(Me.ucrBase, "ucrBase")
+        Me.ucrBase.Name = "ucrBase"
+        '
+        'ucrSelectorPivot
+        '
+        Me.ucrSelectorPivot.bDropUnusedFilterLevels = False
+        Me.ucrSelectorPivot.bShowHiddenColumns = False
+        Me.ucrSelectorPivot.bUseCurrentFilter = True
+        resources.ApplyResources(Me.ucrSelectorPivot, "ucrSelectorPivot")
+        Me.ucrSelectorPivot.Name = "ucrSelectorPivot"
+        '
+        'ucrChkIncludeSubTotals
+        '
+        Me.ucrChkIncludeSubTotals.Checked = False
+        resources.ApplyResources(Me.ucrChkIncludeSubTotals, "ucrChkIncludeSubTotals")
+        Me.ucrChkIncludeSubTotals.Name = "ucrChkIncludeSubTotals"
         '
         'dlgPivotTable
         '
         resources.ApplyResources(Me, "$this")
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.ucrChkIncludeSubTotals)
+        Me.Controls.Add(Me.ucrReceiverSelectedVariable)
+        Me.Controls.Add(Me.ucrChkSelectedVariable)
+        Me.Controls.Add(Me.UcrInputComboBox2)
+        Me.Controls.Add(Me.UcrInputComboBox1)
+        Me.Controls.Add(Me.ucrInputSummaries)
+        Me.Controls.Add(Me.lblAdditionalRowFactor)
+        Me.Controls.Add(Me.ucrReceiveradditionalRowFactor)
+        Me.Controls.Add(Me.lblInitialColumnFactor)
+        Me.Controls.Add(Me.ucrReceiverInitialColumnFactor)
+        Me.Controls.Add(Me.lblInitialRowFactor)
+        Me.Controls.Add(Me.ucrReceiverInitialRowFactor)
+        Me.Controls.Add(Me.ucrBase)
+        Me.Controls.Add(Me.ucrSelectorPivot)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "dlgPivotTable"
         Me.ShowIcon = False
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents ucrSelectorPivot As ucrSelectorByDataFrameAddRemove
+    Friend WithEvents ucrBase As ucrButtons
+    Friend WithEvents lblInitialRowFactor As Label
+    Friend WithEvents ucrReceiverInitialRowFactor As ucrReceiverSingle
+    Friend WithEvents lblAdditionalRowFactor As Label
+    Friend WithEvents ucrReceiveradditionalRowFactor As ucrReceiverSingle
+    Friend WithEvents lblInitialColumnFactor As Label
+    Friend WithEvents ucrReceiverInitialColumnFactor As ucrReceiverSingle
+    Friend WithEvents UcrInputComboBox2 As ucrInputComboBox
+    Friend WithEvents UcrInputComboBox1 As ucrInputComboBox
+    Friend WithEvents ucrInputSummaries As ucrInputComboBox
+    Friend WithEvents ucrChkSelectedVariable As ucrCheck
+    Friend WithEvents ttSelectedVariable As ToolTip
+    Friend WithEvents ucrReceiverSelectedVariable As ucrReceiverMultiple
+    Friend WithEvents ucrChkIncludeSubTotals As ucrCheck
 End Class
--- instat/dlgPivotTable.vb
+++ instat/dlgPivotTable.vb
@@ -1,3 +1,90 @@
-﻿Public Class dlgPivotTable
+﻿' R- Instat
+' Copyright (C) 2015-2017
+'
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+'
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License 
+' along with this program.  If not, see <http://www.gnu.org/licenses/>.
+Imports instat.Translations
+Public Class dlgPivotTable
+    Private bFirstLoad As Boolean = True
+    Private bReset As Boolean = True
+    Private clsRPivotTable As New RFunction
 
+    Private Sub dlgBoxPlot_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If bFirstLoad Then
+            InitialiseDialog()
+            bFirstLoad = False
+        End If
+
+        If bReset Then
+            SetDefaults()
+        End If
+        SetRCodeForControls(bReset)
+        bReset = False
+        autoTranslate(Me)
+        TestOkEnabled()
+    End Sub
+
+    Private Sub InitialiseDialog()
+        ucrBase.clsRsyntax.iCallType = 2
+
+        ucrSelectorPivot.SetParameter(New RParameter("data", iNewPosition:=0))
+        ucrSelectorPivot.SetParameterIsrfunction()
+
+        ucrReceiverInitialRowFactor.SetParameter(New RParameter("rows", iNewPosition:=1))
+        ucrReceiverInitialRowFactor.SetParameterIsString()
+
+        ucrReceiverInitialColumnFactor.SetParameter(New RParameter("cols", iNewPosition:=2))
+        ucrReceiverInitialColumnFactor.SetParameterIsString()
+
+        ucrChkSelectedVariable.SetText("Selected Variable")
+        ucrChkSelectedVariable.AddToLinkedControls(ucrReceiverSelectedVariable, {"TRUE"}, bNewLinkedHideIfParameterMissing:=True)
+        ttSelectedVariable.SetToolTip(ucrChkSelectedVariable.chkCheck, "Include the dataframe if unchecked")
+
+        ucrChkIncludeSubTotals.SetText("Include Sub Totals")
+        ucrChkIncludeSubTotals.SetParameter(New RParameter("subtotals", iNewPosition:=3))
+        ucrChkIncludeSubTotals.SetValuesCheckedAndUnchecked("TRUE", "FALSE")
+        ucrChkIncludeSubTotals.SetRDefault("FALSE")
+    End Sub
+
+
+    Private Sub SetDefaults()
+        clsRPivotTable = New RFunction
+
+        ucrReceiverInitialRowFactor.SetMeAsReceiver()
+
+        clsRPivotTable.SetRCommand("rpivotTable")
+
+    End Sub
+
+    Private Sub SetRCodeForControls(bReset As Boolean)
+        ucrSelectorPivot.SetRCode(clsRPivotTable, bReset)
+        ucrReceiverInitialColumnFactor.SetRCode(clsRPivotTable, bReset)
+        ucrReceiverInitialRowFactor.SetRCode(clsRPivotTable, bReset)
+    End Sub
+
+    Private Sub TestOkEnabled()
+
+    End Sub
+
+    Private Sub ucrBase_ClickReset(sender As Object, e As EventArgs) Handles ucrBase.ClickReset
+        SetDefaults()
+        SetRCodeForControls(True)
+        TestOkEnabled()
+    End Sub
+
+    Private Sub ucrChkSelectedVariable_ControlValueChanged(ucrChangedControl As ucrCore) Handles ucrChkSelectedVariable.ControlValueChanged
+        If ucrChkSelectedVariable.Checked Then
+            ucrReceiverSelectedVariable.SetMeAsReceiver()
+        End If
+    End Sub
 End Class
