959a19ebff8f4fa938f4c5b6d9a84900c4ff9a6e
Barbra2020 <bmootian@gmail.com>
2021-09-07 18:25:06 +0300

    Adding the pivot_longer function

--- instat/dlgStack.Designer.vb
+++ instat/dlgStack.Designer.vb
@@ -38,25 +38,37 @@ Partial Class dlgStack
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(dlgStack))
         Me.lblColumnsTostack = New System.Windows.Forms.Label()
         Me.lblStackDataInto = New System.Windows.Forms.Label()
-        Me.lblFactorInto = New System.Windows.Forms.Label()
-        Me.lblSets = New System.Windows.Forms.Label()
-        Me.rdoGather = New System.Windows.Forms.RadioButton()
+        Me.lblNamesTo = New System.Windows.Forms.Label()
+        Me.rdoPivotLonger = New System.Windows.Forms.RadioButton()
         Me.rdoUnnest = New System.Windows.Forms.RadioButton()
         Me.ucrPnlStack = New instat.UcrPanel()
-        Me.ucrNudNoSets = New instat.ucrNud()
-        Me.ucrChkStackMultipleSets = New instat.ucrCheck()
         Me.ucrSaveNewDataName = New instat.ucrSave()
         Me.ucrChkCarryColumns = New instat.ucrCheck()
         Me.ucrStackDataInto = New instat.ucrInputTextBox()
-        Me.ucrFactorInto = New instat.ucrInputTextBox()
+        Me.ucrInputNamesTo = New instat.ucrInputTextBox()
         Me.ucrColumnsToCarryReceiver = New instat.ucrReceiverMultiple()
         Me.ucrSelectorStack = New instat.ucrSelectorByDataFrameAddRemove()
         Me.ucrBase = New instat.ucrButtons()
         Me.ucrReceiverColumnsToBeStack = New instat.ucrReceiverMultiple()
         Me.ucrReceiverTextColumn = New instat.ucrReceiverSingle()
+        Me.lblToken = New System.Windows.Forms.Label()
+        Me.ucrInputToken = New instat.ucrInputComboBox()
+        Me.lblFormat = New System.Windows.Forms.Label()
+        Me.ucrInputFormat = New instat.ucrInputComboBox()
+        Me.ucrChkToLowerCase = New instat.ucrCheck()
+        Me.lblPattern = New System.Windows.Forms.Label()
+        Me.ucrInputPattern = New instat.ucrInputTextBox()
+        Me.ttPattern = New System.Windows.Forms.ToolTip(Me.components)
+        Me.ucrChkPunctuation = New instat.ucrCheck()
+        Me.ucrChkNonAlphanum = New instat.ucrCheck()
+        Me.ucrChkNumeric = New instat.ucrCheck()
+        Me.ucrChkUrl = New instat.ucrCheck()
+        Me.ucrInputOutput = New instat.ucrInputTextBox()
+        Me.lblOutput = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'lblColumnsTostack
@@ -71,25 +83,20 @@ Partial Class dlgStack
         Me.lblStackDataInto.Name = "lblStackDataInto"
         Me.lblStackDataInto.Tag = "Stack_Data_Into"
         '
-        'lblFactorInto
+        'lblNamesTo
         '
-        resources.ApplyResources(Me.lblFactorInto, "lblFactorInto")
-        Me.lblFactorInto.Name = "lblFactorInto"
-        Me.lblFactorInto.Tag = "Factor_Into"
+        resources.ApplyResources(Me.lblNamesTo, "lblNamesTo")
+        Me.lblNamesTo.Name = "lblNamesTo"
+        Me.lblNamesTo.Tag = ""
         '
-        'lblSets
+        'rdoPivotLonger
         '
-        resources.ApplyResources(Me.lblSets, "lblSets")
-        Me.lblSets.Name = "lblSets"
-        '
-        'rdoGather
-        '
-        resources.ApplyResources(Me.rdoGather, "rdoGather")
-        Me.rdoGather.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaption
-        Me.rdoGather.FlatAppearance.BorderSize = 2
-        Me.rdoGather.FlatAppearance.CheckedBackColor = System.Drawing.SystemColors.ActiveCaption
-        Me.rdoGather.Name = "rdoGather"
-        Me.rdoGather.UseVisualStyleBackColor = True
+        resources.ApplyResources(Me.rdoPivotLonger, "rdoPivotLonger")
+        Me.rdoPivotLonger.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoPivotLonger.FlatAppearance.BorderSize = 2
+        Me.rdoPivotLonger.FlatAppearance.CheckedBackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoPivotLonger.Name = "rdoPivotLonger"
+        Me.rdoPivotLonger.UseVisualStyleBackColor = True
         '
         'rdoUnnest
         '
@@ -104,22 +111,6 @@ Partial Class dlgStack
         '
         resources.ApplyResources(Me.ucrPnlStack, "ucrPnlStack")
         Me.ucrPnlStack.Name = "ucrPnlStack"
-        '
-        'ucrNudNoSets
-        '
-        Me.ucrNudNoSets.DecimalPlaces = New Decimal(New Integer() {0, 0, 0, 0})
-        Me.ucrNudNoSets.Increment = New Decimal(New Integer() {1, 0, 0, 0})
-        resources.ApplyResources(Me.ucrNudNoSets, "ucrNudNoSets")
-        Me.ucrNudNoSets.Maximum = New Decimal(New Integer() {100, 0, 0, 0})
-        Me.ucrNudNoSets.Minimum = New Decimal(New Integer() {0, 0, 0, 0})
-        Me.ucrNudNoSets.Name = "ucrNudNoSets"
-        Me.ucrNudNoSets.Value = New Decimal(New Integer() {0, 0, 0, 0})
-        '
-        'ucrChkStackMultipleSets
-        '
-        Me.ucrChkStackMultipleSets.Checked = False
-        resources.ApplyResources(Me.ucrChkStackMultipleSets, "ucrChkStackMultipleSets")
-        Me.ucrChkStackMultipleSets.Name = "ucrChkStackMultipleSets"
         '
         'ucrSaveNewDataName
         '
@@ -140,13 +131,13 @@ Partial Class dlgStack
         resources.ApplyResources(Me.ucrStackDataInto, "ucrStackDataInto")
         Me.ucrStackDataInto.Name = "ucrStackDataInto"
         '
-        'ucrFactorInto
+        'ucrInputNamesTo
         '
-        Me.ucrFactorInto.AddQuotesIfUnrecognised = True
-        Me.ucrFactorInto.IsMultiline = False
-        Me.ucrFactorInto.IsReadOnly = False
-        resources.ApplyResources(Me.ucrFactorInto, "ucrFactorInto")
-        Me.ucrFactorInto.Name = "ucrFactorInto"
+        Me.ucrInputNamesTo.AddQuotesIfUnrecognised = True
+        Me.ucrInputNamesTo.IsMultiline = False
+        Me.ucrInputNamesTo.IsReadOnly = False
+        resources.ApplyResources(Me.ucrInputNamesTo, "ucrInputNamesTo")
+        Me.ucrInputNamesTo.Name = "ucrInputNamesTo"
         '
         'ucrColumnsToCarryReceiver
         '
@@ -188,28 +179,120 @@ Partial Class dlgStack
         Me.ucrReceiverTextColumn.strNcFilePath = ""
         Me.ucrReceiverTextColumn.ucrSelector = Nothing
         '
+        'lblToken
+        '
+        resources.ApplyResources(Me.lblToken, "lblToken")
+        Me.lblToken.Name = "lblToken"
+        '
+        'ucrInputToken
+        '
+        Me.ucrInputToken.AddQuotesIfUnrecognised = True
+        Me.ucrInputToken.GetSetSelectedIndex = -1
+        Me.ucrInputToken.IsReadOnly = False
+        resources.ApplyResources(Me.ucrInputToken, "ucrInputToken")
+        Me.ucrInputToken.Name = "ucrInputToken"
+        '
+        'lblFormat
+        '
+        resources.ApplyResources(Me.lblFormat, "lblFormat")
+        Me.lblFormat.Name = "lblFormat"
+        '
+        'ucrInputFormat
+        '
+        Me.ucrInputFormat.AddQuotesIfUnrecognised = True
+        Me.ucrInputFormat.GetSetSelectedIndex = -1
+        Me.ucrInputFormat.IsReadOnly = False
+        resources.ApplyResources(Me.ucrInputFormat, "ucrInputFormat")
+        Me.ucrInputFormat.Name = "ucrInputFormat"
+        '
+        'ucrChkToLowerCase
+        '
+        Me.ucrChkToLowerCase.Checked = False
+        resources.ApplyResources(Me.ucrChkToLowerCase, "ucrChkToLowerCase")
+        Me.ucrChkToLowerCase.Name = "ucrChkToLowerCase"
+        '
+        'lblPattern
+        '
+        resources.ApplyResources(Me.lblPattern, "lblPattern")
+        Me.lblPattern.Name = "lblPattern"
+        '
+        'ucrInputPattern
+        '
+        Me.ucrInputPattern.AddQuotesIfUnrecognised = True
+        Me.ucrInputPattern.IsMultiline = False
+        Me.ucrInputPattern.IsReadOnly = False
+        resources.ApplyResources(Me.ucrInputPattern, "ucrInputPattern")
+        Me.ucrInputPattern.Name = "ucrInputPattern"
+        '
+        'ucrChkPunctuation
+        '
+        Me.ucrChkPunctuation.Checked = False
+        resources.ApplyResources(Me.ucrChkPunctuation, "ucrChkPunctuation")
+        Me.ucrChkPunctuation.Name = "ucrChkPunctuation"
+        '
+        'ucrChkNonAlphanum
+        '
+        Me.ucrChkNonAlphanum.Checked = False
+        resources.ApplyResources(Me.ucrChkNonAlphanum, "ucrChkNonAlphanum")
+        Me.ucrChkNonAlphanum.Name = "ucrChkNonAlphanum"
+        '
+        'ucrChkNumeric
+        '
+        Me.ucrChkNumeric.Checked = False
+        resources.ApplyResources(Me.ucrChkNumeric, "ucrChkNumeric")
+        Me.ucrChkNumeric.Name = "ucrChkNumeric"
+        '
+        'ucrChkUrl
+        '
+        Me.ucrChkUrl.Checked = False
+        resources.ApplyResources(Me.ucrChkUrl, "ucrChkUrl")
+        Me.ucrChkUrl.Name = "ucrChkUrl"
+        '
+        'ucrInputOutput
+        '
+        Me.ucrInputOutput.AddQuotesIfUnrecognised = True
+        Me.ucrInputOutput.IsMultiline = False
+        Me.ucrInputOutput.IsReadOnly = False
+        resources.ApplyResources(Me.ucrInputOutput, "ucrInputOutput")
+        Me.ucrInputOutput.Name = "ucrInputOutput"
+        '
+        'lblOutput
+        '
+        resources.ApplyResources(Me.lblOutput, "lblOutput")
+        Me.lblOutput.Name = "lblOutput"
+        '
         'dlgStack
         '
         resources.ApplyResources(Me, "$this")
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.ucrInputOutput)
+        Me.Controls.Add(Me.ucrChkUrl)
         Me.Controls.Add(Me.ucrReceiverColumnsToBeStack)
         Me.Controls.Add(Me.ucrReceiverTextColumn)
         Me.Controls.Add(Me.rdoUnnest)
-        Me.Controls.Add(Me.rdoGather)
+        Me.Controls.Add(Me.rdoPivotLonger)
         Me.Controls.Add(Me.ucrPnlStack)
-        Me.Controls.Add(Me.lblSets)
-        Me.Controls.Add(Me.ucrNudNoSets)
-        Me.Controls.Add(Me.ucrChkStackMultipleSets)
         Me.Controls.Add(Me.ucrSaveNewDataName)
         Me.Controls.Add(Me.ucrChkCarryColumns)
         Me.Controls.Add(Me.ucrStackDataInto)
-        Me.Controls.Add(Me.ucrFactorInto)
+        Me.Controls.Add(Me.ucrInputNamesTo)
         Me.Controls.Add(Me.ucrColumnsToCarryReceiver)
         Me.Controls.Add(Me.ucrSelectorStack)
         Me.Controls.Add(Me.ucrBase)
-        Me.Controls.Add(Me.lblFactorInto)
+        Me.Controls.Add(Me.lblNamesTo)
         Me.Controls.Add(Me.lblStackDataInto)
         Me.Controls.Add(Me.lblColumnsTostack)
+        Me.Controls.Add(Me.ucrInputToken)
+        Me.Controls.Add(Me.lblToken)
+        Me.Controls.Add(Me.ucrInputFormat)
+        Me.Controls.Add(Me.lblFormat)
+        Me.Controls.Add(Me.ucrChkToLowerCase)
+        Me.Controls.Add(Me.lblPattern)
+        Me.Controls.Add(Me.ucrInputPattern)
+        Me.Controls.Add(Me.ucrChkPunctuation)
+        Me.Controls.Add(Me.ucrChkNonAlphanum)
+        Me.Controls.Add(Me.ucrChkNumeric)
+        Me.Controls.Add(Me.lblOutput)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.MaximizeBox = False
         Me.MinimizeBox = False
@@ -222,19 +305,30 @@ Partial Class dlgStack
     Friend WithEvents ucrReceiverColumnsToBeStack As ucrReceiverMultiple
     Friend WithEvents lblColumnsTostack As Label
     Friend WithEvents lblStackDataInto As Label
-    Friend WithEvents lblFactorInto As Label
+    Friend WithEvents lblNamesTo As Label
     Friend WithEvents ucrBase As ucrButtons
     Friend WithEvents ucrSelectorStack As ucrSelectorByDataFrameAddRemove
     Friend WithEvents ucrColumnsToCarryReceiver As ucrReceiverMultiple
-    Friend WithEvents ucrFactorInto As ucrInputTextBox
+    Friend WithEvents ucrInputNamesTo As ucrInputTextBox
     Friend WithEvents ucrStackDataInto As ucrInputTextBox
     Friend WithEvents ucrChkCarryColumns As ucrCheck
     Friend WithEvents ucrSaveNewDataName As ucrSave
-    Friend WithEvents ucrNudNoSets As ucrNud
-    Friend WithEvents ucrChkStackMultipleSets As ucrCheck
-    Friend WithEvents lblSets As Label
     Friend WithEvents ucrPnlStack As UcrPanel
     Friend WithEvents rdoUnnest As RadioButton
-    Friend WithEvents rdoGather As RadioButton
+    Friend WithEvents rdoPivotLonger As RadioButton
     Friend WithEvents ucrReceiverTextColumn As ucrReceiverSingle
+    Friend WithEvents ucrInputToken As ucrInputComboBox
+    Friend WithEvents lblToken As Label
+    Friend WithEvents ucrInputFormat As ucrInputComboBox
+    Friend WithEvents lblFormat As Label
+    Friend WithEvents ucrChkToLowerCase As ucrCheck
+    Friend WithEvents lblPattern As Label
+    Friend WithEvents ucrInputPattern As ucrInputTextBox
+    Friend WithEvents ttPattern As ToolTip
+    Friend WithEvents ucrChkUrl As ucrCheck
+    Friend WithEvents ucrChkPunctuation As ucrCheck
+    Friend WithEvents ucrChkNonAlphanum As ucrCheck
+    Friend WithEvents ucrChkNumeric As ucrCheck
+    Friend WithEvents ucrInputOutput As ucrInputTextBox
+    Friend WithEvents lblOutput As Label
 End Class
