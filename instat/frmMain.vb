f5516ab43d8a5538438ad3be158baf9770f72cbe
VICTOR\AMI-ADMIN <mmulahvictor@gmail.com>
2021-09-24 09:21:39 +0300

    Adding Column Selection dialog

--- instat/frmMain.vb
+++ instat/frmMain.vb
@@ -2415,4 +2415,8 @@ Public Class frmMain
     Private Sub mnuEditScript_Click(sender As Object, e As EventArgs) Handles mnuEditScript.Click
         dlgScript.ShowDialog()
     End Sub
+
+    Private Sub ColumnSelectionToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ColumnSelectionToolStripMenuItem.Click
+        dlgSelect.ShowDialog()
+    End Sub
 End Class
