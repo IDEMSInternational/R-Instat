02e52a94196ba93ffa4710117d31eb1c56f05386
Antoine Ntalumeso <antoine.ntalumeso@aims.ac.rw>
2021-09-27 15:40:51 +0200

    Improved the recode factor with forcats functions

--- instat/dlgRecodeFactor.Designer.vb
+++ instat/dlgRecodeFactor.Designer.vb
@@ -39,32 +39,121 @@ Partial Class dlgRecodeFactor
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(dlgRecodeFactor))
-        Me.ucrBase = New instat.ucrButtons()
-        Me.ucrReceiverFactor = New instat.ucrReceiverSingle()
         Me.lblFactor = New System.Windows.Forms.Label()
+        Me.grpSelectedValues = New System.Windows.Forms.GroupBox()
+        Me.rdoKeep = New System.Windows.Forms.RadioButton()
+        Me.rdoDrop = New System.Windows.Forms.RadioButton()
+        Me.lblOther = New System.Windows.Forms.Label()
+        Me.rdoRecode = New System.Windows.Forms.RadioButton()
+        Me.rdoOther = New System.Windows.Forms.RadioButton()
+        Me.rdoLump = New System.Windows.Forms.RadioButton()
+        Me.ucrPnlOptions = New instat.UcrPanel()
+        Me.ucrInputOther = New instat.ucrInputTextBox()
+        Me.ucrPnlMethods = New instat.UcrPanel()
         Me.ucrSaveNewColumn = New instat.ucrSave()
         Me.ucrFactorGrid = New instat.ucrFactor()
+        Me.ucrReceiverFactor = New instat.ucrReceiverSingle()
         Me.ucrSelectorForRecode = New instat.ucrSelectorByDataFrameAddRemove()
+        Me.ucrBase = New instat.ucrButtons()
+        Me.grpKeep = New System.Windows.Forms.GroupBox()
+        Me.ucrPnlKeep = New instat.UcrPanel()
+        Me.rdoLevels = New System.Windows.Forms.RadioButton()
+        Me.rdoCommonValues = New System.Windows.Forms.RadioButton()
+        Me.rdoFrequentValues = New System.Windows.Forms.RadioButton()
+        Me.rdoMore = New System.Windows.Forms.RadioButton()
+        Me.ucrNudLevels = New instat.ucrNud()
+        Me.ucrNudCommonValues = New instat.ucrNud()
+        Me.ucrNudFrequentValues = New instat.ucrNud()
+        Me.grpSelectedValues.SuspendLayout()
+        Me.grpKeep.SuspendLayout()
         Me.SuspendLayout()
-        '
-        'ucrBase
-        '
-        resources.ApplyResources(Me.ucrBase, "ucrBase")
-        Me.ucrBase.Name = "ucrBase"
-        '
-        'ucrReceiverFactor
-        '
-        Me.ucrReceiverFactor.frmParent = Me
-        resources.ApplyResources(Me.ucrReceiverFactor, "ucrReceiverFactor")
-        Me.ucrReceiverFactor.Name = "ucrReceiverFactor"
-        Me.ucrReceiverFactor.Selector = Nothing
-        Me.ucrReceiverFactor.strNcFilePath = ""
-        Me.ucrReceiverFactor.ucrSelector = Nothing
         '
         'lblFactor
         '
         resources.ApplyResources(Me.lblFactor, "lblFactor")
         Me.lblFactor.Name = "lblFactor"
+        '
+        'grpSelectedValues
+        '
+        Me.grpSelectedValues.Controls.Add(Me.rdoKeep)
+        Me.grpSelectedValues.Controls.Add(Me.rdoDrop)
+        Me.grpSelectedValues.Controls.Add(Me.ucrPnlMethods)
+        resources.ApplyResources(Me.grpSelectedValues, "grpSelectedValues")
+        Me.grpSelectedValues.Name = "grpSelectedValues"
+        Me.grpSelectedValues.TabStop = False
+        '
+        'rdoKeep
+        '
+        resources.ApplyResources(Me.rdoKeep, "rdoKeep")
+        Me.rdoKeep.Name = "rdoKeep"
+        Me.rdoKeep.TabStop = True
+        Me.rdoKeep.UseVisualStyleBackColor = True
+        '
+        'rdoDrop
+        '
+        resources.ApplyResources(Me.rdoDrop, "rdoDrop")
+        Me.rdoDrop.Name = "rdoDrop"
+        Me.rdoDrop.TabStop = True
+        Me.rdoDrop.UseVisualStyleBackColor = True
+        '
+        'lblOther
+        '
+        resources.ApplyResources(Me.lblOther, "lblOther")
+        Me.lblOther.Name = "lblOther"
+        '
+        'rdoRecode
+        '
+        resources.ApplyResources(Me.rdoRecode, "rdoRecode")
+        Me.rdoRecode.BackColor = System.Drawing.SystemColors.Control
+        Me.rdoRecode.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoRecode.FlatAppearance.BorderSize = 2
+        Me.rdoRecode.FlatAppearance.CheckedBackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoRecode.Name = "rdoRecode"
+        Me.rdoRecode.TabStop = True
+        Me.rdoRecode.Tag = "Recode"
+        Me.rdoRecode.UseVisualStyleBackColor = False
+        '
+        'rdoOther
+        '
+        resources.ApplyResources(Me.rdoOther, "rdoOther")
+        Me.rdoOther.BackColor = System.Drawing.SystemColors.Control
+        Me.rdoOther.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoOther.FlatAppearance.BorderSize = 2
+        Me.rdoOther.FlatAppearance.CheckedBackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoOther.Name = "rdoOther"
+        Me.rdoOther.TabStop = True
+        Me.rdoOther.Tag = "Other"
+        Me.rdoOther.UseVisualStyleBackColor = False
+        '
+        'rdoLump
+        '
+        resources.ApplyResources(Me.rdoLump, "rdoLump")
+        Me.rdoLump.BackColor = System.Drawing.SystemColors.Control
+        Me.rdoLump.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoLump.FlatAppearance.BorderSize = 2
+        Me.rdoLump.FlatAppearance.CheckedBackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoLump.Name = "rdoLump"
+        Me.rdoLump.TabStop = True
+        Me.rdoLump.Tag = "Lump"
+        Me.rdoLump.UseVisualStyleBackColor = False
+        '
+        'ucrPnlOptions
+        '
+        resources.ApplyResources(Me.ucrPnlOptions, "ucrPnlOptions")
+        Me.ucrPnlOptions.Name = "ucrPnlOptions"
+        '
+        'ucrInputOther
+        '
+        Me.ucrInputOther.AddQuotesIfUnrecognised = True
+        Me.ucrInputOther.IsMultiline = False
+        Me.ucrInputOther.IsReadOnly = False
+        resources.ApplyResources(Me.ucrInputOther, "ucrInputOther")
+        Me.ucrInputOther.Name = "ucrInputOther"
+        '
+        'ucrPnlMethods
+        '
+        resources.ApplyResources(Me.ucrPnlMethods, "ucrPnlMethods")
+        Me.ucrPnlMethods.Name = "ucrPnlMethods"
         '
         'ucrSaveNewColumn
         '
@@ -80,6 +169,15 @@ Partial Class dlgRecodeFactor
         Me.ucrFactorGrid.shtCurrSheet = Nothing
         Me.ucrFactorGrid.ucrChkLevels = Nothing
         '
+        'ucrReceiverFactor
+        '
+        Me.ucrReceiverFactor.frmParent = Me
+        resources.ApplyResources(Me.ucrReceiverFactor, "ucrReceiverFactor")
+        Me.ucrReceiverFactor.Name = "ucrReceiverFactor"
+        Me.ucrReceiverFactor.Selector = Nothing
+        Me.ucrReceiverFactor.strNcFilePath = ""
+        Me.ucrReceiverFactor.ucrSelector = Nothing
+        '
         'ucrSelectorForRecode
         '
         Me.ucrSelectorForRecode.bDropUnusedFilterLevels = False
@@ -88,10 +186,100 @@ Partial Class dlgRecodeFactor
         resources.ApplyResources(Me.ucrSelectorForRecode, "ucrSelectorForRecode")
         Me.ucrSelectorForRecode.Name = "ucrSelectorForRecode"
         '
+        'ucrBase
+        '
+        resources.ApplyResources(Me.ucrBase, "ucrBase")
+        Me.ucrBase.Name = "ucrBase"
+        '
+        'grpKeep
+        '
+        Me.grpKeep.Controls.Add(Me.ucrNudFrequentValues)
+        Me.grpKeep.Controls.Add(Me.ucrNudCommonValues)
+        Me.grpKeep.Controls.Add(Me.ucrNudLevels)
+        Me.grpKeep.Controls.Add(Me.rdoMore)
+        Me.grpKeep.Controls.Add(Me.rdoFrequentValues)
+        Me.grpKeep.Controls.Add(Me.rdoCommonValues)
+        Me.grpKeep.Controls.Add(Me.rdoLevels)
+        Me.grpKeep.Controls.Add(Me.ucrPnlKeep)
+        resources.ApplyResources(Me.grpKeep, "grpKeep")
+        Me.grpKeep.Name = "grpKeep"
+        Me.grpKeep.TabStop = False
+        '
+        'ucrPnlKeep
+        '
+        resources.ApplyResources(Me.ucrPnlKeep, "ucrPnlKeep")
+        Me.ucrPnlKeep.Name = "ucrPnlKeep"
+        '
+        'rdoLevels
+        '
+        resources.ApplyResources(Me.rdoLevels, "rdoLevels")
+        Me.rdoLevels.Name = "rdoLevels"
+        Me.rdoLevels.TabStop = True
+        Me.rdoLevels.UseVisualStyleBackColor = True
+        '
+        'rdoCommonValues
+        '
+        resources.ApplyResources(Me.rdoCommonValues, "rdoCommonValues")
+        Me.rdoCommonValues.Name = "rdoCommonValues"
+        Me.rdoCommonValues.TabStop = True
+        Me.rdoCommonValues.UseVisualStyleBackColor = True
+        '
+        'rdoFrequentValues
+        '
+        resources.ApplyResources(Me.rdoFrequentValues, "rdoFrequentValues")
+        Me.rdoFrequentValues.Name = "rdoFrequentValues"
+        Me.rdoFrequentValues.TabStop = True
+        Me.rdoFrequentValues.UseVisualStyleBackColor = True
+        '
+        'rdoMore
+        '
+        resources.ApplyResources(Me.rdoMore, "rdoMore")
+        Me.rdoMore.Name = "rdoMore"
+        Me.rdoMore.TabStop = True
+        Me.rdoMore.UseVisualStyleBackColor = True
+        '
+        'ucrNudLevels
+        '
+        Me.ucrNudLevels.DecimalPlaces = New Decimal(New Integer() {0, 0, 0, 0})
+        Me.ucrNudLevels.Increment = New Decimal(New Integer() {1, 0, 0, 0})
+        resources.ApplyResources(Me.ucrNudLevels, "ucrNudLevels")
+        Me.ucrNudLevels.Maximum = New Decimal(New Integer() {100, 0, 0, 0})
+        Me.ucrNudLevels.Minimum = New Decimal(New Integer() {0, 0, 0, 0})
+        Me.ucrNudLevels.Name = "ucrNudLevels"
+        Me.ucrNudLevels.Value = New Decimal(New Integer() {0, 0, 0, 0})
+        '
+        'ucrNudCommonValues
+        '
+        Me.ucrNudCommonValues.DecimalPlaces = New Decimal(New Integer() {0, 0, 0, 0})
+        Me.ucrNudCommonValues.Increment = New Decimal(New Integer() {1, 0, 0, 0})
+        resources.ApplyResources(Me.ucrNudCommonValues, "ucrNudCommonValues")
+        Me.ucrNudCommonValues.Maximum = New Decimal(New Integer() {100, 0, 0, 0})
+        Me.ucrNudCommonValues.Minimum = New Decimal(New Integer() {0, 0, 0, 0})
+        Me.ucrNudCommonValues.Name = "ucrNudCommonValues"
+        Me.ucrNudCommonValues.Value = New Decimal(New Integer() {0, 0, 0, 0})
+        '
+        'ucrNudFrequentValues
+        '
+        Me.ucrNudFrequentValues.DecimalPlaces = New Decimal(New Integer() {0, 0, 0, 0})
+        Me.ucrNudFrequentValues.Increment = New Decimal(New Integer() {1, 0, 0, 0})
+        resources.ApplyResources(Me.ucrNudFrequentValues, "ucrNudFrequentValues")
+        Me.ucrNudFrequentValues.Maximum = New Decimal(New Integer() {100, 0, 0, 0})
+        Me.ucrNudFrequentValues.Minimum = New Decimal(New Integer() {0, 0, 0, 0})
+        Me.ucrNudFrequentValues.Name = "ucrNudFrequentValues"
+        Me.ucrNudFrequentValues.Value = New Decimal(New Integer() {0, 0, 0, 0})
+        '
         'dlgRecodeFactor
         '
         resources.ApplyResources(Me, "$this")
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.grpKeep)
+        Me.Controls.Add(Me.rdoLump)
+        Me.Controls.Add(Me.rdoRecode)
+        Me.Controls.Add(Me.rdoOther)
+        Me.Controls.Add(Me.ucrPnlOptions)
+        Me.Controls.Add(Me.lblOther)
+        Me.Controls.Add(Me.ucrInputOther)
+        Me.Controls.Add(Me.grpSelectedValues)
         Me.Controls.Add(Me.ucrSaveNewColumn)
         Me.Controls.Add(Me.ucrFactorGrid)
         Me.Controls.Add(Me.lblFactor)
@@ -102,6 +290,10 @@ Partial Class dlgRecodeFactor
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "dlgRecodeFactor"
+        Me.grpSelectedValues.ResumeLayout(False)
+        Me.grpSelectedValues.PerformLayout()
+        Me.grpKeep.ResumeLayout(False)
+        Me.grpKeep.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -113,4 +305,23 @@ Partial Class dlgRecodeFactor
     Friend WithEvents lblFactor As Label
     Friend WithEvents ucrFactorGrid As ucrFactor
     Friend WithEvents ucrSaveNewColumn As ucrSave
+    Friend WithEvents grpSelectedValues As GroupBox
+    Friend WithEvents ucrPnlMethods As UcrPanel
+    Friend WithEvents rdoKeep As RadioButton
+    Friend WithEvents rdoDrop As RadioButton
+    Friend WithEvents lblOther As Label
+    Friend WithEvents ucrInputOther As ucrInputTextBox
+    Friend WithEvents rdoLump As RadioButton
+    Friend WithEvents rdoRecode As RadioButton
+    Friend WithEvents rdoOther As RadioButton
+    Friend WithEvents ucrPnlOptions As UcrPanel
+    Friend WithEvents grpKeep As GroupBox
+    Friend WithEvents rdoMore As RadioButton
+    Friend WithEvents rdoFrequentValues As RadioButton
+    Friend WithEvents rdoCommonValues As RadioButton
+    Friend WithEvents rdoLevels As RadioButton
+    Friend WithEvents ucrPnlKeep As UcrPanel
+    Friend WithEvents ucrNudFrequentValues As ucrNud
+    Friend WithEvents ucrNudCommonValues As ucrNud
+    Friend WithEvents ucrNudLevels As ucrNud
 End Class
--- instat/dlgRecodeFactor.vb
+++ instat/dlgRecodeFactor.vb
@@ -19,7 +19,9 @@ Imports instat.Translations
 
 Public Class dlgRecodeFactor
     Private bFirstLoad As Boolean = True
-    Private clsReplaceFunction, clsRevalueFunction As RFunction
+    Private clsFctRecodeFunction, clsFctOtherFunction As New RFunction
+    Private clsFctLowFreqFunction, clsFctLumpPropFunction As New RFunction
+    Private clsCFunction As New RFunction
     Private bReset As Boolean = True
     Private Sub dlgRecodeFactor_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         If bFirstLoad Then
@@ -38,7 +40,34 @@ Public Class dlgRecodeFactor
     Private Sub InitialiseDialog()
         ucrBase.iHelpTopicID = 37
 
-        ucrReceiverFactor.SetParameter(New RParameter("x", 0))
+        ucrPnlOptions.AddRadioButton(rdoRecode)
+        ucrPnlOptions.AddRadioButton(rdoOther)
+        ucrPnlOptions.AddRadioButton(rdoLump)
+
+        ucrPnlOptions.AddFunctionNamesCondition(rdoRecode, "fct_recode")
+        ucrPnlOptions.AddFunctionNamesCondition(rdoOther, "fct_other")
+        ucrPnlOptions.AddFunctionNamesCondition(rdoLump, "fct_lump")
+
+        ucrPnlMethods.AddRadioButton(rdoKeep)
+        ucrPnlMethods.AddRadioButton(rdoDrop)
+
+        ucrPnlKeep.AddRadioButton(rdoLevels)
+        ucrPnlKeep.AddRadioButton(rdoCommonValues)
+        ucrPnlKeep.AddRadioButton(rdoFrequentValues)
+        ucrPnlKeep.AddRadioButton(rdoMore)
+
+        ucrNudLevels.SetParameter(New RParameter(""))
+        ucrNudLevels.SetMinMax(0, Integer.MaxValue)
+        ucrNudLevels.Increment = 1
+
+        ucrNudCommonValues.SetParameter(New RParameter(""))
+        ucrNudCommonValues.SetMinMax(Integer.MinValue, Integer.MaxValue)
+
+        ucrNudFrequentValues.SetParameter(New RParameter("prop", 1))
+        ucrNudFrequentValues.SetMinMax(-1, 1)
+        ucrNudFrequentValues.Increment = 0.01
+
+        ucrReceiverFactor.SetParameter(New RParameter(".f", 0))
         ucrReceiverFactor.Selector = ucrSelectorForRecode
         ucrReceiverFactor.SetIncludedDataTypes({"factor"}, bStrict:=True)
         ucrReceiverFactor.strSelectorHeading = "Factors"
@@ -51,34 +80,72 @@ Public Class dlgRecodeFactor
         ucrFactorGrid.bIncludeCopyOfLevels = True
         ucrFactorGrid.AddEditableColumns({"New Label"})
 
+        ucrInputOther.SetText("Other")
+
         ucrSaveNewColumn.SetSaveTypeAsColumn()
         ucrSaveNewColumn.SetDataFrameSelector(ucrSelectorForRecode.ucrAvailableDataFrames)
         ucrSaveNewColumn.SetIsComboBox()
         ucrSaveNewColumn.SetLabelText("New Column Name:")
         ucrSaveNewColumn.setLinkedReceiver(ucrReceiverFactor)
+
+        ucrPnlOptions.AddToLinkedControls({ucrFactorGrid}, {rdoRecode, rdoOther}, bNewLinkedHideIfParameterMissing:=True)
+        ucrPnlOptions.AddToLinkedControls({ucrInputOther}, {rdoOther, rdoLump}, bNewLinkedHideIfParameterMissing:=True)
+        ucrPnlOptions.AddToLinkedControls(ucrPnlMethods, {rdoOther}, bNewLinkedHideIfParameterMissing:=True)
+        ucrPnlOptions.AddToLinkedControls(ucrPnlKeep, {rdoLump}, bNewLinkedHideIfParameterMissing:=True)
+        ucrPnlMethods.SetLinkedDisplayControl(grpSelectedValues)
+        ucrPnlKeep.SetLinkedDisplayControl(grpKeep)
+        ucrInputOther.SetLinkedDisplayControl(lblOther)
+
+        ucrPnlMethods.AddToLinkedControls(ucrNudLevels, {rdoLevels}, bNewLinkedHideIfParameterMissing:=True)
+        ucrPnlMethods.AddToLinkedControls(ucrNudCommonValues, {rdoCommonValues}, bNewLinkedHideIfParameterMissing:=True)
+        ucrPnlMethods.AddToLinkedControls(ucrNudFrequentValues, {rdoFrequentValues}, bNewLinkedHideIfParameterMissing:=True, bNewLinkedChangeParameterValue:=True, objNewDefaultState:=0.1)
+
     End Sub
 
     Private Sub SetDefaults()
-        clsReplaceFunction = New RFunction
-        clsRevalueFunction = New RFunction
+        clsFctRecodeFunction = New RFunction
+        clsFctOtherFunction = New RFunction
+        clsFctLowFreqFunction = New RFunction
+        clsFctLumpPropFunction = New RFunction
+        clsCFunction = New RFunction
 
         ucrSelectorForRecode.Reset()
         ucrSelectorForRecode.Focus()
         ucrFactorGrid.ResetText()
         ucrSaveNewColumn.Reset()
 
-        clsRevalueFunction.SetPackageName("plyr")
-        clsRevalueFunction.SetRCommand("revalue")
-        clsRevalueFunction.AddParameter("replace", clsRFunctionParameter:=clsReplaceFunction)
-        clsRevalueFunction.SetAssignTo(strTemp:=ucrSaveNewColumn.GetText(), strTempDataframe:=ucrSelectorForRecode.ucrAvailableDataFrames.cboAvailableDataFrames.Text, strTempColumn:=ucrSaveNewColumn.GetText())
+        rdoRecode.Checked = True
+        rdoKeep.Checked = True
 
-        clsReplaceFunction.SetRCommand("c")
+        clsCFunction.SetRCommand("c")
 
-        ucrBase.clsRsyntax.SetBaseRFunction(clsRevalueFunction)
+        clsFctLumpPropFunction.SetPackageName("forcats")
+        clsFctLumpPropFunction.SetRCommand("fct_lumn_prop")
+
+        clsFctLowFreqFunction.SetPackageName("forcats")
+        clsFctLowFreqFunction.SetRCommand("fct_lump_lowfreq")
+
+        clsFctOtherFunction.SetPackageName("forcats")
+        clsFctOtherFunction.SetRCommand("fct_other")
+        clsFctOtherFunction.AddParameter("keep", clsRFunctionParameter:=clsCFunction, iPosition:=1)
+
+        clsFctRecodeFunction.SetPackageName("forcats")
+        clsFctRecodeFunction.SetRCommand("fct_recode")
+        clsFctRecodeFunction.SetAssignTo(strTemp:=ucrSaveNewColumn.GetText(), strTempDataframe:=ucrSelectorForRecode.ucrAvailableDataFrames.cboAvailableDataFrames.Text, strTempColumn:=ucrSaveNewColumn.GetText())
+
+        ucrBase.clsRsyntax.SetBaseRFunction(clsFctRecodeFunction)
     End Sub
 
     Private Sub SetRCodeforControls(bReset As Boolean)
-        SetRCode(Me, clsRevalueFunction, bReset)
+        ucrReceiverFactor.AddAdditionalCodeParameterPair(clsFctOtherFunction, New RParameter("f", 0), iAdditionalPairNo:=1)
+        ucrReceiverFactor.AddAdditionalCodeParameterPair(clsFctLumpPropFunction, New RParameter("f", 0), iAdditionalPairNo:=2)
+        ucrReceiverFactor.AddAdditionalCodeParameterPair(clsFctLowFreqFunction, New RParameter("f", 0), iAdditionalPairNo:=3)
+        ucrSaveNewColumn.AddAdditionalRCode(clsFctLumpPropFunction, iAdditionalPairNo:=1)
+        ucrSaveNewColumn.AddAdditionalRCode(clsFctLowFreqFunction, iAdditionalPairNo:=2)
+        ucrSaveNewColumn.AddAdditionalRCode(clsFctOtherFunction, bReset)
+        ucrReceiverFactor.SetRCode(clsFctRecodeFunction, bReset)
+        ucrSaveNewColumn.SetRCode(clsFctRecodeFunction, bReset)
+        ucrNudFrequentValues.SetRCode(clsFctLumpPropFunction, bReset)
     End Sub
 
     Private Sub TestOKEnabled()
@@ -104,22 +171,53 @@ Public Class dlgRecodeFactor
     Private Sub ucrFactorGrid_GridContentChanged() Handles ucrFactorGrid.GridContentChanged
         Dim strCurrentLabels As List(Of String)
         Dim strNewLabels As List(Of String)
-        Dim strReplace As String = ""
 
         strCurrentLabels = ucrFactorGrid.GetColumnAsList(ucrFactorGrid.strLabelsName, False)
         strNewLabels = ucrFactorGrid.GetColumnAsList("New Label", True)
-        clsReplaceFunction.ClearParameters()
 
         If ucrFactorGrid.IsColumnComplete("New Label") AndAlso strCurrentLabels.Count = strNewLabels.Count Then
             For i = 0 To strCurrentLabels.Count - 1
                 ' Backtick needed for names of the vector incase the levels are not valid R names
-                clsReplaceFunction.AddParameter(Chr(96) & strCurrentLabels(i) & Chr(96), strNewLabels(i))
+                clsFctRecodeFunction.AddParameter(Chr(96) & strCurrentLabels(i) & Chr(96), strNewLabels(i))
+                clsCFunction.AddParameter(Chr(96) & strCurrentLabels(i) & Chr(96), strNewLabels(i))
             Next
-            ucrBase.clsRsyntax.AddParameter("replace", clsRFunctionParameter:=clsReplaceFunction)
-        Else
-            ucrBase.clsRsyntax.RemoveParameter("replace")
+            'ucrBase.clsRsyntax.AddParameter("replace", clsRFunctionParameter:=clsReplaceFunction)
+            ' Else
+            '    ucrBase.clsRsyntax.RemoveParameter("replace")
         End If
         TestOKEnabled()
+    End Sub
+
+    Private Sub ucrPnlOptions_ControlValueChanged(ucrChangedControl As ucrCore) Handles ucrPnlOptions.ControlValueChanged, ucrPnlMethods.ControlValueChanged, ucrPnlKeep.ControlValueChanged, ucrInputOther.ControlValueChanged
+        If rdoRecode.Checked Then
+            ucrBase.clsRsyntax.SetBaseRFunction(clsFctRecodeFunction)
+        ElseIf rdoOther.Checked OrElse rdoLump.Checked Then
+            If Not ucrInputOther.bUserTyped OrElse ucrInputOther.GetText = "Other" Then
+                clsFctOtherFunction.AddParameter("other_level", Chr(34) & "Other" & Chr(34), iPosition:=2)
+                clsFctLowFreqFunction.AddParameter("other_level", Chr(34) & "Other" & Chr(34), iPosition:=2)
+                clsFctLumpPropFunction.AddParameter("other_level", Chr(34) & "Other" & Chr(34), iPosition:=2)
+            Else
+                clsFctOtherFunction.AddParameter("other_level", Chr(34) & ucrInputOther.GetText() & Chr(34), iPosition:=2)
+                clsFctLowFreqFunction.AddParameter("other_level", Chr(34) & ucrInputOther.GetText() & Chr(34), iPosition:=2)
+                clsFctLumpPropFunction.AddParameter("other_level", Chr(34) & ucrInputOther.GetText() & Chr(34), iPosition:=2)
+            End If
+            If rdoOther.Checked Then
+                clsFctOtherFunction.RemoveParameterByName("keep")
+                clsFctOtherFunction.RemoveParameterByName("drop")
+                If rdoKeep.Checked Then
+                    clsFctOtherFunction.AddParameter("keep", clsRFunctionParameter:=clsCFunction, iPosition:=1)
+                Else
+                    clsFctOtherFunction.AddParameter("drop", clsRFunctionParameter:=clsCFunction, iPosition:=1)
+                End If
+                ucrBase.clsRsyntax.SetBaseRFunction(clsFctOtherFunction)
+            ElseIf rdoLump.Checked Then
+                If rdoFrequentValues.Checked Then
+                    ucrBase.clsRsyntax.SetBaseRFunction(clsFctLumpPropFunction)
+                ElseIf rdoMore.Checked Then
+                    ucrBase.clsRsyntax.SetBaseRFunction(clsFctLowFreqFunction)
+                End If
+            End If
+        End If
     End Sub
 
     Private Sub ucrReceiverFactor_ControlValueChanged(ucrChangedControl As ucrCore) Handles ucrReceiverFactor.ControlValueChanged
