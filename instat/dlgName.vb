c8ba9db12a170c59f88a309db6a724e4c1867c98
shadrack kibet <shadrackkibet36@outlook.com>
2021-10-01 16:59:27 +0300

    enabled rename with functionality

--- instat/dlgName.vb
+++ instat/dlgName.vb
@@ -24,6 +24,7 @@ Public Class dlgName
     Dim strSelectedColumn As String = ""
     Dim strSelectedDataFrame As String = ""
     Private clsDefaultRFunction As New RFunction
+    Private clsRenameWithFunction As RFunction
 
     Private Sub dlgName_Load(sender As Object, e As EventArgs) Handles Me.Load
         If bFirstLoad Then
@@ -52,7 +53,6 @@ Public Class dlgName
         ucrReceiverName.SetParameter(New RParameter("column_name", 1))
         ucrReceiverName.SetParameterIsString()
         ucrReceiverName.Selector = ucrSelectVariables
-        ucrReceiverName.SetMeAsReceiver()
 
         'New Name Input
         ucrInputNewName.SetParameter(New RParameter("new_val", 2))
@@ -61,30 +61,81 @@ Public Class dlgName
 
         'Label Input
         ucrInputVariableLabel.SetParameter(New RParameter("label", 3))
+
+        ucrPnlOptions.AddRadioButton(rdoSingle)
+        ucrPnlOptions.AddRadioButton(rdoMultiple)
+        ucrPnlOptions.AddFunctionNamesCondition(rdoSingle, frmMain.clsRLink.strInstatDataObject & "$rename_column_in_data")
+        ucrPnlOptions.AddFunctionNamesCondition(rdoMultiple, "rename_with")
+
+        ucrPnlOptions.AddToLinkedControls({ucrReceiverName, ucrInputNewName, ucrInputVariableLabel}, {rdoSingle}, bNewLinkedAddRemoveParameter:=True, bNewLinkedHideIfParameterMissing:=True)
+        ucrPnlOptions.AddToLinkedControls({ucrReceiverColumns, ucrSaveDataFrame}, {rdoMultiple}, bNewLinkedAddRemoveParameter:=True, bNewLinkedHideIfParameterMissing:=True)
+        ucrPnlOptions.AddToLinkedControls(ucrPnlCase, {rdoMultiple}, bNewLinkedHideIfParameterMissing:=True)
+        ucrReceiverName.SetLinkedDisplayControl(lblCurrentName)
+        ucrInputNewName.SetLinkedDisplayControl(lblName)
+        ucrInputVariableLabel.SetLinkedDisplayControl(lblVariableLabel)
+        ucrReceiverColumns.SetLinkedDisplayControl(lblColumns)
+
+        ucrReceiverColumns.SetParameter(New RParameter(".cols", 2))
+        ucrReceiverColumns.Selector = ucrSelectVariables
+        ucrReceiverColumns.SetParameterIsString()
+        ucrReceiverColumns.bWithQuotes = False
+
+        ucrPnlCase.SetParameter(New RParameter(".fn", 1))
+        ucrPnlCase.AddRadioButton(rdoToUpper, "toupper")
+        ucrPnlCase.AddRadioButton(rdoToLower, "tolower")
+        ucrPnlCase.AddRadioButton(rdoToTitle, "stringr::str_to_title")
+        ucrPnlCase.AddRadioButton(rdoToSentence, "stringr::str_to_sentence")
+        ucrPnlCase.AddParameterValuesCondition(rdoToUpper, ".fn", "toupper")
+        ucrPnlCase.AddParameterValuesCondition(rdoToLower, ".fn", "tolower")
+        ucrPnlCase.AddParameterValuesCondition(rdoToTitle, ".fn", "stringr::str_to_title")
+        ucrPnlCase.AddParameterValuesCondition(rdoToSentence, ".fn", "stringr::str_to_sentence")
+
+        ucrSaveDataFrame.SetLabelText("New Data Frame Name:")
+        ucrSaveDataFrame.SetSaveTypeAsDataFrame()
+        ucrSaveDataFrame.SetPrefix("new_data_frame")
+        ucrSaveDataFrame.SetDataFrameSelector(ucrSelectVariables.ucrAvailableDataFrames)
     End Sub
 
     Public Sub SetDefaults()
         clsDefaultRFunction = New RFunction
+        clsRenameWithFunction = New RFunction
 
         ucrSelectVariables.Reset()
-        ucrInputNewName.Reset()
-        ucrInputVariableLabel.Reset()
 
         clsDefaultRFunction.SetRCommand(frmMain.clsRLink.strInstatDataObject & "$rename_column_in_data")
-        ucrBase.clsRsyntax.SetBaseRFunction(clsDefaultRFunction)
 
+        clsRenameWithFunction.SetPackageName("dplyr")
+        clsRenameWithFunction.SetRCommand("rename_with")
+        clsRenameWithFunction.AddParameter(".fn", "toupper", iPosition:=1)
+
+        ucrBase.clsRsyntax.SetBaseRFunction(clsDefaultRFunction)
     End Sub
 
     Private Sub TestOKEnabled()
-        If Not ucrReceiverName.IsEmpty() AndAlso Not ucrInputNewName.IsEmpty() Then
-            ucrBase.OKEnabled(True)
+        If rdoSingle.Checked Then
+            If Not ucrReceiverName.IsEmpty() AndAlso Not ucrInputNewName.IsEmpty() Then
+                ucrBase.OKEnabled(True)
+            Else
+                ucrBase.OKEnabled(False)
+            End If
         Else
-            ucrBase.OKEnabled(False)
+            If ucrSaveDataFrame.IsComplete Then
+                ucrBase.OKEnabled(True)
+            Else
+                ucrBase.OKEnabled(False)
+            End If
         End If
     End Sub
 
     Private Sub SetRCodeForControls(bReset As Boolean)
-        SetRCode(Me, ucrBase.clsRsyntax.clsBaseFunction, bReset)
+        ucrSelectVariables.SetRCode(clsDefaultRFunction, bReset)
+        ucrReceiverName.SetRCode(clsDefaultRFunction, bReset)
+        ucrInputNewName.SetRCode(clsDefaultRFunction, bReset)
+        ucrInputVariableLabel.SetRCode(clsDefaultRFunction, bReset)
+        ucrPnlOptions.SetRCode(ucrBase.clsRsyntax.clsBaseFunction, bReset)
+        ucrReceiverColumns.SetRCode(clsRenameWithFunction, bReset)
+        ucrPnlCase.SetRCode(clsRenameWithFunction, bReset)
+        ucrSaveDataFrame.SetRCode(clsRenameWithFunction, bReset)
     End Sub
 
     Public Sub SetCurrentColumn(strColumn As String, strDataFrame As String)
@@ -102,10 +153,6 @@ Public Class dlgName
     Private Sub ucrBase_ClickReset(sender As Object, e As EventArgs) Handles ucrBase.ClickReset
         SetDefaults()
         SetRCodeForControls(True)
-        TestOKEnabled()
-    End Sub
-
-    Private Sub ucrCoreControls_ControlContentsChanged() Handles ucrInputNewName.ControlContentsChanged, ucrReceiverName.ControlContentsChanged, ucrSelectVariables.ControlContentsChanged
         TestOKEnabled()
     End Sub
 
@@ -143,7 +190,7 @@ Public Class dlgName
                 'the number of labels for a column expected is 1
                 If strArr.Length = 1 Then
                     strColLabel = strArr(0)
-                ElseIf strArr.Length > 1
+                ElseIf strArr.Length > 1 Then
                     MessageBox.Show(Me, "Developer error: retrieved column label should be one.", "Developer Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
                     strColLabel = strArr(strArr.Length - 1)
                 End If
@@ -152,5 +199,25 @@ Public Class dlgName
         Return strColLabel
     End Function
 
+    Private Sub ucrPnlOptions_ControlValueChanged(ucrChangedControl As ucrCore) Handles ucrPnlOptions.ControlValueChanged
+        If rdoSingle.Checked Then
+            ucrBase.clsRsyntax.SetBaseRFunction(clsDefaultRFunction)
+            ucrReceiverName.SetMeAsReceiver()
+        Else
+            ucrBase.clsRsyntax.SetBaseRFunction(clsRenameWithFunction)
+            ucrReceiverColumns.SetMeAsReceiver()
+        End If
+    End Sub
 
+    Private Sub ucrSelectVariables_ControlValueChanged(ucrChangedControl As ucrCore) Handles ucrSelectVariables.ControlValueChanged
+        If Not ucrSelectVariables.IsEmpty Then
+            clsRenameWithFunction.AddParameter(".data", clsRFunctionParameter:=ucrSelectVariables.ucrAvailableDataFrames.clsCurrDataFrame, iPosition:=0)
+        Else
+            clsRenameWithFunction.RemoveParameterByName(".data")
+        End If
+    End Sub
+
+    Private Sub ucrCoreControls_ControlContentsChanged() Handles ucrInputNewName.ControlContentsChanged, ucrReceiverName.ControlContentsChanged, ucrSelectVariables.ControlContentsChanged, ucrSaveDataFrame.ControlContentsChanged, ucrPnlOptions.ControlContentsChanged
+        TestOKEnabled()
+    End Sub
 End Class
