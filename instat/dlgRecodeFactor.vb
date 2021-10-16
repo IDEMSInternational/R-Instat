a221556a4367fdceb6ec452f39cb1773966eb633
Antoine Ntalumeso <antoine.ntalumeso@aims.ac.rw>
2021-10-16 18:35:13 +0200

    Additional code change

--- instat/dlgRecodeFactor.vb
+++ instat/dlgRecodeFactor.vb
@@ -205,20 +205,21 @@ Public Class dlgRecodeFactor
         TestOKEnabled()
     End Sub
 
-    Private Sub ucrFactorGrid_GridContentChanged() Handles ucrFactorGrid.GridContentChanged, ucrReceiverFactor.ControlContentsChanged
+    Private Sub ucrFactorGrid_GridContentChanged() Handles ucrFactorGrid.GridContentChanged
         Dim strCurrentLabels As List(Of String)
         Dim strNewLabels As List(Of String)
 
         strCurrentLabels = ucrFactorGrid.GetColumnAsList(ucrFactorGrid.strLabelsName, False)
         strNewLabels = ucrFactorGrid.GetColumnAsList("New Label", False)
-        clsFctRecodeFunction.ClearParameters()
-        ucrReceiverFactor.SetParameterIsRFunction()
-        clsFctRecodeFunction.AddParameter(".f", ucrReceiverFactor.GetVariableNames(), iPosition:=0)
+        For i = 0 To strCurrentLabels.Count - 1
+            'Backtick needed for names of the vector incase the levels are not valid R names
+            clsFctRecodeFunction.RemoveParameterByPosition(i + 1)
+        Next
 
         If ucrFactorGrid.IsColumnComplete("New Label") AndAlso strCurrentLabels.Count = strNewLabels.Count Then
             For i = 0 To strCurrentLabels.Count - 1
                 'Backtick needed for names of the vector incase the levels are not valid R names
-                clsFctRecodeFunction.AddParameter(Chr(96) & strNewLabels(i) & Chr(96), Chr(34) & strCurrentLabels(i) & Chr(34))
+                clsFctRecodeFunction.AddParameter(Chr(96) & strNewLabels(i) & Chr(96), Chr(34) & strCurrentLabels(i) & Chr(34), iPosition:=i + 1)
             Next
         End If
         TestOKEnabled()
