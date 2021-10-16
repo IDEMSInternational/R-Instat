76d9a7d327b2afb1f14a6216d63bb85e00d36c0a
shadrack kibet <shadrackkibet36@outlook.com>
2021-10-16 12:36:03 +0300

    code restructure and bug fixes

--- instat/dlgRank.vb
+++ instat/dlgRank.vb
@@ -19,21 +19,21 @@ Imports instat.Translations
 Public Class dlgRank
     Public bFirstLoad As Boolean = True
     Private bReset As Boolean = True
-    Private clsRankFunction As New RFunction
-    Private clsSortFunction As New RFunction
-    Private clsRoundFunction As New RFunction
-    Private clsSignifFunction As New RFunction
-    Private clsLagFunction As New RFunction
-    Private clsLeadFunction As New RFunction
-    Private clsDiffFunction As New RFunction
-    Private clsCallDiffFunction As New RFunction
-    Private clsScaleFunction As New RFunction
-    Private clsReplicateFunction As New RFunction
-    Private clsMeanFunction As New RFunction
-    Private clsStandardDevFunction As New RFunction
-    Private clsDeviationFunction As New RFunction
-    Private clsSubtractOperator As New ROperator
-    Private clsDivisionOperator As New ROperator
+    Private bSetRcode As Boolean = True
+    Private clsRankFunction As RFunction
+    Private clsSortFunction As RFunction
+    Private clsRoundFunction As RFunction
+    Private clsSignifFunction As RFunction
+    Private clsLagFunction As RFunction
+    Private clsLeadFunction As RFunction
+    Private clsDiffFunction As RFunction
+    Private clsConcDiffFunction As RFunction
+    Private clsReplicateFunction As RFunction
+    Private clsMeanFunction As RFunction
+    Private clsStandardDevFunction As RFunction
+    Private clsNumericRcodeStructure As RCodeStructure
+    Private clsSubtractOperator As ROperator
+    Private clsDivisionOperator As ROperator
 
     Private Sub dlgRank_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         If bFirstLoad Then
@@ -55,40 +55,42 @@ Public Class dlgRank
         ucrPnlTransformOptions.AddRadioButton(rdoRank)
         ucrPnlTransformOptions.AddRadioButton(rdoNumeric)
         ucrPnlTransformOptions.AddRadioButton(rdoSort)
-        'ucrPnlTransformOptions.AddRadioButton(rdoNonNegative)
 
         ucrPnlTransformOptions.AddFunctionNamesCondition(rdoRank, "rank")
-        ucrPnlTransformOptions.AddFunctionNamesCondition(rdoNumeric, {"round", "signif", "lag", "lead", "c", "scale"})
+        ucrPnlTransformOptions.AddFunctionNamesCondition(rdoNumeric, {"round", "signif", "lag", "lead", "c"})
         ucrPnlTransformOptions.AddFunctionNamesCondition(rdoSort, "sort")
 
-        ucrPnlTransformOptions.AddToLinkedControls({ucrPnlTies, ucrPnlMissingValues}, {rdoRank}, bNewLinkedAddRemoveParameter:=True, bNewLinkedHideIfParameterMissing:=True)
-        ucrPnlTransformOptions.AddToLinkedControls({ucrChkDecreasing, ucrChkMissingLast}, {rdoSort}, bNewLinkedAddRemoveParameter:=True, bNewLinkedHideIfParameterMissing:=True)
-        ucrPnlTransformOptions.AddToLinkedControls(ucrPnlNumericOptions, {rdoNumeric}, bNewLinkedAddRemoveParameter:=True, bNewLinkedHideIfParameterMissing:=True)
-
-        'Setting Parameters and Data types allowed
         ucrReceiverRank.SetParameter(New RParameter("x", 0))
         ucrReceiverRank.Selector = ucrSelectorForRank
         ucrReceiverRank.SetMeAsReceiver()
         ucrReceiverRank.bUseFilteredData = False
         ucrReceiverRank.SetParameterIsRFunction()
-        ucrReceiverRank.SetLinkedDisplayControl(lblSelectColumns)
+        ucrReceiverRank.SetDataType("numeric")
+        ucrReceiverRank.strSelectorHeading = "Numerics"
 
-        ucrPnlMissingValues.SetParameter(New RParameter("na.last", 1))
+        ucrPnlMissingValues.SetParameter(New RParameter("na.last", 2))
         ucrPnlMissingValues.AddRadioButton(rdoKeptAsMissing, Chr(34) & "keep" & Chr(34))
         ucrPnlMissingValues.AddRadioButton(rdoFirstMissingValues, "FALSE")
         ucrPnlMissingValues.AddRadioButton(rdoLast, "TRUE")
         ucrPnlMissingValues.SetRDefault("TRUE")
-        ucrPnlMissingValues.SetLinkedDisplayControl(grpMissingValues)
 
-        'Setting Parameters for the respective radio buttons
-        ucrPnlTies.SetParameter(New RParameter("ties.method", 2))
+        ucrPnlMissingValues.AddParameterValuesCondition(rdoKeptAsMissing, "na.last", Chr(34) & "keep" & Chr(34))
+        ucrPnlMissingValues.AddParameterValuesCondition(rdoFirstMissingValues, "na.last", "FALSE")
+        ucrPnlMissingValues.AddParameterValuesCondition(rdoLast, "na.last", "TRUE")
+
+        ucrPnlTies.SetParameter(New RParameter("ties.method", 3))
         ucrPnlTies.AddRadioButton(rdoAverage, Chr(34) & "average" & Chr(34))
         ucrPnlTies.AddRadioButton(rdoMinimum, Chr(34) & "min" & Chr(34))
         ucrPnlTies.AddRadioButton(rdoMaximum, Chr(34) & "max" & Chr(34))
         ucrPnlTies.AddRadioButton(rdoFirst, Chr(34) & "first" & Chr(34))
         ucrPnlTies.AddRadioButton(rdoRandom, Chr(34) & "random" & Chr(34))
         ucrPnlTies.SetRDefault(Chr(34) & "average" & Chr(34))
-        ucrPnlTies.SetLinkedDisplayControl(grpTies)
+
+        ucrPnlTies.AddParameterValuesCondition(rdoAverage, "ties.method", Chr(34) & "average" & Chr(34))
+        ucrPnlTies.AddParameterValuesCondition(rdoMinimum, "ties.method", Chr(34) & "min" & Chr(34))
+        ucrPnlTies.AddParameterValuesCondition(rdoMaximum, "ties.method", Chr(34) & "max" & Chr(34))
+        ucrPnlTies.AddParameterValuesCondition(rdoFirst, "ties.method", Chr(34) & "first" & Chr(34))
+        ucrPnlTies.AddParameterValuesCondition(rdoRandom, "ties.method", Chr(34) & "random" & Chr(34))
 
         ucrPnlNumericOptions.AddRadioButton(rdoRoundOf)
         ucrPnlNumericOptions.AddRadioButton(rdoSignificantDigits)
@@ -99,40 +101,50 @@ Public Class dlgRank
 
         ucrPnlNumericOptions.AddFunctionNamesCondition(rdoRoundOf, "round")
         ucrPnlNumericOptions.AddFunctionNamesCondition(rdoSignificantDigits, "signif")
-        ucrPnlNumericOptions.AddFunctionNamesCondition(rdoScale, "scale")
+        ucrPnlNumericOptions.AddParameterIsROperatorCondition(rdoScale, "/")
         ucrPnlNumericOptions.AddFunctionNamesCondition(rdoLag, "lag")
         ucrPnlNumericOptions.AddFunctionNamesCondition(rdoLead, "lead")
         ucrPnlNumericOptions.AddFunctionNamesCondition(rdoDifference, "c")
 
         ucrPnlNumericOptions.AddToLinkedControls(ucrNudSignifDigits, {rdoSignificantDigits}, bNewLinkedAddRemoveParameter:=True, bNewLinkedHideIfParameterMissing:=True)
         ucrPnlNumericOptions.AddToLinkedControls(ucrNudRoundOfDigits, {rdoRoundOf}, bNewLinkedAddRemoveParameter:=True, bNewLinkedHideIfParameterMissing:=True)
-        ucrPnlNumericOptions.AddToLinkedControls(ucrNudLagLeadPosition, {rdoLag, rdoLead}, bNewLinkedAddRemoveParameter:=True, bNewLinkedHideIfParameterMissing:=True)
-        ucrPnlNumericOptions.AddToLinkedControls({ucrNudDiffLag}, {rdoDifference}, bNewLinkedAddRemoveParameter:=True, bNewLinkedHideIfParameterMissing:=True)
+        ucrPnlNumericOptions.AddToLinkedControls(ucrNudLagLeadPosition, {rdoLead}, bNewLinkedAddRemoveParameter:=True, bNewLinkedHideIfParameterMissing:=True)
+        ucrPnlNumericOptions.AddToLinkedControls(ucrNudDiffLag, {rdoDifference}, bNewLinkedAddRemoveParameter:=True, bNewLinkedHideIfParameterMissing:=True)
+        ucrPnlNumericOptions.AddToLinkedControls(ucrNudLagPosition, {rdoLag}, bNewLinkedAddRemoveParameter:=True, bNewLinkedHideIfParameterMissing:=True)
+        ucrPnlTransformOptions.AddToLinkedControls(ucrPnlMissingValues, {rdoRank}, bNewLinkedAddRemoveParameter:=True, bNewLinkedHideIfParameterMissing:=True)
+        ucrPnlTransformOptions.AddToLinkedControls(ucrPnlTies, {rdoRank}, bNewLinkedAddRemoveParameter:=True, bNewLinkedHideIfParameterMissing:=True)
+        ucrPnlTransformOptions.AddToLinkedControls(ucrChkDecreasing, {rdoSort}, bNewLinkedAddRemoveParameter:=True, bNewLinkedHideIfParameterMissing:=True)
+        ucrPnlTransformOptions.AddToLinkedControls(ucrChkMissingLast, {rdoSort}, bNewLinkedAddRemoveParameter:=True, bNewLinkedHideIfParameterMissing:=True)
+        ucrPnlTransformOptions.AddToLinkedControls(ucrPnlNumericOptions, {rdoNumeric}, bNewLinkedAddRemoveParameter:=True, bNewLinkedHideIfParameterMissing:=True)
         ucrNudSignifDigits.SetLinkedDisplayControl(lblDigits)
         ucrNudRoundOfDigits.SetLinkedDisplayControl(lblRoundofDigits)
         ucrNudLagLeadPosition.SetLinkedDisplayControl(lblLagLeadPosition)
         ucrNudDiffLag.SetLinkedDisplayControl(lblDiffLag)
         ucrPnlNumericOptions.SetLinkedDisplayControl(grpNumericOptions)
+        ucrNudLagPosition.SetLinkedDisplayControl(lblLagPosition)
+        ucrReceiverRank.SetLinkedDisplayControl(lblSelectColumns)
+        ucrPnlMissingValues.SetLinkedDisplayControl(grpMissingValues)
+        ucrPnlTies.SetLinkedDisplayControl(grpTies)
 
         ucrNudSignifDigits.SetParameter(New RParameter("digits", 1))
         ucrNudSignifDigits.SetMinMax(iNewMin:=0, iNewMax:=22)
-        ucrNudSignifDigits.Increment = 1
         ucrNudSignifDigits.SetRDefault(6)
 
         ucrNudRoundOfDigits.SetParameter(New RParameter("digits", 1))
-        ucrNudRoundOfDigits.Increment = 1
-        ucrNudRoundOfDigits.SetMinMax(iNewMax:=Integer.MaxValue)
+        ucrNudRoundOfDigits.SetMinMax(iNewMin:=0, iNewMax:=Integer.MaxValue)
         ucrNudRoundOfDigits.SetRDefault(0)
 
         ucrNudLagLeadPosition.SetParameter(New RParameter("n", 1))
-        ucrNudLagLeadPosition.SetMinMax(iNewMin:=1)
-        ucrNudLagLeadPosition.Increment = 1
+        ucrNudLagLeadPosition.SetMinMax(iNewMin:=1, iNewMax:=Integer.MaxValue)
         ucrNudLagLeadPosition.SetRDefault(1)
 
         ucrNudDiffLag.SetParameter(New RParameter("lag", 1))
-        ucrNudDiffLag.SetMinMax(iNewMin:=1)
-        ucrNudDiffLag.Increment = 1
+        ucrNudDiffLag.SetMinMax(iNewMin:=1, iNewMax:=Integer.MaxValue)
         ucrNudDiffLag.SetRDefault(1)
+
+        ucrNudLagPosition.SetParameter(New RParameter("lag", 1))
+        ucrNudLagPosition.SetMinMax(iNewMin:=1, iNewMax:=Integer.MaxValue)
+        ucrNudLagPosition.SetRDefault(1)
 
         ucrSaveNew.SetPrefix("rank")
         ucrSaveNew.SetSaveTypeAsColumn()
@@ -149,12 +161,10 @@ Public Class dlgRank
         ucrChkMissingLast.SetText("Missing Last")
         ucrChkMissingLast.SetParameter(New RParameter("na.last"))
         ucrChkMissingLast.SetValuesCheckedAndUnchecked("TRUE", "FALSE")
-
     End Sub
 
     ' Sub that runs only the first time the dialog loads it sets default RFunction as the base function
     Private Sub SetDefaults()
-
         clsRankFunction = New RFunction
         clsSortFunction = New RFunction
         clsRoundFunction = New RFunction
@@ -162,22 +172,22 @@ Public Class dlgRank
         clsLagFunction = New RFunction
         clsLeadFunction = New RFunction
         clsDiffFunction = New RFunction
-        clsCallDiffFunction = New RFunction
-        clsScaleFunction = New RFunction
+        clsConcDiffFunction = New RFunction
         clsReplicateFunction = New RFunction
         clsMeanFunction = New RFunction
         clsStandardDevFunction = New RFunction
-        clsDeviationFunction = New RFunction
         clsSubtractOperator = New ROperator
         clsDivisionOperator = New ROperator
+        clsNumericRcodeStructure = New RCodeStructure
+
 
         ucrSelectorForRank.Reset()
         ucrReceiverRank.SetMeAsReceiver()
         ucrSaveNew.Reset()
 
-        'Setting default parameters for the base function
         clsRankFunction.SetRCommand("rank")
-        clsRankFunction.AddParameter("na.last", Chr(34) & "keep" & Chr(34))
+        clsRankFunction.AddParameter("na.last", Chr(34) & "keep" & Chr(34), iPosition:=2)
+        clsRankFunction.AddParameter("ties.method", Chr(34) & "average" & Chr(34), iPosition:=3)
 
         clsSortFunction.SetRCommand("sort")
         clsSortFunction.AddParameter("decreasing", "TRUE", iPosition:=1)
@@ -199,9 +209,9 @@ Public Class dlgRank
         clsReplicateFunction.SetRCommand("rep")
         clsReplicateFunction.AddParameter("x", "NA", iPosition:=0)
 
-        clsCallDiffFunction.SetRCommand("c")
-        clsCallDiffFunction.AddParameter("y", clsRFunctionParameter:=clsReplicateFunction, iPosition:=0, bIncludeArgumentName:=False)
-        clsCallDiffFunction.AddParameter("x", clsRFunctionParameter:=clsDiffFunction, iPosition:=1, bIncludeArgumentName:=False)
+        clsConcDiffFunction.SetRCommand("c")
+        clsConcDiffFunction.AddParameter("y", clsRFunctionParameter:=clsReplicateFunction, iPosition:=0, bIncludeArgumentName:=False)
+        clsConcDiffFunction.AddParameter("x", clsRFunctionParameter:=clsDiffFunction, iPosition:=1, bIncludeArgumentName:=False)
 
         clsMeanFunction.SetRCommand("mean")
         clsMeanFunction.AddParameter("na.rm", "TRUE", iPosition:=1)
@@ -209,21 +219,17 @@ Public Class dlgRank
         clsStandardDevFunction.SetRCommand("sd")
         clsStandardDevFunction.AddParameter("na.rm", "TRUE", iPosition:=1)
 
-        clsDeviationFunction.SetRCommand("c")
-        clsDeviationFunction.AddParameter("x", clsROperatorParameter:=clsSubtractOperator, bIncludeArgumentName:=False)
-
         clsSubtractOperator.SetOperation("-")
         clsSubtractOperator.AddParameter("y", clsRFunctionParameter:=clsMeanFunction, iPosition:=1)
 
         clsDivisionOperator.SetOperation("/")
-        clsDivisionOperator.AddParameter("x", clsRFunctionParameter:=clsDeviationFunction, iPosition:=0)
+        clsDivisionOperator.AddParameter("x", clsROperatorParameter:=clsSubtractOperator, iPosition:=0)
         clsDivisionOperator.AddParameter("y", clsRFunctionParameter:=clsStandardDevFunction, iPosition:=1)
 
-        clsScaleFunction.SetRCommand("scale")
-        clsScaleFunction.AddParameter("x", clsROperatorParameter:=clsDivisionOperator, iPosition:=0, bIncludeArgumentName:=False)
-
         ' Set default RFunction as the base function
-        ucrBase.clsRsyntax.SetBaseRFunction(clsRoundFunction)
+        clsNumericRcodeStructure = clsRoundFunction
+        ucrBase.clsRsyntax.SetBaseRFunction(clsNumericRcodeStructure)
+        bSetRcode = True
     End Sub
 
     Private Sub SetRCodeForControls(bReset As Boolean)
@@ -236,15 +242,14 @@ Public Class dlgRank
         ucrReceiverRank.AddAdditionalCodeParameterPair(clsMeanFunction, New RParameter("x", 0), iAdditionalPairNo:=7)
         ucrReceiverRank.AddAdditionalCodeParameterPair(clsSubtractOperator, New RParameter("x", 0), iAdditionalPairNo:=8)
         ucrReceiverRank.AddAdditionalCodeParameterPair(clsStandardDevFunction, New RParameter("x", 0), iAdditionalPairNo:=9)
-        ucrNudLagLeadPosition.AddAdditionalCodeParameterPair(clsLeadFunction, New RParameter("n", 1), iAdditionalPairNo:=1)
         ucrNudDiffLag.AddAdditionalCodeParameterPair(clsReplicateFunction, New RParameter("times", 1), iAdditionalPairNo:=1)
         ucrSaveNew.AddAdditionalRCode(clsLeadFunction, iAdditionalPairNo:=1)
         ucrSaveNew.AddAdditionalRCode(clsLagFunction, iAdditionalPairNo:=2)
         ucrSaveNew.AddAdditionalRCode(clsSignifFunction, iAdditionalPairNo:=3)
-        ucrSaveNew.AddAdditionalRCode(clsCallDiffFunction, iAdditionalPairNo:=4)
+        ucrSaveNew.AddAdditionalRCode(clsConcDiffFunction, iAdditionalPairNo:=4)
         ucrSaveNew.AddAdditionalRCode(clsRankFunction, iAdditionalPairNo:=5)
         ucrSaveNew.AddAdditionalRCode(clsSortFunction, iAdditionalPairNo:=6)
-        ucrSaveNew.AddAdditionalRCode(clsScaleFunction, iAdditionalPairNo:=7)
+        ucrSaveNew.AddAdditionalRCode(clsDivisionOperator, iAdditionalPairNo:=7)
 
         ucrPnlTransformOptions.SetRCode(ucrBase.clsRsyntax.clsBaseFunction, bReset)
         ucrReceiverRank.SetRCode(clsRankFunction, bReset)
@@ -255,21 +260,18 @@ Public Class dlgRank
         ucrPnlMissingValues.SetRCode(clsRankFunction, bReset)
         ucrNudRoundOfDigits.SetRCode(clsRoundFunction, bReset)
         ucrNudSignifDigits.SetRCode(clsSignifFunction, bReset)
-        ucrNudLagLeadPosition.SetRCode(clsLagFunction, bReset)
+        ucrNudLagLeadPosition.SetRCode(clsLeadFunction, bReset)
         ucrNudDiffLag.SetRCode(clsDiffFunction, bReset)
-        ucrPnlNumericOptions.SetRCode(ucrBase.clsRsyntax.clsBaseFunction, bReset)
-    End Sub
-
-    'Testing when to Enable the OK button
-    Private Sub TestOKEnabled()
-        If Not ucrReceiverRank.IsEmpty() AndAlso ucrSaveNew.IsComplete Then
-            ucrBase.OKEnabled(True)
-        Else
-            ucrBase.OKEnabled(False)
+        ucrNudLagPosition.SetRCode(clsLagFunction, bReset)
+        If bSetRcode Then
+            ucrPnlNumericOptions.SetRCode(clsNumericRcodeStructure, bReset)
         End If
     End Sub
 
-    'When the reset button is clicked, set the defaults again
+    Private Sub TestOKEnabled()
+        ucrBase.OKEnabled(Not ucrReceiverRank.IsEmpty() AndAlso ucrSaveNew.IsComplete)
+    End Sub
+
     Private Sub ucrBase_ClickReset(sender As Object, e As EventArgs) Handles ucrBase.ClickReset
         SetDefaults()
         SetRCodeForControls(True)
@@ -277,31 +279,39 @@ Public Class dlgRank
     End Sub
 
     Private Sub ucrPnlTransformOptions_ControlContentsChanged(ucrChangedControl As ucrCore) Handles ucrPnlTransformOptions.ControlContentsChanged, ucrPnlNumericOptions.ControlContentsChanged
-        If rdoRank.Checked = True Then
+        bSetRcode = True
+        If rdoRank.Checked Then
             ucrSaveNew.SetPrefix("rank")
             ucrBase.clsRsyntax.SetBaseRFunction(clsRankFunction)
-        ElseIf rdoSort.Checked = True Then
+        ElseIf rdoSort.Checked Then
             ucrSaveNew.SetPrefix("sort")
             ucrBase.clsRsyntax.SetBaseRFunction(clsSortFunction)
         ElseIf rdoNumeric.Checked Then
             If rdoRoundOf.Checked Then
                 ucrSaveNew.SetPrefix("round")
-                ucrBase.clsRsyntax.SetBaseRFunction(clsRoundFunction)
+                clsNumericRcodeStructure = clsRoundFunction
+                ucrBase.clsRsyntax.SetBaseRFunction(clsNumericRcodeStructure)
             ElseIf rdoSignificantDigits.Checked Then
                 ucrSaveNew.SetPrefix("significant")
-                ucrBase.clsRsyntax.SetBaseRFunction(clsSignifFunction)
+                clsNumericRcodeStructure = clsSignifFunction
+                ucrBase.clsRsyntax.SetBaseRFunction(clsNumericRcodeStructure)
             ElseIf rdoLag.Checked Then
                 ucrSaveNew.SetPrefix("lag")
-                ucrBase.clsRsyntax.SetBaseRFunction(clsLagFunction)
+                clsNumericRcodeStructure = clsLagFunction
+                ucrBase.clsRsyntax.SetBaseRFunction(clsNumericRcodeStructure)
             ElseIf rdoLead.Checked Then
                 ucrSaveNew.SetPrefix("lead")
-                ucrBase.clsRsyntax.SetBaseRFunction(clsLeadFunction)
+                clsNumericRcodeStructure = clsLeadFunction
+                ucrBase.clsRsyntax.SetBaseRFunction(clsNumericRcodeStructure)
             ElseIf rdoDifference.Checked Then
                 ucrSaveNew.SetPrefix("difference")
-                ucrBase.clsRsyntax.SetBaseRFunction(clsCallDiffFunction)
+                clsNumericRcodeStructure = clsConcDiffFunction
+                ucrBase.clsRsyntax.SetBaseRFunction(clsNumericRcodeStructure)
             ElseIf rdoScale.Checked Then
+                bSetRcode = False
                 ucrSaveNew.SetPrefix("scale")
-                ucrBase.clsRsyntax.SetBaseRFunction(clsScaleFunction)
+                clsNumericRcodeStructure = clsDivisionOperator
+                ucrBase.clsRsyntax.SetBaseROperator(clsDivisionOperator)
             End If
         End If
     End Sub
