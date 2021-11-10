df7639845f2a39f97227232d39ffc7a8ad42b436
Gioche6 <gioche6@gmail.com>
2021-11-10 22:06:11 +0300

    Removing commented codes

--- instat/dlgInfillMissingValues.vb
+++ instat/dlgInfillMissingValues.vb
@@ -108,8 +108,6 @@ Public Class dlgInfillMissingValues
         ucrChkCopyFromBelow.SetRDefault("FALSE")
 
         ucrChkBy.SetText("By:")
-        'ucrChkBy.AddParameterPresentCondition(True, "by")
-        'ucrChkBy.AddParameterPresentCondition(False, "by", False)
 
         ucrChkMaxGap.SetText("Maximum Gap:")
         ucrChkMaxGap.AddParameterPresentCondition(True, "maxgap")
