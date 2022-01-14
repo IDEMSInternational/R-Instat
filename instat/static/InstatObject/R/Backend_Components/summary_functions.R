f8d028c6846e0580224ab4c1a0fc1ab82f995f92
lilyclements <lclements1004@outlook.com>
2022-01-14 20:20:55 +0000

    Update instat/static/InstatObject/R/Backend_Components/summary_functions.R
    Co-authored-by: lloyddewit <57253949+lloyddewit@users.noreply.github.com>

--- instat/static/InstatObject/R/Backend_Components/summary_functions.R
+++ instat/static/InstatObject/R/Backend_Components/summary_functions.R
@@ -1458,8 +1458,8 @@ DataBook$set("public", "summary_table", function(data_name, columns_to_summarise
   shaped_cell_values <- shaped_cell_values %>% dplyr::mutate(value = round(value, signif_fig))
 if (treat_columns_as_factor){
   shaped_cell_values <- shaped_cell_values %>%
-  dplyr::mutate(summary = as.factor(summary)) %>% dplyr::mutate(summary = forcats::fct_relevel(summary, summaries_display)) %>%
-  dplyr::mutate(variable = as.factor(variable)) %>% dplyr::mutate(variable= forcats::fct_relevel(variable, columns_to_summarise))
+      dplyr::mutate(summary = as.factor(summary)) %>% dplyr::mutate(summary = forcats::fct_relevel(summary, summaries_display)) %>%
+      dplyr::mutate(variable = as.factor(variable)) %>% dplyr::mutate(variable= forcats::fct_relevel(variable, columns_to_summarise))
 }
 if (store_table) {
     data_book$import_data(data_tables = list(shaped_cell_values = shaped_cell_values))
