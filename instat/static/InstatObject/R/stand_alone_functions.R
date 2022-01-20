243203fd51fa2a1e1bab073f151f87eaeccacdf3
shadrack kibet <shadrackkibet36@outlook.com>
2022-01-20 15:03:45 +0300

    code refactor

--- instat/static/InstatObject/R/stand_alone_functions.R
+++ instat/static/InstatObject/R/stand_alone_functions.R
@@ -2513,11 +2513,8 @@ slopegraph <- function(data, x, y, colour, data_label = NULL,
 get_quarter_label <-   function(quarter, start_month){
   if (!start_month %in% 1:12) stop(start_month, " is an invalid start month, must be in range of 1:12")
   if (!all(quarter %in% 1:4)) stop(quarter, " is an invalid quarter, must be in range of 1:4")
-  if (start_month == 1) {
-    mabb <- substr(month.abb, 1, 1)
-  } else {
-    mabb <- rep(substr(month.abb, 1, 1), times = 2)[start_month:(11 + start_month)]
-  }
-  qtr <- ifelse(quarter == 1, paste(mabb[1:3], collapse = ""), ifelse(quarter == 2, paste(mabb[4:6], collapse = ""), ifelse(quarter == 3, paste(mabb[7:9], collapse = ""), paste(mabb[10:12], collapse = ""))))
+  mabb <- rep(substr(month.abb, 1, 1), times = 2)[start_month:(11 + start_month)]
+  qtr <- sapply(quarter, function(x){start_pos <- 1 + ((x-1) * 3)
+  paste(mabb[start_pos:(start_pos+2)], collapse = "")})
   return(factor(x = qtr, levels = unique(qtr)))
 }
