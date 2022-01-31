b9eee18e01e6712d0ea74f6986bc59e884124b53
Derrick Agorhom <agorhomderrick@gmail.com>
2022-01-31 06:07:35 -0800

    Partially fixes delete implementation

--- instat/Interface/IDataViewGrid.vb
+++ instat/Interface/IDataViewGrid.vb
@@ -19,6 +19,8 @@ Public Interface IDataViewGrid
 
     Event CellDataChanged()
 
+    Event DeleteValuesToDataframe()
+
     Event PasteValuesToDataframe()
 
     Event ReplaceValueInData(strNewValue As String, strColumnName As String, strRowText As String)
