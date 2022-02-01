3e04a11065f3fd104aa121c3017abf62474d326e
Gioche6 <gioche6@gmail.com>
2022-02-01 10:53:56 +0300

    Working on Textfile

--- instat/dlgImportDataset.vb
+++ instat/dlgImportDataset.vb
@@ -729,7 +729,7 @@ Public Class dlgImportDataset
             Dim iLinesToPreview As Integer = ucrNudPreviewLines.Value + If(IsCSVFileFormat(), ucrNudRowsToSkipCSV.Value, ucrNudRowsToSkipText.Value)
             lblTextFilePreview.Text = "Text File Preview:"
             Try
-                Using sReader As New StreamReader(strFilePathSystem)
+                Using sReader As New StreamReader(strFilePathSystem.Replace("\", "/"))
                     For k = 0 To iLinesToPreview
                         txtTextFilePreview.Text &= sReader.ReadLine() & Environment.NewLine
                         If sReader.Peek() = -1 Then
