4cb1165eb7f386a3b353b2772e46b35985f5204d
anastasia-mbithe <anastasiambithe7@gmail.com>
2022-03-02 09:44:30 +0300

    Minor fix

--- instat/static/InstatObject/R/instat_object_R6.R
+++ instat/static/InstatObject/R/instat_object_R6.R
@@ -2717,6 +2717,7 @@ DataBook$set("public", "add_flag_fields", function(data_name, col_names, key_col
 
 DataBook$set("public", "remove_empty", function(data_name,  which = c("rows","cols")) {
   self$get_data_objects(data_name)$remove_empty(which = which)
+})
 
 DataBook$set("public", "replace_values_with_NA", function(data_name, row_index, column_index) {
   self$get_data_objects(data_name)$replace_values_with_NA(row_index = row_index, column_index = column_index)
