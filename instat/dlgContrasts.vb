56dfaac639e0044fe595b1bfd2a21abcb5d6a9d2
anastasia-mbithe <anastasiambithe7@gmail.com>
2022-04-22 13:09:22 +0300

    Minor change

--- instat/dlgContrasts.vb
+++ instat/dlgContrasts.vb
@@ -219,7 +219,7 @@ Public Class dlgContrasts
             vecColums = expContrasts.AsNumericMatrix()
             For j = 0 To vecColums.ColumnCount - 1
                 For i = 0 To vecColums.RowCount - 1
-                    grdCurrSheet.Item(row:=i, col:=j) = vecColums(i, j).ToString("0.###")
+                    grdCurrSheet.Item(row:=i, col:=j) = vecColums(i, j)
                 Next
             Next
         End If
