9061bf42845ea9f027c28695614d34e190a02b73
Antoine Ntalumeso <antoine.ntalumeso@aims.ac.rw>
2022-05-30 12:25:42 +0300

    Minor code change

--- instat/static/InstatObject/R/instat_object_R6.R
+++ instat/static/InstatObject/R/instat_object_R6.R
@@ -125,7 +125,7 @@ DataBook$set("public", "replace_instat_object", function(new_instat_object) {
 )
 
 DataBook$set("public", "set_data_objects", function(new_data_objects) {
-  if(!is.list(new_data_objects) || (length(new_data_objects) > 0 && !all("data_object" %in% sapply(new_data_objects, class)))) {
+  if(!is.list(new_data_objects) || (length(new_data_objects) > 0 && !all("DataSheet" %in% sapply(new_data_objects, class)))) {
     stop("new_data_objects must be a list of data_objects")
   }
   else private$.data_sheets <- new_data_objects
@@ -1104,7 +1104,7 @@ DataBook$set("public", "add_metadata_field", function(data_name, property, new_v
 DataBook$set("public", "reorder_dataframes", function(data_frames_order) {
   if(length(data_frames_order) != length(names(private$.data_sheets))) stop("number data frames to order should be equal to number of dataframes in the object")
   if(!setequal(data_frames_order,names(private$.data_sheets))) stop("data_frames_order must be a permutation of the dataframe names.")
-  
+
   self$set_data_objects(private$.data_sheets[data_frames_order])
   self$data_objects_changed <- TRUE
 } 
