6633a7f9d95df1b0568b97a2a3603796c6cac5bc
Vitalis95 <vitaliskwambai@gmail.com>
2022-06-22 17:06:45 +0300

    adding controls dialog

--- instat/dlgExportToClimsoft.Designer.vb
+++ instat/dlgExportToClimsoft.Designer.vb
@@ -22,18 +22,197 @@ Partial Class dlgExportToClimsoft
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.ucrSelectorImportToClimsoft = New instat.ucrSelectorByDataFrameAddRemove()
+        Me.ucrReceiverDate = New instat.ucrReceiverSingle()
+        Me.ucrReceiverElements = New instat.ucrReceiverMultiple()
+        Me.lblElement = New System.Windows.Forms.Label()
+        Me.lblDate = New System.Windows.Forms.Label()
+        Me.lblExport = New System.Windows.Forms.Label()
+        Me.cmdBrowse = New System.Windows.Forms.Button()
+        Me.ucrInputExportFile = New instat.ucrInputTextBox()
+        Me.ucrBase = New instat.ucrButtons()
+        Me.ucrInputHour = New instat.ucrInputTextBox()
+        Me.ucrInputLevel = New instat.ucrInputTextBox()
+        Me.lblHour = New System.Windows.Forms.Label()
+        Me.lblLevel = New System.Windows.Forms.Label()
         Me.SuspendLayout()
+        '
+        'ucrSelectorImportToClimsoft
+        '
+        Me.ucrSelectorImportToClimsoft.AutoSize = True
+        Me.ucrSelectorImportToClimsoft.bDropUnusedFilterLevels = False
+        Me.ucrSelectorImportToClimsoft.bShowHiddenColumns = False
+        Me.ucrSelectorImportToClimsoft.bUseCurrentFilter = True
+        Me.ucrSelectorImportToClimsoft.Location = New System.Drawing.Point(9, 15)
+        Me.ucrSelectorImportToClimsoft.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrSelectorImportToClimsoft.Name = "ucrSelectorImportToClimsoft"
+        Me.ucrSelectorImportToClimsoft.Size = New System.Drawing.Size(213, 183)
+        Me.ucrSelectorImportToClimsoft.TabIndex = 1
+        '
+        'ucrReceiverDate
+        '
+        Me.ucrReceiverDate.AutoSize = True
+        Me.ucrReceiverDate.frmParent = Me
+        Me.ucrReceiverDate.Location = New System.Drawing.Point(331, 56)
+        Me.ucrReceiverDate.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverDate.Name = "ucrReceiverDate"
+        Me.ucrReceiverDate.Selector = Nothing
+        Me.ucrReceiverDate.Size = New System.Drawing.Size(120, 20)
+        Me.ucrReceiverDate.strNcFilePath = ""
+        Me.ucrReceiverDate.TabIndex = 5
+        Me.ucrReceiverDate.ucrSelector = Nothing
+        '
+        'ucrReceiverElements
+        '
+        Me.ucrReceiverElements.AutoSize = True
+        Me.ucrReceiverElements.frmParent = Me
+        Me.ucrReceiverElements.Location = New System.Drawing.Point(325, 195)
+        Me.ucrReceiverElements.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverElements.Name = "ucrReceiverElements"
+        Me.ucrReceiverElements.Selector = Nothing
+        Me.ucrReceiverElements.Size = New System.Drawing.Size(120, 100)
+        Me.ucrReceiverElements.strNcFilePath = ""
+        Me.ucrReceiverElements.TabIndex = 6
+        Me.ucrReceiverElements.ucrSelector = Nothing
+        '
+        'lblElement
+        '
+        Me.lblElement.AutoSize = True
+        Me.lblElement.Location = New System.Drawing.Point(327, 178)
+        Me.lblElement.Name = "lblElement"
+        Me.lblElement.Size = New System.Drawing.Size(53, 13)
+        Me.lblElement.TabIndex = 10
+        Me.lblElement.Text = "Elements:"
+        '
+        'lblDate
+        '
+        Me.lblDate.AutoSize = True
+        Me.lblDate.Location = New System.Drawing.Point(334, 38)
+        Me.lblDate.Name = "lblDate"
+        Me.lblDate.Size = New System.Drawing.Size(33, 13)
+        Me.lblDate.TabIndex = 11
+        Me.lblDate.Text = "Date:"
+        '
+        'lblExport
+        '
+        Me.lblExport.AutoSize = True
+        Me.lblExport.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.lblExport.Location = New System.Drawing.Point(20, 314)
+        Me.lblExport.Name = "lblExport"
+        Me.lblExport.Size = New System.Drawing.Size(59, 13)
+        Me.lblExport.TabIndex = 14
+        Me.lblExport.Text = "Export File:"
+        '
+        'cmdBrowse
+        '
+        Me.cmdBrowse.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.cmdBrowse.Location = New System.Drawing.Point(336, 310)
+        Me.cmdBrowse.Name = "cmdBrowse"
+        Me.cmdBrowse.Size = New System.Drawing.Size(80, 23)
+        Me.cmdBrowse.TabIndex = 16
+        Me.cmdBrowse.Text = "Browse"
+        Me.cmdBrowse.UseVisualStyleBackColor = True
+        '
+        'ucrInputExportFile
+        '
+        Me.ucrInputExportFile.AddQuotesIfUnrecognised = True
+        Me.ucrInputExportFile.AutoSize = True
+        Me.ucrInputExportFile.IsMultiline = False
+        Me.ucrInputExportFile.IsReadOnly = False
+        Me.ucrInputExportFile.Location = New System.Drawing.Point(152, 311)
+        Me.ucrInputExportFile.Margin = New System.Windows.Forms.Padding(6, 8, 6, 8)
+        Me.ucrInputExportFile.Name = "ucrInputExportFile"
+        Me.ucrInputExportFile.Size = New System.Drawing.Size(182, 21)
+        Me.ucrInputExportFile.TabIndex = 15
+        '
+        'ucrBase
+        '
+        Me.ucrBase.AutoSize = True
+        Me.ucrBase.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.ucrBase.Location = New System.Drawing.Point(22, 345)
+        Me.ucrBase.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.ucrBase.Name = "ucrBase"
+        Me.ucrBase.Size = New System.Drawing.Size(408, 52)
+        Me.ucrBase.TabIndex = 17
+        '
+        'ucrInputHour
+        '
+        Me.ucrInputHour.AddQuotesIfUnrecognised = True
+        Me.ucrInputHour.AutoSize = True
+        Me.ucrInputHour.IsMultiline = False
+        Me.ucrInputHour.IsReadOnly = False
+        Me.ucrInputHour.Location = New System.Drawing.Point(331, 104)
+        Me.ucrInputHour.Name = "ucrInputHour"
+        Me.ucrInputHour.Size = New System.Drawing.Size(123, 21)
+        Me.ucrInputHour.TabIndex = 18
+        '
+        'ucrInputLevel
+        '
+        Me.ucrInputLevel.AddQuotesIfUnrecognised = True
+        Me.ucrInputLevel.AutoSize = True
+        Me.ucrInputLevel.IsMultiline = False
+        Me.ucrInputLevel.IsReadOnly = False
+        Me.ucrInputLevel.Location = New System.Drawing.Point(331, 141)
+        Me.ucrInputLevel.Name = "ucrInputLevel"
+        Me.ucrInputLevel.Size = New System.Drawing.Size(125, 21)
+        Me.ucrInputLevel.TabIndex = 19
+        '
+        'lblHour
+        '
+        Me.lblHour.AutoSize = True
+        Me.lblHour.Location = New System.Drawing.Point(333, 88)
+        Me.lblHour.Name = "lblHour"
+        Me.lblHour.Size = New System.Drawing.Size(33, 13)
+        Me.lblHour.TabIndex = 20
+        Me.lblHour.Text = "Hour:"
+        '
+        'lblLevel
+        '
+        Me.lblLevel.AutoSize = True
+        Me.lblLevel.Location = New System.Drawing.Point(327, 125)
+        Me.lblLevel.Name = "lblLevel"
+        Me.lblLevel.Size = New System.Drawing.Size(36, 13)
+        Me.lblLevel.TabIndex = 21
+        Me.lblLevel.Text = "Level:"
         '
         'dlgExportToClimsoft
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
         Me.AutoSize = True
-        Me.ClientSize = New System.Drawing.Size(521, 312)
+        Me.ClientSize = New System.Drawing.Size(458, 400)
+        Me.Controls.Add(Me.lblLevel)
+        Me.Controls.Add(Me.lblHour)
+        Me.Controls.Add(Me.ucrInputLevel)
+        Me.Controls.Add(Me.ucrInputHour)
+        Me.Controls.Add(Me.ucrBase)
+        Me.Controls.Add(Me.lblExport)
+        Me.Controls.Add(Me.cmdBrowse)
+        Me.Controls.Add(Me.ucrInputExportFile)
+        Me.Controls.Add(Me.lblDate)
+        Me.Controls.Add(Me.lblElement)
+        Me.Controls.Add(Me.ucrReceiverElements)
+        Me.Controls.Add(Me.ucrReceiverDate)
+        Me.Controls.Add(Me.ucrSelectorImportToClimsoft)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
         Me.Name = "dlgExportToClimsoft"
         Me.Text = "Export For Climsoft"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents ucrSelectorImportToClimsoft As ucrSelectorByDataFrameAddRemove
+    Friend WithEvents ucrReceiverDate As ucrReceiverSingle
+    Friend WithEvents ucrReceiverElements As ucrReceiverMultiple
+    Friend WithEvents lblElement As Label
+    Friend WithEvents lblDate As Label
+    Friend WithEvents lblExport As Label
+    Friend WithEvents cmdBrowse As Button
+    Friend WithEvents ucrInputExportFile As ucrInputTextBox
+    Friend WithEvents ucrBase As ucrButtons
+    Friend WithEvents lblLevel As Label
+    Friend WithEvents lblHour As Label
+    Friend WithEvents ucrInputLevel As ucrInputTextBox
+    Friend WithEvents ucrInputHour As ucrInputTextBox
 End Class
