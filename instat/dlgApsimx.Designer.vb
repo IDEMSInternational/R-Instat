86c7c5ed3411becb5ad1a3fec9ec5bb3be42dbde
MeSophie <106779233+MeSophie@users.noreply.github.com>
2022-07-19 16:19:19 +0200

    Almost

--- instat/dlgApsimx.Designer.vb
+++ instat/dlgApsimx.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class dlgApsimx
     Inherits System.Windows.Forms.Form
 
     'Form remplace la méthode Dispose pour nettoyer la liste des composants.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,25 +20,84 @@ Partial Class dlgApsimx
     'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
     'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
     'Ne la modifiez pas à l'aide de l'éditeur de code.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.lblPath = New System.Windows.Forms.Label()
+        Me.cmdBrowse = New System.Windows.Forms.Button()
+        Me.ucrChkSilent = New instat.ucrCheck()
         Me.ucrBase = New instat.ucrButtons()
+        Me.ucrInputPath = New instat.ucrInputTextBox()
+        Me.ucrSaveDataframeName = New instat.ucrSave()
         Me.SuspendLayout()
+        '
+        'lblPath
+        '
+        Me.lblPath.AutoSize = True
+        Me.lblPath.Location = New System.Drawing.Point(13, 33)
+        Me.lblPath.Name = "lblPath"
+        Me.lblPath.Size = New System.Drawing.Size(32, 13)
+        Me.lblPath.TabIndex = 2
+        Me.lblPath.Text = "Path:"
+        '
+        'cmdBrowse
+        '
+        Me.cmdBrowse.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.cmdBrowse.Location = New System.Drawing.Point(312, 33)
+        Me.cmdBrowse.Name = "cmdBrowse"
+        Me.cmdBrowse.Size = New System.Drawing.Size(109, 23)
+        Me.cmdBrowse.TabIndex = 5
+        Me.cmdBrowse.Tag = "browse"
+        Me.cmdBrowse.Text = "Browse"
+        Me.cmdBrowse.UseVisualStyleBackColor = True
+        '
+        'ucrChkSilent
+        '
+        Me.ucrChkSilent.AutoSize = True
+        Me.ucrChkSilent.Checked = False
+        Me.ucrChkSilent.Location = New System.Drawing.Point(16, 81)
+        Me.ucrChkSilent.Name = "ucrChkSilent"
+        Me.ucrChkSilent.Size = New System.Drawing.Size(100, 23)
+        Me.ucrChkSilent.TabIndex = 4
         '
         'ucrBase
         '
         Me.ucrBase.AutoSize = True
         Me.ucrBase.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
-        Me.ucrBase.Location = New System.Drawing.Point(12, 255)
+        Me.ucrBase.Location = New System.Drawing.Point(13, 188)
         Me.ucrBase.Name = "ucrBase"
         Me.ucrBase.Size = New System.Drawing.Size(408, 52)
         Me.ucrBase.TabIndex = 1
+        '
+        'ucrInputPath
+        '
+        Me.ucrInputPath.AddQuotesIfUnrecognised = True
+        Me.ucrInputPath.AutoSize = True
+        Me.ucrInputPath.IsMultiline = False
+        Me.ucrInputPath.IsReadOnly = False
+        Me.ucrInputPath.Location = New System.Drawing.Point(50, 33)
+        Me.ucrInputPath.Name = "ucrInputPath"
+        Me.ucrInputPath.Size = New System.Drawing.Size(256, 21)
+        Me.ucrInputPath.TabIndex = 6
+        '
+        'ucrSaveDataframeName
+        '
+        Me.ucrSaveDataframeName.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.ucrSaveDataframeName.Location = New System.Drawing.Point(16, 133)
+        Me.ucrSaveDataframeName.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.ucrSaveDataframeName.Name = "ucrSaveDataframeName"
+        Me.ucrSaveDataframeName.Size = New System.Drawing.Size(405, 24)
+        Me.ucrSaveDataframeName.TabIndex = 7
         '
         'dlgApsimx
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(433, 310)
+        Me.ClientSize = New System.Drawing.Size(423, 251)
+        Me.Controls.Add(Me.ucrSaveDataframeName)
+        Me.Controls.Add(Me.ucrInputPath)
+        Me.Controls.Add(Me.cmdBrowse)
+        Me.Controls.Add(Me.ucrChkSilent)
+        Me.Controls.Add(Me.lblPath)
         Me.Controls.Add(Me.ucrBase)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.MaximizeBox = False
@@ -52,4 +111,9 @@ Partial Class dlgApsimx
     End Sub
 
     Friend WithEvents ucrBase As ucrButtons
+    Friend WithEvents lblPath As Label
+    Friend WithEvents ucrChkSilent As ucrCheck
+    Friend WithEvents cmdBrowse As Button
+    Friend WithEvents ucrInputPath As ucrInputTextBox
+    Friend WithEvents ucrSaveDataframeName As ucrSave
 End Class
--- instat/dlgApsimx.vb
+++ instat/dlgApsimx.vb
@@ -15,27 +15,108 @@
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 Imports instat.Translations
+Imports System.IO
 Public Class dlgApsimx
     Public bFirstLoad As Boolean = True
+    Private bFromLibrary As Boolean = False
+    Private strFilePath As String = ""
+    Private strLibraryPath As String = Path.Combine(frmMain.strStaticPath, "Library", "Climatic", "Shapefiles/")
+    Private bReset As Boolean = True
+    Private clsApsimxExampleFunction As New RFunction
+
     Private Sub dlgApsimx_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         If bFirstLoad Then
             InitialiseDialog()
-            SetDefaults()
             bFirstLoad = False
         End If
-        TestOKEnabled()
+        If bReset Then
+            SetDefaults()
+        End If
+        SetRCodeForControls(bReset)
+        bReset = False
         autoTranslate(Me)
+        TestOKEnabled()
     End Sub
 
     Private Sub InitialiseDialog()
         ucrBase.iHelpTopicID = 479
+
+        ucrInputPath.IsReadOnly = True
+
+        ucrSaveDataframeName.SetSaveTypeAsDataFrame()
+        ucrSaveDataframeName.SetIsTextBox()
+        ucrSaveDataframeName.SetLabelText("Dataframe Name:")
+
+        ucrChkSilent.SetText("Silent")
+        ucrChkSilent.SetParameter(New RParameter("silent", 1))
+        ucrChkSilent.SetValuesCheckedAndUnchecked("TRUE", "FALSE")
+        ucrChkSilent.SetRDefault("FALSE")
+    End Sub
+
+
+    Private Sub SetDefaults()
+        clsApsimxExampleFunction = New RFunction
+
+        ucrInputPath.SetName("")
+        clsApsimxExampleFunction.SetPackageName("apsimx")
+        clsApsimxExampleFunction.SetRCommand("apsimx_example")
+
+
+    End Sub
+
+    Private Sub SetRCodeForControls(bReset As Boolean)
+        ucrChkSilent.SetRCode(clsApsimxExampleFunction, bReset)
     End Sub
 
     Private Sub TestOKEnabled()
 
     End Sub
-
-    Private Sub SetDefaults()
+    Private Sub ucrBase_ClickReset(sender As Object, e As EventArgs) Handles ucrBase.ClickReset
+        SetDefaults()
+        SetRCodeForControls(True)
         TestOKEnabled()
+    End Sub
+    Public Sub GetFileFromOpenDialog()
+        Dim strFileName As String = ""
+        Dim strFileExt As String = ""
+        Dim strTemp As String = ""
+
+        Using dlgOpen As New OpenFileDialog
+            dlgOpen.Filter = "Apsimxfiles|*.apsimx|All Apsimxfiles|*.apsimx;*.apsim"
+            dlgOpen.Title = "Import Shape File"
+
+            If bFromLibrary Then
+                dlgOpen.InitialDirectory = Path.GetDirectoryName(Replace(strLibraryPath, "/", "\"))
+            ElseIf Not ucrInputPath.IsEmpty() Then
+                dlgOpen.InitialDirectory = Path.GetDirectoryName(Replace(ucrInputPath.GetText(), "/", "\"))
+            Else
+                dlgOpen.InitialDirectory = frmMain.clsInstatOptions.strWorkingDirectory
+            End If
+
+            If dlgOpen.ShowDialog() = DialogResult.OK Then
+
+                If dlgOpen.FileName <> "" Then
+                    strFileName = Path.GetFileNameWithoutExtension(dlgOpen.FileName)
+                    strFilePath = dlgOpen.FileName
+                    strFileExt = Path.GetExtension(strFilePath)
+                    ucrInputPath.SetName(Replace(strFilePath, "\", "/"))
+                    If strFileExt = ".apsimx" OrElse strFileExt = ".apsim" Then
+                        ucrSaveDataframeName.SetName(frmMain.clsRLink.MakeValidText(strFileName))
+                    End If
+                End If
+            End If
+        End Using
+    End Sub
+
+    Private Sub cmdBrowse_Click(sender As Object, e As EventArgs) Handles cmdBrowse.Click
+        GetFileFromOpenDialog()
+    End Sub
+
+    Private Sub ucrChkSilent_ControlValueChanged(ucrChangedControl As ucrCore) Handles ucrChkSilent.ControlValueChanged
+        GetFileFromOpenDialog()
+    End Sub
+
+    Private Sub ucrSaveDataframeName_Load(sender As Object, e As EventArgs) Handles ucrSaveDataframeName.Load
+
     End Sub
 End Class
