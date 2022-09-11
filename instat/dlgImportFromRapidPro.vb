a1edf9c07fc994d038641e3d37420d610eebd48a
lilyclements <lily@idems.international>
2022-09-11 12:39:56 +0300

    Update dlgImportFromRapidPro.vb

--- instat/dlgImportFromRapidPro.vb
+++ instat/dlgImportFromRapidPro.vb
@@ -135,7 +135,7 @@ Public Class dlgImportFromRapidPro
         clsGetUserDataFunction.AddParameter("rapidpro_site", "rapidpror::get_rapidpro_site()", iPosition:=0)
         clsGetUserDataFunction.AddParameter("token", "rapidpror::get_rapidpro_key()", iPosition:=1)
         clsGetUserDataFunction.AddParameter("flatten", "TRUE", iPosition:=2)
-        clsGetUserDataFunction.AddParameter("unlist_variable", "TRUE", iPosition:=3)
+        clsGetUserDataFunction.AddParameter("unlist_consent", "TRUE", iPosition:=3)
 
         clsGetFlowDataFunction.SetPackageName("rapidpror")
         clsGetFlowDataFunction.SetRCommand("get_flow_names")
