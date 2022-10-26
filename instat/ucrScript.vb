cf7f91f960d8e90d69c8598cb42746cd2c0e07fd
lloyddewit <57253949+lloyddewit@users.noreply.github.com>
2022-10-26 13:00:05 +0200

    Update instat/ucrScript.vb

--- instat/ucrScript.vb
+++ instat/ucrScript.vb
@@ -380,7 +380,7 @@ Public Class ucrScript
             txtScript.Paste()
             EnableDisableButtons()
         Else
-            MsgBox(Prompt:="You can only paste text data on the script window.", Title:="Paste to Script Window")
+            MsgBox("You can only paste text data on the script window.", MsgBoxStyle.Exclamation, "Paste to Script Window")
         End If
     End Sub
 
