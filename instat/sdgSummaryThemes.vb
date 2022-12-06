b0dcb6ee9f68f2ab7a255abab6d389060bcbcf83
anastasia-mbithe <anastasiambithe7@gmail.com>
2022-12-06 11:52:44 +0300

    Linking controls to parameters

--- instat/sdgSummaryThemes.vb
+++ instat/sdgSummaryThemes.vb
@@ -18,6 +18,53 @@ Imports instat.Translations
 
 Public Class sdgSummaryThemes
     Private Sub sdgSummaryThemes_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        autoTranslate(Me)
+    End Sub
+
+    Private Sub InitialiseControls()
+        Dim dctBackgroundColor As New Dictionary(Of String, String)
+        Dim dctFontSize As New Dictionary(Of String, String)
+        Dim dctAlignment As New Dictionary(Of String, String)
+        Dim dctWeights As New Dictionary(Of String, String)
+        Dim dctStyles As New Dictionary(Of String, String)
+
+        ucrChkBackgroundColour.SetText("Background Colour")
+        ucrInputBackgroundColour.SetParameter(New RParameter("heading.background.color"))
+        dctBackgroundColor.Add("None", Chr(34) & "none" & Chr(34))
+        dctBackgroundColor.Add("Left", Chr(34) & "left" & Chr(34))
+        dctBackgroundColor.Add("Right", Chr(34) & "right" & Chr(34))
+        dctBackgroundColor.Add("Top", Chr(34) & "top" & Chr(34))
+        dctBackgroundColor.Add("Bottom", Chr(34) & "bottom" & Chr(34))
+        ucrInputBackgroundColour.SetItems(dctBackgroundColor)
+        ucrChkBackgroundColour.AddParameterPresentCondition(True, "heading.background.color")
+        ucrChkBackgroundColour.AddParameterPresentCondition(False, "heading.background.color", False)
+
+        ucrChkAlignment.SetText("Font Size")
+        ucrInputAlignment.SetParameter(New RParameter("heading.align"))
+        dctAlignment.Add("Left", Chr(34) & "left" & Chr(34))
+        dctAlignment.Add("Right", Chr(34) & "right" & Chr(34))
+        dctAlignment.Add("Center", Chr(34) & "center" & Chr(34))
+        ucrInputAlignment.SetItems(dctAlignment)
+        ucrChkAlignment.AddParameterPresentCondition(True, "heading.align")
+        ucrChkAlignment.AddParameterPresentCondition(False, "heading.align", False)
+
+        dctFontSize.Add("Small", Chr(34) & "small" & Chr(34))
+        ucrChkFontSize.SetText("Font Size")
+        ucrInputFontSize.SetParameter(New RParameter("title.font.size"))
+        ucrInputFontSize.SetItems(dctFontSize)
+        ucrChkFontSize.AddParameterPresentCondition(True, "title.font.size")
+        ucrChkFontSize.AddParameterPresentCondition(False, "title.font.size", False)
+
+        dctWeights.Add("Normal", Chr(34) & "normal" & Chr(34))
+        dctWeights.Add("Bold", Chr(34) & "bold" & Chr(34))
+        dctWeights.Add("Lighter", Chr(34) & "lighter" & Chr(34))
+        dctWeights.Add("Bolder", Chr(34) & "bolder" & Chr(34))
+
+        ucrChkFontWeight.SetText("Font Weights")
+        ucrInputFontWeight.SetParameter(New RParameter("title.font.weight"))
+        ucrInputFontWeight.SetItems(dctWeights)
+        ucrChkFontWeight.AddParameterPresentCondition(True, "title.font.weight")
+        ucrChkFontWeight.AddParameterPresentCondition(False, "title.font.weight", False)
 
     End Sub
 End Class
