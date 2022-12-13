be194091439e461e5db29169771c84b8de6a8fb0
Antoine Ntalumeso <antoine.ntalumeso@aims.ac.rw>
2022-12-13 07:19:55 +0300

    added column header type for list

--- instat/Model/DataFrame/clsDataFramePage.vb
+++ instat/Model/DataFrame/clsDataFramePage.vb
@@ -273,7 +273,7 @@ Public Class clsDataFramePage
         ElseIf strHeaderType.Contains("logical") Then
             columnHeader.strTypeShortCode = "(L)"
             ' Structured columns e.g. "circular" are coded with "(S)"
-        ElseIf strHeaderType.Contains("circular") Then
+        ElseIf strHeaderType.Contains("circular") OrElse strHeaderType.Contains("list") Then
             columnHeader.strTypeShortCode = "(S)"
             ' Types of data for specific Application areas e.g. survival are coded with "(A)"
             ' No examples implemented yet.
