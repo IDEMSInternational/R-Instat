93af779bb96a3ef2038d16ac02e368364ed6dc19
patowhiz <conlooptechnologies@gmail.com>
2022-12-19 15:12:12 +0300

    view objects

--- instat/dlgViewObjects.vb
+++ instat/dlgViewObjects.vb
@@ -39,10 +39,6 @@ Public Class dlgViewObjects
     Private Sub InitialiseDialog()
         ucrBase.iHelpTopicID = 349
 
-        ' ucr selector
-        'ucrSelectorForViewObject.SetParameter(New RParameter("data_name", 0))
-        'ucrSelectorForViewObject.SetParameterIsString()
-
         ' ucr receiver
         ucrReceiverSelectedObject.SetParameter(New RParameter("x", 1))
         ucrReceiverSelectedObject.SetParameterIsRFunction()
@@ -73,35 +69,35 @@ Public Class dlgViewObjects
         ucrSelectorForViewObject.Reset()
         rdoPrint.Checked = True
 
-        'set R function for getting and viewing objects
-        'clsGetObjectRFunction.SetRCommand(frmMain.clsRLink.strInstatDataObject & "$get_object_data")
-        'clsGetObjectRFunction.AddParameter("as_file", strParameterValue:="FALSE", iPosition:=3)
-
         'set R function for showing selected object structure
         clsStructureRFunction.SetRCommand("str")
-        'clsStructureRFunction.AddParameter(New RParameter("object", clsGetObjectRFunction, iNewPosition:=0))
-
-        'clsViewObjectRFunction.SetRCommand("view_object_data")
-        'clsViewObjectRFunction.AddParameter("object", clsRFunctionParameter:=clsGetObjectRFunction)
-
         clsPrintRFunction.SetRCommand("print")
-        'clsPrintRFunction.AddParameter("x", clsRFunctionParameter:=clsGetObjectRFunction, iPosition:=0)
 
         'set the base function
         ucrBase.clsRsyntax.SetBaseRFunction(clsPrintRFunction)
     End Sub
 
     Private Sub SetRCodeforControls(bReset As Boolean)
-        'ucrSelectorForViewObject.SetRCode(clsGetObjectRFunction, bReset)
-
         ucrReceiverSelectedObject.AddAdditionalCodeParameterPair(clsStructureRFunction, New RParameter("object", 1))
         ucrReceiverSelectedObject.SetRCode(clsPrintRFunction, bReset)
-        'todo. check
         ucrPnlContentsToView.SetRCode(ucrBase.clsRsyntax.clsBaseFunction, bReset)
     End Sub
 
     Private Sub TestOKEnabled()
         ucrBase.OKEnabled(Not ucrReceiverSelectedObject.IsEmpty)
+    End Sub
+
+    Private Sub ucrPnlContentsToReview_ControlContentsChanged(ucrChangedControl As ucrCore) Handles ucrPnlContentsToView.ControlContentsChanged
+        'set the appropriate Base RFunction
+        If rdoPrint.Checked Then
+            ucrBase.clsRsyntax.SetBaseRFunction(clsPrintRFunction)
+        ElseIf rdoStructure.Checked Then
+            ucrBase.clsRsyntax.SetBaseRFunction(clsStructureRFunction)
+        End If
+    End Sub
+
+    Private Sub ucrReceiverSelectedObject_ControlContentsChanged(ucrChangedControl As ucrCore) Handles ucrReceiverSelectedObject.ControlContentsChanged
+        TestOKEnabled()
     End Sub
 
     Private Sub ucrBase_ClickReset(sender As Object, e As EventArgs) Handles ucrBase.ClickReset
@@ -110,21 +106,5 @@ Public Class dlgViewObjects
         TestOKEnabled()
     End Sub
 
-    Private Sub ucrPnlContentsToReview_ControlContentsChanged(ucrChangedControl As ucrCore) Handles ucrPnlContentsToView.ControlContentsChanged
-        'set the appropriate Base RFunction
-        If rdoPrint.Checked Then
-            'clsViewObjectRFunction.AddParameter("object", clsRFunctionParameter:=clsGetObjectRFunction)
-            'clsViewObjectRFunction.RemoveParameterByName("object_format")
-            ucrBase.clsRsyntax.SetBaseRFunction(clsPrintRFunction)
-        ElseIf rdoStructure.Checked Then
-            'clsViewObjectRFunction.AddParameter("object", clsRFunctionParameter:=clsStructureRFunction)
-            'clsViewObjectRFunction.AddParameter("object_format", strParameterValue:=Chr(34) & RObjectFormat.Text & Chr(34))
-            ucrBase.clsRsyntax.SetBaseRFunction(clsStructureRFunction)
-        End If
-    End Sub
-
-    Private Sub ucrReceiverSelectedObject_ControlContentsChanged(ucrChangedControl As ucrCore) Handles ucrReceiverSelectedObject.ControlContentsChanged
-        TestOKEnabled()
-    End Sub
 
 End Class
