0aa0868206f8a1460f951a100ba4f8322fab9f76
Stephen Lloyd <stephen.lloyd03@gmail.com>
2023-03-29 11:04:17 +0200

    Added image to Log/Script button

--- instat/frmMain.Designer.vb
+++ instat/frmMain.Designer.vb
@@ -3184,7 +3184,7 @@ Partial Class frmMain
         'ToolStripButton1
         '
         Me.ToolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
-        Me.ToolStripButton1.Image = CType(resources.GetObject("ToolStripButton1.Image"), System.Drawing.Image)
+        Me.ToolStripButton1.Image = Global.instat.My.Resources.Resources.scriptwindow
         Me.ToolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta
         Me.ToolStripButton1.Name = "ToolStripButton1"
         Me.ToolStripButton1.Size = New System.Drawing.Size(34, 34)
