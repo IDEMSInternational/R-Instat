f141340657344049ffd08cd5b90079ab05bb9203
Derrick Agorhom <76208189+derekagorhom@users.noreply.github.com>
2023-03-29 12:31:32 +0200

    TestOK Modifications

--- instat/dlgRandomSplit.vb
+++ instat/dlgRandomSplit.vb
@@ -158,36 +158,32 @@ Public Class dlgRandomSplit
     End Sub
 
     Private Sub TestOkEnabled()
-        If rdoSample.Checked Then
-            If ucrChkStratifyingFactor.Checked Then
-                If Not ucrReceiverRanSplit.IsEmpty AndAlso Not ucrNudBreaks.IsEmpty AndAlso Not ucrNudPool.IsEmpty Then
-                    If ucrSaveTrainingData.IsComplete AndAlso ucrSaveTestingData.IsComplete AndAlso Not ucrNudFraction.IsEmpty Then
+        If Not ucrSaveTrainingData.IsComplete Or Not ucrSaveTestingData.IsComplete Or ucrNudFraction.IsEmpty Then
+            ucrBase.OKEnabled(False)
+        Else
+            If rdoSample.Checked Then
+                If ucrChkStratifyingFactor.Checked Then
+                    If Not ucrReceiverRanSplit.IsEmpty AndAlso Not ucrNudBreaks.IsEmpty AndAlso Not ucrNudPool.IsEmpty Then
                         ucrBase.OKEnabled(True)
                     Else
                         ucrBase.OKEnabled(False)
                     End If
                 Else
-                    ucrBase.OKEnabled(False)
+                    ucrBase.OKEnabled(True)
                 End If
             Else
-                ucrBase.OKEnabled(False)
-            End If
-        Else
-            If rdoTimeSeries.Checked Then
+                If rdoTimeSeries.Checked Then
                 If ucrChkLag.Checked Then
-                    If Not ucrNudLag.IsEmpty Then
-                        If ucrSaveTrainingData.IsComplete AndAlso ucrSaveTestingData.IsComplete AndAlso Not ucrNudFraction.IsEmpty Then
+                        If Not ucrNudLag.IsEmpty Then
                             ucrBase.OKEnabled(True)
                         Else
                             ucrBase.OKEnabled(False)
                         End If
                     Else
-                        ucrBase.OKEnabled(False)
+                        ucrBase.OKEnabled(True)
                     End If
-                Else
-                    ucrBase.OKEnabled(False)
-                End If
             End If
+        End If
         End If
     End Sub
 
