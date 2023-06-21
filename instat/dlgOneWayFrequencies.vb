fbdb6341a3f0b893b6e54107543ba22b1a3282b2
patowhiz <conlooptechnologies@gmail.com>
2023-06-21 12:29:58 +0300

    removed unused import

--- instat/dlgOneWayFrequencies.vb
+++ instat/dlgOneWayFrequencies.vb
@@ -13,8 +13,6 @@
 '
 ' You should have received a copy of the GNU General Public License 
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-Imports System.Windows.Media.Media3D
 Imports instat.Translations
 Public Class dlgOneWayFrequencies
     Private bFirstLoad As Boolean = True
