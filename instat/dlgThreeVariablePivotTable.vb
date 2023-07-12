d19e004f667853d8a350b55eae52f500e542dc6d
Sophie Malla Tatchum <sophie.malla@aims-cameroon.org>
2023-07-12 06:42:56 +0100

    minor change

--- instat/dlgThreeVariablePivotTable.vb
+++ instat/dlgThreeVariablePivotTable.vb
@@ -331,7 +331,7 @@ Public Class dlgThreeVariablePivotTable
                     ucrReceiverFactorLevels.Add(strMonthCol, strDataFrame)
                     ucrReceiverInitialColumnFactor.Add(strMonthCol, strDataFrame)
                 End If
-                Dim arrYearAndDay() As String = {strYearCol, strDayCol}
+                Dim arrYearAndDay As String() = New String() {strYearCol, strDayCol}
                 Dim lstItems(arrYearAndDay.Count - 1) As KeyValuePair(Of String, String)
                 Dim i As Integer = 0
                 For Each strColumn As String In arrYearAndDay
