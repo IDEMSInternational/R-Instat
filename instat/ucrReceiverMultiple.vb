35ed68f2e120847eb5cfa57a9e5791b87e8446ea
Derrick Agorhom <76208189+derekagorhom@users.noreply.github.com>
2023-08-23 19:12:59 +0200

    Removing Code changes done on ucrReceiverMultiple

--- instat/ucrReceiverMultiple.vb
+++ instat/ucrReceiverMultiple.vb
@@ -76,7 +76,6 @@ Public Class ucrReceiverMultiple
                 lstSelectedVariables.Items.Remove(objItem)
                 Selector.RemoveFromVariablesList(objItem.Text, objItem.Tag)
             Next
-            lstSelectedVariables.Groups(0).Header = Selector.strCurrentDataFrame & " " & "(" & GetCount() & ")"
         End If
         OnSelectionChanged()
         MyBase.RemoveSelected()
@@ -358,7 +357,6 @@ Public Class ucrReceiverMultiple
                 Selector.AddToVariablesList(kvpTempItem.Value, kvpTempItem.Key)
             End If
         Next
-        grpCurr.Header = lstItems(0).Key & " " & "(" & GetCount() & ")"
         OnSelectionChanged()
     End Sub
 
