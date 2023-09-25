05fe897c5c86724109aaf4fcc074b4d6a6fca171
n-thony <antoine.ntalumeso@gmail.com>
2023-09-25 11:04:11 +0300

    minor change

--- instat/static/InstatObject/R/data_object_R6.R
+++ instat/static/InstatObject/R/data_object_R6.R
@@ -4400,7 +4400,7 @@ DataSheet$set("public", "remove_empty", function(which = c("rows", "cols")) {
   if(self$column_selection_applied()){
     df_with_Selection <- self$get_data_frame()
     df_without_Selection <- self$get_data_frame(use_column_selection = FALSE)
-    # Check for missing columns in B and remove them from A
+    # Check for missing columns in new_df and remove them from df_with_Selection
     missing_columns <- setdiff(names(df_with_Selection), names(new_df))
     if (length(missing_columns) > 0) {
       new_df <- df_without_Selection[, !names(df_without_Selection) %in% missing_columns]
