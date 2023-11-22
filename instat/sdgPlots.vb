73b9d52a216bd3d61fd77f9d25d4ef98fe2a745c
Sophie Malla Tatchum <sophie.malla@aims-cameroon.org>
2023-11-22 10:38:45 +0100

    minor change

--- instat/sdgPlots.vb
+++ instat/sdgPlots.vb
@@ -1355,4 +1355,7 @@ Public Class sdgPlots
     Private Sub ucrChkIncludeTitles_ControlValueChanged(ucrChangedControl As ucrCore) Handles ucrChkIncludeTitles.ControlValueChanged
         AddRemoveLabs()
     End Sub
+    Private Sub ucrBaseSubdialog_ClickReturn(sender As Object, e As EventArgs) Handles ucrBaseSubdialog.ClickReturn
+        dlgGeneralForGraphics.ucrAdditionalLayers.SetRCodeForControl(clsNewBaseOperator:=clsBaseOperator, clsRNewggplotFunc:=clsRggplotFunction, clsNewAesFunc:=clsGlobalAesFunction, strNewGlobalDataFrame:=strDataFrame, bReset:=True)
+    End Sub
 End Class
