50e1ccb91601326153b100e5e0a197c06da6781f
Derrick Agorhom <76208189+derekagorhom@users.noreply.github.com>
2024-02-19 11:43:01 +0100

    Design Changes

--- instat/dlgRestoreBackup.Designer.vb
+++ instat/dlgRestoreBackup.Designer.vb
@@ -240,8 +240,9 @@ Partial Class dlgRestoreBackup
         '
         'dlgRestoreBackup
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+        Me.AutoSize = True
         Me.ClientSize = New System.Drawing.Size(901, 359)
         Me.Controls.Add(Me.ucrBase)
         Me.Controls.Add(Me.grpEndChoice)
