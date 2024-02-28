e747775c6460b641d0e77f9d0d240417a04493e0
JUMA <JUMA@BEEB-CORP>
2024-02-28 18:38:07 +0300

    final

--- instat/dlgBarAndPieChart.vb
+++ instat/dlgBarAndPieChart.vb
@@ -1060,6 +1060,7 @@ Public Class dlgBarAndPieChart
             If ucrReceiverX.IsEmpty Then
                 clsBarAesFunction.AddParameter("x", Chr(34) & Chr(34), iPosition:=0)
                 clsPieAesFunction.AddParameter("x", Chr(34) & Chr(34), iPosition:=0)
+                clsGeomLollipopAesFunction.AddParameter("x", Chr(34) & Chr(34), iPosition:=0)
             End If
             If ucrVariablesAsFactorForBarChart.IsEmpty Then
                 clsBarAesFunction.AddParameter("y", Chr(34) & Chr(34), iPosition:=1)
