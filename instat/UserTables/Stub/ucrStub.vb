ffc52291bcfbf3c7daad56f6ad6d8dae37d90200
patowhiz <conlooptechnologies@gmail.com>
2024-05-30 15:27:49 +0300

    Stub design changes

--- instat/UserTables/Stub/ucrStub.vb
+++ instat/UserTables/Stub/ucrStub.vb
@@ -1,4 +1,11 @@
-﻿Public Class ucrStub
+﻿Imports Antlr.Runtime
+
+Public Class ucrStub
+
+    Private clsOperator As New ROperator
+    Private clsStubHeadRParameter As RParameter
+    Private clsGtRFunction As New RFunction
+    Private bFirstload As Boolean = True
 
     Private Sub initialiseDialog()
         ucrReceiverSingleRowName.SetParameter(New RParameter("rowname_col", 0))
@@ -11,13 +18,55 @@
         ucrReceiverSingleGroupByCol.Selector = ucrSelectorCols
         ucrReceiverSingleGroupByCol.SetLinkedDisplayControl(lblGroupByCol)
 
-
-        'ucrInputHeaderTitle.SetParameter(New RParameter("title_text_param", 0, bNewIncludeArgumentName:=False))
+        ucrInputStubHead.SetParameter(New RParameter("label", 0))
 
     End Sub
 
     Public Sub Setup(strDataFrameName As String, clsOperator As ROperator)
 
+        If 1 = 1 Then
+            Exit Sub
+        End If
+
+        If bFirstload Then
+            initialiseDialog()
+            bFirstload = False
+        End If
+
+        Me.clsOperator = clsOperator
+
+        Dim lstRParams As List(Of RParameter)
+
+        lstRParams = clsTablesUtils.FindRFunctionsParamsWithRCommand("gt", clsOperator)
+
+        ' The GT paramter should always be there. 
+        If lstRParams.Count > 0 Then
+            clsGtRFunction = lstRParams(0).clsArgumentCodeStructure
+            ucrReceiverSingleRowName.SetRCode(clsGtRFunction, True, bCloneIfNeeded:=True)
+            ucrReceiverSingleGroupByCol.SetRCode(clsGtRFunction, True, bCloneIfNeeded:=True)
+        End If
+
+
+        lstRParams = clsTablesUtils.FindRFunctionsParamsWithRCommand("tab_stubhead", clsOperator)
+        If lstRParams.Count > 0 Then
+            clsStubHeadRParameter = lstRParams(0)
+        Else
+            Dim clstabStubHeadFunction As New RFunction
+            clstabStubHeadFunction.SetPackageName("gt")
+            clstabStubHeadFunction.SetRCommand("tab_stubhead")
+            clsStubHeadRParameter = New RParameter(strParameterName:="tab_stubhead_param", strParamValue:=clstabStubHeadFunction, bNewIncludeArgumentName:=False)
+        End If
+
+
+    End Sub
+
+    Private Sub ucrInputStubHead_ControlValueChanged(ucrChangedControl As ucrCore) Handles ucrInputStubHead.ControlValueChanged
+        If ucrInputStubHead.IsEmpty Then
+            clsOperator.RemoveParameter(clsStubHeadRParameter)
+        Else
+            clsStubHeadRParameter.clsArgumentCodeStructure.AddParameter("label", strParameterValue:=clsTablesUtils.GetStringValue(ucrInputStubHead.GetValue(), True)
+            clsOperator.AddParameter(clsStubHeadRParameter)
+        End If
     End Sub
 
 
