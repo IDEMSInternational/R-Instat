31320d3e542af46306ffa916874167d0771023d7
Derrick Agorhom <76208189+derekagorhom@users.noreply.github.com>
2024-09-12 14:48:08 +0200

    unticked the NTotal checkbox when opening the column summaries subdialog

--- instat/dlgColumnStats.vb
+++ instat/dlgColumnStats.vb
@@ -112,7 +112,6 @@ Public Class dlgColumnStats
 
         clsSummariesList.SetRCommand("c")
         clsSummariesList.AddParameter("summary_count_non_missing", Chr(34) & "summary_count_non_missing" & Chr(34), bIncludeArgumentName:=False, iPosition:=1)
-        clsSummariesList.AddParameter("summary_count", Chr(34) & "summary_count" & Chr(34), bIncludeArgumentName:=False, iPosition:=3)
         clsSummariesList.AddParameter("summary_sum", Chr(34) & "summary_sum" & Chr(34), bIncludeArgumentName:=False, iPosition:=11)
 
         clsDefaultFunction.SetRCommand(frmMain.clsRLink.strInstatDataObject & "$calculate_summary")
