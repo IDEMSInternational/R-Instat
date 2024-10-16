1a60e3be1a7837161cfd1718685cb37a075dbd2c
Ntalumeso <antoine.ntalumeso@idems.international>
2024-10-16 18:24:33 +0300

    set the deault column to 100

--- instat/clsInstatOptionsDefaults.vb
+++ instat/clsInstatOptionsDefaults.vb
@@ -33,7 +33,7 @@ Public Class clsInstatOptionsDefaults
     Public Shared ReadOnly DEFAULTiPreviewRows As Integer = 10
     Public Shared ReadOnly DEFAULTiMaxRows As Integer = 1000
     Public Shared ReadOnly DEFAULTiMaxCols As Integer = 50
-    Public Shared ReadOnly DEFAULTiUndoColLimit As Integer = 0
+    Public Shared ReadOnly DEFAULTiUndoColLimit As Integer = 100
     Public Shared ReadOnly DEFAULTiUndoRowLimit As Integer = 100000
     Public Shared ReadOnly DEFAULTstrComment As String = "Dialog:"
     Public Shared ReadOnly DEFAULTstrGraphDisplayOption As String = "view_output_window"
