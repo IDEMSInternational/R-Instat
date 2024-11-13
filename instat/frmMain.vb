f9ae18bc83df1cdf28f56c35e742608ca4eb7506
Derrick Agorhom <76208189+derekagorhom@users.noreply.github.com>
2024-11-13 11:06:56 +0100

    Code changes

--- instat/frmMain.vb
+++ instat/frmMain.vb
@@ -852,7 +852,7 @@ Public Class frmMain
     End Sub
 
     Private Sub mnuHelpHelp_Click(sender As Object, e As EventArgs)
-        Help.ShowHelp(Me, Me.strStaticPath & "/" & Me.strHelpFilePath, HelpNavigator.TopicId, "135")
+        Help.ShowHelp(Me, strStaticPath & "\" & strHelpFilePath, HelpNavigator.TopicId, "135")
     End Sub
 
     Private Sub mnuTbHelp_Click(sender As Object, e As EventArgs) Handles mnuTbHelp.ButtonClick, mnuToolBarHelp.Click
@@ -860,15 +860,15 @@ Public Class frmMain
     End Sub
 
     Private Sub mnuDataWindowHelp_Click(sender As Object, e As EventArgs) Handles mnuDataWindowHelp.Click
-        Help.ShowHelp(Me, Me.strStaticPath & "/" & Me.strHelpFilePath, HelpNavigator.TopicId, "134")
+        Help.ShowHelp(Me, strStaticPath & "\" & strHelpFilePath, HelpNavigator.TopicId, "134")
     End Sub
 
     Private Sub mnuOutputHelp_Click(sender As Object, e As EventArgs) Handles mnuOutputHelp.Click
-        Help.ShowHelp(Me, Me.strStaticPath & "/" & Me.strHelpFilePath, HelpNavigator.TopicId, "540")
+        Help.ShowHelp(Me, strStaticPath & "\" & strHelpFilePath, HelpNavigator.TopicId, "540")
     End Sub
 
     Private Sub mnuLogScriptHelp_Click(sender As Object, e As EventArgs) Handles mnuLogScriptHelp.Click
-        Help.ShowHelp(Me, Me.strStaticPath & "/" & Me.strHelpFilePath, HelpNavigator.TopicId, "542")
+        Help.ShowHelp(Me, strStaticPath & "\" & strHelpFilePath, HelpNavigator.TopicId, "542")
     End Sub
 
     Private Sub mnuPrepareFactorRecode_Click(sender As Object, e As EventArgs) Handles mnuPrepareColumnFactorRecodeFactor.Click
