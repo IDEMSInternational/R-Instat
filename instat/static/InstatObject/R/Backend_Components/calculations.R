e9aeee288887b64d5cdcde8bb2ce7c2d116dbca8
lilyclements <lily@idems.international>
2025-02-16 19:43:12 +0000

    Removing code for testing

--- instat/static/InstatObject/R/Backend_Components/calculations.R
+++ instat/static/InstatObject/R/Backend_Components/calculations.R
@@ -1,11 +1,2 @@
 library(instatCalculations)
 library(databook)
-
-# Todo: Remove these - are these set elsewhere
-
-# Calculation labels
-#These are the names of the list returned by apply_instat_calculation
-c_data_label <- "data"
-c_link_label <- "link"
-c_has_summary_label <- "has_summary"
-c_has_filter_label <- "has_filter"
