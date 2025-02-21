bff1c0adb5fa3d9f71405f6639998ebaabdc45c5
lilyclements <lily@idems.international>
2025-02-21 12:03:10 +0000

    adding certain functions to the environment for old RDS files

--- instat/static/InstatObject/R/Backend_Components/calculations.R
+++ instat/static/InstatObject/R/Backend_Components/calculations.R
@@ -1,2 +1,5 @@
-library(instatCalculations)
-library(databook)
+# to run on load due to issues with old RDS files
+calculation <- instatCalculations::calculation
+get_data_book_output_object_names <- databook:::get_data_book_output_object_names
+get_data_book_scalar_names <- databook:::get_data_book_scalar_names
+overall_label="[Overall]"
