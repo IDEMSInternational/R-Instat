754faacd9abf8fb124a54584b3d8078da35dbd42
Stephen Lloyd <stephen.lloyd03@gmail.com>
2025-03-10 10:56:35 +0100

    added find replace options to script window right mouse button menu

--- instat/ucrScript.Designer.vb
+++ instat/ucrScript.Designer.vb
@@ -77,6 +77,11 @@ Partial Class ucrScript
         Me.cmdRunStatementSelection = New System.Windows.Forms.Button()
         Me.TabControl = New System.Windows.Forms.TabControl()
         Me.toolTipScriptWindow = New System.Windows.Forms.ToolTip(Me.components)
+        Me.ToolStripSeparator6 = New System.Windows.Forms.ToolStripSeparator()
+        Me.mnuFindNext = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuFindPrev = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuReplace = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuReplaceAll = New System.Windows.Forms.ToolStripMenuItem()
         Me.mnuContextScript.SuspendLayout()
         Me.tlpTableContainer.SuspendLayout()
         Me.Panel.SuspendLayout()
@@ -86,9 +91,9 @@ Partial Class ucrScript
         'mnuContextScript
         '
         Me.mnuContextScript.ImageScalingSize = New System.Drawing.Size(24, 24)
-        Me.mnuContextScript.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuUndo, Me.mnuRedo, Me.ToolStripSeparator1, Me.mnuCut, Me.mnuCopy, Me.mnuPaste, Me.mnuSelectAll, Me.mnuClear, Me.ToolStripSeparator2, Me.mnuRunCurrentStatementSelection, Me.mnuRunAllText, Me.ToolStripSeparator5, Me.mnuReformatCode, Me.ToolStripSeparator3, Me.mnuOpenScriptasFile, Me.mnuInsertScript, Me.mnuLoadScriptFromFile, Me.mnuSaveScript, Me.ToolStripSeparator4, Me.mnuHelp})
+        Me.mnuContextScript.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuUndo, Me.mnuRedo, Me.ToolStripSeparator1, Me.mnuCut, Me.mnuCopy, Me.mnuPaste, Me.mnuSelectAll, Me.mnuClear, Me.ToolStripSeparator2, Me.mnuFindNext, Me.mnuFindPrev, Me.mnuReplace, Me.mnuReplaceAll, Me.ToolStripSeparator6, Me.mnuRunCurrentStatementSelection, Me.mnuRunAllText, Me.ToolStripSeparator5, Me.mnuReformatCode, Me.ToolStripSeparator3, Me.mnuOpenScriptasFile, Me.mnuInsertScript, Me.mnuLoadScriptFromFile, Me.mnuSaveScript, Me.ToolStripSeparator4, Me.mnuHelp})
         Me.mnuContextScript.Name = "mnuContextLogFile"
-        Me.mnuContextScript.Size = New System.Drawing.Size(306, 364)
+        Me.mnuContextScript.Size = New System.Drawing.Size(306, 480)
         '
         'mnuUndo
         '
@@ -377,6 +382,45 @@ Partial Class ucrScript
         Me.TabControl.Size = New System.Drawing.Size(664, 441)
         Me.TabControl.TabIndex = 1
         '
+        'ToolStripSeparator6
+        '
+        Me.ToolStripSeparator6.Name = "ToolStripSeparator6"
+        Me.ToolStripSeparator6.Size = New System.Drawing.Size(302, 6)
+        '
+        'mnuFindNext
+        '
+        Me.mnuFindNext.Name = "mnuFindNext"
+        Me.mnuFindNext.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.F), System.Windows.Forms.Keys)
+        Me.mnuFindNext.Size = New System.Drawing.Size(305, 22)
+        Me.mnuFindNext.Text = "Find Next"
+        Me.mnuFindNext.ToolTipText = "Highlights the next occurrence of the selected text, and moves the cursor there"
+        '
+        'mnuFindPrev
+        '
+        Me.mnuFindPrev.Name = "mnuFindPrev"
+        Me.mnuFindPrev.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.P), System.Windows.Forms.Keys)
+        Me.mnuFindPrev.Size = New System.Drawing.Size(305, 22)
+        Me.mnuFindPrev.Text = "Find Prev"
+        Me.mnuFindPrev.ToolTipText = "Highlights the previous occurrence of the selected text, and moves the cursor the" &
+    "re"
+        '
+        'mnuReplace
+        '
+        Me.mnuReplace.Name = "mnuReplace"
+        Me.mnuReplace.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.R), System.Windows.Forms.Keys)
+        Me.mnuReplace.Size = New System.Drawing.Size(305, 22)
+        Me.mnuReplace.Text = "Replace"
+        Me.mnuReplace.ToolTipText = "Replaces the next occurrence of the selected text with the contents of the clipbo" &
+    "ard, and moves the cursor there"
+        '
+        'mnuReplaceAll
+        '
+        Me.mnuReplaceAll.Name = "mnuReplaceAll"
+        Me.mnuReplaceAll.Size = New System.Drawing.Size(305, 22)
+        Me.mnuReplaceAll.Text = "Replace All"
+        Me.mnuReplaceAll.ToolTipText = "Replaces all occurrences of the selected text with the contents of the clipboard," &
+    " and moves the cursor to the last occurrence replaced"
+        '
         'ucrScript
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
@@ -432,4 +476,9 @@ Partial Class ucrScript
     Friend WithEvents contextMenuStriptInsert As ContextMenuStrip
     Friend WithEvents toolStripMenuItemInsertStatement As ToolStripMenuItem
     Friend WithEvents toolStripMenuItemInsertCommentUncomment As ToolStripMenuItem
+    Friend WithEvents mnuFindNext As ToolStripMenuItem
+    Friend WithEvents mnuFindPrev As ToolStripMenuItem
+    Friend WithEvents mnuReplace As ToolStripMenuItem
+    Friend WithEvents mnuReplaceAll As ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator6 As ToolStripSeparator
 End Class
--- instat/ucrScript.vb
+++ instat/ucrScript.vb
@@ -29,6 +29,24 @@ Public Class ucrScript
     Private clsRScript As RScript = Nothing
     Private strRInstatLogFilesFolderPath As String = Path.Combine(Path.GetFullPath(FileIO.SpecialDirectories.MyDocuments), "R-Instat_Log_files")
 
+    Private ReadOnly Property IsFindValid As Boolean
+        Get
+            Dim bScriptExists As Boolean = clsScriptActive.TextLength > 0
+            Dim bTextSelected As Boolean = clsScriptActive.SelectedText.Length > 0
+
+            Return bScriptExists AndAlso bTextSelected
+        End Get
+    End Property
+
+    Private ReadOnly Property IsReplaceValid As Boolean
+        Get
+            Dim bClipoardContainsText As Boolean = Clipboard.ContainsData(DataFormats.Text)
+            Dim bIsScriptTab As Boolean = TabControl.SelectedIndex <> iTabIndexLog
+
+            Return IsFindValid AndAlso bIsScriptTab AndAlso bClipoardContainsText
+        End Get
+    End Property
+
     Friend WithEvents clsScriptActive As Scintilla
     Friend WithEvents clsScriptLog As Scintilla
 
@@ -330,6 +348,10 @@ Public Class ucrScript
         mnuPaste.Enabled = bEnable
         mnuSelectAll.Enabled = bEnable
         mnuClear.Enabled = bEnable
+        mnuFindNext.Enabled = bEnable
+        mnuFindPrev.Enabled = bEnable
+        mnuReplace.Enabled = bEnable
+        mnuReplaceAll.Enabled = bEnable
         mnuRunCurrentStatementSelection.Enabled = bEnable
         mnuRunAllText.Enabled = bEnable
         mnuLoadScriptFromFile.Enabled = bEnable
@@ -824,13 +846,19 @@ Public Class ucrScript
             mnuUndo.Enabled = clsScriptActive.CanUndo
             mnuRedo.Enabled = clsScriptActive.CanRedo
             mnuCut.Enabled = bScriptSelected
-            mnuCopy.Enabled = bScriptSelected
             mnuPaste.Enabled = Clipboard.ContainsData(DataFormats.Text)
             mnuClear.Enabled = bScriptExists
             mnuLoadScriptFromFile.Enabled = True
         End If
 
+        'enable find/replace options
+        mnuFindNext.Enabled = IsFindValid
+        mnuFindPrev.Enabled = IsFindValid
+        mnuReplace.Enabled = IsReplaceValid
+        mnuReplaceAll.Enabled = IsReplaceValid
+
         'enable remaining options based on tab state
+        mnuCopy.Enabled = bScriptSelected
         mnuSelectAll.Enabled = bScriptExists
         mnuRunCurrentStatementSelection.Enabled = bScriptExists
         mnuRunAllText.Enabled = bScriptExists
@@ -870,6 +898,34 @@ Public Class ucrScript
         End If
         clsScriptActive.ClearAll()
         EnableDisableButtons()
+    End Sub
+
+    Private Sub mnuFindNext_Click(sender As Object, e As EventArgs) Handles mnuFindNext.Click
+        If Not IsFindValid Then
+            Exit Sub
+        End If
+
+    End Sub
+
+    Private Sub mnuFindPrev_Click(sender As Object, e As EventArgs) Handles mnuFindPrev.Click
+        If Not IsFindValid Then
+            Exit Sub
+        End If
+
+    End Sub
+
+    Private Sub mnuReplace_Click(sender As Object, e As EventArgs) Handles mnuReplace.Click
+        If Not IsReplaceValid Then
+            Exit Sub
+        End If
+
+    End Sub
+
+    Private Sub mnuReplaceAll_Click(sender As Object, e As EventArgs) Handles mnuReplaceAll.Click
+        If Not IsReplaceValid Then
+            Exit Sub
+        End If
+
     End Sub
 
     Private Sub mnuHelp_Click(sender As Object, e As EventArgs) Handles mnuHelp.Click, cmdHelp.Click
