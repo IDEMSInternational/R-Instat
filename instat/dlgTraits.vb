b34a78e4a1a915c011cbe01319f7aa1d72937925
Ntalumeso <antoine.ntalumeso@idems.international>
2025-04-07 11:08:20 +0300

    trait data type

--- instat/dlgTraits.vb
+++ instat/dlgTraits.vb
@@ -15,6 +15,59 @@
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 Imports instat.Translations
+
 Public Class dlgTraits
+    Private bFirstLoad As Boolean = True
+    Private bReset As Boolean = True
+    Private Sub dlgTraits_Load(sender As Object, e As EventArgs) Handles Me.Load
+        If bFirstLoad Then
+            InitialiseDialog()
+            bFirstLoad = False
+        End If
+        If bReset Then
+            SetDefaults()
+        End If
+        SetRCodeForControls(bReset)
+        bReset = False
+        TestOKEnabled()
+        autoTranslate(Me)
+    End Sub
+
+    Private Sub InitialiseDialog()
+        ucrBase.clsRsyntax.bExcludeAssignedFunctionOutput = False
+
+        ucrTraitGraphSelector.SetParameter(New RParameter("data_name", 0))
+        ucrTraitGraphSelector.SetParameterIsString()
+
+        ucrReceiverTrait.SetParameter(New RParameter("", 1))
+        ucrReceiverTrait.Selector = ucrTraitGraphSelector
+        ucrReceiverTrait.SetParameterIsString()
+        ucrReceiverTrait.strSelectorHeading = "Traits"
+        ucrReceiverTrait.SetIncludedDataTypes({"traits"})
+    End Sub
+
+    Private Sub SetDefaults()
+
+
+        ucrTraitGraphSelector.Reset()
+        ucrReceiverTrait.SetMeAsReceiver()
+    End Sub
+
+    Private Sub SetRCodeForControls(bReset As Boolean)
+    End Sub
+
+    Private Sub TestOKEnabled()
+        ucrBase.OKEnabled(Not ucrReceiverTrait.IsEmpty)
+    End Sub
+
+    Private Sub ucrBase_ClickReset(sender As Object, e As EventArgs) Handles ucrBase.ClickReset
+        SetDefaults()
+        SetRCodeForControls(True)
+        TestOKEnabled()
+    End Sub
+
+    Private Sub ucrCoreControls_ControlContentsChanged(ucrChangedControl As ucrCore) Handles ucrTraitGraphSelector.ControlContentsChanged, ucrReceiverTrait.ControlContentsChanged
+        TestOKEnabled()
+    End Sub
 
 End Class
