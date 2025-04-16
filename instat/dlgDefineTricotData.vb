41acb36dcd43bb289ff28abc352f284b3b8e4b5b
Ntalumeso <antoine.ntalumeso@idems.international>
2025-04-16 11:49:58 +0300

    implementing the define as tricot functions

--- instat/dlgDefineTricotData.vb
+++ instat/dlgDefineTricotData.vb
@@ -22,6 +22,8 @@ Public Class dlgDefineTricotData
     Private bReset As Boolean = True
     Private lstReceiversLevelID, lstReceiversVarityLevel, lstReceiversIDVarietyLevel As New List(Of ucrReceiverSingle)
     Private lstRecognisedTypes As New List(Of KeyValuePair(Of String, List(Of String)))
+    Private clsDATIDLevelFunction, clsDATVarietyLevelFunction, clsDATIDVarietyFunction As New RFunction
+    Private clsCDATIDLevelFunction, cslCDATVarietyLevelFunction, clsCDATIDLevelVarietiesFunction, clsCDATIDLevelTraitsFunction As New RFunction
 
     Private Sub dlgDefineTricotData_Load(sender As Object, e As EventArgs) Handles Me.Load
         autoTranslate(Me)
@@ -43,7 +45,7 @@ Public Class dlgDefineTricotData
         Dim kvpLatitude As KeyValuePair(Of String, List(Of String)) = New KeyValuePair(Of String, List(Of String))("latitude", {"lat", "latitude", "Lat", "Latitude"}.ToList())
         Dim kvpPlantingDate As KeyValuePair(Of String, List(Of String)) = New KeyValuePair(Of String, List(Of String))("planting_date", {"planting_date", "Planting_date", "plantingdate"}.ToList())
         Dim kvpOverAll As KeyValuePair(Of String, List(Of String)) = New KeyValuePair(Of String, List(Of String))("overall", {"overall", "overall_perf", "Overall"}.ToList())
-        Dim kvpVariety As KeyValuePair(Of String, List(Of String)) = New KeyValuePair(Of String, List(Of String))("varieties", {"item", "items", "variety", "varieties"}.ToList())
+        Dim kvpVariety As KeyValuePair(Of String, List(Of String)) = New KeyValuePair(Of String, List(Of String))("variety", {"item", "items", "variety", "varieties"}.ToList())
 
 
         lstRecognisedTypes.AddRange({kvpID, kvpLongitude, kvpLatitude, kvpPlantingDate, kvpOverAll, kvpVariety})
@@ -51,21 +53,78 @@ Public Class dlgDefineTricotData
         lstReceiversVarityLevel.AddRange({ucrReceiverVarietyLevelVariety})
         lstReceiversIDVarietyLevel.AddRange({ucrReceiverIDVarietyLevelID, ucrReceiverIDVarietyLevelVariety})
 
+        ucrSelectorIDLevelData.SetParameter(New RParameter("data_name", 0))
+        ucrSelectorIDLevelData.SetParameterIsString()
 
+        'ID Level Data (run by the top groupbox)
+        ucrReceiverLevelOverallTraits.SetParameter(New RParameter("overall_trait", 5))
         ucrReceiverLevelOverallTraits.Selector = ucrSelectorIDLevelData
-        ucrReceiverLevelVarieties.Selector = ucrSelectorIDLevelData
+        ucrReceiverLevelOverallTraits.SetParameterIsString()
+        ucrReceiverLevelOverallTraits.strSelectorHeading = "Variables"
+        'ucrReceiverLevelOverallTraits.SetIncludedDataTypes({"numeric"})
 
+        ucrReceiverLevelVarieties.SetParameter(New RParameter("varieties", 1))
+        ucrReceiverLevelVarieties.Selector = ucrSelectorIDLevelData
+        ucrReceiverLevelVarieties.SetParameterIsString()
+        ucrReceiverLevelVarieties.strSelectorHeading = "Variables"
         SetRSelector(ucrSelectorIDLevelData, lstReceiversLevelID)
 
+        'Variety Level Data (run by the middle groupbox)
+        ucrSelectorVarietyLevelData.SetParameter(New RParameter("data_name", 0))
+        ucrSelectorVarietyLevelData.SetParameterIsString()
+
+        ucrReceiverVarietyLevelVariety.SetParameter(New RParameter("key_col_names", 1))
         ucrReceiverVarietyLevelVariety.Selector = ucrSelectorVarietyLevelData
+        ucrReceiverVarietyLevelVariety.SetParameterIsString()
+        ucrReceiverVarietyLevelVariety.strSelectorHeading = "Id"
+
+
+        'ID-Variety Level Data (run by the middle groupbox)
+        ucrSelectorIDVarietyLevel.SetParameter(New RParameter("data_name", 0))
+        ucrSelectorIDVarietyLevel.SetParameterIsString()
 
         SetRSelector(ucrSelectorIDVarietyLevel, lstReceiversIDVarietyLevel)
         ucrReceiverIDVarietyLevelTraits.Selector = ucrSelectorIDVarietyLevel
+        ucrReceiverIDVarietyLevelTraits.SetParameter(New RParameter("traits", 2))
+        ucrReceiverIDVarietyLevelTraits.Selector = ucrSelectorIDVarietyLevel
+        ucrReceiverIDVarietyLevelTraits.SetParameterIsString()
+        ucrReceiverIDVarietyLevelTraits.strSelectorHeading = "Id"
 
     End Sub
 
     Private Sub SetDefaults()
+        clsDATIDLevelFunction = New RFunction
+        clsDATIDVarietyFunction = New RFunction
+        clsDATVarietyLevelFunction = New RFunction
+        clsCDATIDLevelFunction = New RFunction
+        clsCDATIDLevelVarietiesFunction = New RFunction
+        clsCDATIDLevelTraitsFunction = New RFunction
+        cslCDATVarietyLevelFunction = New RFunction
+
         ucrReceiverLevelID.SetMeAsReceiver()
+
+        clsCDATIDLevelFunction.SetRCommand("c")
+        clsCDATIDLevelVarietiesFunction.SetRCommand("c")
+        cslCDATVarietyLevelFunction.SetRCommand("c")
+        clsCDATIDLevelTraitsFunction.SetRCommand("c")
+
+        clsDATIDLevelFunction.SetRCommand("define_as_tricot")
+        clsDATIDLevelFunction.AddParameter("types", clsRFunctionParameter:=clsCDATIDLevelFunction, iPosition:=2)
+        clsDATIDLevelFunction.AddParameter("auto_selection", "TRUE", iPosition:=4)
+
+        clsDATVarietyLevelFunction.SetRCommand("define_as_tricot")
+        clsDATVarietyLevelFunction.AddParameter("types", clsRFunctionParameter:=cslCDATVarietyLevelFunction, iPosition:=2)
+        clsDATVarietyLevelFunction.AddParameter("auto_selection", "TRUE", iPosition:=3)
+
+        clsDATIDVarietyFunction.SetRCommand("define_as_tricot")
+        clsDATIDVarietyFunction.AddParameter("key_col_names", clsRFunctionParameter:=clsCDATIDLevelTraitsFunction, iPosition:=1)
+        clsDATIDVarietyFunction.AddParameter("types", clsRFunctionParameter:=clsCDATIDLevelVarietiesFunction, iPosition:=2)
+        clsDATIDVarietyFunction.AddParameter("auto_selection", "TRUE", iPosition:=3)
+
+        ucrBase.clsRsyntax.ClearCodes()
+        ucrBase.clsRsyntax.SetBaseRFunction(clsDATIDLevelFunction)
+        ucrBase.clsRsyntax.AddToAfterCodes(clsDATVarietyLevelFunction, 1)
+        ucrBase.clsRsyntax.AddToAfterCodes(clsDATIDVarietyFunction, 2)
 
         AutoFillReceivers(ucrSelectorIDLevelData, lstReceiversLevelID)
         AutoFillReceivers(ucrSelectorVarietyLevelData, lstReceiversVarityLevel)
@@ -73,7 +132,26 @@ Public Class dlgDefineTricotData
     End Sub
 
     Private Sub SetRCodeForControls(bReset As Boolean)
+        ucrReceiverLevelID.AddAdditionalCodeParameterPair(clsDATIDLevelFunction, New RParameter("key_col_names", 1), iAdditionalPairNo:=1)
+        ucrReceiverVarietyLevelVariety.AddAdditionalCodeParameterPair(cslCDATVarietyLevelFunction, New RParameter("variety", 0), iAdditionalPairNo:=1)
+        ucrReceiverIDVarietyLevelID.AddAdditionalCodeParameterPair(clsCDATIDLevelTraitsFunction, New RParameter("id", 0, bNewIncludeArgumentName:=False), iAdditionalPairNo:=1)
+        ucrReceiverIDVarietyLevelVariety.AddAdditionalCodeParameterPair(clsCDATIDLevelTraitsFunction, New RParameter("variety", 1, bNewIncludeArgumentName:=False), iAdditionalPairNo:=1)
 
+        ucrSelectorIDLevelData.SetRCode(clsDATIDLevelFunction, bReset)
+        ucrReceiverLevelID.SetRCode(clsCDATIDLevelFunction, bReset)
+        ucrReceiverLevelLatitude.SetRCode(clsCDATIDLevelFunction, bReset)
+        ucrReceiverLevelLongitude.SetRCode(clsCDATIDLevelFunction, bReset)
+        ucrReceiverLevelPlantingDate.SetRCode(clsCDATIDLevelFunction, bReset)
+        ucrReceiverLevelOverallTraits.SetRCode(clsCDATIDLevelFunction, bReset)
+        ucrReceiverLevelVarieties.SetRCode(clsCDATIDLevelFunction, bReset)
+
+        ucrSelectorVarietyLevelData.SetRCode(clsDATVarietyLevelFunction, bReset)
+        ucrReceiverVarietyLevelVariety.SetRCode(clsDATVarietyLevelFunction, bReset)
+
+        ucrSelectorIDVarietyLevel.SetRCode(clsDATIDVarietyFunction, bReset)
+        ucrReceiverIDVarietyLevelID.SetRCode(clsCDATIDLevelVarietiesFunction, bReset)
+        ucrReceiverIDVarietyLevelVariety.SetRCode(clsCDATIDLevelVarietiesFunction, bReset)
+        ucrReceiverIDVarietyLevelTraits.SetRCode(clsCDATIDLevelVarietiesFunction, bReset)
     End Sub
 
     Private Sub SetRSelector(sender As ucrSelectorByDataFrameAddRemove, lstReceivers As List(Of ucrReceiverSingle))
