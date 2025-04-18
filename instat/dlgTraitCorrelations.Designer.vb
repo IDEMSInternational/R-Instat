eadc86b49fe8e680885202f081f8ecdf1680ef25
KWAMBAI VITALIS <vitaliskwambai@gmail.com>
2025-04-18 12:03:43 +0300

    CHANGES

--- instat/dlgTraitCorrelations.Designer.vb
+++ instat/dlgTraitCorrelations.Designer.vb
@@ -23,21 +23,219 @@ Partial Class dlgTraitCorrelations
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.ucrBase = New instat.ucrButtons()
+        Me.ucrSelecetorTraits = New instat.ucrSelectorByDataFrameAddRemove()
+        Me.ucrReceiverTrait = New instat.ucrReceiverSingle()
+        Me.lblTraits = New System.Windows.Forms.Label()
+        Me.ucrReceiverTraitsToCompare = New instat.ucrReceiverMultiple()
+        Me.lblTraitsCompare = New System.Windows.Forms.Label()
+        Me.ucrChkLeadingZeros = New instat.ucrCheck()
+        Me.lblDecimalPlaces = New System.Windows.Forms.Label()
+        Me.ucrNudDecimalPlaces = New instat.ucrNud()
+        Me.grpDisplayOptions = New System.Windows.Forms.GroupBox()
+        Me.grpOutput = New System.Windows.Forms.GroupBox()
+        Me.rdoAsDataFrame = New System.Windows.Forms.RadioButton()
+        Me.rdoAsText = New System.Windows.Forms.RadioButton()
+        Me.ucrPnlOutput = New instat.UcrPanel()
+        Me.ucrChkIncludePValues = New instat.ucrCheck()
+        Me.ucrChkDisplayOptions = New instat.ucrCheck()
+        Me.ucrSaveCorrelation = New instat.ucrSave()
+        Me.grpDisplayOptions.SuspendLayout()
+        Me.grpOutput.SuspendLayout()
         Me.SuspendLayout()
         '
         'ucrBase
         '
         Me.ucrBase.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
-        Me.ucrBase.Location = New System.Drawing.Point(23, 225)
+        Me.ucrBase.Location = New System.Drawing.Point(12, 463)
         Me.ucrBase.Name = "ucrBase"
         Me.ucrBase.Size = New System.Drawing.Size(410, 52)
         Me.ucrBase.TabIndex = 0
+        '
+        'ucrSelecetorTraits
+        '
+        Me.ucrSelecetorTraits.AutoSize = True
+        Me.ucrSelecetorTraits.bDropUnusedFilterLevels = False
+        Me.ucrSelecetorTraits.bShowHiddenColumns = False
+        Me.ucrSelecetorTraits.bUseCurrentFilter = True
+        Me.ucrSelecetorTraits.Location = New System.Drawing.Point(12, 27)
+        Me.ucrSelecetorTraits.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrSelecetorTraits.Name = "ucrSelecetorTraits"
+        Me.ucrSelecetorTraits.Size = New System.Drawing.Size(213, 183)
+        Me.ucrSelecetorTraits.TabIndex = 1
+        '
+        'ucrReceiverTrait
+        '
+        Me.ucrReceiverTrait.AutoSize = True
+        Me.ucrReceiverTrait.frmParent = Me
+        Me.ucrReceiverTrait.Location = New System.Drawing.Point(331, 74)
+        Me.ucrReceiverTrait.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverTrait.Name = "ucrReceiverTrait"
+        Me.ucrReceiverTrait.Selector = Nothing
+        Me.ucrReceiverTrait.Size = New System.Drawing.Size(152, 24)
+        Me.ucrReceiverTrait.strNcFilePath = ""
+        Me.ucrReceiverTrait.TabIndex = 2
+        Me.ucrReceiverTrait.ucrSelector = Nothing
+        '
+        'lblTraits
+        '
+        Me.lblTraits.AutoSize = True
+        Me.lblTraits.Location = New System.Drawing.Point(335, 56)
+        Me.lblTraits.Name = "lblTraits"
+        Me.lblTraits.Size = New System.Drawing.Size(67, 13)
+        Me.lblTraits.TabIndex = 3
+        Me.lblTraits.Text = "Overall Trait:"
+        '
+        'ucrReceiverTraitsToCompare
+        '
+        Me.ucrReceiverTraitsToCompare.AutoSize = True
+        Me.ucrReceiverTraitsToCompare.frmParent = Me
+        Me.ucrReceiverTraitsToCompare.Location = New System.Drawing.Point(333, 124)
+        Me.ucrReceiverTraitsToCompare.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverTraitsToCompare.Name = "ucrReceiverTraitsToCompare"
+        Me.ucrReceiverTraitsToCompare.Selector = Nothing
+        Me.ucrReceiverTraitsToCompare.Size = New System.Drawing.Size(120, 100)
+        Me.ucrReceiverTraitsToCompare.strNcFilePath = ""
+        Me.ucrReceiverTraitsToCompare.TabIndex = 4
+        Me.ucrReceiverTraitsToCompare.ucrSelector = Nothing
+        '
+        'lblTraitsCompare
+        '
+        Me.lblTraitsCompare.AutoSize = True
+        Me.lblTraitsCompare.Location = New System.Drawing.Point(335, 106)
+        Me.lblTraitsCompare.Name = "lblTraitsCompare"
+        Me.lblTraitsCompare.Size = New System.Drawing.Size(113, 13)
+        Me.lblTraitsCompare.TabIndex = 5
+        Me.lblTraitsCompare.Text = "Traits To Compare To:"
+        '
+        'ucrChkLeadingZeros
+        '
+        Me.ucrChkLeadingZeros.AutoSize = True
+        Me.ucrChkLeadingZeros.Checked = False
+        Me.ucrChkLeadingZeros.Location = New System.Drawing.Point(22, 329)
+        Me.ucrChkLeadingZeros.Name = "ucrChkLeadingZeros"
+        Me.ucrChkLeadingZeros.Size = New System.Drawing.Size(132, 23)
+        Me.ucrChkLeadingZeros.TabIndex = 29
+        '
+        'lblDecimalPlaces
+        '
+        Me.lblDecimalPlaces.AutoSize = True
+        Me.lblDecimalPlaces.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.lblDecimalPlaces.Location = New System.Drawing.Point(22, 299)
+        Me.lblDecimalPlaces.Name = "lblDecimalPlaces"
+        Me.lblDecimalPlaces.Size = New System.Drawing.Size(83, 13)
+        Me.lblDecimalPlaces.TabIndex = 25
+        Me.lblDecimalPlaces.Text = "Decimal Places:"
+        '
+        'ucrNudDecimalPlaces
+        '
+        Me.ucrNudDecimalPlaces.AutoSize = True
+        Me.ucrNudDecimalPlaces.DecimalPlaces = New Decimal(New Integer() {0, 0, 0, 0})
+        Me.ucrNudDecimalPlaces.Increment = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.ucrNudDecimalPlaces.Location = New System.Drawing.Point(145, 295)
+        Me.ucrNudDecimalPlaces.Maximum = New Decimal(New Integer() {100, 0, 0, 0})
+        Me.ucrNudDecimalPlaces.Minimum = New Decimal(New Integer() {0, 0, 0, 0})
+        Me.ucrNudDecimalPlaces.Name = "ucrNudDecimalPlaces"
+        Me.ucrNudDecimalPlaces.Size = New System.Drawing.Size(54, 33)
+        Me.ucrNudDecimalPlaces.TabIndex = 26
+        Me.ucrNudDecimalPlaces.Value = New Decimal(New Integer() {0, 0, 0, 0})
+        '
+        'grpDisplayOptions
+        '
+        Me.grpDisplayOptions.Controls.Add(Me.grpOutput)
+        Me.grpDisplayOptions.Location = New System.Drawing.Point(14, 276)
+        Me.grpDisplayOptions.Name = "grpDisplayOptions"
+        Me.grpDisplayOptions.Size = New System.Drawing.Size(426, 121)
+        Me.grpDisplayOptions.TabIndex = 24
+        Me.grpDisplayOptions.TabStop = False
+        Me.grpDisplayOptions.Tag = "Display Options"
+        Me.grpDisplayOptions.Text = "Display Options"
+        '
+        'grpOutput
+        '
+        Me.grpOutput.Controls.Add(Me.rdoAsDataFrame)
+        Me.grpOutput.Controls.Add(Me.rdoAsText)
+        Me.grpOutput.Controls.Add(Me.ucrPnlOutput)
+        Me.grpOutput.Location = New System.Drawing.Point(251, 15)
+        Me.grpOutput.Name = "grpOutput"
+        Me.grpOutput.Size = New System.Drawing.Size(165, 75)
+        Me.grpOutput.TabIndex = 11
+        Me.grpOutput.TabStop = False
+        Me.grpOutput.Text = "Output "
+        '
+        'rdoAsDataFrame
+        '
+        Me.rdoAsDataFrame.AutoSize = True
+        Me.rdoAsDataFrame.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.rdoAsDataFrame.Location = New System.Drawing.Point(6, 43)
+        Me.rdoAsDataFrame.Name = "rdoAsDataFrame"
+        Me.rdoAsDataFrame.Size = New System.Drawing.Size(123, 17)
+        Me.rdoAsDataFrame.TabIndex = 2
+        Me.rdoAsDataFrame.Text = "Save As Data Frame"
+        Me.rdoAsDataFrame.UseVisualStyleBackColor = True
+        '
+        'rdoAsText
+        '
+        Me.rdoAsText.AutoSize = True
+        Me.rdoAsText.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.rdoAsText.Location = New System.Drawing.Point(6, 20)
+        Me.rdoAsText.Name = "rdoAsText"
+        Me.rdoAsText.Size = New System.Drawing.Size(95, 17)
+        Me.rdoAsText.TabIndex = 1
+        Me.rdoAsText.Text = "Save As Table"
+        Me.rdoAsText.UseVisualStyleBackColor = True
+        '
+        'ucrPnlOutput
+        '
+        Me.ucrPnlOutput.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.ucrPnlOutput.Location = New System.Drawing.Point(3, 16)
+        Me.ucrPnlOutput.Name = "ucrPnlOutput"
+        Me.ucrPnlOutput.Size = New System.Drawing.Size(150, 53)
+        Me.ucrPnlOutput.TabIndex = 0
+        '
+        'ucrChkIncludePValues
+        '
+        Me.ucrChkIncludePValues.AutoSize = True
+        Me.ucrChkIncludePValues.Checked = False
+        Me.ucrChkIncludePValues.Location = New System.Drawing.Point(22, 363)
+        Me.ucrChkIncludePValues.Name = "ucrChkIncludePValues"
+        Me.ucrChkIncludePValues.Size = New System.Drawing.Size(149, 23)
+        Me.ucrChkIncludePValues.TabIndex = 30
+        '
+        'ucrChkDisplayOptions
+        '
+        Me.ucrChkDisplayOptions.AutoSize = True
+        Me.ucrChkDisplayOptions.Checked = False
+        Me.ucrChkDisplayOptions.Location = New System.Drawing.Point(15, 244)
+        Me.ucrChkDisplayOptions.Name = "ucrChkDisplayOptions"
+        Me.ucrChkDisplayOptions.Size = New System.Drawing.Size(192, 23)
+        Me.ucrChkDisplayOptions.TabIndex = 34
+        '
+        'ucrSaveCorrelation
+        '
+        Me.ucrSaveCorrelation.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.ucrSaveCorrelation.Location = New System.Drawing.Point(15, 413)
+        Me.ucrSaveCorrelation.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.ucrSaveCorrelation.Name = "ucrSaveCorrelation"
+        Me.ucrSaveCorrelation.Size = New System.Drawing.Size(266, 24)
+        Me.ucrSaveCorrelation.TabIndex = 35
         '
         'dlgTraitCorrelations
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(492, 289)
+        Me.ClientSize = New System.Drawing.Size(492, 532)
+        Me.Controls.Add(Me.ucrSaveCorrelation)
+        Me.Controls.Add(Me.ucrChkDisplayOptions)
+        Me.Controls.Add(Me.ucrChkIncludePValues)
+        Me.Controls.Add(Me.ucrChkLeadingZeros)
+        Me.Controls.Add(Me.lblDecimalPlaces)
+        Me.Controls.Add(Me.ucrNudDecimalPlaces)
+        Me.Controls.Add(Me.grpDisplayOptions)
+        Me.Controls.Add(Me.lblTraitsCompare)
+        Me.Controls.Add(Me.ucrReceiverTraitsToCompare)
+        Me.Controls.Add(Me.lblTraits)
+        Me.Controls.Add(Me.ucrReceiverTrait)
+        Me.Controls.Add(Me.ucrSelecetorTraits)
         Me.Controls.Add(Me.ucrBase)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.MaximizeBox = False
@@ -45,9 +243,29 @@ Partial Class dlgTraitCorrelations
         Me.Name = "dlgTraitCorrelations"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Trait Correlations"
+        Me.grpDisplayOptions.ResumeLayout(False)
+        Me.grpOutput.ResumeLayout(False)
+        Me.grpOutput.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents ucrBase As ucrButtons
+    Friend WithEvents ucrSelecetorTraits As ucrSelectorByDataFrameAddRemove
+    Friend WithEvents ucrReceiverTrait As ucrReceiverSingle
+    Friend WithEvents lblTraitsCompare As Label
+    Friend WithEvents ucrReceiverTraitsToCompare As ucrReceiverMultiple
+    Friend WithEvents lblTraits As Label
+    Friend WithEvents ucrChkIncludePValues As ucrCheck
+    Friend WithEvents ucrChkLeadingZeros As ucrCheck
+    Friend WithEvents lblDecimalPlaces As Label
+    Friend WithEvents ucrNudDecimalPlaces As ucrNud
+    Friend WithEvents grpDisplayOptions As GroupBox
+    Friend WithEvents grpOutput As GroupBox
+    Friend WithEvents rdoAsDataFrame As RadioButton
+    Friend WithEvents rdoAsText As RadioButton
+    Friend WithEvents ucrPnlOutput As UcrPanel
+    Friend WithEvents ucrChkDisplayOptions As ucrCheck
+    Friend WithEvents ucrSaveCorrelation As ucrSave
 End Class
--- instat/dlgTraitCorrelations.vb
+++ instat/dlgTraitCorrelations.vb
@@ -1,3 +1,306 @@
-﻿Public Class dlgTraitCorrelations
+﻿' R- Instat
+' Copyright (C) 2015-2017
+'
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+'
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License 
+' along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Imports instat.Translations
+Public Class dlgTraitCorrelations
+    Private bFirstload As Boolean = True
+    Private bReset As Boolean = True
+    Private bRcodeSet As Boolean = True
+    Private clsGetVariablesMetadataFunction, clsDummyFunction, clsCombineVarsFunction, clsFashionFunction, clsGetObjectFunction, clsGetRankingItemsFunction, clsNamesFunction, clsMapDfrFunction, clsKendallTauFunction, clsMutateFunction, clsSelectFunction As New RFunction
+    Private clsBaseLineOperator, clsObjectOperator, clsPipe2Operator, clsPipeOperator, clsNamesOperator, clsMultivarsOpeator As New ROperator
+
+    Private Sub dlgTraitCorrelations_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If bFirstload Then
+            InitialiseDialog()
+            bFirstload = False
+        End If
+        If bReset Then
+            SetDefaults()
+        End If
+        SetRCodeForControls(bReset)
+        bReset = False
+        autoTranslate(Me)
+        TestOkEnabled()
+    End Sub
+
+    Private Sub InitialiseDialog()
+
+        ucrReceiverTrait.SetParameterIsRFunction()
+        ucrReceiverTrait.Selector = ucrSelecetorTraits
+
+        ucrReceiverTraitsToCompare.SetParameterIsRFunction()
+        ucrReceiverTraitsToCompare.Selector = ucrSelecetorTraits
+
+        ucrSelecetorTraits.SetParameterIsrfunction()
+
+        ucrNudDecimalPlaces.SetParameter(New RParameter("decimals", 1))
+        ucrNudDecimalPlaces.SetMinMax(0, 5)
+        ucrNudDecimalPlaces.Increment = 1
+        ucrNudDecimalPlaces.SetRDefault(2)
+        ucrNudDecimalPlaces.SetLinkedDisplayControl(lblDecimalPlaces)
+
+        ucrChkLeadingZeros.SetParameter(New RParameter("leading_zeros", 2))
+        ucrChkLeadingZeros.SetText("Leading Zeros")
+        ucrChkLeadingZeros.SetValuesCheckedAndUnchecked("TRUE", "FALSE")
+
+        ucrChkIncludePValues.SetText("Include P-Values")
+        ucrChkIncludePValues.AddParameterValuesCondition(True, "value", "False")
+        ucrChkIncludePValues.AddParameterValuesCondition(False, "value", "True")
+
+        ucrPnlOutput.AddRadioButton(rdoAsDataFrame)
+        ucrPnlOutput.AddRadioButton(rdoAsText)
+        ucrPnlOutput.AddParameterValuesCondition(rdoAsText, "output", "as.table")
+        ucrPnlOutput.AddParameterValuesCondition(rdoAsDataFrame, "output", "as.dataframe")
+
+        ucrChkDisplayOptions.SetText("Display Options")
+        ucrChkDisplayOptions.AddParameterValuesCondition(True, "display", "True")
+        ucrChkDisplayOptions.AddParameterValuesCondition(False, "display", "False")
+
+        ucrChkDisplayOptions.AddToLinkedControls({ucrChkLeadingZeros, ucrChkIncludePValues, ucrNudDecimalPlaces, ucrPnlOutput}, {True}, bNewLinkedHideIfParameterMissing:=True)
+        ucrChkIncludePValues.SetLinkedDisplayControl(grpDisplayOptions)
+        ucrPnlOutput.SetLinkedDisplayControl(grpOutput)
+
+        ucrSaveCorrelation.SetDataFrameSelector(ucrSelecetorTraits.ucrAvailableDataFrames)
+        ucrSaveCorrelation.SetIsComboBox()
+
+    End Sub
+
+    Private Sub SetDefaults()
+        clsGetVariablesMetadataFunction = New RFunction
+        clsNamesFunction = New RFunction
+        clsMutateFunction = New RFunction
+        clsMapDfrFunction = New RFunction
+        clsGetObjectFunction = New RFunction
+        clsKendallTauFunction = New RFunction
+        clsFashionFunction = New RFunction
+        clsCombineVarsFunction = New RFunction
+        clsDummyFunction = New RFunction
+        clsBaseLineOperator = New ROperator
+        clsMultivarsOpeator = New ROperator
+        clsObjectOperator = New ROperator
+        clsNamesOperator = New ROperator
+        clsPipeOperator = New ROperator
+        clsPipe2Operator = New ROperator
+
+        ucrSelecetorTraits.Reset()
+        ucrSaveCorrelation.Reset()
+        ucrReceiverTrait.SetMeAsReceiver()
+
+        clsDummyFunction.AddParameter("display", "False", iPosition:=0)
+        clsDummyFunction.AddParameter("output", "as.table", iPosition:=1)
+
+
+        clsGetVariablesMetadataFunction.SetRCommand(frmMain.clsRLink.strInstatDataObject & "get_variables_metadata")
+        clsGetVariablesMetadataFunction.SetAssignTo("get_index_names")
+
+        clsGetObjectFunction.SetRCommand(frmMain.clsRLink.strInstatDataObject & "get_object")
+        clsGetObjectFunction.AddParameter("object_name", Chr(34) & "rankings_list" & Chr(34), iPosition:=1)
+
+        clsObjectOperator.SetOperation("$")
+        clsObjectOperator.AddParameter("left", clsRFunctionParameter:=clsGetObjectFunction, iPosition:=0)
+        clsObjectOperator.AddParameter("right", "object", iPosition:=1)
+        clsObjectOperator.SetAssignTo("rankings_object")
+
+        clsBaseLineOperator.SetOperation("<-", bBracketsTemp:=False)
+        clsBaseLineOperator.SetAssignTo("baseline")
+
+        clsMultivarsOpeator.SetOperation("<-", bBracketsTemp:=False)
+        clsMultivarsOpeator.SetAssignTo("multiple_vars")
+
+        clsCombineVarsFunction.SetRCommand("c")
+        clsCombineVarsFunction.AddParameter("var1", "baseline", iPosition:=0, bIncludeArgumentName:=False)
+        clsCombineVarsFunction.AddParameter("var2", "multiple_vars", iPosition:=1, bIncludeArgumentName:=False)
+        clsCombineVarsFunction.SetAssignTo("all_vars ")
+
+        clsGetRankingItemsFunction.SetRCommand("get_ranking_items")
+        clsGetRankingItemsFunction.AddParameter("data", clsRFunctionParameter:=clsGetVariablesMetadataFunction, iPosition:=0)
+        clsGetRankingItemsFunction.AddParameter("vars_to_get", clsRFunctionParameter:=clsCombineVarsFunction, iPosition:=1)
+        clsGetRankingItemsFunction.AddParameter("index ", Chr(34) & "rankings_index" & Chr(34), iPosition:=2)
+        clsGetRankingItemsFunction.AddParameter("data_object", clsRFunctionParameter:=clsGetObjectFunction, iPosition:=1, bIncludeArgumentName:=False)
+        clsGetRankingItemsFunction.SetAssignTo("compare_variables ")
+
+        clsNamesOperator.SetOperation("", bBracketsTemp:=False)
+        clsNamesOperator.AddParameter("vars", clsRFunctionParameter:=clsCombineVarsFunction, bIncludeArgumentName:=False)
+        clsNamesOperator.SetAssignTo("names(compare_variables)")
+
+        clsKendallTauFunction.SetPackageName("~ gosset")
+        clsKendallTauFunction.SetRCommand("kendallTau")
+        clsKendallTauFunction.AddParameter("x", "compare_variables[[.x]]", iPosition:=0)
+        clsKendallTauFunction.AddParameter("y", "compare_variables[[baseline]]", iPosition:=1)
+
+
+        clsMapDfrFunction.SetPackageName("purrr")
+        clsMapDfrFunction.SetRCommand("map_dfr")
+        clsMapDfrFunction.AddParameter(".x", "multiple_vars", iPosition:=0)
+        clsMapDfrFunction.AddParameter("kendal", clsRFunctionParameter:=clsKendallTauFunction, iPosition:=1, bIncludeArgumentName:=False)
+
+        clsMutateFunction.SetPackageName("dplyr")
+        clsMutateFunction.SetRCommand("mutate")
+        clsMutateFunction.AddParameter("trait", "multiple_vars", iPosition:=0)
+        clsMutateFunction.AddParameter(".before", "everything()", iPosition:=0)
+
+        clsPipeOperator.SetOperation("%>%")
+        clsPipeOperator.AddParameter("left", clsRFunctionParameter:=clsMapDfrFunction, iPosition:=0)
+        clsPipeOperator.AddParameter("right", clsRFunctionParameter:=clsMutateFunction, iPosition:=1)
+        clsPipeOperator.SetAssignTo("kendall_rankings")
+
+        clsSelectFunction.SetPackageName("dplyr")
+        clsSelectFunction.SetRCommand("select")
+
+
+        clsFashionFunction.SetPackageName("corrr")
+        clsFashionFunction.SetRCommand("fashion")
+        clsFashionFunction.SetAssignTo("kendall_rankings")
+
+        clsPipe2Operator.SetOperation("%>%")
+        clsPipe2Operator.SetAssignTo("kendall_rankings")
+
+
+    End Sub
+
+    Private Sub SetRCodeForControls(bReset As Boolean)
+
+        ucrNudDecimalPlaces.SetRCode(clsFashionFunction, bReset)
+        ucrChkLeadingZeros.SetRCode(clsFashionFunction, bReset)
+        ucrPnlOutput.SetRCode(clsDummyFunction, bReset)
+        ucrChkDisplayOptions.SetRCode(clsDummyFunction, bReset)
+        ucrSaveCorrelation.AddAdditionalRCode(clsPipeOperator, iAdditionalPairNo:=1)
+
+
+
+    End Sub
+
+    Private Sub TestOkEnabled()
+
+    End Sub
+
+    Private Sub ucrBase_ClickReset(sender As Object, e As EventArgs) Handles ucrBase.ClickReset
+        SetDefaults()
+        SetRCodeForControls(True)
+        TestOkEnabled()
+    End Sub
+
+    Private Sub ucrSelecetorTraits_ControlValueChanged(ucrChangedControl As ucrCore) Handles ucrSelecetorTraits.ControlValueChanged
+        Dim DataFrame As String = ucrSelecetorTraits.strCurrentDataFrame
+
+        clsGetVariablesMetadataFunction.AddParameter("data", Chr(34) & DataFrame & Chr(34), iPosition:=0, bIncludeArgumentName:=False)
+        clsGetObjectFunction.AddParameter("data_name", Chr(34) & DataFrame & Chr(34), iPosition:=0)
+    End Sub
+
+    Private Sub ucrReceiverTraitsToCompare_ControlValueChanged(ucrChangedControl As ucrCore) Handles ucrReceiverTraitsToCompare.ControlValueChanged
+        clsMultivarsOpeator.AddParameter("data", ucrReceiverTraitsToCompare.GetVariableNames, iPosition:=0, bIncludeArgumentName:=False)
+    End Sub
+
+    Private Sub ucrReceiverTrait_ControlValueChanged(ucrChangedControl As ucrCore) Handles ucrReceiverTrait.ControlValueChanged
+        clsBaseLineOperator.AddParameter("var", ucrReceiverTrait.GetVariableNames, iPosition:=0, bIncludeArgumentName:=False)
+    End Sub
+
+    Private Sub ucrChkIncludePValues_ControlValueChanged(ucrChangedControl As ucrCore) Handles ucrChkIncludePValues.ControlValueChanged
+        If ucrChkIncludePValues.Checked Then
+            clsPipe2Operator.AddParameter("left", clsROperatorParameter:=clsPipeOperator, iPosition:=0)
+            clsSelectFunction.AddParameter("select", "-c(`Pr(>|z|)`", iPosition:=0, bIncludeArgumentName:=False)
+            clsSelectFunction.AddParameter("z", "`Zvalue`", iPosition:=1, bIncludeArgumentName:=False)
+            clsPipe2Operator.AddParameter("right", clsRFunctionParameter:=clsSelectFunction, iPosition:=1)
+        Else
+            clsPipe2Operator.RemoveParameterByName("left")
+            clsPipe2Operator.RemoveParameterByName("right")
+            clsSelectFunction.RemoveParameterByName("select")
+            clsSelectFunction.RemoveParameterByName("z")
+        End If
+    End Sub
+
+    Private Sub AddingDecimals()
+        If ucrChkDisplayOptions.Checked Then
+            If Not ucrNudDecimalPlaces.IsEmpty Then
+                clsFashionFunction.AddParameter("decimals", ucrNudDecimalPlaces.GetText, iPosition:=1)
+            Else
+                clsFashionFunction.RemoveParameterByName("decimals")
+            End If
+        End If
+    End Sub
+
+    Private Sub AddingLeadingZeros()
+        If ucrChkDisplayOptions.Checked Then
+            If ucrChkLeadingZeros.Checked Then
+                clsFashionFunction.AddParameter("leading_zeros", "TRUE", iPosition:=1)
+            Else
+                clsFashionFunction.RemoveParameterByName("leading_zeros")
+            End If
+        End If
+    End Sub
+    Private Sub ucrNudDecimalPlaces_ControlValueChanged(ucrChangedControl As ucrCore) Handles ucrNudDecimalPlaces.ControlValueChanged
+        AddingDecimals()
+    End Sub
+
+    Private Sub ucrChkDisplayOptions_ControlValueChanged(ucrChangedControl As ucrCore) Handles ucrChkDisplayOptions.ControlValueChanged, ucrPnlOutput.ControlValueChanged
+        AddingDecimals()
+        AddingLeadingZeros()
+        ChangeBaseFunction()
+        ChangeOutputObject()
+    End Sub
+
+    Private Sub ucrChkLeadingZeros_ControlValueChanged(ucrChangedControl As ucrCore) Handles ucrChkLeadingZeros.ControlValueChanged
+        AddingLeadingZeros()
+    End Sub
+
+    Private Sub ChangeOutputObject()
+        If ucrChkDisplayOptions.Checked Then
+            If rdoAsDataFrame.Checked Then
+                clsDummyFunction.AddParameter("output", "as.table", iPosition:=2)
+                ucrSaveCorrelation.SetSaveType(strRObjectType:=RObjectTypeLabel.Table, strRObjectFormat:=RObjectFormat.Text)
+                ucrSaveCorrelation.SetCheckBoxText("Store Table")
+                ucrSaveCorrelation.SetPrefix("summary_table")
+                ucrSaveCorrelation.SetAssignToIfUncheckedValue("last_table")
+                clsFashionFunction.SetAssignToOutputObject(strRObjectToAssignTo:="last_table",
+                                        strRObjectTypeLabelToAssignTo:=RObjectTypeLabel.Table,
+                                        strRObjectFormatToAssignTo:=RObjectFormat.Text,
+                                        strRDataFrameNameToAddObjectTo:=ucrSelecetorTraits.strCurrentDataFrame,
+                                        strObjectName:="last_table")
+            Else
+                clsDummyFunction.AddParameter("output", "as.dataframe", iPosition:=2)
+                clsPipeOperator.RemoveAssignTo()
+                ucrSaveCorrelation.SetSaveTypeAsDataFrame()
+                ucrSaveCorrelation.SetCheckBoxText("Store Data Frame")
+                ucrSaveCorrelation.SetPrefix("data_frame")
+                ucrSaveCorrelation.SetAssignToIfUncheckedValue("last_dataframe")
+            End If
+        Else
+            ucrSaveCorrelation.SetSaveType(strRObjectType:=RObjectTypeLabel.Table, strRObjectFormat:=RObjectFormat.Text)
+            ucrSaveCorrelation.SetCheckBoxText("Store Table")
+            ucrSaveCorrelation.SetPrefix("summary_table")
+            ucrSaveCorrelation.SetAssignToIfUncheckedValue("last_table")
+            clsPipeOperator.SetAssignToOutputObject(strRObjectToAssignTo:="last_table",
+                                       strRObjectTypeLabelToAssignTo:=RObjectTypeLabel.Table,
+                                       strRObjectFormatToAssignTo:=RObjectFormat.Text,
+                                       strRDataFrameNameToAddObjectTo:=ucrSelecetorTraits.strCurrentDataFrame,
+                                       strObjectName:="last_table")
+        End If
+    End Sub
+
+    Private Sub ChangeBaseFunction()
+        If bRcodeSet Then
+            If ucrChkDisplayOptions.Checked Then
+                ucrBase.clsRsyntax.SetBaseRFunction(clsFashionFunction)
+                If rdoAsDataFrame.Checked Then
+                    ucrBase.clsRsyntax.iCallType = 0
+                End If
+            Else
+                ucrBase.clsRsyntax.SetBaseROperator(clsPipeOperator)
+            End If
+        End If
+    End Sub
 
 End Class
