285da8c45378d5f9354428896a23b40ea5c730b8
lilyclements <lily@idems.international>
2025-04-28 21:44:25 +0300

    Adding check to R dialog

--- instat/dlgTransformTricotData.Designer.vb
+++ instat/dlgTransformTricotData.Designer.vb
@@ -34,31 +34,33 @@ Partial Class dlgTransformTricotData
         '
         Me.ucrReceiverTricotData.AutoSize = True
         Me.ucrReceiverTricotData.frmParent = Me
-        Me.ucrReceiverTricotData.Location = New System.Drawing.Point(292, 52)
+        Me.ucrReceiverTricotData.Location = New System.Drawing.Point(391, 92)
         Me.ucrReceiverTricotData.Margin = New System.Windows.Forms.Padding(0)
         Me.ucrReceiverTricotData.Name = "ucrReceiverTricotData"
         Me.ucrReceiverTricotData.Selector = Nothing
-        Me.ucrReceiverTricotData.Size = New System.Drawing.Size(123, 110)
+        Me.ucrReceiverTricotData.Size = New System.Drawing.Size(184, 154)
         Me.ucrReceiverTricotData.strNcFilePath = ""
         Me.ucrReceiverTricotData.TabIndex = 6
         Me.ucrReceiverTricotData.ucrSelector = Nothing
         '
         'cmdCheckTricotData
         '
-        Me.cmdCheckTricotData.Location = New System.Drawing.Point(7, 252)
+        Me.cmdCheckTricotData.Location = New System.Drawing.Point(10, 307)
+        Me.cmdCheckTricotData.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.cmdCheckTricotData.Name = "cmdCheckTricotData"
-        Me.cmdCheckTricotData.Size = New System.Drawing.Size(101, 23)
+        Me.cmdCheckTricotData.Size = New System.Drawing.Size(152, 35)
         Me.cmdCheckTricotData.TabIndex = 7
-        Me.cmdCheckTricotData.Text = "Check:"
+        Me.cmdCheckTricotData.Text = "Check"
         Me.cmdCheckTricotData.UseVisualStyleBackColor = True
         '
         'ucrBase
         '
         Me.ucrBase.AutoSize = True
         Me.ucrBase.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
-        Me.ucrBase.Location = New System.Drawing.Point(7, 310)
+        Me.ucrBase.Location = New System.Drawing.Point(10, 396)
+        Me.ucrBase.Margin = New System.Windows.Forms.Padding(9, 9, 9, 9)
         Me.ucrBase.Name = "ucrBase"
-        Me.ucrBase.Size = New System.Drawing.Size(408, 52)
+        Me.ucrBase.Size = New System.Drawing.Size(611, 77)
         Me.ucrBase.TabIndex = 8
         '
         'ucrInputTricotData
@@ -67,35 +69,37 @@ Partial Class dlgTransformTricotData
         Me.ucrInputTricotData.AutoSize = True
         Me.ucrInputTricotData.IsMultiline = False
         Me.ucrInputTricotData.IsReadOnly = True
-        Me.ucrInputTricotData.Location = New System.Drawing.Point(116, 254)
+        Me.ucrInputTricotData.Location = New System.Drawing.Point(174, 310)
+        Me.ucrInputTricotData.Margin = New System.Windows.Forms.Padding(14, 14, 14, 14)
         Me.ucrInputTricotData.Name = "ucrInputTricotData"
-        Me.ucrInputTricotData.Size = New System.Drawing.Size(183, 21)
+        Me.ucrInputTricotData.Size = New System.Drawing.Size(274, 32)
         Me.ucrInputTricotData.TabIndex = 9
         '
         'cmdOptions
         '
-        Me.cmdOptions.Location = New System.Drawing.Point(314, 252)
+        Me.cmdOptions.Location = New System.Drawing.Point(471, 307)
+        Me.cmdOptions.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.cmdOptions.Name = "cmdOptions"
-        Me.cmdOptions.Size = New System.Drawing.Size(101, 23)
+        Me.cmdOptions.Size = New System.Drawing.Size(152, 35)
         Me.cmdOptions.TabIndex = 10
-        Me.cmdOptions.Text = "Options:"
+        Me.cmdOptions.Text = "Options"
         Me.cmdOptions.UseVisualStyleBackColor = True
         '
         'ucrSelectorTricotData
         '
         Me.ucrSelectorTricotData.AutoSize = True
         Me.ucrSelectorTricotData.bShowHiddenColumns = False
-        Me.ucrSelectorTricotData.Location = New System.Drawing.Point(18, 52)
+        Me.ucrSelectorTricotData.Location = New System.Drawing.Point(10, 49)
         Me.ucrSelectorTricotData.Margin = New System.Windows.Forms.Padding(0)
         Me.ucrSelectorTricotData.Name = "ucrSelectorTricotData"
-        Me.ucrSelectorTricotData.Size = New System.Drawing.Size(218, 147)
+        Me.ucrSelectorTricotData.Size = New System.Drawing.Size(327, 226)
         Me.ucrSelectorTricotData.TabIndex = 11
         '
         'dlgTransformTricotData
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(464, 385)
+        Me.ClientSize = New System.Drawing.Size(630, 485)
         Me.Controls.Add(Me.ucrSelectorTricotData)
         Me.Controls.Add(Me.cmdOptions)
         Me.Controls.Add(Me.ucrInputTricotData)
@@ -103,6 +107,7 @@ Partial Class dlgTransformTricotData
         Me.Controls.Add(Me.cmdCheckTricotData)
         Me.Controls.Add(Me.ucrReceiverTricotData)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.MinimizeBox = False
         Me.Name = "dlgTransformTricotData"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
--- instat/dlgTransformTricotData.vb
+++ instat/dlgTransformTricotData.vb
@@ -19,10 +19,12 @@ Imports instat.Translations
 Imports RDotNet
 
 Public Class dlgTransformTricotData
-    Private clsOutputDataLevel, clsCreateTricotData As New RFunction
+    Private clsOutputDataLevel, clsCheckDataLevel, clsCreateTricotData As New RFunction
     Private clsOutputLevelsOperator, OverallSymbolOperator As New ROperator
     Private bFirstLoad As Boolean = True
     Private bReset As Boolean = True
+    Private bResetSubdialog As Boolean = True
+    Private bUniqueChecked As Boolean = False
 
     Private Sub dlgTransformTricotData_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         If bFirstLoad Then
@@ -33,11 +35,12 @@ Public Class dlgTransformTricotData
             SetDefaults()
         End If
         SetRCodeForControls(bReset)
+        bUniqueChecked = False
     End Sub
 
     Private Sub InitialiseDialog()
         ucrReceiverTricotData.SetParameter(New RParameter("data_list", 0))
-        ucrReceiverTricotData.SetParameterIsRFunction()
+        ucrReceiverTricotData.SetParameterIsString()
         ucrReceiverTricotData.Selector = ucrSelectorTricotData
         ucrReceiverTricotData.strSelectorHeading = "Data Frames"
         ucrReceiverTricotData.SetItemType("dataframe")
@@ -48,6 +51,10 @@ Public Class dlgTransformTricotData
         clsCreateTricotData = New RFunction
         clsOutputLevelsOperator = New ROperator
         OverallSymbolOperator = New ROperator
+        bUniqueChecked = False
+        'ucrSelectorTricotData.Reset()
+        'ucrReceiverTricotData.SetMeAsReceiver()
+        'bResetSubdialog = True
 
         ucrReceiverTricotData.SetMeAsReceiver()
         clsOutputDataLevel.SetPackageName("instatExtras")
@@ -67,7 +74,6 @@ Public Class dlgTransformTricotData
         OverallSymbolOperator.AddParameter("right", "1,]", iPosition:=1)
         OverallSymbolOperator.bSpaceAroundOperation = False
 
-        ucrBase.clsRsyntax.AddToBeforeCodes(clsOutputLevelsOperator)
         ucrBase.clsRsyntax.SetBaseRFunction(clsCreateTricotData)
     End Sub
 
@@ -82,23 +88,63 @@ Public Class dlgTransformTricotData
     End Sub
 
     Private Sub TestOKEnabled()
-        If ucrReceiverTricotData.IsEmpty Then
-            ucrBase.OKEnabled(False)
-        Else
-            ucrBase.OKEnabled(True)
-        End If
+        ucrBase.OKEnabled(Not ucrReceiverTricotData.IsEmpty AndAlso bUniqueChecked)
     End Sub
 
-    Private Sub cmdFindForms_click(sender As Object, e As EventArgs) Handles cmdCheckTricotData.Click
+
+    Private Sub cmdCheckTricotData_click(sender As Object, e As EventArgs) Handles cmdCheckTricotData.Click
+        Check()
+    End Sub
+
+
+    Private Sub Check()
+        Dim clsPackageCheck As New RFunction
+        Dim expOutput As SymbolicExpression
+        Dim chrOutput As CharacterVector
         Dim strFormNames() As String
         Dim expTemp As SymbolicExpression
 
-        Cursor = Cursors.WaitCursor
-        expTemp = frmMain.clsRLink.RunInternalScriptGetValue(clsOutputLevelsOperator.ToScript(), bSeparateThread:=False, bShowWaitDialogOverride:=False)
-        Cursor = Cursors.Default
-        If expTemp IsNot Nothing Then
-            strFormNames = expTemp.AsCharacter().ToArray()
-            ucrInputTricotData.SetName(String.Join(", ", strFormNames))
+        clsPackageCheck.SetPackageName("instatExtras")
+        clsPackageCheck.SetRCommand("check_data_levels")
+        clsPackageCheck.AddParameter("x", clsRFunctionParameter:=clsOutputDataLevel)
+
+        expOutput = frmMain.clsRLink.RunInternalScriptGetValue(clsPackageCheck.ToScript(), bSilent:=True)
+
+        If expOutput Is Nothing OrElse expOutput.Type = Internals.SymbolicExpressionType.Null Then
+            ucrInputTricotData.SetText("Cannot get data information.")
+            Exit Sub
         End If
+
+        chrOutput = expOutput.AsCharacter
+        If chrOutput.Count < 1 Then
+            ucrInputTricotData.SetText("Cannot get data information.")
+            Exit Sub
+        End If
+        Select Case chrOutput(0)
+            Case "0"
+                bUniqueChecked = False
+                ucrInputTricotData.SetText("Need ID level data to proceed. If you have an ID variable, are they all unique?")
+                ucrInputTricotData.txtInput.BackColor = Color.LightCoral
+            Case "1"
+                bUniqueChecked = False
+                ucrInputTricotData.SetText("Multiple data frames given at ID level. Should only have one data frame at each level.")
+                ucrInputTricotData.txtInput.BackColor = Color.LightCoral
+            Case "2"
+                bUniqueChecked = False
+                ucrInputTricotData.SetText("Tricot Data not found. Try using Options.")
+                ucrInputTricotData.txtInput.BackColor = Color.LightCoral
+            Case Else
+                bUniqueChecked = True
+                Cursor = Cursors.WaitCursor
+                expTemp = frmMain.clsRLink.RunInternalScriptGetValue(clsOutputLevelsOperator.ToScript(), bSeparateThread:=False, bShowWaitDialogOverride:=False)
+                Cursor = Cursors.Default
+                If expTemp IsNot Nothing Then
+                    strFormNames = expTemp.AsCharacter().ToArray()
+                    ucrInputTricotData.SetName(String.Join(", ", strFormNames))
+                End If
+                ucrInputTricotData.txtInput.BackColor = Color.LightGreen
+        End Select
+        TestOKEnabled()
     End Sub
+
 End Class
