4ba37ec977201716def81aaf44a736d6bab5449f
Derrick Agorhom <76208189+derekagorhom@users.noreply.github.com>
2025-04-28 15:26:51 +0200

    Implemented the Transform Tricot Dialog

--- instat/dlgTransformTricotData.Designer.vb
+++ instat/dlgTransformTricotData.Designer.vb
@@ -22,19 +22,99 @@ Partial Class dlgTransformTricotData
     'Ne la modifiez pas à l'aide de l'éditeur de code.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.ucrReceiverTricotData = New instat.ucrReceiverMultiple()
+        Me.cmdCheckTricotData = New System.Windows.Forms.Button()
+        Me.ucrBase = New instat.ucrButtons()
+        Me.ucrInputTricotData = New instat.ucrInputTextBox()
+        Me.cmdOptions = New System.Windows.Forms.Button()
+        Me.ucrSelectorTricotData = New instat.ucrSelectorAddRemove()
         Me.SuspendLayout()
+        '
+        'ucrReceiverTricotData
+        '
+        Me.ucrReceiverTricotData.AutoSize = True
+        Me.ucrReceiverTricotData.frmParent = Me
+        Me.ucrReceiverTricotData.Location = New System.Drawing.Point(292, 52)
+        Me.ucrReceiverTricotData.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverTricotData.Name = "ucrReceiverTricotData"
+        Me.ucrReceiverTricotData.Selector = Nothing
+        Me.ucrReceiverTricotData.Size = New System.Drawing.Size(123, 110)
+        Me.ucrReceiverTricotData.strNcFilePath = ""
+        Me.ucrReceiverTricotData.TabIndex = 6
+        Me.ucrReceiverTricotData.ucrSelector = Nothing
+        '
+        'cmdCheckTricotData
+        '
+        Me.cmdCheckTricotData.Location = New System.Drawing.Point(7, 252)
+        Me.cmdCheckTricotData.Name = "cmdCheckTricotData"
+        Me.cmdCheckTricotData.Size = New System.Drawing.Size(101, 23)
+        Me.cmdCheckTricotData.TabIndex = 7
+        Me.cmdCheckTricotData.Text = "Check:"
+        Me.cmdCheckTricotData.UseVisualStyleBackColor = True
+        '
+        'ucrBase
+        '
+        Me.ucrBase.AutoSize = True
+        Me.ucrBase.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.ucrBase.Location = New System.Drawing.Point(7, 310)
+        Me.ucrBase.Name = "ucrBase"
+        Me.ucrBase.Size = New System.Drawing.Size(408, 52)
+        Me.ucrBase.TabIndex = 8
+        '
+        'ucrInputTricotData
+        '
+        Me.ucrInputTricotData.AddQuotesIfUnrecognised = True
+        Me.ucrInputTricotData.AutoSize = True
+        Me.ucrInputTricotData.IsMultiline = False
+        Me.ucrInputTricotData.IsReadOnly = True
+        Me.ucrInputTricotData.Location = New System.Drawing.Point(116, 254)
+        Me.ucrInputTricotData.Name = "ucrInputTricotData"
+        Me.ucrInputTricotData.Size = New System.Drawing.Size(183, 21)
+        Me.ucrInputTricotData.TabIndex = 9
+        '
+        'cmdOptions
+        '
+        Me.cmdOptions.Location = New System.Drawing.Point(314, 252)
+        Me.cmdOptions.Name = "cmdOptions"
+        Me.cmdOptions.Size = New System.Drawing.Size(101, 23)
+        Me.cmdOptions.TabIndex = 10
+        Me.cmdOptions.Text = "Options:"
+        Me.cmdOptions.UseVisualStyleBackColor = True
+        '
+        'ucrSelectorTricotData
+        '
+        Me.ucrSelectorTricotData.AutoSize = True
+        Me.ucrSelectorTricotData.bShowHiddenColumns = False
+        Me.ucrSelectorTricotData.Location = New System.Drawing.Point(18, 52)
+        Me.ucrSelectorTricotData.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrSelectorTricotData.Name = "ucrSelectorTricotData"
+        Me.ucrSelectorTricotData.Size = New System.Drawing.Size(218, 147)
+        Me.ucrSelectorTricotData.TabIndex = 11
         '
         'dlgTransformTricotData
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(464, 450)
+        Me.ClientSize = New System.Drawing.Size(464, 385)
+        Me.Controls.Add(Me.ucrSelectorTricotData)
+        Me.Controls.Add(Me.cmdOptions)
+        Me.Controls.Add(Me.ucrInputTricotData)
+        Me.Controls.Add(Me.ucrBase)
+        Me.Controls.Add(Me.cmdCheckTricotData)
+        Me.Controls.Add(Me.ucrReceiverTricotData)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.MinimizeBox = False
         Me.Name = "dlgTransformTricotData"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Transform Tricot Data"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents ucrReceiverTricotData As ucrReceiverMultiple
+    Friend WithEvents cmdCheckTricotData As Button
+    Friend WithEvents ucrBase As ucrButtons
+    Friend WithEvents cmdOptions As Button
+    Friend WithEvents ucrInputTricotData As ucrInputTextBox
+    Friend WithEvents ucrSelectorTricotData As ucrSelectorAddRemove
 End Class
--- instat/dlgTransformTricotData.vb
+++ instat/dlgTransformTricotData.vb
@@ -1,3 +1,99 @@
-﻿Public Class dlgTransformTricotData
+﻿' R- Instat
+' Copyright (C) 2015-2017
+'
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+'
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License 
+' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
+Imports instat.Translations
+Imports RDotNet
+
+Public Class dlgTransformTricotData
+    Private clsOutputDataLevel, clsCreateTricotData As New RFunction
+    Private clsOutputLevelsOperator, OverallSymbolOperator As New ROperator
+    Private bFirstLoad As Boolean = True
+    Private bReset As Boolean = True
+
+    Private Sub dlgTransformTricotData_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If bFirstLoad Then
+            InitialiseDialog()
+            bFirstLoad = False
+        End If
+        If bReset Then
+            SetDefaults()
+        End If
+        SetRCodeForControls(bReset)
+    End Sub
+
+    Private Sub InitialiseDialog()
+        ucrReceiverTricotData.SetParameter(New RParameter("data_list", 0))
+        ucrReceiverTricotData.SetParameterIsRFunction()
+        ucrReceiverTricotData.Selector = ucrSelectorTricotData
+        ucrReceiverTricotData.strSelectorHeading = "Data Frames"
+        ucrReceiverTricotData.SetItemType("dataframe")
+    End Sub
+
+    Private Sub SetDefaults()
+        clsOutputDataLevel = New RFunction
+        clsCreateTricotData = New RFunction
+        clsOutputLevelsOperator = New ROperator
+        OverallSymbolOperator = New ROperator
+
+        ucrReceiverTricotData.SetMeAsReceiver()
+        clsOutputDataLevel.SetPackageName("instatExtras")
+        clsOutputDataLevel.SetRCommand("summarise_data_levels")
+        'clsOutputDataLevel.SetAssignTo("output_data_levels")
+
+        clsCreateTricotData.SetPackageName("databook")
+        clsCreateTricotData.SetRCommand("create_tricot_data")
+        clsCreateTricotData.AddParameter("output_data_levels", clsRFunctionParameter:=clsOutputDataLevel)
+
+        clsOutputLevelsOperator.SetOperation("$")
+        clsOutputLevelsOperator.AddParameter("left", clsROperatorParameter:=OverallSymbolOperator, iPosition:=0)
+        clsOutputLevelsOperator.AddParameter("right", "print", iPosition:=1)
+        clsOutputLevelsOperator.bSpaceAroundOperation = False
+
+        OverallSymbolOperator.SetOperation("[")
+        OverallSymbolOperator.AddParameter("left", clsRFunctionParameter:=clsOutputDataLevel, iPosition:=0)
+        OverallSymbolOperator.AddParameter("right", "1,]", iPosition:=1)
+        OverallSymbolOperator.bSpaceAroundOperation = False
+
+        'ucrBase.clsRsyntax.AddToBeforeCodes(clsOutputDataLevel)
+        ucrBase.clsRsyntax.AddToBeforeCodes(clsOutputLevelsOperator)
+        ucrBase.clsRsyntax.SetBaseRFunction(clsCreateTricotData)
+    End Sub
+
+    Private Sub SetRCodeForControls(bReset As Boolean)
+        ucrReceiverTricotData.SetRCode(clsOutputDataLevel, bReset)
+    End Sub
+
+    Private Sub ucrBase_ClickReset(sender As Object, e As EventArgs) Handles ucrBase.ClickReset
+        SetDefaults()
+        SetRCodeForControls(True)
+        'TestOKEnabled()
+    End Sub
+
+    Private Sub cmdFindForms_click(sender As Object, e As EventArgs) Handles cmdCheckTricotData.Click
+        Dim strFormNames() As String
+        Dim expTemp As SymbolicExpression
+
+
+        Cursor = Cursors.WaitCursor
+        expTemp = frmMain.clsRLink.RunInternalScriptGetValue(clsOutputLevelsOperator.ToScript(), bSeparateThread:=False, bShowWaitDialogOverride:=False)
+        Cursor = Cursors.Default
+        If expTemp IsNot Nothing Then
+            strFormNames = expTemp.AsCharacter().ToArray()
+            ucrInputTricotData.SetName(String.Join(", ", strFormNames))
+        End If
+    End Sub
 End Class
