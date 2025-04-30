6e24a9d93011cc7349a5520813ae77ce4f64c959
anastasia-mbithe <anastasiambithe7@gmail.com>
2025-04-30 15:50:43 +0300

    More changes to the code

--- instat/dlgTricotModelOneVarCov.Designer.vb
+++ instat/dlgTricotModelOneVarCov.Designer.vb
@@ -32,7 +32,8 @@ Partial Class dlgTricotModelOneVarCov
         Me.ucrBase = New instat.ucrButtons()
         Me.btnModelOptions = New System.Windows.Forms.Button()
         Me.btnDisplayOptions = New System.Windows.Forms.Button()
-        Me.ucrTryOneVarCov = New instat.ucrTry()
+        Me.cmdCheckVariety = New System.Windows.Forms.Button()
+        Me.ucrInputCheckVariety = New instat.ucrInputTextBox()
         Me.SuspendLayout()
         '
         'ucrSelectorTraitsRanking
@@ -41,10 +42,10 @@ Partial Class dlgTricotModelOneVarCov
         Me.ucrSelectorTraitsRanking.bDropUnusedFilterLevels = False
         Me.ucrSelectorTraitsRanking.bShowHiddenColumns = False
         Me.ucrSelectorTraitsRanking.bUseCurrentFilter = True
-        Me.ucrSelectorTraitsRanking.Location = New System.Drawing.Point(9, 8)
+        Me.ucrSelectorTraitsRanking.Location = New System.Drawing.Point(6, 4)
         Me.ucrSelectorTraitsRanking.Margin = New System.Windows.Forms.Padding(0)
         Me.ucrSelectorTraitsRanking.Name = "ucrSelectorTraitsRanking"
-        Me.ucrSelectorTraitsRanking.Size = New System.Drawing.Size(318, 274)
+        Me.ucrSelectorTraitsRanking.Size = New System.Drawing.Size(213, 183)
         Me.ucrSelectorTraitsRanking.TabIndex = 0
         '
         'ucrSelectorVarietyLevel
@@ -53,21 +54,21 @@ Partial Class dlgTricotModelOneVarCov
         Me.ucrSelectorVarietyLevel.bDropUnusedFilterLevels = False
         Me.ucrSelectorVarietyLevel.bShowHiddenColumns = False
         Me.ucrSelectorVarietyLevel.bUseCurrentFilter = True
-        Me.ucrSelectorVarietyLevel.Location = New System.Drawing.Point(9, 289)
+        Me.ucrSelectorVarietyLevel.Location = New System.Drawing.Point(6, 183)
         Me.ucrSelectorVarietyLevel.Margin = New System.Windows.Forms.Padding(0)
         Me.ucrSelectorVarietyLevel.Name = "ucrSelectorVarietyLevel"
-        Me.ucrSelectorVarietyLevel.Size = New System.Drawing.Size(318, 274)
+        Me.ucrSelectorVarietyLevel.Size = New System.Drawing.Size(213, 183)
         Me.ucrSelectorVarietyLevel.TabIndex = 1
         '
         'ucrTraitsReceiver
         '
         Me.ucrTraitsReceiver.AutoSize = True
         Me.ucrTraitsReceiver.frmParent = Me
-        Me.ucrTraitsReceiver.Location = New System.Drawing.Point(378, 84)
+        Me.ucrTraitsReceiver.Location = New System.Drawing.Point(252, 50)
         Me.ucrTraitsReceiver.Margin = New System.Windows.Forms.Padding(0)
         Me.ucrTraitsReceiver.Name = "ucrTraitsReceiver"
         Me.ucrTraitsReceiver.Selector = Nothing
-        Me.ucrTraitsReceiver.Size = New System.Drawing.Size(269, 198)
+        Me.ucrTraitsReceiver.Size = New System.Drawing.Size(179, 129)
         Me.ucrTraitsReceiver.strNcFilePath = ""
         Me.ucrTraitsReceiver.TabIndex = 2
         Me.ucrTraitsReceiver.ucrSelector = Nothing
@@ -76,11 +77,11 @@ Partial Class dlgTricotModelOneVarCov
         '
         Me.ucrVarietyLevelReceiver.AutoSize = True
         Me.ucrVarietyLevelReceiver.frmParent = Me
-        Me.ucrVarietyLevelReceiver.Location = New System.Drawing.Point(378, 375)
+        Me.ucrVarietyLevelReceiver.Location = New System.Drawing.Point(252, 244)
         Me.ucrVarietyLevelReceiver.Margin = New System.Windows.Forms.Padding(0)
         Me.ucrVarietyLevelReceiver.Name = "ucrVarietyLevelReceiver"
         Me.ucrVarietyLevelReceiver.Selector = Nothing
-        Me.ucrVarietyLevelReceiver.Size = New System.Drawing.Size(269, 30)
+        Me.ucrVarietyLevelReceiver.Size = New System.Drawing.Size(179, 20)
         Me.ucrVarietyLevelReceiver.strNcFilePath = ""
         Me.ucrVarietyLevelReceiver.TabIndex = 3
         Me.ucrVarietyLevelReceiver.ucrSelector = Nothing
@@ -88,74 +89,90 @@ Partial Class dlgTricotModelOneVarCov
         'lblTraitsToModel
         '
         Me.lblTraitsToModel.AutoSize = True
-        Me.lblTraitsToModel.Location = New System.Drawing.Point(374, 43)
+        Me.lblTraitsToModel.Location = New System.Drawing.Point(249, 28)
+        Me.lblTraitsToModel.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.lblTraitsToModel.Name = "lblTraitsToModel"
-        Me.lblTraitsToModel.Size = New System.Drawing.Size(117, 20)
+        Me.lblTraitsToModel.Size = New System.Drawing.Size(80, 13)
         Me.lblTraitsToModel.TabIndex = 4
         Me.lblTraitsToModel.Text = "Traits to Model:"
         '
         'lblExplanatory
         '
         Me.lblExplanatory.AutoSize = True
-        Me.lblExplanatory.Location = New System.Drawing.Point(374, 338)
+        Me.lblExplanatory.Location = New System.Drawing.Point(249, 220)
+        Me.lblExplanatory.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.lblExplanatory.Name = "lblExplanatory"
-        Me.lblExplanatory.Size = New System.Drawing.Size(158, 20)
+        Me.lblExplanatory.Size = New System.Drawing.Size(106, 13)
         Me.lblExplanatory.TabIndex = 5
         Me.lblExplanatory.Text = "Explanatory Variable:"
         '
         'ucrTricOneVarSave
         '
         Me.ucrTricOneVarSave.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
-        Me.ucrTricOneVarSave.Location = New System.Drawing.Point(9, 654)
-        Me.ucrTricOneVarSave.Margin = New System.Windows.Forms.Padding(6, 7, 6, 7)
+        Me.ucrTricOneVarSave.Location = New System.Drawing.Point(6, 406)
+        Me.ucrTricOneVarSave.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.ucrTricOneVarSave.Name = "ucrTricOneVarSave"
-        Me.ucrTricOneVarSave.Size = New System.Drawing.Size(605, 50)
+        Me.ucrTricOneVarSave.Size = New System.Drawing.Size(403, 32)
         Me.ucrTricOneVarSave.TabIndex = 6
         '
         'ucrBase
         '
         Me.ucrBase.AutoSize = True
         Me.ucrBase.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
-        Me.ucrBase.Location = New System.Drawing.Point(9, 712)
-        Me.ucrBase.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.ucrBase.Location = New System.Drawing.Point(6, 441)
+        Me.ucrBase.Margin = New System.Windows.Forms.Padding(4)
         Me.ucrBase.Name = "ucrBase"
-        Me.ucrBase.Size = New System.Drawing.Size(611, 77)
+        Me.ucrBase.Size = New System.Drawing.Size(408, 52)
         Me.ucrBase.TabIndex = 9
         '
         'btnModelOptions
         '
-        Me.btnModelOptions.Location = New System.Drawing.Point(475, 515)
+        Me.btnModelOptions.Location = New System.Drawing.Point(317, 312)
+        Me.btnModelOptions.Margin = New System.Windows.Forms.Padding(2)
         Me.btnModelOptions.Name = "btnModelOptions"
-        Me.btnModelOptions.Size = New System.Drawing.Size(172, 33)
+        Me.btnModelOptions.Size = New System.Drawing.Size(115, 21)
         Me.btnModelOptions.TabIndex = 10
         Me.btnModelOptions.Text = "Model Options"
         Me.btnModelOptions.UseVisualStyleBackColor = True
         '
         'btnDisplayOptions
         '
-        Me.btnDisplayOptions.Location = New System.Drawing.Point(475, 557)
+        Me.btnDisplayOptions.Location = New System.Drawing.Point(317, 337)
+        Me.btnDisplayOptions.Margin = New System.Windows.Forms.Padding(2)
         Me.btnDisplayOptions.Name = "btnDisplayOptions"
-        Me.btnDisplayOptions.Size = New System.Drawing.Size(172, 33)
+        Me.btnDisplayOptions.Size = New System.Drawing.Size(115, 21)
         Me.btnDisplayOptions.TabIndex = 11
         Me.btnDisplayOptions.Text = "Display Options"
         Me.btnDisplayOptions.UseVisualStyleBackColor = True
         '
-        'ucrTryOneVarCov
+        'cmdCheckVariety
         '
-        Me.ucrTryOneVarCov.AutoSize = True
-        Me.ucrTryOneVarCov.Location = New System.Drawing.Point(9, 599)
-        Me.ucrTryOneVarCov.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
-        Me.ucrTryOneVarCov.Name = "ucrTryOneVarCov"
-        Me.ucrTryOneVarCov.RunCommandAsMultipleLines = False
-        Me.ucrTryOneVarCov.Size = New System.Drawing.Size(594, 55)
-        Me.ucrTryOneVarCov.TabIndex = 12
+        Me.cmdCheckVariety.Location = New System.Drawing.Point(6, 378)
+        Me.cmdCheckVariety.Margin = New System.Windows.Forms.Padding(2)
+        Me.cmdCheckVariety.Name = "cmdCheckVariety"
+        Me.cmdCheckVariety.Size = New System.Drawing.Size(113, 21)
+        Me.cmdCheckVariety.TabIndex = 12
+        Me.cmdCheckVariety.Text = "Check Variety"
+        Me.cmdCheckVariety.UseVisualStyleBackColor = True
+        '
+        'ucrInputCheckVariety
+        '
+        Me.ucrInputCheckVariety.AddQuotesIfUnrecognised = True
+        Me.ucrInputCheckVariety.AutoSize = True
+        Me.ucrInputCheckVariety.IsMultiline = False
+        Me.ucrInputCheckVariety.IsReadOnly = False
+        Me.ucrInputCheckVariety.Location = New System.Drawing.Point(124, 378)
+        Me.ucrInputCheckVariety.Name = "ucrInputCheckVariety"
+        Me.ucrInputCheckVariety.Size = New System.Drawing.Size(307, 21)
+        Me.ucrInputCheckVariety.TabIndex = 13
         '
         'dlgTricotModelOneVarCov
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(681, 801)
-        Me.Controls.Add(Me.ucrTryOneVarCov)
+        Me.ClientSize = New System.Drawing.Size(443, 501)
+        Me.Controls.Add(Me.ucrInputCheckVariety)
+        Me.Controls.Add(Me.cmdCheckVariety)
         Me.Controls.Add(Me.btnDisplayOptions)
         Me.Controls.Add(Me.btnModelOptions)
         Me.Controls.Add(Me.ucrBase)
@@ -166,7 +183,7 @@ Partial Class dlgTricotModelOneVarCov
         Me.Controls.Add(Me.ucrTraitsReceiver)
         Me.Controls.Add(Me.ucrSelectorVarietyLevel)
         Me.Controls.Add(Me.ucrSelectorTraitsRanking)
-        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "dlgTricotModelOneVarCov"
         Me.ShowIcon = False
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
@@ -186,5 +203,6 @@ Partial Class dlgTricotModelOneVarCov
     Friend WithEvents ucrBase As ucrButtons
     Friend WithEvents btnDisplayOptions As Button
     Friend WithEvents btnModelOptions As Button
-    Friend WithEvents ucrTryOneVarCov As ucrTry
+    Friend WithEvents cmdCheckVariety As Button
+    Friend WithEvents ucrInputCheckVariety As ucrInputTextBox
 End Class
--- instat/dlgTricotModelOneVarCov.vb
+++ instat/dlgTricotModelOneVarCov.vb
@@ -15,12 +15,18 @@
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 Imports instat.Translations
+Imports RDotNet
 Public Class dlgTricotModelOneVarCov
     Public bFirstLoad As Boolean = True
-    Private clsGetVariablesMetadataFunction, clsVarsFunction As New RFunction
-    Private clsGetObjectFunction, clsGetRankingItemsFunction, clsGetDataFrameFunction, clsGetColumn, clsLevelsFunction, clsFactorsFunction,
-    clsMappingFunction, clsPladmmFunction, clsNamesFunction As New RFunction
-    Private clsObjectOperator, clsTildeOperator, clsTilde2Operator, clsBracketOperator, clsNamesOperator As New ROperator
+    Private bReset As Boolean = True
+    Private bIsUnique As Boolean = True
+    Private clsGetVariablesMetadataFunction As New RFunction
+    Private clsGetObjectFunction, clsGetRankingItemsFunction, clsGetDataFrameFunction, clsGetColumn,
+        clsLevelsFunction, clsFactorsFunction, clsMappingFunction, clsPladmmFunction, clsNamesFunction,
+    clsCheckUniqueFunction As New RFunction
+    Private clsObjectOperator, clsTildeOperator, clsTilde2Operator, clsBracketOperator,
+        clsNamesOperator, clsModelOperator, clsSpaceOperator, clsTilde3Operator As New ROperator
+
 
 
     Private Sub dlgTricotModelOneVarCov_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -28,8 +34,18 @@ Public Class dlgTricotModelOneVarCov
             InitialiseDialog()
             bFirstLoad = False
         End If
+        If bReset Then
+            setDefaults()
+        End If
+
+        SetRcodeForControls(bReset)
+        bReset = False
+        autoTranslate(Me)
+        TestOkEnabled()
     End Sub
     Private Sub InitialiseDialog()
+        ucrBase.clsRsyntax.bExcludeAssignedFunctionOutput = False
+
         ucrSelectorTraitsRanking.SetParameter(New RParameter("data_name", 0))
         ucrSelectorTraitsRanking.SetParameterIsString()
 
@@ -53,6 +69,9 @@ Public Class dlgTricotModelOneVarCov
         ucrTricOneVarSave.SetCheckBoxText("Store Model")
         ucrTricOneVarSave.SetIsComboBox()
         ucrTricOneVarSave.SetAssignToIfUncheckedValue("last_model")
+
+        btnModelOptions.Enabled = False
+        btnDisplayOptions.Enabled = False
     End Sub
 
     Private Sub setDefaults()
@@ -60,7 +79,7 @@ Public Class dlgTricotModelOneVarCov
         clsGetVariablesMetadataFunction = New RFunction
         clsGetObjectFunction = New RFunction
         clsObjectOperator = New ROperator
-        clsVarsFunction = New RFunction
+        clsSpaceOperator = New ROperator
         clsGetRankingItemsFunction = New RFunction
         clsGetColumn = New RFunction
         clsLevelsFunction = New RFunction
@@ -72,8 +91,15 @@ Public Class dlgTricotModelOneVarCov
         clsBracketOperator = New ROperator
         clsNamesFunction = New RFunction
         clsNamesOperator = New ROperator
+        clsModelOperator = New ROperator
+        clsCheckUniqueFunction = New RFunction
+        clsTilde3Operator = New ROperator
 
         ucrTricOneVarSave.Reset()
+        ucrSelectorTraitsRanking.Reset()
+        ucrSelectorVarietyLevel.Reset()
+
+        clsCheckUniqueFunction.SetRCommand("check_variety_data_level")
 
         clsGetVariablesMetadataFunction.SetRCommand(frmMain.clsRLink.strInstatDataObject & "$get_variables_metadata")
         clsGetVariablesMetadataFunction.SetAssignTo("get_index_names")
@@ -86,68 +112,86 @@ Public Class dlgTricotModelOneVarCov
         clsObjectOperator.AddParameter("right", "object", iPosition:=1)
         clsObjectOperator.SetAssignTo("rankings_object")
 
-        clsVarsFunction.SetRCommand("c")
-        clsVarsFunction.SetAssignTo("vars")
+        clsSpaceOperator.SetOperation(" ")
+        clsSpaceOperator.AddParameter("x", ucrTraitsReceiver.GetVariableNames(), iPosition:=0, bIncludeArgumentName:=False)
+        clsSpaceOperator.SetAssignTo("vars")
 
         clsGetRankingItemsFunction.SetRCommand("get_ranking_items")
         clsGetRankingItemsFunction.AddParameter("data", clsRFunctionParameter:=clsGetVariablesMetadataFunction, iPosition:=0)
-        clsGetRankingItemsFunction.AddParameter("vars_to_get", clsRFunctionParameter:=clsVarsFunction, iPosition:=1)
+        clsGetRankingItemsFunction.AddParameter("vars_to_get", clsROperatorParameter:=clsSpaceOperator, iPosition:=1)
         clsGetRankingItemsFunction.AddParameter("index ", Chr(34) & "rankings_index" & Chr(34), iPosition:=2)
         clsGetRankingItemsFunction.AddParameter("data_object", clsROperatorParameter:=clsObjectOperator, iPosition:=1, bIncludeArgumentName:=False)
-        clsGetRankingItemsFunction.SetAssignTo("rankings_objects ")
+        clsGetRankingItemsFunction.SetAssignTo("rankings_object")
 
         clsGetColumn.SetRCommand(frmMain.clsRLink.strInstatDataObject & "$get_columns_from_data")
-        clsGetColumn.AddParameter("col_name", ucrVarietyLevelReceiver.GetVariableNames(), iPosition:=1)
         clsGetColumn.SetAssignTo("column")
 
         clsLevelsFunction.SetRCommand("levels")
-        clsLevelsFunction.AddParameter("x", clsRFunctionParameter:=clsFactorsFunction, iPosition:=0)
+        clsLevelsFunction.AddParameter("x", clsRFunctionParameter:=clsFactorsFunction, iPosition:=0, bIncludeArgumentName:=False)
 
         clsFactorsFunction.SetRCommand("factor")
+        clsFactorsFunction.AddParameter("x", clsRFunctionParameter:=clsGetColumn, iPosition:=0, bIncludeArgumentName:=False)
 
         clsBracketOperator.SetOperation("[")
         clsBracketOperator.AddParameter("left", clsRFunctionParameter:=clsLevelsFunction, iPosition:=0)
-        clsBracketOperator.AddParameter("right", "1,]", iPosition:=1)
+        clsBracketOperator.AddParameter("right", "1]", iPosition:=1)
         clsBracketOperator.SetAssignTo("baseline")
+
+        clsTildeOperator.SetOperation("~")
+        clsTildeOperator.AddParameter("left", "", iPosition:=0, bIncludeArgumentName:=False)
+        clsTildeOperator.AddParameter("right", clsRFunctionParameter:=clsPladmmFunction, iPosition:=1, bIncludeArgumentName:=False)
+
+        clsMappingFunction.SetPackageName("purrr")
+        clsMappingFunction.SetRCommand("map")
+        clsMappingFunction.AddParameter(".x", "rankings_object", iPosition:=0)
+        clsMappingFunction.AddParameter(".f", clsROperatorParameter:=clsTildeOperator, iPosition:=1)
+        clsMappingFunction.SetAssignTo("mod_list")
 
         clsPladmmFunction.SetPackageName("PlackettLuce")
         clsPladmmFunction.SetRCommand("pladmm")
         clsPladmmFunction.AddParameter("x", ".x", iPosition:=0, bIncludeArgumentName:=False)
         clsPladmmFunction.AddParameter("y", clsROperatorParameter:=clsTilde2Operator, iPosition:=1, bIncludeArgumentName:=False)
 
-        clsTildeOperator.SetOperation("~")
-        clsTildeOperator.AddParameter("y", clsRFunctionParameter:=clsPladmmFunction, iPosition:=1)
-
-        clsMappingFunction.SetPackageName("purr")
-        clsMappingFunction.SetRCommand("map")
-        clsMappingFunction.AddParameter(".x", clsRFunctionParameter:=clsGetRankingItemsFunction, iPosition:=0)
-        clsMappingFunction.AddParameter(".f", clsROperatorParameter:=clsTildeOperator, iPosition:=1)
-        clsMappingFunction.SetAssignTo("mod_list")
 
         clsTilde2Operator.SetOperation("~")
-        clsTilde2Operator.AddParameter("y", ucrVarietyLevelReceiver.GetVariableNames(), iPosition:=1, bIncludeArgumentName:=False)
+        clsTilde2Operator.AddParameter("left", "", iPosition:=0, bIncludeArgumentName:=False)
 
         clsNamesFunction.SetRCommand("names")
         clsNamesFunction.AddParameter("x", clsRFunctionParameter:=clsMappingFunction, bIncludeArgumentName:=False)
 
         clsNamesOperator.SetOperation("<-")
         clsNamesOperator.AddParameter("left", clsRFunctionParameter:=clsNamesFunction, iPosition:=0)
-        clsNamesOperator.AddParameter("right", clsRFunctionParameter:=clsVarsFunction, iPosition:=1)
+        clsNamesOperator.AddParameter("right", clsROperatorParameter:=clsSpaceOperator, iPosition:=1)
+
+        clsModelOperator.SetOperation("")
+        clsModelOperator.AddParameter("left", "mod_list", iPosition:=0)
+        clsModelOperator.SetAssignToOutputObject(strRObjectToAssignTo:="last_model",
+                                                  strRObjectTypeLabelToAssignTo:=RObjectTypeLabel.Model,
+                                                  strRObjectFormatToAssignTo:=RObjectFormat.Text,
+                                                  strRDataFrameNameToAddObjectTo:=ucrSelectorVarietyLevel.strCurrentDataFrame,
+                                                  strObjectName:="last_model")
+
+        ucrBase.clsRsyntax.ClearCodes()
+        ucrBase.clsRsyntax.AddToBeforeCodes(clsGetRankingItemsFunction, iPosition:=3)
+        ucrBase.clsRsyntax.AddToBeforeCodes(clsBracketOperator, iPosition:=4)
+        ucrBase.clsRsyntax.AddToBeforeCodes(clsNamesOperator, iPosition:=5)
+        ucrBase.clsRsyntax.SetBaseROperator(clsModelOperator)
 
     End Sub
+
     Private Sub SetRcodeForControls(bReset As Boolean)
         ucrSelectorTraitsRanking.AddAdditionalCodeParameterPair(clsGetVariablesMetadataFunction, ucrSelectorTraitsRanking.GetParameter())
-        ucrSelectorVarietyLevel.AddAdditionalCodeParameterPair(clsGetColumn, ucrSelectorVarietyLevel.GetParameter())
 
         ucrSelectorTraitsRanking.SetRCode(clsGetObjectFunction, bReset)
         ucrSelectorVarietyLevel.SetRCode(clsPladmmFunction, bReset)
-        ucrTraitsReceiver.SetRCode(clsVarsFunction, bReset)
-        ucrVarietyLevelReceiver.SetRCode(clsFactorsFunction, bReset)
+        ucrTraitsReceiver.SetRCode(clsSpaceOperator, bReset)
+        ucrTricOneVarSave.SetRCode(clsModelOperator, bReset)
 
     End Sub
 
     Private Sub TestOkEnabled()
-        If Not ucrTraitsReceiver.IsEmpty AndAlso Not ucrVarietyLevelReceiver.IsEmpty AndAlso ucrTricOneVarSave.IsComplete Then
+        If Not ucrTraitsReceiver.IsEmpty AndAlso Not ucrVarietyLevelReceiver.IsEmpty AndAlso
+            ucrTricOneVarSave.IsComplete AndAlso Not ucrInputCheckVariety.IsEmpty AndAlso bIsUnique = True Then
             ucrBase.OKEnabled(True)
         Else
             ucrBase.OKEnabled(False)
@@ -161,7 +205,48 @@ Public Class dlgTricotModelOneVarCov
         SetRcodeForControls(True)
         TestOkEnabled()
     End Sub
-    Private Sub Controls_ControlContentsChanged(ucrChangedControl As ucrCore) Handles ucrTraitsReceiver.ControlContentsChanged, ucrVarietyLevelReceiver.ControlContentsChanged
+    Private Sub Controls_ControlContentsChanged(ucrChangedControl As ucrCore) Handles ucrTraitsReceiver.ControlContentsChanged, ucrVarietyLevelReceiver.ControlContentsChanged,
+        ucrTricOneVarSave.ControlContentsChanged, ucrSelectorVarietyLevel.ControlContentsChanged
         TestOkEnabled()
+    End Sub
+    Private Sub ucrVarietyLevelReceiver_ControlValueChanged(ucrChangedControl As ucrCore) Handles ucrVarietyLevelReceiver.ControlValueChanged
+        clsTilde2Operator.AddParameter("right", ucrVarietyLevelReceiver.GetVariableNames(bWithQuotes:=False), iPosition:=1, bIncludeArgumentName:=False)
+        clsGetColumn.AddParameter("col_name", ucrVarietyLevelReceiver.GetVariableNames(), iPosition:=1, bIncludeArgumentName:=False)
+
+    End Sub
+    Private Sub cmdCheckVariety_Click(sender As Object, e As EventArgs) Handles cmdCheckVariety.Click
+        Dim iAnyDuplicated As Integer
+
+        Try
+            iAnyDuplicated = frmMain.clsRLink.RunInternalScriptGetValue(clsCheckUniqueFunction.ToScript()).AsInteger(0)
+        Catch ex As Exception
+            iAnyDuplicated = 0
+        End Try
+
+        If iAnyDuplicated = 0 Then
+            ucrInputCheckVariety.SetName("Model fails. There is no variety variable that is Tricot-Defined in this data.")
+            ucrInputCheckVariety.txtInput.BackColor = Color.Coral
+            bIsUnique = False
+        ElseIf iAnyDuplicated = 1 Then
+            ucrInputCheckVariety.SetName("Model fails. No key columns are defined in the dataset.")
+            ucrInputCheckVariety.txtInput.BackColor = Color.Coral
+            bIsUnique = False
+
+        ElseIf iAnyDuplicated = 2 Then
+            ucrInputCheckVariety.SetName("Model fails. Only variety level data can be used for this data. This is data where there is a unique row for each variety given.")
+            ucrInputCheckVariety.txtInput.BackColor = Color.Coral
+            bIsUnique = False
+        Else
+            ucrInputCheckVariety.SetName("Model runs OK.")
+            ucrInputCheckVariety.txtInput.BackColor = Color.LightGreen
+            bIsUnique = True
+        End If
+        TestOkEnabled()
+    End Sub
+
+    Private Sub ucrSelectorVarietyLevel_ControlValueChanged(ucrChangedControl As ucrCore) Handles ucrSelectorVarietyLevel.ControlValueChanged
+        clsGetColumn.AddParameter("x", Chr(34) & ucrSelectorVarietyLevel.strCurrentDataFrame & Chr(34), iPosition:=0, bIncludeArgumentName:=False)
+        clsCheckUniqueFunction.AddParameter("x", Chr(34) & ucrSelectorVarietyLevel.strCurrentDataFrame & Chr(34), iPosition:=0, bIncludeArgumentName:=False)
+
     End Sub
 End Class
