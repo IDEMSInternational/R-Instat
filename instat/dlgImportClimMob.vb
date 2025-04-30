dd5bf2433418b3469e5036cf1378dcfe32eeecce
Armstrong Opondo <157476091+ARMSTRONGOPONDO@users.noreply.github.com>
2025-04-30 13:54:32 +0300

    update

--- instat/dlgImportClimMob.vb
+++ instat/dlgImportClimMob.vb
@@ -87,7 +87,7 @@ Public Class dlgImportfromClimMob
 
         clsFirstOperator.SetOperation("$")
         clsFirstOperator.AddParameter("left", clsRFunctionParameter:=clsProjectsFunction, iPosition:=0)
-        clsFirstOperator.AddParameter("right", "project_id", iPosition:=1)
+        clsFirstOperator.AddParameter("right", "project_code", iPosition:=1)
         clsFirstOperator.bSpaceAroundOperation = False
 
         clsSecondOperator.SetOperation("%>%")
@@ -97,7 +97,7 @@ Public Class dlgImportfromClimMob
         clsSecondOperator.SetAssignTo("user_owner")
 
         clsThirdOperator.SetOperation("==")
-        clsThirdOperator.AddParameter("left", "project_id", iPosition:=0)
+        clsThirdOperator.AddParameter("left", "project_code", iPosition:=0)
         clsThirdOperator.AddParameter("right", Chr(34) & ucrInputChooseForm.GetText & Chr(34), iPosition:=1) ' right = value in the ucrInputBox
         clsThirdOperator.bSpaceAroundOperation = False
 
@@ -163,6 +163,10 @@ Public Class dlgImportfromClimMob
         Else
             clsThirdOperator.AddParameter("right", Chr(34) & ucrInputChooseForm.GetText & Chr(34))
         End If
+    End Sub
+
+    Private Sub ucrBase_Load(sender As Object, e As EventArgs) Handles ucrBase.Load
+
     End Sub
 
     Private Sub cmdChooseFile_Click(sender As Object, e As EventArgs) Handles cmdKey.Click
