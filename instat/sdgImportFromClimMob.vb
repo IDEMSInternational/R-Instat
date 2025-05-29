69fe869939f7c0d1c2c1a041722f2332840eba88
RODGERS <r28081999m@gmail.com>
2025-05-29 09:55:01 +0300

    Updated the Import from climod subdialog

--- instat/sdgImportFromClimMob.vb
+++ instat/sdgImportFromClimMob.vb
@@ -23,6 +23,12 @@ Public Class sdgImportFromClimMob
         autoTranslate(Me)
     End Sub
 
+    Private Sub InitialiseDialog()
+
+        ucrBaseSubdialog.iHelpTopicID = 716
+
+    End Sub
+
     Public Sub Setup(clsNewKeyParameter As RParameter)
         ucrBaseSubdialog.iHelpTopicID = 716
         ucrInputKeyPath.SetParameter(clsNewKeyParameter, 0)
