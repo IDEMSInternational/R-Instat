3534307b05184be7ec1e699c48ff165acdd3c6dc
lilyclements <lily@idems.international>
2025-05-30 12:36:30 +0100

    Adding ucrSelectorByTable

---
+++ instat/ucrSelectorByTable.Designer.vb
@@ -0,0 +1,76 @@
+﻿' R- Instat
+' Copyright (C) 2015-2017
+'
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+'
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License 
+' along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class ucrSelectorByTable
+    Inherits instat.ucrSelector
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ucrAvailableDataFrames = New instat.ucrDataFrame()
+        Me.SuspendLayout()
+        '
+        'lstAvailableVariable
+        '
+        Me.lstAvailableVariable.Dock = System.Windows.Forms.DockStyle.None
+        Me.lstAvailableVariable.Location = New System.Drawing.Point(0, 50)
+        Me.lstAvailableVariable.Margin = New System.Windows.Forms.Padding(0)
+        '
+        'ucrAvailableDataFrames
+        '
+        Me.ucrAvailableDataFrames.AutoSize = True
+        Me.ucrAvailableDataFrames.bDropUnusedFilterLevels = False
+        Me.ucrAvailableDataFrames.bUseCurrentFilter = True
+        Me.ucrAvailableDataFrames.Location = New System.Drawing.Point(0, 0)
+        Me.ucrAvailableDataFrames.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrAvailableDataFrames.Name = "ucrAvailableDataFrames"
+        Me.ucrAvailableDataFrames.Size = New System.Drawing.Size(151, 50)
+        Me.ucrAvailableDataFrames.TabIndex = 84
+        '
+        'ucrSelectorByDataFrame
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+        Me.Controls.Add(Me.ucrAvailableDataFrames)
+        Me.Name = "ucrSelectorByDataFrame"
+        Me.Size = New System.Drawing.Size(151, 180)
+        Me.Controls.SetChildIndex(Me.lstAvailableVariable, 0)
+        Me.Controls.SetChildIndex(Me.ucrAvailableDataFrames, 0)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Public WithEvents ucrAvailableDataFrames As ucrDataFrame
+End Class
---
+++ instat/ucrSelectorByTable.vb
@@ -0,0 +1,166 @@
+﻿' R- Instat
+' Copyright (C) 2015-2017
+'
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+'
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License 
+' along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Imports instat
+
+Public Class ucrSelectorByTable
+
+    '    Public Overrides Sub LoadList()
+    '        If ucrAvailableDataFrames.cboAvailableDataFrames.Text <> "" Then
+    '            MyBase.LoadList()
+    '        Else
+    '            lstAvailableVariable.Clear()
+    '        End If
+    '    End Sub
+
+    '    Private Sub ucrAvailableDataFrames_ControlValueChanged(sender As Object) Handles ucrAvailableDataFrames.ControlValueChanged
+    '        strCurrentDataFrame = ucrAvailableDataFrames.cboAvailableDataFrames.Text
+    '        If CurrentReceiver Is Nothing OrElse CurrentReceiver.bAttachedToPrimaryDataFrame Then
+    '            strPrimaryDataFrame = strCurrentDataFrame
+    '        End If
+    '        LoadList()
+    '        OnDataFrameChanged()
+    '    End Sub
+
+    '    Public Overrides Sub Reset()
+    '        ucrAvailableDataFrames.Reset()
+    '        MyBase.Reset()
+    '    End Sub
+
+    '    Public Overrides Sub SetDataframe(strDataframe As String, Optional bEnableDataframe As Boolean = True, Optional bSilent As Boolean = False)
+    '        bSilentDataFrameChange = bSilent
+    '        ucrAvailableDataFrames.SetDataframe(strDataframe, bEnableDataframe)
+    '    End Sub
+
+    '    Public Overridable Sub SetVariablesEnabled(bEnable As Boolean)
+    '        lstAvailableVariable.Enabled = bEnable
+    '    End Sub
+
+    '    Public Overridable Sub SetVariablesVisible(bVisible As Boolean)
+    '        lstAvailableVariable.Visible = bVisible
+    '    End Sub
+
+    '    Public Property bUseCurrentFilter As Boolean
+    '        Get
+    '            Return ucrAvailableDataFrames.bUseCurrentFilter
+    '        End Get
+    '        Set(bValue As Boolean)
+    '            ucrAvailableDataFrames.bUseCurrentFilter = bValue
+    '        End Set
+    '    End Property
+
+    '    Public Property bDropUnusedFilterLevels As Boolean
+    '        Get
+    '            Return ucrAvailableDataFrames.bDropUnusedFilterLevels
+    '        End Get
+    '        Set(bValue As Boolean)
+    '            ucrAvailableDataFrames.bDropUnusedFilterLevels = bValue
+    '        End Set
+    '    End Property
+
+    '    Public Sub SetParameterIsString()
+    '        ucrAvailableDataFrames.SetParameterIsString()
+    '    End Sub
+
+    '    Public Sub SetParameterIsrfunction()
+    '        ucrAvailableDataFrames.SetParameterIsRFunction()
+    '    End Sub
+
+    '    Private Sub ucrAvailableDataFrames_ControlContentsChanged(ucrChangedControl As ucrCore) Handles ucrAvailableDataFrames.ControlContentsChanged
+    '        OnControlContentsChanged()
+    '    End Sub
+
+    '    Private Sub ucrAvailableDataFrames_ControlValueChanged(ucrChangedControl As ucrCore) Handles ucrAvailableDataFrames.ControlValueChanged
+    '        OnControlValueChanged()
+    '    End Sub
+
+    '    Public Overrides Sub UpdateControl(Optional bReset As Boolean = False, Optional bCloneIfNeeded As Boolean = False)
+    '        MyBase.UpdateControl(bReset, bCloneIfNeeded)
+    '        ucrAvailableDataFrames.UpdateControl(bReset)
+    '    End Sub
+
+    '    Public Overrides Sub SetParameter(clsNewParameter As RParameter, Optional iIndex As Integer = 0)
+    '        If bHasOwnParameter Then
+    '            MyBase.SetParameter(clsNewParameter, iIndex)
+    '        Else
+    '            ucrAvailableDataFrames.SetParameter(clsNewParameter, iIndex)
+    '        End If
+    '    End Sub
+
+    '    Public Overrides Function GetParameterName() As String
+    '        If bHasOwnParameter Then
+    '            Return MyBase.GetParameterName()
+    '        Else
+    '            Return ucrAvailableDataFrames.GetParameterName()
+    '        End If
+    '    End Function
+
+    '    Public Overrides Function IsRDefault() As Boolean
+    '        If bHasOwnParameter Then
+    '            Return MyBase.IsRDefault()
+    '        Else
+    '            Return ucrAvailableDataFrames.IsRDefault()
+    '        End If
+    '    End Function
+
+    '    Public Overrides Sub AddOrRemoveParameter(bAdd As Boolean)
+    '        If bHasOwnParameter Then
+    '            MyBase.AddOrRemoveParameter(bAdd)
+    '        Else
+    '            ucrAvailableDataFrames.AddOrRemoveParameter(bAdd)
+    '        End If
+    '    End Sub
+
+    '    Public Overrides Function GetParameter(Optional iIndex As Integer = 0) As RParameter
+    '        If bHasOwnParameter Then
+    '            Return MyBase.GetParameter()
+    '        Else
+    '            Return ucrAvailableDataFrames.GetParameter()
+    '        End If
+    '    End Function
+
+    '    Public Overrides Sub SetRCode(clsNewCodeStructure As RCodeStructure, Optional bReset As Boolean = False, Optional bUpdate As Boolean = True, Optional bCloneIfNeeded As Boolean = False)
+    '        MyBase.SetRCode(clsNewCodeStructure, bReset, bUpdate, bCloneIfNeeded)
+    '        ucrAvailableDataFrames.SetRCode(clsNewCodeStructure, bReset)
+    '    End Sub
+
+    '    Public Overrides Sub UpdateParameter(clsTempParam As RParameter)
+    '        If bHasOwnParameter Then
+    '            MyBase.UpdateParameter(clsTempParam)
+    '        End If
+    '    End Sub
+
+    '    Public Overrides Sub AddAdditionalCodeParameterPair(clsNewRCode As RCodeStructure, clsNewRParameter As RParameter, Optional iAdditionalPairNo As Integer = -1)
+    '        MyBase.AddAdditionalCodeParameterPair(clsNewRCode, clsNewRParameter, iAdditionalPairNo)
+    '        ucrAvailableDataFrames.AddAdditionalCodeParameterPair(clsNewRCode, clsNewRParameter, iAdditionalPairNo)
+    '    End Sub
+
+    '    Public Sub SetLabelText(strText As String)
+    '        ucrAvailableDataFrames.SetLabelText(strText)
+    '    End Sub
+
+    '    Public Overrides Sub SetPrimaryDataFrameOptions(strNewPrimaryDataFrame As String, bNewOnlyLinkedToPrimaryDataFrames As Boolean, Optional bNewIncludePrimaryDataFrameAsLinked As Boolean = False)
+    '        ucrAvailableDataFrames.SetPrimaryDataFrameOptions(strNewPrimaryDataFrame:=strNewPrimaryDataFrame, bNewOnlyLinkedToPrimaryDataFrames:=bNewOnlyLinkedToPrimaryDataFrames, bNewIncludePrimaryDataFrameAsLinked:=bNewIncludePrimaryDataFrameAsLinked)
+    '    End Sub
+
+    '    Public Function IsEmpty() As Boolean
+    '        If ucrAvailableDataFrames.cboAvailableDataFrames.Text = "" Then
+    '            Return True
+    '        Else
+    '            Return False
+    '        End If
+    '    End Function
+End Class
