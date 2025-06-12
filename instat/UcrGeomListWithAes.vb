0a27cf7136a7715f8b9fa41e61450e10f64a859c
Derrick Agorhom <209739786+Ag-Derek@users.noreply.github.com>
2025-06-12 12:34:42 +0200

    Minor Code changes

--- instat/UcrGeomListWithAes.vb
+++ instat/UcrGeomListWithAes.vb
@@ -116,12 +116,16 @@ Public Class ucrGeomListWithParameters
         ucrChkApplyOnAllLayers.Checked = bApplyAesGlobally
         ucrChkIgnoreGlobalAes.SetRCode(clsGeomFunction, bReset)
         If ucrChkApplyOnAllLayers.Checked AndAlso ucrChkIgnoreGlobalAes.Checked Then
-            MsgBox("Error: Cannot check both 'Apply On All Layers' and 'Ignore Global Aesthetics' as this will remove all aesthetics from this layer. Setting both values to checked.", vbOKOnly)
+            MsgBox("Error: Cannot check both 'Apply On All Layers' and 'Ignore Global Aesthetics' as this will remove all aesthetics from this layer. Setting both values to unchecked.", vbOKOnly)
             ucrChkApplyOnAllLayers.Checked = False
             ucrChkIgnoreGlobalAes.Checked = False
         End If
         bAllowShowGlobalAsLocal = True
-
+        If ucrChkIgnoreGlobalAes.Checked Then
+            ucrChkApplyOnAllLayers.Checked = False
+        Else
+            ucrChkApplyOnAllLayers.Show()
+        End If
         ' This might stay as data frame depends on global/local option and therefore can't be linked to a function
         InitialiseSelectedDataFrame()
 
@@ -234,6 +238,25 @@ Public Class ucrGeomListWithParameters
         End If
         SetParameters()
         SetInterAes()
+        ShowHideControls()
+    End Sub
+
+    Private Sub ShowHideControls()
+        Dim filledCount As Integer = 0
+
+        For Each receiver In lstAesParameterReceivers
+            If Not receiver.IsEmpty Then
+                filledCount += 1
+            End If
+        Next
+
+        If filledCount <= 1 OrElse (lstAesParameterReceivers.Count - filledCount) > 2 Then
+            ucrChkIgnoreGlobalAes.Hide()
+            ucrChkApplyOnAllLayers.Hide()
+        Else
+            ucrChkIgnoreGlobalAes.Show()
+            ucrChkApplyOnAllLayers.Show()
+        End If
     End Sub
 
     Public Function TestForOkEnabled() As Boolean
@@ -299,6 +322,7 @@ Public Class ucrGeomListWithParameters
         ucrChkIgnoreGlobalAes.Show()
 
         SetReceiverColour()
+        ShowHideControls()
     End Sub
 
     Private Sub AddRFunctionParameter(parentFunction As RFunction, paramName As String, paramValue As String)
