8e005400b2c5541f78635d2e9dc2e09432443ec8
anastasia-mbithe <anastasiambithe7@gmail.com>
2025-06-24 17:56:57 +0300

    small change

--- instat/ucrAxes.vb
+++ instat/ucrAxes.vb
@@ -249,14 +249,14 @@ Public Class ucrAxes
         ucrInputPosition.SetDropDownStyleAsNonEditable()
         ucrChkPosition.AddParameterPresentCondition(True, "position")
         ucrChkPosition.AddParameterPresentCondition(False, "position", False)
-        ucrChkPosition.AddToLinkedControls(ucrInputPosition, {True}, bNewLinkedAddRemoveParameter:=True, bNewLinkedHideIfParameterMissing:=True, bNewLinkedChangeToDefaultState:=True, objNewDefaultState:=Nothing)
+        ucrChkPosition.AddToLinkedControls(ucrInputPosition, {True}, bNewLinkedAddRemoveParameter:=True, bNewLinkedHideIfParameterMissing:=True)
 
         ucrChkPositionDiscrete.SetText("Position")
         ucrInputPositionDiscrete.SetParameter(New RParameter("position"))
         ucrInputPositionDiscrete.SetDropDownStyleAsNonEditable()
         ucrChkPositionDiscrete.AddParameterPresentCondition(True, "position")
         ucrChkPositionDiscrete.AddParameterPresentCondition(False, "position", False)
-        ucrChkPositionDiscrete.AddToLinkedControls(ucrInputPositionDiscrete, {True}, bNewLinkedAddRemoveParameter:=True, bNewLinkedHideIfParameterMissing:=True, bNewLinkedChangeToDefaultState:=True, objNewDefaultState:=Nothing)
+        ucrChkPositionDiscrete.AddToLinkedControls(ucrInputPositionDiscrete, {True}, bNewLinkedAddRemoveParameter:=True, bNewLinkedHideIfParameterMissing:=True)
 
         ucrChkExpand.SetText("Expand")
         ucrChkExpand.AddParameterPresentCondition(True, "expand")
@@ -622,8 +622,8 @@ Public Class ucrAxes
 
         If bReset Then
             ucrChkLimitsFrom.SetRCode(clsXYScaleDiscreteFunction, bReset, bCloneIfNeeded:=bCloneIfNeeded)
-            ucrInputPositionDiscrete.SetRCode(clsXYScaleDiscreteFunction, bReset, bCloneIfNeeded:=bCloneIfNeeded)
             ucrChkPositionDiscrete.SetRCode(clsXYScaleDiscreteFunction, bReset, bCloneIfNeeded:=bCloneIfNeeded)
+            ucrInputPositionDiscrete.SetRCode(clsXYScaleDiscreteFunction, bReset, bCloneIfNeeded:=bCloneIfNeeded)
             ucrInputLimitDiscrete.SetRCode(clsXYScaleDiscreteFunction, bReset, bCloneIfNeeded:=bCloneIfNeeded)
             ucrInputBreaksDiscrete.SetRCode(clsXYScaleDiscreteFunction, bReset, bCloneIfNeeded:=bCloneIfNeeded)
             ucrChkDropUnusedLevels.SetRCode(clsXYScaleDiscreteFunction, bReset, bCloneIfNeeded:=bCloneIfNeeded)
@@ -804,8 +804,8 @@ Public Class ucrAxes
         End If
     End Sub
 
-    Private Sub ScalesCheckboxes_ControlValueChanged(ucrChangedControl As ucrCore) Handles ucrChkExpand.ControlValueChanged, ucrChkPosition.ControlValueChanged, ucrChkNaValue.ControlValueChanged, ucrChkTransformation.ControlValueChanged,
-        ucrChkLimitsFrom.ControlValueChanged, ucrChkLongLabels.ControlValueChanged, ucrChkDropUnusedLevels.ControlValueChanged, ucrChkNaValueDiscrete.ControlValueChanged, ucrInputPosition.ControlValueChanged
+    Private Sub ScalesCheckboxes_ControlValueChanged(ucrChangedControl As ucrCore) Handles ucrChkExpand.ControlValueChanged, ucrChkPosition.ControlValueChanged, ucrChkPositionDiscrete.ControlValueChanged, ucrChkNaValue.ControlValueChanged, ucrChkTransformation.ControlValueChanged,
+        ucrChkLimitsFrom.ControlValueChanged, ucrChkLongLabels.ControlValueChanged, ucrChkDropUnusedLevels.ControlValueChanged, ucrChkNaValueDiscrete.ControlValueChanged, ucrInputPosition.ControlValueChanged, ucrInputPositionDiscrete.ControlValueChanged
         AddRemoveScaleFunctions()
         AddRemoveLimits()
         AddRemoveLonglabels()
