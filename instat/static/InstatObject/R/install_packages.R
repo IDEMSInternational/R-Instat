5bdf17e7200c7503a54b714fbf33ffadbc87eb19
Chris Marsh <chris.marsh@idems.international>
2025-06-30 21:43:57 +0100

    updated climdex.pcic and ncdf4.helpers installers

--- instat/static/InstatObject/R/install_packages.R
+++ instat/static/InstatObject/R/install_packages.R
@@ -3,6 +3,8 @@ Sys.setlocale("LC_TIME", "C")
 
 #Install packages from CRAN archive
 install.packages("http://cran.r-project.org/src/contrib/Archive/signmedian.test/signmedian.test_1.5.1.tar.gz", repos=NULL, type="source")
+install.packages("https://cran.r-project.org/src/contrib/Archive/ncdf4.helpers/ncdf4.helpers_0.3-7.tar.gz", repos=NULL, type="source")
+install.packages("https://cran.r-project.org/src/contrib/Archive/climdex.pcic/climdex.pcic_1.1-11.tar.gz", repos=NULL, type="source")
 
 #Install packages from win.binary
 install.packages("stringi", dependencies = FALSE,  repos='https://cloud.r-project.org', type = "win.binary")
@@ -50,10 +52,8 @@ packs <-
     "jsonlite",
     "fitdistrplus",
     "visreg",
-    "climdex.pcic",
     "mosaic",
     "ncdf4",
-    "ncdf4.helpers",
     "RMySQL",
     "DBI",
     "EnvStats",
