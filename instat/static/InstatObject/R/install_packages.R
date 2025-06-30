6fc380294949741e42450d8f89fd11de67acc47e
Chris Marsh <chris.marsh@idems.international>
2025-06-30 22:08:11 +0100

    Update install_packages.R

--- instat/static/InstatObject/R/install_packages.R
+++ instat/static/InstatObject/R/install_packages.R
@@ -3,8 +3,6 @@ Sys.setlocale("LC_TIME", "C")
 
 #Install packages from CRAN archive
 install.packages("http://cran.r-project.org/src/contrib/Archive/signmedian.test/signmedian.test_1.5.1.tar.gz", repos=NULL, type="source")
-install.packages("https://cran.r-project.org/src/contrib/Archive/ncdf4.helpers/ncdf4.helpers_0.3-7.tar.gz", repos=NULL, type="source")
-install.packages("https://cran.r-project.org/src/contrib/Archive/climdex.pcic/climdex.pcic_1.1-11.tar.gz", repos=NULL, type="source")
 
 #Install packages from win.binary
 install.packages("stringi", dependencies = FALSE,  repos='https://cloud.r-project.org', type = "win.binary")
@@ -199,6 +197,8 @@ packs <-
 install.packages(packs, dependencies = TRUE, repos='https://cloud.r-project.org', type="win.binary")
 
 install.packages("https://cran.r-project.org/src/contrib/Archive/getPass/getPass_0.2-2.tar.gz", repos=NULL, type="source")
+install.packages("https://cran.r-project.org/src/contrib/Archive/ncdf4.helpers/ncdf4.helpers_0.3-7.tar.gz", repos=NULL, type="source")
+install.packages("https://cran.r-project.org/src/contrib/Archive/climdex.pcic/climdex.pcic_1.1-11.tar.gz", repos=NULL, type="source")
 
 # Only use internal library
 if (length(.libPaths()) >= 2){
