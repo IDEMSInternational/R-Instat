7a91f73fbd94062dfe2d21f4f5ece578fb8fef95
Vitalis95 <vitaliskwambai@gmail.com>
2026-01-15 20:56:33 +0300

    changes

--- instat/dlgDescribeTwoVariable.vb
+++ instat/dlgDescribeTwoVariable.vb
@@ -32,8 +32,8 @@ Public Class dlgDescribeTwoVariable
     'SUMMARY FUNCTIoNS
     Private clsCombineFrequencyParametersFunction, clsCombineFrequencyColParametersFunction, clsCombineFunction, clsCombineSwapAnova2Table, clsCombineAnova2Function, clsSummariseFunction,
        clsDummyFunction, clsGroupByFunction, clsRAnovaFunction, clsCorrFunction, clsRAnovaTableFunction,
-        clsRCorrelationFunction, clsSkimrFunction, clsSummariesListFunction, clsCombineAnovaFunction, clsMutateFunction, clsAcrossFunction,
-        clsSummaryTableCombineFactorsFunction, clsSummaryTableFunction, clsRAnovaSwapTable2Funtion, clsEverythingFunction, clsReplaceNaFunction,
+        clsRCorrelationFunction, clsSkimrFunction, clsSummariesListFunction, clsCombineAnovaFunction,
+        clsSummaryTableCombineFactorsFunction, clsSummaryTableFunction, clsRAnovaSwapTable2Funtion,
         clsThreeVariableCombineFrequencyParametersFunction, clsPivotWiderFunction, clsMappingFunction, clsMapping2Function, clsRAnovaTable2Function As New RFunction
 
     'FORMAT TABLE FUNCTIONS
@@ -41,7 +41,7 @@ Public Class dlgDescribeTwoVariable
 
     Private clsGroupByPipeOperator, clsSummaryOperator, clsGroupByPipeOperator2, clsGroupByPipeOperator3, clsGroupByPipeOperator4, clsGroupByPipeOperatorData As New ROperator
 
-    Private clsTildOperator, clsMapOperator, clsMutateOperator, clsGtTableROperator, clsPivotOperator As New ROperator
+    Private clsTildOperator, clsMapOperator, clsGtTableROperator, clsPivotOperator As New ROperator
 
     Private clsgtFunction, clsMapSummaryFunction, clsMap2SummaryFunction, clsMapGtFunction As New RFunction
     'Frequency Parameters
@@ -223,10 +223,7 @@ Public Class dlgDescribeTwoVariable
         clsPipeOperator = New ROperator
         clsTabFootnoteOperator = New ROperator
         clsgtFunction = New RFunction
-        clsMutateFunction = New RFunction
-        clsEverythingFunction = New RFunction
-        clsReplaceNaFunction = New RFunction
-        clsAcrossFunction = New RFunction
+
         clsSummaryOperator = New ROperator
         clsMapOperator = New ROperator
         clsPivotWiderFunction = New RFunction
@@ -249,7 +246,6 @@ Public Class dlgDescribeTwoVariable
         clsAnovaSwapTable2Opeator = New ROperator
         clsYlistOperator = New ROperator
         clsYlist2Operator = New ROperator
-        clsMutateOperator = New ROperator
         clsRAnovaTable2Function = New RFunction
         clsRAnovaSwapTable2Funtion = New RFunction
         clsCrossDfFunction = New RFunction
@@ -430,30 +426,8 @@ Public Class dlgDescribeTwoVariable
         clsgtFunction.SetRCommand("generate_summary_tables")
         clsgtFunction.AddParameter("rm_summary_title", "TRUE", iPosition:=0)
 
-        clsEverythingFunction.SetPackageName("dplyr")
-        clsEverythingFunction.SetRCommand("everything")
-
-        clsReplaceNaFunction.SetPackageName("~ tidyr")
-        clsReplaceNaFunction.SetRCommand("replace_na")
-        clsReplaceNaFunction.AddParameter("var", ".x", bIncludeArgumentName:=False, iPosition:=0)
-        clsReplaceNaFunction.AddParameter("x", "0", bIncludeArgumentName:=False, iPosition:=1)
-
-        clsAcrossFunction.SetPackageName("dplyr")
-        clsAcrossFunction.SetRCommand("across")
-        clsAcrossFunction.AddParameter("everything", clsRFunctionParameter:=clsEverythingFunction, bIncludeArgumentName:=False, iPosition:=0)
-        clsAcrossFunction.AddParameter("replace", clsRFunctionParameter:=clsReplaceNaFunction, bIncludeArgumentName:=False, iPosition:=1)
-
-        clsMutateFunction.SetPackageName("dplyr")
-        clsMutateFunction.SetRCommand("mutate")
-        clsMutateFunction.AddParameter("across", clsRFunctionParameter:=clsAcrossFunction, bIncludeArgumentName:=False, iPosition:=0)
-
         clsGtTableROperator.SetOperation("%>%")
         clsGtTableROperator.bBrackets = False
-
-        clsMutateOperator.SetOperation("%>%")
-        clsMutateOperator.bBrackets = False
-        clsMutateOperator.AddParameter("left", clsRFunctionParameter:=clsMutateFunction, iPosition:=0)
-        clsMutateOperator.AddParameter("right", clsROperatorParameter:=clsGtTableROperator, iPosition:=1)
 
         clsSummaryOperator.SetOperation("%>%")
         clsSummaryOperator.AddParameter("data", clsRFunctionParameter:=ucrSelectorDescribeTwoVar.ucrAvailableDataFrames.clsCurrDataFrame, iPosition:=0)
@@ -463,7 +437,7 @@ Public Class dlgDescribeTwoVariable
 
         clsPivotOperator.SetOperation("%>%")
         clsPivotOperator.AddParameter("left", clsRFunctionParameter:=clsPivotWiderFunction)
-        clsPivotOperator.AddParameter("right", clsROperatorParameter:=clsMutateOperator)
+        clsPivotOperator.AddParameter("right", clsROperatorParameter:=clsGtTableROperator)
         clsPivotOperator.bBrackets = False
 
         clsMapOperator.SetOperation("%>%")
