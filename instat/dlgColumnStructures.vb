c186e9138a1e854367c0f68dbbd800201f7648fb
Baya <149116499+bayakarisa@users.noreply.github.com>
2026-05-15 09:40:18 +0300

    bot fixes

--- instat/dlgColumnStructures.vb
+++ instat/dlgColumnStructures.vb
@@ -126,7 +126,8 @@ Public Class dlgColumnStructure
     Private Sub SetColumnStructureInReceiver()
         If ucrSelectorColumnStructure.ucrAvailableDataFrames.cboAvailableDataFrames.Text <> "" Then
             Dim strDataName As String = ucrSelectorColumnStructure.ucrAvailableDataFrames.cboAvailableDataFrames.Text
-            Dim currentColumns As List(Of String) = frmMain.clsRLink.GetColumnNames(strDataName)
+            'Dim currentColumns As List(Of String) = frmMain.clsRLink.GetColumnNames(strDataName)
+            Dim currentColumns As New HashSet(Of String)(frmMain.clsRLink.GetColumnNames(strDataName))
 
             AddValidColumnsToReceiver(ucrReceiverLayout, strDataName, currentColumns, "Structure", {"structure_type_1_label"})
             AddValidColumnsToReceiver(ucrReceiverTreatment, strDataName, currentColumns, "Structure", {"structure_type_2_label"})
@@ -136,7 +137,7 @@ Public Class dlgColumnStructure
         End If
     End Sub
 
-    Private Sub AddValidColumnsToReceiver(receiver As ucrReceiver, strDataName As String, currentColumns As List(Of String), strProperty As String, strValues As String())
+    Private Sub AddValidColumnsToReceiver(receiver As ucrReceiver, strDataName As String, currentColumns As HashSet(Of String), strProperty As String, strValues As String())
         ' Get columns with the metadata property from R
         Dim clsGetItems As New RFunction
         Dim clsIncludeList As New RFunction
@@ -168,7 +169,8 @@ Public Class dlgColumnStructure
                 End If
             Next
         End If
-        DirectCast(receiver, ucrReceiverMultiple).AddMultiple(lstItems)
+        'DirectCast(receiver, ucrReceiverMultiple).AddMultiple(lstItems)
+        DirectCast(receiver, ucrReceiverMultiple).AddMultiple(lstItems.ToArray())
     End Sub
 
     Private Sub ucrSelectorColumnStructure_ControlContentsChanged(ucrChangedControl As ucrCore) Handles ucrSelectorColumnStructure.ControlContentsChanged
