c9ed82508cdf37772b324d2e7addea5b1e228321
Emmanuel-Afrifa <emmaquame9@gmail.com>
2026-05-22 15:36:04 +0100

    Made small changes

--- instat/dlgStartofRains.vb
+++ instat/dlgStartofRains.vb
@@ -238,6 +238,7 @@ Public Class dlgStartofRains
         SetReceiver()
         AdditionalCondition()
         ChangeDSValue()
+        AddStartRainsDefinitionParams()
     End Sub
 
     Private Sub SetDefaults()
