891c8e00e9df36c0537989b3ac7715e775039686
Emmanuel-Afrifa <emmaquame9@gmail.com>
2026-07-09 11:23:50 +0100

    Small changes

--- instat/dlgExportClimaticDefinitions.vb
+++ instat/dlgExportClimaticDefinitions.vb
@@ -14,7 +14,6 @@
 ' You should have received a copy of the GNU General Public License 
 ' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-Imports System.Diagnostics
 Imports instat.Translations
 Public Class dlgExportClimaticDefinitions
     Private bFirstLoad As Boolean = True
