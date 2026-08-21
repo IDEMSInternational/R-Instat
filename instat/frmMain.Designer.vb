82d3a6c61d29b4a7d8b0939fd1aaf322ff9eb738
Baya <149116499+bayakarisa@users.noreply.github.com>
2026-08-21 11:44:33 +0300

    Add Multiple Mean Comparison dialog using biometryassist (#10475)
    Also fixes partly #9751

--- instat/frmMain.Designer.vb
+++ instat/frmMain.Designer.vb
@@ -5317,7 +5317,6 @@ Partial Class frmMain
         '
         'mnuMultipleComparisons
         '
-        Me.mnuMultipleComparisons.Enabled = False
         Me.mnuMultipleComparisons.Name = "mnuMultipleComparisons"
         Me.mnuMultipleComparisons.Size = New System.Drawing.Size(200, 22)
         Me.mnuMultipleComparisons.Text = "Multiple Comparisons..."
